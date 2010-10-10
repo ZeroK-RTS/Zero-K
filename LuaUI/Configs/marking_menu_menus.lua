434fd4f9cb87ea4a969d503292f544cd32bd1972
KingRaptor <kingraptorowns@gmail.com>
2010-10-10 15:16:29 +0000

    Updated marking menu.
    svn path=/trunk/mods/zk/; revision=83

--- LuaUI/Configs/marking_menu_menus.lua
+++ LuaUI/Configs/marking_menu_menus.lua
@@ -7,39 +7,27 @@ local menu_corcsa = include("Configs/marking_menu_menu_corcsa.lua")
 
 
 local menu_use = {
-  armbeaver = menu_armcom,
   armca = menu_armcom,
-  armch = menu_armcom,
-  armcombattle2 = menu_armcom,
-  armcombuild2 = menu_armcom,
-  armcomdgun2 = menu_armcom,
-  armcomdgun = menu_armcom,
-  armcom = menu_armcom,
-  armcs = menu_armcom,
-  armdecom = menu_armcom,
   armrectr = menu_armcom,
   arm_spider = menu_armcom,
-  consul = menu_armcom,
-  pioneer = menu_armcom,
 
-  armcsa = menu_armcsa,
+  coracv = menu_corcom,
+  corch = menu_armcom,
+  corcs = menu_armcom,
+  corfast = menu_armcom,
+  cornecro = menu_armcom,
+  corned = menu_armcom,
   
-  corcom = menu_corcom,
-  coracv =menu_corcom,
-  corca = menu_corcom,
-  corch = menu_corcom,
-  corcombattle2 = menu_corcom,
-  corcombuild2 = menu_corcom,
-  corcomdgun2 = menu_corcom,
-  corcomdgun = menu_corcom,
-  corcs = menu_corcom,
-  cordecom = menu_corcom,
-  corfast = menu_corcom,
-  cornecro = menu_corcom,
-  corned = menu_corcom,
-  pinchy = menu_corcom,
+  armcsa = menu_armcsa
   
-  corcsa = menu_corcsa,
+  armcom = menu_armcom,
+  corcom = menu_armcom,
+  commrecon = menu_armcom,
+  commsupport = menu_armcom,
+  armadvcom = menu_armcom,
+  coradvcom = menu_armcom,
+  commadvrecon = menu_armcom,
+  commadvsupport = menu_armcom,
 }
 
 -- override menus for 1 faction
@@ -47,9 +35,7 @@ if Game.modShortName == "CA1f" then
   for i,v in pairs(menu_use) do
     menu_use[i] = menu_armcom
   end
-  menu_use.armcsa = menu_armcsa
-  menu_use.commrecon = menu_armcom
-  menu_use.commsupport = menu_armcom
+
 end
 
 return menu_use
