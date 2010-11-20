9dfe85a0f5421a1a506f0dfae9ef5bbd4756efca
GoogleFrog <googl3frog@gmail.com>
2010-11-20 12:00:20 +0000

    Set resource bars to "noob mode = false" by default.
    Expense greater than income may confuse some noobs but only show expense confuses EVERYONE.
    
    svn path=/trunk/mods/zk/; revision=673

--- LuaUI/Widgets/gui_chili_resource_bars.lua
+++ LuaUI/Widgets/gui_chili_resource_bars.lua
@@ -90,7 +90,7 @@ end
 
 options = { 
   eexcessflashalways = {name='Always Flash On Energy Excess', type='bool', value=false},
-  onlyShowExpense = {name='Only Show Expense', type='bool', value=true},
+  onlyShowExpense = {name='Only Show Expense', type='bool', value=false},
   workerUsage = {name = "Show Worker Usage", type = "bool", value=false, OnChange = option_workerUsageUpdate},
   energyFlash = {name = "Energy Stall Flash", type = "number", value=0.1, min=0,max=1,step=0.02},
   opacity = {
