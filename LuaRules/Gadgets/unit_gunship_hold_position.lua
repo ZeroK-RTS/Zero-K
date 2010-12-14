f018d3de389bc7503661e1a888a68d1b49500d21
GoogleFrog <googl3frog@gmail.com>
2010-12-14 16:39:25 +0000

    Added gunship hold position as it was removed in the engine a while ago and I really liked it. Basically it is exactly like a Tactical AI that you cannot turn off but coded in reverse. I removed strafe instead of adding it.
    Exactly the same UI as tactical AI. If you set gunships to hold fire and it is out of orders it will not stray from it's position. If you give it an explicit attack order it will start strafing again.
    
    The engine was changed such that units on hold position still automatically acquired targets, this is so that BA gunships (with no tolerance) can fire and S44 inf doesn't require a lot of facing micro. Unfortunately as soon as a gunship automatically acquires a target it automatically strafes.
    
    svn path=/trunk/mods/zk/; revision=965

---
+++ LuaRules/Gadgets/unit_gunship_hold_position.lua
@@ -0,0 +1,121 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function gadget:GetInfo()
+   return {
+      name      = "Gunship Hold Position",
+      desc      = "Makes gunships actually hold position when told to do so",
+      author    = "Google Frog",
+      date      = "15 Dec 2010",
+      license   = "GNU GPL, v2 or later",
+      layer     = 0,
+      enabled   = true  
+   }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+--SYNCED
+if (not gadgetHandler:IsSyncedCode()) then
+   return false
+end
+
+local CMD_REPEAT = CMD.REPEAT
+local CMD_MOVE = CMD.MOVE
+local CMD_SET_WANTED_MAX_SPEED = CMD.SET_WANTED_MAX_SPEED
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local holdUnits = {
+	[UnitDefNames["armkam"].id] = true,
+	[UnitDefNames["bladew"].id] = true,
+	[UnitDefNames["corape"].id] = true,
+	[UnitDefNames["armbrawl"].id] = true,
+	[UnitDefNames["blackdawn"].id] = true,
+	[UnitDefNames["corcrw"].id] = true,
+}
+
+local units = {}
+local unitsByIndex = {count = 0, units = {}}
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local function removeUnit(unitID)
+	if units[unitID].index ~= unitsByIndex.count then
+		unitsByIndex.units[units[unitID].index] = unitsByIndex.units[unitsByIndex.count]
+		units[unitsByIndex.units[unitsByIndex.count]].index = units[unitID].index
+		unitsByIndex.units[unitsByIndex.count] = nil
+	end
+	unitsByIndex.count = unitsByIndex.count - 1
+	units[unitID] = nil
+end
+
+local function addUnit(unitID)
+	local x,y,z = Spring.GetUnitPosition(unitID)
+	unitsByIndex.count = unitsByIndex.count + 1
+	unitsByIndex.units[unitsByIndex.count] = unitID
+	units[unitID] = {re = re, x = x, y = y, z = z, index = unitsByIndex.count}
+end
+
+
+function gadget:UnitCmdDone(unitID, unitDefID, unitTeam, cmdID, cmdTag)
+	if holdUnits[unitDefID] then
+		local cmd = Spring.GetCommandQueue(unitID)
+		local movestate = Spring.GetUnitStates(unitID)["movestate"]
+		if (not units[unitID]) and #cmd == 0 and movestate == 0 then
+			addUnit(unitID)
+		end
+	end
+
+end
+
+function gadget:GameFrame(n)
+
+	if n%6 == 3 and unitsByIndex.count ~= 0 then
+		local i = 1
+		while i <= unitsByIndex.count do
+			local unitID = unitsByIndex.units[i]
+			if Spring.ValidUnitID(unitID) then
+			
+				local cx, cy, cz = units[unitID].x, units[unitID].y, units[unitID].z
+				
+				local ux,uy,uz = Spring.GetUnitPosition(unitID)
+				if (cx-ux)^2 + (cz-uz)^2 > 20^2 then
+					Spring.SetUnitMoveGoal(unitID,cx,cy,cz)
+				end
+
+				i = i + 1
+			else
+				removeUnit(UnitID)
+			end
+		end
+
+	end
+	
+end
+
+function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
+
+	if holdUnits[unitDefID] then
+		if units[unitID] then
+			if not (cmdID == CMD.STOP or cmdID == CMD.FIRE_STATE or cmdID == CMD.SELFD or cmdID == CMD.ONOFF or 
+				cmdID == CMD.CLOAK or cmdID == CMD.REPEAT or cmdID == CMDTYPE.ICON_MODE or cmdID == CMDTYPE.ICON_MODE) then
+				removeUnit(unitID)
+			end
+		else
+			if cmdID == CMD.MOVE_STATE and cmdParams[1] == 0 then
+				local cmd = Spring.GetCommandQueue(unitID)
+				if #cmd == 0 then
+					addUnit(unitID)
+				end
+			end
+		end
+	end
+	
+	return true
+end
+
+
