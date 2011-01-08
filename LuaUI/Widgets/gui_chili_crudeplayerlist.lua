6faa9be5a2cf620491467c594a9d1813b970ce83
wagonrepairer <wagonrepairer@gmail.com>
2011-01-08 01:27:28 +0000

    Added crude player list widget. Shows player names with team numbers, and that's all. First and final version.
    svn path=/trunk/mods/zk/; revision=1226

---
+++ LuaUI/Widgets/gui_chili_crudeplayerlist.lua
@@ -0,0 +1,126 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function widget:GetInfo()
+  return {
+    name      = "Chili Crude Player List",
+    desc      = "v1.00000 Chili Crude Player List.",
+    author    = "CarRepairer",
+    date      = "2011-01-06",
+    license   = "GNU GPL, v2 or later",
+    layer     = 50,
+    enabled   = true,
+  }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+local spSendCommands			= Spring.SendCommands
+
+local echo = Spring.Echo
+
+local Chili
+local Button
+local Window
+local ScrollPanel
+local StackPanel
+local Label
+local screen0
+local color2incolor
+local incolor2color
+local myName -- my console name
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local window_cpl
+
+local colorNames = {}
+local colors = {}
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+
+local function SetupPlayerNames()
+	window_cpl:ClearChildren()
+	
+	local playerroster = Spring.GetPlayerList()
+	
+	myName = Spring.GetPlayerInfo(Spring.GetMyPlayerID())
+	
+	for i,v in ipairs(playerroster) do
+		local name,_,_,teamID = Spring.GetPlayerInfo(playerroster[i])
+		
+		window_cpl:AddChild(
+			Label:New{
+				y=12*(i-1),
+				caption = teamID .. ') ' .. name,
+				textColor = {Spring.GetTeamColor(teamID)},
+			}
+		)
+	end
+	
+	
+end
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+
+
+function widget:Shutdown()
+end
+
+local timer = 0
+function widget:Update(s)
+	timer = timer + s
+	if timer > 5 then
+		timer = 0
+		SetupPlayerNames()
+	end
+end
+
+-----------------------------------------------------------------------
+
+function widget:Initialize()
+	if (not WG.Chili) then
+		widgetHandler:RemoveWidget()
+		return
+	end
+
+	Chili = WG.Chili
+	Button = Chili.Button
+	Window = Chili.Window
+	ScrollPanel = Chili.ScrollPanel
+	StackPanel = Chili.StackPanel
+	Label = Chili.Label
+	screen0 = Chili.Screen0
+	color2incolor = Chili.color2incolor
+	incolor2color = Chili.incolor2color
+	
+	
+	window_cpl = Window:New{  
+		--margin = {2,2,2,2},
+		--padding = {2,2,2,2},
+		dockable = true,
+		name = "crudeplayerlist",
+		x = 300,  
+		y = 0,
+		width  = 350,
+		height = 250,
+		autosize   = true;
+		parent = screen0,
+		draggable = false,
+		resizable = false,
+		tweakDraggable = true,
+		tweakResizable = true,
+		minimumSize = {MIN_WIDTH, MIN_HEIGHT},
+		children = {
+		},
+	}
+	
+	SetupPlayerNames()
+
+end
