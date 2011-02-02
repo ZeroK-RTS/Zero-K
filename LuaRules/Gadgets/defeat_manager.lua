b6300f1ea49b039785aee072716d5896754ff520
GoogleFrog <googl3frog@gmail.com>
2011-02-02 14:39:11 +0000

    Reverted stay on team as /take seems to be hardcoded.
    svn path=/trunk/mods/zk/; revision=1519

--- LuaRules/Gadgets/defeat_manager.lua
+++ LuaRules/Gadgets/defeat_manager.lua
@@ -9,7 +9,7 @@ function gadget:GetInfo()
     date      = "Feb 3, 2010",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
