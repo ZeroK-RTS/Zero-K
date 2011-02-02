6c11119986834ffab809e1f9e4ec0e5db0596c27
GoogleFrog <googl3frog@gmail.com>
2011-02-02 13:21:52 +0000

    Min buildDistance back to what it used to be in the engine as there is no real reason for us to change ours.
    svn path=/trunk/mods/zk/; revision=1510

--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -370,6 +370,17 @@ for name, ud in pairs(UnitDefs) do
 	end
 end
 --]]
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Min Build Range back to what it used to be
+-- 
+for name, ud in pairs(UnitDefs) do
+	if ud.builddistance and ud.builddistance < 128 and name ~= "armasp" then
+		ud.builddistance = 128 
+	end
+end
+
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 --  No leveling ground
