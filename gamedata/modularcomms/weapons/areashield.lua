bbe9f931ddf5d517fb458c3bea8d774bdf8ba9b1
KingRaptor <kingraptorowns@gmail.com>
2011-02-11 09:35:43 +0000

    Added missing area shield weapondef. Test icons for heatray and riot cannon (not too fond of this one).
    svn path=/trunk/mods/zk/; revision=1714

---
+++ gamedata/modularcomms/weapons/areashield.lua
@@ -0,0 +1,35 @@
+local name = "commweapon_areashield"
+local weaponDef = {
+    name                    = [[Energy Shield]],
+    craterMult              = 0,
+
+	customParams			= {
+		slot = [[2]],
+	},
+	
+    damage                  = {
+      default = 10,
+    },
+
+    exteriorShield          = true,
+    impulseFactor           = 0,
+    interceptedByShieldType = 1,
+    isShield                = true,
+    shieldAlpha             = 0.4,
+    shieldBadColor          = [[1 0.1 0.1]],
+    shieldGoodColor         = [[0.1 0.1 1]],
+    shieldInterceptType     = 3,
+    shieldPower             = 3500,
+    shieldPowerRegen        = 60,
+    shieldPowerRegenEnergy  = 6,
+    shieldRadius            = 300,
+    shieldRepulser          = false,
+    smartShield             = true,
+    texture1                = [[wakelarge]],
+    visibleShield           = true,
+    visibleShieldHitFrames  = 4,
+    visibleShieldRepulse    = true,
+    weaponType              = [[Shield]],
+}
+
+return name, weaponDef
