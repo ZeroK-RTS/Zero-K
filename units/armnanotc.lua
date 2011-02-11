5bd5b5fe3c98d8d0901b89e95e7160a4f9ac2bbf
3rexcit <3rexcit@gmail.com>
2011-02-11 15:54:46 +0000

    *translations
    svn path=/trunk/mods/zk/; revision=1715

--- units/armnanotc.lua
+++ units/armnanotc.lua
@@ -32,7 +32,9 @@ unitDef = {
   corpse                        = [[DEAD]],
 
   customParams                  = {
-    helptext = [[The most cost-effective source of buildpower, the Caretaker is ideal for increasing the output of factories or automatically repairing nearby defenses. It cannot start any construction projects itself, however.]],
+    helptext 		= [[The most cost-effective source of buildpower, the Caretaker is ideal for increasing the output of factories or automatically repairing nearby defenses. It cannot start any construction projects itself, however.]],
+	helptext_de 	= [[Die effektivste Quelle an Baukraft: der Caretake ist ideal, um die Produktionsrate von Fabriken zu erhöhen oder nahegelegene Verteidigungsanlagen zu reparieren. Der Caretaker kann aber selbst keine neuen Bauten beginnen.]],
+	description_de 	= [[Nanoturm, Baut mit 12 M/s]],
   },
 
   defaultmissiontype            = [[Standby]],
