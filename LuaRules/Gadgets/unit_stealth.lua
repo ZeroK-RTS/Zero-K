6f389f0d4c55d96a9f1e831f2f8a42aa76cb9b0f
GoogleFrog <googl3frog@gmail.com>
2011-02-23 14:40:00 +0000

    Disabled stealth gadget it's only purpose currently is to create red spiky circles that confuse people.
    svn path=/trunk/mods/zk/; revision=1828

--- LuaRules/Gadgets/unit_stealth.lua
+++ LuaRules/Gadgets/unit_stealth.lua
@@ -22,7 +22,7 @@ function gadget:GetInfo()
     date      = "May 02, 2007",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
