fb31f1e2b2799aae2216fb739026a18e60c4afb6
wagonrepairer <wagonrepairer@gmail.com>
2011-03-02 05:56:39 +0000

    Added a widget that alerts the user if a key is held down more than 10 seconds. Incomplete - doesn't yet alert on alt-tab bug.
    svn path=/trunk/mods/zk/; revision=1858

---
+++ LuaUI/Widgets/dbg_stuckkeys.lua
@@ -0,0 +1,60 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function widget:GetInfo()
+  return {
+    name      = "Stuck Keys",
+    desc      = "v0.01 Alerts user when a key is stuck.",
+    author    = "CarRepairer",
+    date      = "2011-03-01",
+    license   = "GNU GPL, v2 or later",
+    layer     = 1,
+    enabled   = true,
+  }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local echo = Spring.Echo
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+include("keysym.h.lua")
+local keysyms = {}
+for k,v in pairs(KEYSYMS) do
+	keysyms[v] = k
+end
+
+
+local keys = {}
+local cycle = 1
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function widget:KeyPress(key, modifier, isRepeat)
+	if not keys[key] then
+		keys[key] = 10
+	end
+end
+function widget:KeyRelease(key)
+	keys[key] = nil
+end
+
+function widget:Update()
+	cycle = (cycle + 1) % 32
+	
+	if cycle == 1 then
+		for key, time in pairs(keys) do
+			keys[key] = time - 1
+			if keys[key] < 0 then
+				echo( 'The key "' .. keysyms[key] .. '" has been pressed for over 10 seconds. It might be stuck, try tapping it.' )
+				keys[key] = 10
+			end
+		end
+	end
+end
+
+
+--------------------------------------------------------------------------------
