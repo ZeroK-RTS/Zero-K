6d5537fa44ce8da39e81310381981d2747604e25
GoogleFrog <googl3frog@gmail.com>
2011-03-12 02:33:15 +0000

    Fixed Lucky's UI. Show all commands has a menu option for only show all when shift is held.
    svn path=/trunk/mods/zk/; revision=1939

--- LuaUI/Widgets/unit_showallcommands.lua
+++ LuaUI/Widgets/unit_showallcommands.lua
@@ -19,55 +19,56 @@ local spGetAllUnits = Spring.GetAllUnits
 
 local drawUnits = {}
 
+options_path = 'Settings/View/Show All Commands'
+options_order = { 'showonlyonshift'}
+options = {
+	
+	showonlyonshift = {
+		name = 'Show only on shift',
+		type = 'bool',
+		value = false,
+		--OnChange = function() Spring.SendCommands{'showhealthbars'} end,
+	},
+}
+
 function widget:DrawWorld()
- 
-  if not Spring.IsGUIHidden() then 
-    for i, v in pairs(drawUnits) do
-      if i then
-	    spDrawUnitCommands(i)
-      end
-    end
-  end
+	if not Spring.IsGUIHidden() and (not options.showonlyonshift.value or select(4,Spring.GetModKeyState())) then 
+		for i, v in pairs(drawUnits) do
+			if i then
+				spDrawUnitCommands(i)
+			end
+		end
+	end
 end
 
 function widget:UnitCreated(unitID)
-
-  drawUnits[unitID] = true
-
+	drawUnits[unitID] = true
 end
 
 function widget:UnitGiven(unitID, unitDefID, oldTeam, newTeam)
-
-  drawUnits[unitID] = true
-
+	drawUnits[unitID] = true
 end
 
 function widget:UnitDestroyed(unitID)
-
-  if (drawUnits[unitID]) then
-    drawUnits[unitID] = nil
-  end
-
+	if (drawUnits[unitID]) then
+		drawUnits[unitID] = nil
+	end
 end
 
 function widget:GameFrame(n)
-  if (n == 1) then
+	if (n == 1) then
+		local units = spGetAllUnits()
   
-    local units = spGetAllUnits()
-  
-    for i, id in pairs(units) do
-      widget:UnitCreated(id)
-    end
-  end
+		for i, id in pairs(units) do
+			widget:UnitCreated(id)
+		end
+	end
 end
 
 function widget:Initialize()
-
-  local units = spGetAllUnits()
-  
-  for i, id in pairs(units) do
-    widget:UnitCreated(id)
-  end
-
+	local units = spGetAllUnits()
+	for i, id in pairs(units) do
+		widget:UnitCreated(id)
+	end
 end
 
