a8f0278505a64cf8d10e407d734eb2496fd92f9c
kiss2cool <kiss2cool@gmail.com>
2011-03-12 02:25:55 +0000

    Updated logic to avoid retreat activated on units being built.
    svn path=/trunk/mods/zk/; revision=1938

--- LuaUI/Widgets/unit_auto_set_retreat60.lua
+++ LuaUI/Widgets/unit_auto_set_retreat60.lua
@@ -13,15 +13,15 @@ function widget:GetInfo()
 end
 --[[
 Features:
-_ Set every units exiting from factories to retreat @60% automatically. Don't trigger auto retreat.
-_ Set Commander to same value @ game start.
-_ Set everything to 60% when reloaded (/luaui reload) as a safety.
+_ Set every units exiting from factories to retreat @60% automatically. Don't trigger auto retreat for units being built.
+_ Should set Commander to same value @ game start.
 _ Is disabled when player go spec/use replay. NEED TESTING TO KNOW IF WIDGET STOPS (and need to be reactivated) WHEN RE-JOINING GAME AFTER CRASH !!
 
 -- to do : probably fusion with unit_news.lua.
 
 Changelog:
--- versus666, 			v1.3	(07jan2011)	: changed detection logic to avoid to triger auto retreat for units being built. Doesn't check
+-- versus666,			v1.3b	(01mar2011)	: fixed logic.
+-- versus666, 			v1.3	(07jan2011)	: changed detection logic to avoid to triger auto retreat for units being built. 
 -- versus666, 			v1.2	(01nov2010)	: some changes.
 -- versus666, 			v1.1	(18oct2010)	: added debug mode and optimised.
 -- versus666, 			v1.0	(17oct2010)	: creation.
@@ -37,6 +37,8 @@ local spGetMyTeamID		= Spring.GetMyTeamID
 local spGetTeamUnits	= Spring.GetTeamUnits
 local spGetUnitDefID	= Spring.GetUnitDefID
 local spGetUnitTeam		= Spring.GetUnitTeam
+local spGetUnitHealth	= Spring.GetUnitHealth
+
 local debug = false --generates debug message
 
 --------------------------------------------------------------------------------
@@ -51,49 +53,56 @@ end
 function widget:Initialize()
 	MyTeam = spGetMyTeamID()
 	if (IsSpec()) then return false end
-	-- init already present units
-	for _,unitID in ipairs(spGetTeamUnits(spGetMyTeamID())) do
+	-- init already present units, probably superfluous if CREATED is checked too
+--[[	for _,unitID in ipairs(spGetTeamUnits(spGetMyTeamID())) do
 		local unitDefID = spGetUnitDefID(unitID)
-		local ud = UnitDefs[unitDefID]
-		printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID " .. unitID .. " presence detected !")
+		--local ud = UnitDefs[unitDefID]
+		--printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID " .. unitID .. " presence detected !")
 		checkUnits(unitID,unitDefID)
 	end
+	--]]
 end
 
-function checkUnits(unitID,unitDefID)
-	local ud = UnitDefs[unitDefID]
-	printDebug("<unit_auto_retreat60>: checking " .. ud.humanName .. " with ID " .. unitID .. " in team " .. spGetUnitTeam(unitID))
-	if ((spGetUnitTeam(unitID) == MyTeam) and ((ud ~= nil) and not (ud.isBuilding or not ud.canMove))) then -- test if exist and (is mobile and not a building) [I know buildings can't move but you never know]
+function checkUnits(unitID,unitDefID,unitTeam)
+	--local ud = UnitDefs[unitDefID]
+	--printDebug("<unit_auto_retreat60>: checking " .. " with ID " .. unitID .. " in team " .. unitTeam)
+	if ((unitTeam == MyTeam) and ((ud ~= nil) and not (ud.isBuilding or not ud.canMove))) then -- test if exist and (is mobile and not a building) [I know buildings can't move but you never know]
 			WG['retreat'].addRetreatCommand(unitID, unitDefID, 2)
-			printDebug("<unit_auto_retreat60>: set autorepair level 2 for " .. ud.humanName .. " with ID " .. unitID)
-		else printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID ".. unitID .. " NOT suitable -> rejected !")
+		--printDebug("<unit_auto_retreat60>: set autorepair level 2 for " .. ud.humanName .. " with ID " .. unitID)
+		--else printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID ".. unitID .. " & team " .. unitTeam .." NOT suitable -> rejected !")
 		end
 	end
 
---[[function widget:UnitFromFactory(unitID, unitDefID, unitTeam)
+--[[ function widget:UnitFromFactory(unitID,unitDefID,unitTeam) -- every 'from factory' is created BUT autoretreat makes them retreat if retreat is set while they are built.
 	printDebug("<unit_auto_retreat60> : unit " .. unitID .. " from factory detected !")
-	checkUnits(unitID)
-end]]--
+	checkUnits(unitID,unitDefID,unitTeam)
+end ]]--
 
-function widget:UnitCreated(unitID,unitDefID)-- needed for resurrected units
-	local ud = UnitDefs[unitDefID]
-	printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID" .. unitID .. " created detected !")
-	checkUnits(unitID)
+function widget:UnitCreated(unitID,unitDefID,unitTeam)-- needed for resurrected units/spawned units. check health to know if they are being built.
+	--local ud = UnitDefs[unitDefID]
+	--printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID " .. unitID .. " and team " .. unitTeam .. " created detected !")
+	local health = spGetUnitHealth(unitID)
+	--printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID " .. unitID .. " just created have a health of " .. health)
+	if health <= 0.3 then
+	--printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID " .. unitID .. " is  being built :  do not touch yet.")
+		return end
+	checkUnits(unitID,unitDefID,unitTeam)
 end
 
-function widget:UnitFinished(unitID)-- test
-	local ud = UnitDefs[unitDefID]
-	printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID ".. unitID .. " finished detected !")
-	checkUnits(unitID)
+function widget:UnitFinished(unitID,unitDefID,unitTeam)--  every 'from factory' is also finished after being created BUT autoretreat makes them retreat if retreat is set while they are built.
+	--local ud = UnitDefs[unitDefID]
+	--printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID ".. unitID .. " finished detected !")
+	checkUnits(unitID,unitDefID,unitTeam)
 end
 
-function widget:UnitTaken(unitID)-- needed for taken units
-	local ud = UnitDefs[unitDefID]
-	printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID ".. unitID .. " taken detected !")
-	checkUnits(unitID)
+function widget:UnitTaken(unitID,unitDefID,unitTeam)-- needed for taken units
+	--local ud = UnitDefs[unitDefID]
+	--printDebug("<unit_auto_retreat60>: " .. ud.humanName .. " with ID ".. unitID .. " taken detected !")
+	checkUnits(unitID,unitDefID,unitTeam)
 end
 
 --[[
+
 function widget:UnitIdle()-- test, check if executed every frame -> bad
 	printDebug("<unit_auto_retreat60>: unit idle detected !")
 	checkUnits()
