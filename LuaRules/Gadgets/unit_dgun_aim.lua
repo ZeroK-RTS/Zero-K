af4e6f0c362820c0e25af885ca9ea6b509088209
KingRaptor <kingraptorowns@gmail.com>
2011-04-09 17:30:11 +0000

    Wrote a gadget to make dguns actually able to aim at units.
    svn path=/trunk/mods/zk/; revision=2272

---
+++ LuaRules/Gadgets/unit_dgun_aim.lua
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function gadget:GetInfo()
+  return {
+    name      = "D-Gun Aim Fix",
+    desc      = 'Makes d-guns aim at units instead of the ground',
+    author    = "KingRaptor",
+    date      = "10 April 2011",
+    license   = "Public Domain",
+    layer     = 0,
+    enabled   = true,
+  }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+if (not gadgetHandler:IsSyncedCode()) then
+  return false  --  silent removal
+end
+
+local spFindUnitCmdDesc = Spring.FindUnitCmdDesc
+local spEditUnitCmdDesc = Spring.EditUnitCmdDesc
+
+function gadget:UnitCreated(unitID, unitDefID, team)
+	local cmd = spFindUnitCmdDesc(unitID, CMD.DGUN)
+	local desc = {
+		type = CMDTYPE.ICON_UNIT_OR_MAP,
+	}
+	if cmd then spEditUnitCmdDesc(unitID, cmd, desc) end
+end
