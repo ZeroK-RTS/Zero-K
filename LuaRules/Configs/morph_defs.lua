9317f9f518564894dad3f0f6f6a3bb62ca3ce53a
KingRaptor <kingraptorowns@gmail.com>
2011-04-09 17:57:18 +0000

    Comm morph drain down to 10 metal/s. With combat morph there is no real risk in morphing so the least I can do is delay your instant gratification.
    svn path=/trunk/mods/zk/; revision=2274

--- LuaRules/Configs/morph_defs.lua
+++ LuaRules/Configs/morph_defs.lua
@@ -481,13 +481,13 @@ local morphDefs = {
 --------------------------------------------------------------------------------
 local comMorph = {
 	[1] = {
-		time = 10,
+		time = 20,
 	},
 	[2] = {
-		time = 23,
+		time = 45,
 	},
 	[3] = {
-		time = 20,
+		time = 40,
 	},
 }
 
@@ -576,7 +576,7 @@ for id, playerData in pairs(customComms) do
 					morphOption.into = array[i+1]
 					-- set time
 					--morphOption.time = math.floor( (targetDef.metalCost - originDef.metalCost) / (5 * (i+1)) ) or morphOption.time
-					morphOption.time = math.floor((targetDef.metalCost - originDef.metalCost)/15) or morphOption.time
+					morphOption.time = math.floor((targetDef.metalCost - originDef.metalCost)/10) or morphOption.time
 					morphOption.combatMorph = true
 					-- copy, checking that this morph isn't already defined
 					morphDefs[sourceName] = morphDefs[sourceName]  or {}
