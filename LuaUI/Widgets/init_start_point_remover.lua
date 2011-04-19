7bb1f4bdd03ab5e4138ff60b3114512d524a098f
kiss2cool <kiss2cool@gmail.com>
2011-04-19 08:58:41 +0000

    Merged gui_center_n_select.lua with init_start_point_remover.lua, centering should works now even if more than one unit is spawned and the Commander is not the first one. Note that if the Commander is spawned with an offset from the start point the marker may not be erased.
    svn path=/trunk/mods/zk/; revision=2470

--- LuaUI/Widgets/init_start_point_remover.lua
+++ LuaUI/Widgets/init_start_point_remover.lua
@@ -1,8 +1,8 @@
 -- $Id: init_start_point_remover.lua 3171 2008-11-06 09:06:29Z det $
 function widget:GetInfo()
   return {
-    name      = "Start Point Remover",
-    desc      = "Deletes your start point once the game begins",
+    name      = "Start Point Remover & Comm Selector",
+    desc      = "Deletes your start point once the game begins & select commander",
     author    = "TheFatController and jK",
     date      = "Jul 11, 2007",
     license   = "GNU GPL, v2 or later",
@@ -11,8 +11,12 @@ function widget:GetInfo()
   }
 end
 
+	n=1 --counter just in case there would be more than one unit spawned with only one being the comm but not being the first one. Note that if the Commander is spawned with an offset from the start point the marker may not be erased.
+
 function widget:Initialize()
+
   if (CheckForSpec()) then return false end
+ 
 end
 
 function CheckForSpec()
@@ -23,17 +27,19 @@ function CheckForSpec()
 end
 
 function widget:Update()
-  local t = Spring.GetGameSeconds()
-  if (t < 0.1) then return end
-
-  local teamUnits = Spring.GetTeamUnits(Spring.GetMyTeamID())
-  for _,unitID in ipairs(teamUnits) do
-    local unitDefID = Spring.GetUnitDefID(unitID)
-    local unitDef   = UnitDefs[unitDefID]
-    if (unitDef.isCommander) then
-      local x, y, z = Spring.GetUnitPosition(unitID)
-      Spring.MarkerErasePosition(x, y, z)
-    end
-  end
-  widgetHandler:RemoveWidget()
+	local t = Spring.GetGameSeconds()
+	if (t < 0.1) then return end
+	local teamUnits = Spring.GetTeamUnits(Spring.GetMyTeamID())
+	for _,unitID in ipairs(teamUnits) do
+	local unitDefID = Spring.GetUnitDefID(unitID)
+	local unitDef   = UnitDefs[unitDefID]
+		if (unitDef.isCommander) then
+			local x, y, z = Spring.GetUnitPosition(unitID)
+			Spring.MarkerErasePosition(x, y, z)
+			Spring.SetCameraTarget(x, y, z)
+			Spring.SelectUnitArray{teamUnits[n]}
+		end
+		n=n+1
+	end
+	widgetHandler:RemoveWidget()
 end
