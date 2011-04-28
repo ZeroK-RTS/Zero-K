1ae19d70959115dbd13e75bde84f76cb75e601c8
GoogleFrog <googl3frog@gmail.com>
2011-04-28 12:16:41 +0000

    Fixed the change introduced in r2648 which broke everything.
    svn path=/trunk/mods/zk/; revision=2649

--- LuaRules/Gadgets/start_unit_setup.lua
+++ LuaRules/Gadgets/start_unit_setup.lua
@@ -6,7 +6,7 @@ function gadget:GetInfo()
     date      = "2008-2010",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = false  --  loaded by default?
+    enabled   = true  --  loaded by default?
   }
 end
 
