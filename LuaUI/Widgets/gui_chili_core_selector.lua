6c9a9c40d1c95b5e31c894ce12607918721a3828
Licho <jalich@gmail.com>
2011-06-10 22:46:10 +0000

    selector base columns 8 to make it fit at least non chicken game screen
    svn path=/trunk/mods/zk/; revision=3019

--- LuaUI/Widgets/gui_chili_core_selector.lua
+++ LuaUI/Widgets/gui_chili_core_selector.lua
@@ -65,7 +65,7 @@ local echo = Spring.Echo
 
 local BUTTON_WIDTH = 64
 local BUTTON_HEIGHT = 52
-local BASE_COLUMNS = 10
+local BASE_COLUMNS = 8
 local NUM_FAC_COLUMNS = BASE_COLUMNS - 1	-- unused
 
 -------------------------------------------------------------------------------
