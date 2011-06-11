d68ceb8fe5ab896925e95ee2b872a96fe71761bc
wagonrepairer <wagonrepairer@gmail.com>
2011-06-11 16:03:45 +0000

    Changed cofc track to alt+t since zk uses t for rez.
    svn path=/trunk/mods/zk/; revision=3048

--- LuaUI/Widgets/camera_cofc.lua
+++ LuaUI/Widgets/camera_cofc.lua
@@ -4,7 +4,7 @@
 function widget:GetInfo()
   return {
     name      = "Combo Overhead/Free Camera (experimental)",
-    desc      = "v0.061 Camera featuring 6 actions. Type \255\90\90\255/luaui cofc help\255\255\255\255 for help.",
+    desc      = "v0.062 Camera featuring 6 actions. Type \255\90\90\255/luaui cofc help\255\255\255\255 for help.",
     author    = "CarRepairer",
     date      = "2011-03-16",
     license   = "GNU GPL, v2 or later",
@@ -239,7 +239,7 @@ options = {
 		name = "Enter Trackmode",
 		desc = "Track the selected unit (midclick to cancel)",
 		type = 'button',
-        hotkey = {key='t', mod=''},
+        hotkey = {key='t', mod='a'},
 		OnChange = function(self) trackmode = true; end,
 	},
 	
