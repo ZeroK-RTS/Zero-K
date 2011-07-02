a5f500221b9b4f8c572a7071291cfb121b8aac4e
luckywaldo7 <luckywaldo7@gmail.com>
2011-07-02 02:19:43 +0000

    Cursor hides on f5. Currently behavior is a bit stange. Instead of hiding immediately it hides only after cursor changes after f5, and same with re-appearing. This is how it works when changing cursor theme in epic menu though so I don't know if there is any other way to do it. Whatever, close enough. (useable for videos now anyway)
    svn path=/trunk/mods/zk/; revision=3267

--- LuaUI/Widgets/gui_hide_marks_on_f5.lua
+++ LuaUI/Widgets/gui_hide_marks_on_f5.lua
@@ -1,7 +1,7 @@
 function widget:GetInfo()
   return {
-    name      = "Hide map marks",
-    desc      = "Hides map marks on f5",
+    name      = "Hide map marks and cursor",
+    desc      = "Hides map marks and cursor on f5",
     author    = "who cares",
     date      = "dec, 2010",
     license   = "PD, anything else would be uncilized",
@@ -14,13 +14,61 @@ end
 --------------------------------------------------------------------------------
 include("keysym.h.lua")
 
+--------------------------------------------------------------------------------
+--Mouse cursor icons
+
+local cursorNames = {
+  'cursornormal',
+  'cursorareaattack',
+  'cursorattack',
+  'cursorattack',
+  'cursorbuildbad',
+  'cursorbuildgood',
+  'cursorcapture',
+  'cursorcentroid',
+  'cursordwatch',
+  'cursorwait',
+  'cursordgun',
+  'cursorattack',
+  'cursorfight',
+  'cursorattack',
+  'cursorgather',
+  'cursorwait',
+  'cursordefend',
+  'cursorpickup',
+  'cursormove',
+  'cursorpatrol',
+  'cursorreclamate',
+  'cursorrepair',
+  'cursorrevive',
+  'cursorrepair',
+  'cursorrestore',
+  'cursorrepair',
+  'cursorselfd',
+  'cursornumber',
+  'cursorwait',
+  'cursortime',
+  'cursorwait',
+  'cursorunload',
+  'cursorwait',
+}
+  
+--------------------------------------------------------------------------------
+  
 function widget:KeyPress(key, modifier, isRepeat)
 	if ( key == KEYSYMS.F5) then
 		if Spring.IsGUIHidden() then
 			Spring.SendCommands("mapmarks 1")
+			for _, cursor in ipairs(cursorNames) do
+				local topLeft = (cursor == 'cursornormal')
+				Spring.ReplaceMouseCursor(cursor, cursor, topLeft)
+			end
 		else
-			Spring.SetMouseCursor("none")
 			Spring.SendCommands("mapmarks 0")
+			for _, cursor in ipairs(cursorNames) do
+				local topLeft = (cursor == 'cursornormal' and cursorSet ~= 'k_haos_girl')
+				Spring.ReplaceMouseCursor(cursor, "empty/"..cursor, topLeft)
+			end
 		end 
 	end 
  end
