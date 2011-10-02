df44c186d277cacced6bb732b301b78568fcec94
Saktoth <oscar.evans@gmail.com>
2011-10-02 18:35:25 +0000

    Added WIP unit, the Felon (shieldfelon). Not finished yet, meant to drain its own shields when it fires (not working?). Bertha leaves larger debris.
    svn path=/trunk/mods/zk/; revision=3906

---
+++ units/shieldfelon.lua
@@ -0,0 +1,247 @@
+unitDef = {
+  unitname            = [[shieldfelon]],
+  name                = [[Felon]],
+  description         = [[Shielded Skirmisher]],
+  acceleration        = 0.25,
+  activateWhenBuilt   = true,
+  bmcode              = [[1]],
+  brakeRate           = 0.22,
+  buildCostEnergy     = 320,
+  buildCostMetal      = 320,
+  builder             = false,
+  buildPic            = [[corthud.png]],
+  buildTime           = 320,
+  canAttack           = true,
+  canGuard            = true,
+  canMove             = true,
+  canPatrol           = true,
+  canstop             = [[1]],
+  category            = [[LAND]],
+  corpse              = [[DEAD]],
+
+  customParams        = {
+    helptext       = [[The Felon charges its shield over time, and can release that energy in accurate bursts in all directions. Link it to other shields to increase its rate of fire.]],
+  },
+
+  explodeAs           = [[BIG_UNITEX]],
+  footprintX          = 2,
+  footprintZ          = 2,
+  iconType            = [[walkerskirm]],
+  idleAutoHeal        = 5,
+  idleTime            = 1800,
+  leaveTracks         = true,
+  mass                = 300,
+  maxDamage           = 400,
+  maxSlope            = 36,
+  maxVelocity         = 1.5,
+  maxWaterDepth       = 22,
+  minCloakDistance    = 75,
+  movementClass       = [[KBOT2]],
+  noAutoFire          = false,
+  noChaseCategory     = [[TERRAFORM FIXEDWING SATELLITE SUB]],
+  objectName          = [[shieldfelon.s3o]],
+  onoffable           = true,
+  seismicSignature    = 4,
+  selfDestructAs      = [[BIG_UNITEX]],
+
+  sfxtypes            = {
+
+    explosiongenerators = {
+      [[custom:THUDMUZZLE]],
+      [[custom:THUDSHELLS]],
+      [[custom:THUDDUST]],
+    },
+
+  },
+
+  side                = [[CORE]],
+  sightDistance       = 420,
+  smoothAnim          = true,
+  trackOffset         = 0,
+  trackStrength       = 8,
+  trackStretch        = 1,
+  trackType           = [[ComTrack]],
+  trackWidth          = 22,
+  turnRate            = 500,
+  upright             = true,
+  workerTime          = 0,
+
+  weapons             = {
+  
+    {
+      def = [[SHIELD]],
+    },
+
+    {
+      def                = [[SHIELDGUN]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	  mainDir            = [[0 0.5 1]],
+	  maxAngleDif        = 230,
+    },
+	
+    {
+      def                = [[SHIELDGUN]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	  mainDir            = [[1 0.5 1]],
+	  maxAngleDif        = 230,
+    },
+
+    {
+      def                = [[SHIELDGUN]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	  mainDir            = [[1 0.5 0]],
+	  maxAngleDif        = 230,
+    },
+	
+    {
+      def                = [[SHIELDGUN]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	  mainDir            = [[1 0.5 -1]],
+	  maxAngleDif        = 230,
+    },
+
+    {
+      def                = [[SHIELDGUN]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	  mainDir            = [[0 0.5 -1]],
+	  maxAngleDif        = 230,
+    },
+	
+    {
+      def                = [[SHIELDGUN]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	  mainDir            = [[-1 0.5 -1]],
+	  maxAngleDif        = 230,
+    },
+
+    {
+      def                = [[SHIELDGUN]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	  mainDir            = [[-1 0.5 0]],
+	  maxAngleDif        = 230,
+    },
+	
+    {
+      def                = [[SHIELDGUN]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+	  mainDir            = [[-1 0.5 1]],
+	  maxAngleDif        = 230,
+    },
+	
+  },
+
+
+  weaponDefs          = {
+	
+    SHIELD      = {
+      name                    = [[Energy Shield]],
+      craterMult              = 0,
+
+      damage                  = {
+        default = 10,
+      },
+
+      exteriorShield          = true,
+      impulseFactor           = 0,
+      interceptedByShieldType = 1,
+      isShield                = true,
+      shieldAlpha             = 0.4,
+      shieldBadColor          = [[1 0.1 0.1]],
+      shieldGoodColor         = [[0.1 0.1 1]],
+      shieldInterceptType     = 3,
+      shieldPower             = 500,
+      shieldPowerRegen        = 10,
+      shieldPowerRegenEnergy  = 0,
+      shieldRadius            = 120,
+      shieldRepulser          = false,
+      shieldStartingPower     = 0,
+      smartShield             = true,
+      texture1                = [[shield3mist]],
+      visibleShield           = true,
+      visibleShieldHitFrames  = 4,
+      visibleShieldRepulse    = true,
+      weaponType              = [[Shield]],
+	  range                   = 420,
+    },
+	
+    SHIELDGUN = {
+      name                    = [[Shield Gun]],
+      areaOfEffect            = 8,
+      craterBoost             = 0,
+      craterMult              = 0,
+      cylinderTargetting      = 0,
+
+      damage                  = {
+        default        = 1,
+      },
+
+      explosionGenerator      = [[custom:flash2purple]],
+      fireStarter             = 50,
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      intensity               = 6,
+      interceptedByShieldType = 1,
+      range                   = 300,
+      reloadtime              = 0.2,
+      rgbColor                = [[0.5 0 0.7]],
+      soundStart              = [[weapon/small_lightning]],
+      soundTrigger            = true,
+      startsmoke              = [[1]],
+      targetMoveError         = 0,
+      texture1                = [[corelaser]],
+      thickness               = 2,
+      turret                  = true,
+      weaponType              = [[LightningCannon]],
+    },
+
+  },
+
+
+  featureDefs         = {
+
+    DEAD  = {
+      description      = [[Wreckage - Thug]],
+      blocking         = true,
+      category         = [[corpses]],
+      damage           = 700,
+      energy           = 0,
+      featureDead      = [[HEAP]],
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      height           = [[20]],
+      hitdensity       = [[100]],
+      metal            = 128,
+      object           = [[thug_d.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 128,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+
+    HEAP  = {
+      description      = [[Debris - Felon]],
+      blocking         = false,
+      category         = [[heaps]],
+      damage           = 700,
+      energy           = 0,
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      height           = [[4]],
+      hitdensity       = [[100]],
+      metal            = 64,
+      object           = [[debris2x2c.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 64,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+  },
+
+}
+
+return lowerkeys({ shieldfelon = unitDef })
