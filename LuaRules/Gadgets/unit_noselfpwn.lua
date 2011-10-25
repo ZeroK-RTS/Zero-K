e84fcec6bf4d5639247c167ce7db0c613d187764
KingRaptor <kingraptorowns@gmail.com>
2011-10-25 09:41:10 +0000

    Disabled no self pwn gadget; the weaponDef tag works fine
    svn path=/trunk/mods/zk/; revision=4097

--- LuaRules/Gadgets/unit_noselfpwn.lua
+++ LuaRules/Gadgets/unit_noselfpwn.lua
@@ -10,7 +10,7 @@ function gadget:GetInfo()
     date      = "Feb 1, 2008",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
