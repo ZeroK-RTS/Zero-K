85aaf7cf49eb370dfbdcf44ee93321d5ec77ff72
xponen <msafwan86msafwan86_nnss@hotmail.com>
2011-11-23 21:46:43 +0000

    "snd_music.lua" fix: improve startup music randomizer, pausing game will stop & change music. Other implementation is not touched...
    svn path=/trunk/mods/zk/; revision=4409

--- LuaUI/Widgets/snd_music.lua
+++ LuaUI/Widgets/snd_music.lua
@@ -15,8 +15,8 @@ function widget:GetInfo()
 	return {
 		name			= "Music Player",
 		desc			= "Plays music based on situation",
-		author		= "cake, trepan, Smoth, Licho",
-		date			= "Mar 01, 2008, Aug 20 2009",
+		author		= "cake, trepan, Smoth, Licho, xponen",
+		date			= "Mar 01, 2008, Aug 20 2009, Nov 23 2011",
 		license	= "GNU GPL, v2 or later",
 		layer		= 0,
 		enabled	= true	--	loaded by default?
@@ -36,8 +36,8 @@ local PEACE_THRESHOLD = 1000
 local musicType = 'peace'
 local dethklok = {} -- keeps track of the number of doods killed in each time frame
 local timeframetimer = 0
-local lastTrack = ''
-local lastTrackType = ''
+local previousTrack = ''
+local previousTrackType = ''
 local newTrackWait = 1000
 local numVisibleEnemy = 0
 local fadeVol
@@ -48,12 +48,11 @@ local warTracks		=	VFS.DirList('sounds/music/war/', '*.ogg')
 local peaceTracks	=	VFS.DirList('sounds/music/peace/', '*.ogg')
 
 
-local firsttime = true
+local musicIsOn = false
 local firstFade = true
-local initTrack
+local initSeed = 0
+local seedInitialized = false
 	
-initTrack = peaceTracks[math.random(1, #peaceTracks)]
-
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
@@ -64,7 +63,7 @@ function widget:Initialize()
 	-- for TrackName,TrackDef in pairs(peaceTracks) do
 		-- Spring.Echo("Track: " .. TrackDef)	
 	-- end
-	math.randomseed(os.clock()* 101.01)--lurker wants you to burn in hell rgn
+	--math.randomseed(os.clock()* 101.01)--lurker wants you to burn in hell rgn
 	-- for i=1,20 do Spring.Echo(math.random()) end
 	
 	for i = 1, 30, 1 do
@@ -83,103 +82,124 @@ function widget:Shutdown()
 end
 
 function widget:Update(dt)
-	if (Spring.GetGameSeconds() < 3) then
+	if (Spring.GetGameSeconds() < 1) then
+		initSeed = initSeed + math.random(-100,100)
+	elseif (Spring.GetGameSeconds() < 2) then
+		if not seedInitialized then
+			math.randomseed(initSeed)
+			seedInitialized=true
+		end
+	elseif (Spring.GetGameSeconds() < 3) then
 		dethklok[1] = 0
 	else
-		timeframetimer = timeframetimer + dt
-		if (timeframetimer > 1) then	-- every second
-			timeframetimer = 0
-			newTrackWait = newTrackWait + 1
-
-			local PlayerTeam = Spring.GetMyTeamID()
-			numVisibleEnemy = 0
-			local doods = Spring.GetVisibleUnits()
-		for _, u in ipairs(doods) do
-			if (Spring.IsUnitAllied(u) ~= true) then
-				numVisibleEnemy = numVisibleEnemy + 1
-			end
-		end
-			
-		totalKilled = 0
-		for i = 1, 10, 1 do
-			totalKilled = totalKilled + (dethklok[i] * 2)
-		end
-		
-		for i = 11, 20, 1 do
-			totalKilled = totalKilled + dethklok[i]
-		end
-		
-		for i = 20, 1, -1 do
-			dethklok[i+1] = dethklok[i]
-		end
-		dethklok[1] = 0
-		
-		--Spring.Echo (totalKilled)
-		
-		if (totalKilled > WAR_THRESHOLD) then
-			musicType = 'war'
-		end
-		
-		if (totalKilled <= PEACE_THRESHOLD) then
-			musicType = 'peace'
-		end
-		
-		if (firsttime) then
-			Spring.PlaySoundStream(initTrack,WG.music_volume or 0.5) --init to something
-			WG.music_start_volume = WG.music_volume
-			firsttime = false -- pop this cherry	
-		end
-		
-		local playedTime, totalTime = Spring.GetSoundStreamTime()
-		playedTime = math.floor(playedTime)
-		totalTime = math.floor(totalTime)
-		
-		--Spring.Echo(playedTime, totalTime, newTrackWait)
-		
-			--if((totalTime - playedTime) <= 6 and (totalTime >= 1) ) then
-				--Spring.Echo("time left:", (totalTime - playedTime))
-				--Spring.Echo("volume:", (totalTime - playedTime)/6)
-				--if ((totalTime - playedTime)/6 >= 0) then
-				--	Spring.SetSoundStreamVolume((totalTime - playedTime)/6)
-				--else
-				--	Spring.SetSoundStreamVolume(0.1)
-				--end
-			--elseif(playedTime <= 5 )then--and not firstFade
-				--Spring.Echo("time playing:", playedTime)
-				--Spring.Echo("volume:", playedTime/5)
-				--Spring.SetSoundStreamVolume( playedTime/5)
-			--end
-		
-
-			if ( (musicType ~= lastTrackType and musicType == 'war') or (playedTime > totalTime)	) then
-				lastTrackType = musicType
-				local newTrack
-				repeat
-				if musicType == 'peace' then
-					newTrack = peaceTracks[math.random(1, #peaceTracks)]
-				elseif musicType == 'war' then
-					newTrack = warTracks[math.random(1, #warTracks)]
+		local _, _, paused = Spring.GetGameSpeed()
+		if (not paused) then
+			timeframetimer = timeframetimer + dt
+			if (timeframetimer > 1) then	-- every second
+				timeframetimer = 0
+				newTrackWait = newTrackWait + 1
+				local PlayerTeam = Spring.GetMyTeamID()
+				numVisibleEnemy = 0
+				local doods = Spring.GetVisibleUnits()
+			for _, u in ipairs(doods) do
+				if (Spring.IsUnitAllied(u) ~= true) then
+					numVisibleEnemy = numVisibleEnemy + 1
 				end
-				until newTrack ~= lastTrack
-				
-				-- for key, val in pairs(oggInfo) do
-					-- Spring.Echo(key, val)	
-				-- end
-				firstFade = false
-				lastTrack = newTrack
-				
-				-- if (oggInfo.comments.TITLE and oggInfo.comments.TITLE) then
-					-- Spring.Echo("Song changed to: " .. oggInfo.comments.TITLE .. " By: " .. oggInfo.comments.ARTIST)
-				-- else
-					-- Spring.Echo("Song changed but unable to get the artist and title info")
-				-- end
-				curTrack = newTrack
-				Spring.PlaySoundStream(newTrack,WG.music_volume or 0.5)
-				WG.music_start_volume = WG.music_volume
-				
-				--Spring.Echo("Track: " .. newTrack)
-				newTrackWait = 0
 			end
+				
+			totalKilled = 0
+			for i = 1, 10, 1 do --calculate the first half of the table (1-15)
+				totalKilled = totalKilled + (dethklok[i] * 2)
+			end
+			
+			for i = 11, 20, 1 do -- calculate the second half of the table (16-45)
+				totalKilled = totalKilled + dethklok[i]
+			end
+			
+			for i = 20, 1, -1 do -- shift value(s) to the end of table
+				dethklok[i+1] = dethklok[i]
+			end
+			dethklok[1] = 0 -- empty the first row
+			
+			--Spring.Echo (totalKilled)
+			
+			if (totalKilled > WAR_THRESHOLD) then
+				musicType = 'war'
+			end
+			
+			if (totalKilled <= PEACE_THRESHOLD) then
+				musicType = 'peace'
+			end
+			
+			if (not musicIsOn) then
+				local newTrack = previousTrack
+				repeat
+					if musicType == 'peace' then
+						newTrack = peaceTracks[math.random(1, #peaceTracks)]
+					elseif musicType == 'war' then
+						newTrack = warTracks[math.random(1, #warTracks)]
+					end
+				until newTrack ~= previousTrack
+				previousTrack = newTrack
+				
+				Spring.PlaySoundStream(newTrack,WG.music_volume or 0.5) --init to random
+				WG.music_start_volume = WG.music_volume
+				musicIsOn = true -- pop this cherry	
+			end
+			
+			local playedTime, totalTime = Spring.GetSoundStreamTime()
+			playedTime = math.floor(playedTime)
+			totalTime = math.floor(totalTime)
+			
+			--Spring.Echo(playedTime, totalTime, newTrackWait)
+			
+				--if((totalTime - playedTime) <= 6 and (totalTime >= 1) ) then
+					--Spring.Echo("time left:", (totalTime - playedTime))
+					--Spring.Echo("volume:", (totalTime - playedTime)/6)
+					--if ((totalTime - playedTime)/6 >= 0) then
+					--	Spring.SetSoundStreamVolume((totalTime - playedTime)/6)
+					--else
+					--	Spring.SetSoundStreamVolume(0.1)
+					--end
+				--elseif(playedTime <= 5 )then--and not firstFade
+					--Spring.Echo("time playing:", playedTime)
+					--Spring.Echo("volume:", playedTime/5)
+					--Spring.SetSoundStreamVolume( playedTime/5)
+				--end
+			
+
+				if ( (musicType ~= previousTrackType and musicType == 'war') or (playedTime > totalTime)	) then
+					previousTrackType = musicType
+					local newTrack = previousTrack
+					repeat
+						if musicType == 'peace' then
+							newTrack = peaceTracks[math.random(1, #peaceTracks)]
+						elseif musicType == 'war' then
+							newTrack = warTracks[math.random(1, #warTracks)]
+						end
+					until newTrack ~= previousTrack
+					-- for key, val in pairs(oggInfo) do
+						-- Spring.Echo(key, val)	
+					-- end
+					firstFade = false
+					previousTrack = newTrack
+					
+					-- if (oggInfo.comments.TITLE and oggInfo.comments.TITLE) then
+						-- Spring.Echo("Song changed to: " .. oggInfo.comments.TITLE .. " By: " .. oggInfo.comments.ARTIST)
+					-- else
+						-- Spring.Echo("Song changed but unable to get the artist and title info")
+					-- end
+					curTrack = newTrack
+					Spring.PlaySoundStream(newTrack,WG.music_volume or 0.5)
+					WG.music_start_volume = WG.music_volume
+					
+					--Spring.Echo("Track: " .. newTrack)
+					newTrackWait = 0
+				end
+			end
+		elseif (paused) then
+			Spring.StopSoundStream()
+			musicIsOn = false
 		end
 	end
 end
