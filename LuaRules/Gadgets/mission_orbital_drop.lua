afc72902b241a0ef4a13ad8c8184dc7e8934ee30
KingRaptor <kingraptorowns@gmail.com>
2011-12-10 04:57:24 +0000

    Fix/workaround for "Return to sender" space comms in 85.0
    svn path=/trunk/mods/zk/; revision=4548

--- LuaRules/Gadgets/mission_orbital_drop.lua
+++ LuaRules/Gadgets/mission_orbital_drop.lua
@@ -58,8 +58,12 @@ function gadget:GameFrame(frame)
     if Spring.ValidUnitID(unitID) then
       local x, y, z = Spring.GetUnitBasePosition(unitID)
       local h = Spring.GetGroundHeight(x, z)
-      local _, dy = Spring.GetUnitVelocity(unitID) 
-      if y < h or dy >= 1 then 
+      local _, dy = Spring.GetUnitVelocity(unitID)
+	  -- FIXME: dy is always zero in 84.0+
+	  if frame%15 == 0 then
+		--Spring.Echo(y, dy)
+	  end
+      if y <= h or dy >= 1 then 
         -- unit has landed (or is moving up, which means it has missed the ground)
         Spring.MoveCtrl.SetPosition(unitID, x, h, z)
         Spring.MoveCtrl.SetVelocity(unitID, 0, 0, 0)
