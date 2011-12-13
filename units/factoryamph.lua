c519eec05bce97d868effbc7bfc9764eab9a6b83
KingRaptor <kingraptorowns@gmail.com>
2011-12-13 10:08:20 +0000

    Added scout/searoach sub to amph fac.
    svn path=/trunk/mods/zk/; revision=4571

--- units/factoryamph.lua
+++ units/factoryamph.lua
@@ -19,6 +19,7 @@ unitDef = {
 	[[amphraider]],
 	[[amphassault]],
 	[[amphaa]],
+	[[subscout]],
 	[[corsub]],
 	[[serpent]],
   },
---
+++ weapons/subscout_death.lua
@@ -0,0 +1,26 @@
+return {
+
+  SUBSCOUT_DEATH = {
+    name                    = [[Super Torpedo]],
+    areaofeffect            = 256,
+    craterboost             = 1,
+    cratermult              = 4,
+    
+    damage                  = {
+      default = 1500,
+    },
+    
+    edgeeffectiveness       = 0.5,
+    explosionGenerator      = [[custom:exp_medium_building_small]],
+    fireStarter             = 200,
+    impulseboost            = 0,
+    impulsefactor           = 0,
+    interceptedbyshieldtype = 1,
+    range                   = 200,
+    reloadtime              = 3.6,
+    rendertype              = 4,
+    soundhit                = [[explosion/mini_nuke]],
+    turret                  = 1,
+    weaponvelocity          = 250,
+  },
+}
