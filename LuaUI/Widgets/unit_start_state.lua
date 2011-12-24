70a5e33388340ecd44e09931b74f0d3b81c1601c
GoogleFrog <googl3frog@gmail.com>
2011-12-24 10:06:14 +0000

    Added commanders to initial states. Fixed small issue with Hold Position preset.
    svn path=/trunk/mods/zk/; revision=4701

--- LuaUI/Widgets/unit_start_state.lua
+++ LuaUI/Widgets/unit_start_state.lua
@@ -17,16 +17,17 @@ end
 local holdPosException = { 
     ["factoryplane"] = true,
     ["factorygunship"] = true,
+    ["armnanotc"] = true,
 }
 
 local rememberToSetHoldPositionPreset = false
 
 local function IsGround(ud)
-    return (not (ud.canFly or ud.isBuilding or (ud.builder and not ud.canMove and not ud.isFactory) ) )
+    return not ud.canFly
 end
 
 options_path = 'Game/Unit AI/Initial States'
-options_order = { 'presetlabel', 'holdPosition'}
+options_order = { 'presetlabel', 'holdPosition', 'commander_label', 'commander_firestate', 'commander_movestate'}
 options = {
 	presetlabel = {name = "presetlabel", type = 'label', value = "Presets", path = options_path},
     
@@ -37,7 +38,36 @@ options = {
 		OnChange = function ()
             rememberToSetHoldPositionPreset = true
         end,
-	}
+	},
+    
+    commander_label = {
+        name = "label", 
+        type = 'label', 
+        value = "Commander",
+        path = "Game/Unit AI/Initial States/Misc",
+    },
+    
+    commander_firestate = {
+        name = "Firestate",
+        desc = "Values: inherit from factory, hold fire, return fire, fire at will",
+        type = 'number',
+        value = -1, -- most firestates are -1
+        min = -1,
+        max = 2,
+        step = 1,
+        path = "Game/Unit AI/Initial States/Misc",
+    },
+
+    commander_movestate = {
+        name = "Movestate",
+        desc = "Values: inherit from factory, hold positon, manuver, roam",
+        type = 'number',
+        value = -1,
+        min = -1,
+        max = 2,
+        step = 1,
+        path = "Game/Unit AI/Initial States/Misc",
+    },
 }
 
 local tacticalAIDefs = VFS.Include("LuaRules/Configs/tactical_ai_defs.lua", nil, VFS.ZIP)
@@ -53,92 +83,92 @@ local unitAlreadyAdded = {}
 local function addLabel(text, path) -- doesn't work with order
     path = (path and "Game/Unit AI/Initial States/" .. path) or "Game/Unit AI/Initial States"
     options[text .. "_label"] = {
-            name = "label", 
-            type = 'label', 
-            value = text,
-            path = path,
+        name = "label", 
+        type = 'label', 
+        value = text,
+        path = path,
     }
     options_order[#options_order+1] = text .. "_label"
 end
 
 local function addUnit(defName, path)
-        
-        if unitAlreadyAdded[defName] then
-                return
-        end
+    
+    if unitAlreadyAdded[defName] then
+        return
+    end
 
-        unitAlreadyAdded[defName] = true
-        
-        path = "Game/Unit AI/Initial States/" .. path
-        local ud = UnitDefNames[defName]
-        
-        options[defName .. "_label"] = {
-                name = "label", 
-                type = 'label', 
-                value = ud.humanName,
-                path = path,
+    unitAlreadyAdded[defName] = true
+    
+    path = "Game/Unit AI/Initial States/" .. path
+    local ud = UnitDefNames[defName]
+    
+    options[defName .. "_label"] = {
+        name = "label", 
+        type = 'label', 
+        value = ud.humanName,
+        path = path,
+    }
+    options_order[#options_order+1] = defName .. "_label"
+    
+    if ud.canAttack then
+        options[defName .. "_firestate"] = {
+            name = "Firestate",
+            desc = "Values: inherit from factory, hold fire, return fire, fire at will",
+            type = 'number',
+            value = ud.fireState, -- most firestates are -1
+            min = -1,
+            max = 2,
+            step = 1,
+            path = path,
         }
-        options_order[#options_order+1] = defName .. "_label"
-        
-        if ud.canAttack then
-                options[defName .. "_firestate"] = {
-                        name = "Firestate",
-                        desc = "Values: inherit from factory, hold fire, return fire, fire at will",
-                        type = 'number',
-                        value = ud.fireState, -- most firestates are -1
-                        min = -1,
-                        max = 2,
-                        step = 1,
-                        path = path,
-                }
-                options_order[#options_order+1] = defName .. "_firestate"
-        end
+        options_order[#options_order+1] = defName .. "_firestate"
+    end
 
-        if ud.canMove or ud.canPatrol then
-                options[defName .. "_movestate"] = {
-                        name = "Movestate",
-                        desc = "Values: inherit from factory, hold positon, manuver, roam",
-                        type = 'number',
-                        value = ud.moveState,
-                        min = -1,
-                        max = 2,
-                        step = 1,
-                        path = path,
-                }
-                options_order[#options_order+1] = defName .. "_movestate"
-        end
+    if ud.canMove or ud.canPatrol then
+        options[defName .. "_movestate"] = {
+            name = "Movestate",
+            desc = "Values: inherit from factory, hold positon, manuver, roam",
+            type = 'number',
+            value = ud.moveState,
+            min = -1,
+            max = 2,
+            step = 1,
+            path = path,
+        }
+        options_order[#options_order+1] = defName .. "_movestate"
+    end
 
-        if tacticalAIUnits[defName] then
-                options[defName .. "_tactical_ai"] = {
-                        name = "Smart AI",
-                        desc = "Values: check box to turn it on",
-                        type = 'bool',
-                        value = true,
-                        path = path,
-                }
-                options_order[#options_order+1] = defName .. "_tactical_ai"
-        end
-        
-        if ud.canCloak then
-                options[defName .. "_personal_cloak"] = {
-                        name = "Personal Cloak",
-                        desc = "Values: check box to turn it on",
-                        type = 'bool',
-                        value = ud.startCloaked,
-                        path = path,
-                }
-                options_order[#options_order+1] = defName .. "_personal_cloak"
-        
-        end
+    if tacticalAIUnits[defName] then
+        options[defName .. "_tactical_ai"] = {
+            name = "Smart AI",
+            desc = "Values: check box to turn it on",
+            type = 'bool',
+            value = true,
+            path = path,
+        }
+        options_order[#options_order+1] = defName .. "_tactical_ai"
+    end
+    
+    if ud.canCloak then
+        options[defName .. "_personal_cloak"] = {
+            name = "Personal Cloak",
+            desc = "Values: check box to turn it on",
+            type = 'bool',
+            value = ud.startCloaked,
+            path = path,
+        }
+        options_order[#options_order+1] = defName .. "_personal_cloak"
+    
+    end
 end
 
 local function AddFactoryOfUnits(defName)
-        local ud = UnitDefNames[defName]
-        local name = ud.humanName
-        addUnit(defName, name)
-        for i = 1, #ud.buildOptions do
-                addUnit(UnitDefs[ud.buildOptions[i]].name, name)
-        end
+    local ud = UnitDefNames[defName]
+    local name = ud.humanName
+    addUnit(defName, name)
+    for i = 1, #ud.buildOptions do
+            addUnit(UnitDefs[ud.buildOptions[i]].name, name)
+    end
 end
 AddFactoryOfUnits("factoryshield")
 AddFactoryOfUnits("factorycloak")
@@ -163,52 +193,63 @@ addUnit("armnanotc","Static")
 local CMD_UNIT_AI = 36214
 
 function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID) 
-        if unitTeam == Spring.GetMyTeamID() and unitDefID and UnitDefs[unitDefID] then
-                local name = UnitDefs[unitDefID].name
-                if unitAlreadyAdded[name] then
-                        
-                        if options[name .. "_firestate"] and options[name .. "_firestate"].value then
-                                if options[name .. "_firestate"].value == -1 then
-                                        if builderID then
-                                                local bdid = Spring.GetUnitDefID(builderID)
-                                                if UnitDefs[bdid] and UnitDefs[bdid].isFactory then
-                                                        local firestate = Spring.GetUnitStates(builderID).firestate
-                                                        if firestate then
-                                                                Spring.GiveOrderToUnit(unitID, CMD.FIRE_STATE, {firestate}, 0)
-                                                        end
-                                                end
-                                        end
-                                else
-                                        Spring.GiveOrderToUnit(unitID, CMD.FIRE_STATE, {options[name .. "_firestate"].value}, 0)
-                                end
-                        end
-                        
-                        if options[name .. "_movestate"] and options[name .. "_movestate"].value then
-                                if options[name .. "_movestate"].value == -1 then
-                                        if builderID then
-                                                local bdid = Spring.GetUnitDefID(builderID)
-                                                if UnitDefs[bdid] and UnitDefs[bdid].isFactory then
-                                                        local movestate = Spring.GetUnitStates(builderID).movestate
-                                                        if movestate then
-                                                                Spring.GiveOrderToUnit(unitID, CMD.MOVE_STATE, {movestate}, 0)
-                                                        end
-                                                end
-                                        end
-                                else
-                                        Spring.GiveOrderToUnit(unitID, CMD.MOVE_STATE, {options[name .. "_movestate"].value}, 0)
-                                end
-                        end
-                        
-                        if options[name .. "_tactical_ai"] and options[name .. "_tactical_ai"].value ~= nil then
-                                Spring.GiveOrderToUnit(unitID, CMD_UNIT_AI, {options[name .. "_tactical_ai"].value and 1 or 0}, 0)
-                        end
-                        
-                        if options[name .. "_personal_cloak"] and options[name .. "_personal_cloak"].value ~= nil then
-                                Spring.GiveOrderToUnit(unitID, CMD.CLOAK, {options[name .. "_personal_cloak"].value and 1 or 0}, 0)
-                        end
-                        
-                end
+    if unitTeam == Spring.GetMyTeamID() and unitDefID and UnitDefs[unitDefID] then
+        
+        if UnitDefs[unitDefID].customParams.level then
+            if options.commander_firestate.value ~= -1 then
+                Spring.GiveOrderToUnit(unitID, CMD.FIRE_STATE, {options.commander_firestate.value}, 0)
+            end
+            
+            if options.commander_movestate.value ~= -1 then
+                Spring.GiveOrderToUnit(unitID, CMD.MOVE_STATE, {options.commander_movestate.value}, 0)
+            end
         end
+        
+        local name = UnitDefs[unitDefID].name
+        if unitAlreadyAdded[name] then
+            
+            if options[name .. "_firestate"] and options[name .. "_firestate"].value then
+                if options[name .. "_firestate"].value == -1 then
+                    if builderID then
+                        local bdid = Spring.GetUnitDefID(builderID)
+                        if UnitDefs[bdid] and UnitDefs[bdid].isFactory then
+                            local firestate = Spring.GetUnitStates(builderID).firestate
+                            if firestate then
+                                Spring.GiveOrderToUnit(unitID, CMD.FIRE_STATE, {firestate}, 0)
+                            end
+                        end
+                    end
+                else
+                    Spring.GiveOrderToUnit(unitID, CMD.FIRE_STATE, {options[name .. "_firestate"].value}, 0)
+                end
+            end
+            
+            if options[name .. "_movestate"] and options[name .. "_movestate"].value then
+                if options[name .. "_movestate"].value == -1 then
+                    if builderID then
+                        local bdid = Spring.GetUnitDefID(builderID)
+                        if UnitDefs[bdid] and UnitDefs[bdid].isFactory then
+                            local movestate = Spring.GetUnitStates(builderID).movestate
+                            if movestate then
+                                Spring.GiveOrderToUnit(unitID, CMD.MOVE_STATE, {movestate}, 0)
+                            end
+                        end
+                    end
+                else
+                    Spring.GiveOrderToUnit(unitID, CMD.MOVE_STATE, {options[name .. "_movestate"].value}, 0)
+                end
+            end
+            
+            if options[name .. "_tactical_ai"] and options[name .. "_tactical_ai"].value ~= nil then
+                Spring.GiveOrderToUnit(unitID, CMD_UNIT_AI, {options[name .. "_tactical_ai"].value and 1 or 0}, 0)
+            end
+            
+            if options[name .. "_personal_cloak"] and options[name .. "_personal_cloak"].value ~= nil then
+                Spring.GiveOrderToUnit(unitID, CMD.CLOAK, {options[name .. "_personal_cloak"].value and 1 or 0}, 0)
+            end
+            
+        end
+    end
 end
 
 function widget:UnitGiven(unitID, unitDefID, newTeamID, teamID)
