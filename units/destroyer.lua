fc45fd0ffa9534f89f46c2ac13f2d3dfc9889058
KingRaptor <kingraptorowns@gmail.com>
2011-12-28 12:59:17 +0000

    New destroyer (/give destroyer).
    svn path=/trunk/mods/zk/; revision=4799

---
+++ units/destroyer.lua
@@ -0,0 +1,211 @@
+unitDef = {
+  unitname               = [[destroyer]],
+  name                   = [[Daimyo]],
+  description            = [[Destroyer (Riot/Antisub)]],
+  acceleration           = 0.0417,
+  activateWhenBuilt      = true,
+  brakeRate              = 0.142,
+  buildAngle             = 16384,
+  buildCostEnergy        = 700,
+  buildCostMetal         = 700,
+  builder                = false,
+  buildPic               = [[destroyer.png]],
+  buildTime              = 700,
+  canAttack              = true,
+  canGuard               = true,
+  canMove                = true,
+  canPatrol              = true,
+  category               = [[SHIP]],
+  collisionVolumeOffsets = [[0 0 3]],
+  collisionVolumeScales  = [[32 46 102]],
+  collisionVolumeTest    = 1,
+  collisionVolumeType    = [[box]],
+  corpse                 = [[DEAD]],
+
+  customParams           = {
+    description_fr = [[Destroyer]],
+    description_pl = [[Niszczyciel]],
+	description_de = [[Zerstörer]],
+    helptext       = [[The Daimyo class destroyer packs a wallop with its main gun, a powerful riot cannon. It also features missiles that can hit submarines and surface targets alike. The Daimyo is best attacked from afar, using surface ships or hovercraft with long-range weapons.]],
+    --helptext_fr    = [[Ce Destroyer embarque un puissant canon longue port?e et un lance grenade sous marines. Utile pour se d?barrasser de menaces sous marines ou de positions fixes, son canon est cependant trop peu pr?cis pour d?truire des menaces rapides.]],
+    --helptext_pl    = [[Crusader posiada pot?n? armat? ?redniego zasi?gu idealn? do bombardowania nieruchomych wie?yczek broni?cych wybrze?y. Jego drug? broni? jest wyrzutnia ?adunk?w g??binowych przeciwko ?odziom podwodnym. ?atwo pada ofiar? jednostek lataj?cych i Korwet, gdy? nie posiada broni skutecznym przeciwko szybkim celom.]],
+	--helptext_de    = [[Der Zerstörer kombiniert eine kraftvolle, weitreichende Hauptkanone, nützlich für das Bombadieren von festen Standorten und Küstenzielen, mit einem Torpedowerfer gegen U-Boote. Hüte dich vor Flugzeugen und Korvetten - Zerstörer haben einige Probleme damit, schnelle Ziele zu treffen.]],
+    extradrawrange = 420,
+  },
+
+  explodeAs              = [[BIG_UNITEX]],
+  floater                = true,
+  footprintX             = 4,
+  footprintZ             = 4,
+  iconType               = [[destroyer]],
+  idleAutoHeal           = 5,
+  idleTime               = 1800,
+  mass                   = 320,
+  maxDamage              = 3300,
+  maxVelocity            = 2.9,
+  minCloakDistance       = 75,
+  minWaterDepth          = 10,
+  movementClass          = [[BOAT4]],
+  noAutoFire             = false,
+  noChaseCategory        = [[TERRAFORM FIXEDWING SATELLITE]],
+  objectName             = [[destroyer.s3o]],
+  script				 = [[destroyer.lua]],
+  seismicSignature       = 4,
+  selfDestructAs         = [[BIG_UNITEX]],
+  side                   = [[ARM]],
+  sightDistance          = 500,
+  
+  sfxtypes               = {
+
+    explosiongenerators = {
+	  [[custom:LARGE_MUZZLE_FLASH_FX]],
+      [[custom:PULVMUZZLE]],
+    },
+
+  },  
+  
+  smoothAnim             = true,
+  sonarDistance          = 700,
+  turninplace            = 0,
+  turnRate               = 311,
+  waterline              = 0,
+  workerTime             = 0,
+
+  weapons                = {
+
+    {
+      def                = [[PLASMA]],
+      badTargetCategory  = [[GUNSHIP]],
+      onlyTargetCategory = [[SWIM LAND SHIP SINK FLOAT GUNSHIP HOVER]],
+    },
+
+
+    {
+      def                = [[MISSILE]],
+      badTargetCategory  = [[FIXEDWING]],
+      onlyTargetCategory = [[SWIM FIXEDWING LAND SUB SINK FLOAT SHIP GUNSHIP HOVER]],
+    },
+
+  },
+
+
+  weaponDefs             = {
+
+    PLASMA      = {
+      name                    = [[Medium Plasma Cannon]],
+      accuracy                = 200,
+      areaOfEffect            = 160,
+      craterBoost             = 1,
+      craterMult              = 2,
+
+      damage                  = {
+        default = 350,
+        planes  = 350,
+        subs    = 17.5,
+      },
+
+      explosionGenerator      = [[custom:bigbulletimpact]],
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      noSelfDamage            = true,
+      range                   = 350,
+      reloadtime              = 2,
+      soundHit                = [[weapon/cannon/cannon_hit2]],
+      soundStart              = [[weapon/cannon/heavy_cannon]],
+      startsmoke              = [[1]],
+      targetMoveError         = 0.3,
+      turret                  = true,
+      weaponType              = [[Cannon]],
+      weaponVelocity          = 330,
+    },
+	
+	MISSILE      = {
+      name                    = [[Destroyer Missiles]],
+      areaOfEffect            = 48,
+      cegTag                  = [[missiletrailyellow]],
+      craterBoost             = 1,
+      craterMult              = 2,
+
+      damage                  = {
+        default = 160,
+        subs    = 160,
+      },
+
+      edgeEffectiveness       = 0.5,
+      fireStarter             = 100,
+	  fixedLauncher			  = true,	  
+      flighttime              = 4,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 2,
+      model                   = [[wep_m_hailstorm.s3o]],
+      noSelfDamage            = true,
+      range                   = 420,
+      reloadtime              = 2,
+      smokeTrail              = true,
+      soundHit                = [[weapon/missile/missile_fire12]],
+      soundStart              = [[weapon/missile/missile_fire10]],
+      startVelocity			  = 100,
+      tolerance               = 4000,
+	  tracks				  = true,
+	  turnrate				  = 30000,
+	  turret				  = true,	  
+	  waterWeapon			  = true,
+      weaponAcceleration      = 300,
+      weaponTimer             = 1,
+      weaponType              = [[StarburstLauncher]],
+      weaponVelocity          = 1800,
+    },
+  },
+
+
+  featureDefs            = {
+
+    DEAD  = {
+      description      = [[Wreckage - Daimyo]],
+      blocking         = false,
+      category         = [[corpses]],
+	  collisionVolumeOffsets = [[0 0 3]],
+	  collisionVolumeScales  = [[32 46 102]],
+	  collisionVolumeTest    = 1,
+	  collisionVolumeType    = [[box]],	  
+      damage           = 3300,
+      energy           = 0,
+      featureDead      = [[HEAP]],
+      footprintX       = 5,
+      footprintZ       = 5,
+      height           = [[4]],
+      hitdensity       = [[100]],
+      metal            = 280,
+      object           = [[destroyer_dead.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 280,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+    
+    HEAP  = {
+      description      = [[Debris - Daimyo]],
+      blocking         = false,
+      category         = [[heaps]],
+      damage           = 3300,
+      energy           = 0,
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 4,
+      footprintZ       = 4,
+      hitdensity       = [[100]],
+      metal            = 140,
+      object           = [[debris4x4b.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 140,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+  },
+
+}
+
+return lowerkeys({ destroyer = unitDef })
