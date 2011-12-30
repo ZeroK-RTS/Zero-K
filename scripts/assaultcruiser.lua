417c6979d8fcef3c849a67f4c96e2ab2bab4eb7d
KingRaptor <kingraptorowns@gmail.com>
2011-12-30 09:56:50 +0000

    Assault cruiser (/give assaultcruiser). Wants AO rebake. Most likely not balanced.
    svn path=/trunk/mods/zk/; revision=4827

---
+++ scripts/assaultcruiser.lua
@@ -0,0 +1,154 @@
+include 'constants.lua'
+
+--------------------------------------------------------------------
+--pieces
+--------------------------------------------------------------------
+local base, lowerhull, upperhull, antenna, radarbase, radardish = piece('base', 'lowerhull', 'upperhull', 'antenna', 'radarbase', 'radardish')
+local wake1, wake2 = piece('wake1', 'wake2')
+
+local turretPrefixes = {"fl", "fr", "sl", "sr", "ml", "mr"}
+local turretPieces = {}
+local gunStates = {}
+
+-- generate turretPieces table
+for i=2,7 do
+	turretPieces[i] = {}
+	local array = turretPieces[i]
+	array.turret = piece(turretPrefixes[i-1].."turret")
+	array.pivot = piece(turretPrefixes[i-1].."pivot")
+	array.barrel = {}
+	array.barrel[0] = piece(turretPrefixes[i-1].."barrel1")
+	array.barrel[1] = piece(turretPrefixes[i-1].."barrel2")
+	array.flare = {}
+	array.flare[0] = piece(turretPrefixes[i-1].."flare1")
+	array.flare[1] = piece(turretPrefixes[i-1].."flare2")
+	
+	gunStates[i] = 0
+end
+
+--------------------------------------------------------------------
+--constants
+--------------------------------------------------------------------
+smokePiece = {hull, turret1, turret2, turret3}
+
+local SIG_RESTORE = 1
+local SIG_MOVE = 2
+
+local RESTORE_DELAY = 5000
+
+local TURRET_PITCH_SPEED = math.rad(90)
+local TURRET_YAW_SPEED = math.rad(180)
+local RECOIL_DISTANCE =	-3
+local RECOIL_RESTORE_SPEED = 2.5
+
+
+--------------------------------------------------------------------
+--variables
+--------------------------------------------------------------------
+local gun = {1, 1, 1}
+local gunHeading = {0, 0, 0}
+
+local dead = false
+
+function script.Create()
+	Turn(turretPieces[4].turret, y_axis, math.rad(180))
+	Turn(turretPieces[5].turret, y_axis, math.rad(180))
+	Spin(radardish, y_axis, math.rad(100))
+	StartThread(SmokeUnit)
+end
+
+local function RestoreAfterDelay()
+	Signal(SIG_RESTORE)
+	SetSignalMask(SIG_RESTORE)
+	Sleep(RESTORE_DELAY)
+	if dead then return false end
+	for i=2,7 do
+		if i == 4 or i == 5 then Turn( turretPieces[i].turret , y_axis, math.pi, math.rad(35) )
+		else Turn( turretPieces[i].turret , y_axis, 0, math.rad(35) ) end
+		Turn( turretPieces[i].pivot , x_axis, 0, math.rad(15) )
+	end
+end
+
+local function Wake()
+	Signal(SIG_MOVE)
+	SetSignalMask(SIG_MOVE)
+	while true do
+		EmitSfx( wake1,  2 )
+		EmitSfx( wake2,  2 )
+		Sleep( 200)
+	end
+end
+
+function script.StartMoving()
+	StartThread(Wake)
+end
+
+function script.StopMoving()
+	Signal(SIG_Move)
+end
+
+function script.AimWeapon(num, heading, pitch)
+	if dead or (num == 1) then return false end
+	local SIG_Aim = 2^num
+	Signal(SIG_Aim)
+	SetSignalMask(SIG_Aim)
+	Turn( turretPieces[num].turret, y_axis, heading, TURRET_YAW_SPEED )
+	Turn( turretPieces[num].pivot , x_axis, -pitch, TURRET_PITCH_SPEED  )
+	WaitForTurn(turretPieces[num].turret, y_axis)
+	WaitForTurn(turretPieces[num].pivot, x_axis)	
+	StartThread(RestoreAfterDelay)
+	return true
+end
+
+function script.FireWeapon(num)
+end
+
+function script.Shot(num)
+	local gun_num = gunStates[num]
+	local barrel = turretPieces[num].barrel[gun_num]
+	local flare = turretPieces[num].flare[gun_num]
+	local toEmit = (num <= 5) and 1024 or 1025 
+	EmitSfx(flare, toEmit)
+	if (num <= 5) then
+		Move( barrel, z_axis, RECOIL_DISTANCE  )
+		Move( barrel, z_axis, 0 , RECOIL_RESTORE_SPEED )
+	end
+	gunStates[num] = 1 - gunStates[num]
+end
+
+function script.AimFromWeapon(num)
+	if num == 1 then return base end
+	return turretPieces[num].turret
+end
+
+function script.QueryWeapon(num)
+	if num == 1 then return base end
+	return turretPieces[num].flare[gunStates[num]]
+end
+
+local function ExplodeTurret(num, severity)
+end
+
+--not actually called; copypasta into Killed()
+local function DeathAnim()
+	dead = true
+end
+
+function script.Killed(recentDamage, maxHealth)
+	local severity = recentDamage/maxHealth
+	if severity < 0.25 then
+		DeathAnim()
+		Explode(lowerhull, sfxNone)
+		Explode(upperhull, sfxNone)
+		return 1
+	elseif severity < 0.5 then
+		DeathAnim()
+		Explode(lowerhull, sfxNone)
+		Explode(upperhull, sfxNone)
+		return 1	
+	else
+		Explode(lowerhull, sfxShatter)
+		Explode(upperhull, sfxShatter)
+		return 2
+	end
+end
---
+++ units/assaultcruiser.lua
@@ -0,0 +1,294 @@
+unitDef = {
+  unitname               = [[assaultcruiser]],
+  name                   = [[Vanquisher]],
+  description            = [[Heavy Cruiser (Assault)]],
+  acceleration           = 0.0768,
+  activateWhenBuilt      = true,
+  brakeRate              = 0.042,
+  buildAngle             = 16384,
+  buildCostEnergy        = 1600,
+  buildCostMetal         = 1600,
+  builder                = false,
+  buildPic               = [[assaultcruiser.png]],
+  buildTime              = 1600,
+  canAttack              = true,
+  canGuard               = true,
+  canMove                = true,
+  canPatrol              = true,
+  category               = [[SHIP]],
+  collisionVolumeOffsets = [[0 4 -2]],
+  collisionVolumeScales  = [[72 42 128]],
+  collisionVolumeTest    = 1,
+  collisionVolumeType    = [[Box]],
+  corpse                 = [[DEAD]],
+
+  customParams           = {
+    --description_fr = [[Corvette d'Assaut/Pillage]],
+	--description_de = [[Korvette (Sturmangriff/Raider)]],
+    helptext       = [[The Vanquisher cruiser boasts excellent armor and lethal close-in firepower. Its gauss cannons slice through surface targets like butter, while its missiles support it against enemies above and below the water surface. Its short range and lack of anti-air firepower leaves it vulnerable to standoff units and aircraft.]],
+    --helptext_fr    = [[La corvette est ? la fois bon-march? et rapide. Son blindage moyen et sa forte puissance de feu laser en font un bon compromis, mais est vuln?rable aux attaques sousmarines. ]],
+	--helptext_de    = [[Der Typhoon ist ein Schiff, welches Geschwindigkeite, Panzerung und Feuerkraft verhältnismäßig günstig verbindet - zumindest für ein Schiff. Nutze Korvetten gegen alles auf der Oberfläche, achte aber auf feindliche U-Boote.]],
+  },
+
+  explodeAs              = [[BIG_UNIT]],
+  floater                = true,
+  footprintX             = 5,
+  footprintZ             = 5,
+  iconType               = [[heavyship]],
+  idleAutoHeal           = 5,
+  idleTime               = 1800,
+  mass                   = 237,
+  maxDamage              = 9600,
+  maxVelocity            = 2.7,
+  minCloakDistance       = 75,
+  minWaterDepth          = 15,
+  movementClass          = [[BOAT6]],
+  noAutoFire             = false,
+  noChaseCategory        = [[TERRAFORM FIXEDWING SATELLITE SUB SINK]],
+  objectName             = [[cremcrus.s3o]],
+  script				 = [[assaultcruiser.lua]],
+  seismicSignature       = 4,
+  selfDestructAs         = [[BIG_UNIT]],
+
+  sfxtypes               = {
+
+    explosiongenerators = {
+      [[custom:brawlermuzzle]],
+      [[custom:emg_shells_l]],
+    },
+
+  },
+
+  side                   = [[CORE]],
+  sightDistance          = 500,
+  turninplace            = 0,
+  turnRate               = 260,
+  workerTime             = 0,
+
+  weapons                = {
+
+    {
+      def                = [[FAKELASER]],
+      mainDir            = [[0 0 1]],
+      maxAngleDif        = 120,
+    },
+	
+    {
+      def                = [[GAUSS]],
+      mainDir            = [[-1 0 1]],
+      maxAngleDif        = 240,
+	  badTargetCategory	 = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+	
+    {
+      def                = [[GAUSS]],
+      mainDir            = [[1 0 1]],
+      maxAngleDif        = 240,
+	  badTargetCategory	 = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+	
+    {
+      def                = [[GAUSS]],
+      mainDir            = [[-1 0 -1]],
+      maxAngleDif        = 240,
+	  badTargetCategory	 = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+	
+    {
+      def                = [[GAUSS]],
+      mainDir            = [[1 0 -1]],
+      maxAngleDif        = 240,
+	  badTargetCategory	 = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },	
+	
+    {
+      def                = [[MISSILE]],
+      mainDir            = [[-1 0 0]],
+      maxAngleDif        = 240,
+	  badTargetCategory	 = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER SUB]],
+    },	
+
+    {
+      def                = [[MISSILE]],
+      mainDir            = [[1 0 0]],
+      maxAngleDif        = 240,
+	  badTargetCategory	 = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER SUB]],
+    },		
+	
+  },
+
+
+  weaponDefs             = {
+  
+    FAKELASER     = {
+      name                    = [[Fake Laser]],
+      areaOfEffect            = 12,
+      beamlaser               = 1,
+      beamTime                = 0.1,
+      coreThickness           = 0.5,
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      damage                  = {
+        default = 0,
+        subs    = 0,
+      },
+
+      duration                = 0.11,
+      fireStarter             = 70,
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      laserFlareSize          = 5.53,
+      range                   = 400,
+      reloadtime              = 0.11,
+      rgbColor                = [[0 1 0]],
+      texture1                = [[largelaser]],
+      texture2                = [[flare]],
+      texture3                = [[flare]],
+      texture4                = [[smallflare]],
+      thickness               = 5,
+      turret                  = true,
+      weaponType              = [[BeamLaser]],
+    },	
+	
+    GAUSS = {
+      name                    = [[Gauss Cannon]],
+      alphaDecay              = 0.12,
+      areaOfEffect            = 16,
+      bouncerebound           = 0.15,
+      bounceslip              = 1,
+      burst                   = 2,
+      burstrate               = 0.4,
+      cegTag                  = [[gauss_tag_l]],
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      damage                  = {
+        default = 200,
+        planes  = 200,
+        subs    = 10,
+      },
+
+      explosionGenerator      = [[custom:gauss_hit_m]],
+      groundbounce            = 1,
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0,
+      interceptedByShieldType = 0,
+      minbarrelangle          = [[-15]],
+      noExplode               = true,
+      numbounce               = 40,
+      range                   = 450,
+      reloadtime              = 4,
+      rgbColor                = [[0.5 1 1]],
+      separation              = 0.5,
+      size                    = 0.8,
+      sizeDecay               = -0.1,
+      soundHit                = [[weapon/gauss_hit]],
+      soundStart              = [[weapon/gauss_fire]],
+      sprayangle              = 800,
+      stages                  = 32,
+      startsmoke              = [[1]],
+      turret                  = true,
+      waterbounce             = 1,
+      weaponType              = [[Cannon]],
+      weaponVelocity          = 2400,
+    },
+	
+	MISSILE      = {
+      name                    = [[Cruiser Missiles]],
+      areaOfEffect            = 48,
+	  burst					  = 2,
+	  burstRate				  = 0.25,
+      cegTag                  = [[missiletrailyellow]],
+      craterBoost             = 1,
+      craterMult              = 2,
+
+      damage                  = {
+        default = 160,
+        subs    = 160,
+      },
+
+      edgeEffectiveness       = 0.5,
+      fireStarter             = 100,
+	  fixedLauncher			  = true,	  
+      flighttime              = 4,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 2,
+      model                   = [[wep_m_hailstorm.s3o]],
+      noSelfDamage            = true,
+      range                   = 420,
+      reloadtime              = 2,
+      smokeTrail              = true,
+      soundHit                = [[weapon/missile/missile_fire12]],
+      soundStart              = [[weapon/missile/missile_fire10]],
+      startVelocity			  = 300,
+      tolerance               = 4000,
+	  tracks				  = true,
+	  trajectoryHeight		  = 0.5,
+	  turnrate				  = 30000,
+	  turret				  = true,	  
+	  waterWeapon			  = true,
+      weaponAcceleration      = 300,
+      weaponTimer             = 1,
+      weaponType              = [[MissileLauncher]],
+      weaponVelocity          = 600,
+    },	
+  },
+
+
+  featureDefs            = {
+
+    DEAD = {
+      description      = [[Wreckage - Vanquisher]],
+      blocking         = false,
+      category         = [[corpses]],
+      damage           = 9600,
+      energy           = 0,
+      featureDead      = [[HEAP]],
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 5,
+      footprintZ       = 5,
+      height           = [[20]],
+      hitdensity       = [[100]],
+      metal            = 640,
+      object           = [[cremcrus_dead.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 640,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+
+    HEAP = {
+      description      = [[Debris - Vanquisher]],
+      blocking         = false,
+      category         = [[heaps]],
+      damage           = 9600,
+      energy           = 0,
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 4,
+      footprintZ       = 4,
+      hitdensity       = [[100]],
+      metal            = 320,
+      object           = [[debris4x4a.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 320,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+  },
+
+}
+
+return lowerkeys({ assaultcruiser = unitDef })
