a591f915aa5a47bc11cd6dd3ed8e12d15e230083
KingRaptor <kingraptorowns@gmail.com>
2012-01-21 17:19:20 +0000

    VERSION{v0.9.9.1}
    svn path=/trunk/mods/zk/; revision=5136

--- LuaRules/Gadgets/lups_flame_jitter.lua
+++ LuaRules/Gadgets/lups_flame_jitter.lua
@@ -89,6 +89,8 @@ else
     local speedx,speedy,speedz = Spring.GetUnitVelocity(unitID)
     local partpos = "x*delay,y*delay,z*delay|x="..speedx..",y="..speedy..",z="..speedz
 
+	local altFlameTexture = wd.customParams.altflametex	-- FIXME: more elegant solution when this is actually implemented (as in, one that doesn't rely on different unitdef)
+	
     particleList[particleCnt] = {
       class        = 'JitterParticles2',
       colormap     = { {1,1,1,1},{1,1,1,1} },
@@ -151,7 +153,7 @@ else
       sizeExp      = 0.7,
 
       --texture     = "bitmaps/smoke/smoke06.tga",
-      texture     = "bitmaps/GPL/flame.png",
+      texture     = altFlameTexture and "bitmaps/GPL/flame_alt.png" or "bitmaps/GPL/flame.png",
     }
     particleCnt = particleCnt + 1
 
@@ -185,7 +187,7 @@ else
       sizeExp      = 0.65,
 
       --texture     = "bitmaps/smoke/smoke06.tga",
-      texture     = "bitmaps/GPL/flame.png",
+      texture     = altFlameTexture and "bitmaps/GPL/flame_alt.png" or "bitmaps/GPL/flame.png",
     }
     particleCnt = particleCnt + 1
 
