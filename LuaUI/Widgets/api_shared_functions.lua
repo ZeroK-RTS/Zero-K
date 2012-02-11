99968d2d5cbc9a35282fdb7896394087b209e649
KingRaptor <kingraptorowns@gmail.com>
2012-02-11 08:42:19 +0000

    api_shared_functions: functionality for writing a Lua table to file
    svn path=/trunk/mods/zk/; revision=5299

--- LuaUI/Widgets/api_shared_functions.lua
+++ LuaUI/Widgets/api_shared_functions.lua
@@ -40,7 +40,62 @@ local function GetBuildIconFrame(udef)
   end
 end 
 
+--------------------------------------------------------------------------------
+-- table writing funcs
+--------------------------------------------------------------------------------
+local function WriteIndents(num)
+	local str = ""
+	for i=1, num do
+		str = str .. "\t"
+	end
+	return str
+end
 
+local keywords = {
+	["repeat"] = true,
+}
+
+local function WriteTable(array, numIndents, endOfFile)
+	local str = ""	--WriteIndents(numIndents)
+	str = str .. "{\n"
+	for i,v in pairs(array) do
+		str = str .. WriteIndents(numIndents + 1)
+		if type(i) == "number" then
+			str = str .. "[" .. i .. "] = "
+		elseif keywords[i] then
+			str = str .. [[["]] .. i .. [["] ]] .. "= "
+		else
+			str = str .. i .. " = "
+		end
+		
+		if type(v) == "table" then
+			str = str .. WriteTable(v, numIndents + 1)
+		elseif type(v) == "boolean" then
+			str = str .. tostring(v) .. ",\n"
+		elseif type(v) == "string" then
+			str = str .. [["]] .. v .. [["]] .. ",\n"
+		else
+			str = str .. v .. ",\n"
+		end
+	end
+	str = str ..WriteIndents(numIndents) .. "}"
+	if not endOfFile then
+		str = str .. ",\n"
+	end
+	
+	return str
+end
+
+function WG.PrintTable(f, table)
+	file = io.open (f, "w")
+	if (file== nil) then Spring.Echo ("could not open file for writing!") return end
+	file:write(WriteTable(table, 0, true))
+	file:flush()
+	file:close()
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 function widget:Initialize()
   WG.GetBuildIconFrame = GetBuildIconFrame
 end
