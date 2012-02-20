2f20e678773b8c16b5cd2aac55fcb209a158e196
KingRaptor <kingraptorowns@gmail.com>
2012-02-20 12:38:19 +0000

    Grizzly buildicon, model biggification, script completion.
    svn path=/trunk/mods/zk/; revision=5408

--- scripts/amphassault.lua
+++ scripts/amphassault.lua
@@ -1,9 +1,18 @@
 include "constants.lua"
 
-local base, body, turret, rbarrel1, rbarrel2, lbarrel1, lbarrel2 = piece('base', 'body', 'turret', 'rbarrel1', 'rbarrel2', 'lbarrel1', 'lbarrel2')
+local base, body, turret, rbarrel1, rbarrel2, lbarrel1, lbarrel2, rflare, lflare = piece('base', 'body', 'turret', 'rbarrel1', 'rbarrel2', 'lbarrel1', 'lbarrel2', 'rflare', 'lflare')
 local rfleg, rffoot, lfleg, lffoot, rbleg, rbfoot, lbleg, lbfoot =  piece('rfleg', 'rffoot', 'lfleg', 'lffoot', 'rbleg', 'rbfoot', 'lbleg', 'lbfoot')
 
 local SIG_WALK = 1
+local SIG_AIM = 2
+local SIG_RESTORE = 4
+
+local gunPieces = {
+    [0] = {flare = lflare, recoil = lbarrel2},
+    [1] = {flare = rflare, recoil = rbarrel2},
+
+}
+local gun_1 = 0
 
 local function Walk()
 	Signal(SIG_WALK)
@@ -121,18 +130,69 @@ function script.StopMoving()
 	
 end
 
-
-function script.Killed(recentDamage, maxHealth)
-	return 0
-end
-
 function script.Create()
-Spring.MoveCtrl.SetGroundMoveTypeData(unitID, {maxSpeed = 10})
-
 	Turn( rfleg, x_axis, math.rad(0))
 	Turn( rffoot, x_axis, math.rad(0))
 	
 	Turn( rbleg, x_axis, math.rad(0))
 	Turn( rbfoot, x_axis, math.rad(0))
-	
+end
+
+function script.QueryWeapon(num)
+    return gunPieces[gun_1].flare
+end
+
+function script.AimFromWeapon(num)
+    return turret
+end
+
+local function RestoreAfterDelay()
+    Signal(SIG_RESTORE)
+    SetSignalMask(SIG_RESTORE)
+    Sleep(6000)
+    Turn(turret, y_axis, 0, math.rad(90))
+    Turn(lbarrel1, x_axis, 0, math.rad(45))
+    Turn(lbarrel2, x_axis, 0, math.rad(45))    
+end
+
+function script.AimWeapon(num, heading, pitch)
+    Signal(SIG_AIM)
+    SetSignalMask(SIG_AIM)
+    Turn(turret, y_axis, heading, math.rad(180))
+    Turn(lbarrel1, x_axis, -pitch, math.rad(90))
+    Turn(rbarrel1, x_axis, -pitch, math.rad(90))
+    WaitForTurn(turret, y_axis)
+    WaitForTurn(rbarrel1, x_axis)
+    WaitForTurn(lbarrel1, x_axis)
+    StartThread(RestoreAfterDelay)
+    return true
+end
+
+function script.FireWeapon(num)
+    EmitSfx(gunPieces[gun_1].flare, 1024)
+    Move(gunPieces[gun_1].recoil, z_axis, -10)
+    Move(gunPieces[gun_1].recoil, z_axis, 0, 5)
+    gun_1 = 1 - gun_1
+end
+
+-- should also explode the leg pieces but I really cba...
+function script.Killed(recentDamage, maxHealth)
+    local severity = recentDamage/maxHealth
+    if severity <= 50 then
+        Explode(turret, sfxNone)
+        Explode(body, sfxNone)
+        return 1
+    elseif severity <= 99 then
+        Explode(body, sfxShatter)
+        Explode(turret, sfxShatter)
+        Explode(lbarrel1, sfxFall + sfxSmoke)
+        Explode(rbarrel2, sfxFall + sfxSmoke)
+        return 2
+    else
+        Explode(body, sfxShatter)
+        Explode(turret, sfxShatter)
+        Explode(lbarrel1, sfxFall + sfxSmoke + sfxFire + sfxExplode)
+        Explode(rbarrel2, sfxFall + sfxSmoke + sfxFire + sfxExplode)    
+        return 2
+    end
 end
