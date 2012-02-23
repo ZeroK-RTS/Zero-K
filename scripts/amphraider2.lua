61dbcdfaaf1e342daec7fe2b3606098e5d12d1cc
KingRaptor <kingraptorowns@gmail.com>
2012-02-23 12:09:00 +0000

    amphraider2 (needs: better fire sound and four-stroke walkscript)
    svn path=/trunk/mods/zk/; revision=5434

---
+++ scripts/amphraider2.lua
@@ -0,0 +1,163 @@
+include "constants.lua"
+
+local base, pelvis, torso = piece('base', 'pelvis', 'torso')
+local rthigh, rcalf, rfoot, lthigh, lcalf, lfoot = piece('rthigh', 'rcalf', 'rfoot', 'lthigh', 'lcalf', 'lfoot')
+local rshoulder, rgun, rflare, lshoulder, lgun, lflare = piece('rshoulder', 'rgun', 'rflare', 'lshoulder', 'lgun', 'lflare')
+
+local firepoints = {[0] = lflare, [1] = rflare}
+
+smokePiece = {torso}
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+local PACE = 2
+
+local THIGH_FRONT_ANGLE = -math.rad(50)
+local THIGH_FRONT_SPEED = math.rad(60) * PACE
+local THIGH_BACK_ANGLE = math.rad(30)
+local THIGH_BACK_SPEED = math.rad(60) * PACE
+local CALF_FRONT_ANGLE = math.rad(-20)
+local CALF_FRONT_SPEED = math.rad(90) * PACE
+local CALF_BACK_ANGLE = math.rad(10)
+local CALF_BACK_SPEED = math.rad(90) * PACE
+
+local ARM_FRONT_ANGLE = -math.rad(20)
+local ARM_FRONT_SPEED = math.rad(22.5) * PACE
+local ARM_BACK_ANGLE = math.rad(10)
+local ARM_BACK_SPEED = math.rad(22.5) * PACE
+local FOREARM_FRONT_ANGLE = -math.rad(40)
+local FOREARM_FRONT_SPEED = math.rad(45) * PACE
+local FOREARM_BACK_ANGLE = math.rad(10)
+local FOREARM_BACK_SPEED = math.rad(45) * PACE
+
+local SIG_WALK = 1
+local SIG_AIM1 = 2
+local SIG_AIM2 = 4
+local SIG_RESTORE = 8
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+local gun_1 = 1
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+local function Walk()
+	Signal(SIG_WALK)
+	SetSignalMask(SIG_WALK)
+	while true do
+		--left leg up, right leg back
+		Turn(lthigh, x_axis, THIGH_FRONT_ANGLE, THIGH_FRONT_SPEED)
+		Turn(lcalf, x_axis, CALF_FRONT_ANGLE, CALF_FRONT_SPEED)
+		Turn(rthigh, x_axis, THIGH_BACK_ANGLE, THIGH_BACK_SPEED)
+		Turn(rcalf, x_axis, CALF_BACK_ANGLE, CALF_BACK_SPEED)
+		WaitForTurn(lthigh, x_axis)
+		Sleep(0)
+		
+		--right leg up, left leg back
+		Turn(lthigh, x_axis,  THIGH_BACK_ANGLE, THIGH_BACK_SPEED)
+		Turn(lcalf, x_axis, CALF_BACK_ANGLE, CALF_BACK_SPEED)
+		Turn(rthigh, x_axis, THIGH_FRONT_ANGLE, THIGH_FRONT_SPEED)
+		Turn(rcalf, x_axis, CALF_FRONT_ANGLE, CALF_FRONT_SPEED)
+		WaitForTurn(rthigh, x_axis)		
+		Sleep(0)
+	end
+end
+
+function script.StartMoving()
+	StartThread(Walk)
+end
+
+function script.StopMoving()
+	Signal(SIG_WALK)
+	Turn( rthigh , x_axis, 0, math.rad(80)*PACE  )
+	Turn( rcalf , x_axis, 0, math.rad(120)*PACE  )
+	Turn( rfoot , x_axis, 0, math.rad(80)*PACE  )
+	Turn( lthigh , x_axis, 0, math.rad(80)*PACE  )
+	Turn( lcalf , x_axis, 0, math.rad(80)*PACE  )
+	Turn( lfoot , x_axis, 0, math.rad(80)*PACE  )
+	Turn( pelvis , z_axis, 0, math.rad(20)*PACE  )
+	Move( pelvis , y_axis, 0, 12*PACE )
+end
+
+function script.Create()
+	StartThread(SmokeUnit)	
+end
+
+local function RestoreAfterDelay()
+	Signal(SIG_RESTORE)
+	SetSignalMask(SIG_RESTORE)
+	Sleep(5000)
+	Turn( torso, y_axis, 0, math.rad(65) )
+end
+
+function script.AimFromWeapon()
+	return torso
+end
+
+function script.AimWeapon(num, heading, pitch)
+	if num == 1 then
+		Signal(SIG_AIM1)
+		SetSignalMask(SIG_AIM1)
+		Turn( torso, y_axis, heading, math.rad(360) )
+		Turn( lshoulder, x_axis, -pitch, math.rad(150) )
+		Turn( rshoulder, x_axis, -pitch, math.rad(150) )
+		WaitForTurn(torso, y_axis)
+		WaitForTurn(lshoulder, x_axis)
+		StartThread(RestoreAfterDelay)
+		return true
+	end
+end
+
+function script.QueryWeapon(num)
+	return firepoints[gun_1]
+end
+
+function script.FireWeapon(num)
+end
+
+function script.Shot(num)
+	gun_1 = 1 - gun_1
+end
+
+function script.Killed(recentDamage, maxHealth)
+	local severity = recentDamage/maxHealth
+	if severity >= .25  then
+		Explode(lfoot, sfxNone)
+		Explode(lcalf, sfxNone)
+		Explode(lthigh, sfxNone)
+		Explode(pelvis, sfxNone)
+		Explode(rfoot, sfxNone)
+		Explode(rcalf, sfxNone)
+		Explode(rthigh, sfxNone)
+		Explode(torso, sfxNone)
+		return 1
+	elseif severity >= .50  then
+		Explode(lfoot, sfxFall)
+		Explode(lcalf, sfxFall)
+		Explode(lthigh, sfxFall)
+		Explode(pelvis, sfxFall)
+		Explode(rfoot, sfxFall)
+		Explode(rcalf, sfxFall)
+		Explode(rthigh, sfxFall)
+		Explode(torso, sfxShatter)
+		return 1
+	elseif severity >= .99  then
+		Explode(lfoot, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(lcalf, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(lthigh, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(pelvis, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rfoot, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rcalf, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rthigh, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(torso, sfxShatter)
+		return 2
+	else
+		Explode(lfoot, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(lcalf, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(lthigh, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(pelvis, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rfoot, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rcalf, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rthigh, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(torso, sfxShatter + sfxExplode )
+		return 2
+	end
+end
---
+++ units/amphraider2.lua
@@ -0,0 +1,170 @@
+unitDef = {
+  unitname               = [[amphraider2]],
+  name                   = [[Ray]],
+  description            = [[Amphibious Raider Bot]],
+  acceleration           = 0.2,
+  activateWhenBuilt      = true,
+  amphibious             = [[1]],
+  brakeRate              = 0.4,
+  buildCostEnergy        = 200,
+  buildCostMetal         = 200,
+
+  buildoptions           = {
+  },
+
+  buildPic               = [[amphraider2.png]],
+  buildTime              = 200,
+  canAttack              = true,
+  canGuard               = true,
+  canMove                = true,
+  canPatrol              = true,
+  canstop                = [[1]],
+  category               = [[SINK]],
+  collisionVolumeTest    = 1,
+  corpse                 = [[DEAD]],
+
+  customParams           = {
+  },
+
+  explodeAs              = [[BIG_UNITEX]],
+  footprintX             = 2,
+  footprintZ             = 2,
+  hideDamage             = false,
+  iconType               = [[walkerraider]],
+  idleAutoHeal           = 5,
+  idleTime               = 1800,
+  leaveTracks            = true,
+  mass                   = 411,
+  maxDamage              = 620,
+  maxSlope               = 36,
+  maxVelocity            = 2.4,
+  maxWaterDepth          = 5000,
+  minCloakDistance       = 75,
+  movementClass          = [[AKBOT2]],
+  noChaseCategory        = [[TERRAFORM SATELLITE FIXEDWING GUNSHIP HOVER SHIP SWIM SUB LAND FLOAT SINK]],
+  objectName             = [[amphraider2.s3o]],
+  script                 = [[amphraider2.lua]],
+  seismicSignature       = 4,
+  selfDestructAs         = [[BIG_UNITEX]],
+
+  sfxtypes               = {
+    explosiongenerators = {
+    },
+  },
+
+  side                   = [[ARM]],
+  sightDistance          = 500,
+  sonarDistance          = 300,
+  trackOffset            = 0,
+  trackStrength          = 8,
+  trackStretch           = 1,
+  trackType              = [[ComTrack]],
+  trackWidth             = 22,
+  turnRate               = 1200,
+  upright                = true,
+
+  weapons                = {
+    {
+      def                = [[WATERCANNON]],
+      badTargetCategory  = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+
+  },
+
+
+  weaponDefs             = {
+
+    WATERCANNON = {
+      name                    = [[Water Cannon]],
+      alphaDecay              = 0.1,
+      areaOfEffect            = 8,
+      --cegTag		      = [[torpedo_trail]],
+      colormap                = [[0.6 0.8 1 0.7   0.6 0.8 1 1    0.6 0.8 1 0.01    0.6 0.8 1 0.7]],
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      damage                  = {
+        default = 6,
+        planes  = 6,
+        subs    = 0.3,
+      },
+
+      explosionGenerator      = [[custom:NONE]],
+      impactOnly              = true,
+      impulseBoost            = 15,
+      impulseFactor           = 60,
+      intensity               = 0.7,
+      interceptedByShieldType = 1,
+      myGravity		      = 0.2,
+      noGap                   = false,
+      noSelfDamage            = true,
+      projectiles	      = 2,
+      range                   = 270,
+      reloadtime              = 0.1,
+      rgbColor                = [[0.6 0.8 1]],
+      rgbColor2               = [[0.6 0.8 1]],
+      separation              = 1,
+      stages		      = 24,
+      --size                    = 0,
+      sizeDecay               = 0,
+      soundStart              = [[weapon/hiss]],
+      soundStartVolume        = 4,
+      startsmoke              = [[0]],
+      texture1		      = [[wake]],	-- donut work
+      tolerance               = 5000,
+      turret                  = true,
+      weaponTimer             = 0.1,
+      weaponType              = [[Cannon]],
+      weaponVelocity          = 300,
+    },
+
+  },
+
+
+  featureDefs            = {
+
+    DEAD      = {
+      description      = [[Wreckage - Grebe]],
+      blocking         = true,
+      category         = [[corpses]],
+      damage           = 900,
+      energy           = 0,
+      featureDead      = [[HEAP]],
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 3,
+      footprintZ       = 3,
+      height           = [[20]],
+      hitdensity       = [[100]],
+      metal            = 80,
+      object           = [[wreck2x2b.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 80,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+    HEAP      = {
+      description      = [[Debris - Grebe]],
+      blocking         = false,
+      category         = [[heaps]],
+      damage           = 900,
+      energy           = 0,
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      hitdensity       = [[100]],
+      metal            = 40,
+      object           = [[debris2x2c.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 40,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+
+  },
+
+}
+
+return lowerkeys({ amphraider2 = unitDef })
