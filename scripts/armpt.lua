0edae1c9f9172a79d23e9b9707b6f31307ca7cdd
Saktoth <oscar.evans@gmail.com>
2012-03-05 10:32:07 +0000

    New scout boat model.
    svn path=/trunk/mods/zk/; revision=5594

--- scripts/armpt.lua
+++ scripts/armpt.lua
@@ -2,31 +2,28 @@ include "constants.lua"
 
 local base = piece 'base' 
 local turret = piece 'turret' 
-local firepoint1, firepoint2 = piece('firepoint1', 'firepoint2')
 local hull = piece 'hull' 
 local wake1 = piece 'wake1' 
 local wake2 = piece 'wake2'
-
-local flares = {[0] = firepoint1, [1] = firepoint2}
+local missile = piece 'missile' 
+local firepoint = piece 'firepoint' 
+local doorl = piece 'doorl'
+local doorr = piece 'doorr'
 
 smokePiece = {base}
 
 -- Signal definitions
 local SIG_MOVE = 1
-local SIG_AIM = 2
-local SIG_AIM_2 = 4
-local SIG_AIM_3 = 8
 
 local gun_1 = 0
 
 function script.Create()
 	restore_delay = 3000
 	StartThread(SmokeUnit)
-end
-
-local function RestoreAfterDelay()
-	Sleep(restore_delay)
-	Turn( turret , x_axis, 0, math.rad(60) )
+	Turn(turret, x_axis, math.rad(-90), math.rad(10000) )
+	Turn(doorl, z_axis, math.rad(-100), math.rad(240) )
+	Turn(doorr, z_axis, math.rad(100), math.rad(240) )
+	Move(turret, y_axis, 20, 16)
 end
 
 local function Motion()
@@ -47,40 +44,29 @@ function script.StopMoving()
 	Signal( SIG_MOVE)
 end
 
-function script.AimWeapon(num, heading, pitch)
-	if num == 1 then return end
-	Signal( SIG_AIM)
-	SetSignalMask( SIG_AIM)
-	Turn( turret , x_axis, math.rad(-20), math.rad(150) )
-	WaitForTurn(turret, x_axis)
-	StartThread(RestoreAfterDelay)
+function script.AimWeapon1(heading, pitch)
+	return false
+end
+
+function script.AimFromWeapon1()
+	return missile
+end
+
+function script.QueryWeapon1()
+	return missile
+end
+
+function script.AimWeapon2(heading, pitch)
+--	Turn(turret, x_axis, math.rad(-40), math.rad(50) )
 	return true
 end
 
-function script.AimFromWeapon(num)
-	return turret
+function script.AimFromWeapon2()
+	return firepoint
 end
 
-function script.QueryWeapon(num)
-	return flares[gun_1]
-end
-
-function script.Shot(num)
-	--[[
-	--Spring.Echo(num)
-	local num2 = 3
-	if num == 3 then num2 = 2 end
-	--num2 = num2 - 1
-	--local frame = select(3, Spring.GetUnitWeaponState(unitID, num))
-	local frame = Spring.GetUnitWeaponState(unitID, num, "reloadTime")
-	if frame then
-		frame = math.floor(30*frame) + Spring.GetGameFrame()
-		--Spring.Echo("Setting reload frame to "..frame)
-		Spring.SetUnitWeaponState(unitID, num2, "reloadFrame", frame)
-	end
-	]]--
-	EmitSfx(flares[gun_1], 1024)
-	gun_1 = 1 - gun_1
+function script.QueryWeapon2()
+	return firepoint
 end
 
 function script.Killed(recentDamage, maxHealth)
