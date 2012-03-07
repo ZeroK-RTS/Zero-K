1908b1796bf159c5f0d6fc8cbafff47750650c52
Saktoth <oscar.evans@gmail.com>
2012-03-07 11:00:25 +0000

    Amphfloater. (Note: Does not float).
    svn path=/trunk/mods/zk/; revision=5612

---
+++ scripts/amphfloater.lua
@@ -0,0 +1,151 @@
+--linear constant 65536
+
+include "constants.lua"
+
+local base, head, body, barrel, firepoint = piece('base', 'head', 'body', 'barrel', 'firepoint')
+local rthigh, rshin, rfoot, lthigh, lshin, lfoot = piece('rthigh', 'rshin', 'rfoot', 'lthigh', 'lshin', 'lfoot')
+
+smokePiece = {body}
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+local PACE = 2
+
+local THIGH_FRONT_ANGLE = -math.rad(50)
+local THIGH_FRONT_SPEED = math.rad(60) * PACE
+local THIGH_BACK_ANGLE = math.rad(30)
+local THIGH_BACK_SPEED = math.rad(60) * PACE
+local SHIN_FRONT_ANGLE = math.rad(45)
+local SHIN_FRONT_SPEED = math.rad(90) * PACE
+local SHIN_BACK_ANGLE = math.rad(10)
+local SHIN_BACK_SPEED = math.rad(90) * PACE
+
+local ARM_FRONT_ANGLE = -math.rad(20)
+local ARM_FRONT_SPEED = math.rad(22.5) * PACE
+local ARM_BACK_ANGLE = math.rad(10)
+local ARM_BACK_SPEED = math.rad(22.5) * PACE
+local FOREARM_FRONT_ANGLE = -math.rad(40)
+local FOREARM_FRONT_SPEED = math.rad(45) * PACE
+local FOREARM_BACK_ANGLE = math.rad(10)
+local FOREARM_BACK_SPEED = math.rad(45) * PACE
+
+local SIG_WALK = 1
+local SIG_AIM1 = 2
+local SIG_AIM2 = 4
+local SIG_RESTORE = 8
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+local gun_1 = 1
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+local function Walk()
+	Signal(SIG_WALK)
+	SetSignalMask(SIG_WALK)
+	while true do
+		--left leg up, right leg back
+		Turn(lthigh, x_axis, THIGH_FRONT_ANGLE, THIGH_FRONT_SPEED)
+		Turn(lshin, x_axis, SHIN_FRONT_ANGLE, SHIN_FRONT_SPEED)
+		Turn(rthigh, x_axis, THIGH_BACK_ANGLE, THIGH_BACK_SPEED)
+		Turn(rshin, x_axis, SHIN_BACK_ANGLE, SHIN_BACK_SPEED)
+		WaitForTurn(lthigh, x_axis)
+		Sleep(0)
+		
+		--right leg up, left leg back
+		Turn(lthigh, x_axis,  THIGH_BACK_ANGLE, THIGH_BACK_SPEED)
+		Turn(lshin, x_axis, SHIN_BACK_ANGLE, SHIN_BACK_SPEED)
+		Turn(rthigh, x_axis, THIGH_FRONT_ANGLE, THIGH_FRONT_SPEED)
+		Turn(rshin, x_axis, SHIN_FRONT_ANGLE, SHIN_FRONT_SPEED)
+		WaitForTurn(rthigh, x_axis)		
+		Sleep(0)
+	end
+end
+
+function script.StartMoving()
+	StartThread(Walk)
+end
+
+function script.StopMoving()
+	Signal(SIG_WALK)
+	Turn( rthigh , x_axis, 0, math.rad(80)*PACE  )
+	Turn( rshin , x_axis, 0, math.rad(120)*PACE  )
+	Turn( rfoot , x_axis, 0, math.rad(80)*PACE  )
+	Turn( lthigh , x_axis, 0, math.rad(80)*PACE  )
+	Turn( lshin , x_axis, 0, math.rad(80)*PACE  )
+	Turn( lfoot , x_axis, 0, math.rad(80)*PACE  )
+end
+
+function script.Create()
+	StartThread(SmokeUnit)	
+end
+
+local function RestoreAfterDelay()
+	Signal(SIG_RESTORE)
+	SetSignalMask(SIG_RESTORE)
+	Sleep(5000)
+	Turn( head, y_axis, 0, math.rad(65) )
+	Turn( barrel, x_axis, 0, math.rad(65) )
+end
+
+function script.AimFromWeapon()
+	return barrel
+end
+
+function script.AimWeapon(num, heading, pitch)
+	if num == 1 then
+		Signal(SIG_AIM1)
+		SetSignalMask(SIG_AIM1)
+		Turn( head, y_axis, heading, math.rad(360) )
+		Turn( barrel, x_axis, -pitch, math.rad(180) )
+		WaitForTurn(head, y_axis)
+		WaitForTurn(barrel, x_axis)
+		StartThread(RestoreAfterDelay)
+		return true
+	end
+end
+
+function script.QueryWeapon(num)
+	if num == 1 then
+		return firepoint
+	end
+end
+
+function script.Killed(recentDamage, maxHealth)
+	local severity = recentDamage/maxHealth
+	if severity >= .25  then
+		Explode(lfoot, sfxNone)
+		Explode(lshin, sfxNone)
+		Explode(lthigh, sfxNone)
+		Explode(rfoot, sfxNone)
+		Explode(rshin, sfxNone)
+		Explode(rthigh, sfxNone)
+		Explode(body, sfxNone)
+		return 1
+	elseif severity >= .50  then
+		Explode(lfoot, sfxFall)
+		Explode(lshin, sfxFall)
+		Explode(lthigh, sfxFall)
+		Explode(rfoot, sfxFall)
+		Explode(rshin, sfxFall)
+		Explode(rthigh, sfxFall)
+		Explode(body, sfxShatter)
+		return 1
+	elseif severity >= .99  then
+		Explode(lfoot, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(lshin, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(lthigh, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rfoot, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rshin, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rthigh, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(body, sfxShatter)
+		return 2
+	else
+		Explode(lfoot, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(lshin, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(lthigh, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rfoot, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rshin, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(rthigh, sfxFall + sfxSmoke  + sfxFire  + sfxExplode )
+		Explode(body, sfxShatter + sfxExplode )
+		return 2
+	end
+end
---
+++ units/amphfloater.lua
@@ -0,0 +1,165 @@
+unitDef = {
+  unitname               = [[amphfloater]],
+  name                   = [[Flagnacious]],
+  description            = [[Amphibious Floating Bot]],
+  acceleration           = 0.2,
+  activateWhenBuilt      = true,
+  amphibious             = [[1]],
+  brakeRate              = 0.4,
+  buildCostEnergy        = 300,
+  buildCostMetal         = 300,
+
+  buildoptions           = {
+  },
+
+  buildPic               = [[amphraider.png]],
+  buildTime              = 300,
+  canAttack              = true,
+  canGuard               = true,
+  canMove                = true,
+  canPatrol              = true,
+  canstop                = [[1]],
+  category               = [[SINK]],
+  collisionVolumeTest    = 1,
+  corpse                 = [[DEAD]],
+
+  customParams           = {
+  },
+
+  explodeAs              = [[BIG_UNITEX]],
+  footprintX             = 2,
+  footprintZ             = 2,
+  hideDamage             = false,
+  iconType               = [[walkerraider]],
+  idleAutoHeal           = 5,
+  idleTime               = 1800,
+  leaveTracks            = true,
+  mass                   = 411,
+  maxDamage              = 1200,
+  maxSlope               = 36,
+  maxVelocity            = 2,
+  maxWaterDepth          = 5000,
+  minCloakDistance       = 75,
+  movementClass          = [[AKBOT2]],
+  noChaseCategory        = [[TERRAFORM SATELLITE FIXEDWING GUNSHIP HOVER SHIP SWIM SUB LAND FLOAT SINK TURRET]],
+  objectName             = [[can.s3o]],
+  script                 = [[amphfloater.lua]],
+  seismicSignature       = 4,
+  selfDestructAs         = [[BIG_UNITEX]],
+
+  sfxtypes               = {
+    explosiongenerators = {
+    },
+  },
+
+  side                   = [[ARM]],
+  sightDistance          = 500,
+  trackOffset            = 0,
+  trackStrength          = 8,
+  trackStretch           = 1,
+  trackType              = [[ComTrack]],
+  trackWidth             = 22,
+  turnRate               = 1200,
+  upright                = true,
+
+  weapons                = {
+    {
+      def                = [[GRENADE]],
+      badTargetCategory  = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+
+    --{
+    --  def                = [[TORPEDO]],
+    --  badTargetCategory  = [[FIXEDWING]],
+    --  onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+    --},
+  },
+
+
+  weaponDefs             = {
+
+	GRENADE = {
+      name                    = [[Grenade Launcher]],
+      accuracy                = 200,
+      areaOfEffect            = 32,
+      craterBoost             = 1,
+      craterMult              = 2,
+	  
+      damage                  = {
+        default = 240,
+        planes  = 240,
+        subs    = 12,
+      },
+      
+      explosionGenerator      = [[custom:PLASMA_HIT_32]],
+      fireStarter             = 180,
+      impulseBoost            = 0,
+      impulseFactor           = 0.2,
+      interceptedByShieldType = 2,
+      model                   = [[diskball.s3o]],
+      projectiles             = 1,
+      range                   = 360,
+      reloadtime              = 2.2,
+      smokeTrail              = true,
+      soundHit                = [[explosion/ex_med6]],
+      soundHitVolume          = 8,
+      soundStart              = [[weapon/cannon/cannon_fire3]],
+      soundStartVolume        = 2,
+      soundTrigger			= true,
+      sprayangle              = 512,
+      startsmoke              = [[1]],
+      turret                  = true,
+      weaponType              = [[Cannon]],
+      weaponVelocity          = 300,
+	},
+
+  },
+
+
+  featureDefs            = {
+
+    DEAD      = {
+      description      = [[Wreckage - Flagnacious]],
+      blocking         = true,
+      category         = [[corpses]],
+      damage           = 900,
+      energy           = 0,
+      featureDead      = [[HEAP]],
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 3,
+      footprintZ       = 3,
+      height           = [[20]],
+      hitdensity       = [[100]],
+      metal            = 120,
+      object           = [[wreck2x2b.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 120,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+    HEAP      = {
+      description      = [[Debris - Flagnacious]],
+      blocking         = false,
+      category         = [[heaps]],
+      damage           = 900,
+      energy           = 0,
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      hitdensity       = [[100]],
+      metal            = 60,
+      object           = [[debris2x2c.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 60,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+
+  },
+
+}
+
+return lowerkeys({ amphfloater = unitDef })
