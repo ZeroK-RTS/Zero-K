1e1de61d8535d9240afe6e93848c9ee78e37e2f5
luckywaldo7 <luckywaldo7@gmail.com>
2012-03-10 04:00:06 +0000

    Amph icon states. If they suck too much I'll probably just remake them after finals are over.
    svn path=/trunk/mods/zk/; revision=5626

--- LuaUI/Configs/integral_menu_commands.lua
+++ LuaUI/Configs/integral_menu_commands.lua
@@ -257,7 +257,8 @@ local overrides = {
 	[CMD_UNIT_KILL_SUBORDINATES] = {texture = {imageDir .. 'states/capturekill_off.png', imageDir .. 'states/capturekill_on.png'}, text=''},
 	[CMD.TRAJECTORY] = { texture = {imageDir .. 'states/traj_low.png', imageDir .. 'states/traj_high.png'}, text=''},
 	[CMD_AIR_STRAFE] = { texture = {imageDir .. 'states/strafe_off.png', imageDir .. 'states/strafe_on.png'}, text=''},
-}
+	[CMD_UNIT_FLOAT_STATE] = { texture = {imageDir .. 'states/amph_sink.png', imageDir .. 'states/amph_attack.png', imageDir .. 'states/amph_float.png'}, text=''},
+	}
 
 local custom_cmd_actions = {
 
