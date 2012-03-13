79b65503eaf36e626e7a60f480fa268188e13201
GoogleFrog <googl3frog@gmail.com>
2012-03-13 07:10:26 +0000

    Factory Guard toggle for amphib factory.
    svn path=/trunk/mods/zk/; revision=5682

--- LuaUI/Widgets/unit_factory_guard.lua
+++ LuaUI/Widgets/unit_factory_guard.lua
@@ -50,6 +50,7 @@ local factoryDefs = {
 	[UnitDefNames["factoryjump"].id] = 0,
 	[UnitDefNames["factoryveh"].id] = 0,
 	[UnitDefNames["factoryhover"].id] = 0,
+	[UnitDefNames["factoryamph"].id] = 0,
 	[UnitDefNames["factorytank"].id] = 0,
 	[UnitDefNames["factoryplane"].id] = 0,
 	[UnitDefNames["factorygunship"].id] = 0,
