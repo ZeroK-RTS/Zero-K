75f31f50949b8e0be4c66c59a327f4b2916d683c
Saktoth <oscar.evans@gmail.com>
2012-04-21 05:50:48 +0000

    vehaa. Sorta slasherish now. Do whatever to it. Currently has tracks etc but should work as a placeholder if this is what we awnt.
    svn path=/trunk/mods/zk/; revision=6147

---
+++ units/vehaa.lua
@@ -0,0 +1,183 @@
+unitDef = {
+  unitname               = [[vehaa]],
+  name                   = [[Devestator]],
+  description            = [[AA vehicle]],
+  acceleration           = 0.05952,
+  brakeRate              = 0.14875,
+  buildCostEnergy        = 300,
+  buildCostMetal         = 300,
+  builder                = false,
+  buildPic               = [[vehaa.png]],
+  buildTime              = 300,
+  canAttack              = true,
+  canGuard               = true,
+  canMove                = true,
+  canPatrol              = true,
+  canstop                = [[1]],
+  category               = [[LAND]],
+  collisionVolumeOffsets = [[0 0 0]],
+  collisionVolumeScales  = [[38 52 38]],
+  collisionVolumeTest    = 1,
+  collisionVolumeType    = [[cylY]],
+  corpse                 = [[DEAD]],
+
+  customParams           = {
+    helptext       = [[Shoots missiles at enemy air.]],
+  },
+
+  explodeAs              = [[BIG_UNITEX]],
+  footprintX             = 2,
+  footprintZ             = 2,
+  iconType               = [[tankaa]],
+  idleAutoHeal           = 5,
+  idleTime               = 1800,
+  leaveTracks            = true,
+  maneuverleashlength    = [[30]],
+  mass                   = 242,
+  maxDamage              = 1300,
+  maxSlope               = 18,
+  maxVelocity            = 2.9,
+  maxWaterDepth          = 22,
+  minCloakDistance       = 75,
+  movementClass          = [[TANK3]],
+  noAutoFire             = false,
+  noChaseCategory        = [[TERRAFORM LAND SINK TURRET SHIP SATELLITE SWIM FLOAT SUB HOVER]],
+  objectName             = [[vehaa.s3o]],
+  seismicSignature       = 4,
+  selfDestructAs         = [[BIG_UNITEX]],
+  
+  sfxtypes               = {
+
+  explosiongenerators = {
+      [[custom:STORMMUZZLE]],
+      [[custom:STORMBACK]],
+    },
+
+  },
+  
+  side                   = [[CORE]],
+  sightDistance          = 660,
+  smoothAnim             = true,
+  trackOffset            = 6,
+  trackStrength          = 5,
+  trackStretch           = 1,
+  trackType              = [[StdTank]],
+  trackWidth             = 32,
+  turninplace            = 0,
+  turnRate               = 653,
+  upright                = false,
+  workerTime             = 0,
+
+  weapons                       = {
+
+    {
+      def                = [[MISSILE]],
+      badTargetCategory  = [[GUNSHIP]],
+      onlyTargetCategory = [[FIXEDWING GUNSHIP]],
+    },
+
+  },
+
+
+  weaponDefs                    = {
+
+    MISSILE = {
+      name                    = [[Homing Missiles]],
+      areaOfEffect            = 24,
+      burst                   = 2,
+      burstrate               = 1.4,
+      canattackground         = false,
+      cegTag                  = [[missiletrailbluebig]],
+      craterBoost             = 0,
+      craterMult              = 0,
+      cylinderTargetting      = 1,
+
+      damage                  = {
+        default = 40,
+        planes  = 400,
+        subs    = 20,
+      },
+
+      explosionGenerator      = [[custom:FLASH2]],
+      fireStarter             = 70,
+      flightTime              = 3,
+      guidance                = true,
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 2,
+      lineOfSight             = true,
+      metalpershot            = 0,
+      model                   = [[wep_m_phoenix.s3o]],
+      noSelfDamage            = true,
+      range                   = 700,
+      reloadtime              = 7,
+      renderType              = 1,
+      selfprop                = true,
+      smokedelay              = [[0.1]],
+      smokeTrail              = true,
+      soundHit                = [[explosion/ex_med11]],
+      soundStart              = [[weapon/missile/missile_fire3]],
+      startsmoke              = [[1]],
+      startVelocity           = 650,
+      texture2                = [[AAsmoketrail]],
+      tolerance               = 9000,
+      tracks                  = true,
+      turnRate                = 63000,
+      turret                  = true,
+      weaponAcceleration      = 141,
+      weaponTimer             = 5,
+      weaponType              = [[MissileLauncher]],
+      weaponVelocity          = 900,
+    },
+
+  },
+
+
+  featureDefs            = {
+
+    DEAD  = {
+      description      = [[Wreckage - Devestator]],
+      blocking         = true,
+      category         = [[corpses]],
+      damage           = 1200,
+      energy           = 0,
+      featureDead      = [[HEAP]],
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      height           = [[20]],
+      hitdensity       = [[100]],
+      metal            = 150,
+      object           = [[vehaa_dead.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 150,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+
+    HEAP  = {
+      description      = [[Debris - Devestator]],
+      blocking         = false,
+      category         = [[heaps]],
+      damage           = 1200,
+      energy           = 0,
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      height           = [[4]],
+      hitdensity       = [[100]],
+      metal            = 60,
+      object           = [[debris2x2a.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 60,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+  },
+
+}
+
+return lowerkeys({ vehaa = unitDef })
