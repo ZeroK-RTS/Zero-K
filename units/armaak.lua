ZeroK-RTS/Zero-K

commit 8fef34fe8b1044857e0b73843b459d9cc614fd77

Reworked many DPS values to compensate for unintended consequences of XP change. Also made Krow lasers blue; changed Bandit and Venom helptexts.
svn path=/trunk/mods/zk/; revision=3933

units/armaak.lua
@@ -84,9 +84,9 @@ unitDef = {
       cylinderTargetting      = 1,
 
       damage                  = {
-        default = 0.9,
-        planes  = 9,
-        subs    = 0.45,
+        default = 1.25,
+        planes  = 1.25,
+        subs    = 0.675,
       },
 
       endsmoke                = [[0]],
@@ -130,9 +130,9 @@ unitDef = {
       cylinderTargetting      = 1,
 
       damage                  = {
-        default = 1.2,
-        planes  = 12,
-        subs    = 0.6,
+        default = 2,
+        planes  = 20,
+        subs    = 1,
       },
 
       explosionGenerator      = [[custom:flash_teal7]],

effects/beam.lua
@@ -458,6 +458,74 @@ return {
     },
   },
 
+  ["beamweapon_hit_teal"] = {
+    usedefaultexplosions = false,
+    groundflash = {
+      circlealpha        = 1,
+      circlegrowth       = 0.125,
+      flashalpha         = 0.5,
+      flashsize          = 8,
+      ttl                = 64,
+      color = {
+        [1]  = 0,
+        [2]  = 0.4,
+        [3]  = 0.8,
+      },
+    },
+    pikes = {
+      air                = true,
+      class              = [[CExpGenSpawner]],
+      count              = 1,
+      ground             = true,
+      water              = true,
+      properties = {
+        delay              = 0,
+        dir                = [[dir]],
+        explosiongenerator = [[custom:BEAMWEAPON_MUZZLE_TEAL]],
+        pos                = [[0, 0, 0]],
+      },
+    },
+    sparks = {
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      unit               = 1,
+      properties = {
+        airdrag            = 0.97,
+        alwaysvisible      = false,
+        colormap           = [[1 1 0.5 0.01   0.01 0.01 0.005 0.01]],
+        directional        = true,
+        emitrot            = 0,
+        emitrotspread      = 80,
+        emitvector         = [[0, 1, 0]],
+        gravity            = [[0, -0.4, 0]],
+        numparticles       = 64,
+        particlelife       = 15,
+        particlelifespread = 0,
+        particlesize       = 3,
+        particlesizespread = 2,
+        particlespeed      = 6,
+        particlespeedspread = 4,
+        pos                = [[0, 0, 0]],
+        sizegrowth         = 0,
+        sizemod            = 1.0,
+        texture            = [[plasma]],
+      },
+    },
+	
+    steam = {
+      class              = [[CExpGenSpawner]],
+      count              = 16,
+      nounit             = 1,
+      properties = {
+        delay              = [[i1]],
+        dir                = [[dir]],
+        explosiongenerator = [[custom:BEAMWEAPON_HIT_YELLOW_STEAM]],
+        pos                = [[0, 0, 0]],
+      },
+    },
+  },  
+  
   ["beamweapon_muzzle_red"] = {
     usedefaultexplosions = false,
     glow = {
@@ -685,6 +753,44 @@ return {
       },
     },
   },
-  
+ 
+  ["beamweapon_muzzle_teal"] = {
+    usedefaultexplosions = false,
+    glow = {
+      air                = true,
+      class              = [[explspike]],
+      count              = 12,
+      ground             = true,
+      water              = true,
+      properties = {
+        alpha              = 1,
+        alphadecay         = 0.125,
+        alwaysvisible      = false,
+        color              = [[0,0.4,0.8]],
+        dir                = [[-4 r8, -4 r8, -4 r8]],
+        length             = 1,
+        lengthgrowth       = 1,
+        width              = 4,
+      },
+    },
+    white = {
+      air                = true,
+      class              = [[explspike]],
+      count              = 2,
+      ground             = true,
+      water              = true,
+      properties = {
+        alpha              = 1,
+        alphadecay         = 0.125,
+        alwaysvisible      = false,
+        color              = [[1,1,1]],
+        dir                = [[-2 r4, -2 r4, -2 r4]],
+        length             = 1,
+        lengthgrowth       = 1,
+        width              = 2,
+      },
+    },
+  },
+ 
 }
 

gamedata/modularcomms/weapons/flamethrower.lua
@@ -15,9 +15,9 @@ local weaponDef = {
 	},
 
     damage                  = {
-      default = 7,
-      planes  = 7,
-      subs    = 0.0015,
+        default = 8.75,
+        planes  = 8.75,
+        subs    = 0.0875,
     },
 
     explosionGenerator      = [[custom:SMOKE]],

gamedata/modularcomms/weapons/heatray.lua
@@ -3,7 +3,6 @@ local weaponDef = {
 	name                    = [[Heat Ray]],
 	accuracy                = 512,
 	areaOfEffect            = 20,
-	beamWeapon              = true,
 	cegTag                  = [[HEATRAY_CEG]],
 	coreThickness           = 0.5,
 	craterBoost             = 0,
@@ -13,11 +12,10 @@ local weaponDef = {
 		slot = [[5]],
 	},		  
 	
-	-- nominal DPS is a lie, actual value is over 30% higher
 	damage                  = {
-		default = 25,
-		planes  = 25,
-		subs    = 1.25,
+		default = 35,
+		planes  = 35,
+		subs    = 1.75,
 	},
 	
 	duration                = 0.3,
@@ -25,19 +23,16 @@ local weaponDef = {
 	dynDamageInverted       = false,
 	explosionGenerator      = [[custom:HEATRAY_HIT]],
 	fallOffRate             = 1,
-	fireStarter             = 90,
+	fireStarter             = 150,
 	heightMod               = 1,
 	impactOnly              = true,
 	impulseBoost            = 0,
 	impulseFactor           = 0.4,
 	interceptedByShieldType = 1,
-	lineOfSight             = true,
 	lodDistance             = 10000,
-	noSelfDamage            = true,
 	proximityPriority       = 4,
 	range                   = 320,
 	reloadtime              = 0.1,
-	renderType              = 0,
 	rgbColor                = [[1 0.1 0]],
 	rgbColor2               = [[1 1 0.25]],
 	soundStart              = [[weapon/heatray_fire]],

gamedata/modularcomms/weapons/heavymachinegun.lua
@@ -30,7 +30,7 @@ local weaponDef = {
 	lineOfSight             = true,
 	noSelfDamage            = true,
 	range                   = 290,
-	reloadtime              = 0.2,
+	reloadtime              = 0.167,
 	rgbColor                = [[1 0.95 0.4]],
 	separation              = 1.5,
 	soundHit                = [[weapon/cannon/emg_hit]],

gamedata/modularcomms/weapons/peashooter.lua
@@ -13,9 +13,9 @@ local weaponDef = {
 	},		  
 	  
 	damage                  = {
-		default = 8,
-		planes  = 8,
-		subs    = 0.4,
+		default = 11,
+		planes  = 11,
+		subs    = 0.55,
 	},
 	
 	duration                = 0.02,

units/arm_venom.lua
@@ -28,7 +28,7 @@ unitDef = {
     description_fr = [[Araign?e ?meuti?re EMP]],
     description_it = [[Ragno PEM da rissa]],
     description_de = [[Unterstützende EMP Spinne]],	
-    helptext       = [[The Venom is an all-terrain unit designed to paralyze enemies so other units can easily destroy them. It has AoE and is useful as a riot unit, for keeping swarms at bay. Works well in tandem with the recluse to keep enemies from closing range with that fragile skirmisher.]],
+    helptext       = [[The Venom is an all-terrain unit designed to paralyze enemies so other units can easily destroy them. It moves particularly fast for a riot unit. Works well in tandem with the Recluse to keep enemies from closing range with the fragile skirmisher.]],
     helptext_bp    = [[Venon é uma unidade escaladora projetada para paralizar inimigos para que outras unidades possam destruílos facilmente. Seus tiros podem atingir múltiplas unidades e portanto é útil como dispersadora. Funciona bem junto com o Recluse para impedir os inimigos de se aproximarem deste.]],
     helptext_es    = [[El Venom es una unidad all-terrain hecha para paralizar a los nemigos, permitiendo que otras unidades puedan destruirlos fácilmente. Tiene AdE y es útil como unidad de alboroto, para tener a la larga pelotones de enemigos. Funciona bien juntado con los recluse para no dejar que los enemigos se acerquen demasiado al frágil escaramuzador.]],
     helptext_fi    = [[Maastokelpoinen Venom kykenee EMP-aseellaan halvaannuttamaan vihollisen yksik?t niin, ett? ne voidaan tuhota vaaratta. Tehokas toimiessaan yhdess? Recluse:n kanssa. Tuhoutuu nopeasti vihollisen tuliksen alla.]],

units/armbrawl.lua
@@ -102,8 +102,8 @@ unitDef = {
       craterMult              = 0.3,
 
       damage                  = {
-        default = 20,
-        subs    = 1,
+        default = 21.6,
+        subs    = 1.08,
       },
 
       edgeEffectiveness       = 0.5,

commit cb95225d73db18035debfacb8b14a87a68ddf0b9

Fixed Archangel cannon damage broken by r3933 (at least I hope it was broken and KR didn't mean to reduce damage by 7x)
svn path=/trunk/mods/zk/; revision=6176

units/armaak.lua
@@ -84,8 +84,8 @@ unitDef = {
       cylinderTargetting      = 1,
 
       damage                  = {
-        default = 0.117,
-        planes  = 1.17,
+        default = 1.17,
+        planes  = 11.7,
         subs    = 0.5,
       },
 