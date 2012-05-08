4a2c17df991383d6d3f034864cd955d6f6498eca
KingRaptor <kingraptorowns@gmail.com>
2012-05-08 18:30:59 +0000

    New MS Office Assistant widget, Clippy Comments. Ever wanted units that talk back and second-guess your build order? Now you can!
    svn path=/trunk/mods/zk/; revision=6440

---
+++ LuaUI/Configs/clippy.lua
@@ -0,0 +1,123 @@
+local THRESHOLD_EXPENSIVE = 800
+INCOME_TO_SPLURGE = 25	-- ignore expensive warning if you have this much income
+METAL_PER_NANO = 12		-- suggested nanos per metal ^ -1
+NANO_DEF_ID = UnitDefNames.armnanotc.id
+ENERGY_TO_METAL_RATIO = 2.4	-- suggested maximum for energy
+DEFENSE_QUOTA = 0.2	-- suggested maximum proportion of total assets that is defense
+
+--seconds
+TIMER_EXPENSIVE_UNITS = 60 * 10
+TIMER_ADV_FACTORY = 60 * 15
+TIMER_SUPERWEAPON = 60 * 30
+
+tips = {
+	nano_excess = {str = {"Don't we have enough\nnanos already?"}, life = 7, cooldown = 20},
+	expensive_unit = {str = {"Boss, I'm not sure\nwe can afford that at\nthis stage of the game.",
+				"Sir, I don't think that\nunit is in our price\nrange right now."}, life = 9, cooldown = 20},
+	superweapon = {str = {"A superweapon,\nnow? Seriously?"}, life = 7, cooldown = 60},
+	adv_factory = {str = {"Are you sure that's\na good starting\nfac, chief?"}, life = 7, cooldown = 60},
+	retreat_repair = {str = {"Getting shot up!\nRequesting permission\nto pull out, sir!",
+				"Get me out of here!\nI need repairs!",}, life = 7, cooldown = 20},
+	energy_excess  = {str = {"I think we've got\nenough energy for\nnow, boss.",
+				"We're not playing\nEnergy Tycoon, man...",
+				"Sir, we're swimming\nin energy as it is."}, life = 7, cooldown = 20},
+	energy_stall  = {str = {"Stalling energy, sir.\nSome more E structures?"}, life = 7, cooldown = 20},
+	
+	facplop = {str = {"This facplop is burning\na hole in my pocket..."}, life = 7, cooldown = 30},
+	
+	defense_excess = {str = {"Boss, this isn't a\nTower Defense game...",
+				"Chief, don't you think\nwe have enough\ndefenses already?",
+				"Sir, I don't think\nwe should be building\nany more defense."}, life = 8, cooldown = 20}
+}
+
+for name,data in pairs(tips) do
+	data.lastUsed = -10000
+end
+
+local superweaponDefs = {
+	"armbrtha",
+	"corsilo",
+	"mahlazer",
+	"zenith",
+	"raveparty",
+}
+local canRetreatDefs = {
+	"armbrawl",
+	"blackdawn",
+	"corcrw",
+	
+	"correap",
+	"corgol",
+	--"trem",
+	"corcan",
+	"corsumo",
+	
+	"dante",
+	--"armraven",
+	"scorpion",
+	"armbanth",
+	"armorco",
+	
+	"corbats",
+	"armcarry",
+	"corroy",
+	"serpent",
+}
+
+local energyDefs = {
+	"armsolar",
+	"armwin",
+	"armfus",
+	--"geo",
+	"cafus",
+}
+
+local defenseDefs = {
+	"corllt",
+	"corrl",
+	"armdeva",
+	"corgrav",
+	"corhlt",
+	"armpb",
+	
+	"corrazor",
+	"missiletower",
+	"corflak",
+	"armcir",
+	"screamer",
+	
+	"cordoom",
+	"armanni",
+	
+	"cortl",
+	
+	"armjamt",
+	"corjamt"
+}
+
+--unitDefID-indexed tables
+expensive_units = {}
+superweapons = {}
+commanders = {}
+adv_factories = {}
+energy = {}
+defenses = {}
+canRetreat = {}
+
+for i=1,#UnitDefs do
+	if UnitDefs[i].customParams.commtype then commanders[i]=true
+	--elseif (not UnitDefs[i].canMove) and UnitDefs[i].canAttack then defenses[i]=true	-- bad idea: includes superweapons
+	elseif UnitDefs[i].metalCost > THRESHOLD_EXPENSIVE then expensive_units[i] = true end
+end
+
+local function CreateArray(source, target)
+	for i=1, #source do
+		local def = UnitDefNames[source[i]]
+		if def then target[def.id] = true end
+	end
+end
+
+CreateArray(superweaponDefs, superweapons)
+CreateArray(canRetreatDefs, canRetreat)
+CreateArray(energyDefs, energy)
+CreateArray(defenseDefs, defenses)
---
+++ LuaUI/Widgets/unit_clippy.lua
@@ -0,0 +1,351 @@
+local playerID = Spring.GetMyPlayerID()
+local customkeys = playerID and select(10, Spring.GetPlayerInfo(playerID))
+local rank = (customkeys and tonumber(customkeys.level) or 0) or select(9, Spring.GetPlayerInfo(playerID))
+
+function widget:GetInfo()
+	return {
+		name = "Clippy Comments",
+		desc = "v0.4 Makes units give tips.",
+		author = "KingRaptor",
+		date = "2011.5.6",
+		license = "Public Domain",
+		layer = 0,
+		enabled = true,	-- (rank and rank == 1) or true,
+	}
+end
+
+------------------------
+-- speedups
+local spGetGameSeconds = Spring.GetGameSeconds
+local spGetMyTeamID = Spring.GetMyTeamID
+local spGetTeamUnitsByDefs = Spring.GetTeamUnitsByDefs
+local spGetTeamResources = Spring.GetTeamResources
+local spGetUnitDefID = Spring.GetUnitDefID
+local spGetUnitHealth = Spring.GetUnitHealth
+
+------------------------
+--  CONFIG
+------------------------
+options_path = 'Settings/Interface/Clippy Comments'
+options = {
+	noRankLimit = {
+		name = "No Rank Limit",
+		type = 'bool',
+		value = false,
+		desc = 'Units make comments even to pros.',
+	},
+}
+
+VFS.Include("LuaUI/Configs/clippy.lua",nil)
+
+local activeTips = {}	-- [unitID] = {stuff for tip being displayed}
+
+local units = {}
+local totalValue = 0
+local defenseValue = 0
+
+local airSpotted = false
+local nukeSpotted = false
+local fontSize = 12
+
+local updateFrequency = 0.2
+
+local myTeam = spGetMyTeamID()
+local activeCommand
+local currentBuilder
+
+local gameframe = Spring.GetGameFrame()
+-----------------------
+-- minimum complexity of tips to display
+-- 1 = new to RTS
+-- 2 = a bit of experience with ZK
+-- 3 = up to intermediate
+local helpLevel = rank and math.min(rank, 3) or 1
+
+-- Chili classes
+local Chili
+local Window
+local TextBox
+local Image
+local Font
+
+-- Chili instances
+local screen0
+
+local font = "LuaUI/Fonts/komtxt__.ttf"
+
+------------------------
+------------------------
+local function DisposeTip(unitID)
+	if activeTips[unitID] and activeTips[unitID].img then
+		activeTips[unitID].img:Dispose()
+	end
+	activeTips[unitID] = nil
+end
+
+local function GetTipDimensions(unitID, str, height, invert)
+	local textHeight, _, numLines = gl.GetTextHeight(str)
+	textHeight = textHeight*fontSize*numLines
+	local textWidth = gl.GetTextWidth(str)*fontSize
+
+	local ux, uy, uz = Spring.GetUnitBasePosition(unitID)
+	uy = uy + height
+	local x,y,z = Spring.WorldToScreenCoords(ux, uy, uz)
+	if not invert then
+		y = screen0.height - y
+	end
+	
+	return textWidth, textHeight, x, y, height
+end
+
+local function MakeTip(unitID, tip)
+	if not (options.noRankLimit.value or (rank <= 2)) then
+		return
+	end
+	DisposeTip(unitID)
+
+	local strings = tips[tip].str
+	local str = strings[math.random(#strings)]
+	
+	local height = Spring.GetUnitHeight(unitID)
+	
+	local textWidth, textHeight, x, y = GetTipDimensions(unitID, str, height)
+
+	local img = Image:New {
+		width = textWidth + 4,
+		height = textHeight + 4 + fontSize,
+		x = x - (textWidth+8)/2;
+		y = y - textHeight - 4 - fontSize;
+		keepAspect = false,
+		file = "LuaUI/Images/speechbubble.png";
+		parent = screen0;
+	}
+	local textBox = TextBox:New{
+		parent  = img;
+		text    = str,
+		height	= textHeight,
+		width   = textWidth,
+		x = 4,
+		y = 4,
+		valign  = "center";
+		align   = "left";
+		font    = {
+			font   = font;
+			size   = fontSize;
+			color  = {0,0,0,1}
+		},
+	}
+	
+	activeTips[unitID] = {str = str, expire = gameframe + tips[tip].life*30, height = height, img = img, textBox = textBox}
+	tips[tip].lastUsed = gameframe
+end
+
+local function ProcessCommand(unitID, command)
+	if not unitID then return end
+	if -command == NANO_DEF_ID then
+		if tips.nano_excess.lastUsed > gameframe - tips.nano_excess.cooldown*30 then
+			return
+		end
+		local metalIncome = select(4, spGetTeamResources(myTeam, "metal"))
+		local numNanos = #(Spring.GetTeamUnitsByDefs(myTeam, NANO_DEF_ID) or {})
+		if numNanos > 0 and (metalIncome/numNanos < METAL_PER_NANO) then
+			MakeTip(unitID, "nano_excess")
+		end
+	elseif superweapons[-command] and TIMER_SUPERWEAPON > gameframe/30 then
+		if tips.superweapon.lastUsed > gameframe - tips.superweapon.cooldown*30 then
+			return
+		end
+		MakeTip(unitID, "superweapon")			
+	elseif expensive_units[-command] and TIMER_EXPENSIVE_UNITS > gameframe/30 then
+		if tips.expensive_unit.lastUsed > gameframe - tips.expensive_unit.cooldown*30 then
+			return
+		end
+		local metalIncome = select(4, spGetTeamResources(myTeam, "metal"))
+		if metalIncome < INCOME_TO_SPLURGE then
+			MakeTip(currentBuilder, "expensive_unit")
+		end
+	elseif defenses[-command] then
+		if tips.defense_excess.lastUsed > gameframe - tips.defense_excess.cooldown*30 then
+			return
+		end
+		if totalValue == 0 then return end
+		if defenseValue/totalValue > DEFENSE_QUOTA then
+			MakeTip(unitID, "defense_excess")
+		end		
+	elseif energy[-command] then
+		if tips.energy_excess.lastUsed > gameframe - tips.energy_excess.cooldown*30 then
+			return
+		end
+		local metalIncome = select(4, spGetTeamResources(myTeam, "metal"))
+		local energyIncome = select(4, spGetTeamResources(myTeam, "energy"))
+		if energyIncome/metalIncome > ENERGY_TO_METAL_RATIO then
+			MakeTip(unitID, "energy_excess")
+		end
+	end
+end
+
+------------------------
+------------------------
+
+local timer = 0
+
+function widget:Update(dt)
+	timer = timer + dt
+	if timer > updateFrequency then
+		if (Spring.GetSpectatingState() or Spring.IsReplay()) then
+			Spring.Echo("<Clippy Comments> Spectator mode or replay. Widget removed.")
+			widgetHandler:RemoveWidget()
+		end
+		
+		myTeam = spGetMyTeamID()	-- just refresh for fun
+		timer = 0
+		local command = select(2, Spring.GetActiveCommand())
+		if command and (activeCommand ~= command) then
+			ProcessCommand(currentBuilder, command)
+			activeCommand = command
+		end
+	end
+	
+	-- chili code
+	for unitID, tipData in pairs(activeTips) do
+		if Spring.IsUnitInView(unitID) then
+			local textWidth, textHeight, x, y = GetTipDimensions(unitID, tipData.str, tipData.height)
+			
+			local img = tipData.img
+			if img.hidden then
+				screen0:AddChild(img)
+				img.hidden = false
+			end
+			
+			img.x = x - (textWidth+8)/2
+			img.y = y - textHeight - 4 - fontSize
+			img:Invalidate()
+		elseif not tipData.img.hidden then
+			screen0:RemoveChild(tipData.img)
+			tipData.img.hidden = true
+		end
+		if tipData.expire < gameframe then
+			DisposeTip(unitID)
+		end
+	end
+end
+
+function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
+	if units[unitID] then
+		local cost = UnitDefs[unitDefID].metalCost or 0
+		totalValue = totalValue - cost
+		if defenses[unitDefID] then
+			defenseValue = defenseValue - cost
+		end
+	end
+	DisposeTip(unitID)
+end
+
+function widget:UnitFinished(unitID, unitDefID, unitTeam)
+	if unitTeam == myTeam then
+		units[unitID] = true
+		local cost = UnitDefs[unitDefID].metalCost or 0
+		totalValue = totalValue + cost
+		if defenses[unitDefID] then
+			defenseValue = defenseValue + cost
+		end
+	end
+end
+
+function widget:GameFrame(n)
+	gameframe = n
+end
+
+function widget:UnitCommand(unitID, unitDefID, unitTeam, cmdID, cmdOpts, cmdParams)
+	if cmdID < 0 then
+		ProcessCommand(unitID, cmdID)
+	end
+end
+-- obscures healthbars
+--[[
+function widget:UnitDamaged(unitID, unitDefID, unitTeam)
+	if unitTeam == myTeam and canRetreat[unitDefID] then
+		if tips.retreat_repair.lastUsed > gameframe - tips.retreat_repair.cooldown*30 then
+			return
+		end
+		local health, maxHealth = spGetUnitHealth(unitID)
+		if health/maxHealth < 0.5 then
+			MakeTip(unitID, "retreat_repair")
+		end
+	end
+end
+]]--
+
+function widget:SelectionChanged(newSelection)
+	--get new selected con, if any
+	for i=1,#newSelection do
+		local id = newSelection[i]
+		if UnitDefs[spGetUnitDefID(id)].builder then
+			currentBuilder = id
+			return
+		end
+	end
+	currentBuilder = nil
+end
+
+function widget:Initialize()
+	if VFS.FileExists("LuaRules/Gadgets/mission.lua") then
+		widgetHandler:RemoveWidget()	-- no need for tips in mission
+	end
+	local selection = Spring.GetSelectedUnits()
+	widget:SelectionChanged(selection)
+	
+	Chili = WG.Chili
+	TextBox = Chili.TextBox
+	Image = Chili.Image
+	Font = Chili.Font
+	screen0 = Chili.Screen0
+	
+	-- reload compatibility
+	local units = Spring.GetTeamUnits(myTeam)
+	for i=1,#units do
+		widget:UnitFinished(units[i], Spring.GetUnitDefID(units[i]), myTeam)
+	end
+end
+
+-- non-chili code
+--[[
+local function DrawUnitFunc(xshift, yshift, text)
+	gl.Translate(xshift,yshift,0)
+	gl.Billboard()
+	gl.Text(text, 0,0, 14, "cs")
+end
+
+function widget:DrawScreen()
+	if Spring.IsGUIHidden() then return end
+
+	gl.DepthMask(true)
+	gl.DepthTest(true)
+	gl.AlphaTest(GL.GREATER, 0.001)
+	gl.Texture("LuaUI/Images/speechbubble.png")
+	
+	for unitID, tipData in pairs(activeTips) do
+		if Spring.IsUnitInView(unitID) then
+			gl.PushMatrix()
+			local textWidth, textHeight, x, y = GetTipDimensions(unitID, tipData.str, tipData.height, true)
+			gl.Translate(x, y + textHeight + 8, 0)
+			gl.Color(1,1,1,1)
+			local bubbleHeight = (textHeight+4)
+			local bubbleWidth = textWidth+4
+			gl.TexRect(-bubbleWidth/2, -bubbleHeight, bubbleWidth/2, bubbleHeight)
+			gl.Color(0,0,0,1)
+			gl.Translate(0, textHeight/2, 0)
+			fontHandler.UseFont("LuaUI/Fonts/KOMTXT___16")
+			--fontHandler.DrawCentered(tipData.str, 0, 0)
+			gl.Text(tipData.str, 0,0, fontSize, "co")
+
+			--gl.DrawFuncAtUnit(unitID, false, DrawUnitFunc,
+			--		0, tipData.height, tipData.str)
+			gl.PopMatrix()
+		end
+		if tipData.expire < gameframe then
+			activeTips[unitID] = nil
+		end
+	end
+end
+]]--
