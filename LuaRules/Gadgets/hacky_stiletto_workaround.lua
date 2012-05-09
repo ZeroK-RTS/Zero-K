6aed0397775e766b668afb4f6af3eb099be90631
KingRaptor <kingraptorowns@gmail.com>
2012-05-09 15:56:56 +0000

    Fixed broken Stiletto gadget.
    svn path=/trunk/mods/zk/; revision=6465

--- LuaRules/Gadgets/hacky_stiletto_workaround.lua
+++ LuaRules/Gadgets/hacky_stiletto_workaround.lua
@@ -18,8 +18,6 @@ local frame = 0
 local unitFrames = {}
 
 local spValidUnitID = Spring.ValidUnitID
-local spUsCallAsUnit = Spring.UnitScript.CallAsUnit
-local spUsGetScriptEnv = Spring.UnitScript.GetScriptEnv
 
 function GG.Hacky_Stiletto_Workaround_gadget_func(unitID, offset, number)
 	
@@ -37,8 +35,8 @@ function gadget:GameFrame(n)
 	if unitFrames[n] then
 		for i = 1, unitFrames[n].count do
 			if spValidUnitID(unitFrames[n].data[i].id) then
-				local func = spUsGetScriptEnv(unitFrames[n].data[i].id).Hacky_Stiletto_Workaround_stiletto_func
-				spUsCallAsUnit(unitFrames[n].data[i].id,func,unitFrames[n].data[i].number)
+				local func = Spring.UnitScript.GetScriptEnv(unitFrames[n].data[i].id).Hacky_Stiletto_Workaround_stiletto_func
+				Spring.UnitScript.CallAsUnit(unitFrames[n].data[i].id,func,unitFrames[n].data[i].number)
 			end
 		end
 		unitFrames[n] = nil
