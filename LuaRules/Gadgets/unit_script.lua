ZeroK-RTS/Zero-K

commit 025db7f7cfea153f6ef6ae4ed07c8e69c4f366c0

Fixed Crabe script.
svn path=/trunk/mods/zk/; revision=3350

scripts/armcrabe.lua
@@ -1,5 +1,3 @@
---ARM CRABRE SCRIPT BY TA-FOREVER (Sephiroth) (http:--www.planetannihilation.com/taforever, http:--www.planetannihilation.com/taan)
-
 include 'constants.lua'
 
 --------------------------------------------------------------------------------
@@ -39,9 +37,9 @@ local legRaiseSpeed = math.rad(45)*PACE
 local legRaiseAngle = math.rad(20)
 local legLowerSpeed = math.rad(50)*PACE
 
-local legForwardSpeed = math.rad(45)*PACE
+local legForwardSpeed = math.rad(40)*PACE
 local legForwardAngle = -math.rad(20)
-local legBackwardSpeed = math.rad(40)*PACE
+local legBackwardSpeed = math.rad(35)*PACE
 local legBackwardAngle = math.rad(25)
 local legBackwardAngleMinor = math.rad(10)
 
@@ -55,16 +53,14 @@ local nocurl = true
 
 local gun_0 = 0
 
-local SIG_MOVE = 1	
-local SIG_AIM1 = 2
-local SIG_AIM2 = 4
-local SIG_CURL = 8
+local SIG_MOVE = 2	
+local SIG_AIM1 = 4
+local SIG_AIM2 = 8
+local SIG_CURL = 16
 
 smokePiece = {base, turret}
 
 local function Walk()
-	Signal(SIG_MOVE)
-	SetSignalMask(SIG_MOVE)
 	while bCurled or bCurling do Sleep(100) end
 	SetUnitValue(COB.MAX_SPEED, base_speed)
 	while true do
@@ -86,8 +82,8 @@ local function Walk()
 		-- Spring.Echo("lower left fore and right back")
 		Turn(leg4, z_axis, 0, legLowerSpeed)	-- LF leg down		
 		Turn(leg2, z_axis, 0, legLowerSpeed)	-- RB leg down
+		Sleep(0)
 		WaitForTurn(leg4, z_axis)
-		--Sleep(0)
 		
 		-- Spring.Echo("left back and right fore move, left fore and right back anchor")
 		--Turn(leg4, z_axis, 0, legLowerSpeed)	-- LF leg down
@@ -105,9 +101,9 @@ local function Walk()
 
 		-- Spring.Echo("lower left back and right fore")
 		Turn(leg3, z_axis, 0, legLowerSpeed)	-- LB leg down		
-		Turn(leg1, z_axis, 0, legLowerSpeed)	-- RF leg down		
+		Turn(leg1, z_axis, 0, legLowerSpeed)	-- RF leg down
+		Sleep(0)
 		WaitForTurn(leg3, z_axis)
-		--Sleep(0)
 	end
 end
 
@@ -121,7 +117,7 @@ local function Curl()
 	bCurling = true
 	SetUnitValue(COB.MAX_SPEED, 1)
 
-	Move( canon , y_axis, 4 , 2.5 )
+	Move( canon , y_axis, 5 , 2.5 )
 	Move( base , y_axis, -5 , 2.5 )
 	Move( base , z_axis, -5 , 2.5 )
 	
@@ -184,20 +180,21 @@ local function ResetLegs()
 	Turn(leg3 , x_axis, math.rad(90), math.rad(95))
 	Turn(leg1 , x_axis, math.rad(-90), math.rad(95))
 	Turn(leg4 , x_axis, math.rad(-90), math.rad(95))
-	Sleep(100)	
+	Sleep(100)
 	Turn( leg1 , x_axis, 0, math.rad(95) )
 	Turn( leg2 , x_axis, 0, math.rad(95) )
 	Turn( leg3 , x_axis, 0, math.rad(95) )
-	Turn( leg4 , x_axis, 0, math.rad(95) )	
+	Turn( leg4 , x_axis, 0, math.rad(95) )
+	Spring.SetUnitArmored(unitID,false)
 end
 
 local function Uncurl()
+	if not bMoving then return end
 	--Spring.Echo("Initiating uncurl")
 	Signal( SIG_CURL) 
 	SetSignalMask( SIG_CURL)
 	bCurled = false
 	bCurling = true
-	Spring.SetUnitArmored(unitID,false)
 	
 	ResetLegs()
 	
@@ -259,19 +256,22 @@ function script.Create()
 	StartThread(CurlDelay)
 end
 
-local function Move()
+local function Motion()
+	Signal(SIG_MOVE)
+	SetSignalMask(SIG_MOVE)
 	bMoving = true
-	StartThread(Uncurl)
 	Sleep(66)
-	StartThread(Walk)
+	Uncurl()
+	Walk()
 end
 
 function script.StartMoving()
-	StartThread(Move)
+	--Spring.Echo("Moving")
+	StartThread(Motion)
 end
 
 function script.StopMoving()
-	--Spring.Echo("Unit stopped moving")
+	--Spring.Echo("Stopped moving")
 	bMoving = false
 	Signal(SIG_MOVE)
 	StartThread(Curl)

commit 10966bd8c14a8e6c65ec413cff17e7fbdb572fde

- Fixed Crabe script broken by KR in r3350. You can't call function containing SetSignalMask without using StartThread, otherwise SetSignalMask will overwrite parent function signal mask too. In this case Uncurl() was overwriting Motion() and Walk() signal mask to SIG_CURL which in turn prevented Motion() to kill previous self with Signal(SIG_MOVE). Now I removed SIG_CURL as there is no point in it - Curl(), UnCurl() and Walk() cant work simultaneously anyway.
At least this issue revealed a crashbug in Lua Unit Script Framework which I fixed in r6505.

- Added a warning message to unit_script.lua to find and prevent similar issue in other unit scripts.

svn path=/trunk/mods/zk/; revision=6506

LuaRules/Gadgets/unit_script.lua
@@ -389,7 +389,13 @@ end
 function Spring.UnitScript.SetSignalMask(mask)
 	local activeUnit = GetActiveUnit()
 	local activeThread = activeUnit.threads[co_running() or error("[SetSignalMask] not in a thread", 2)]
+	if (activeThread.signal_mask_set) then
+		local ud = UnitDefs[Spring.GetUnitDefID(activeUnit.unitID)]
+		Spring.Echo("Warning: Spring.UnitScript.SetSignalMask called second time for the same thread (possible lack of StartThread?)")
+		Spring.Echo("UnitDef: " .. ud.name .. " Old mask: " .. activeThread.signal_mask .. " New mask: " .. mask)
+	end
 	activeThread.signal_mask = mask
+	activeThread.signal_mask_set = true
 end
 
 function Spring.UnitScript.Signal(mask)

scripts/armcrabe.lua
@@ -36,7 +36,6 @@ local base_speed = 100
 local SIG_MOVE = 2	
 local SIG_AIM1 = 4
 local SIG_AIM2 = 8
-local SIG_CURL = 16
 
 local PACE = 1.2
 
@@ -62,8 +61,6 @@ local gun_0 = 0
 
 -- four-stroke tetrapedal walkscript
 local function Walk()
-	while bCurled or bCurling do Sleep(100) end
-	SetUnitValue(COB.MAX_SPEED, base_speed)
 	while true do
 		-- Spring.Echo("left fore and right back move, left back and right fore anchor")
 		Turn(leg4, z_axis, legRaiseAngle, legRaiseSpeed)	-- LF leg up
@@ -111,8 +108,7 @@ end
 local function Curl()
 	if nocurl then return end
 	--Spring.Echo("Initiating curl")
-	Signal( SIG_CURL)
-	SetSignalMask( SIG_CURL)
+	SetSignalMask(SIG_MOVE)
 	
 	Sleep(100)
 	bCurling = true
@@ -186,18 +182,15 @@ local function ResetLegs()
 	Turn( leg2 , x_axis, 0, math.rad(95) )
 	Turn( leg3 , x_axis, 0, math.rad(95) )
 	Turn( leg4 , x_axis, 0, math.rad(95) )
-	Spring.SetUnitArmored(unitID,false)
 end
 
 local function Uncurl()
-	if not bMoving then return end
 	--Spring.Echo("Initiating uncurl")
-	Signal( SIG_CURL) 
-	SetSignalMask( SIG_CURL)
 	bCurled = false
 	bCurling = true
 	
 	ResetLegs()
+	Spring.SetUnitArmored(unitID,false)
 	
 	Move( canon , y_axis, 0 , 2.5 )
 	Move( base , y_axis, 0 , 2.5 )
@@ -218,7 +211,7 @@ local function Uncurl()
     WaitForTurn(leg3, z_axis)
     WaitForTurn(leg4, z_axis)    
 	
-    --SetUnitValue(COB.MAX_SPEED, base_speed)
+    SetUnitValue(COB.MAX_SPEED, base_speed)
     bCurling = false
 end
 