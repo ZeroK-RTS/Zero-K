b2f842421fd99e9f22a73e2d0466c7d17ad6847a
frameworld <frameworld@arcor.de>
2012-05-14 11:25:19 +0000

    Google made an error - it required only 500 instead of 5000 energy to build
    svn path=/trunk/mods/zk/; revision=6522

--- units/armbrtha.lua
+++ units/armbrtha.lua
@@ -5,7 +5,7 @@ unitDef = {
   acceleration                  = 0,
   brakeRate                     = 0,
   buildAngle                    = 32700,
-  buildCostEnergy               = 500,
+  buildCostEnergy               = 5000,
   buildCostMetal                = 5000,
   builder                       = false,
   buildingGroundDecalDecaySpeed = 30,
@@ -26,7 +26,7 @@ unitDef = {
   customParams                  = {
     description_fr = [[Canon ? Plasma Strat?gique]],
 	description_de = [[Strategische Plasma Kanone]],
-	helptext_de    = [[Die Bertha ist eine massive Kanone, welche hochenergetische Plasmoide über die Karte verschiesst. Angemessener Gebrauch der Waffe kann gengerische Operationen von der eigenen, sicheren Basis aus schnell unterdrücken. Trotzdem erwarte nicht, dass du nur dich diese Waffe die Schlachten gewinnen wirst.]],
+	helptext_de    = [[Die Bertha ist eine massive Kanone, welche hochenergetische Plasmoide Ã¼ber die Karte verschiesst. Angemessener Gebrauch der Waffe kann gengerische Operationen von der eigenen, sicheren Basis aus schnell unterdrÃ¼cken. Trotzdem erwarte nicht, dass du nur dich diese Waffe die Schlachten gewinnen wirst.]],
     helptext       = [[The Bertha is a massive cannon that fires high-energy plasmoids across the map. Used appropriately, it can effectively suppress enemy operations from the safety of your base. Do not expect it to win battles alone for you, however.]],
     helptext_fr    = [[Le Big Bertha est un canon ? plasma lourd, tr?s lourd. Un seul impact de son tir peut r?duire ? n?ant plusieurs unit?s ou structures. Sa port?e de tir op?rationnelle est immense et n'?gale que son co?t de construction et d'usage. En effet chaque tir consomme 300 unit?s d'?nergie. Notez que le Big Bertha effectue des tirs tendus. Autrement dit, pensez ? le placer en hauteur, ou le moindre relief servira de refuge ? l'ennemi.]],
   },
