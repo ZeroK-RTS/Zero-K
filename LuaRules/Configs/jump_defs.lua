d2e19c64da8c97d25486506c1d24f8febc365583
KingRaptor <kingraptorowns@gmail.com>
2012-05-14 03:12:05 +0000

    Sumo  * Cost 1900 -> 2000 (was 2200)  * Damage 50 -> 45 (was 40)  * Range 430 -> 390  * Jump distance 360 -> 320 (was 300)
    svn path=/trunk/mods/zk/; revision=6509

--- LuaRules/Configs/jump_defs.lua
+++ LuaRules/Configs/jump_defs.lua
@@ -93,7 +93,7 @@ overCategory = {
     cobscript = false},	
 	
  corsumo = {
-    delay = 30, height = 110, range = 360, reload = 13, cobscript = false, rotateMidAir = false},	
+    delay = 30, height = 110, range = 320, reload = 13, cobscript = false, rotateMidAir = false},	
 
  cadenza = {
     delay = 30, reload = 20, cobscript = false, rotateMidAir = false},
