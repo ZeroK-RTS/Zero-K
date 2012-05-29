62b88d6838efa2acc65ff4218713bed02e2c48ba
Licho <jalich@gmail.com>
2012-05-29 13:21:29 +0000

    tremor range 1200->1000 (roughly same as penetrator) tremor shot damage 200->180
    svn path=/trunk/mods/zk/; revision=6689

--- units/trem.lua
+++ units/trem.lua
@@ -102,8 +102,8 @@ unitDef = {
 	  },
 	  
       damage                  = {
-        default = 200,
-        planes  = 200,
+        default = 180,
+        planes  = 180,
         subs    = 10,
       },
 	  
@@ -112,7 +112,7 @@ unitDef = {
       impulseFactor           = 0.4,
       interceptedByShieldType = 1,
       myGravity               = 0.1,
-      range                   = 1300,
+      range                   = 1000,
       reloadtime              = 0.36,
       soundHit                = [[weapon/cannon/cannon_hit2]],
       soundStart              = [[weapon/cannon/tremor_fire]],
