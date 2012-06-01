931f52bd42833d3c4f1755cf0750d681f3c196a4
KingRaptor <kingraptorowns@gmail.com>
2012-06-01 14:21:54 +0000

    VERSION{v1.0.6.2} updated vote display for new syntax
    svn path=/trunk/mods/zk/; revision=6753

--- LuaUI/Widgets/gui_chili_vote.lua
+++ LuaUI/Widgets/gui_chili_vote.lua
@@ -37,13 +37,12 @@ local voteCount, voteMax = {}, {}
 local pollActive = false
 local votingForceStart = false
 
-local string_success = " vote successful"
-local string_fail = " not enough votes"
-local string_vote1 = " option 1 has "
-local string_vote2 = " option 2 has "
-local string_votetopic = " Do you want to "
+local string_success = "END:SUCCESS"
+local string_fail = "END:FAILED"
+local string_vote = {"!y=", "!n="}
+local string_titleStart = "Poll: "
 local string_endvote = " poll cancelled"
-local string_titleEnd = "? !vote 1 = yes, !vote 2 = no"
+local string_titleEnd = "?"
 local string_noVote = "There is no poll going on, start some first"
 
 local springieName = Spring.GetModOptions().springiename or ''
@@ -61,18 +60,20 @@ local index_voteTitle = 15
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 local function GetVotes(line)
-	local voteNum = 1
-	if line:find(string_vote2) then
-		voteNum = 2
-	end
-	local index_init = line:find("%s"..voteNum.."%s")
-	local index_votesHave = line:find("%s%d[%s%d]", index_init + 1)
-	local index_votesNeeded = line:find("%s%d[%s%d]", index_votesHave + 1)
-	--Spring.Echo(index_init, index_votesHave, index_votesNeeded)
-	local numVotes = tonumber(line:sub(index_votesHave, index_votesHave + 2))
-	local maxVotes = tonumber(line:sub(index_votesNeeded, index_votesNeeded + 2))
-	voteCount[voteNum] = numVotes
 	for i=1, 2 do
+		local index_init = line:find(string_vote[i])
+		local index_votesHave = line:find("%d[%d/]", index_init)
+		local index_votesHaveEnd = line:find("/", index_votesHave) - 1
+		local index_votesNeeded = index_votesHaveEnd + 2
+		local index_votesNeededEnd = ( line:find("[,]", index_votesNeeded) or line:find("\]", index_votesNeeded) ) - 1
+		
+		--Spring.Echo(index_votesHave, index_votesHaveEnd, index_votesNeeded, index_votesNeededEnd)
+		--Spring.Echo(line:sub(index_votesHave, index_votesHaveEnd))
+		--Spring.Echo(line:sub(index_votesNeeded, index_votesNeededEnd))
+		
+		local numVotes = tonumber(line:sub(index_votesHave, index_votesHaveEnd))
+		local maxVotes = tonumber(line:sub(index_votesNeeded, index_votesNeededEnd))
+		voteCount[i] = numVotes
 		voteMax[i] = maxVotes
 		progress_vote[i]:SetCaption(voteCount[i]..'/'..voteMax[i])
 		progress_vote[i]:SetValue(voteCount[i]/voteMax[i])
@@ -90,52 +91,46 @@ local function CheckForVoteSpam (currentTime) --// function return "true" if not
 	end
 end
 
+local function RemoveWindow()
+	pollActive = false
+	screen0:RemoveChild(window)
+	for i=1,2 do
+		voteCount[i] = 0
+		voteMax[i] = 1	-- protection against div0
+		--progress_vote[i]:SetCaption('?/?')
+		progress_vote[i]:SetValue(0)
+	end
+end
+
 function widget:AddConsoleLine(line,priority)
 	if votingForceStart and line:sub(1,7) == "GameID:" then
-		pollActive = false
-		screen0:RemoveChild(window)
-		for i=1,2 do
-			voteCount[i] = 0
-			voteMax[i] = 1	-- protection against div0
-			progress_vote[i]:SetCaption('?/?')
-			progress_vote[i]:SetValue(0)
-		end
+		RemoveWindow()
 		votingForceStart = false
 	end
 	if line:sub(1,springieName:len()) ~= springieName then	-- no spoofing messages
-		return
+		return false
 	end
 	if line:find(string_success) or line:find(string_fail) or line:find(string_endvote) or line:find(string_noVote) then	--terminate existing vote
-		pollActive = false
-		screen0:RemoveChild(window)
-		for i=1,2 do
-			voteCount[i] = 0
-			voteMax[i] = 1	-- protection against div0
-			progress_vote[i]:SetCaption('?/?')
-			progress_vote[i]:SetValue(0)
-		end
+		RemoveWindow()
 		votingForceStart = false
-	elseif line:find(string_votetopic) and line:find(string_titleEnd) then	--start new vote
-		if pollActive then --//close previous windows in case Springie started a new vote without terminating the last one.
-			screen0:RemoveChild(window)
-			for i=1,2 do
-				voteCount[i] = 0
-				voteMax[i] = 1	-- protection against div0
-				progress_vote[i]:SetCaption('?/?')
-				progress_vote[i]:SetValue(0)
-			end
-			votingForceStart = false		
+	elseif line:find(string_titleStart) and line:find(string_vote[1]) and line:find(string_vote[2]) then	--start new vote
+		--if pollActive then --//close previous windows in case Springie started a new vote without terminating the last one.
+		--	RemoveWindow()
+		--	votingForceStart = false
+		--end
+		if not pollActive then
+			pollActive = true
+			screen0:AddChild(window)
 		end
-		pollActive = true
-		screen0:AddChild(window)
-		local indexStart = select(2, line:find(string_votetopic))
+		local indexStart = select(2, line:find(string_titleStart))
 		local indexEnd = line:find(string_titleEnd)
 		local title = line:sub(indexStart, indexEnd - 1)
 		votingForceStart = ((title:find("force game"))~=nil)
 		label_title:SetCaption("Poll: "..title)
-	elseif line:find(string_vote1) or line:find(string_vote2) then	--apply a vote
+	--elseif line:find(string_vote1) or line:find(string_vote2) then	--apply a vote
 		GetVotes(line)
 	end
+	return false
 end
 
 --[[
