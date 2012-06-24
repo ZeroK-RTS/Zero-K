535ec52e1e1aa0b5c743490028e8ff57c8e21ab4
Setokaiva <authorityblade@yahoo.com>
2012-06-24 18:45:23 +0000

    Changed the word 'Hovercon' to 'Quill' to match with its name change.
    svn path=/trunk/mods/zk/; revision=6984

--- units/corch.lua
+++ units/corch.lua
@@ -29,9 +29,9 @@ unitDef = {
   customParams        = {
     description_fr = [[Hovercraft de Construction, Construit r 6 m/s]],
 	description_de = [[Konstruktionsluftkissenboot, Baut mir 6 M/s]],
-    helptext       = [[The Hovercon allows smooth expansion across both land and sea. It can also detect submarines that get too close.]],
+    helptext       = [[The Quill allows smooth expansion across both land and sea. It can also detect submarines that get too close.]],
     helptext_fr    = [[L'Hovercon est rapide et agile mais son blindage et ses nanoconstructeurs sont de mauvaise facture.]],
-	helptext_de    = [[Der Hovercan erlaubt es dir leichtgängige Expansionen über Land und See. Dabei kann er sogar U-Boote entdecken, die ihm zu nahe kommen.]],
+	helptext_de    = [[Der Hovercan erlaubt es dir leichtgÃ¤ngige Expansionen Ã¼ber Land und See. Dabei kann er sogar U-Boote entdecken, die ihm zu nahe kommen.]],
   },
 
   energyMake          = 0.15,
