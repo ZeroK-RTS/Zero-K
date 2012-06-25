5ad93b2eb608f7b748632bd6d54a754982877ac7
xponen <msafwan86msafwan86_nnss@hotmail.com>
2012-06-25 14:15:22 +0000

    'gui_take_remind.lua': allow it to communicate with lagMonitor to take a timeout player (but a resigned player will still be taken like original version), and some tiny cosmetic code change (ipairs, rename, ect).
    'game_lagmonitor.lua': some bugfix for case where player with ID '0' might have problem to considered as AFK/lag (because table index can't start with 0). 
    
    svn path=/trunk/mods/zk/; revision=6997

--- LuaUI/Widgets/gui_take_remind.lua
+++ LuaUI/Widgets/gui_take_remind.lua
@@ -1,5 +1,5 @@
 -- $Id: gui_take_remind.lua 3550 2008-12-26 04:50:47Z evil4zerggin $
-local versionNumber = "v3.3"
+local versionNumber = "v3.5"
 
 function widget:GetInfo()
   return {
@@ -9,7 +9,7 @@ function widget:GetInfo()
     date      = "31 March 2007,2008,2009",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = false  --  loaded by default?
+    enabled   = true  --  loaded by default?
   }
 end
 
@@ -21,6 +21,16 @@ end
 --  SirMaverick: works now when someone uses "/spectator"
 --  jK: now even faster
 ------------------------------------------------
+------------------------------------------------
+--Crude Documentation:
+-- Main Logic:
+-- 1) 'widget:Player Changed' or 'widget:Player Removed' --> count lagger's units --> reset button text to "Click here..." (textPlsWait = false)  --> show button IF units ">0"
+-- 2) press the button --> count lagger's units --> change button text to "Wait..." (textPlsWait = true) --> [execute a "/take" when alternateTake== false OR execute a LUA-msg-lagmonitor when alternateTake== true] IF units ">0"
+-- 3) 'widget:Unit Taken' --> count lagger's units --> reset button text to "Click here..." (textPlsWait = false) --> hide button IF units "=0"
+
+--  logic:
+-- 1) console-message "Giving all unit.." --> alternateTake= false --> count lagger's units --> change button text to "Click here..." (textPlsWait = false) --> hide button IF units "=0"
+-- 2) console-message "player xxx left the game: normal quit/timeout" --> alternateTake= true
 
 ------------------------------------------------
 -- config
@@ -41,24 +51,26 @@ local falseColor = "\255\127\127\1"
 local buttonX = 240
 local buttonY = 36
 local recheck = false
-local lastActivePlayers = 0
+--local lastActivePlayers = 0
 local gameStarted = false
+local textPlsWait = false
+local alternateTake = false
 
 ------------------------------------------------
 -- speedups
 ------------------------------------------------
-local GetTeamList = Spring.GetTeamList
-local GetMyAllyTeamID  = Spring.GetMyAllyTeamID
-local GetTeamUnitCount = Spring.GetTeamUnitCount
-local GetPlayerList  = Spring.GetPlayerList
-local GetPlayerInfo  = Spring.GetPlayerInfo
-local GetGameSeconds = Spring.GetGameSeconds
-local GetSpectatingState = Spring.GetSpectatingState
-local GetUnitPosition = Spring.GetUnitPosition
-local GetVisibleUnits = Spring.GetVisibleUnits
-local GetUnitTeam = Spring.GetUnitTeam
-local AreTeamsAllied = Spring.AreTeamsAllied
-local GetMyTeamID = Spring.GetMyTeamID
+local spGetTeamList = Spring.GetTeamList
+local spGetMyAllyTeamID  = Spring.GetMyAllyTeamID
+local spGetTeamUnitCount = Spring.GetTeamUnitCount
+local spGetPlayerList  = Spring.GetPlayerList
+local spGetPlayerInfo  = Spring.GetPlayerInfo
+local spGetGameSeconds = Spring.GetGameSeconds
+local spGetSpectatingState = Spring.GetSpectatingState
+local spGetUnitPosition = Spring.GetUnitPosition
+local spGetVisibleUnits = Spring.GetVisibleUnits
+local spGetUnitTeam = Spring.GetUnitTeam
+local spAreTeamsAllied = Spring.AreTeamsAllied
+local spGetMyTeamID = Spring.GetMyTeamID
 local glBillboard         = gl.Billboard
 local glPushMatrix        = gl.PushMatrix
 local glPopMatrix         = gl.PopMatrix
@@ -70,28 +82,29 @@ local glColor = gl.Color
 -- helper functions
 ------------------------------------------------
 
-local function GetTeamIsTakeable(team)
-  local _,_,_,isAI = Spring.GetTeamInfo(team)
-  -- don't take AI teams
-  if isAI then
-    return false
-  end
+local function GetTeamIsTakeable(teamID)
+	local takeAble = true --assume a takeable team
+	local _,_,_,isAI = Spring.GetTeamInfo(teamID)
+	if isAI then 
+		takeAble = false  -- AI teams is not takeable
+	end
 
-  local players = GetPlayerList(team,true)
-  for _, player in ipairs(players) do
-    local _, _, spec = GetPlayerInfo(player)
-    if (not spec) then
-      return false
-    end
-  end
-  return true
+	local players = spGetPlayerList(teamID,true)
+	for i=1, #players, 1 do -- check shared player in a team
+		local playerID = players[i]
+		local _, _, spec = spGetPlayerInfo(playerID)
+		if not (spec) then
+			takeAble = false -- active team is not takeable
+		end
+	end
+	return takeAble
 end
 
 
 local takeableTeamsCached = {}
 local function GetTeamIsTakeableCached(team)
   local takeable = takeableTeamsCached[team]
-  if (takeable ~= nil) then
+  if (takeable == nil) then
     takeable = GetTeamIsTakeable(team)
     takeableTeamsCached[team] = takeable
   end
@@ -100,44 +113,64 @@ end
 
 
 local function UpdateUnitsToTake()
-  local teamList = GetTeamList(myAllyTeamID)
-  count = 0
-  for _, team in ipairs(teamList) do
-    local unitsOwned = GetTeamUnitCount(team)
-    if (unitsOwned > 0 and GetTeamIsTakeable(team)) then
-      count = count + unitsOwned
-    end
-  end
+	local unitCount = 0 -- the number of units to take
+	local teamList = spGetTeamList(myAllyTeamID)
+	for i= 1, #teamList ,1 do
+		local teamID = teamList[i]
+		local unitsOwned = spGetTeamUnitCount(teamID)
+		if (unitsOwned > 0 and GetTeamIsTakeable(teamID)) then
+			unitCount = unitCount + unitsOwned -- count lagger's unit
+		end
+	end
+	return unitCount
 end
 
-
+--[[
 local function SomeoneDropped(playerid)
-  local activePlayers = GetPlayerList(true)
-  if (#activePlayers ~= lastActivePlayers) then
-    lastActivePlayers = #activePlayers
+	local activePlayers = spGetPlayerList(true)
+	if (#activePlayers ~= lastActivePlayers) then
+		lastActivePlayers = #activePlayers
 
-    local _, _, _, teamID, allyTeamID = GetPlayerInfo(playerid)
-    if allyTeamID == myAllyTeamID then
-      local playersInTeam  = GetPlayerList(teamID, true)
-      -- check if team has at least 1 active player
-      -- (e.g. team 0 has all the specs)
-      if playersInTeam then
-        for i,p in ipairs(playersInTeam) do
-          local _, active, spec = GetPlayerInfo(playerid)
-          if not spec and active then
-            return false
-          end
-        end
-        return true
-      else
-        -- no player in team
-        return true
-      end
-    end
-
-  end
+		local _, _, _, teamID, allyTeamID = spGetPlayerInfo(playerid)
+		if allyTeamID == myAllyTeamID then
+			local playersInTeam  = spGetPlayerList(teamID, true)
+			-- check if team has at least 1 active player
+			-- (e.g. team 0 has all the specs)
+			if #playersInTeam > 0 then
+				for i,p in ipairs(playersInTeam) do
+					local _, active, spec = spGetPlayerInfo(playerid)
+					if not spec and active then
+						return false
+					end
+				end
+				return true
+			else -- no player in team
+				return true
+			end
+		end
+	end
 end
+-]]
 
+local function ProcessButton()
+	if not spGetSpectatingState() then
+		takeableTeamsCached = {}
+		myAllyTeamID = spGetMyAllyTeamID()
+		textPlsWait = false -- cancel show "Wait.."
+		count = UpdateUnitsToTake()
+		if (count > 0) then
+			if (autoTake) then
+				Take()
+			else
+				BindCallins()
+			end
+		else
+			UnbindCallins()
+		end
+	else
+		UnbindCallins()
+	end
+end
 
 local function IsOnButton(x, y)
   return x >= posx - buttonX and x <= posx + buttonX
@@ -146,9 +179,11 @@ end
 
 
 function Take()
-  Spring.SendCommands("take")
-  --Spring.SendLuaUIMsg("TAKE")
-  return
+	if alternateTake then
+		Spring.SendLuaUIMsg("TAKE")
+	else
+		Spring.SendCommands("take")
+	end
 end
 
 ------------------------------------------------
@@ -159,19 +194,26 @@ function _Update(_,dt)
   blinkTimer = blinkTimer + dt
   if (blinkTimer>1) then blinkTimer = 0 end
   colorBool = (blinkTimer > 0.5)
-
+	--[[
   if (recheck) then
-    UpdateUnitsToTake()
+  	textPlsWait = false -- cancel show "Wait.."
+    count = UpdateUnitsToTake()
     if (count == 0) then
       UnbindCallins()
     end
     recheck = false
   end
+  --]]
 end
 
 
 function widget:UnitTaken()
-  recheck = true
+	--recheck = true
+  	textPlsWait = false -- cancel show "Wait.."
+    count = UpdateUnitsToTake()
+    if (count == 0) then
+		UnbindCallins()
+    end
 end
 
 
@@ -182,12 +224,14 @@ end
 
 function _MouseRelease(_,x, y, button)
   if (IsOnButton(x, y)) then
-    UpdateUnitsToTake()
+	textPlsWait = false -- cancel show "Wait.."
+    count = UpdateUnitsToTake()
     if (count > 0) then
       Take()
     else
       UnbindCallins()
     end
+	textPlsWait = true -- show "Wait.."
     return -1
   end
   return false
@@ -211,21 +255,22 @@ function _DrawScreen()
   else
     colorStr = falseColor
   end
-  gl.Text(colorStr .. "Click here to take " .. count .. " unit(s)!", posx, posy + buttonY * 0.5, 24, "ovc")
+  local displayText = (textPlsWait and (trueColor .. "Wait...")) or (colorStr .. "Click here to take " .. count .. " unit(s)!")
+  gl.Text(displayText, posx, posy + buttonY * 0.5, 24, "ovc")
 end
 
 
 function _DrawWorld()
   if colorBool then
-    myTeamID = GetMyTeamID()
+    myTeamID = spGetMyTeamID()
     glColor(1,1,1,1)
-    local visibleUnits = GetVisibleUnits(Spring.ALLY_UNITS,nil,true) 
+    local visibleUnits = spGetVisibleUnits(Spring.ALLY_UNITS,nil,true) 
     for i=1,#visibleUnits do 
       local currUnit = visibleUnits[i]
-      local currTeam = GetUnitTeam(currUnit)
-      if currTeam and (AreTeamsAllied(myTeamID, currTeam) and GetTeamIsTakeableCached(currTeam)) then
+      local currTeam = spGetUnitTeam(currUnit)
+      if currTeam and (spAreTeamsAllied(myTeamID, currTeam) and GetTeamIsTakeableCached(currTeam)) then
         glPushMatrix()
-        local ux, uy, uz = GetUnitPosition(currUnit)
+        local ux, uy, uz = spGetUnitPosition(currUnit)
         glTranslate(ux, uy, uz)
         glBillboard()
         glText("\255\255\255\1T", 0, -24, 48, "c")
@@ -281,8 +326,8 @@ function widget:Initialize()
   posx = vsx * 0.75
   posy = vsy * 0.75
   count = 0
-  myAllyTeamID = GetMyAllyTeamID()
-  lastActivePlayers = #(GetPlayerList(true) or {})
+  myAllyTeamID = spGetMyAllyTeamID()
+  --lastActivePlayers = #(spGetPlayerList(true) or {})
 end
 
 
@@ -293,44 +338,31 @@ function widget:ViewResize(viewSizeX, viewSizeY)
   posy = vsy * 0.75
 end
 
-
-function widget:PlayerChanged()
-  if not GetSpectatingState() then
-    takeableTeamsCached = {}
-    myAllyTeamID = GetMyAllyTeamID()
-    UpdateUnitsToTake()
-    if (count > 0) then
-      if (autoTake) then
-        Take()
-      else
-        BindCallins()
-      end
-    else
-      UnbindCallins()
-    end
-  else
-    UnbindCallins()
-  end
+function widget:GameStart() -- check at game start for players who dropped or didn't connect at all
+	gameStarted = true
+	ProcessButton()
 end
 
--- don't check for dropped players before the game starts, they might reconnect
-function widget:PlayerRemoved(player, reason)
-  if gameStarted then
-    if (SomeoneDropped(player)) then
-      widget:PlayerChanged()
-    end
-  end
+function widget:PlayerChanged() --check for player who became spec or un-spec
+	ProcessButton()
 end
 
--- check on game start for players who dropped or didn't connect at all
-function widget:GameStart()
-  gameStarted = true
-
-  widget:PlayerChanged()
+function widget:PlayerRemoved(player, reason)-- check for dropped players
+	if gameStarted then
+		ProcessButton()
+	end
 end
 
-function widget:AddConsoleLine(line,priority)
-	if (line:sub(1,20) == "Giving all units of ") then --needed to function properly with "game_lagmonitor.lua". Used to re-display the "take button"/the blipping neon-light if there's more unit to take.
-		widget:PlayerChanged()
+function widget:AddConsoleLine(line,priority) -- update button when game_lagmonitor.lua gave away units & change the TAKE method based on how the lagger left the game
+
+	local _,endString = line:find(" left the game: ",7, true)
+	if endString then --ref:http://lua-users.org/wiki/StringLibraryTutorial
+		local reason = line:sub(endString+1, endString+8)
+		if ( reason == "timeout" or reason == "normal ") then
+			alternateTake = true
+		end
+	elseif (line:sub(1,20) == "Giving all units of ") then --needed to function properly with "game_lagmonitor.lua". Used to re-display the "take button"/the blipping neon-light if there's more unit to take.
+		alternateTake =false
+		ProcessButton()
 	end
 end
