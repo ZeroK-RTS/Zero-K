46d321872bf15f07c7975c9b0b9307dd826990a3
xponen <msafwan86msafwan86_nnss@hotmail.com>
2012-06-25 14:17:55 +0000

    ops...
    svn path=/trunk/mods/zk/; revision=6998

--- LuaUI/Widgets/gui_take_remind.lua
+++ LuaUI/Widgets/gui_take_remind.lua
@@ -9,7 +9,7 @@ function widget:GetInfo()
     date      = "31 March 2007,2008,2009",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
