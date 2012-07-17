0e346320823fac16af2953f35e5a8b27c652db80
GoogleFrog <googl3frog@gmail.com>
2012-07-17 08:00:08 +0000

    Disabled radar colours widget. It is still useful for those without minimap.
    svn path=/trunk/mods/zk/; revision=7300

--- LuaUI/Widgets/gui_radar_colors.lua
+++ LuaUI/Widgets/gui_radar_colors.lua
@@ -7,7 +7,7 @@ function widget:GetInfo()
     date      = "8 April 2012",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true --  loaded by default?
+    enabled   = false --  loaded by default?
   }
 end
 
