714efbb060b5491993a2594bfec2cb519cdedc9d
GoogleFrog <googl3frog@gmail.com>
2012-08-07 13:05:07 +0000

    It is probably a good idea to fill amgeo yardmap with gs.
    svn path=/trunk/mods/zk/; revision=7705

--- units/amgeo.lua
+++ units/amgeo.lua
@@ -57,7 +57,7 @@ unitDef = {
   turnRate                      = 0,
   useBuildingGroundDecal        = true,
   workerTime                    = 0,
-  yardMap                       = [[ooooo ooooo ooooo ooooo ooooo]],
+  yardMap                       = [[ooooo ogggo ogggo ogggo ooooo]],
 
   featureDefs                   = {
 
