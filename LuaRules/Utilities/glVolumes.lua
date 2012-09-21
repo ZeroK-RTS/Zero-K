b993ca3114a878114b4706957b86aa173caf342c
Rafal <rafal_karczm@o2.pl>
2012-09-21 02:45:45 +0000

    Utilities\glVolumes.lua: - optimized gl.Utilities.DrawMyCylinder function, mostly to use less math.sin and math.cos calls
    svn path=/trunk/mods/zk/; revision=8087

--- LuaRules/Utilities/glVolumes.lua
+++ LuaRules/Utilities/glVolumes.lua
@@ -1,9 +1,10 @@
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 -- Exported Functions:
---  gl.Utilities.DrawGroundCircle(x,z,radius)
 --  gl.Utilities.DrawMyBox(minX,minY,minZ, maxX,maxY,maxZ)
 --  gl.Utilities.DrawMyCylinder(x,y,z, height,radius,divs)
+--  gl.Utilities.DrawGroundRectangle(x1,z1,x2,z2)
+--  gl.Utilities.DrawGroundCircle(x,z,radius)
 --  gl.Utilities.DrawVolume(vol_dlist)
 
 --------------------------------------------------------------------------------
@@ -68,59 +69,63 @@ function gl.Utilities.DrawMyBox(minX,minY,minZ, maxX,maxY,maxZ)
 end
 
 
+local function CreateSinCosTable(divs)
+  local sinTable = {}
+  local cosTable = {}
+  
+  local divAngle = TWO_PI / divs
+  local alpha = 0
+  local i = 1
+  repeat
+    sinTable[i] = sin(alpha)
+    cosTable[i] = cos(alpha)
+    
+    alpha = alpha + divAngle
+    i = i + 1
+  until (alpha >= TWO_PI)
+  sinTable[i] = 0.0 -- sin(TWO_PI)
+  cosTable[i] = 1.0 -- cos(TWO_PI)
+  
+  return sinTable, cosTable
+end
+
+
 function gl.Utilities.DrawMyCylinder(x,y,z, height,radius,divs)
   divs = divs or 25
-  local halfHeight = height / 2
-  local divAngle = TWO_PI / divs
+  local sinTable, cosTable = CreateSinCosTable(divs)
+  local bottomY = y - (height / 2)
+  local topY    = y + (height / 2)
   
   gl.BeginEnd(GL.TRIANGLE_STRIP, function()
-    --//Note: We can't use for-loops here cause of precision issues!
-
     --// top
-    local alpha = -divAngle
-    repeat
-      alpha = alpha + divAngle
-      alpha = min(alpha, TWO_PI)
-
-      local sa = sin(alpha)
-      local ca = cos(alpha)
-      glVertex(x+radius*sa, y+halfHeight, z+radius*ca)
-      glVertex(x, y+halfHeight, z)
-    until (alpha >= TWO_PI)
+    for i = #sinTable, 1, -1 do
+      glVertex(x + radius*sinTable[i], topY, z + radius*cosTable[i])
+      glVertex(x, topY, z)
+    end
 
     --// degenerate
-    glVertex(x, y+halfHeight, z)
-    glVertex(x, y-halfHeight, z)
-    glVertex(x, y-halfHeight, z)
+    glVertex(x, topY   , z)
+    glVertex(x, bottomY, z)
+    glVertex(x, bottomY, z)
 
     --// bottom
-    alpha = divAngle
-    repeat
-      alpha = alpha - divAngle
-      alpha = max(alpha, -TWO_PI)
-
-      local sa = sin(alpha)
-      local ca = cos(alpha)
-      glVertex(x+radius*sa, y-halfHeight, z+radius*ca)
-      glVertex(x, y-halfHeight, z)
-    until (alpha <= -TWO_PI)
+    for i = #sinTable, 1, -1 do
+      glVertex(x + radius*sinTable[i], bottomY, z + radius*cosTable[i])
+      glVertex(x, bottomY, z)
+    end
 
     --// degenerate
-    glVertex(x, y-halfHeight, z)
-    glVertex(x, y-halfHeight, z+radius)
-    glVertex(x, y-halfHeight, z+radius)
+    glVertex(x, bottomY, z)
+    glVertex(x, bottomY, z+radius)
+    glVertex(x, bottomY, z+radius)
 
     --// sides
-    alpha = -divAngle
-    repeat
-      alpha = alpha + divAngle
-      alpha = min(alpha, TWO_PI)
-
-      local sa = sin(alpha)
-      local ca = cos(alpha)
-      glVertex(x+radius*sa, y+halfHeight, z+radius*ca)
-      glVertex(x+radius*sa, y-halfHeight, z+radius*ca)
-    until (alpha >= TWO_PI)
+    for i = 1, #sinTable do
+      local rx = x + radius * sinTable[i]
+      local rz = z + radius * cosTable[i]
+      glVertex(rx, topY   , rz)
+      glVertex(rx, bottomY, rz)
+    end
   end)
 end
 
@@ -130,21 +135,21 @@ local minheight, maxheight = Spring.GetGroundExtremes()  --the returned values d
 local averageGroundHeight = (minheight + maxheight) / 2
 local shapeHeight = heightMargin + (maxheight - minheight) + heightMargin
 
-local cylinder = gl.CreateList(gl.Utilities.DrawMyCylinder,0,0,0,1,1,35)
-function gl.Utilities.DrawGroundCircle(x,z,radius)
-  gl.PushMatrix()
-  gl.Translate(x, averageGroundHeight, z)
-  gl.Scale(radius, shapeHeight, radius)
-  gl.Utilities.DrawVolume(cylinder)
-  gl.PopMatrix()
-end
-
 local box = gl.CreateList(gl.Utilities.DrawMyBox,0,0.5,0,1,0.5,1)
 function gl.Utilities.DrawGroundRectangle(x1,z1,x2,z2)
   gl.PushMatrix()
   gl.Translate(x1, averageGroundHeight, z1)
   gl.Scale(x2-x1, shapeHeight, z2-z1)
   gl.Utilities.DrawVolume(box)
+  gl.PopMatrix()
+end
+
+local cylinder = gl.CreateList(gl.Utilities.DrawMyCylinder,0,0,0,1,1,35)
+function gl.Utilities.DrawGroundCircle(x,z,radius)
+  gl.PushMatrix()
+  gl.Translate(x, averageGroundHeight, z)
+  gl.Scale(radius, shapeHeight, radius)
+  gl.Utilities.DrawVolume(cylinder)
   gl.PopMatrix()
 end
 
