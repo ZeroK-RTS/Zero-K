23b06e39a722bfdc1356ae1767fb9883314c96ae
GoogleFrog <googl3frog@gmail.com>
2012-12-18 03:38:29 +0000

    Whitespace change for carrier drones.
    svn path=/trunk/mods/zk/; revision=8650

--- LuaRules/Gadgets/unit_carrier_drones.lua
+++ LuaRules/Gadgets/unit_carrier_drones.lua
@@ -65,62 +65,62 @@ local function InitCarrier(unitDefID, unitAllyID)
 end
 
 local function NewDrone(unitID, unitDefID, droneName)
-  local x, y, z = GetUnitPosition(unitID)
-  local angle = math.rad(random(1,360))
-  local xS = (x + (math.sin(angle) * 20))
-  local zS = (z + (math.cos(angle) * 20))
-  local droneID = CreateUnit(droneName,x,y,z,1,carrierList[unitID].unitAllyID)
-  carrierList[unitID].reload = carrierDefs[unitDefID].reloadTime
-  carrierList[unitID].droneCount = (carrierList[unitID].droneCount + 1)
-  carrierList[unitID].drones[droneID] = true
-  
-  SetUnitPosition(droneID, xS, zS, true)
-  GiveOrderToUnit(droneID, CMD.MOVE_STATE, { 2 }, {})
-  GiveOrderToUnit(droneID, CMD.IDLEMODE, { 0 }, {})
-  --GiveOrderToUnit(droneID, CMD.AUTOREPAIRLEVEL, { 3 }, {})
-  GiveOrderToUnit(droneID, CMD.FIGHT,  {(x + (random(0,600) - 300)), 60, (z + (random(0,600) - 300))}, {""})
-  GiveOrderToUnit(droneID, CMD.GUARD, {unitID} , {"shift"})
-    
-  SetUnitNoSelect(droneID,true)
-  
-  droneList[droneID] = unitID
+	local x, y, z = GetUnitPosition(unitID)
+	local angle = math.rad(random(1,360))
+	local xS = (x + (math.sin(angle) * 20))
+	local zS = (z + (math.cos(angle) * 20))
+	local droneID = CreateUnit(droneName,x,y,z,1,carrierList[unitID].unitAllyID)
+	carrierList[unitID].reload = carrierDefs[unitDefID].reloadTime
+	carrierList[unitID].droneCount = (carrierList[unitID].droneCount + 1)
+	carrierList[unitID].drones[droneID] = true
+	
+	SetUnitPosition(droneID, xS, zS, true)
+	GiveOrderToUnit(droneID, CMD.MOVE_STATE, { 2 }, {})
+	GiveOrderToUnit(droneID, CMD.IDLEMODE, { 0 }, {})
+	--GiveOrderToUnit(droneID, CMD.AUTOREPAIRLEVEL, { 3 }, {})
+	GiveOrderToUnit(droneID, CMD.FIGHT,	{(x + (random(0,600) - 300)), 60, (z + (random(0,600) - 300))}, {""})
+	GiveOrderToUnit(droneID, CMD.GUARD, {unitID} , {"shift"})
+		
+	SetUnitNoSelect(droneID,true)
+	
+	droneList[droneID] = unitID
 end
 
 function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
-  if (carrierList[unitID]) then
-    for droneID in pairs(carrierList[unitID].drones) do
-		droneList[droneID] = nil
-		AddUnitDamage(droneID,1000)
-    end
-    carrierList[unitID] = nil
-  elseif (droneList[unitID]) then
-    local i = droneList[unitID]
-	carrierList[i].droneCount = (carrierList[i].droneCount - 1)
-	carrierList[i].drones[unitID] = nil
-	droneList[unitID] = nil
-  end
+	if (carrierList[unitID]) then
+		for droneID in pairs(carrierList[unitID].drones) do
+			droneList[droneID] = nil
+			AddUnitDamage(droneID,1000)
+		end
+		carrierList[unitID] = nil
+	elseif (droneList[unitID]) then
+		local i = droneList[unitID]
+		carrierList[i].droneCount = (carrierList[i].droneCount - 1)
+		carrierList[i].drones[unitID] = nil
+		droneList[unitID] = nil
+	end
 end
 
 function gadget:UnitFinished(unitID, unitDefID, unitTeam)
-  if (carrierDefs[unitDefID]) then
-    carrierList[unitID] = InitCarrier(unitDefID, unitTeam)
-  end
+	if (carrierDefs[unitDefID]) then
+		carrierList[unitID] = InitCarrier(unitDefID, unitTeam)
+	end
 end
 
 function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
-  if (droneList[unitID]) then 
-	if capture then
-		gadget:UnitDestroyed(unitID, unitDefID, oldTeam)
+	if (droneList[unitID]) then 
+		if capture then
+			gadget:UnitDestroyed(unitID, unitDefID, oldTeam)
+			return true
+		end
+		return false
+	else
+		if (carrierList[unitID] ~= nil) then
+			gadget:UnitDestroyed(unitID, unitDefID, oldTeam)
+			gadget:UnitFinished(unitID, unitDefID, newTeam)
+		end		
 		return true
 	end
-    return false
-  else
-    if (carrierList[unitID] ~= nil) then
-      gadget:UnitDestroyed(unitID, unitDefID, oldTeam)
-      gadget:UnitFinished(unitID, unitDefID, newTeam)
-    end    
-    return true
-  end
 end
 
 local function GetDistance(x1, x2, y1, y2)
@@ -138,7 +138,9 @@ local function UpdateCarrierTarget(carrierID)
 		else
 			px,py,pz = unpack(params)
 		end
-		if not px then return end
+		if not px then 
+			return 
+		end
 		-- check range
 		local dist = GetDistance(ox,px,oz,pz)
 		if dist > carrierDefs[carrierList[carrierID].unitDefID].range then
@@ -155,10 +157,10 @@ local function UpdateCarrierTarget(carrierID)
 			]]--
 			--if not engaged then
 				--Spring.Echo("Ordering drone " .. droneID .. " to attack")
-				droneList[droneID] = nil	-- to keep AllowCommand from blocking the order
-				GiveOrderToUnit(droneID, CMD.FIGHT, {(px + (random(0,200) - 100)), (py+120), (pz + (random(0,200) - 100))} , {""})
-				GiveOrderToUnit(droneID, CMD.GUARD, {carrierID} , {"shift"})
-				droneList[droneID] = carrierID
+			droneList[droneID] = nil	-- to keep AllowCommand from blocking the order
+			GiveOrderToUnit(droneID, CMD.FIGHT, {(px + (random(0,200) - 100)), (py+120), (pz + (random(0,200) - 100))} , {""})
+			GiveOrderToUnit(droneID, CMD.GUARD, {carrierID} , {"shift"})
+			droneList[droneID] = carrierID
 			--end 
 		end
 	else
@@ -183,11 +185,11 @@ local function UpdateCarrierTarget(carrierID)
 end
 
 function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
-  if (droneList[unitID] ~= nil) then
-    return false
-  else
-    return true
-  end
+	if (droneList[unitID] ~= nil) then
+		return false
+	else
+		return true
+	end
 end
 
 --[[
@@ -199,27 +201,27 @@ end
 ]]--
 
 function gadget:GameFrame(n)
-  if (((n+1) % 30) < 0.1) then
-    for i,_ in pairs(carrierList) do
-	  local carrier = carrierList[i]
-	  local carrierDef = carrierDefs[carrier.unitDefID]
-      if (carrier.reload > 0) then
-        carrier.reload = (carrier.reload - 1)
-      elseif (carrier.droneCount < carrierDef.maxDrones) then
-		for n=1,carrierDef.spawnSize do
-			if (carrier.droneCount >= carrierDef.maxDrones) then
-				break
+	if (((n+1) % 30) < 0.1) then
+		for i,_ in pairs(carrierList) do
+			local carrier = carrierList[i]
+			local carrierDef = carrierDefs[carrier.unitDefID]
+			if (carrier.reload > 0) then
+				carrier.reload = (carrier.reload - 1)
+			elseif (carrier.droneCount < carrierDef.maxDrones) then
+				for n=1,carrierDef.spawnSize do
+					if (carrier.droneCount >= carrierDef.maxDrones) then
+						break
+					end
+					NewDrone(i, carrier.unitDefID, carrierDef.drone) 
+				end
 			end
-			NewDrone(i, carrier.unitDefID, carrierDef.drone) 
-        end
-      end
-    end
-  end
-  if ((n % DEFAULT_UPDATE_ORDER_FREQUENCY) < 0.1) then
-	for i,_ in pairs(carrierList) do
-		UpdateCarrierTarget(i)
+		end
 	end
-  end
+	if ((n % DEFAULT_UPDATE_ORDER_FREQUENCY) < 0.1) then
+		for i,_ in pairs(carrierList) do
+			UpdateCarrierTarget(i)
+		end
+	end
 end
 
 --[[
