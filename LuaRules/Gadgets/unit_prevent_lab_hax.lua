3f4d85322127f5cc96146c8e4a1e3db1b0749586
xponen <msafwan86msafwan86_nnss@hotmail.com>
2013-01-14 13:40:36 +0000

    'unit_prevent_lab_hax.lua': fix r8895 (the one that cause googlefrog to be angry!), and include factoryplane for anti-labhax because fleas can still go into enemy air factory.
    svn path=/trunk/mods/zk/; revision=8955

--- LuaRules/Gadgets/unit_prevent_lab_hax.lua
+++ LuaRules/Gadgets/unit_prevent_lab_hax.lua
@@ -38,7 +38,7 @@ local min = math.min
 local terraunitDefID = UnitDefNames["terraunit"].id
 
 local EXCEPTION_LIST = {
-  factoryplane = true,
+  --factoryplane = true,
   factorygunship = true,
   missilesilo = true,
   armasp = true,
@@ -237,9 +237,10 @@ function gadget:UnitCreated(unitID, unitDefID)
 		--Spring.MarkerAddLine(lab[unitID].maxx,0,lab[unitID].maxz,lab[unitID].maxx,0,lab[unitID].minz)
 		--Spring.MarkerAddLine(lab[unitID].maxx,0,lab[unitID].maxz,lab[unitID].minx,0,lab[unitID].maxz)
 
-		lab[unitID].miny = spGetGroundHeight(ux,uz)
-		lab[unitID].maxy = lab[unitID].miny+100
-		end
+		local _,sizeY,_,_,offsetY = Spring.GetUnitCollisionVolumeData(unitID)
+		lab[unitID].miny = uy + offsetY - sizeY/2 --set the box bottom
+		lab[unitID].maxy = uy + offsetY + 20 --set the box height +20 elmo above the factory
+	end
   end
   
 end
