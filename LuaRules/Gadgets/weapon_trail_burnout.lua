bed1e8cd89aeca8eb51033b8102faa1b8e3c15b7
Anarchid <anarchid@mail.ru>
2013-01-14 20:58:40 +0000

    gadget to alter ceg trails of certain projectiles midflight (such as high-altitude kinetic missiles)
    svn path=/trunk/mods/zk/; revision=8965

---
+++ LuaRules/Gadgets/weapon_trail_burnout.lua
@@ -0,0 +1,60 @@
+function gadget:GetInfo()
+	return {
+		name = "Trail Burnout",
+		desc = "Changes weapon trail CEG after a customParam-specified time",
+		author = "Anarchid",
+		date = "14.01.2013",
+		license = "Public domain",
+		layer = 21,
+		enabled = true
+	}
+end
+
+------ SYNCED -------------------------------------------------------
+if (gadgetHandler:IsSyncedCode()) then 
+
+local defaultCeg = ''
+local burnoutWeapon = {}
+local burnoutProjectile = {}
+local spSetProjectileCeg = Spring.SetProjectileCEG
+local curFrame = 0
+
+function gadget:Initialize()
+	local j = 0;
+	for i=1,#WeaponDefs do
+		j=j+1;
+		local wd = WeaponDefs[i]
+		if wd.customParams then
+			if wd.customParams.trail_burnout then
+				burnoutWeapon[wd.id] = {
+					burnout = wd.customParams.trail_burnout,
+					burnoutCeg = wd.customParams.trail_burnout_ceg or defaultCeg
+				}
+				Script.SetWatchWeapon(wd.id, true)
+			end
+		end
+	end
+end
+
+function gadget:ProjectileCreated(proID, proOwnerID, weaponID)
+	if burnoutWeapon[weaponID] then
+		burnoutProjectile[proID] = {
+			startFrame = curFrame,
+			burnout = burnoutWeapon[weaponID].burnout,
+			burnoutCeg = burnoutWeapon[weaponID].burnoutCeg or defaultCeg
+		}
+	end
+end	
+
+function gadget:GameFrame(f)
+	curFrame = f
+	for id, proj in pairs(burnoutProjectile) do
+		if proj.startFrame+proj.burnout <= f then
+			spSetProjectileCeg(id, proj.burnoutCeg)
+			burnoutProjectile[id] = nil
+		end
+	end	
+end
+	
+end 
+----- END SYNCED ---------------------------------------------------
