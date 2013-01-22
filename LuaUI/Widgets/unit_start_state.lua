13b220fe8ac79e762420bb882e7bff6e8766994c
xponen <msafwan86msafwan86_nnss@hotmail.com>
2013-01-22 22:39:09 +0000

    Fix for unit 'stuck' in factory. Don't know where else to put so put it in 'unit_start_state.lua'.
    svn path=/trunk/mods/zk/; revision=9055

--- LuaUI/Widgets/unit_start_state.lua
+++ LuaUI/Widgets/unit_start_state.lua
@@ -449,7 +449,7 @@ for i = 1, #buildOpts do
 	end
 end
 
-function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID) 
+function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
 	if unitTeam == Spring.GetMyTeamID() and unitDefID and UnitDefs[unitDefID] then
 		local orderArray={}
         if UnitDefs[unitDefID].customParams.commtype or UnitDefs[unitDefID].customParams.level then
@@ -605,6 +605,20 @@ end
 --]]
 
 function widget:UnitFromFactory(unitID, unitDefID, unitTeam, factID, factDefID, userOrders)
+	---Order unit to move away from factory's build yard---
+	local queue = Spring.GetUnitCommands(unitID, 1)
+	local firstCommand = queue and queue[1]
+	if firstCommand then
+		if firstCommand.id ~= CMD.MOVE then --we leave unit with CMD.MOVE alone because we don't want to disturb factory's move command (factory's rallying behaviour)
+			local dx,_,dz = Spring.GetUnitDirection(unitID)
+			local x,y,z = Spring.GetUnitPosition(unitID)
+			dx = dx*100 --Note: don't need trigonometry here because factory direction is either {0+-,1+-} or {1+-,0+-} (1 or 0), so multiply both with 100 elmo is enough
+			dz = dz*100
+			Spring.GiveOrderToUnit(unitID, CMD.INSERT, {0, CMD.MOVE, CMD.OPT_INTERNAL, x+dx, y, z+dz}, {"alt"})--insert move command behind existing command
+			--Spring.Echo(CMD[firstCommand.id])
+		end
+	end
+	------
 	if unitTeam == Spring.GetMyTeamID() and unitDefID and UnitDefs[unitDefID] then
 		local name = UnitDefs[unitDefID].name
 		if options[name .. "_constructor_buildpriority"] and options[name .. "_constructor_buildpriority"].value then
