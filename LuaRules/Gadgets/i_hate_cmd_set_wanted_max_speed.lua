e8b8d07b296756923a3914c0093ff6a626e46dc6
Licho <jalich@gmail.com>
2013-01-26 21:03:53 +0000

    VERSION{v1.1.1.4}
    svn path=/trunk/mods/zk/; revision=9131

--- LuaRules/Gadgets/i_hate_cmd_set_wanted_max_speed.lua
+++ LuaRules/Gadgets/i_hate_cmd_set_wanted_max_speed.lua
@@ -7,7 +7,7 @@ function gadget:GetInfo()
     date      = "1 Sep 2012",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = Game.version == '91.0'  --  loaded by default?
+    enabled   = Game.version:find("91.0")  --  loaded by default?
   }
 end
 
