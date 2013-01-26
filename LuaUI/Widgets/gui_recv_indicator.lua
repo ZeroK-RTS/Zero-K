92a5c757e2afa79c75d210e28dd5f51f1d79d2f6
KingRaptor <kingraptorowns@gmail.com>
2013-01-26 15:54:24 +0000

    gui_recv_indicator doesn't care about units received from Gaia.
    svn path=/trunk/mods/zk/; revision=9122

--- LuaUI/Widgets/gui_recv_indicator.lua
+++ LuaUI/Widgets/gui_recv_indicator.lua
@@ -50,6 +50,7 @@ local glPolygonMode = gl.PolygonMode
 local glText = gl.Text
 --end----------------------------------------------------------------------------
 ---------------------------------------------------------------------------------
+local gaiaTeamID = Spring.GetGaiaTeamID()
 local myTeamID_gbl = -1 --//variable: myTeamID
 local receivedUnitList_gbl = {} --//variable: store unitID and its corresponding unitPosition
 local givenByTeamID_gbl = -1 --//variable: store sender's ID
@@ -283,6 +284,9 @@ end
 
 function widget:UnitGiven(unitID, unitDefID, unitTeamID, oldTeamID) --//will be executed repeatedly if there's more than 1 unit transfer
 	if iNotLagging then
+		if oldTeamID == gaiaTeamID then
+			return
+		end
 		if spValidUnitID(unitID) and unitTeamID == myTeamID_gbl then --if my unit
 			if spAreTeamsAllied(unitTeamID, oldTeamID) or notifyCapture_gbl[oldTeamID] then --if from my ally, or from a captured enemy unit
 				--myTeamID_gbl = unitTeamID --//uncomment this and comment 'unitTeamID == myTeamID_gbl' (above) when testing
