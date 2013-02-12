d4355f5b1bff19b026256ae635170fadc12871b0
KingRaptor <kingraptorowns@gmail.com>
2013-02-12 06:20:49 +0000

    Blind safety for LayoutPanel.lua to fix Chili Selections crash
    svn path=/trunk/mods/zk/; revision=9336

--- LuaUI/Widgets/chili/Controls/LayoutPanel.lua
+++ LuaUI/Widgets/chili/Controls/LayoutPanel.lua
@@ -248,6 +248,9 @@ function LayoutPanel:_AutoArrangeOrdinate(freeSpace)
   local lineSizes = {}
   for i=1,#_lines do
     local first_cell_in_line = _cells[ _lines[i] ]
+    if not first_cell_in_line then
+      break
+    end
     if (self.orientation == "horizontal") then --FIXME
       lineSizes[i] = {i,first_cell_in_line[4]}
     else
