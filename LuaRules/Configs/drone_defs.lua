331387532f0cadd712486cfbd5d1500226c54d1b
KingRaptor <kingraptorowns@gmail.com>
2013-02-13 15:19:24 +0000

    Fix drone spawn rates accidentally broken in previous commit
    svn path=/trunk/mods/zk/; revision=9346

--- LuaRules/Configs/drone_defs.lua
+++ LuaRules/Configs/drone_defs.lua
@@ -3,14 +3,14 @@
 local carrierDefs = {}
 
 local carrierDefNames = {
-	armcarry = { {drone = UnitDefNames.carrydrone.id, reloadTime = 3, maxDrones = 8, spawnSize = 2, range = 1600} },
+	armcarry = { {drone = UnitDefNames.carrydrone.id, reloadTime = 15, maxDrones = 8, spawnSize = 2, range = 1600} },
 	--corcrw = { {drone = UnitDefNames.attackdrone.id, reloadTime = 15, maxDrones = 6, spawnSize = 2, range = 900} },
 	funnelweb = {{drone = UnitDefNames.attackdrone.id, reloadTime = 15, maxDrones = 6, spawnSize = 2, range = 600}},
 }
 
 local presets = {
-	module_companion_drone = {drone = UnitDefNames.attackdrone.id, reloadTime = 2, maxDrones = 2, spawnSize = 1, range = 450},
-	module_battle_drone = {drone = UnitDefNames.battledrone.id, reloadTime = 4, maxDrones = 1, spawnSize = 1, range = 600},
+	module_companion_drone = {drone = UnitDefNames.attackdrone.id, reloadTime = 10, maxDrones = 2, spawnSize = 1, range = 450},
+	module_battle_drone = {drone = UnitDefNames.battledrone.id, reloadTime = 20, maxDrones = 1, spawnSize = 1, range = 600},
 }
 
 --[[
