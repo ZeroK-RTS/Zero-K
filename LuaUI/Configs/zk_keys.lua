299dc44b43f8af92bcf9d301f814d01d10eb9c6f
KingRaptor <kingraptorowns@gmail.com>
2013-02-19 13:45:32 +0000

    Forgot zk_keys.lua.
    svn path=/trunk/mods/zk/; revision=9470

---
+++ LuaUI/Configs/zk_keys.lua
@@ -0,0 +1,8 @@
+local date = 20130219	-- yyyymmdd
+			-- if newer than user's, overwrite ALL zir zk_keys
+			-- else just add any that are missing from local config
+local keybinds = {
+	-- only keybinds that differ from uikeys.txt need to be specified
+}
+
+return keybinds, date
