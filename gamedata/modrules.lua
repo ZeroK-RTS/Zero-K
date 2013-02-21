8a0418e28cf30f329934bd89f8561ef843c3ba8d
KingRaptor <kingraptorowns@gmail.com>
2013-02-21 15:23:22 +0000

    allowGroundUnitGravity = false
    svn path=/trunk/mods/zk/; revision=9534

--- gamedata/modrules.lua
+++ gamedata/modrules.lua
@@ -16,6 +16,7 @@ local modrules  = {
     allowCrushingAlliedUnits = false; -- defaults to false
     allowUnitCollisionDamage = true; -- defaults to false
     allowUnitCollisionOverlap = false,	-- defaults to true	-- this lets units clump close together when moving, after which they are pushed apart
+    allowGroundUnitGravity = false,
   },
   
   construction = {
