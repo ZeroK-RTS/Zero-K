eea46ef925c82075756f73795d5a94a28f1a3223
xponen <msafwan86msafwan86_nnss@hotmail.com>
2013-02-21 18:03:04 +0000

    'mission_orbital_drop.lua': add a calculator that automatically calculate the necessary drop speed & braking rate when you configure the following: 1) timeToGround (desired time from space to landing) 2) fallGravity (vertical forces to apply during falling period) 3) unitSpawnHeight (height when unit suddenly appear!) 4) unitBrakeHeight (height when braking thruster fire)
    svn path=/trunk/mods/zk/; revision=9539

--- LuaRules/Gadgets/mission_orbital_drop.lua
+++ LuaRules/Gadgets/mission_orbital_drop.lua
@@ -3,13 +3,13 @@ function gadget:GetInfo()
     name      = "Orbital Drop",
     desc      = "Makes units spawned with GG.DropUnit fall from the sky.",
     author    = "quantum",
-    date      = "November 2010",
+    date      = "November 2010", --February 2013
     license   = "GNU GPL, v2 or later",
     layer     = 0,
     enabled   = true,  --  loaded by default?
   }
 end
-
+--Note: 2 new FIXME: unit wiggle hax & MoveCtrl.SetGravity magic number.
 
 if not gadgetHandler:IsSyncedCode() then
   return false -- no unsynced code
@@ -19,13 +19,27 @@ local Spring = Spring
 
 ----- Settings -----------------------------------------------------------------
 
+local timeToGround = 60 --customizable (frame)
 
-local fallGravity = 1.5
-local brakeGravity = -7.8
-local unitSpawnHeight = 3000
-local unitBrakeHeight = 500
+local fallGravity = Game.gravity/30/30 --customizable (elmo/frame^2)
+local unitSpawnHeight = 3000 --customizable (elmo)
+local unitBrakeHeight = 500 --customizable (elmo)
 
 ----------------------------------------------------------------------------------
+
+local initialFallVelocity = 0 --non-customizable/is-overriden (elmo/frame)
+local brakeGravity = -7.8 --non-customizable/is-overriden (elmo/frame^2)
+
+do	--Question:What this do? Answer:it automatically configure drop behaviour using value in Settings.
+	--see Appendix for sources of the following formula:
+	local fallPeriod =  timeToGround/(1+(2*unitBrakeHeight)/(unitSpawnHeight-unitBrakeHeight)) --estimated
+	local brakePeriod = (2*unitBrakeHeight)/((unitSpawnHeight-unitBrakeHeight)/fallPeriod+fallGravity*fallPeriod/2)
+	brakeGravity = -1*((unitSpawnHeight-unitBrakeHeight)/fallPeriod+fallGravity*fallPeriod/2)/brakePeriod
+	initialFallVelocity = -1*((unitSpawnHeight-unitBrakeHeight)/fallPeriod-fallGravity*fallPeriod/2)
+	
+	brakeGravity = brakeGravity/0.1444444 --have no choice, a magic scalar. FIXME: Spring91 require this magic number
+	fallGravity = fallGravity/0.1444444
+end
 
 local units = {}
 
@@ -48,6 +62,7 @@ function GG.DropUnit(unitDefName, x, y, z, facing, teamID)
     units[unitID] = y
     Spring.MoveCtrl.Enable(unitID)
     Spring.MoveCtrl.SetPosition(unitID, x, y, z)
+    Spring.MoveCtrl.SetVelocity(unitID,0,initialFallVelocity,0)
     Spring.MoveCtrl.SetGravity(unitID, fallGravity)
   end
   return unitID
@@ -57,17 +72,20 @@ end
 function gadget:GameFrame(frame)
   for unitID, yLast in pairs(units) do
     if Spring.ValidUnitID(unitID) then
-      local x, y, z = Spring.GetUnitBasePosition(unitID)
+      local x, y, z = Spring.GetUnitPosition(unitID)
       local h = Spring.GetGroundHeight(x, z)
       local _, dy = Spring.GetUnitVelocity(unitID)
       
-      -- FIXME: dy is always zero in 84.0+
-      if y <= h or y > yLast then 
-        -- unit has landed (or is moving upwards, which means it has missed the ground)
+      if y <= h or dy > 0 then -- unit has landed (or is moving upwards, which means it has missed the ground)
+		if (y > unitBrakeHeight) then --unit is moving upwards but is not yet at BrakeHeight (which mean its being thrown upward purposely)
+			Spring.Echo("Warning: Unit is thrown upward! timeToGround might be too big.")
+		end
         Spring.MoveCtrl.SetPosition(unitID, x, h, z)
         Spring.MoveCtrl.SetVelocity(unitID, 0, 0, 0)
         Spring.MoveCtrl.Disable(unitID)
         units[unitID] = nil
+		Spring.AddUnitImpulse(unitID,0,1,0) --wiggle abit. FIXME: Spring91 unit will APPEARs to stuck (not actually stuck) in the sky when SetGravity = 1, SetVelocity = 8, and then Disable.
+		Spring.AddUnitImpulse(unitID,0,-1,0)
       elseif y < h + unitBrakeHeight then
         -- unit is braking
         Spring.MoveCtrl.SetGravity(unitID, brakeGravity)
@@ -91,3 +109,32 @@ function gadget:GameFrame(frame)
     end
   end
 end
+
+
+
+
+
+--Appendix for auto-configure algorithm (FOR REFERENCE ONLY!) by msafwan
+--[[
+--all start with equation of motion:
+fallDist = initVel*fallPeriod + fallAcc*(fallPeriod^2)/2 <---START(1) equation of motion
+initVel = fallDist/fallPeriod - fallAcc*fallPeriod/2 <--FINISH(1) (equation for getting initial velocity)
+
+finalVel = initVel + fallAcc*fallPeriod <---START(2) equation of motion
+finalVel = (fallDist/fallPeriod - fallAcc*fallPeriod/2) + fallAcc*fallPeriod
+finalVel = fallDist/fallPeriod + fallAcc*fallPeriod/2 <--FINISH(2) (equation for getting final velocity)
+
+0 = finalVel - brakeAcc* brakePeriod <---START(3) equation of motion
+brakeAcc = (fallDist/fallPeriod + fallAcc*fallPeriod/2)/brakePeriod <--FINISH(3) (equation for getting brake acceleration)
+
+brakeDist = finalVel*brakePeriod - brakeAcc*(brakePeriod^2)/2 <---START(4) equation of motion
+brakeDist = (fallDist/fallPeriod + fallAcc*fallPeriod/2)*brakePeriod - (fallDist/fallPeriod + fallAcc*fallPeriod/2)*brakePeriod/2
+brakeDist = (fallDist/fallPeriod + fallAcc*fallPeriod/2)*brakePeriod*(1-1/2)
+2*brakeDist/brakePeriod = (fallDist/fallPeriod + fallAcc*fallPeriod/2)
+brakePeriod = 2*brakeDist/(fallDist/fallPeriod + fallAcc*fallPeriod/2) <--FINISH(4) (equation for getting brake period)
+
+totalTime = fallPeriod + brakePeriod <---START(5) sum
+totalTime = fallPeriod + 2*brakeDist/(fallDist/fallPeriod + fallAcc*fallPeriod/2)
+--assume gravity is 0, then:
+totalTime = fallPeriod*(1+ 2*brakeDist/fallDist) --FINISH(5) (equation for estimating fallperiod from total time)
+--]]
