5057e78d737af03ba47bc2995599b49f28993c12
meep.wav <meep.wav@googlemail.com>
2013-02-23 16:40:09 +0000

    new seismic missile trail and explosion fx
    svn path=/trunk/mods/zk/; revision=9596

--- effects/missiletrail.lua
+++ effects/missiletrail.lua
@@ -7,6 +7,7 @@
 -- cruisetrail
 -- tactrail
 -- napalmtrail
+-- seismictrail
 -- bigemptrail
 -- emptrail
 -- raventrail
@@ -569,7 +570,103 @@ return {
       },
     },
   },
-  
+
+  ["seismictrail"] = {
+    alwaysvisible      = false,
+    usedefaultexplosions = false,
+    largeflash = {
+      air                = true,
+      class              = [[CBitmapMuzzleFlame]],
+      count              = 1,
+      ground             = true,
+      underwater         = 1,
+      water              = true,
+      properties = {
+        colormap           = [[0.8 1.0 0.1 0.01 0.2 0.4 0.1 0.01 0 0 0 0.01]],
+        dir                = [[dir]],
+        frontoffset        = 0,
+        fronttexture       = [[muzzlefront]],
+        length             = -45,
+        sidetexture        = [[muzzleside]],
+        size               = -8,
+        sizegrowth         = 0.75,
+        ttl                = 8,
+      },
+    },
+
+    smoke_front = {
+      air                = true,
+      class              = [[CSimpleParticleSystem]],
+      count              = 1,
+      ground             = true,
+      water              = true,
+      properties = {
+        airdrag            = 0.8,
+        colormap           = [[0.6 0.8 0.1 0.4 0.25 0.3 0.1 0.6 0.0 0.0 0.0 0.01]],
+        directional        = false,
+        emitrot            = 0,
+        emitrotspread      = 50,
+        emitvector         = [[dir]],
+        gravity            = [[0.05 r-0.1, 0.05 r-0.1, 0.05 r-0.1]],
+        numparticles       = 2,
+        particlelife       = 20,
+        particlelifespread = 0,
+        particlesize       = 8,
+        particlesizespread = 2,
+        particlespeed      = 10,
+        particlespeedspread = -4,
+        pos                = [[0, 1, 3]],
+        sizegrowth         = 1.5,
+        sizemod            = 1.0,
+        texture            = [[smoke]],
+      },
+    },
+
+    dirt = {
+      air                   = true,
+      class                 = [[CSimpleParticleSystem]],
+      count                 = 1,
+      ground                = true,
+      water                 = true,
+      properties = {
+        airdrag             = 0.97,
+        colormap            = [[0.2 0.3 0.1 0.01   0.1 0.2 0.0 0.01   0.0 0.0 0 0.01]],
+        directional         = true,
+        emitrot             = 0,
+        emitrotspread       = 60,
+        emitvector          = [[0, 1, 0]],
+        gravity             = [[0, -0.25, 0]],
+        numparticles        = 1,
+        particlelife        = 14,
+        particlelifespread  = 6,
+        particlesize        = 6,
+        particlesizespread  = 2,
+        particlespeed       = 2,
+        particlespeedspread = 2,
+        pos                 = [[0, 0, 0]],
+        sizegrowth          = 0,
+        sizemod             = 1.0,
+        texture             = [[dirt]],
+      },
+    },
+
+    spikes = {
+      air                = true,
+      class              = [[explspike]],
+      count              = 4,
+      ground             = true,
+      water              = true,
+      properties = {
+        alpha              = 1,
+        alphadecay         = 0.25,
+        color              = [[0.7, 0.8, 0]],
+        dir                = [[-6 r12,-6 r12,-6 r12]],
+        length             = 24,
+        width              = 12,
+      },
+    },
+  },
+
   ["bigemptrail"] = {
     alwaysvisible      = false,
     usedefaultexplosions = false,
