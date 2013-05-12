5de6c1cc41df29a73a4def0c2587f8f455dd1f32
KingRaptor <kingraptorowns@gmail.com>
2013-05-12 06:00:47 +0000

    Edge Extension off on medium graphics
    svn path=/trunk/mods/zk/; revision=10121

--- LuaUI/Widgets/map_edge_extension.lua
+++ LuaUI/Widgets/map_edge_extension.lua
@@ -10,7 +10,7 @@ function widget:GetInfo()
     license   = "GPL",
     layer     = 0,
     enabled   = true,
-    detailsDefault = 2
+    detailsDefault = 3
   }
 end
 --------------------------------------------------------------------------------
