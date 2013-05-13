a67185cd1c072d8f9e827d9cb920d96db17ab8b4
xponen <msafwan86msafwan86_nnss@hotmail.com>
2013-05-13 13:05:56 +0000

    cawidgets.lua: -move chat-preprocess into an "include(chat_preprocess.lua)" (not a logic change, just rearranging) -move file-backup into an "include(utility_two.lua)" (for use by epicmenu.lua) -remove backup for "zk_keys.lua" from cawidgets (will do in epicmenu.lua) -make "hack fix for missions not using right config files" (r9562) less strict, only enforce the hax when "Mission Mutator" exist in modDescription.
    svn path=/trunk/mods/zk/; revision=10144

---
+++ LuaUI/utility_two.lua
@@ -0,0 +1,24 @@
+-- Utility function for ZK
+
+-- Will try to read LUA content from target file and create BACKUP if have a successful read (in user's Spring folder) OR DELETE them if have a failure read
+-- This prevent corrupted file from being used.
+function CheckLUAFileAndBackup(filePath, headerString)
+	local chunk, err = loadfile(filePath)
+	if (chunk) then --if original content is LUA OK:
+		local tmp = {}
+		setfenv(chunk, tmp)
+		table.save(chunk(),filePath..".bak",headerString) --write to backup
+	else --if original content is not LUA OK:
+		Spring.Log("CheckLUAFileAndBackup","warning", tostring(err) .. " (Now will find backup file)")
+		chunk, err = loadfile(filePath..".bak")
+		if (chunk) then --if backup content is LUA OK:
+			local tmp = {}
+			setfenv(chunk, tmp)
+			table.save(chunk(),filePath,headerString) --overwrite original
+		else --if backup content is also not LUA OK:
+			Spring.Log("CheckLUAFileAndBackup","warning", tostring(err) .. " (Backup file not available)")
+			Spring.Echo(os.remove (filePath)) --delete original
+			Spring.Echo(os.remove (filePath..".bak")) --delete backup
+		end
+	end
+end
