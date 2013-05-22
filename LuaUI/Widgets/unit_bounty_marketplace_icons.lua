c427d0765dce48efedfa07c7682a155d8b34aab8
wagonrepairer <wagonrepairer@gmail.com>
2013-05-22 22:14:57 +0000

    Fixed bug in marketplace.
    svn path=/trunk/mods/zk/; revision=10194

--- LuaUI/Widgets/unit_bounty_marketplace_icons.lua
+++ LuaUI/Widgets/unit_bounty_marketplace_icons.lua
@@ -6,11 +6,11 @@ function widget:GetInfo()
     name      = "Bounty & Marketplace Icons (experimental)",
     desc      = "Shows bounty and marketplace icons",
     author    = "CarRepairer",
-	version   = "0.021",
+	version   = "0.022",
     date      = "2012-01-28",
     license   = "GNU GPL, v2 or later",
     layer     = 5,
-    enabled   = false,
+    enabled   = true,
   }
 end
 
@@ -27,18 +27,6 @@ local floor = math.floor
 ----------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------
 
-options_path = 'Settings/Interface/Hovering Icons'
-options = {
-	
-	showstateonshift = {
-		name = "Show move/fire states on shift",
-		desc = "When holding shift, icons appear over units indicating move state and fire state.",
-		type = 'bool',
-		value = false,
-	},
-	
-}
-
 
 include("keysym.h.lua")
 local iconTypes = { 'bounty', 'buy', 'sell' }
@@ -50,6 +38,10 @@ local imageDir = 'LuaUI/Images/bountymarketplaceicons/'
 
 -------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------
+
+local function ToBool(x)
+  return x and x ~= 0 and x ~= "false"
+end
 
 function SetIcons(unitID, iconType)
 	--for _, teamID in ipairs(Spring.GetTeamList()) do
@@ -108,13 +100,17 @@ end
 
 --needed if icon widget gets disabled/enabled after this one. find a better way?
 function widget:GameFrame(f)
-	if f%(32) == 0 then --1 second
+	if f%(64) == 0 then --1 second
 		UpdateAllUnits()
 	end
 end
 
 
 function widget:Initialize()
+	if not ToBool(Spring.GetModOptions().marketandbounty) then
+		echo('Marketplace and Bounty Icons: Widget removed.')
+		widgetHandler:RemoveWidget()
+	end 
 	for _, teamID in ipairs(Spring.GetTeamList()) do
 		WG.icons.SetOrder( 'bounty'..teamID, 9+teamID )
 		WG.icons.SetDisplay('bounty'..teamID, true)
