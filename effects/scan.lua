fc60929912b5fa28daff60d3210198fd56d5689d
sprunk <gnurps@o2.pl>
2013-05-28 20:48:44 +0000

    Added CEG wanted by Scan gadget
    svn path=/trunk/mods/zk/; revision=10290

---
+++ effects/scan.lua
@@ -0,0 +1,16 @@
+return {
+	["scan_sweep"] = {
+		groundflash = {
+			circlealpha = 0.2,
+			circlegrowth = 5,
+			flashalpha = 0.2,
+			flashsize = 300,
+			ttl = 60,
+			color = {
+				[1]  = 0,
+				[2]  = 0.5,
+				[3]  = 1,
+			},
+		},
+	},
+}
