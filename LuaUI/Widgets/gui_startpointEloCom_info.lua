b381c2096b115c0a2af7b164bd651da594798f9a
KingRaptor <kingraptorowns@gmail.com>
2013-06-02 09:21:42 +0000

    Disable unit_comm_nametags and gui_startpointEloCom_info by default.
    svn path=/trunk/mods/zk/; revision=10320

--- LuaUI/Widgets/gui_startpointEloCom_info.lua
+++ LuaUI/Widgets/gui_startpointEloCom_info.lua
@@ -7,7 +7,7 @@ function widget:GetInfo()
     date      = "2013 April 10",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
