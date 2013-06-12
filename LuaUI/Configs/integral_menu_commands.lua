754296f1781918ed666344efb3139798607e1bf4
wagonrepairer <wagonrepairer@gmail.com>
2013-06-12 17:42:41 +0000

    Added a gadget that replaces guard with areaguard. It works exactly the same as guard, but you can also drag a circle. It hides the existing guard, sets itself as the default command, and the hotkey is updated in zk_keys.
    svn path=/trunk/mods/zk/; revision=10437

--- LuaUI/Configs/integral_menu_commands.lua
+++ LuaUI/Configs/integral_menu_commands.lua
@@ -211,6 +211,8 @@ local overrides = {
 	[CMD_RESTORE] = {texture = imageDir .. 'restore.png'},
 	[CMD_BUMPY] = {texture = imageDir .. 'bumpy.png'},
 	
+	[CMD_AREA_GUARD] = { texture = imageDir .. 'Bold/guard.png'},
+	
 	[CMD_AREA_MEX] = {text = ' ', texture = imageDir .. 'Bold/mex.png'},
 	
 	[CMD_JUMP] = {texture = imageDir .. 'Bold/jump.png'},	
@@ -311,6 +313,7 @@ local custom_cmd_actions = {
 	radialmenu=3,
 	placebeacon=1,
 	buildprev=1,
+	areaguard=1,
 	
 	-- terraform
 	rampground=1,
