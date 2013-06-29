e9f23e5373b9ae1f305964a30930ff8d343ae02c
sprunk <gnurps@o2.pl>
2013-06-29 19:51:04 +0000

    Minor speed-ups (localized functions)
    svn path=/trunk/mods/zk/; revision=10624

--- LuaRules/Gadgets/game_lagmonitor.lua
+++ LuaRules/Gadgets/game_lagmonitor.lua
@@ -34,7 +34,7 @@ end
 if (gadgetHandler:IsSyncedCode()) then -- SYNCED ---
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
-local lineage = {} --keep track of unit ownership: Is populated when gadget give away units, and when units is created. Depopulated when units is destroyed, or is finished construction, or when gadget return units to owner. 
+local lineage = {} --keep track of unit ownership: Is populated when gadget give away units, and when units is created. Depopulated when units is destroyed, or is finished construction, or when gadget return units to owner.
 local afkTeams = {}
 local tickTockCounter = {} --remember how many second a player is in AFK mode. To add a delay before unit transfer commence.
 local unitAlreadyFinished = {}
@@ -43,7 +43,21 @@ local oldAllyTeam = {} -- allyTeam which player was on last frame
 
 GG.Lagmonitor_activeTeams = {}
 
-local spGetUnitDefID = Spring.GetUnitDefID
+local spAddTeamResources  = Spring.AddTeamResources
+local spEcho              = Spring.Echo
+local spGetGameSeconds    = Spring.GetGameSeconds
+local spGetPlayerInfo     = Spring.GetPlayerInfo
+local spGetTeamInfo       = Spring.GetTeamInfo
+local spGetTeamList       = Spring.GetTeamList
+local spGetTeamResources  = Spring.GetTeamResources
+local spGetTeamRulesParam = Spring.GetTeamRulesParam
+local spGetTeamUnits      = Spring.GetTeamUnits
+local spGetUnitAllyTeam   = Spring.GetUnitAllyTeam
+local spGetUnitDefID      = Spring.GetUnitDefID
+local spGetUnitTeam       = Spring.GetUnitTeam
+local spGetPlayerList     = Spring.GetPlayerList
+local spTransferUnit      = Spring.TransferUnit
+local spUseTeamResources  = Spring.UseTeamResources
 
 local allyTeamList = Spring.GetAllyTeamList()
 for i=1,#allyTeamList do
@@ -55,7 +69,6 @@ for i=1,#allyTeamList do
 		GG.Lagmonitor_activeTeams[allyTeamID][teamID] = true
 	end
 end
-	
 
 local LAG_THRESHOLD = 25000
 local AFK_THRESHOLD = 30
@@ -68,7 +81,7 @@ function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
 end
 
 function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
-	--lineage[unitID] = builderID and (lineage[builderID] or Spring.GetUnitTeam(builderID)) or unitTeam
+	--lineage[unitID] = builderID and (lineage[builderID] or spGetUnitTeam(builderID)) or unitTeam
 	if builderID ~= nil then
 		local builderDefID = spGetUnitDefID(builderID)
 		local ud = (builderDefID and UnitDefs[builderDefID])
@@ -92,14 +105,13 @@ end
 GG.allowTransfer = false
 --FIXME block transfers for /take but allow manual H/crude list gives
 --[[
-function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture) 
+function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
 	if capture then return true end
-	return GG.allowTransfer  
+	return GG.allowTransfer
 end
 ]]--
 
 local pActivity = {}
-
 
 function gadget:RecvLuaMsg(msg, playerID)
 	if msg:find("AFK",1,true) then
@@ -111,17 +123,16 @@ end
 --------------------------------------------------------------------------------
 local UPDATE_PERIOD = 50	-- gameframes, 1.67 second
 
-
 local function GetRecepient(allyTeam, laggers)
-	local teams = Spring.GetTeamList(allyTeam)
+	local teams = spGetTeamList(allyTeam)
 	local highestRank = 0
 	local candidatesForTake = {}
 	local target
 	-- look for active people to give units to, including AIs
 	for i=1,#teams do
-		local leader = select(2, Spring.GetTeamInfo(teams[i]))
-		local name, active, spectator, _, _, _, _, _, _, customKeys = Spring.GetPlayerInfo(leader)
-		if active and not spectator and not laggers[leader] and not Spring.GetTeamRulesParam(teams[i], "WasKilled") then -- only consider giving to someone in position to take!
+		local leader = select(2, spGetTeamInfo(teams[i]))
+		local name, active, spectator, _, _, _, _, _, _, customKeys = spGetPlayerInfo(leader)
+		if active and not spectator and not laggers[leader] and not spGetTeamRulesParam(teams[i], "WasKilled") then -- only consider giving to someone in position to take!
 			candidatesForTake[#candidatesForTake+1] = {name = name, team = teams[i], rank = ((tonumber(customKeys.elo) or 0))}
 		end
 	end
@@ -134,12 +145,12 @@ local function GetRecepient(allyTeam, laggers)
 			target = player
 		end
 	end
-	
+
 	-- no rank info? pick at random
 	if not target and #candidatesForTake > 0 then
 		target = candidatesForTake[math.random(1,#candidatesForTake)]
 	end
-	
+
 	return target
 end
 
@@ -147,15 +158,15 @@ function gadget:GameFrame(n)
 	if n%UPDATE_PERIOD == 0 then --check every UPDATE_PERIOD-th frame
 		local laggers = {}
 		local specList = {}
-		local players = Spring.GetPlayerList()
+		local players = spGetPlayerList()
 		local recepientByAllyTeam = {}
-		local gameSecond = Spring.GetGameSeconds()
+		local gameSecond = spGetGameSeconds()
 		local afkPlayer = "" -- remember which player is AFK/Lagg. Information will be sent to 'gui_take_remind.lua' as string
-		
+
 		for i=1,#players do
 			local playerID = players[i]
-			local name,active,spec,team,allyTeam,ping = Spring.GetPlayerInfo(playerID)
-	
+			local name,active,spec,team,allyTeam,ping = spGetPlayerInfo(playerID)
+
 			local justResigned = false
 			if oldTeam[playerID] then
 				if spec then
@@ -171,21 +182,21 @@ function gadget:GameFrame(n)
 				oldTeam[playerID] = team
 				oldAllyTeam[playerID] = allyTeam
 			end
-			
-			local afk = Spring.GetGameSeconds() - (pActivity[playerID] or 0)
-			local _,_,_,isAI,_,_ = Spring.GetTeamInfo(team)
-			if not (spec or isAI) then 
-				if (afkTeams[team] == true) then  -- team was AFK 
+
+			local afk = gameSecond - (pActivity[playerID] or 0)
+			local _,_,_,isAI,_,_ = spGetTeamInfo(team)
+			if not (spec or isAI) then
+				if (afkTeams[team] == true) then  -- team was AFK
 					if active and ping <= 2000 and afk < AFK_THRESHOLD then -- team no longer AFK, return his or her units
-						Spring.Echo("Player " .. name .. " is no longer lagging or AFK; returning all his or her units")
+						spEcho("Player " .. name .. " is no longer lagging or AFK; returning all his or her units")
 						GG.allowTransfer = true
-						local spTransferUnit = Spring.TransferUnit
+
 						for unitID, teamList in pairs(lineage) do
 							local delete = false;
 							for i=1,#teamList do
 								local uteam = teamList[i];
 								if (uteam == team) then
-									if allyTeam == Spring.GetUnitAllyTeam(unitID) then
+									if allyTeam == spGetUnitAllyTeam(unitID) then
 										spTransferUnit(unitID, team, true)
 										delete = true
 									end
@@ -200,14 +211,14 @@ function gadget:GameFrame(n)
 						afkTeams[team] = nil
 						GG.Lagmonitor_activeTeams[allyTeam].count = GG.Lagmonitor_activeTeams[allyTeam].count + 1
 						GG.Lagmonitor_activeTeams[allyTeam][team] = true
-					end 
+					end
 				end
 				if (not active or ping >= LAG_THRESHOLD or afk > AFK_THRESHOLD) then -- player afk: mark him, except AIs
 					afkPlayer = afkPlayer .. (10000 + playerID*100 + allyTeam) --compose a string of number that contain playerID & allyTeam information
 					tickTockCounter[playerID] = (tickTockCounter[playerID] or 0) + 1 --tick tock counter ++. count-up 1
 					if tickTockCounter[playerID] >= 2 or justResigned then --team is to be tagged as lagg-er/AFK-er after 3 passes (3 times 50frame = 5 second).
-						local units = Spring.GetTeamUnits(team)
-						if units ~= nil and #units > 0 then 
+						local units = spGetTeamUnits(team)
+						if units ~= nil and #units > 0 then
 							laggers[playerID] = {name = name, team = team, allyTeam = allyTeam, units = units, resigned = justResigned}
 						end
 					end
@@ -218,14 +229,14 @@ function gadget:GameFrame(n)
 				specList[playerID] = true --record spectator list in non-AI team
 			end
 		end
-		afkPlayer = afkPlayer .. "#".. players[#players] --cap the string with the largest playerID information. ie: (string)1010219899#98 can be read like this-> id:01 ally:02, id:98 ally:99, highestID:98  
+		afkPlayer = afkPlayer .. "#".. players[#players] --cap the string with the largest playerID information. ie: (string)1010219899#98 can be read like this-> id:01 ally:02, id:98 ally:99, highestID:98
 		SendToUnsynced("LagmonitorAFK",afkPlayer) --tell widget about AFK list
-		
+
 		for playerID, data in pairs(laggers) do
 			-- FIRST! check if everyone else on the team is also lagging
 			local team = data.team
 			local allyTeam = data.allyTeam
-			local playersInTeam = Spring.GetPlayerList(team, true) --return only active player
+			local playersInTeam = spGetPlayerList(team, true) --return only active player
 			local discontinue = false
 			for j=1,#playersInTeam do
 				local playerID = playersInTeam[j]
@@ -238,11 +249,11 @@ function gadget:GameFrame(n)
 			-- no-one on team not lagging (the likely situation in absence of commshare), continue working
 			if not discontinue then
 				recepientByAllyTeam[allyTeam] = recepientByAllyTeam[allyTeam] or GetRecepient(allyTeam, laggers)
-			
+
 				-- okay, we have someone to give to, prep transfer
 				if recepientByAllyTeam[allyTeam] then
 					if (afkTeams[team] == nil) then -- if team was not an AFK-er (but now is an AFK-er) then process the following... else do nothing for the same AFK-er.
-						--REASON for WHY THE ABOVE^ CHECK was ADDED: if someone sent units to this AFK-er then (typically) var:"laggers[playerID]" will be filled twice for the same player (line 161) & normally unit will be sent (redirected) to the non-AFK-er (line 198), but (unfortunately) equation:"GG.Lagmonitor_activeTeams[allyTeam].count = GG.Lagmonitor_activeTeams[allyTeam].count - 1" will also run twice for the AFK ally (line 193) and it will effect 'unit_mex_overdrive.lua on line 999'. 			
+						--REASON for WHY THE ABOVE^ CHECK was ADDED: if someone sent units to this AFK-er then (typically) var:"laggers[playerID]" will be filled twice for the same player (line 161) & normally unit will be sent (redirected) to the non-AFK-er (line 198), but (unfortunately) equation:"GG.Lagmonitor_activeTeams[allyTeam].count = GG.Lagmonitor_activeTeams[allyTeam].count - 1" will also run twice for the AFK ally (line 193) and it will effect 'unit_mex_overdrive.lua on line 999'.
 						GG.Lagmonitor_activeTeams[allyTeam].count = GG.Lagmonitor_activeTeams[allyTeam].count - 1
 						GG.Lagmonitor_activeTeams[allyTeam][team] = false
 					end
@@ -251,14 +262,13 @@ function gadget:GameFrame(n)
 					if #units > 0 then -- transfer units when number of units in AFK team is > 0
 						-- Transfer Units
 						GG.allowTransfer = true
-						local spTransferUnit = Spring.TransferUnit
 						for j=1,#units do
 							local unit = units[j]
-							if allyTeam == Spring.GetUnitAllyTeam(unit) then
+							if allyTeam == spGetUnitAllyTeam(unit) then
 								-- add this team to the lineage list, then send the unit away
 								if lineage[unit] == nil then
 									lineage[unit] = { team }
-								else 
+								else
 									-- this unit belonged to someone else before me, add me to the end of the list
 									lineage[unit][#lineage[unit]+1] = team
 								end
@@ -266,17 +276,17 @@ function gadget:GameFrame(n)
 							end
 						end
 						GG.allowTransfer = false
-						
+
 						-- Transfer metal to reviever, engine handles excess going to allies if it occurs.
-						local spareMetal = Spring.GetTeamResources(team,"metal") or 0
-						Spring.UseTeamResource(team,"metal",spareMetal)
-						Spring.AddTeamResource(recepientByAllyTeam[allyTeam].team,"metal",spareMetal)
-						
+						local spareMetal = spGetTeamResources(team,"metal") or 0
+						spUseTeamResource(team,"metal",spareMetal)
+						spAddTeamResource(recepientByAllyTeam[allyTeam].team,"metal",spareMetal)
+
 						-- Send message
 						if data.resigned then
-							Spring.Echo(data.name .. " resigned, giving all units to ".. recepientByAllyTeam[allyTeam].name .. " (ally #".. allyTeam ..")")
+							spEcho(data.name .. " resigned, giving all units to ".. recepientByAllyTeam[allyTeam].name .. " (ally #".. allyTeam ..")")
 						else
-							Spring.Echo("Giving all units of "..data.name .. " to " .. recepientByAllyTeam[allyTeam].name .. " due to lag/AFK (ally #".. allyTeam ..")")
+							spEcho("Giving all units of "..data.name .. " to " .. recepientByAllyTeam[allyTeam].name .. " due to lag/AFK (ally #".. allyTeam ..")")
 						end
 					end
 				end	-- if
@@ -284,11 +294,9 @@ function gadget:GameFrame(n)
 		end	-- for
 	end	-- if
 end
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-else-- UNSYNCED ---
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+
+else -- UNSYNCED ---
+
 	function WrapToLuaUI(_,afkPlayer)
 		if (Script.LuaUI('LagmonitorAFK')) then
 			Script.LuaUI.LagmonitorAFK(afkPlayer)
