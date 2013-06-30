b9822d012a3339eeefc8d6ebcabbf8c739ec741a
sprunk <gnurps@o2.pl>
2013-06-30 11:34:25 +0000

    Added (currently disabled) chaining ticks
    svn path=/trunk/mods/zk/; revision=10630

---
+++ LuaRules/Gadgets/unit_chain_ticks.lua
@@ -0,0 +1,21 @@
+function gadget:GetInfo() return {
+	name    = "chaining ticks",
+	desc    = "secret buffs to sprung for being awesome (acuelly is chaining ticks)",
+	author  = "sprung",
+	date    = "2013"
+	license = "PD",
+	layer   = 0,
+	enabled = false
+} end
+
+if (not gadgetHandler:IsSyncedCode()) then return end
+
+local tickDefID = UnitDefNames.armtick.id
+local tickWeaponID = WeaponDefNames.ARMTICK_DEATH.id
+local spDestroyUnit = Spring.DestroyUnit
+
+function gadget:UnitDamaged(unitID, unitDefID, unitTeam, fullDamage, paralyzer, weaponID, attackerID)
+	if((unitDefID == tickDefID) and (weaponID == tickWeaponID)) then
+		spDestroyUnit(unitID, true, attackerID or nil)
+	end
+end
