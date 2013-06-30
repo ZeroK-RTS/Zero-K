4ff508aec015784a5ace644bd748680db41b167c
sprunk <gnurps@o2.pl>
2013-06-30 13:17:16 +0000

    fix
    svn path=/trunk/mods/zk/; revision=10634

--- LuaRules/Gadgets/unit_chain_ticks.lua
+++ LuaRules/Gadgets/unit_chain_ticks.lua
@@ -12,10 +12,28 @@ if (not gadgetHandler:IsSyncedCode()) then return end
 
 local tickDefID = UnitDefNames.armtick.id
 local tickWeaponID = WeaponDefNames.armtick_death.id
+
 local spDestroyUnit = Spring.DestroyUnit
+local spValidUnitID = Spring.ValidUnitID
+
+local ticks_to_pwn = {}
+local table_size = 0
 
 function gadget:UnitDamaged(unitID, unitDefID, unitTeam, fullDamage, paralyzer, weaponID, attackerID)
-	if((unitDefID == tickDefID) and (weaponID == tickWeaponID)) then
-		spDestroyUnit(unitID, true, attackerID or nil)
+	if ((unitDefID == tickDefID) and (weaponID == tickWeaponID)) then
+		table_size = table_size + 1
+		ticks_to_pwn[table_size] = unitID
+	end
+end
+
+function gadget:GameFrame()
+	if (table_size > 0) then
+		for i = 1, table_size do
+			if (spValidUnitID(ticks_to_pwn[i])) -- one tick could get damaged twice 
+				spDestroyUnit(ticks_to_pwn[i], true)
+			end
+			ticks_to_pwn[i] = nil
+		end
+		table_size = 0
 	end
 end
