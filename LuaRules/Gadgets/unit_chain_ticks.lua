051ef9e88793d9da86bcfb56b3afd3db47444905
sprunk <gnurps@o2.pl>
2013-06-30 12:53:30 +0000

    fix
    svn path=/trunk/mods/zk/; revision=10632

--- LuaRules/Gadgets/unit_chain_ticks.lua
+++ LuaRules/Gadgets/unit_chain_ticks.lua
@@ -2,7 +2,7 @@ function gadget:GetInfo() return {
 	name    = "chaining ticks",
 	desc    = "secret buffs to sprung for being awesome (acuelly is chaining ticks)",
 	author  = "sprung",
-	date    = "2013"
+	date    = "2013",
 	license = "PD",
 	layer   = 0,
 	enabled = false
@@ -11,7 +11,7 @@ function gadget:GetInfo() return {
 if (not gadgetHandler:IsSyncedCode()) then return end
 
 local tickDefID = UnitDefNames.armtick.id
-local tickWeaponID = WeaponDefNames.ARMTICK_DEATH.id
+local tickWeaponID = WeaponDefNames.armtick_death.id
 local spDestroyUnit = Spring.DestroyUnit
 
 function gadget:UnitDamaged(unitID, unitDefID, unitTeam, fullDamage, paralyzer, weaponID, attackerID)
