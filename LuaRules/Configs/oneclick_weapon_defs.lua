fecf931846be243004ff5694aaee718ef4ab1860
KingRaptor <kingraptorowns@gmail.com>
2013-07-20 14:58:27 +0000

    Menu icons for Krow and Avenger special functions.
    svn path=/trunk/mods/zk/; revision=10816

--- LuaRules/Configs/oneclick_weapon_defs.lua
+++ LuaRules/Configs/oneclick_weapon_defs.lua
@@ -4,10 +4,10 @@ local oneClickWepDefs = {}
 
 local oneClickWepDefNames = {
 	corcrw = {
-		{ functionToCall = "ClusterBomb", reloadTime = 854, name = "Carpet Bomb", tooltip = "Drop a huge number of bombs in a circle under the Krow", weaponToReload = 3,},
+		{ functionToCall = "ClusterBomb", reloadTime = 854, name = "Carpet Bomb", tooltip = "Drop a huge number of bombs in a circle under the Krow", weaponToReload = 3, texture = "LuaUI/Images/Commands/Bold/bomb.png",},
 	},
 	fighter = {
-		{ functionToCall = "Sprint", reloadTime = 850, name = "Speed Boost", tooltip = "Speed Boost", useSpecialReloadFrame = true, weaponToReload = 3,},
+		{ functionToCall = "Sprint", reloadTime = 850, name = "Speed Boost", tooltip = "Speed boost (5x for 1 second)", useSpecialReloadFrame = true, weaponToReload = 3, texture = "LuaUI/Images/Commands/Bold/sprint.png",},
 	},
 }
 
