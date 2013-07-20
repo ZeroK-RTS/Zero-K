be85a79bbc2378d77a5b80705c760969c7c95aef
KingRaptor <kingraptorowns@gmail.com>
2013-07-20 05:49:10 +0000

    Fix Funnelweb gesture menu entry Fixes issue 1910
    svn path=/trunk/mods/zk/; revision=10796

--- LuaUI/Configs/marking_menu_menu_striderhub.lua
+++ LuaUI/Configs/marking_menu_menu_striderhub.lua
@@ -1,58 +1,58 @@
 local menu = {
 	items = {
-	  {
-		angle = 0,
-		unit = "armcomdgun",
-        label = "Stealth",
-	  },
-	  {
-		angle = -45,
-		unit = "scorpion",
-		label = "Spider",
+		{
+			angle = 0,
+			unit = "armcomdgun",
+			label = "Stealth",
+		},
+		{
+			angle = -45,
+			unit = "scorpion",
+			label = "Spider",
+			items = {
+				{
+					angle = 45,
+					unit = "funnelweb"
+				}
+			},  
+		},
+		{
+			angle = -90,
+			unit = "dante",
+			label = "Light",
+		},
+		{
+			angle = -135,
+			unit = "armraven",
+			label = "Artillery",
+		},
 		{
 			angle = 45,
-			unit = "funnelweb"
-		},  
-	  },
-      {
-		angle = -90,
-		unit = "dante",
-		label = "Light",
-	  },
-      {
-		angle = -135,
-		unit = "armraven",
-		label = "Artillery",
-	  },
-	  {
-		angle = 45,
-		unit = "armbanth",
-		label = "Heavy",
-		items = {	  
-		}
-	  },
-      {
-		angle = 90,
-		unit = "cornukesub",
-		label = "Sub",
-	  },
-	  {
-		angle = 135,
-		unit = "corbats",
-		label = "Ship",
-		items = {
-		  {
-			angle = -135,
-			unit = "armcarry"
-		  },
-		}
-	  },
-      {
-		angle = 180,
-		unit = "armorco",
-		label = "Massive",
-	  },
-  }
+			unit = "armbanth",
+			label = "Heavy",
+		},
+		{
+			angle = 90,
+			unit = "cornukesub",
+			label = "Sub",
+		},
+		{
+			angle = 135,
+			unit = "corbats",
+			label = "Ship",
+			items = {
+				{
+					angle = -135,
+					unit = "armcarry"
+				},
+			}
+		},
+		{
+			angle = 180,
+			unit = "armorco",
+			label = "Massive",
+		},
+	}
 }
 
 return menu
