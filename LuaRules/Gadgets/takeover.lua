6a7821b77c91b1471d983b72e894a9efd34a1d10
TomFyuri <tomfyuri@gmail.com>
2013-07-28 13:20:44 +0000

    Takeover: capture progress label shouldn't freeze at same value graphically anymore (internally gadget works good).
    svn path=/trunk/mods/zk/; revision=11001

--- LuaRules/Gadgets/takeover.lua
+++ LuaRules/Gadgets/takeover.lua
@@ -957,7 +957,7 @@ local function PerformCaptureLoop(unitID, i, data, hp, maxHealth, emp, empHP, ca
 	  data.AllyTeamsProgress[allyteam] = sc/2 -- halves all enemy progress, because unit was captured
 	end
 	data.TeamsProgress = {} -- entire team data is emptied, since it is no more up to date
-	data.siege = false -- siege will reset if enemy units are still present
+-- 	data.siege = false -- siege will reset if enemy units are still present
 	spSetGameRulesParam("takeover_siege_unit"..i, 0)
 --       else
 -- 	Spring.Echo("sum thin wong") -- >_>
