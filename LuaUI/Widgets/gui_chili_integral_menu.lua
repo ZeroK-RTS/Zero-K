ZeroK-RTS/Zero-K

commit 486819b0f4b51994ef2f337286daa341bd1ef547

gui_chili_integral_menu.lua ,gui_chili_keyboardmenu.lua: 1) CommandChanged() won't called twice upon deselect units. 2) SelectionChanged() won't disabled if IntegralMenu or KeyboardMenu is shut down, Issue 1925 (ConfigLayoutHandler() require a function or a boolean to avoid disabling CommandChanged() or SelectionChanged())
ca_layout.lua: same stuff as no.2, except that if shut down it activate engine's menu rather than no menu at all. (ConfigLayoutHandler(true))

camera_cofc.lua: tweak option's text, some r11166 related tweaks

unit_priority.lua: tooltip for Misc Priority, Issue 1940




svn path=/trunk/mods/zk/; revision=11167

LuaUI/Widgets/gui_chili_integral_menu.lua
@@ -1,12 +1,11 @@
--- TODO: commandschanged gets called 2x for some reason, investigate
 -- TODO: proper tooltips for queue buttons
 
 function widget:GetInfo()
   return {
     name      = "Chili Integral Menu",
-    desc      = "v0.361 Integral Command Menu",
+    desc      = "v0.362 Integral Command Menu",
     author    = "Licho, KingRaptor, Google Frog",
-    date      = "12.10.2010",
+    date      = "12.10.2010", --21.August.2013
     license   = "GNU GPL, v2 or later",
     layer     = math.huge-1,
     enabled   = true,
@@ -1034,6 +1033,9 @@ local function LayoutHandler(xIcons, yIcons, cmdCount, commands)
 	end
 
 	Update()
+	if (cmdCount <= 0) then
+		return "", xIcons, yIcons, {}, {}, {}, {}, {}, {}, {}, {} --prevent CommandChanged() from being called twice when deselecting all units  (copied from ca_layout.lua)
+	end
 	return "", xIcons, yIcons, {}, customCmds, {}, {}, {}, {}, reParamsCmds, {[1337]=9001}
 end 
 
@@ -1167,6 +1169,7 @@ local function RemoveAction(cmd, types)
 	return widgetHandler.actionHandler:RemoveAction(widget, cmd, types)
 end
 
+--this function add hotkey text to each tab name
 local function updateTabName(num, choice)
 	local hotkey = WG.crude.GetHotkey(choice.actionName)
 	if hotkey ~= '' then
@@ -1475,8 +1478,8 @@ function widget:SelectionChanged(newSelection)
 end
 
 function widget:Shutdown()
-  widgetHandler:ConfigLayoutHandler(nil)
-  Spring.ForceLayoutUpdate()
+	widgetHandler:ConfigLayoutHandler(false) --true: activate Default menu, false: activate dummy (empty) menu, nil: disable menu & CommandChanged() callins. See Layout.lua
+	Spring.ForceLayoutUpdate()
 end
 
 options.hidetabs.OnChange = function(self) 

LuaUI/Widgets/gui_chili_keyboardmenu.lua
@@ -4,7 +4,7 @@
 function widget:GetInfo()
   return {
     name      = "Chili Keyboard Menu",
-    desc      = "v0.025 Chili Keyboard Menu",
+    desc      = "v0.026 Chili Keyboard Menu",
     author    = "CarRepairer",
     date      = "2012-03-27",
     license   = "GNU GPL, v2 or later",
@@ -375,6 +375,9 @@ local function LayoutHandler(xIcons, yIcons, cmdCount, commands)
 	end
 
 	Update()
+	if (cmdCount <= 0) then
+		return "", xIcons, yIcons, {}, {}, {}, {}, {}, {}, {}, {} --prevent CommandChanged() from being called twice when deselecting all units (copied from ca_layout.lua)
+	end	
 	return "", xIcons, yIcons, {}, customCmds, {}, {}, {}, {}, reParamsCmds, {[1337]=9001}
 end 
 
@@ -1157,7 +1160,7 @@ function widget:Initialize()
 end 
 
 function widget:Shutdown()
-	widgetHandler:ConfigLayoutHandler(nil)
+	widgetHandler:ConfigLayoutHandler(false) --true: activate Default menu, false: activate dummy (empty) menu, nil: disable menu & CommandChanged() callins . See Layout.lua
 	Spring.ForceLayoutUpdate()
 	if not customKeyBind then
 		Spring.SendCommands("unbind d radialbuildmenu")

LuaUI/Widgets/ca_layout.lua
@@ -421,9 +421,9 @@ end
 
 
 function widget:Shutdown()
-  widgetHandler:ConfigLayoutHandler(nil)
-  Spring.ForceLayoutUpdate()
-  widgetHandler.actionHandler:RemoveAction(widget,"layout")
+	widgetHandler:ConfigLayoutHandler(true) --true: activate Default menu, false: activate dummy (empty) menu, nil: disable menu & CommandChanged() callins
+	Spring.ForceLayoutUpdate()
+	widgetHandler.actionHandler:RemoveAction(widget,"layout")
 end
 
 --------------------------------------------------------------------------------

LuaUI/Widgets/camera_cofc.lua
@@ -4,9 +4,9 @@
 function widget:GetInfo()
   return {
     name      = "Combo Overhead/Free Camera (experimental)",
-    desc      = "v0.121 Camera featuring 6 actions. Type \255\90\90\255/luaui cofc help\255\255\255\255 for help.",
+    desc      = "v0.122 Camera featuring 6 actions. Type \255\90\90\255/luaui cofc help\255\255\255\255 for help.",
     author    = "CarRepairer, msafwan",
-    date      = "2011-03-16", --2013-June-30
+    date      = "2011-03-16", --2013-August-21
     license   = "GNU GPL, v2 or later",
     layer     = 1002,
 	handler   = true,
@@ -361,7 +361,7 @@ options = {
     
     thirdpersontrack = {
 		name = "Enter 3rd Person Trackmode",
-		desc = "3rd Person track the selected unit (mouse midclick to exit mode). Press arrow key to jump to nearby units, and move mouse to edge of screen to jump to current unit selection.",
+		desc = "3rd Person track the selected unit (mouse midclick to exit mode). Press arrow key to jump to nearby units, or move mouse to edge of screen to jump to current unit selection (will exit mode if no selection).",
 		type = 'button',
 		hotkey = {key='k', mod='alt+'},
 		path = cameraFollowPath,
@@ -388,7 +388,7 @@ options = {
 		type = 'bool',
 		value = false,
 		path = cameraFollowPath,
-		desc = "Tap the same group numbers to focus camera view toward each units within the same group. This option use \'Receive Indicator\' widget to intelligently cycle focus when appropriate.",
+		desc = "If you tap the group numbers (1,2,3..ect) it will focus camera view toward the cluster of unit rather than toward the average position.",
 		OnChange = function(self) 
 			if self.value==true then
 				Spring.SendCommands("luaui enablewidget Receive Units Indicator")
@@ -1945,7 +1945,7 @@ function GroupRecallFix(key, modifier, isRepeat)
 			previousKey = key
 			previousTime = spGetTimer()
 			
-			if options.enableCycleView.value and WG.recvIndicator then 
+			if options.enableCycleView.value then 
 				local slctUnitUnordered = {}
 				for i=1 , #selectedUnit do
 					local unitID = selectedUnit[i]
@@ -1953,7 +1953,7 @@ function GroupRecallFix(key, modifier, isRepeat)
 					slctUnitUnordered[unitID] = {x,y,z}
 				end
 				selectedUnit = nil
-				local cluster, lonely = WG.recvIndicator.OPTICS_cluster(slctUnitUnordered, 600,2, Spring.GetMyTeamID(),300) --//find clusters with atleast 2 unit per cluster and with at least within 300-elmo from each other with 600-elmo detection range
+				local cluster, lonely = WG.OPTICS_cluster(slctUnitUnordered, 600,2, Spring.GetMyTeamID(),300) --//find clusters with atleast 2 unit per cluster and with at least within 300-elmo from each other with 600-elmo detection range
 				if previousGroup == group then
 					currentIteration = currentIteration +1
 					if currentIteration > (#cluster + #lonely) then

commit 2f8937b4a0e211971fee6242d8160d1250c7ba42

gui_chili_integral_menu.lua & gui_chili_keyboardmenu.lua: -OPS!... fix crash when deselect unit + click custom command (such as: retreat zone), caused by r11167 -reenable engine's menu at shutdown for safety, added option (at menu) to disable this if wanted.
svn path=/trunk/mods/zk/; revision=11177

LuaUI/Widgets/gui_chili_integral_menu.lua
@@ -3,7 +3,7 @@
 function widget:GetInfo()
   return {
     name      = "Chili Integral Menu",
-    desc      = "v0.362 Integral Command Menu",
+    desc      = "v0.363 Integral Command Menu",
     author    = "Licho, KingRaptor, Google Frog",
     date      = "12.10.2010", --21.August.2013
     license   = "GNU GPL, v2 or later",
@@ -47,7 +47,7 @@ NOTE FOR OTHER GAME DEVS:
 ------------------------
 ------------------------
 options_path = 'Settings/HUD Panels/Integral Menu'
-options_order = { 'disablesmartselect', 'hidetabs', 'unitstabhotkey', 'unitshotkeyrequiremeta', 'unitshotkeyaltaswell', 'tab_factory', 'tab_economy', 'tab_defence', 'tab_special' }
+options_order = { 'disablesmartselect', 'hidetabs', 'unitstabhotkey', 'unitshotkeyrequiremeta', 'unitshotkeyaltaswell', 'tab_factory', 'tab_economy', 'tab_defence', 'tab_special','old_menu_at_shutdown'}
 options = {
 	disablesmartselect = {
 		name = 'Disable Smart Tab Select',
@@ -94,6 +94,13 @@ options = {
 		name = "Special Tab",
 		desc = "Switches to special tab, enables grid hotkeys",
 		type = 'button',
+	},
+	old_menu_at_shutdown = {
+		name = 'Reenable Spring Menu at Shutdown',
+		desc = "Upon widget shutdown (manual or upon crash) reenable Spring's original command menu.",
+		type = 'bool',
+		advanced = true,
+		value = true,
 	},
 }
 
@@ -1034,7 +1041,7 @@ local function LayoutHandler(xIcons, yIcons, cmdCount, commands)
 
 	Update()
 	if (cmdCount <= 0) then
-		return "", xIcons, yIcons, {}, {}, {}, {}, {}, {}, {}, {} --prevent CommandChanged() from being called twice when deselecting all units  (copied from ca_layout.lua)
+		return "", xIcons, yIcons, {}, customCmds, {}, {}, {}, {}, reParamsCmds, {} --prevent CommandChanged() from being called twice when deselecting all units  (copied from ca_layout.lua)
 	end
 	return "", xIcons, yIcons, {}, customCmds, {}, {}, {}, {}, reParamsCmds, {[1337]=9001}
 end 
@@ -1478,7 +1485,7 @@ function widget:SelectionChanged(newSelection)
 end
 
 function widget:Shutdown()
-	widgetHandler:ConfigLayoutHandler(false) --true: activate Default menu, false: activate dummy (empty) menu, nil: disable menu & CommandChanged() callins. See Layout.lua
+	widgetHandler:ConfigLayoutHandler(options.old_menu_at_shutdown.value) --true: activate Default menu, false: activate dummy (empty) menu, nil: disable menu & CommandChanged() callins. See Layout.lua
 	Spring.ForceLayoutUpdate()
 end
 

LuaUI/Widgets/gui_chili_keyboardmenu.lua
@@ -4,7 +4,7 @@
 function widget:GetInfo()
   return {
     name      = "Chili Keyboard Menu",
-    desc      = "v0.026 Chili Keyboard Menu",
+    desc      = "v0.027 Chili Keyboard Menu",
     author    = "CarRepairer",
     date      = "2012-03-27",
     license   = "GNU GPL, v2 or later",
@@ -127,6 +127,7 @@ options_order = {
 	'goToCommands',
 	'goToSelections',
 	'opacity',
+	'old_menu_at_shutdown'
 }
 options = {
 
@@ -176,9 +177,13 @@ options = {
 		value = 0.4, min = 0, max = 1, step = 0.01,
 		OnChange = function(self) window_main.color = {1,1,1,self.value}; window_main:Invalidate() end,
 	},
-	
-
-	
+	old_menu_at_shutdown = {
+		name = 'Reenable Spring Menu at Shutdown',
+		desc = "Upon widget shutdown (manual or upon crash) reenable Spring's original command menu.",
+		type = 'bool',
+		advanced = true,
+		value = true,
+	},		
 }
 
 
@@ -376,7 +381,7 @@ local function LayoutHandler(xIcons, yIcons, cmdCount, commands)
 
 	Update()
 	if (cmdCount <= 0) then
-		return "", xIcons, yIcons, {}, {}, {}, {}, {}, {}, {}, {} --prevent CommandChanged() from being called twice when deselecting all units (copied from ca_layout.lua)
+		return "", xIcons, yIcons, {}, customCmds, {}, {}, {}, {}, reParamsCmds, {} --prevent CommandChanged() from being called twice when deselecting all units (copied from ca_layout.lua)
 	end	
 	return "", xIcons, yIcons, {}, customCmds, {}, {}, {}, {}, reParamsCmds, {[1337]=9001}
 end 
@@ -1160,7 +1165,7 @@ function widget:Initialize()
 end 
 
 function widget:Shutdown()
-	widgetHandler:ConfigLayoutHandler(false) --true: activate Default menu, false: activate dummy (empty) menu, nil: disable menu & CommandChanged() callins . See Layout.lua
+	widgetHandler:ConfigLayoutHandler(options.old_menu_at_shutdown.value) --true: activate Default menu, false: activate dummy (empty) menu, nil: disable menu & CommandChanged() callins . See Layout.lua
 	Spring.ForceLayoutUpdate()
 	if not customKeyBind then
 		Spring.SendCommands("unbind d radialbuildmenu")