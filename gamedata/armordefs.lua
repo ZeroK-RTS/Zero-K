262a66194d65795c1bb7f6d038c559e297b5903b
GoogleFrog <googl3frog@gmail.com>
2013-08-26 14:34:07 +0000

    armordefs for 95.0
    svn path=/trunk/mods/zk/; revision=11241

--- gamedata/armordefs.lua
+++ gamedata/armordefs.lua
@@ -160,20 +160,6 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
--- convert to named maps  (does anyone know what 99 is for?  :)
-
-for categoryName, categoryTable in pairs(armorDefs) do
-  local t = {}
-  for _, unitName in pairs(categoryTable) do
-    t[unitName] = 99
-  end
-  armorDefs[categoryName] = t
-end
-
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
 local system = VFS.Include('gamedata/system.lua')
 
 return system.lowerkeys(armorDefs)
