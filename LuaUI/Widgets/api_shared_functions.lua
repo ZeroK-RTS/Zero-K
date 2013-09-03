ebc06d816d6c3b788a63aea41dd7ca8a4bafa61f
KingRaptor <kingraptorowns@gmail.com>
2013-09-03 11:14:47 +0000

    Fix Python dict writing in api_shared_functions
    svn path=/trunk/mods/zk/; revision=11347

--- LuaUI/Widgets/api_shared_functions.lua
+++ LuaUI/Widgets/api_shared_functions.lua
@@ -181,13 +181,15 @@ local function WritePythonDict(dict, dictName, params)
 	local endLine = comma .. "\n"
 	local separator = params.raw and " = " or  " : "
 	local str = ""
-	if not params.raw then
-	      str = dictName .. " = "	--WriteIndents(numIndents)
-	      str = str .. "{\n"
+	if (not params.raw) then
+		if params.endOfFile then
+			str = dictName .. " = "	--WriteIndents(numIndents)
+		end
+		str = str .. "{\n"
 	end
 	for i,v in pairs(dict) do
 		if not params.raw then
-			str = str .. WriteIndents(numIndents + 1)
+			str = str .. WriteIndents(params.numIndents + 1)
 		end
 		if (type(i) == "string") and not params.raw then
 			str = str .. string.format("%q", i) .. separator
@@ -199,7 +201,7 @@ local function WritePythonDict(dict, dictName, params)
 			local arg = {numIndents =  params.numIndents + 1, endOfFile = false}
 			str = str .. WritePythonDict(v, nil, arg)
 		elseif type(v) == "boolean" then
-			str = str .. v and "True" or "False" .. endLine
+			str = str .. ((v and "True") or "False") .. endLine
 		elseif type(v) == "string" then
 			str = str .. string.format("%q", v) .. endLine
 		else
