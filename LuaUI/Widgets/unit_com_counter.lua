5e68dcaff0125e217922c51f922a97374f6da2f4
m_ritoniemi <m_ritoniemi@windowslive.com>
2013-10-29 10:46:46 +0000

    Turned the CommCounter widget off by default, since ZK doesn't use comm ends by default, and since the widget spams chat when people morph their commanders.
    svn path=/trunk/mods/zk/; revision=11849

--- LuaUI/Widgets/unit_com_counter.lua
+++ LuaUI/Widgets/unit_com_counter.lua
@@ -9,7 +9,7 @@ function widget:GetInfo()
     date      = "Apr 28, 2008",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
