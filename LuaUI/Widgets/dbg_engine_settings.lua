78513d86fedac89a289b1f303514d6099c9dceb0
KingRaptor <kingraptorowns@gmail.com>
2013-11-11 06:01:44 +0000

    Log flush widget renamed and extended.
    svn path=/trunk/mods/zk/; revision=12028

--- LuaUI/Widgets/dbg_engine_settings.lua
+++ LuaUI/Widgets/dbg_engine_settings.lua
@@ -3,10 +3,10 @@
 
 function widget:GetInfo()
   return {
-    name      = "LogFlush Disabler",
-    desc      = "Disables log flushing for Spring 94.1",
+    name      = "Engine Setting Fix",
+    desc      = "Fixes engine settings as appropriate for engine",
     author    = "KingRaptor",
-    date      = "20130414",
+    date      = "2013.04.14",
     license   = "Public Domain",
     layer     = 0,
     enabled   = true --  loaded by default?
@@ -16,10 +16,19 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+local tags = {
+  LogFlush = 0,
+  AllowDeferredMapRendering = 0,
+  AllowDeferredModelRendering = 0,
+  --WorkerThreadCount = 0,
+}
+
 function widget:Initialize()
-  if not (Game.version:find('91')) then
-    Spring.SetConfigInt("LogFlush", 0)
-    Spring.Log(widget:GetInfo().name, LOG.WARNING, "Spring 94.1 or 95.0 detected. Disabling LogFlush.")
+  if not (Game.version:find('91.')) then
+    for tag, value in pairs(tags) do
+      Spring.SetConfigInt(tag, value)
+    end
+    Spring.Log(widget:GetInfo().name, LOG.WARNING, "Spring >91.0 detected. Setting config tags...")
     --Spring.SendCommands("luaui disablewidget " .. widget:GetInfo().name)
     widgetHandler:RemoveWidget()
   end
