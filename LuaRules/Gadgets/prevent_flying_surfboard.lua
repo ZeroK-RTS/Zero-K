e2bbfdfe1016f4342af89fcab9d0e88066fdb885
Rafal <rafal_karczm@o2.pl>
2013-11-17 16:43:20 +0000

    prevent_flying_surfboard.lua: Added support of CMD.INSERT to Sprung's failgadget.
    svn path=/trunk/mods/zk/; revision=12106

--- LuaRules/Gadgets/prevent_flying_surfboard.lua
+++ LuaRules/Gadgets/prevent_flying_surfboard.lua
@@ -13,10 +13,17 @@ if (not gadgetHandler:IsSyncedCode()) then return end
 local surfboardDefID = UnitDefNames["armtboat"].id
 
 function gadget:AllowCommand (unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
+	local numParams = #cmdParams
+    if (cmdID == CMD.INSERT and cmdParams[2]) then
+		cmdID = cmdParams[2]
+		cmdParams[1] = cmdParams[4] -- this only supports commands with 1 param
+		numParams = numParams - 3
+    end
+
 	if ((cmdID == CMD.LOAD_ONTO) and (unitDefID == surfboardDefID)) then -- surfboard can't ask to embark
 		return false
 	elseif ((cmdID == CMD.LOAD_UNITS)
-	and (#cmdParams == 1) -- 1 for load specific unit; 3 for area load
+	and (numParams == 1) -- 1 for load specific unit; 3 for area load
 	and Spring.ValidUnitID(cmdParams[1])
 	and Spring.GetUnitDefID(cmdParams[1]) == surfboardDefID) then
 		return false
