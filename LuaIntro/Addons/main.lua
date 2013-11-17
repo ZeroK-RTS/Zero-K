8b433331b854becdcd64a76ee304c16297ee1c6a
KingRaptor <kingraptorowns@gmail.com>
2013-11-17 07:07:33 +0000

    Testing new progress bar that parses the message strings - jerkier, but generally more accurate.
    svn path=/trunk/mods/zk/; revision=12095

--- LuaIntro/Addons/main.lua
+++ LuaIntro/Addons/main.lua
@@ -15,9 +15,41 @@ end
 ------------------------------------------
 
 local lastLoadMessage = ""
+local lastProgress = 0
+
+local progressByLastLine = {
+	["Parsing Map Information"] = 0,
+	["Loading SMF"] = 5,
+	["Loading Radar Icons"] = 10,
+	["Loading GameData Definitions"] = 15,
+	["Loading Sound Definitions"] = 20,
+	["Creating Smooth Height Mesh"] = 22,
+	["Creating QuadField & CEGs"] = 25,
+	["Creating Unit Textures"] = 30,
+	["Creating Sky"] = 35,
+	["Loading Weapon Definitions"] = 40,
+	["Loading Unit Definitions"] = 45,
+	["Loading Feature Definitions"] = 50,
+	["PathCosts: writing"] = 55,
+	["Initializing Map Features"] = 60,
+	["Creating ShadowHandler & DecalHandler"] = 65,
+	["Loading Map Tiles"] = 68,
+	["Loading Square Textures"] = 70,
+	["Creating TreeDrawer"] = 72,
+	["Creating ProjectileDrawer & UnitDrawer"] = 75,
+	["Creating Projectile Textures"] = 78,
+	["Loading LuaRules"] = 80,
+	["Loading LuaUI"] = 90,
+	["Initializing PathCache"] = 95,
+	["Finalizing"] = 100
+}
+for name,val in pairs(progressByLastLine) do
+	progressByLastLine[name] = val*0.01
+end
 
 function addon.LoadProgress(message, replaceLastLine)
 	lastLoadMessage = message
+	lastProgress = progressByLastLine[message] or lastProgress
 end
 
 ------------------------------------------
@@ -25,7 +57,7 @@ end
 local font = gl.LoadFont("FreeSansBold.otf", 50, 20, 1.95)
 
 function addon.DrawLoadScreen()
-	local loadProgress = SG.GetLoadProgress()
+	local loadProgress = lastProgress or SG.GetLoadProgress()
 
 	local vsx, vsy = gl.GetViewSizes()
 
@@ -159,15 +191,14 @@ function addon.DrawLoadScreen()
 	gl.PushMatrix()
 	gl.Scale(1/vsx,1/vsy,1)
 	local barTextSize = vsy * (0.05 - 0.015)
-	local barTextSizeSmall = math.ceil(barTextSize * 0.5)
 
 	--font:Print(lastLoadMessage, vsx * 0.5, vsy * 0.3, 50, "sc")
 	--font:Print(Game.gameName, vsx * 0.5, vsy * 0.95, vsy * 0.07, "sca")
-	font:Print(lastLoadMessage, vsx * 0.2, vsy * 0.14, barTextSizeSmall, "sa")
+	font:Print(lastLoadMessage, vsx * 0.2, vsy * 0.14, barTextSize*0.5, "sa")
 	if loadProgress>0 then
-		font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * 0.165, barTextSize, "oc")
+		--font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * 0.165, barTextSize, "oc")
 	else
-		font:Print("???", vsx * 0.5, vsy * 0.165, barTextSize, "oc")
+		--font:Print("Loading...", vsx * 0.5, vsy * 0.165, barTextSize, "oc")
 	end
 	gl.PopMatrix()
 end
