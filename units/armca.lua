ZeroK-RTS/Zero-K

commit 6f42b285f8e4b2006d9d438047f21227c1d06233

Made some changes that should fix the Crane/Athena "dance" bug. Not Mantising because the chances of report being closed without resolution are too high for me to want to bother.
svn path=/trunk/mods/zk/; revision=12091

units/armca.lua
@@ -2,13 +2,13 @@ unitDef = {
   unitname            = [[armca]],
   name                = [[Crane]],
   description         = [[Construction Aircraft, Builds at 4 m/s]],
-  acceleration        = 0.35,
+  acceleration        = 0.1,
   airStrafe           = 0,
   amphibious          = true,
-  brakeRate           = 3,
+  brakeRate           = 4.5,
   buildCostEnergy     = 220,
   buildCostMetal      = 220,
-  buildDistance       = 40,
+  buildDistance       = 160,
   builder             = true,
 
   buildoptions        = {
@@ -45,7 +45,7 @@ unitDef = {
     helptext_es    = [[El Crane vuela rápidamente sobre qualquier terreno, pero es frágil a qualquier AA. Aunque tenga relativamente poco nano comparado con otros constructores, pude construir en muchos lugares inaccesibles y puede ampliar el territorio de un jugador en una manera no linear. Gracias a su mobilidad, es ideal para reclamar pecios.]],
     helptext_fr    = [[Le Crane vole rapidement au dessus de tous les obstacles. Tr?s vuln?rable ? la d?fense a?rienne, il est id?al pour construire dans des endroits tres difficile d'acces ou pour r?cup?rer le m?tal des carcasses sur le champ de bataille.]],
     helptext_it    = [[Il Crane vola velocemnte su qualunque terreno, ma ? fragile a qualunque AA. Anche se ha relativamente poco nano rispetto ad altri costruttori, riesce a costruire un posti inaccessibili e pu? espandere il territorio di un giocatore un una maniera non lineare. Grazie alla sua mobilit? ? ideale pere reclamare relitti.]],
-    helptext_pl    = [[Crane może przelatywać z dość dużą prędkością nad dowolnym terenem. Choć w porównaniu do pozostałych konstruktorów ma niską moc, jego wysoka ruchliwość pozwala mu budować w trudno dostępnych miejscach, szybko przeczesywać pole bitwy w poszukiwaniu złomu i wykonywać inne wymagające mobilności czynności.]],
+    helptext_pl    = [[Crane może przelatywa�?z doś�?duż�?prędkości�?nad dowolnym terenem. Cho�?w porównaniu do pozostałych konstruktorów ma nisk�?moc, jego wysoka ruchliwoś�?pozwala mu budowa�?w trudno dostępnych miejscach, szybko przeczesywa�?pole bitwy w poszukiwaniu złomu i wykonywa�?inne wymagaj�?e mobilności czynności.]],
 	modelradius    = [[10]],
 	midposoffset   = [[0 4 0]],
   },

units/armcsa.lua
@@ -2,14 +2,14 @@ unitDef = {
   unitname            = [[armcsa]],
   name                = [[Athena]],
   description         = [[Airborne SpecOps Engineer, Builds at 7.5 m/s]],
-  acceleration        = 0.35,
+  acceleration        = 0.1,
   airStrafe           = 0,
   amphibious          = true,
   autoHeal            = 20,
-  brakeRate           = 3,
+  brakeRate           = 4.5,
   buildCostEnergy     = 500,
   buildCostMetal      = 500,
-  buildDistance       = 60,
+  buildDistance       = 180,
   builder             = true,
 
   buildoptions        = {
@@ -59,13 +59,13 @@ unitDef = {
 
   customParams        = {
     airstrafecontrol = [[1]],
-    description_pl = [[Latający Konstruktor do Zadań Specjalnych, moc 7.5 m/s]],
+    description_pl = [[Lataj�?y Konstruktor do Zada�?Specjalnych, moc 7.5 m/s]],
     description_fr = [[ADAV de Construcion Furtif Camouflable, Construit r 7.5 m/s]],
     description_de = [[Fliegender SpecOps Bauleiter, Baut mit 7.5 m/s]],
     helptext       = [[The Athena is the pinnacle of stealth strike capability. Equipped with a cloaking device and a radar jammer, it can slip through enemy lines to assemble squads of raiders, inflicting havoc on the opposition's logistics. The Athena can also resurrect wrecks on the battlefield while cloaked, adding them to your army.]],
     helptext_de    = [[Athene ist die Spitze der Tarnungsschlagkraft. Ausgerüstet mit einem Verhüllungsgeräte und einem Radarstörer kann es durch gegnerische Verteidigungslinien fliegen und Truppen aus Angreifern zusammenstellen, die dann Verwüstung nachsichziehen werden und die gegnerische Logistik vernichten können.]],
     helptext_fr    = [[Le Athena est un ingénieur de combat non armé. Équipé d'un brouilleur radar et d'un camouflage optique il peut construire certaines infrastructures et des unités nimporte ou, et ainsi surprendre l'enneme.]],
-    helptext_pl    = [[Athena to technologiczne cacko pod względem kamuflażu i precyzyjnych operacji. Maskowanie i zakłócacz radaru pozwalają Athenie prześlizgnąć się niezauważenie przez linię obrony przeciwnika, gdzie może śledzić jego ruchy i budować jednostki bezpośrednio na polu walki.]],
+    helptext_pl    = [[Athena to technologiczne cacko pod względem kamuflażu i precyzyjnych operacji. Maskowanie i zakłócacz radaru pozwalaj�?Athenie prześlizgn�??si�?niezauważenie przez lini�?obrony przeciwnika, gdzie może śledzi�?jego ruchy i budowa�?jednostki bezpośrednio na polu walki.]],
     modelradius    = [[15]],
   },
 

commit 76a04b08e1d94b7b5c0b39b66278a2056231a8eb

Fix Polish helptexts broken in r12091, part 2
svn path=/trunk/mods/zk/; revision=12097

units/armca.lua
@@ -33,19 +33,19 @@ unitDef = {
 
   customParams        = {
     airstrafecontrol = [[0]],
-    description_bp = [[Aeronave de construç?o, constrói a 4 m/s]],
+    description_bp = [[Aeronave de construÃ§?o, constrÃ³i a 4 m/s]],
 	description_de = [[Konstruktionsflugzeug, Baut mit 4 m/s]],
-    description_es = [[Avión de construcción, construye a 4 m/s]],
+    description_es = [[AviÃ³n de construcciÃ³n, construye a 4 m/s]],
     description_fr = [[Avion de Construction, Construit ? 4 m/s]],
     description_it = [[Aereo da costruzzione, costruisce a 4 m/s]],
     description_pl = [[Samolot Konstruktor, moc 4 m/s]],
     helptext       = [[The Crane flies quickly over any terrain, but is fragile to any AA. Though it has relatively poor nano power compared to other constructors, it is able to build in many hard to reach places and expand an air players territory in a nonlinear fashion. Due to its mobility, it is ideal for reclaiming wrecks and other repetetitive tasks.]],
-    helptext_bp    = [[Crane voa rapidamente sobre qualquer terreno mas é derrubada facilmente por qualquer fogo anti-aéreo. Embora seu poder de construç?o seja ligeiramente mais baixo que o damaioria dos contrutores, sua agilidade e capacidade de voar sobre obstáculos permite a um jogador aéreo expandir seu território de forma n?o-linear. Sua mobilidade alta tamb?m a torna eficiente em reclamar destroços e outras atividades nas quais construtores terrestres gastariam mais tempo viajando que trabalhando.]],
-	helptext_de    = [[Der Crane bewegt sich flink über das Terrain, ist aber auch anfällig gegenüber AA. Obwohl er ziemlich wenig Baukraft hat, ist er in der Lage auch auf außergewöhnlichen Plätzen zu bauen. Durch seine Mobilität ist er ideal dafür geschaffen, Wracks zu absorbieren und daraus das Metall zu gewinnen.]],
-    helptext_es    = [[El Crane vuela rápidamente sobre qualquier terreno, pero es frágil a qualquier AA. Aunque tenga relativamente poco nano comparado con otros constructores, pude construir en muchos lugares inaccesibles y puede ampliar el territorio de un jugador en una manera no linear. Gracias a su mobilidad, es ideal para reclamar pecios.]],
+    helptext_bp    = [[Crane voa rapidamente sobre qualquer terreno mas Ã© derrubada facilmente por qualquer fogo anti-aÃ©reo. Embora seu poder de construÃ§?o seja ligeiramente mais baixo que o damaioria dos contrutores, sua agilidade e capacidade de voar sobre obstÃ¡culos permite a um jogador aÃ©reo expandir seu territÃ³rio de forma n?o-linear. Sua mobilidade alta tamb?m a torna eficiente em reclamar destroÃ§os e outras atividades nas quais construtores terrestres gastariam mais tempo viajando que trabalhando.]],
+	helptext_de    = [[Der Crane bewegt sich flink Ã¼ber das Terrain, ist aber auch anfÃ¤llig gegenÃ¼ber AA. Obwohl er ziemlich wenig Baukraft hat, ist er in der Lage auch auf auÃergewÃ¶hnlichen PlÃ¤tzen zu bauen. Durch seine MobilitÃ¤t ist er ideal dafÃ¼r geschaffen, Wracks zu absorbieren und daraus das Metall zu gewinnen.]],
+    helptext_es    = [[El Crane vuela rÃ¡pidamente sobre qualquier terreno, pero es frÃ¡gil a qualquier AA. Aunque tenga relativamente poco nano comparado con otros constructores, pude construir en muchos lugares inaccesibles y puede ampliar el territorio de un jugador en una manera no linear. Gracias a su mobilidad, es ideal para reclamar pecios.]],
     helptext_fr    = [[Le Crane vole rapidement au dessus de tous les obstacles. Tr?s vuln?rable ? la d?fense a?rienne, il est id?al pour construire dans des endroits tres difficile d'acces ou pour r?cup?rer le m?tal des carcasses sur le champ de bataille.]],
     helptext_it    = [[Il Crane vola velocemnte su qualunque terreno, ma ? fragile a qualunque AA. Anche se ha relativamente poco nano rispetto ad altri costruttori, riesce a costruire un posti inaccessibili e pu? espandere il territorio di un giocatore un una maniera non lineare. Grazie alla sua mobilit? ? ideale pere reclamare relitti.]],
-    helptext_pl    = [[Crane może przelatywa�?z doś�?duż�?prędkości�?nad dowolnym terenem. Cho�?w porównaniu do pozostałych konstruktorów ma nisk�?moc, jego wysoka ruchliwoś�?pozwala mu budowa�?w trudno dostępnych miejscach, szybko przeczesywa�?pole bitwy w poszukiwaniu złomu i wykonywa�?inne wymagaj�?e mobilności czynności.]],
+    helptext_pl    = [[Crane może przelatywać z dość dużą prędkością nad dowolnym terenem. Choć w porównaniu do pozostałych konstruktorów ma niską moc, jego wysoka ruchliwość pozwala mu budować w trudno dostępnych miejscach, szybko przeczesywać pole bitwy w poszukiwaniu złomu i wykonywać inne wymagające mobilności czynności.]],
 	modelradius    = [[10]],
 	midposoffset   = [[0 4 0]],
   },