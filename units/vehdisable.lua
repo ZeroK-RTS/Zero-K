1efb6f535d1415352a8cbbfca75b11dd51369ec5
KingRaptor <kingraptorowns@gmail.com>
2013-11-28 15:25:49 +0000

    vehdisable, disable form of Dominatrix. Does slow, disarm and paralyze damage in one convenient package.
    svn path=/trunk/mods/zk/; revision=12168

---
+++ units/vehdisable.lua
@@ -0,0 +1,178 @@
+unitDef = {
+  unitname            = [[vehdisable]],
+  name                = [[Dominatrix]],
+  description         = [[Electronic Warfare Vehicle]],
+  acceleration        = 0.0444,
+  brakeRate           = 0.0385,
+  buildCostEnergy     = 420,
+  buildCostMetal      = 420,
+  builder             = false,
+  buildPic            = [[capturecar.png]],
+  buildTime           = 420,
+  canAttack           = false,
+  canGuard            = true,
+  canMove             = true,
+  canPatrol           = true,
+  category            = [[LAND]],
+  collisionVolumeOffsets = [[0 0 0]],
+  collisionVolumeScales  = [[26 26 50]],
+  collisionVolumeTest    = 1,
+  collisionVolumeType    = [[cylZ]],
+  corpse              = [[DEAD]],
+
+  customParams        = {
+    --description_de = [[Kaperungsfahrzeug]],
+    helptext       = [[The Dominatrix progressively disables enemies with its electronic warfare attack, starting by slowing their movement and attack, then disarming them and finally paralyzing them outright.]],
+	--helptext_de    = [[Der Dominatrix erobert Einheiten, hetzt sie gegen die ehemaligen Verbündeten auf. Mehrere Dominatrixe können ein Ziel schneller erobern, obwohl nur einer von ihnen der Kontrolleur dieser Einheit wird. Nachdem eine gegnerische Einheit unter Kontrolle gebracht wurde, kann der Dominatrix für fünf Sekunden nicht schießen. Sobald ein Dominatrix zerstört wurde, sind alle Einheiten unter seiner Kontrolle wieder frei.]],
+	modelradius    = [[13]],
+  },
+
+  explodeAs           = [[BIG_UNITEX]],
+  footprintX          = 3,
+  footprintZ          = 3,
+  iconType            = [[vehiclespecial]],
+  idleAutoHeal        = 5,
+  idleTime            = 1800,
+  leaveTracks         = true,
+  mass                = 205,
+  maxDamage           = 1120,
+  maxSlope            = 18,
+  maxVelocity         = 2.2,
+  maxWaterDepth       = 22,
+  minCloakDistance    = 75,
+  movementClass       = [[TANK3]],
+  moveState           = 0,
+  noAutoFire          = false,
+  noChaseCategory     = [[TERRAFORM SATELLITE FIXEDWING GUNSHIP]],
+  objectName          = [[corvrad_big.s3o]],
+  radarDistance       = 1200,
+  script              = [[vehdisable.lua]],
+  seismicSignature    = 4,
+  selfDestructAs      = [[BIG_UNITEX]],
+  side                = [[CORE]],
+  sightDistance       = 550,
+  trackOffset         = -7,
+  trackStrength       = 5,
+  trackStretch        = 1,
+  trackType           = [[StdTank]],
+  trackWidth          = 26,
+  turninplace         = 0,
+  turnRate            = 420,
+  workerTime          = 0,
+
+  weapons             = {
+
+    {
+      def                = [[DISABLERAY]],
+      badTargetCategory  = [[UNARMED FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+
+  },
+
+
+  weaponDefs          = {
+
+    DISABLERAY = {
+      name                    = [[Disable Ray]],
+      beamdecay               = 0.9,
+      beamlaser               = 1,
+      beamTime                = 0.1,
+      beamttl                 = 1,
+      coreThickness           = 0,
+      craterBoost             = 0,
+      craterMult              = 0,
+      
+      customParams        = {
+	disarmDamageMult = 4,
+	disarmDamageOnly = 0,
+	disarmTimer      = 8, -- seconds
+      },
+
+      damage                  = {
+        default = 40,
+        planes  = 40,
+        subs    = 40,
+      },
+
+      explosionGenerator      = [[custom:NONE]],
+      fireStarter             = 30,
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 0,
+      largeBeamLaser          = true,
+      laserFlareSize          = 0,
+      minIntensity            = 1,
+      paralyzer               = true,
+      paralyzeTime            = 5,
+      range                   = 520,
+      reloadtime              = 0.1,
+      rgbColor                = [[0 0.2 0.8]],
+      scrollSpeed             = 2,
+      soundStart              = [[weapon/laser/pulse_laser2]],
+      soundStartVolume        = 0.5,
+      soundTrigger            = true,
+      sweepfire               = false,
+      targetMoveError         = 0,
+      texture1                = [[dosray]],
+      texture2                = [[flare]],
+      texture3                = [[flare]],
+      texture4                = [[smallflare]],
+      thickness               = 4.2,
+      tolerance               = 5000,
+      turret                  = true,
+      weaponType              = [[BeamLaser]],
+      weaponVelocity          = 500,
+    },
+
+  },
+
+
+  featureDefs         = {
+
+    DEAD = {
+      description      = [[Wreckage - Dominatrix]],
+      blocking         = true,
+      category         = [[corpses]],
+      damage           = 860,
+      energy           = 0,
+      featureDead      = [[HEAP]],
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      height           = [[20]],
+      hitdensity       = [[100]],
+      metal            = 168,
+      object           = [[corvrad_big_dead.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 168,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+
+    HEAP = {
+      description      = [[Debris - Dominatrix]],
+      blocking         = false,
+      category         = [[heaps]],
+      damage           = 860,
+      energy           = 0,
+      featurereclamate = [[SMUDGE01]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      height           = [[4]],
+      hitdensity       = [[100]],
+      metal            = 84,
+      object           = [[debris2x2c.s3o]],
+      reclaimable      = true,
+      reclaimTime      = 84,
+      seqnamereclamate = [[TREE1RECLAMATE]],
+      world            = [[All Worlds]],
+    },
+
+  },
+
+}
+
+return lowerkeys({ vehdisable = unitDef })
