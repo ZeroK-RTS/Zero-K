02019e6a02ed65f5ee5f3e5379d09e5631f0e0d4
GoogleFrog <googl3frog@gmail.com>
2013-12-23 14:31:20 +0000

    Oops.
    svn path=/trunk/mods/zk/; revision=12305

---
+++ LuaRules/Utilities/unitTypeChecker.lua
@@ -0,0 +1,25 @@
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+
+local isNewEngine = not (Game.version:find('91.0') and (Game.version:find('91.0.1') == nil))
+
+function Spring.Utilities.getMovetype(ud)
+	if ud.canFly or ud.isAirUnit then
+		if isNewEngine then
+			if ud.isHoveringAirUnit then
+				return 1 -- gunship
+			else
+				return 0 -- fixedwing
+			end
+		else
+			if (ud.isFighter or ud.isBomber) then
+				return 0 -- fixedwing
+			else
+				return 1 -- gunship
+			end
+		end
+	elseif not (ud.isBuilding or ud.isFactory or ud.speed == 0) then
+		return 2 -- ground/sea
+	end
+	return false -- For structures or any other invalid movetype
+end
