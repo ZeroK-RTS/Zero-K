9953955d116b884833b994f49da7aca783d39c65
sprunk <gnurps@o2.pl>
2013-12-27 12:06:24 +0000

    Freaker: descriptions and income changed to match the new buildpower; helptext mentions the slowbeam.
    svn path=/trunk/mods/zk/; revision=12313

--- units/corfast.lua
+++ units/corfast.lua
@@ -1,7 +1,7 @@
 unitDef = {
   unitname            = [[corfast]],
   name                = [[Freaker]],
-  description         = [[Jumpjet Constructor, Builds at 7.5 m/s]],
+  description         = [[Jumpjet Constructor, Builds at 5 m/s]],
   acceleration        = 0.26,
   brakeRate           = 0.26,
   buildCostEnergy     = 160,
@@ -23,22 +23,22 @@ unitDef = {
 
   customParams        = {
     canjump        = [[1]],
-    description_bp = [[Construtor saltador, produz a 7.5 m/s]],
-    description_es = [[Constructor jumpjet, construye a 7.5 m/s]],
-    description_fr = [[Constructeur r Jetpack, Construit r 7.5 m/s]],
-    description_it = [[Costruttore jumpjet, costruisce a 7.5 m/s]],
-	description_de = [[Konstruktionsjumpjet, Baut mit 7.5 M/s]],
-	description_pl = [[Skaczacy konstruktor, moc 7.5 m/s]],
-    helptext       = [[Fast and capable of jumping over short distances or heights, the Freaker is the ideal constructor for rapid expansion.]],
+    description_bp = [[Construtor saltador, produz a 5 m/s]],
+    description_es = [[Constructor jumpjet, construye a 5 m/s]],
+    description_fr = [[Constructeur r Jetpack, Construit r 5 m/s]],
+    description_it = [[Costruttore jumpjet, costruisce a 5 m/s]],
+	description_de = [[Konstruktionsjumpjet, Baut mit 5 M/s]],
+	description_pl = [[Skaczacy konstruktor, moc 5 m/s]],
+    helptext       = [[Fast and capable of jumping over short distances or heights, the Freaker is the ideal constructor for rapid expansion. Armed with a light slowbeam, it can also provide combat support.]],
     helptext_bp    = [[Rápido e capaz de saltar por sobre distâncias curtas ou pequenos obstáculos, Freaker é ideal para expans?o rápida.]],
     helptext_es    = [[Rápido y capaz de brincar sobre cortas distancias o alturas, el Freaker es el constructor ideal para la expansión rápida]],
     helptext_fr    = [[R la fois rapide et capable de sauter sur de courtes distances grâce r son jetpack, le Freaker est un superbe outil pour favoriser son expansion.]],
     helptext_it    = [[Veloce e capace di saltare per corte distanze o altezze, il Freaker é il costruttore ideale per l'espanzione rapida]],
 	helptext_de    = [[Schnell und mit der Möglichkeit ausgestattet über kurze Distanzen oder Höhen zu springen, eignet sich der Freaker als ideale Konstruktionseinheit für rasche Expansion.]],
-	helptext_pl    = [[Predki i majacy mozliwosc skoku, Freaker to idealny konstruktor do szybkiej rozbudowy.]],
+	helptext_pl    = [[Predki i majacy mozliwosc skoku, Freaker to idealny konstruktor do szybkiej rozbudowy. Posiada takze lekki promien spowalniajacy, dzieki ktoremu zapewnia wsparcie w walce.]],
   },
 
-  energyMake          = 0.225,
+  energyMake          = 0.15,
   energyUse           = 0,
   explodeAs           = [[BIG_UNITEX]],
   footprintX          = 2,
@@ -53,13 +53,13 @@ unitDef = {
   maxSlope            = 36,
   maxVelocity         = 2.1,
   maxWaterDepth       = 22,
-  metalMake           = 0.225,
+  metalMake           = 0.15,
   minCloakDistance    = 75,
   movementClass       = [[KBOT2]],
   noAutoFire          = false,
   noChaseCategory     = [[TERRAFORM SATELLITE FIXEDWING GUNSHIP HOVER SHIP SWIM SUB LAND FLOAT SINK TURRET]],
   objectName          = [[behe_coroner.s3o]],
-  script               = [[corfast.lua]],
+  script              = [[corfast.lua]],
   seismicSignature    = 4,
   selfDestructAs      = [[BIG_UNITEX]],
 
@@ -96,7 +96,6 @@ unitDef = {
     },
 
   },
-
 
   weaponDefs          = {
 
