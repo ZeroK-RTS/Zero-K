fb363f88a50f201f695ea14ae3e38d03b4e87f00
xponen <msafwan86msafwan86_nnss@hotmail.com>
2013-12-30 15:14:31 +0000

    weapondefs_post.lua: no longer need workaround for StarburstMissile, yay!
    svn path=/trunk/mods/zk/; revision=12345

--- gamedata/weapondefs_post.lua
+++ gamedata/weapondefs_post.lua
@@ -185,22 +185,6 @@ end
 	end
  end
  
- --------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---
--- Workaround for http://springrts.com/mantis/view.php?id=4177
---
-if not reverseCompat then
-	for _, weaponDef in pairs(WeaponDefs) do
-		if weaponDef.weapontype == "StarburstLauncher" then
-			if weaponDef.weaponvelocity > 1500 then
-				weaponDef.weaponvelocity = 800
-			end
-			weaponDef.startvelocity = weaponDef.weaponvelocity
-		end
-	end
-end
- 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 --
