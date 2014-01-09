f29200bacb22654f007fb7da4bd34dc51062a713
Anarchid <anarchid@mail.ru>
2014-01-09 20:50:07 +0000

    Eclipse the assaultbomber has come! A heavy plane twisted into the shape of a jet zeppelin. Beware its tossed bomb!
    svn path=/trunk/mods/zk/; revision=12412

---
+++ Objects3d/zeppelin.dae.lua
@@ -0,0 +1,8 @@
+model = {
+      radius = 20.0,
+      height = 20.0,
+      tex1 = "zeppelinDiffuse2.png",
+      tex2 = "zeppelinSpecial.png",
+      midpos = {0,0,0},
+}
+return model
---
+++ scripts/bomberassault.lua
@@ -0,0 +1,113 @@
+local bomb = piece('bomb');
+local exhaustLeft = piece('exhaustLeft');
+local exhaustRight = piece('exhaustRight');
+local exhaustTop = piece('exhaustTop');
+local hull = piece('hull');
+local petalLeft = piece('petalLeft');
+local petalRear = piece('petalRear');
+local petalRight = piece('petalRight');
+local turbineLeft = piece('turbineLeft');
+local turbineRight = piece('turbineRight');
+local turbineTop = piece('turbineTop');
+local wingLeftFront = piece('wingLeftFront');
+local wingLeftRear = piece('wingLeftRear');
+local wingRightFront = piece('wingRightFront');
+local wingRightRear = piece('wingRightRear');
+local wingTopFront = piece('wingTopFront');
+local wingTopRear = piece('wingTopRear');
+
+local spGetUnitPosition = Spring.GetUnitPosition
+local spGetUnitHeading = Spring.GetUnitHeading
+local spGetUnitVelocity = Spring.GetUnitVelocity
+
+smokePiece = {exhaustTop, exhaustRight, exhaustLeft}
+
+local bombs = 1
+
+include "bombers.lua"
+include "constants.lua"
+
+function script.StartMoving()
+	--Spin(turbineTop, y_axis, 8,2);
+	--Spin(turbineLeft, y_axis, 8,2);
+	--Spin(turbineRight, y_axis, -8,2);
+end
+
+function script.StopMoving()
+	--StopSpin(turbineTop, y_axis, 0.5);
+	--StopSpin(turbineLeft, y_axis, 0.5);
+	--StopSpin(turbineRight, y_axis, 0.5);
+end
+
+function script.Create()
+	StartThread(SmokeUnit)
+end
+
+function script.QueryWeapon(num)
+	return bomb
+end
+
+function script.AimFromWeapon(num)
+	return bomb
+end
+
+function script.AimWeapon(num, heading, pitch)
+	return (Spring.GetUnitFuel(unitID) >= 1 and Spring.GetUnitRulesParam(unitID, "noammo") ~= 1)
+end
+
+function script.FireWeapon(num)
+    if num == 1 then
+	Sleep(66)
+	Reload()
+    end
+end
+
+local predictMult = 3
+
+function script.BlockShot(num, targetID)
+	if num ~= 2 then
+		return false
+	end
+	
+	return not ((GetUnitValue(COB.CRASHING) == 1) or (Spring.GetUnitFuel(unitID) < 1) or (Spring.GetUnitRulesParam(unitID, "noammo") == 1))
+end
+
+function script.Killed(recentDamage, maxHealth)
+	local severity = recentDamage/maxHealth
+	if  severity <= .25  then
+		Explode(turbineLeft, sfxFall + sfxSmoke  + sfxFire )
+		Explode(turbineLeft, sfxFire)
+		Explode(wingLeftFront, sfxFall + sfxSmoke  + sfxFire)
+		Explode(wingLeftRear, sfxFall + sfxSmoke  + sfxFire)
+		return 1
+	elseif severity <= .50  then
+		Explode(turbineLeft, sfxFall + sfxSmoke  + sfxFire )
+		Explode(turbineLeft, sfxExplode)
+		Explode(wingLeftFront, sfxFall + sfxSmoke  + sfxFire)
+		Explode(wingLeftRear, sfxFall + sfxSmoke  + sfxFire)
+		Explode(hull, sfxShatter)
+		return 1
+	elseif severity <= 0.75  then
+		Explode(turbineLeft, sfxExplode + sfxSmoke  + sfxFire )
+		Explode(turbineLeft, sfxExplode)
+		Explode(wingLeftFront, sfxFall + sfxSmoke  + sfxFire)
+		Explode(wingLeftRear, sfxFall + sfxSmoke  + sfxFire)
+		Explode(hull, sfxShatter )
+		return 1
+	else
+		Explode(turbineLeft, sfxExplode + sfxSmoke  + sfxFire )
+		Explode(turbineLeft, sfxExplode)
+		Explode(wingLeftFront, sfxExplode + sfxSmoke  + sfxFire)
+		Explode(wingLeftRear, sfxExplode + sfxSmoke  + sfxFire)
+		Explode(turbineRight, sfxExplode + sfxSmoke  + sfxFire )
+		Explode(turbineRight, sfxExplode)
+		Explode(wingRightFront, sfxExplode + sfxSmoke  + sfxFire)
+		Explode(wingRightRear, sfxExplode + sfxSmoke  + sfxFire)
+		Explode(turbineTop, sfxExplode)
+		Explode(wingTopFront, sfxExplode + sfxSmoke  + sfxFire)
+		Explode(wingTopRear, sfxExplode + sfxSmoke  + sfxFire)
+		
+		Explode(hull, sfxShatter )
+		return 2
+	end
+end
