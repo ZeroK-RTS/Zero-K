9e109b24a049c21329513cb4a56cd5008aff4c12
Anarchid <anarchid@mail.ru>
2014-02-27 19:08:16 +0000

    bomberassault now has DDS textures
    svn path=/trunk/mods/zk/; revision=12806

--- Objects3d/zeppelin.dae.lua
+++ Objects3d/zeppelin.dae.lua
@@ -1,8 +1,9 @@
 model = {
       radius = 20.0,
       height = 20.0,
-      tex1 = "zeppelinDiffuse2.png",
-      tex2 = "zeppelinSpecial.png",
+      tex1 = "zeppelin_1.dds",
+      tex2 = "zeppelin_2.dds",
       midpos = {0,0,0},
+      invertteamcolor = false
 }
 return model
