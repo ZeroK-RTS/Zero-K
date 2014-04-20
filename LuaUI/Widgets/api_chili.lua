ZeroK-RTS/Zero-K

commit 862be8153f3a44cdcc476350aa62dfc2c946ce0b

Initial Queue for ZK. Supports:  * Integral hotkeys  * Tooltips  * Custom comms Does not support:  * Other keyboard shortcuts  * Command insert  * Factoring comm walk speed into build time  * Non-s3o non-3do models (they draw in a funky manner)
svn path=/trunk/mods/zk/; revision=13272

LuaUI/Widgets/api_chili.lua
@@ -9,7 +9,7 @@ function widget:GetInfo()
 		date      = "WIP",
 		license   = "GPLv2",
 		version   = "2.1",
-		layer     = 1000,
+		layer     = -1000,
 		enabled   = true,  --  loaded by default?
 		handler   = true,
 		api       = true,

LuaUI/Widgets/unit_initial_queue.lua
@@ -1,0 +1,528 @@
+
+function widget:GetInfo()
+	return {
+		name      = "Initial Queue ZK",
+		desc      = "Allows you to queue buildings before game start",
+		author    = "Niobium, KingRaptor",
+		version   = "1.5",
+		date      = "7 April 2010",
+		license   = "GNU GPL, v2 or later",
+		layer     = -1, -- Puts it above minimap_startboxes with layer 0
+		enabled   = true,
+		handler   = true
+	}
+end
+-- 12 jun 2012: "uDef.isMetalExtractor" was replaced by "uDef.extractsMetal > 0" to fix "metal" mode map switching (by [teh]decay, thx to vbs and Beherith)
+-- 20 march 2013: added keyboard support with BA keybinds (Bluestone)
+-- august 2013: send queue length to cmd_idle_players (BrainDamage)
+
+------------------------------------------------------------
+-- Config
+------------------------------------------------------------
+local buildOptions = VFS.Include("gamedata/buildoptions.lua")
+
+-- Colors
+local buildDistanceColor = {0.3, 1.0, 0.3, 0.7}
+local buildLinesColor = {0.3, 1.0, 0.3, 0.7}
+local borderNormalColor = {0.3, 1.0, 0.3, 0.5}
+local borderClashColor = {0.7, 0.3, 0.3, 1.0}
+local borderValidColor = {0.0, 1.0, 0.0, 1.0}
+local borderInvalidColor = {1.0, 0.0, 0.0, 1.0}
+local buildingQueuedAlpha = 0.5
+
+local metalColor = '\255\196\196\255' -- Light blue
+local energyColor = '\255\255\255\128' -- Light yellow
+local buildColor = '\255\128\255\128' -- Light green
+local whiteColor = '\255\255\255\255' -- White
+
+local fontSize = 14
+
+------------------------------------------------------------
+-- Globals
+------------------------------------------------------------
+local myTeamID = Spring.GetMyTeamID()
+local myPlayerID = Spring.GetMyPlayerID()
+
+local sDefID = Spring.GetTeamRulesParam(myTeamID, "commChoice") or UnitDefNames.commbasic.id-- Starting unit def ID
+local sDef = UnitDefs[sDefID]
+local buildDistance = sDef.buildDistance
+
+local selDefID = nil -- Currently selected def ID
+local buildQueue = {}
+local buildNameToID = {}
+local gameStarted = false 
+
+local isMex = {} -- isMex[uDefID] = true / nil
+local weaponRange = {} -- weaponRange[uDefID] = # / nil
+
+local changeStartUnitRegex = '^\138(%d+)$'
+local startUnitParamName = 'startUnit'
+
+local scrW, scrH = Spring.GetViewGeometry()
+
+local mCost, eCost, bCost, buildTime = 0, 0, 0, 0
+
+------------------------------------------------------------
+-- Local functions
+------------------------------------------------------------
+local function GetBuildingDimensions(uDefID, facing)
+	local bDef = UnitDefs[uDefID]
+	if (facing % 2 == 1) then
+		return 4 * bDef.zsize, 4 * bDef.xsize
+	else
+		return 4 * bDef.xsize, 4 * bDef.zsize
+	end
+end
+local function DrawBuilding(buildData, borderColor, buildingAlpha, drawRanges)
+
+	local bDefID, bx, by, bz, facing = buildData[1], buildData[2], buildData[3], buildData[4], buildData[5]
+	local bw, bh = GetBuildingDimensions(bDefID, facing)
+
+	gl.DepthTest(false)
+	gl.Color(borderColor)
+
+	gl.Shape(GL.LINE_LOOP, {{v={bx - bw, by, bz - bh}},
+							{v={bx + bw, by, bz - bh}},
+							{v={bx + bw, by, bz + bh}},
+							{v={bx - bw, by, bz + bh}}})
+
+	if drawRanges then
+		--[[
+		if isMex[bDefID] then
+			gl.Color(1.0, 0.3, 0.3, 0.7)
+			gl.DrawGroundCircle(bx, by, bz, Game.extractorRadius, 40)
+		end
+		]]
+
+		local wRange = weaponRange[bDefID]
+		if wRange then
+			gl.Color(1.0, 0.3, 0.3, 0.7)
+			gl.DrawGroundCircle(bx, by, bz, wRange, 40)
+		end
+	end
+
+	gl.DepthTest(GL.LEQUAL)
+	gl.DepthMask(true)
+	if buildingAlpha == 1 then gl.Lighting(true) end
+	gl.Color(1.0, 1.0, 1.0, buildingAlpha)
+
+	gl.PushMatrix()
+		gl.Translate(bx, by, bz)
+		gl.Rotate(90 * facing, 0, 1, 0)
+		gl.UnitShape(bDefID, Spring.GetMyTeamID())
+	gl.PopMatrix()
+
+	gl.Lighting(false)
+	gl.DepthTest(false)
+	gl.DepthMask(false)
+end
+local function DrawUnitDef(uDefID, uTeam, ux, uy, uz)
+
+	gl.Color(1.0, 1.0, 1.0, 1.0)
+	gl.DepthTest(GL.LEQUAL)
+	gl.DepthMask(true)
+	gl.Lighting(true)
+
+	gl.PushMatrix()
+		gl.Translate(ux, uy, uz)
+		gl.UnitShape(uDefID, uTeam)
+	gl.PopMatrix()
+
+	gl.Lighting(false)
+	gl.DepthTest(false)
+	gl.DepthMask(false)
+end
+local function DoBuildingsClash(buildData1, buildData2)
+
+	local w1, h1 = GetBuildingDimensions(buildData1[1], buildData1[5])
+	local w2, h2 = GetBuildingDimensions(buildData2[1], buildData2[5])
+
+	return math.abs(buildData1[2] - buildData2[2]) < w1 + w2 and
+	       math.abs(buildData1[4] - buildData2[4]) < h1 + h2
+end
+local function SetSelDefID(defID)
+	selDefID = defID
+
+	if (isMex[selDefID] ~= nil) ~= (Spring.GetMapDrawMode() == "metal") then
+		Spring.SendCommands("ShowMetalMap")
+	end
+	if defID then
+		Spring.SetActiveCommand(defID)
+	end
+end
+
+local function GetSelDefID(defID)
+	return selDefID
+end
+
+local function GetUnitCanCompleteQueue(uID)
+
+	local uDefID = Spring.GetUnitDefID(uID)
+	if uDefID == sDefID then
+		return true
+	end
+
+	-- What can this unit build ?
+	local uCanBuild = {}
+	local uBuilds = UnitDefs[uDefID].buildOptions
+	for i = 1, #uBuilds do
+		uCanBuild[uBuilds[i]] = true
+	end
+
+	-- Can it build everything that was queued ?
+	for i = 1, #buildQueue do
+		if not uCanBuild[buildQueue[i][1]] then
+			return false
+		end
+	end
+
+	return true
+end
+local function GetQueueBuildTime()
+	local t = 0
+	for i = 1, #buildQueue do
+		t = t + UnitDefs[buildQueue[i][1]].buildTime
+	end
+	return t / sDef.buildSpeed
+end
+local function GetQueueCosts()
+	local mCost = 0
+	local eCost = 0
+	local bCost = 0
+	for i = 1, #buildQueue do
+		local uDef = UnitDefs[buildQueue[i][1]]
+		mCost = mCost + uDef.metalCost
+		eCost = eCost + uDef.energyCost
+		bCost = bCost + uDef.buildTime
+	end
+	return mCost, eCost, bCost
+end
+
+local function GetBuildOptions()
+	return buildOptions
+end
+------------------------------------------------------------
+-- Initialize/shutdown
+------------------------------------------------------------
+function widget:Initialize()
+	if (Game.startPosType == 1) or			-- Don't run if start positions are random
+	   (Spring.GetGameFrame() > 0) or		-- Don't run if game has already started
+	   (Spring.GetSpectatingState()) then	-- Don't run if we are a spec
+		widgetHandler:RemoveWidget(self)
+		return
+	end
+	for uDefID, uDef in pairs(UnitDefs) do
+		if uDef.customParams.ismex then
+			isMex[uDefID] = true
+		end
+
+		if uDef.maxWeaponRange > 16 then
+			weaponRange[uDefID] = uDef.maxWeaponRange
+		end
+	end
+	
+	WG.InitialQueue = {
+		SetSelDefID = SetSelDefID,
+		GetBuildOptions = GetBuildOptions,
+		GetSelDefID = GetSelDefID,
+	}
+end
+
+function widget:Shutdown()
+	WG.InitialQueue = nil
+end
+
+------------------------------------------------------------
+-- Drawing
+------------------------------------------------------------
+--local queueTimeFormat = whiteColor .. 'Queued: ' .. buildColor .. '%.1f sec ' .. whiteColor .. '[' .. metalColor .. '%d m' .. whiteColor .. ', ' .. energyColor .. '%d e' .. whiteColor .. ']'
+local queueTimeFormat = whiteColor .. 'Queued ' .. metalColor .. '%dm ' .. buildColor .. '%.1f sec'
+--local queueTimeFormat = metalColor .. '%dm ' .. whiteColor .. '/ ' .. energyColor .. '%de ' .. whiteColor .. '/ ' .. buildColor .. '%.1f sec'
+
+
+-- "Queued 23.9 seconds (820m / 2012e)" (I think this one is the best. Time first emphasises point and goodness of widget)
+	-- Also, it is written like english and reads well, none of this colon stuff or figures stacked together
+
+local timer = 0
+local updateFreq = 0.15
+
+-- check if we're chosen a new comm
+
+function widget:Update(dt)
+	timer = timer + dt
+	if timer > updateFreq then
+		local defID = Spring.GetTeamRulesParam(myTeamID, "commChoice")
+		if defID and defID ~= sDefID then
+			local def = UnitDefs[defID]
+			if def then
+				sDefID = defID
+				sDef = def
+				buildDistance = sDef.buildDistance
+				mCost, eCost, bCost = GetQueueCosts()
+				buildTime = bCost / sDef.buildSpeed
+			end
+		end
+		timer = 0
+	end
+end
+	
+function widget:DrawScreen()
+	gl.PushMatrix()
+	gl.Translate(scrW/2, scrH*0.4, 0)
+	if #buildQueue > 0 then	
+		gl.Text(string.format(queueTimeFormat, mCost, buildTime), 0, 0, fontSize, 'cdo')
+	end
+	gl.PopMatrix()
+end
+function widget:DrawWorld()
+	--don't draw anything once the game has started; after that engine can draw queues itself
+	if gameStarted then return end
+
+	-- Set up gl
+	gl.LineWidth(1.49)
+
+	-- We need data about currently selected building, for drawing clashes etc
+	local selBuildData
+	if selDefID then
+		local mx, my = Spring.GetMouseState()
+		local _, pos = Spring.TraceScreenRay(mx, my, true)
+		if pos then
+			local bx, by, bz = Spring.Pos2BuildPos(selDefID, pos[1], pos[2], pos[3])
+			local buildFacing = Spring.GetBuildFacing()
+			selBuildData = {selDefID, bx, by, bz, buildFacing}
+		end
+	end
+
+	local myTeamID = Spring.GetMyTeamID()
+	local sx, sy, sz = Spring.GetTeamStartPosition(myTeamID) -- Returns -100, -100, -100 when none chosen
+	local startChosen = (sx ~= -100)
+	if startChosen then
+		-- Correction for start positions in the air
+		sy = Spring.GetGroundHeight(sx, sz)
+
+		-- Draw the starting unit at start position
+		DrawUnitDef(sDefID, myTeamID, sx, sy, sz)
+
+		-- Draw start units build radius
+		gl.Color(buildDistanceColor)
+		gl.DrawGroundCircle(sx, sy, sz, buildDistance, 40)
+	end
+
+	-- Draw all the buildings
+	local queueLineVerts = startChosen and {{v={sx, sy, sz}}} or {}
+	for b = 1, #buildQueue do
+		local buildData = buildQueue[b]
+
+		if selBuildData and DoBuildingsClash(selBuildData, buildData) then
+			DrawBuilding(buildData, borderClashColor, buildingQueuedAlpha)
+		else
+			DrawBuilding(buildData, borderNormalColor, buildingQueuedAlpha)
+		end
+
+		queueLineVerts[#queueLineVerts + 1] = {v={buildData[2], buildData[3], buildData[4]}}
+	end
+
+	-- Draw queue lines
+	gl.Color(buildLinesColor)
+	gl.LineStipple("springdefault")
+		gl.Shape(GL.LINE_STRIP, queueLineVerts)
+	gl.LineStipple(false)
+
+	-- Draw selected building
+	if selBuildData then
+		if Spring.TestBuildOrder(selDefID, selBuildData[2], selBuildData[3], selBuildData[4], selBuildData[5]) ~= 0 then
+			DrawBuilding(selBuildData, borderValidColor, 1.0, true)
+		else
+			DrawBuilding(selBuildData, borderInvalidColor, 1.0, true)
+		end
+	end
+
+	-- Reset gl
+	gl.Color(1.0, 1.0, 1.0, 1.0)
+	gl.LineWidth(1.0)
+end
+
+function widget:ViewResize(vsx, vsy)
+	scrW = vsx
+	scrH = vsy
+end
+
+------------------------------------------------------------
+-- Game start
+------------------------------------------------------------
+
+function widget:GameFrame(n)
+
+	if not gameStarted then
+		gameStarted = true
+	end
+
+	-- Don't run if we are a spec
+	local areSpec = Spring.GetSpectatingState()
+	if areSpec then
+		widgetHandler:RemoveWidget(self)
+		return
+	end
+	
+	-- Don't run if we didn't queue anything
+	if (#buildQueue == 0) then
+		widgetHandler:RemoveWidget(self)
+		return
+	end
+
+	if (n < 2) then return end -- Give the unit frames 0 and 1 to spawn
+	
+	--inform gadget how long is our queue
+	local buildTime = GetQueueBuildTime()
+	--Spring.SendCommands("luarules initialQueueTime " .. buildTime)
+	
+	if (n == 4) then
+		--Spring.Echo("> Starting unit never spawned !")
+		widgetHandler:RemoveWidget(self)
+		return
+	end
+	
+	local tasker
+	-- Search for our starting unit
+	local units = Spring.GetTeamUnits(Spring.GetMyTeamID())
+	for u = 1, #units do
+		local uID = units[u]
+		if GetUnitCanCompleteQueue(uID) then --Spring.GetUnitDefID(uID) == sDefID then
+			--we found our com, assigning queue to this particular unit
+			tasker = uID
+			break
+		end
+	end
+	if tasker then
+		--Spring.Echo("sending queue to unit")
+		for b = 1, #buildQueue do
+			local buildData = buildQueue[b]
+			Spring.GiveOrderToUnit(tasker, -buildData[1], {buildData[2], buildData[3], buildData[4], buildData[5]}, {"shift"})
+		end
+		widgetHandler:RemoveWidget(self)
+	end
+end
+
+------------------------------------------------------------
+-- Mouse
+------------------------------------------------------------
+function SetBuildFacing()
+	local wx,wy,_,_ = Spring.GetScreenGeometry()
+	local _, pos = Spring.TraceScreenRay(wx/2, wy/2, true)
+	if not pos then return end
+	local x = pos[1]
+	local z = pos[3]
+	
+	if math.abs(Game.mapSizeX - 2*x) > math.abs(Game.mapSizeZ - 2*z) then
+		if (2*x>Game.mapSizeX) then
+			facing=3
+		else
+			facing=1
+		end
+	else
+		if (2*z>Game.mapSizeZ) then
+			facing=2
+		else
+			facing=0
+		end
+	end
+	Spring.SetBuildFacing(facing)
+end
+
+needBuildFacing = true
+
+function widget:MousePress(mx, my, mButton)
+	if selDefID then
+		if mButton == 1 then
+			local mx, my = Spring.GetMouseState()
+			local _, pos = Spring.TraceScreenRay(mx, my, true)
+			if not pos then return end
+			local bx, by, bz = Spring.Pos2BuildPos(selDefID, pos[1], pos[2], pos[3])
+
+			if isMex[selDefID] then
+				local bestSpot = WG.GetClosestMetalSpot(bx, bz)
+				bx, by, bz = bestSpot.x, bestSpot.y, bestSpot.z
+			end
+			local buildFacing = Spring.GetBuildFacing()
+	
+			if Spring.TestBuildOrder(selDefID, bx, by, bz, buildFacing) ~= 0 then
+	
+				local buildData = {selDefID, bx, by, bz, buildFacing}
+				local _, _, meta, shift = Spring.GetModKeyState()
+				if meta then
+					table.insert(buildQueue, 1, buildData)
+	
+				elseif shift then
+	
+					local anyClashes = false
+					for i = #buildQueue, 1, -1 do
+						if DoBuildingsClash(buildData, buildQueue[i]) then
+							anyClashes = true
+							table.remove(buildQueue, i)
+						end
+					end
+	
+					if not anyClashes then
+						buildQueue[#buildQueue + 1] = buildData
+					end
+				else
+					buildQueue = {buildData}
+				end
+				
+				mCost, eCost, bCost = GetQueueCosts()
+				buildTime = bCost / sDef.buildSpeed
+	
+				if not shift then
+					SetSelDefID(nil)
+				end
+			end
+	
+			return true
+	
+		elseif mButton == 3 then
+			SetSelDefID(nil)
+			return true
+		end
+	end
+end
+function widget:MouseMove(mx, my, dx, dy, mButton)
+	if areDragging then
+		wl = wl + dx
+		wt = wt + dy
+	end
+end
+function widget:MouseRelease(mx, my, mButton)
+	areDragging = false
+end
+
+------------------------------------------------------------
+-- Misc
+------------------------------------------------------------
+function widget:TextCommand(cmd)
+	-- Facing commands are only handled by spring if we have a building selected, which isn't possible pre-game
+	local m = cmd:match("^buildfacing (.+)$")
+	if m then
+
+		local oldFacing = Spring.GetBuildFacing()
+		local newFacing
+		if (m == "inc") then
+			newFacing = (oldFacing + 1) % 4
+		elseif (m == "dec") then
+			newFacing = (oldFacing + 3) % 4
+		else
+			return false
+		end
+
+		Spring.SetBuildFacing(newFacing)
+		Spring.Echo("Buildings set to face " .. ({"South", "East", "North", "West"})[1 + newFacing])
+		return true
+	end
+	local buildName = cmd:match("^buildunit_([^%s]+)$")
+	if buildName then
+		local bDefID = buildNameToID[buildName]
+		if bDefID then
+			SetSelDefID(bDefID)
+			return true
+		end
+	end
+end

LuaUI/Widgets/cmd_mex_placement.lua
@@ -186,6 +186,7 @@ local function GetClosestMetalSpot(x, z) --is used by single mex placement, not 
 	end
 	return bestSpot, sqrt(bestDist), bestIndex
 end
+WG.GetClosestMetalSpot = GetClosestMetalSpot
 
 local function Distance(x1,z1,x2,z2)
 	local dis = (x1-x2)*(x1-x2)+(z1-z2)*(z1-z2)
@@ -668,6 +669,8 @@ function widget:DrawWorld()
 	local _, cmdID = spGetActiveCommand()
 	local showecoMode = WG.showeco
 	local peruse = spGetGameFrame() < 1 or showecoMode or spGetMapDrawMode() == 'metal'
+	cmdID = cmdID or (WG.InitialQueue and -(WG.InitialQueue.GetSelDefID() or 0))
+	
 	
 	local mx, my = spGetMouseState()
 	local _, pos = spTraceScreenRay(mx, my, true)

LuaUI/Widgets/gui_chili_integral_menu.lua
@@ -333,21 +333,36 @@ local config = {
 --  FUNCTIONS
 ------------------------
 -- this gets invoked when button is clicked 
-local function ClickFunc(button, x, y, mouse)
+local function ClickFunc(button, x, y, mouse, customFunc, customFuncArgs)
 	local left, right = mouse == 1, mouse == 3
 	local alt,ctrl,meta,shift = Spring.GetModKeyState()
-	local index = Spring.GetCmdDescIndex(button.cmdid)
-	if (left) then
-		Spring.SetActiveCommand(index,1,left,right,alt,ctrl,meta,shift)
-	elseif (right) then
-		Spring.SetActiveCommand(index,3,left,right,alt,ctrl,meta,shift)
+	local mb = (left and 1) or (right and 3)
+	if mb then
+		if customFunc then
+			customFunc(unpack(customFuncArgs))
+		end
+		local index = Spring.GetCmdDescIndex(button.cmdid)
+		if index then
+			Spring.SetActiveCommand(index,mb,left,right,alt,ctrl,meta,shift)
+		end
 	end
-end 
+end
+
+-- this lets you create special buttons in integral
+local function MakeSpecialCmdDesc(cmdID, customFunc, customFuncArgs, params)
+	return {
+		id = cmdID,
+		tooltip = params and params.tooltip,
+		disabled = params and params.disabled,
+		customFunc = customFunc,
+		customFuncArgs = customFuncArgs,
+	}
+end
 
 ------------------------
 --  Generates or updates chili button - either image or text or both based - container is parent of button, cmd is command desc structure
 ------------------------
-local function MakeButton(container, cmd, insertItem, index) 
+local function MakeButton(container, cmd, insertItem, index)
 	local isState = (cmd.type == CMDTYPE.ICON_MODE and #cmd.params > 1) or states_commands[cmd.id]	--is command a state toggle command?
 	local isBuild = (cmd.id < 0)
 	local gridHotkeyed = not isState and menuChoice ~= 1 and menuChoice ~= 6 
@@ -448,7 +463,7 @@ local function MakeButton(container, cmd, insertItem, index)
 			isDisabled = cmd.disabled;
 			tooltip = tooltip;
 			cmdid = cmd.id;
-			OnClick = {ClickFunc} --activate the clicked command	
+			OnClick = {function(self, x, y, mouse) ClickFunc(self, x, y, mouse, cmd.customFunc, cmd.customFuncArgs) end}
 		}
 		if (isState) then 
 			button.padding = {4,4,2.5,2}
@@ -667,11 +682,11 @@ local function BuildRowButtonFunc(num, cmdid, left, right)
 		else return 0 end
 	end
 	
-	--Spring.Echo(CMD.OPT_META) = 4
-	--Spring.Echo(CMD.OPT_RIGHT) = 16
-	--Spring.Echo(CMD.OPT_SHIFT) = 32
-	--Spring.Echo(CMD.OPT_CTRL) = 64
-	--Spring.Echo(CMD.OPT_ALT) = 128
+	--CMD.OPT_META = 4
+	--CMD.OPT_RIGHT = 16
+	--CMD.OPT_SHIFT = 32
+	--CMD.OPT_CTRL = 64
+	--CMD.OPT_ALT = 128
 	
 	--it's not using the options, even though it's receiving them correctly
 	--so we have to do it manually
@@ -871,8 +886,8 @@ local function ManageCommandIcons(useRowSort)
 end
 
 local function Update(buttonpush) 
-    local commands = widgetHandler.commands
-    local customCommands = widgetHandler.customCommands
+	local commands = widgetHandler.commands
+	local customCommands = widgetHandler.customCommands
 	--most commands don't use row sorting; econ, defense and special do
 	local useRowSort = (menuChoice == 3 or menuChoice == 4 or menuChoice == 5)
 	
@@ -902,7 +917,22 @@ local function Update(buttonpush)
 	--Spring.Echo(#commands)
 	for i = 1, #commands do ProcessCommand(commands[i]) end 
 	for i = 1, #customCommands do ProcessCommand(customCommands[i]) end 
-	for i = 1, #globalCommands do ProcessCommand(globalCommands[i]) end 
+	for i = 1, #globalCommands do ProcessCommand(globalCommands[i]) end
+	
+	-- handling for initial queue
+	if Spring.GetGameFrame() <= 0 then
+		local buildOptions = WG.InitialQueue and WG.InitialQueue.GetBuildOptions()
+		if buildOptions then
+			for i=1,#buildOptions do
+				local ud = UnitDefNames[buildOptions[i]]
+				local udid = ud.id
+				local func = WG.InitialQueue.SetSelDefID
+				local args = {udid}
+				local cmdDesc = MakeSpecialCmdDesc(-udid, func, args, {tooltip = "Build: " .. ud.humanName .. " - "})
+				ProcessCommand(cmdDesc)
+			end
+		end
+	end
 
 	menuChoices[1].array = n_common
 	menuChoices[2].array = n_factories
@@ -1090,6 +1120,10 @@ function widget:KeyPress(key, modifier, isRepeat)
 					local index = Spring.GetCmdDescIndex(cmdid)
 					if index then
 						Spring.SetActiveCommand(index,1,true,false,false,false,false,false)
+						thingsDone = true
+					elseif commandButtons[cmdid] then
+						local button = commandButtons[cmdid].button
+						button.OnClick[1](button, nil, nil, 1)
 						thingsDone = true
 					end
 				end

commit 6c427271fd9086460a1fe10e00ad856d75d42b72

Fix Chili crash introduced in r13272 by moving layers appropriately
svn path=/trunk/mods/zk/; revision=13276

LuaUI/Widgets/api_chili.lua
@@ -9,7 +9,7 @@ function widget:GetInfo()
 		date      = "WIP",
 		license   = "GPLv2",
 		version   = "2.1",
-		layer     = -1000,
+		layer     = 1000,
 		enabled   = true,  --  loaded by default?
 		handler   = true,
 		api       = true,

LuaUI/Widgets/minimap_startbox.lua
@@ -19,7 +19,7 @@ function widget:GetInfo()
     author    = "trepan, jK, Rafal",
     date      = "2007-2012",
     license   = "GNU GPL, v2 or later",
-    layer     = 0,
+    layer     = 1002,
     enabled   = true  --  loaded by default?
   }
 end

LuaUI/Widgets/unit_initial_queue.lua
@@ -7,7 +7,7 @@ function widget:GetInfo()
 		version   = "1.5",
 		date      = "7 April 2010",
 		license   = "GNU GPL, v2 or later",
-		layer     = -1, -- Puts it above minimap_startboxes with layer 0
+		layer     = 1001, -- Puts it above minimap_startboxes with layer 1002
 		enabled   = true,
 		handler   = true
 	}