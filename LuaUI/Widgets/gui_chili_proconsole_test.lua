592af8394d9682101e8efdebdb78317328d7c3e8
dvr333 <shadowfury333@gmail.com>
2014-04-24 02:50:38 +0000

    Broadened whitelist for always-visible log messages
    svn path=/trunk/mods/zk/; revision=13327

--- LuaUI/Widgets/gui_chili_proconsole_test.lua
+++ LuaUI/Widgets/gui_chili_proconsole_test.lua
@@ -595,7 +595,8 @@ local function hideMessage(msg)
 		or (msg.msgtype == "point" and options.hidePoint.value)
 		or (msg.msgtype == "label" and options.hideLabel.value)
 		or (msg.msgtype == 'other' and options.hideLog.value and not ((msg.argument):find('enabled!') or (msg.argument):find('disabled!') or 
-			(msg.argument):find('Wind Range') or (msg.argument):find('utogroup') or (msg.argument):find('Speed set to') or (msg.argument:find('following') )))
+			(msg.argument):find('Wind Range') or (msg.argument):find('utogroup') or (msg.argument):find('Speed set to') or (msg.argument):find('following') or (msg.argument):find('Connection attempted') or
+				(msg.argument):find('wins!') or (msg.argument):find('resigned') or (msg.argument):find('exited') or (msg.argument):find('is no more') or (msg.argument):find('paused the game') ))
 end
 
 local function AddMessage(msg, target, fade2, remake)
