ab691485ba2988fce02b95786481d66080e8d4b2
xponen <msafwan86msafwan86_nnss@hotmail.com>
2014-06-03 23:59:47 +0000

    add dbg_forceGC_spring97.lua : an always-run widget that call LUA's Garbage Collector if usage exceed 300MB. Hopefully fix a rare case of abnormally high LUA memory usage while rejoining game.
    svn path=/trunk/mods/zk/; revision=13708

---
+++ LuaUI/Widgets/dbg_forceGC_spring97.lua
@@ -0,0 +1,44 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function widget:GetInfo()
+  return {
+    name      = "GC at >300MB",
+    desc      = "Workaround for abnormal memory usage while rejoining game in Spring 97." 
+				.."Usual ingame usage never exceed 100MB.",
+    author    = "xponen",
+    version   = "1",
+    date      = "4 June 2014",
+    license   = "none",
+    layer     = math.huge,
+	alwaysStart = true,	
+    enabled   = true  --  loaded by default?
+  }
+end
+--Note: cannot use widget:GameProgress() to check for rejoining status because its broken in Spring 97 (the callin didn't tell rejoiner the actual current frame)
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+function widget:Initialize()
+	if (Game.version:find('91.0') == 1) then
+		Spring.Echo("Removed 'GC at >300MB': not needed for Spring 91") 
+		widgetHandler:RemoveWidget()
+	end
+end
+
+local sec = 0 --amount of second since last "collect garbage".
+local interval = 60 -- interval of 1 minute
+local memThreshold = 102400*3 --amount of memory usage (kilobyte) before calling LUA GC
+function widget:Update(dt)
+	sec = sec + dt
+	if (sec >= interval) then --if minimum interval reached:
+		sec = 0
+		local memusage = collectgarbage("count") --get total amount of memory usage for LUAUI
+		if (memusage > memThreshold) then
+			-- local memString = ('%.1f'):format(memusage/1024) .. " MB" --display current memory usage to player
+			-- Spring.Echo(memString)
+			collectgarbage("collect") --collect garbage
+		end
+	end
+end
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
