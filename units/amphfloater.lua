54f73ad66796d38324f5a49318447284e4691b04
sprunk <gnurps@o2.pl>
2014-07-30 19:06:30 +0200

    Cleanup Buoy defs

--- units/amphfloater.lua
+++ units/amphfloater.lua
@@ -17,9 +17,9 @@ unitDef = {
   corpse                 = [[DEAD]],
 
   customParams           = {
-    description_pl = [[Amfibijny Bot-P³ywak]],
+    description_pl = [[Amfibijny bot plywak]],
     helptext       = [[The Buoy works around its inability to shoot while submerged by floating to the surface of the sea. Here it can fire a decently ranged cannon with slow damage. It is unable to move while floating.]],
-    helptext_pl    = [[Buoy nie mo¿e strzelaæ pod wod¹, ale radzi sobie z tym poprzez wyp³ywanie na powierzchniê. Wtedy mo¿e atakowaæ przy u¿yciu dzia³ka o dobrym zasiêgu, zadaj¹c dodatkowe obra¿enia spowalniaj¹ce. Bêd¹c na powierzchni nie mo¿e siê poruszaæ.]],
+    helptext_pl    = [[Buoy nie moze strzelac pod woda, ale radzi sobie z tym poprzez wyplywanie na powierzchnie. Wtedy moze atakowac przy uzyciu dzialka o dobrym zasiegu, zadajac dodatkowo obrazenia spowalniajace. Bedac na powierzchni nie moze sie poruszac.]],
     floattoggle    = [[1]],
   },
 
@@ -31,12 +31,11 @@ unitDef = {
   idleTime               = 1800,
   leaveTracks            = true,
   maxDamage              = 1250,
-  maxSlope               = 36,
   maxVelocity            = 1.4,
   maxWaterDepth          = 5000,
   minCloakDistance       = 75,
   movementClass          = [[AKBOT2]],
-  noChaseCategory        = [[TERRAFORM SATELLITE FIXEDWING GUNSHIP]],
+  noChaseCategory        = [[TERRAFORM FIXEDWING GUNSHIP]],
   objectName             = [[can.s3o]],
   script                 = [[amphfloater.lua]],
   seismicSignature       = 4,
@@ -81,7 +80,6 @@ unitDef = {
 
       damage                  = {
         default = 150,
-        planes  = 150,
         subs    = 7.5,
       },
 
@@ -92,8 +90,8 @@ unitDef = {
       impulseFactor           = 0.2,
       interceptedByShieldType = 2,
       myGravity               = 0.2,
-	  predictBoost            = 1,
-	  range 			      = 450,
+      predictBoost            = 1,
+      range                   = 450,
       reloadtime              = 1.8,
       rgbcolor                = [[0.9 0.1 0.9]],
       soundHit                = [[weapon/laser/small_laser_fire]],
@@ -104,8 +102,8 @@ unitDef = {
       turret                  = true,
       weaponType              = [[Cannon]],
       weaponVelocity          = 340,
-	},
-	FAKE_CANNON = {
+    },
+    FAKE_CANNON = {
       name                    = [[Fake Disruption Cannon]],
       accuracy                = 200,
       areaOfEffect            = 32,
@@ -115,7 +113,6 @@ unitDef = {
 
       damage                  = {
         default = 150,
-        planes  = 150,
         subs    = 7.5,
       },
 
@@ -126,8 +123,8 @@ unitDef = {
       impulseFactor           = 0.2,
       interceptedByShieldType = 2,
       myGravity               = 0.2,
-	  predictBoost            = 1,
-	  range 			      = 450,
+      predictBoost            = 1,
+      range                   = 450,
       reloadtime              = 1.8,
       rgbcolor                = [[0.9 0.1 0.9]],
       soundHit                = [[weapon/laser/small_laser_fire]],
@@ -136,10 +133,10 @@ unitDef = {
       soundStartVolume        = 3.5,
       soundTrigger            = true,
       turret                  = true,
-	  waterWeapon             = true,
+      waterWeapon             = true,
       weaponType              = [[Cannon]],
       weaponVelocity          = 340,
-	},
+    },
 
   },
 
