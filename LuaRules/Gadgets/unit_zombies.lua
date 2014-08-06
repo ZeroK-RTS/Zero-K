07330df82e750e5e2c9e750ecfb41c5cd9a78ddf
N0U <petushkov.egor@gmail.com>
2014-08-06 11:56:11 -0700

    Ops. I forgot to write changes into changelog in the gadget

--- LuaRules/Gadgets/unit_zombies.lua
+++ LuaRules/Gadgets/unit_zombies.lua
@@ -15,6 +15,7 @@ end
 --SYNCED-------------------------------------------------------------------
 
 -- changelog
+-- 6 august 2014 - 0.1.3. Some magic which might fix crash. (At least it fixed the original zombie gadget)
 -- 7 april 2014 - 0.1.2. Added permaslow option. Default on. 50% is max slow for now.
 -- 5 april 2014 - 0.1.1. Sfx, gfx, factory orders added. Slow down upon reclaim added. Thanks Anarchid.
 -- 5 april 2014 - 0.1.0. Release.
