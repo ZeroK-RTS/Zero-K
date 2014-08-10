b69cd3a0cd7f67ab5b0d4ad3050eb3e137575f1a
sprunk <gnurps@o2.pl>
2014-08-10 18:13:49 +0200

    Fix PL helptext & cleanup Quake (Seismic)

--- units/seismic.lua
+++ units/seismic.lua
@@ -2,11 +2,6 @@ unitDef = {
   unitname                      = [[seismic]],
   name                          = [[Quake]],
   description                   = [[Seismic Missile]],
-  acceleration                  = 1,
-  antiweapons                   = [[1]],
-  bmcode                        = [[0]],
-  brakeRate                     = 0,
-  buildAngle                    = 8192,
   buildCostEnergy               = 400,
   buildCostMetal                = 400,
   builder                       = false,
@@ -17,8 +12,6 @@ unitDef = {
   buildPic                      = [[seismic.png]],
   buildTime                     = 400,
   canAttack                     = true,
-  canGuard                      = true,
-  canstop                       = [[1]],
   category                      = [[SINK UNARMED]],
   collisionVolumeOffsets        = [[0 15 0]],
   collisionVolumeScales         = [[20 50 20]],
@@ -29,8 +22,8 @@ unitDef = {
     description_de = [[Seismische Rakete]],
     description_pl = [[Rakieta Sejsmiczna]],
     helptext       = [[The Quake creates a powerful seismic shockwave that smooths a wide area of terrain, while causing minimal harm to units.]],
-	helptext_de    = [[Die Rakete Quake erzeugt eine akustische Schockwelle, welche die anliegend Boden glatt macht, aber nur minimale Schäden an Einheiten aus Metall und Kohlenstoff-Nanoröhrchen verursacht.]],
-	helptext_pl    = [[Jednorazowa rakieta sejsmiczna dalekiego zasięgu. Nie zadaje obrażeń, ale wyrównuje okoliczny teren.]],
+    helptext_de    = [[Die Rakete Quake erzeugt eine akustische Schockwelle, welche die anliegend Boden glatt macht, aber nur minimale Schäden an Einheiten aus Metall und Kohlenstoff-Nanoröhrchen verursacht.]],
+    helptext_pl    = [[Jednorazowa rakieta sejsmiczna dalekiego zasiegu. Nie zadaje obrazen, ale wyrownuje okoliczny teren.]],
     mobilebuilding = [[1]],
   },
 
@@ -40,13 +33,9 @@ unitDef = {
   iconType                      = [[cruisemissilesmall]],
   idleAutoHeal                  = 5,
   idleTime                      = 1800,
-  mass                          = 188,
   maxDamage                     = 1000,
   maxSlope                      = 18,
-  maxVelocity                   = 0,
-  maxWaterDepth                 = 0,
   minCloakDistance              = 150,
-  noAutoFire                    = false,
   objectName                    = [[wep_seismic.s3o]],
   script                        = [[cruisemissile.lua]],
   seismicSignature              = 4,
@@ -60,13 +49,8 @@ unitDef = {
 
   },
 
-  side                          = [[CORE]],
   sightDistance                 = 0,
-  smoothAnim                    = true,
-  TEDClass                      = [[SPECIAL]],
-  turnRate                      = 0,
   useBuildingGroundDecal        = false,
-  workerTime                    = 0,
   yardMap                       = [[o]],
 
   weapons                       = {
@@ -79,11 +63,10 @@ unitDef = {
 
   },
 
-
   weaponDefs                    = {
 
     SEISMIC_WEAPON = {
-      name                    = [[Seismic]],
+      name                    = [[Seismic Missile]],
       areaOfEffect            = 512,
       avoidFriendly           = false,
       cegTag                  = [[seismictrail]],
@@ -91,46 +74,35 @@ unitDef = {
       craterBoost             = 32,
       craterMult              = 1,
 
-	  customParams            = {
-	    gatherradius = [[416]],
-		smoothradius = [[256]],
-		detachmentradius = [[256]],
-		smoothmult   = [[1]],
-	  },
+      customParams            = {
+        gatherradius = [[416]],
+        smoothradius = [[256]],
+        detachmentradius = [[256]],
+        smoothmult   = [[1]],
+      },
 	  
       damage                  = {
         default = 20,
-        planes  = 20,
         subs    = 1,
       },
 
       edgeEffectiveness       = 0.4,
       explosionGenerator      = [[custom:bull_fade]],
       fireStarter             = 0,
-      guidance                = true,
       interceptedByShieldType = 1,
       levelGround             = false,
-      lineOfSight             = true,
       model                   = [[wep_seismic.s3o]],
-      noautorange             = [[1]],
       noSelfDamage            = true,
-      propeller               = [[1]],
       range                   = 6000,
       reloadtime              = 10,
-      renderType              = 1,
-      selfprop                = true,
       shakeduration           = [[4]],
       shakemagnitude          = [[32]],
-      smokedelay              = [[0.1]],
       smokeTrail              = false,
       soundHit                = [[explosion/ex_large4]],
       soundStart              = [[weapon/missile/tacnuke_launch]],
-      startsmoke              = [[1]],
       tolerance               = 4000,
-      twoPhase                = true,
       turnrate                = 18000,
-      vlaunch                 = true,
-	  waterWeapon             = true,
+      waterWeapon             = true,
       weaponAcceleration      = 180,
       weaponTimer             = 3,
       weaponType              = [[StarburstLauncher]],
@@ -138,7 +110,6 @@ unitDef = {
     },
 
   },
-
 
   featureDefs                   = {
   },
