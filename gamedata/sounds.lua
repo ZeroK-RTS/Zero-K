0addd4779f98ec222ba5805abfe4eb5eaf36c1ee
ikinz <danny.b.eml@gmail.com>
2014-08-11 22:15:23 +1100

    Use the decrackled placing sound.

--- gamedata/sounds.lua
+++ gamedata/sounds.lua
@@ -12,7 +12,7 @@ local Sounds = {
 		--   file = "sounds/button9.wav",
 		--},
 		MapPoint = {
-			file = "sounds/beep4.wav", --"sounds/marker_place.wav",
+			file = "sounds/beep4_decrackled.wav", --"sounds/marker_place.wav",
 			--pitchMod = 0.01,
 			--pitch = 0.8,
 			maxconcurrent = 3,
