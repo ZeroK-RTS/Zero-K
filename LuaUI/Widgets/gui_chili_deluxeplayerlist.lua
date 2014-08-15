a1da8cd039a1ff69a2f4ab86e9ad1d9fd1e062e8
Dominic Renaud <drenaud@yahoo.ca>
2014-08-15 09:12:46 -0700

    Revert unrelated colour changes

--- LuaUI/Widgets/gui_chili_deluxeplayerlist.lua
+++ LuaUI/Widgets/gui_chili_deluxeplayerlist.lua
@@ -391,7 +391,7 @@ local function FormatElo(elo,full)
 	local bot = 1400
 	local tc = {1,1,1,1}
 	local mc = {1,1,0,1}
-	local bc = {1,.4,.3,1}
+	local bc = {1,.2,.2,1}
 	
 	if elo_out >= top then eloCol = tc
 	elseif elo_out >= mid then
