85cf55186a258a50f1b0381258ece5298dd10e5b
sprunk <gnurps@o2.pl>
2014-10-03 01:46:25 +0200

    ditto, Angler

--- units/amphaa.lua
+++ units/amphaa.lua
@@ -22,6 +22,8 @@ unitDef = {
   corpse                 = [[DEAD]],
 
   customParams           = {
+    amph_regen = 20,
+    amph_submerged_at = 40,
     description_pl = [[Amfibijny Bot Przeciwlotniczy]],
     helptext       = [[Angler is amphibious AA designed to counter the factory's nemesis - Raven. Two of them together can float to the surface and kill a single Raven.]],
     helptext_pl    = [[Angler to amfibijna jednostka przeciwlotnicza zaprojektowana, by likwidowac glowne lotnicze zagrozenie dla amfibii - bombowiec Raven. Dwa Anglery moga wyplynac na powierzchnie i zestrzelic pojedynczego Ravena.]],
