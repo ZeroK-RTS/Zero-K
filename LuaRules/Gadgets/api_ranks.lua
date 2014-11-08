ddb6330be8c38bfbb75b37571dd48272f30c9c49
sprunk <gnurps@o2.pl>
2014-11-08 02:13:49 +0100

    update rank api

--- LuaRules/Gadgets/api_ranks.lua
+++ LuaRules/Gadgets/api_ranks.lua
@@ -2,39 +2,60 @@ function gadget:GetInfo() return {
 	name      = "Ranks API",
 	desc      = "Handles unit ranks",
 	author    = "jK, rewritten by Sprung",
-	date      = "Nov 2014", -- "Dec 19, 2007",
+	date      = "Nov 2014", -- original: Dec 19, 2007
 	license   = "GNU GPL, v2 or later",
 	layer     = -math.huge,
 	enabled   = true,
 } end
 
-if not gadgetHandler:IsSyncedCode() then return end
-
-local floor = math.floor
-local spGetUnitRulesParam = Spring.GetUnitRulesParam
-local spSetUnitRulesParam = Spring.SetUnitRulesParam
-
 local XP_PER_RANK = 0.2 -- change to 0.1 once lasthit bonus gets removed
 
-Spring.SetExperienceGrade (0.0005) -- how often UnitExperience() is called (less is more often)
-Spring.SetGameRulesParam ("xp_per_rank", XP_PER_RANK) 
+local floor = math.floor
 
-GG.UnitRankUp = {}
+if gadgetHandler:IsSyncedCode() then
 
-function gadget:UnitCreated (unitID)
-	spSetUnitRulesParam (unitID, "rank", 0)
-end
+	local spSetUnitRulesParam = Spring.SetUnitRulesParam
 
-function gadget:UnitExperience (unitID, unitDefID, unitTeam, newxp, oldxp)
-	-- convert xp to rank
-	newxp = floor (newxp / XP_PER_RANK)
-	oldxp = floor (newxp / XP_PER_RANK)
+	Spring.SetExperienceGrade (0.0005) -- UnitExperience call frequency (less = more often)
+	Spring.SetGameRulesParam ("xp_per_rank", XP_PER_RANK)
 
-	if (newxp ~= oldxp) then
-		spSetUnitRulesParam (unitID, "rank", newxp)
+	GG.UnitRankUp = {}
 
-		for _,f in pairs (GG.UnitRankUp) do
-			f (unitID, unitDefID, unitTeam, newxp, oldxp)
+	local access_table = { inlos = true }
+
+	function gadget:UnitCreated (unitID)
+		spSetUnitRulesParam (unitID, "rank", 0, access_table)
+	end
+
+	function gadget:UnitExperience (unitID, unitDefID, unitTeam, newxp, oldxp)
+		newxp = floor (newxp / XP_PER_RANK)
+		oldxp = floor (oldxp / XP_PER_RANK)
+
+		if (newxp ~= oldxp) then
+			spSetUnitRulesParam (unitID, "rank", newxp, access_table)
+
+			for _,f in pairs (GG.UnitRankUp) do
+				f (unitID, unitDefID, unitTeam, newxp, oldxp)
+			end
+		end
+	end
+
+else
+
+	local spGetUnitLosState = Spring.GetUnitLosState
+	local spGetMyAllyTeamID = Spring.GetMyAllyTeamID
+	local spGetSpectatingState = Spring.GetSpectatingState
+
+	function gadget:UnitExperience (unitID, unitDefID, unitTeam, newxp, oldxp)
+		newxp = floor (newxp / XP_PER_RANK)
+		oldxp = floor (oldxp / XP_PER_RANK)
+
+		if (newxp ~= oldxp) then
+			local spec, specFullView = spGetSpectatingState()
+			local isInLos = spGetUnitLosState (unitID, spGetMyAllyTeamID()).los
+			if (Script.LuaUI.UnitRankUp and ((spec and specFullView) or isInLos)) then
+				Script.LuaUI.UnitRankUp (unitID)
+			end
 		end
 	end
 end
