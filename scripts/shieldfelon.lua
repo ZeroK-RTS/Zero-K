28f29e173eac09844c0f02ff4060f234d6f29ccf
sprunk <gnurps@o2.pl>
2014-11-09 18:19:49 +0100

    Felon script reads balance-related stuff from UD

--- scripts/shieldfelon.lua
+++ scripts/shieldfelon.lua
@@ -38,11 +38,11 @@ local gun_1 = 0
 --------------------------------------------------------------------------------
 -- constants
 --------------------------------------------------------------------------------
-local DRAIN = 75
+local DRAIN = tonumber (WeaponDefs[UnitDef.weapons[1].weaponDef].customParams.shield_drain)
 local SHIELD_RADIUS = 100
-local SPEED = 1.4
-local AIM_DELAY = 300
-local RESTORE_DELAY = 4000
+local SPEED = UnitDef.speed / 30
+local AIM_DELAY = tonumber (WeaponDefs[UnitDef.weapons[1].weaponDef].customParams.aim_delay)
+local RESTORE_DELAY = tonumber (WeaponDefs[UnitDef.weapons[1].weaponDef].customParams.aim_restore)
 
 --signals
 local SIG_WALK = 1
