73df538e1845594dcb772b90b5426783198f490d
sprunk <gnurps@o2.pl>
2014-11-11 12:58:54 +0100

    Explosion Spawn defs read from unitdefs

--- LuaRules/Configs/explosion_spawn_defs.lua
+++ LuaRules/Configs/explosion_spawn_defs.lua
@@ -1,30 +1,14 @@
--- $Id: explosion_spawn_defs.lua 4050 2009-03-09 02:56:38Z midknight $
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---Lists post-processing weapon names and the units to spawn when they go off
+local spawn_defs = {}
 
-local spawn_defs = {
-    corhurc_minebomb   = {name = "cormine_impulse", cost = 0, expire = 60},
-	armcent_droppod = {name = "armpw", cost=0, expire=0},
-	armpnix3_armadvbomb = {name = "armflea", cost=0, expire=0},
-	cormine_cortruck_missile = {name = "corareamine", cost=0, expire=0},
-	chicken_blimpy_dodobomb = {name = "chicken_dodo", cost=0, expire=30},
-	corgarp_mine = {name = "wolverine_mine", cost=0, expire=60},
-	hoverminer_mine = {name = "wolverine_mine", cost=0, expire=60},
-	zenith_meteor = {name = "asteroid_dead", cost=0, expire=0, feature = true},
+for i = 1, #WeaponDefs do
+	local cp = WeaponDefs[i].customParams
+	if cp.spawns_name then
+		spawn_defs[i] = {
+			name = cp.spawns_name,
+			expire = tonumber(cp.spawns_expire or 0),
+			feature = (cp.spawns_feature == 1),
+		}
+	end
+end
 
-	chickenflyerqueen_dodobomb = {name = "chicken_dodo", cost=0, expire=30},
-	chickenflyerqueen_basiliskbomb = {name = "chickenc", cost=0, expire=0},
-	chickenflyerqueen_tiamatbomb = {name = "chicken_tiamat", cost=0, expire=0},
-	chickenlandqueen_dodobomb = {name = "chicken_dodo", cost=0, expire=30},
-	chickenlandqueen_basiliskbomb = {name = "chickenc", cost=0, expire=0},
-	chickenlandqueen_tiamatbomb = {name = "chicken_tiamat", cost=0, expire=0},
-}
-
-local shieldCollide = { -- unitDefs as the shield hit callin is setup really strangely
-	corgarp = {damage = 220, gadgetDamage = 200}, -- gadgetDamage = damage - weapon default damage
-}
-
-return spawn_defs, shieldCollide
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+return spawn_defs
