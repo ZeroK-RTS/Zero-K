f09d3509803436ca0db360babeb478f29878f837
GoogleFrog <googl3frog@gmail.com>
2014-11-28 14:10:08 +1100

    Improved new engine prevent lab hax, stop preventing fixed hax.

--- LuaRules/Gadgets/unit_prevent_lab_hax.lua
+++ LuaRules/Gadgets/unit_prevent_lab_hax.lua
@@ -9,12 +9,12 @@ end
 function gadget:GetInfo()
 	return {
 		name      = "Prevent Lab Hax",
-		desc      = "Stops enemy units from entering labs. Blocks construction of structures in labs.",
+		desc      = "Stops enemy units from entering labs.",
 		author    = "Google Frog",
 		date      = "Jul 24, 2007", --May 11, 2013
 		license   = "GNU GPL, v2 or later",
 		layer     = 0,
-		enabled   = true  --  loaded by default?
+		enabled   = not (Game.version:find('91.0') == 1)  --  loaded by default?
 	}
 end
 
@@ -34,6 +34,7 @@ local spGetUnitIsStunned       = Spring.GetUnitIsStunned
 local spGetFeaturesInRectangle = Spring.GetFeaturesInRectangle
 local spGetFeaturePosition     = Spring.GetFeaturePosition
 local spSetFeaturePosition     = Spring.SetFeaturePosition
+local spSetFeatureVelocity     = Spring.SetFeatureVelocity
 local spMoveCtrlGetTag         = Spring.MoveCtrl.GetTag
 
 local abs = math.abs
@@ -50,15 +51,27 @@ local EXCEPTION_LIST = {
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local lab = {}
---_G.lab = lab
+local labs = {}
+local labList = {count = 0, data = {}}
+
+local function removeLab(unitID)
+	labs[labList.data[labList.count] ] = labs[unitID]
+	labList.data[labs[unitID]] = labList.data[labList.count]
+	labList.data[labList.count] = nil
+	labs[unitID] = nil
+	labList.count = labList.count - 1
+end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
-function checkLabs()
-	for Lid,Lv in pairs(lab) do  
-		local units = spGetUnitsInBox(Lv.minx-8, Lv.miny, Lv.minz-8, Lv.maxx+8, Lv.maxy, Lv.maxz+8)
-		local features = spGetFeaturesInRectangle(Lv.minx, Lv.minz, Lv.maxx, Lv.maxz)
+
+function checkLabs(checkFeatures)
+	local labData = labList.data
+	local data, units, features
+	for i = 1, labList.count do
+		data = labData[i]
+		units = spGetUnitsInBox(data.minx-8, data.miny, data.minz-8, data.maxx+8, data.maxy, data.maxz+8)
+		features = spGetFeaturesInRectangle(data.minx, data.minz, data.maxx, data.maxz)
 
 		for i=1,#units do
 			local unitID = units[i]
@@ -69,29 +82,29 @@ function checkLabs()
 			local ally = spGetUnitAllyTeam(unitID)
 			local team = spGetUnitTeam(unitID)
 			if not fly and spMoveCtrlGetTag(unitID) == nil then
-				if (ally ~= Lv.ally) then --teleport unit away
+				if (ally ~= data.ally) then --teleport unit away
 					local ux, _, uz, _,_,_, _, aimY  = spGetUnitPosition(unitID, true, true)
 
 					if aimY > -12 then
-						local l = abs(ux-Lv.minx)
-						local r = abs(ux-Lv.maxx)
-						local t = abs(uz-Lv.minz)
-						local b = abs(uz-Lv.maxz)
+						local l = abs(ux-data.minx)
+						local r = abs(ux-data.maxx)
+						local t = abs(uz-data.minz)
+						local b = abs(uz-data.maxz)
 
 						local side = min(l,r,t,b)
 
 						if (side == l) then
-							spSetUnitPosition(unitID, Lv.minx-8, uz, true)
+							spSetUnitPosition(unitID, data.minx-8, uz, true)
 						elseif (side == r) then
-							spSetUnitPosition(unitID, Lv.maxx+8, uz, true)
+							spSetUnitPosition(unitID, data.maxx+8, uz, true)
 						elseif (side == t) then
-							spSetUnitPosition(unitID, ux, Lv.minz-8, true)
+							spSetUnitPosition(unitID, ux, data.minz-8, true)
 						else
-							spSetUnitPosition(unitID, ux, Lv.maxz+8, true)
+							spSetUnitPosition(unitID, ux, data.maxz+8, true)
 						end
 
 					end		
-				elseif (team ~= Lv.team) and movetype then --order unit blocking ally factory to move away (only if it is not a structure)
+				elseif (team ~= data.team) and movetype then --order unit blocking ally factory to move away (only if it is not a structure)
 					local xVel,_,zVel = spGetUnitVelocity(unitID)
 					local stunned_or_inbuild = spGetUnitIsStunned(unitID)
 					if math.abs(xVel)<0.1 and math.abs(zVel)<0.1 and (not stunned_or_inbuild) then
@@ -104,119 +117,48 @@ function checkLabs()
 				end
 			end
 		end
-		for i=1,#features do
-			local featureID = features[i]
-			local fx, fy, fz = spGetFeaturePosition(featureID)
-			if fy > Lv.miny and fy < Lv.maxy then
-				local l = abs(fx-Lv.minx)
-				local r = abs(fx-Lv.maxx)
-				local t = abs(fz-Lv.minz)
-				local b = abs(fz-Lv.maxz)
+		if checkFeatures then
+			for i=1,#features do
+				local featureID = features[i]
+				local fx, fy, fz = spGetFeaturePosition(featureID)
+				if fy > data.miny and fy < data.maxy then
+					local l = abs(fx-data.minx)
+					local r = abs(fx-data.maxx)
+					local t = abs(fz-data.minz)
+					local b = abs(fz-data.maxz)
 
-				local side = min(l,r,t,b)
-
-				if (side == l) then
-					spSetFeaturePosition(featureID, Lv.minx, fy, fz, true)
-				elseif (side == r) then
-					spSetFeaturePosition(featureID, Lv.maxx, fy, fz, true)
-				elseif (side == t) then
-					spSetFeaturePosition(featureID, fx, fy, Lv.minz, true)
-				else
-					spSetFeaturePosition(featureID, fx, fy, Lv.maxz, true)
+					local side = min(l,r,t,b)
+					if (side == l) then
+						spSetFeaturePosition(featureID, data.minx, fy, fz, true)
+					elseif (side == r) then
+						spSetFeaturePosition(featureID, data.maxx, fy, fz, true)
+					elseif (side == t) then
+						spSetFeaturePosition(featureID, fx, fy, data.minz, true)
+					else
+						spSetFeaturePosition(featureID, fx, fy, data.maxz, true)
+					end
+					spSetFeatureVelocity(featureID, 0, 0, 0)
 				end
 			end
 		end
 	end
 end
 
--- http://springrts.com/mantis/view.php?id=2864
--- http://springrts.com/mantis/view.php?id=2870
-local function AllowUnitCreation(unitDefID, builderID, builderTeam, ux, uy, uz, facing) -- engine one does not have facing
-
-    local ud = UnitDefs[unitDefID]
-
-    if unitDefID ~= terraunitDefID and ud and (ud.isBuilding or ud.speed == 0) then
-    
-        local xsize = ud.xsize*4
-        local zsize = (ud.ysize or ud.zsize)*4
-        
-        local minx, maxx, minz, maxz
-		
-        if ((facing == 0) or (facing == 2))  then
-            
-			if xsize%16 == 0 then
-				ux = math.floor((ux+8)/16)*16
-			else
-				ux = math.floor(ux/16)*16+8
-			end
-
-			if zsize%16 == 0 then
-				uz = math.floor((uz+8)/16)*16
-			else
-				uz = math.floor(uz/16)*16+8
-			end
-			
-			minx = ux-xsize
-            minz = uz-zsize
-            maxx = ux+xsize
-            maxz = uz+zsize
-		else
-			if xsize%16 == 0 then
-				uz = math.floor((uz+8)/16)*16
-			else
-				uz = math.floor(uz/16)*16+8
-			end
-
-			if zsize%16 == 0 then
-				ux = math.floor((ux+8)/16)*16
-			else
-				ux = math.floor(ux/16)*16+8
-			end
-		
-		    minx = ux-zsize
-            minz = uz-xsize
-            maxx = ux+zsize
-            maxz = uz+xsize
-        end        
-		
-       	--Spring.MarkerAddLine(minx,0,minz,maxx,0,minz)
-		--Spring.MarkerAddLine(minx,0,minz,minx,0,maxz)
-		--Spring.MarkerAddLine(maxx,0,maxz,maxx,0,minz)
-		--Spring.MarkerAddLine(maxx,0,maxz,minx,0,maxz)
-		
-        for Lid,Lv in pairs(lab) do  
-            -- intersection of 2 rectangles
-            if Lv.minx < maxx and Lv.maxx > minx and Lv.minz < maxz and Lv.maxz > minz then
-                return false
-            end
-        end
-        
-    end
-    
-    return true
-end
-
 function gadget:UnitCreated(unitID, unitDefID,teamID)
-  
-	-- http://springrts.com/mantis/view.php?id=2871
-	local ux,_,uz,_, uy, _  = spGetUnitPosition(unitID, true)
-	local facing = spGetUnitBuildFacing(unitID)
-	if not AllowUnitCreation(unitDefID, nil, nil, ux, uy, uz, facing) then
-		Spring.DestroyUnit(unitID, false, true)
-		return
-	end
-	-- end 2871
-  
 	local ud = UnitDefs[unitDefID]
-	local name = ud.name
-	if (ud.isFactory == true) and not (EXCEPTION_LIST[name]) then
+	if (ud.isFactory == true) and not (EXCEPTION_LIST[ud.name]) then
 		local ux,_,uz,_, uy, _  = spGetUnitPosition(unitID, true)
 		if uy > -22 then
 			local face = spGetUnitBuildFacing(unitID)
 			local xsize = (ud.xsize)*4
 			local zsize = (ud.ysize or ud.zsize)*4
 			local ally = spGetUnitAllyTeam(unitID)
+			local minx, minz, maxx, maxz
 
+			local _,sizeY,_,_,offsetY = Spring.GetUnitCollisionVolumeData(unitID)
+			local miny = uy + offsetY - sizeY/2 --set the box bottom
+			local maxy = uy + offsetY + 150 --set the box height +200 elmo above the factory midpoint
+			
 			if ((face == 0) or (face == 2))  then
 				if xsize%16 == 0 then
 					ux = math.floor((ux+8)/16)*16
@@ -229,9 +171,11 @@ function gadget:UnitCreated(unitID, unitDefID,teamID)
 				else
 					uz = math.floor(uz/16)*16+8
 				end
-
-				lab[unitID] = { ally = ally, team=teamID, face = 0,
-				minx = ux-xsize+0.1, minz = uz-zsize+0.1, maxx = ux+xsize-0.1, maxz = uz+zsize-0.1}
+				
+				minx = ux-xsize+0.1
+				minz = uz-zsize+0.1
+				maxx = ux+xsize-0.1
+                maxz = uz+zsize-0.1
 			else
 				if xsize%16 == 0 then
 					uz = math.floor((uz+8)/16)*16
@@ -245,42 +189,55 @@ function gadget:UnitCreated(unitID, unitDefID,teamID)
 					ux = math.floor(ux/16)*16+8
 				end
 
-				lab[unitID] = { ally = ally, team=teamID, face = 1,
-				minx = ux-zsize+0.1, minz = uz-xsize+0.1, maxx = ux+zsize-0.1, maxz = uz+xsize-0.1}
+				minx = ux-zsize+0.1
+				minz = uz-xsize+0.1
+				maxx = ux+zsize-0.1
+				maxz = uz+xsize-0.1
 			end
+			
+			labList.count = labList.count + 1
+			labList.data[labList.count] = { 
+				ally = ally, 
+				team=teamID, 
+				face = 0,
+				minx = minx,
+				miny = miny,
+				minz = minz,
+				maxx = maxx,
+				maxy = maxy,
+				maxz = maxz,
+			}
+			labs[unitID] = labList.count
 
 			--Spring.Echo(xsize)
 			--Spring.Echo(zsize)
 
-			--Spring.MarkerAddLine(lab[unitID].minx,0,lab[unitID].minz,lab[unitID].maxx,0,lab[unitID].minz)
-			--Spring.MarkerAddLine(lab[unitID].minx,0,lab[unitID].minz,lab[unitID].minx,0,lab[unitID].maxz)
-			--Spring.MarkerAddLine(lab[unitID].maxx,0,lab[unitID].maxz,lab[unitID].maxx,0,lab[unitID].minz)
-			--Spring.MarkerAddLine(lab[unitID].maxx,0,lab[unitID].maxz,lab[unitID].minx,0,lab[unitID].maxz)
-
-			local _,sizeY,_,_,offsetY = Spring.GetUnitCollisionVolumeData(unitID)
-			lab[unitID].miny = uy + offsetY - sizeY/2 --set the box bottom
-			lab[unitID].maxy = uy + offsetY + 150 --set the box height +200 elmo above the factory midpoint
-
+			--Spring.MarkerAddLine(minx,0,minz,maxx,0,minz)
+			--Spring.MarkerAddLine(minx,0,minz,minx,0,maxz)
+			--Spring.MarkerAddLine(maxx,0,maxz,maxx,0,minz)
+			--Spring.MarkerAddLine(maxx,0,maxz,minx,0,maxz)
 		end
 	end
 
 end
 
 function gadget:UnitDestroyed(unitID, unitDefID)
-	if (lab[unitID]) then
-		lab[unitID] = nil
+	if (labs[unitID]) then
+		removeLab(unitID)
 	end
 end
 
 function gadget:UnitGiven(unitID, unitDefID,unitTeam)
-	if (lab[unitID]) then
-		lab[unitID].ally = spGetUnitAllyTeam(unitID)
-		lab[unitID].team = unitTeam
+	if (labs[unitID]) then
+		labList.data[lab[unitID]].ally = spGetUnitAllyTeam(unitID)
+		labList.data[lab[unitID]].team = unitTeam
 	end
 end
 
 function gadget:GameFrame(n)
-	checkLabs()
+	if n%2 == 0 then
+		checkLabs(n%30 == 0)
+	end
 end
 
 function gadget:Initialize()
