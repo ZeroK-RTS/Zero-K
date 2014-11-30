c683340a5983105fabf1472556eabaeec7aaa6e2
GoogleFrog <googl3frog@gmail.com>
2014-12-01 00:53:18 +1100

    VERSION{v1.2.12.1} Two fixes and the Integral Opacity change. Fix #401

--- LuaRules/Gadgets/unit_transport_speed.lua
+++ LuaRules/Gadgets/unit_transport_speed.lua
@@ -53,7 +53,6 @@ function gadget:UnitLoaded(unitID, unitDefID, unitTeam, transportID, transportTe
 			local speedFactor = math.min(1, 3 * mass[tudid]/(effectiveMass))
 			Spring.SetUnitRulesParam(transportID, "effectiveMass", effectiveMass)
 			Spring.SetUnitRulesParam(transportID, "selfMoveSpeedChange", speedFactor)
-			GG.attUnits[transportID] = true
 			GG.UpdateUnitAttributes(transportID)
 			
 			inTransport[unitID] = transportID
