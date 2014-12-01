8b2bf8f3db66d02e34e1b00d0ecb154b54799d99
GoogleFrog <googl3frog@gmail.com>
2014-12-01 22:32:31 +1100

    Probably fix dev engine swift speed.

--- LuaRules/Gadgets/unit_attributes.lua
+++ LuaRules/Gadgets/unit_attributes.lua
@@ -230,6 +230,7 @@ local function updateMovementSpeed(unitID, ud, speedFactor, turnAccelFactor, max
 				maxAcc          = state.origMaxAcc      *maxAccelerationFactor, --(speedFactor > 0.001 and speedFactor or 0.001)
 			}
 			spSetAirMoveTypeData (unitID, attribute)
+			spSetAirMoveTypeData (unitID, attribute)
 		elseif state.movetype == 1 then
 			local attribute =  {
 				maxSpeed        = state.origSpeed       *speedFactor,
