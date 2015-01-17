2ebc98987cb606f1c216d8392149eba05989d150
GoogleFrog <googl3frog@gmail.com>
2015-01-17 14:58:12 +1100

    Enabled 91.0 tactical AI for all versions as a safety measure.

--- LuaRules/Gadgets/unit_tactical_ai.lua
+++ LuaRules/Gadgets/unit_tactical_ai.lua
@@ -7,7 +7,7 @@ function gadget:GetInfo()
 	date	= "April 20 2010",
 	license	= "GNU GPL, v2 or later",
 	layer	= 0,
-	enabled = not (Game.version:find('91.0') == 1),
+	enabled = false --not (Game.version:find('91.0') == 1),
   }
 end
 
