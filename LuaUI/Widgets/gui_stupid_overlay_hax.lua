12f8c3ea1e769b95934ddae01914df3502c69db5
GoogleFrog <googl3frog@gmail.com>
2015-03-02 22:42:30 +1100

    Added a hacky widget to deal with the slow transition from height/path overlay to LOS overlay. Not used as it needs zk_keys changes.

---
+++ LuaUI/Widgets/gui_stupid_overlay_hax.lua
@@ -0,0 +1,57 @@
+function widget:GetInfo()
+  return {
+    name      = "Stupid Overlay Hax",
+    desc      = "Handles overlay transitions because the slow transition is annoying.",
+    author    = "GoogleFrog",
+    date      = "2nd March 2015",
+    license   = "GNU GPL, v2 or later",
+    layer     = 0,
+    enabled   = false,
+  }
+end
+
+local heightMapEnabled = false
+local pathMapEnabled = false
+local losMapEnabled = false
+
+function WG.Overlay_ToggleHeightMap()
+	if heightMapEnabled then
+		Spring.SendCommands('showelevation')
+		if losMapEnabled then
+			Spring.SendCommands('togglelos')
+		end
+		heightMapEnabled = false
+	else
+		if losMapEnabled then
+			Spring.SendCommands('togglelos')
+		end
+		Spring.SendCommands('showelevation')
+		heightMapEnabled = true
+	end
+end
+
+function WG.Overlay_TogglePathMap()
+	if pathMapEnabled then
+		Spring.SendCommands('showpathtraversability')
+		if losMapEnabled then
+			Spring.SendCommands('togglelos')
+		end
+		pathMapEnabled = false
+	else
+		if losMapEnabled then
+			Spring.SendCommands('togglelos')
+		end
+		Spring.SendCommands('showpathtraversability')
+		pathMapEnabled = true
+	end
+end	
+
+function WG.Overlay_ToggleLOS()
+	if heightMapEnabled or pathMapEnabled then
+		heightMapEnabled = false
+		pathMapEnabled = false
+	else
+		losMapEnabled = not losMapEnabled
+	end
+	Spring.SendCommands('togglelos')
+end
