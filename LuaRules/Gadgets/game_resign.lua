0379d924926247d0ceddba52a5f46ffe561919bd
sprunk <gnurps@o2.pl>
2015-03-20 16:11:26 +0100

    resign made sane
    Now it's handled on reception instead of being redirected through unsynced action handler back to synced. This means:
    * rights check happens in the gadget (and not in the gadget handler) as sender is now known (action handler discarded that info).
    * not going through unsynced means voteresign is instant even if the target is lagging.

--- LuaRules/Gadgets/game_resign.lua
+++ LuaRules/Gadgets/game_resign.lua
@@ -1,52 +1,52 @@
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+function gadget:GetInfo() return {
+	name    = "Resign Gadget",
+	desc    = "Resign stuff",
+	author  = "KingRaptor",
+	date    = "2012.5.1",
+	license = "Public domain",
+	layer   = 0,
+	enabled = true,
+} end
 
-function gadget:GetInfo()
-	return {
-		name      = "Resign Gadget",
-		desc      = "Resign stuff",
-		author    = "KingRaptor",
-		date      = "2012.5.1",
-		license   = "Public domain",
-		layer     = 0,
-		enabled   = true  --  loaded by default?
-	}
-end
+if (not gadgetHandler:IsSyncedCode()) then return end
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-  
-if (gadgetHandler:IsSyncedCode()) then
---------------------------------------------------------------------------------
--- synced
---------------------------------------------------------------------------------
+local spGetPlayerInfo = Spring.GetPlayerInfo
+local spKillTeam = Spring.KillTeam
+local spSetTeamRulesParam = Spring.SetTeamRulesParam
+local spGetPlayerList = Spring.GetPlayerList
 
-function gadget:RecvLuaMsg(msg, playerID)
+function gadget:RecvLuaMsg (msg, senderID)
 	if msg == "forceresign" then
-		local team = select(4, Spring.GetPlayerInfo(playerID))
-		Spring.KillTeam(team)
-		Spring.SetTeamRulesParam(team, "WasKilled", 1)
+		local team = select(4, spGetPlayerInfo(senderID))
+		spKillTeam(team)
+		spSetTeamRulesParam(team, "WasKilled", 1)
 	end
 end
 
-else
---------------------------------------------------------------------------------
--- unsynced
---------------------------------------------------------------------------------
+function gadget:GotChatMsg (msg, senderID)
+	if (string.find(msg, "resignteam") == 1) then
 
-local function Resign(_, name)
-	local playerID = Spring.GetMyPlayerID()
-	local myName = Spring.GetPlayerInfo(playerID)
-	if name == myName then
-		--Spring.SendCommands('spectator')
-		Spring.SendLuaRulesMsg("forceresign")
+		local allowed = false
+		if (senderID == 255) then -- Springie
+			allowed = true
+		else
+			local playerkeys = select (10, spGetPlayerInfo(senderID))
+			if (playerkeys and playerkeys.admin and (playerkeys.admin == "1")) then
+				allowed = true
+			end
+		end
+		if not allowed then return end
+
+		local target = string.sub(msg, 12)
+		local people = spGetPlayerList()
+		for i = 1, #people do
+			local personID = people[i]
+			local nick, _, _, teamID = spGetPlayerInfo(personID)
+			if (target == nick) then
+				spKillTeam (teamID)
+				spSetTeamRulesParam (teamID, "WasKilled", 1)
+				return
+			end
+		end
 	end
 end
-
-function gadget:Initialize()
-	gadgetHandler:AddChatAction('resignteam', Resign, " resigns the player with the specified name")
-end
-
-end
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
