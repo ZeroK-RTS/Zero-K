861c433bed2be10b44f0d49352bfa1ac26795ee2
sprunk <gnurps@o2.pl>
2015-05-15 04:07:54 +0200

    Empty Surfboard transportable.

--- LuaRules/Gadgets/unit_conditional_transportability.lua
+++ LuaRules/Gadgets/unit_conditional_transportability.lua
@@ -2,32 +2,46 @@ function gadget:GetInfo() return {
 	name    = "Conditional Transportability",
 	desc    = "Allows units to only be transportable in some situations.",
 	author  = "sprung",
-	date    = "17/11/13",
+	date    = "15-05-15",
 	license = "PD",
 	layer   = 0,
-	enabled = false
+	enabled = true
 } end
 
 if (not gadgetHandler:IsSyncedCode()) then return end
 
-local surfboardDefID = UnitDefNames["armtboat"].id
-
 function gadget:AllowCommand (unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
 	local numParams = #cmdParams
-    if (cmdID == CMD.INSERT and cmdParams[2]) then
-		cmdID = cmdParams[2]
-		cmdParams[1] = cmdParams[4] -- this only supports commands with 1 param
-		numParams = numParams - 3
-    end
 
-	if ((cmdID == CMD.LOAD_ONTO) and (unitDefID == surfboardDefID)) then -- surfboard can't ask to embark
-		return false
-	elseif ((cmdID == CMD.LOAD_UNITS)
-	and (numParams == 1) -- 1 for load specific unit; 3 for area load
-	and Spring.ValidUnitID(cmdParams[1])
-	and Spring.GetUnitDefID(cmdParams[1]) == surfboardDefID) then
-		return false
-	else
-		return true
+	if (cmdID == CMD.INSERT and cmdParams[2]) then
+		cmdID = cmdParams[2]
+		cmdParams[1] = cmdParams[4]
+		numParams = numParams - 3
 	end
+
+	if ((cmdID == CMD.LOAD_ONTO)
+	and (Spring.GetUnitRulesParam(unitID, "untransportable") == 1)) then
+		return false
+	end
+
+	if  (cmdID == CMD.LOAD_UNITS)
+	and (numParams == 1) -- only block single-target load (not area load)
+	and (Spring.GetUnitRulesParam(cmdParams[1], "untransportable") == 1) then
+		return false
+	end
+
+	return true
+end
+
+function gadget:UnitLoaded(unitID, unitDefID, unitTeam, transportID, transportTeam)
+	Spring.SetUnitRulesParam(transportID, "untransportable", 1)
+
+	if (Spring.GetUnitRulesParam(unitID, "untransportable") == 1) then
+		-- there is no way to prevent load: allow it and immediately drop instead
+		Spring.UnitScript.CallAsUnit(transportID, Spring.UnitScript.GetScriptEnv(transportID).ForceDropUnit, true)
+	else
+end
+
+function gadget:UnitUnloaded(unitID, unitDefID, unitTeam, transportID, transportTeam)
+	Spring.SetUnitRulesParam(transportID, "untransportable", 0)
 end
