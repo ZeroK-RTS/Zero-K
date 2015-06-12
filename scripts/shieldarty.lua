ff3608a8ea2f20dc2889ddc12db2a8198991b25b
lhog <ccna@mail.ru>
2015-06-12 22:00:24 +0300

    Reverted back racketeer's disarmTimer

--- scripts/shieldarty.lua
+++ scripts/shieldarty.lua
@@ -136,8 +136,8 @@ function script.FireWeapon1()
 end
 
 function script.BlockShot(num, targetID)	
-	--return GG.OverkillPrevention_CheckBlockD(unitID, targetID, 1500, 90, 240) --3 seconds - timeout, 8 seconds - disarmTimer
-	return GG.OverkillPrevention_CheckBlockD(unitID, targetID, 1500, 120, 600) --4 seconds - timeout, 20 seconds - disarmTimer
+	return GG.OverkillPrevention_CheckBlockD(unitID, targetID, 1500, 120, 240) --4 seconds - timeout, 8 seconds - disarmTimer
+	--return GG.OverkillPrevention_CheckBlockD(unitID, targetID, 1500, 120, 600) --4 seconds - timeout, 20 seconds - disarmTimer
 end
 
 function script.Killed(recentDamage, maxHealth)
