cba54767803e27dcf9181933f859227feaa57721
GoogleFrog <googl3frog@gmail.com>
2015-06-16 01:48:10 +1000

    Jumpjet units are now able to jump onto structures and features. Fixes #840.

--- LuaRules/Gadgets/unit_jumpjets.lua
+++ LuaRules/Gadgets/unit_jumpjets.lua
@@ -41,6 +41,7 @@ local spSetUnitNoSelect    = Spring.SetUnitNoSelect
 local spSetUnitBlocking    = Spring.SetUnitBlocking
 local spSetUnitMoveGoal    = Spring.SetUnitMoveGoal
 local spGetGroundHeight    = Spring.GetGroundHeight
+local spGetGroundNormal    = Spring.GetGroundNormal
 local spTestMoveOrder      = Spring.TestMoveOrder
 local spTestBuildOrder     = Spring.TestBuildOrder
 local spGetGameSeconds     = Spring.GetGameSeconds
@@ -69,6 +70,8 @@ local landBoxSize = 60
 local jumps = {}
 local jumping = {}
 local goalSet = {}
+local structureCollision = {}
+local structureValues = {}
 
 local quiteNew = Spring.Utilities.IsCurrentVersionNewerThan(95, 0)
 
@@ -98,15 +101,99 @@ local function spTestMoveOrderX(unitDefID, x, y, z)
 end
 
 
-local function GetDist3(a, b)
-	local x, y, z = (a[1] - b[1]), (a[2] - b[2]), (a[3] - b[3])
-	return (x*x + y*y + z*z)^0.5
+local function GetLandStructureCheckValues(x, z, myRadius)
+	-- The largest structure radius is 60 elmos (mahlazer)
+	local units = Spring.GetUnitsInCylinder(x,z, 60 + myRadius)
+	
+	local smallestDistance = 60 + myRadius
+	local structureID = false
+	local smallestObjectRadius = 0
+	
+	-- Find the nearby structure which most likely caused the jump
+	-- to be a structure jump.
+	local spGetUnitRadius = Spring.GetUnitRadius
+	
+	for i = 1, #units do
+		local unitID = units[i]
+		local unitDefID = spGetUnitDefID(unitID)
+		-- Only check immobile units
+		if UnitDefs[unitDefID] and UnitDefs[unitDefID].isImmobile then
+			local radius = spGetUnitRadius(unitID)
+			local sx, sy, sz = spGetUnitPosition(unitID)
+			
+			-- If the unit overlaps the center then distance is negative
+			local dist = math.sqrt((sx - x)^2 + (sz - z)^2) - radius
+			if dist < smallestDistance then
+				structureID = unitID
+				smallestDistance = dist
+				smallestObjectRadius = radius
+			end
+		end
+	end
+	
+	local featureID = false
+	if smallestDistance > myRadius then
+		-- No sufficiently close structure was found, 
+		-- check whether the blocking was due to a feature.
+		local features = Spring.GetFeaturesInCylinder(x,z, 60 + myRadius)
+		
+		local spGetFeatureBlocking = Spring.GetFeatureBlocking
+		local spGetFeatureRadius   = Spring.GetFeatureRadius
+		local spGetFeaturePosition = Spring.GetFeaturePosition
+		
+		local featureDist = 60 + myRadius
+		local featureRadius = 0
+		
+		for i = 1, #features do
+			local fid = features[i]
+			local blocking = spGetFeatureBlocking(fid)
+			if blocking then
+				local radius = spGetFeatureRadius(fid)
+				local sx, sy, sz = spGetFeaturePosition(fid)
+				
+				local dist = math.sqrt((sx - x)^2 + (sz - z)^2) - radius
+				if dist < featureDist then
+					featureID = fid
+					featureDist = dist
+					featureRadius = radius
+				end
+			end
+		end
+		
+		-- Replace closest structure with feature if the feature is closer
+		if featureID and featureDist < smallestDistance then
+			structureID = false
+			smallestObjectRadius = featureRadius
+		end
+	end
+	
+	if structureID then
+		-- Mid position, not base position.
+		local _, _, _, sx, sy, sz = spGetUnitPosition(structureID, true)
+		return {x = sx, y = sy, z = sz, distSq = (smallestObjectRadius + myRadius)^2}
+	elseif featureID then
+		local _, _, _, sx, sy, sz = Spring.GetFeaturePosition(featureID, true)
+		return {x = sx, y = sy, z = sz, distSq = (smallestObjectRadius + myRadius)^2}
+	end
+	
+	return false
+end
+
+local function CheckStructureCollision(unitID, x, y, z)
+	local val = structureValues[unitID]
+	return (val.x - x)^2 + (val.y - y)^2 + (val.z - z)^2 < val.distSq
 end
 
 local function GetDist2Sqr(a, b)
 	local x, z = (a[1] - b[1]), (a[3] - b[3])
 	return (x*x + z*z)
 end
+
+local function GetDist3(a, b)
+	local x, y, z = (a[1] - b[1]), (a[2] - b[2]), (a[3] - b[3])
+	return (x*x + y*y + z*z)^0.5
+end
+
 
 local function Approach(unitID, cmdParams, range)
 	spSetUnitMoveGoal(unitID, cmdParams[1],cmdParams[2],cmdParams[3], range)
@@ -118,18 +205,18 @@ local function StartScript(fn)
 end
 
 local function Jump(unitID, goal, cmdTag, origCmdParams)
-	goal[2]						 = spGetGroundHeight(goal[1],goal[3])
-	local start				 = {spGetUnitPosition(unitID)}
+	goal[2]                = spGetGroundHeight(goal[1],goal[3])
+	local start            = {spGetUnitPosition(unitID)}
 
 	local fakeUnitID
-	local unitDefID		 = spGetUnitDefID(unitID)
-	local jumpDef			 = jumpDefs[unitDefID]
-	local speed				 = jumpDef.speed
-	local delay				= jumpDef.delay
-	local height				= jumpDef.height
-	local cannotJumpMidair		= jumpDef.cannotJumpMidair
-	local reloadTime		= (jumpDef.reload or 0)*30
-	local teamID				= spGetUnitTeam(unitID)
+	local unitDefID	       = spGetUnitDefID(unitID)
+	local jumpDef          = jumpDefs[unitDefID]
+	local speed	           = jumpDef.speed
+	local delay	           = jumpDef.delay
+	local height           = jumpDef.height
+	local cannotJumpMidair = jumpDef.cannotJumpMidair
+	local reloadTime       = (jumpDef.reload or 0)*30
+	local teamID           = spGetUnitTeam(unitID)
 	
 	if cannotJumpMidair and abs(spGetGroundHeight(start[1],start[3]) - start[2]) > 1 then
 		return false, true
@@ -137,7 +224,7 @@ local function Jump(unitID, goal, cmdTag, origCmdParams)
 	
 	local rotateMidAir	= jumpDef.rotateMidAir
 	local env
-
+	
 	local vector = {goal[1] - start[1],
 					goal[2] - start[2],
 					goal[3] - start[3]}
@@ -221,10 +308,13 @@ local function Jump(unitID, goal, cmdTag, origCmdParams)
 			end
 		end
 	
+		local hitStructure
 		local halfJump
 		local i = 0
 		while i <= 1 do
-			if (not Spring.ValidUnitID(unitID) or Spring.GetUnitIsDead(unitID)) then return end
+			if (not Spring.ValidUnitID(unitID) or Spring.GetUnitIsDead(unitID)) then 
+				return 
+			end
 
 			local x0, y0, z0 = spGetUnitPosition(unitID)
 			local x = start[1] + vector[1]*i
@@ -241,7 +331,20 @@ local function Jump(unitID, goal, cmdTag, origCmdParams)
 			if (not halfJump and i > 0.5) then
 				Spring.UnitScript.CallAsUnit(unitID,env.halfJump)
 				halfJump = true
+				
+				-- Do structure collision here to prevent early collision (perhaps a rejump onto the same structure?)
+				if structureCollision[unitID] then
+					local radius = Spring.GetUnitRadius(unitID)
+					structureValues[unitID] = GetLandStructureCheckValues(goal[1], goal[3], radius)
+					structureCollision[unitID] = nil
+				end
 			end
+			
+			if halfJump and structureValues[unitID] and CheckStructureCollision(unitID, x, y, z) then
+				hitStructure = {x, y, z}
+				break
+			end
+			
 			Sleep()
 			--[[ Slow damage
 			local slowMult = 1-(Spring.GetUnitRulesParam(unitID, "slowState") or 0)
@@ -254,6 +357,7 @@ local function Jump(unitID, goal, cmdTag, origCmdParams)
 		local jumpEndTime = spGetGameSeconds()
 		lastJumpPosition[unitID] = origCmdParams
 		jumping[unitID] = nil
+		structureCollision[unitID] = nil
 		SetLeaveTracks(unitID, true)
 		spSetUnitVelocity(unitID, 0, 0, 0)
 		Spring.SetUnitRulesParam(unitID, "is_jumping", 0)
@@ -262,6 +366,15 @@ local function Jump(unitID, goal, cmdTag, origCmdParams)
 		if Spring.ValidUnitID(unitID) and (not Spring.GetUnitIsDead(unitID)) then
 			spGiveOrderToUnit(unitID,CMD_WAIT, {}, {})
 			spGiveOrderToUnit(unitID,CMD_WAIT, {}, {})
+		end
+		
+		if hitStructure then
+			-- Add unstick impulse to make the jumper bound on the structure.
+			Spring.AddUnitImpulse(unitID, 3,0,3)
+			Spring.AddUnitImpulse(unitID, 0,-0.5,0)
+			Spring.AddUnitImpulse(unitID, -3,0,-3)
+			-- Set position because the unit may snap to the ground.
+			Spring.SetUnitPosition(unitID, hitStructure[1], hitStructure[2], hitStructure[3])
 		end
 
 		Sleep()
@@ -342,6 +455,7 @@ end
 function gadget:UnitDestroyed(oldUnitID, unitDefID)
 	if jumping[oldUnitID] then
 		jumping[oldUnitID] = nil --empty old unit's data
+		structureCollision[oldUnitID] = nil
 	end
 end
 
@@ -363,9 +477,31 @@ function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOpt
 		return true
 	end
 	
-	if cmdID == CMD_JUMP and 
-			not spTestMoveOrderX(unitDefID, cmdParams[1], cmdParams[2], cmdParams[3]) then
-		return false
+	if cmdID == CMD_JUMP then
+		if spTestMoveOrderX(unitDefID, cmdParams[1], cmdParams[2], cmdParams[3]) then
+			return true
+		else
+			-- Check whether the terrain is the source of the blockage. If it is not then
+			-- conclude that the blockage is a structure.
+			local height = spGetGroundHeight(cmdParams[1],cmdParams[3])
+			if (not UnitDefs[unitDefID]) or height < -UnitDefs[unitDefID].maxWaterDepth then
+				-- Water too deep for the unit to walk on
+				return false
+			end
+			
+			local normal = select(2, spGetGroundNormal(cmdParams[1],cmdParams[3]))
+			-- Most of the time the normal will be close to 1 because structures are built on
+			-- flat ground. This check captures high slope tolerance things such as Windgens and
+			-- small turrets.
+			if normal < 0.6 then
+				 -- Ground is too steep for bots to walk on.
+				return false
+			end
+			
+			-- Ground is fine, must contain a blocking structure.
+			structureCollision[unitID] = true
+			return true
+		end
 	end
 	if goalSet[unitID] then
 		goalSet[unitID] = nil
