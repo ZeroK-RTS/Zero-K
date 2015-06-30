24f146f2e21891a087db2d2b790dee457c697b61
Sgathec <alcur.sphiloth@gmail.com>
2015-06-30 20:49:04 +0300

    Autorepair activation time reduced to 0 seconds.

--- gamedata/modularcomms/moduledefs.lua
+++ gamedata/modularcomms/moduledefs.lua
@@ -459,7 +459,7 @@ upgrades = {
 					unitDef.idleautoheal = 0
 				end
 				unitDef.idleautoheal = unitDef.idleautoheal + 20
-				unitDef.idletime = 300
+				unitDef.idletime = 0
 			end,
 	},
 	module_companion_drone = {
