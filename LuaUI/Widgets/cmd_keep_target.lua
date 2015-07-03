ca02ba86076d6cb6959631fd90c9cc4dda0af509
KlonZK <alexander.miethaner@t-online.de>
2015-07-03 20:55:19 +0200

    undo latest changes

--- LuaUI/Widgets/cmd_keep_target.lua
+++ LuaUI/Widgets/cmd_keep_target.lua
@@ -10,8 +10,6 @@ function widget:GetInfo()
   }
 end
 
-local CMD_UNIT_SET_TARGET = 34923
-local CMD_UNIT_CANCEL_TARGET = 34924
 
 local unitsWithWeakTargetLink = {}
 
@@ -19,20 +17,40 @@ local function isValidType(ud)
 	return ud and not (ud.isBomber or ud.isFactory)
 end
 
-function widget:CommandNotify(id, params, options)  
-	if id == CMD.SET_WANTED_MAX_SPEED then
-        return false -- FUCK CMD.SET_WANTED_MAX_SPEED what?
-    end
+local function isValidUnit(unitID)
+	local unitDefID = Spring.GetUnitDefID(unitID)
+	if unitDefID and Spring.ValidUnitID(unitID) then
+		local ud = UnitDefs[unitDefID]
+		return ud and not (ud.isBomber or ud.isFactory)
+	end
+	return false
+end
+
+function widget:UnitCmdDone(unitID, unitDefID, unitTeam, cmdID, cmdTag, cmdParams, cmdOpts)
+	
+end
+
+function widget:CommandNotify(id, params, options)  	
+	if (options.internal) then return false end
+	
+	local units = Spring.GetSelectedUnits()
+	for i = 1, #units do
+		local unitID = units[i]			
+		local qq = Spring.GetUnitCommands(unitID)
+		if (#qq>0) then Spring.Echo(qq[1].tag)
+		Spring.Echo()
+	end
+		
 	if id == CMD.ATTACK then
 		local units = Spring.GetSelectedUnits()
 		for i = 1, #units do
-			local unitID = units[i]
-			local unitDefID = Spring.GetUnitDefID(unitID)
-			local ud = UnitDefs[unitDefID]
-			if isValidType(ud) and Spring.ValidUnitID(unitID) then
-				if #params == 1 and not options.internal then
-					Spring.GiveOrderToUnit(unitID,CMD_UNIT_SET_TARGET,params,{})
-					unitsWithWeakTargetLink[unitID] = true
+			local unitID = units[i]			
+			if isValidUnit(unitID) then
+				if #params == 1 then					
+					--Spring.GiveOrderToUnit(unitID,CMD_UNIT_SET_TARGET,params,{CMD.OPT_INTERNAL})
+					--Spring.GiveOrderToUnit(UnitID,CMD.INSERT,
+					--	{,CMD_UNIT_SET_TARGET, {CMD.OPT_SHIFT,CMD.OPT_INTERNAL}, params},{unitID})
+					--unitsWithWeakTargetLink[unitID] = true
 				end
 			end
 		end
