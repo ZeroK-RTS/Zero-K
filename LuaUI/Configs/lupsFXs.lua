2bfab7749351e33929f63e6f5d21fd59437633e4
L.J. Lim <kingraptorowns@gmail.com>
2015-07-18 17:17:07 +0800

    Fix mex glow position

--- LuaUI/Configs/lupsFXs.lua
+++ LuaUI/Configs/lupsFXs.lua
@@ -413,7 +413,7 @@ cormexGlow = {
   piece       = "furnace",
 
   partpos = "x,y,z|"..
-	"y = ((i>3) and 59) or 49,"..
+	"y = ((i>3) and 10) or 0,"..
 	"local i = i%4,"..
 	"local d = 5,"..
 	"x = ((i>1) and -d) or d,"..
