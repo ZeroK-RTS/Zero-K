dbb95ae34ad5bcdcf0b8ce2f7d7843e2191ba6d7
wr <wagonrepairer@gmail.com>
2015-09-09 19:14:13 -0700

    Fix for facpanel.

--- LuaUI/Widgets/gui_chili_facpanel.lua
+++ LuaUI/Widgets/gui_chili_facpanel.lua
@@ -932,7 +932,9 @@ function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
 	local bdid = builderID and Spring.GetUnitDefID(builderID)
     if UnitDefs[bdid] and UnitDefs[bdid].isFactory then
 		local i = facsByUnitId[builderID]
-		updateQSoon[i] = true
+		if i then
+			updateQSoon[i] = true
+		end
 	end
 end
 
