ab49a0109346f7469957f55b354481d278c57c02
GoogleFrog <googl3frog@gmail.com>
2015-09-12 12:54:39 +1000

    Fixed #999. Jumping out of the water is now blocked.  * Of course, it is not blocked for Recon Comm.  * Spring.TestMoveOrder appears broken, it always returns true for flat underwater locations (whether they have a structure on the location or not). Luckily, the severity is moderated by friendly-piercing torpedoes.

--- LuaRules/Gadgets/unit_jumpjets.lua
+++ LuaRules/Gadgets/unit_jumpjets.lua
@@ -92,6 +92,7 @@ local jumpCmdDesc = {
 
 local function spTestMoveOrderX(unitDefID, x, y, z)
 	if quiteNew then
+		-- Note that spTestMoveOrder returns true for jumping underwater.
 		return spTestMoveOrder(unitDefID, x, y, z, 0, 0, 0, true, true, true)
 	else
 		return spTestBuildOrder(unitDefID, x, y, z, 1)
@@ -205,6 +206,8 @@ local function Jump(unitID, goal, cmdTag, origCmdParams)
 	goal[2]                = spGetGroundHeight(goal[1],goal[3])
 	local start            = {spGetUnitPosition(unitID)}
 
+	local startHeight      = spGetGroundHeight(start[1],start[3])
+	
 	local fakeUnitID
 	local unitDefID	       = spGetUnitDefID(unitID)
 	local jumpDef          = jumpDefs[unitDefID]
@@ -215,7 +218,7 @@ local function Jump(unitID, goal, cmdTag, origCmdParams)
 	local reloadTime       = (jumpDef.reload or 0)*30
 	local teamID           = spGetUnitTeam(unitID)
 	
-	if cannotJumpMidair and abs(spGetGroundHeight(start[1],start[3]) - start[2]) > 1 then
+	if (cannotJumpMidair and abs(startHeight - start[2]) > 1) or (startHeight < -UnitDefs[unitDefID].maxWaterDepth) then
 		return false, true
 	end
 	
@@ -484,11 +487,7 @@ function gadget:AllowCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOpt
 		else
 			-- Check whether the terrain is the source of the blockage. If it is not then
 			-- conclude that the blockage is a structure.
-			local height = spGetGroundHeight(cmdParams[1],cmdParams[3])
-			if (not UnitDefs[unitDefID]) or height < -UnitDefs[unitDefID].maxWaterDepth then
-				-- Water too deep for the unit to walk on
-				return false
-			end
+			-- Jumping into water is allowed.
 			
 			local normal = select(2, spGetGroundNormal(cmdParams[1],cmdParams[3]))
 			-- Most of the time the normal will be close to 1 because structures are built on
