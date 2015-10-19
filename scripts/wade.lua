fec417fc546e5d340e6d53cc09159ecb54312907
Anarchid <anarchid@mail.ru>
2015-10-20 00:20:38 +0300

    script-and-globals powered wade-wakes on grizzly

---
+++ scripts/wade.lua
@@ -0,0 +1,36 @@
+inWater = false
+isMoving = false
+
+function script.setSFXoccupy (terrainType)
+	Spring.Echo("SetSFXOccupy: "..terrainType);
+	local nowInWater = (terrainType == 2 or terrainType == 1)
+	
+	if nowInWater then
+		if not inWater and isMoving then
+			StartThread(WADE);
+		end
+	else
+		Signal(SIG_WADE)
+	end
+	inWater = nowInWater
+end
+
+function WADE()
+	local spGetUnitPosition = Spring.GetUnitPosition
+	local maxWadeDepth = -Spring.GetUnitHeight(unitID)
+	
+	if not (WADE_PIECE and WADE_PIECE[1]) then 
+		return 
+	end
+	
+	SetSignalMask(SIG_WAKE);
+	
+	while true do
+		local x,y,z = spGetUnitPosition(unitID)
+		if (y > maxWadeDepth) then -- emit wakes only when not completely submerged
+			EmitSfx(WADE_PIECE[math.random(1,#WADE_PIECE)], WADE_SFX)
+		end
+		Sleep(math.random(5,10)*33)
+	end
+end
+
