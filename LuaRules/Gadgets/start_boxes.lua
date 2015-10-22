d587875625122c3fe254cdf86b7f10653f79dbab
sprunk <gnurps@o2.pl>
2015-10-22 03:49:43 +0200

    Startbox API improvements: * add GetTeamCount() which is callable from the config and returns the real number of allyteams (Spring.GetAllyTeamList not usable because of empty teams and Gaia). * fix clockwise triangles not working.

--- LuaRules/Gadgets/start_boxes.lua
+++ LuaRules/Gadgets/start_boxes.lua
@@ -16,8 +16,38 @@ local startboxConfig
 local manualStartposConfig
 local mapsideBoxes = "mapconfig/map_startboxes.lua"
 
+local function cross_product (px, pz, ax, az, bx, bz)
+	return ((px - bx)*(az - bz) - (ax - bx)*(pz - bz))
+end
+
+function GetTeamCount()
+	local gaiaAllyTeamID = select(6, Spring.GetTeamInfo(Spring.GetGaiaTeamID()))
+	local allyTeamList = Spring.GetAllyTeamList()
+	local actualAllyTeamList = {}
+	for i = 1, #allyTeamList do
+		local teamList = Spring.GetTeamList(allyTeamList[i]) or {}
+		if ((#teamList > 0) and (allyTeamList[i] ~= gaiaAllyTeamID)) then
+			actualAllyTeamList[#actualAllyTeamList+1] = allyTeamList[i]
+		end
+	end
+	return #actualAllyTeamList
+end
+
 if VFS.FileExists (mapsideBoxes) then
 	startboxConfig, manualStartposConfig = VFS.Include (mapsideBoxes)
+	for id, box in pairs(startboxConfig) do
+		for i = 1, #box do
+			local conf = box[i]
+			if cross_product(conf[5], conf[6], conf[1], conf[2], conf[3], conf[4]) > 0 then
+				local temp = conf[5]
+				conf[5] = conf[3]
+				conf[3] = temp
+				temp = conf[6]
+				conf[6] = conf[4]
+				conf[4] = temp
+			end
+		end
+	end
 else
 	startboxConfig = { }
 	local startboxString = Spring.GetModOptions().startboxes
@@ -107,10 +137,6 @@ function gadget:Initialize()
 			end
 		end
 	end
-end
-
-local function cross_product (px, pz, ax, az, bx, bz)
-	return ((px - bx)*(az - bz) - (ax - bx)*(pz - bz))
 end
 
 local function CheckStartbox (boxID, x, z)
--- LuaUI/Widgets/api_startboxes.lua
+++ LuaUI/Widgets/api_startboxes.lua
@@ -13,8 +13,38 @@ if VFS.FileExists("mission.lua") then return end
 local startBoxConfig
 local mapsideBoxes = "mapconfig/map_startboxes.lua"
 
+local function cross_product (px, pz, ax, az, bx, bz)
+	return ((px - bx)*(az - bz) - (ax - bx)*(pz - bz))
+end
+
+function GetTeamCount()
+	local gaiaAllyTeamID = select(6, Spring.GetTeamInfo(Spring.GetGaiaTeamID()))
+	local allyTeamList = Spring.GetAllyTeamList()
+	local actualAllyTeamList = {}
+	for i = 1, #allyTeamList do
+		local teamList = Spring.GetTeamList(allyTeamList[i]) or {}
+		if ((#teamList > 0) and (allyTeamList[i] ~= gaiaAllyTeamID)) then
+			actualAllyTeamList[#actualAllyTeamList+1] = allyTeamList[i]
+		end
+	end
+	return #actualAllyTeamList
+end
+
 if VFS.FileExists (mapsideBoxes) then
 	startBoxConfig = VFS.Include (mapsideBoxes)
+	for id, box in pairs(startBoxConfig) do
+		for i = 1, #box do
+			local conf = box[i]
+			if cross_product(conf[5], conf[6], conf[1], conf[2], conf[3], conf[4]) > 0 then
+				local temp = conf[5]
+				conf[5] = conf[3]
+				conf[3] = temp
+				temp = conf[6]
+				conf[6] = conf[4]
+				conf[4] = temp
+			end
+		end
+	end
 else
 	startBoxConfig = { }
 	local startboxString = Spring.GetModOptions().startboxes
