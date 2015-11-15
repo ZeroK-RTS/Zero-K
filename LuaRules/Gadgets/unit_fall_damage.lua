4b3f3cd59c03f64ba4f5aee0afdd85852d02e3cd
GoogleFrog <googl3frog@gmail.com>
2015-11-15 14:25:22 +1100

    Fix ground collision damage without breaking bounce direction.

--- LuaRules/Gadgets/unit_fall_damage.lua
+++ LuaRules/Gadgets/unit_fall_damage.lua
@@ -223,16 +223,13 @@ function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, w
 		local vx,vy,vz = Spring.GetUnitVelocity(unitID)
 		local x,y,z = Spring.GetUnitPosition(unitID)
 		local nx, ny, nz = Spring.GetGroundNormal(x,z)
-		
-		nx, ny, nz = -nx, -ny, -nz -- For some reason normal is reversed
-		
 		local nMag = math.sqrt(nx^2 + ny^2 + nz^2)
 		local nx, ny, nz = nx/nMag, ny/nMag, nz/nMag -- normal to unit vector
 		nx, ny, nz = vx*nx, vy*ny, vz*nz -- normal is now a component of velocity
 		local tx, ty, tz = vx-nx, vy-ny, vz-nz -- tangent is the other component of velocity
 		local nf = att.elasticity
 		local tf = att.friction
-		vx, vy, vz = tx*tf + nx*nf, ty*tf + ny*nf, tz*tf + nz*nf
+		vx, vy, vz = tx*tf - nx*nf, ty*tf - ny*nf, tz*tf - nz*nf
 		Spring.SetUnitVelocity(unitID,0,0,0)
 		Spring.AddUnitImpulse(unitID,vx,vy,vz) --must do impulse because SetUnitVelocity() is not fully functional in Spring 91 (only work with vertical velocity OR when assigned 0)
 		local damgeSpeed = math.sqrt((nx + tx*TANGENT_DAMAGE)^2 + (ny + ty*TANGENT_DAMAGE)^2 + (nz + tz*TANGENT_DAMAGE)^2)
