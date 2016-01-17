673d4168fdd34be6deff7df828f26f5354703d73
GoogleFrog <googl3frog@gmail.com>
2016-01-17 16:03:33 +1100

    Added Fog Hidden Effects file.

---
+++ LuaRules/Gadgets/api_fog_hidden_effects.lua
@@ -0,0 +1,58 @@
+function gadget:GetInfo() return {
+	name      = "Fog Hidden Effects API",
+	desc      = "API for playing sounds only for players with vision",
+	author    = "Google Frog",
+	date      = "17 Jan 2016",
+	license   = "GNU GPL, v2 or later",
+	layer     = 0,
+	enabled   = true,
+} end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Globals because I hear SendToUnsynced is really slow so copying strings
+-- needlessly is bad.
+
+local soundList = {
+	"sounds/misc/teleport.wav",
+	"sounds/misc/teleport2.wav",
+	"sounds/misc/teleport_loop.wav",
+}
+
+local soundMap = {}
+for i = 1, #soundList do
+	soundMap[soundList[i]] = i
+end
+
+--------------------------------------------------------------------------------
+if gadgetHandler:IsSyncedCode() then -- Synced ---------------------------------
+--------------------------------------------------------------------------------
+
+function GG.PlayFogHiddenSound(sound, volume, x, y, z)
+	soundIndex = soundMap[sound]
+	if soundIndex then
+		SendToUnsynced("playSound", soundIndex, volume, x, y, z)
+	else
+		Spring.Echo("Sound not found in gadget-side sound list", sound)
+	end
+end
+
+--------------------------------------------------------------------------------
+else ----------------------------------Unsynced --------------------------------
+--------------------------------------------------------------------------------
+
+local function playSound(_, sound, volume, x, y, z)
+	local _, fullView = select(2, Spring.GetSpectatingState())
+	local myAllyTeam = Spring.GetLocalAllyTeamID()
+	if (fullView or Spring.IsPosInLos(x, y, z, myAllyTeam)) then
+		Spring.PlaySoundFile(soundList[sound], volume, x, y, z)
+	end
+end
+
+function gadget:Initialize()
+	gadgetHandler:AddSyncAction("playSound", playSound)
+end
+
+--------------------------------------------------------------------------------
+end -------------------------------- End Unsynced ------------------------------
+--------------------------------------------------------------------------------
