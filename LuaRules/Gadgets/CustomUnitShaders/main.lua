de79921b9484f59e8de0027785c4b7f60765fe81
GoogleFrog <googl3frog@gmail.com>
2016-03-30 16:38:16 +1100

    Replaced custom unit shader framework with the BAR one. Replaced the shaders as well.

--- LuaRules/Gadgets/CustomUnitShaders/main.lua
+++ LuaRules/Gadgets/CustomUnitShaders/main.lua
@@ -12,13 +12,13 @@
 
 function gadget:GetInfo()
   return {
-    name      = "CustomUnitShaders",
+    name      = "CustomUnitShaders 100.0",
     desc      = "allows to override the engine unit shader",
     author    = "jK",
     date      = "2008,2009,2010",
     license   = "GNU GPL, v2 or later",
     layer     = 1,
-    enabled   = true  --  loaded by default?
+    enabled   = not Spring.Utilities.IsCurrentVersionNewerThan(100, 0)  --  loaded by default?
   }
 end
 
