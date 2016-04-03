4d047dcfc812f59a6f307a017ada2f9cb3095904
sprunk <gnurps@o2.pl>
2016-04-03 18:29:11 +0200

    Phoenix: can bomb gunships

--- units/corhurc2.lua
+++ units/corhurc2.lua
@@ -54,7 +54,7 @@ unitDef = {
   maxVelocity         = 8,
   minCloakDistance    = 75,
   noAutoFire          = false,
-  noChaseCategory     = [[TERRAFORM FIXEDWING SATELLITE SUB]],
+  noChaseCategory     = [[TERRAFORM FIXEDWING GUNSHIP SUB]],
   objectName          = [[firestorm.s3o]],
   script			  = [[corhurc2.lua]],
   seismicSignature    = 0,
@@ -79,8 +79,8 @@ unitDef = {
 
     {
       def                = [[NAPALM]],
-      badTargetCategory  = [[SWIM LAND SHIP HOVER]],
-      onlyTargetCategory = [[SWIM LAND SINK TURRET FLOAT SHIP HOVER]],
+      badTargetCategory  = [[SWIM LAND SHIP HOVER GUNSHIP]],
+      onlyTargetCategory = [[SWIM LAND SINK TURRET FLOAT SHIP HOVER GUNSHIP]],
     },
 
   },
