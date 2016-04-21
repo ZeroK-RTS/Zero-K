1c601681b655b9e8673fc283fa837b6759681f5c
Dominic Renaud <dre_naud@yahoo.ca>
2016-04-20 17:59:13 -0700

    Candidate fix for #1369

--- LuaUI/Widgets/map_edge_extension.lua
+++ LuaUI/Widgets/map_edge_extension.lua
@@ -162,38 +162,39 @@ local function SetupShaderTable()
 		void main()
 		{
 		gl_TexCoord[0]= gl_TextureMatrix[0]*gl_MultiTexCoord0;
-		gl_Vertex.x = abs(mirrorX-gl_Vertex.x);
-		gl_Vertex.z = abs(mirrorZ-gl_Vertex.z);
+		vec4 mirrorVertex = gl_Vertex;
+		mirrorVertex.x = abs(mirrorX-mirrorVertex.x);
+		mirrorVertex.z = abs(mirrorZ-mirrorVertex.z);
 		
 		float alpha = 1.0;
 		#ifdef curvature
-		  if(mirrorX)gl_Vertex.y -= pow(abs(gl_Vertex.x-left*mirrorX)/150.0, 2.0);
-		  if(mirrorZ)gl_Vertex.y -= pow(abs(gl_Vertex.z-up*mirrorZ)/150.0, 2.0);
+		  if(mirrorX)mirrorVertex.y -= pow(abs(mirrorVertex.x-left*mirrorX)/150.0, 2.0);
+		  if(mirrorZ)mirrorVertex.y -= pow(abs(mirrorVertex.z-up*mirrorZ)/150.0, 2.0);
 		  alpha = 0.0;
-			if(mirrorX) alpha -= pow(abs(gl_Vertex.x-left*mirrorX)/lengthX, 2.0);
-			if(mirrorZ) alpha -= pow(abs(gl_Vertex.z-up*mirrorZ)/lengthZ, 2.0);
+			if(mirrorX) alpha -= pow(abs(mirrorVertex.x-left*mirrorX)/lengthX, 2.0);
+			if(mirrorZ) alpha -= pow(abs(mirrorVertex.z-up*mirrorZ)/lengthZ, 2.0);
 			alpha = 1.0 + (6.0 * (alpha + 0.18));
 		#endif
   
 		float ff = 20000.0;
 		if((mirrorZ && mirrorX))
-		  ff=ff/(pow(abs(gl_Vertex.z-up*mirrorZ)/150.0, 2.0)+pow(abs(gl_Vertex.x-left*mirrorX)/150.0, 2.0)+2.0);
+		  ff=ff/(pow(abs(mirrorVertex.z-up*mirrorZ)/150.0, 2.0)+pow(abs(mirrorVertex.x-left*mirrorX)/150.0, 2.0)+2.0);
 		else if(mirrorX)
-		  ff=ff/(pow(abs(gl_Vertex.x-left*mirrorX)/150.0, 2.0)+2.0);
+		  ff=ff/(pow(abs(mirrorVertex.x-left*mirrorX)/150.0, 2.0)+2.0);
 		else if(mirrorZ)
-		  ff=ff/(pow(abs(gl_Vertex.z-up*mirrorZ)/150.0, 2.0)+2.0);
+		  ff=ff/(pow(abs(mirrorVertex.z-up*mirrorZ)/150.0, 2.0)+2.0);
   
-		gl_Position  = gl_ModelViewProjectionMatrix*gl_Vertex;
+		gl_Position  = gl_ModelViewProjectionMatrix*mirrorVertex;
 		//gl_Position.z+ff;
 		
 		#ifdef edgeFog
-		  gl_FogFragCoord = length((gl_ModelViewMatrix * gl_Vertex).xyz)+ff; //see how Spring shaders do the fog and copy from there to fix this
+		  gl_FogFragCoord = length((gl_ModelViewMatrix * mirrorVertex).xyz)+ff; //see how Spring shaders do the fog and copy from there to fix this
 		#endif
 		
 		gl_FrontColor = vec4(brightness * gl_Color.rgb, alpha);
 
 		color = gl_FrontColor;
-		vertex = gl_Vertex;
+		vertex = mirrorVertex;
 		}
 	  ]],
 	 --  fragment = [[
