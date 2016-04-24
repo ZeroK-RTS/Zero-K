a0054be7237fb56b42a0fa6aa4072aa74a2c1ec6
sprunk <gnurps@o2.pl>
2016-04-24 14:31:34 +0200

    GS icons: remove toggle, inflict as the default.

--- LuaUI/Widgets/gui_icon_sets.lua
+++ LuaUI/Widgets/gui_icon_sets.lua
@@ -8,7 +8,7 @@ function widget:GetInfo() return {
 } end
 
 options_path = 'Settings/Graphics/Unit Visibility/Radar Icons'
-options_order = { 'coniconchassis', 'gunship_pictograms' }
+options_order = { 'coniconchassis' }
 options = {
 	coniconchassis = {
 		name = 'Show constructor chassis',
@@ -38,39 +38,6 @@ options = {
 				Spring.SetUnitDefIcon(UnitDefNames["corch"].id, "hoverbuilder")
 				Spring.SetUnitDefIcon(UnitDefNames["shipcon"].id, "shipbuilder")
 			end
-		end,
-	},
-	gunship_pictograms = {
-		name = 'Use pictograms for gunships',
-		desc = 'Do gunships use pictograms instead of the usual role icons?',
-		type = 'bool',
-		value = true,
-		noHotkey = true,
-		OnChange = function(self)
-			if not self.value then
-				Spring.SetUnitDefIcon(UnitDefNames["blastwing"].id, "gunshipspecial")
-				Spring.SetUnitDefIcon(UnitDefNames["bladew"].id, "gunshipscout")
-				Spring.SetUnitDefIcon(UnitDefNames["armkam"].id, "gunshipraider")
-				Spring.SetUnitDefIcon(UnitDefNames["gunshipsupport"].id, "gunshipskirm")
-				Spring.SetUnitDefIcon(UnitDefNames["armbrawl"].id, "heavygunshipskirm")
-				Spring.SetUnitDefIcon(UnitDefNames["blackdawn"].id, "heavygunshipassault")
-				-- Spring.SetUnitDefIcon(UnitDefNames["corcrw"].id, "gunshipriot")
-				Spring.SetUnitDefIcon(UnitDefNames["corvalk"].id, "gunshiptransport")
-				Spring.SetUnitDefIcon(UnitDefNames["corbtrans"].id, "heavygunshiptransport")
-			else
-				Spring.SetUnitDefIcon(UnitDefNames["blastwing"].id, "airbomb")
-				Spring.SetUnitDefIcon(UnitDefNames["bladew"].id, "smallgunship")
-				Spring.SetUnitDefIcon(UnitDefNames["armkam"].id, "gunship")
-				Spring.SetUnitDefIcon(UnitDefNames["gunshipsupport"].id, "gunshipears")
-				Spring.SetUnitDefIcon(UnitDefNames["armbrawl"].id, "heavygunship")
-				Spring.SetUnitDefIcon(UnitDefNames["blackdawn"].id, "heavygunshipears")
-				-- Spring.SetUnitDefIcon(UnitDefNames["corcrw"].id, "supergunship")
-				Spring.SetUnitDefIcon(UnitDefNames["corvalk"].id, "airtransport")
-				Spring.SetUnitDefIcon(UnitDefNames["corbtrans"].id, "airtransportbig")
-			end
-
-			-- same in both, listed for completeness
-			Spring.SetUnitDefIcon(UnitDefNames["gunshipaa"].id, "gunshipaa")
 		end,
 	},
 }
