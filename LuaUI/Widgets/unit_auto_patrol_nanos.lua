1e1f3772e39d68c94f7cfd35171c26c437a70232
GoogleFrog <googl3frog@gmail.com>
2016-06-07 16:18:56 +1000

    Simplified Caretaker automatic assist options. Fixes #1335.  * Removed the time-to-idle option. The setting is unintuitive and pointless.  * Added an option for automation (ie. patrol on idle) and an option for explicitly disabling automation with the Stop command. Both are on by default.

--- LuaUI/Widgets/unit_auto_patrol_nanos.lua
+++ LuaUI/Widgets/unit_auto_patrol_nanos.lua
@@ -26,8 +26,7 @@ end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
-
--- Automatically generated local definitions
+-- Speeups
 
 local CMD_MOVE_STATE    = CMD.MOVE_STATE
 local CMD_PATROL        = CMD.PATROL
@@ -44,142 +43,149 @@ local abs = math.abs
 
 local reverseCompat = not Spring.Utilities.IsCurrentVersionNewerThan(100, 0)
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
--- if this is >0, then commands are re-issued for immobile
--- builders that have been idling for the number of game frames
--- (in case a player accidentally STOPs them)
-
-local idleFrames = 0
-
-local mapCenterX
-local mapCenterZ
+local mapCenterX = Game.mapSizeX / 2
+local mapCenterZ = Game.mapSizeZ / 2
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+-- Variables
 
-options_path = 'Game/Unit Behaviour'
-options = 
-{
-	idleSeconds = {
-		name = 'Nano Tower Idle Seconds (0-5)',
-		desc = 'Number of seconds a nano turret is idle before it is set to patrol (0 for never).',
-		type = 'number',
-		
-		min = 0,max=5,value = 0,
-		OnChange = function(self) idleFrames = self.value*30 end,
-	},
-}
+local stoppedUnit = {}
+local enableIdleNanos = true
+local stopHalts = true
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+-- Functions
 
 VFS.Include("LuaRules/Utilities/ClampPosition.lua")
 local GiveClampedOrderToUnit = Spring.Utilities.GiveClampedOrderToUnit
 
 local function IsImmobileBuilder(ud)
-  return(ud and ud.isBuilder and not ud.canMove
-         and not ud.isFactory)
+	return(ud and ud.isBuilder and not ud.canMove and not ud.isFactory)
 end
-
 
 local function SetupUnit(unitID)
-  -- set immobile builders (nanotowers?) to the ROAM movestate,
-  -- and give them a PATROL order (does not matter where, afaict)
-  local x, y, z = spGetUnitPosition(unitID)
-  if (x) then
+	-- set immobile builders (nanotowers?) to the ROAM movestate,
+	-- and give them a PATROL order (does not matter where, afaict)
+	local queueLength = spGetCommandQueue(unitID, 0)
+	if not (queueLength and (queueLength == 0)) then
+		return
+	end
+	
+	local x, y, z = spGetUnitPosition(unitID)
+	if (x) then
+		-- point patrol towards map center
+		vx = mapCenterX - x
+		vz = mapCenterZ - z
+		x = x + vx*25/abs(vx)
+		z = z + vz*25/abs(vz)
 
-    -- point patrol towards map center
-    vx = mapCenterX - x
-    vz = mapCenterZ - z
-    x = x + vx*25/abs(vx)
-    z = z + vz*25/abs(vz)
-
-    GiveClampedOrderToUnit(unitID, CMD_PATROL, { x, y, z }, {})
-  end
+		GiveClampedOrderToUnit(unitID, CMD_PATROL, { x, y, z }, {})
+	end
 end
 
-
-function widget:Initialize()
-
-  mapCenterX = Game.mapSizeX / 2
-  mapCenterZ = Game.mapSizeZ / 2
-
-  for _,unitID in ipairs(spGetTeamUnits(spGetMyTeamID())) do
-    local unitDefID = spGetUnitDefID(unitID)
-    if (IsImmobileBuilder(UnitDefs[unitDefID])) then
-      SetupUnit(unitID)
-    end
-  end
-end
-
-
-function widget:UnitCreated(unitID, unitDefID, unitTeam)
-  if (unitTeam ~= spGetMyTeamID()) then
-    return
-  end
-  if (IsImmobileBuilder(UnitDefs[unitDefID])) then
-    SetupUnit(unitID)
-  end
-end
-
-function widget:UnitCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions) 
-	if cmdID == CMD_PATROL and IsImmobileBuilder(UnitDefs[unitDefID]) and ((reverseCompat and (math.bit_and(cmdOptions,CMD.OPT_SHIFT) <= 0)) or ((not reverseCompat) and not cmdOptions.shift)) then
-		local x, y, z = spGetUnitPosition(unitID)
-		if math.abs(x - cmdParams[1]) > 30 or math.abs(z - cmdParams[3]) > 30 then
+local function SetupAll()
+	for _,unitID in ipairs(spGetTeamUnits(spGetMyTeamID())) do
+		local unitDefID = spGetUnitDefID(unitID)
+		if (IsImmobileBuilder(UnitDefs[unitDefID])) then
 			SetupUnit(unitID)
 		end
 	end
 end
 
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Settings
+
+options_path = 'Game/Unit Behaviour'
+options = {
+	patrol_idle_nanos = {
+		name = "Caretaker automation",
+		type = 'bool',
+		value = true,
+		noHotkey = true,
+		desc = 'Caretakers will automatically find tasks when idle. They may assist, repair or reclaim. Also applies to Strider Hub.',
+		OnChange = function (self)
+			enableIdleNanos = self.value
+			if self.value then
+				SetupAll()
+			end
+		end,
+	},
+	stop_disables = {
+		name = "Disable Caretakers with Stop",
+		type = 'bool',
+		value = true,
+		noHotkey = true,
+		desc = 'Caretakers automation is put on hold with the Stop command. Automation resumes after any other command. Also applies to Strider Hub.',
+		OnChange = function (self)
+			stopHalts = self.value
+			if not self.value then
+				stoppedUnit = {}
+				SetupAll()
+			end
+		end,
+	}
+}
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Callins
+
+function widget:Initialize()
+	SetupAll()
+end
+
+function widget:UnitCreated(unitID, unitDefID, unitTeam)
+	if not enableIdleNanos then
+		return
+	end
+	if (unitTeam ~= spGetMyTeamID()) then
+		return
+	end
+	if (IsImmobileBuilder(UnitDefs[unitDefID])) then
+		SetupUnit(unitID)
+	end
+end
+
+function widget:UnitCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOptions) 
+	if not IsImmobileBuilder(UnitDefs[unitDefID]) then
+		return
+	end
+	if cmdID == CMD_PATROL and ((reverseCompat and (math.bit_and(cmdOptions,CMD.OPT_SHIFT) <= 0)) or ((not reverseCompat) and not cmdOptions.shift)) then
+		local x, y, z = spGetUnitPosition(unitID)
+		if math.abs(x - cmdParams[1]) > 30 or math.abs(z - cmdParams[3]) > 30 then
+			SetupUnit(unitID)
+		end
+	end
+	if stopHalts then
+		if cmdID == CMD_STOP then
+			stoppedUnit[unitID] = true
+		elseif stoppedUnit[unitID] then
+			stoppedUnit[unitID] = nil
+		end
+	end
+end
+
 function widget:UnitGiven(unitID, unitDefID, unitTeam)
-  widget:UnitCreated(unitID, unitDefID, unitTeam)
+	if not enableIdleNanos then
+		return
+	end
+	widget:UnitCreated(unitID, unitDefID, unitTeam)
 end
 
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
---if (idleFrames > 0) then
-if true then
-
-  local idlers = {}
-
-
-  function widget:GameFrame(frame)
-	if idleFrames == 0 then return end
-    for unitID, f in pairs(idlers) do
-      local idler = idlers[k]
-      if ((frame - f) > idleFrames) then
-        local cmds = spGetCommandQueue(unitID, 1)
-        if (cmds and (#cmds <= 0)) then
-          SetupUnit(unitID)
-        else
-          idlers[unitID] = nil
-        end
-      end
-    end  
-  end
-
-
-  function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
-    idlers[unitID] = nil
-  end
-
-
-  function widget:UnitIdle(unitID, unitDefID, unitTeam)
-    if (unitTeam ~= spGetMyTeamID()) then
-      return
-    end
-    if (IsImmobileBuilder(UnitDefs[unitDefID])) then
-      idlers[unitID] = spGetGameFrame()
-    end
-  end
-
-
+function widget:UnitIdle(unitID, unitDefID, unitTeam)
+	if not enableIdleNanos then
+		return
+	end
+	if (unitTeam ~= spGetMyTeamID()) then
+		return
+	end
+	if (IsImmobileBuilder(UnitDefs[unitDefID])) and not stoppedUnit[unitID] then
+		SetupUnit(unitID)
+	end
 end
-
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
