d8d37c7282f9af228a3da68a00e8f453cc476518
Anarchid <anarchid@mail.ru>
2016-06-27 19:58:21 +0300

    missing stuff

---
+++ LuaRules/Gadgets/unit_cannot_damage_unit.lua
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+if not gadgetHandler:IsSyncedCode() then
+	return
+end
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+function gadget:GetInfo()
+	return {
+		name = "Cannot Damage Unit",
+		desc = "Prevents units from taking damage from other units with specific UnitID",
+		author = "Anarchid",
+		date = "27.06.2016",
+		license = "Public domain",
+		layer = 21,
+		enabled = true
+	}
+end
+
+function gadget:UnitPreDamaged(unitID,unitDefID,_, damage,_, weaponDefID,attackerID,_,_, projectileID)
+    if attackerID and Spring.GetUnitRulesParam(attackerID,'cannot_damage_unit') == unitID then
+        return 0;
+    end
+    return damage
+end
