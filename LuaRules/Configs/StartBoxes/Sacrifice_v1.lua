13c79f4923e5ebbe3841c3f0342a09eea0659d96
Anarchid <anarchid@mail.ru>
2016-07-26 13:56:18 +0300

    Add missing startboxes

---
+++ LuaRules/Configs/StartBoxes/Sacrifice_v1.lua
@@ -0,0 +1,62 @@
+return {
+	[0] = {
+		nameLong = "Northwest",
+		nameShort = "NW",
+		startpoints = {
+			{1751,1167},
+		},
+		boxes = {
+			{
+				{0,0},
+				{3502,0},
+				{3502,2335},
+				{0,2335},
+			},
+		},
+	},
+	[1] = {
+		nameLong = "Southeast",
+		nameShort = "SE",
+		startpoints = {
+			{7465,4977},
+		},
+		boxes = {
+			{
+				{5714,3809},
+				{9216,3809},
+				{9216,6144},
+				{5714,6144},
+			},
+		},
+	},
+	[2] = {
+		nameLong = "Northeast",
+		nameShort = "NE",
+		startpoints = {
+			{7465,1167},
+		},
+		boxes = {
+			{
+				{5714,0},
+				{9216,0},
+				{9216,2335},
+				{5714,2335},
+			},
+		},
+	},
+	[3] = {
+		nameLong = "Southwest",
+		nameShort = "SW",
+		startpoints = {
+			{2857,4977},
+		},
+		boxes = {
+			{
+				{0,3809},
+				{5714,3809},
+				{5714,6144},
+				{0,6144},
+			},
+		},
+	},
+}
