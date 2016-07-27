3b470d1c0a36c44e372b0fd4da737a73217f5f0d
sprunk <gnurps@o2.pl>
2016-07-27 02:14:55 +0200

    Mearth startboxes.

--- LuaRules/Configs/StartBoxes/Mearth_v4.lua
+++ LuaRules/Configs/StartBoxes/Mearth_v4.lua
@@ -1,210 +1,72 @@
-return {
-	[0] = {
-		startpoints = {
-			{4557,3994},
-		},
-		boxes = {
-			{
-				{4198,3686},
-				{4915,3686},
-				{4915,4301},
-				{4198,4301},
-			},
-		},
-	},
-	[1] = {
-		startpoints = {
-			{5376,9216},
-		},
-		boxes = {
-			{
-				{5018,8909},
-				{5734,8909},
-				{5734,9523},
-				{5018,9523},
-			},
-		},
-	},
-	[2] = {
-		startpoints = {
-			{6502,922},
-		},
-		boxes = {
-			{
-				{6144,614},
-				{6861,614},
-				{6861,1229},
-				{6144,1229},
-			},
-		},
-	},
-	[3] = {
-		startpoints = {
-			{5786,14285},
-		},
-		boxes = {
-			{
-				{5427,13978},
-				{6144,13978},
-				{6144,14592},
-				{5427,14592},
-			},
-		},
-	},
-	[4] = {
-		startpoints = {
-			{9574,11520},
-		},
-		boxes = {
-			{
-				{9216,11213},
-				{9933,11213},
-				{9933,11827},
-				{9216,11827},
-			},
-		},
-	},
-	[5] = {
-		startpoints = {
-			{8141,5376},
-		},
-		boxes = {
-			{
-				{7782,5069},
-				{8499,5069},
-				{8499,5683},
-				{7782,5683},
-			},
-		},
-	},
-	[6] = {
-		startpoints = {
-			{2406,4301},
-		},
-		boxes = {
-			{
-				{2048,3994},
-				{2765,3994},
-				{2765,4608},
-				{2048,4608},
-			},
-		},
-	},
-	[7] = {
-		startpoints = {
-			{9370,922},
-		},
-		boxes = {
-			{
-				{9011,614},
-				{9728,614},
-				{9728,1229},
-				{9011,1229},
-			},
-		},
-	},
-	[8] = {
-		startpoints = {
-			{9574,14746},
-		},
-		boxes = {
-			{
-				{9216,14438},
-				{9933,14438},
-				{9933,15053},
-				{9216,15053},
-			},
-		},
-	},
-	[9] = {
-		startpoints = {
-			{4352,11366},
-		},
-		boxes = {
-			{
-				{3994,11059},
-				{4710,11059},
-				{4710,11674},
-				{3994,11674},
-			},
-		},
-	},
-	[10] = {
-		startpoints = {
-			{3840,1997},
-		},
-		boxes = {
-			{
-				{3482,1690},
-				{4198,1690},
-				{4198,2304},
-				{3482,2304},
-			},
-		},
-	},
-	[11] = {
-		startpoints = {
-			{8960,8294},
-		},
-		boxes = {
-			{
-				{8602,7987},
-				{9318,7987},
-				{9318,8602},
-				{8602,8602},
-			},
-		},
-	},
-	[12] = {
-		startpoints = {
-			{3021,6451},
-		},
-		boxes = {
-			{
-				{2662,6144},
-				{3379,6144},
-				{3379,6758},
-				{2662,6758},
-			},
-		},
-	},
-	[13] = {
-		startpoints = {
-			{7322,2765},
-		},
-		boxes = {
-			{
-				{6963,2458},
-				{7680,2458},
-				{7680,3072},
-				{6963,3072},
-			},
-		},
-	},
-	[14] = {
-		startpoints = {
-			{3635,13824},
-		},
-		boxes = {
-			{
-				{3277,13517},
-				{3994,13517},
-				{3994,14131},
-				{3277,14131},
-			},
-		},
-	},
-	[15] = {
-		startpoints = {
-			{1894,9216},
-		},
-		boxes = {
-			{
-				{1536,8909},
-				{2253,8909},
-				{2253,9523},
-				{1536,9523},
-			},
-		},
-	},
+local supported_playercounts = {}
+for i = 1, 33 do
+	supported_playercounts[i] = i
+end
+
+local starting_triples = {
+	{2355,  4241, "Shire", "Shire"},
+	{9471, 11572, "Mordor", "Mordor"},
+	{4380, 11300, "Gondor", "Gondor"},
+	{5371,  9235, "Rohan", "Rohan"},
+	{5810,  5333, "Moria", "Moria"},
+	{9323,   963, "Withered Heath", "WHeath"},
+	{3853,  2024, "Evendim Hills", "EHills"},
+	{9543, 14740, "Khand", "Khand"},
+	{5844, 14307, "Umbar", "Umbar"},
+	{8100,  5400, "Dol Guldur", "Guldur"},
+	{7340,  2830, "Mirkwood", "Mirkwd"},
+	{4590,  3909, "Fornost", "Frnst"},
+	{2994,  6392, "Minhiriath", "Mnhrth"},
+	{8897,  8244, "Brown Lands", "BrnLnd"},
+	{6500,   936, "Forodwaith", "Frdwth"},
+	{ 3652, 13781, "Bay of Belfalas", "Bay"},
+	{ 1924,  9348, "The Great Sea", "Sea"},
 }
+
+local starting_doubles = {
+	{  877,  2613, "Forlindon", "Frlndn"},
+	{ 3035,  2935, "Annuminas", "Annmns"},
+	{ 4437,  3102, "North Downs", "NDowns"},
+	{ 5040,  1190, "Angmar", "Angmar"},
+	{ 6742,  1986, "Carrock", "Carrck"},
+	{ 8048,   625, "North Rhun", "N Rhun"},
+	{10100,  3587, "South Rhun", "S Rhun"},
+	{ 8676,  2295, "Esgaroth", "Esgrth"},
+	{ 3853,  5120, "South Downs", "SDowns"},
+	{ 7569, 15015, "Harad", "Harad"},
+	{ 6925, 12297, "Ithilien", "Ithln"},
+	{ 7679,  9694, "Dead Marshes", "DMarsh"},
+	{ 2795, 10859, "Druwaith Iaur", "Druwth"},
+	{ 6743,  8087, "Rauros", "Rauros"},
+	{ 4288,  7649, "Enedwaith", "Endwth"},
+	{ 8326,  6765, "Emyn Muil", "EmynMl"},
+}
+
+-- there are 17 triple starts and 16 double starts
+-- for now, we only use doubles if there are not enough triples, but they could sustain <=16 matches on their own
+-- if we do that, randomly pick either set (cannot rely on mapoption since MM has no customizability)
+local teams = GetTeamCount()
+if teams > 17 then
+	for i = 1, #starting_doubles do
+		starting_triples [#starting_triples + 1] = starting_doubles[i]
+	end
+end
+
+-- convert the above to boxes (256 radius circles)
+local ret = {}
+for i = 1, #starting_triples do
+	ret[i-1] = {
+		nameLong  = starting_triples[i][3],
+		nameShort = starting_triples[i][4],
+		startpoints = { { starting_triples[i][1], starting_triples[i][2] } },
+		boxes = { { } },
+	}
+	for j = 1, 10 do
+		ret[i-1].boxes[1][j] = {
+			starting_triples[i][1] + 256 * math.sin(j * math.pi / 5),
+			starting_triples[i][2] + 256 * math.cos(j * math.pi / 5),
+		}
+	end
+end
+
+return ret, supported_playercounts
