13937bf93dc8c5e56a004d68b0fd83ad3f5b29f3
That guy! <turkishshaman@gmail.com>
2016-08-16 07:08:34 -0700

    Bugfix + Removed Redundant stuff.

--- LuaUI/Widgets/ignore.lua
+++ LuaUI/Widgets/ignore.lua
@@ -9,28 +9,17 @@ function widget:GetInfo()
       enabled   = true,
     }
 end
-local playernames = {}
 
-local function ProccessCommand(str)
+local function ProcessString(str)
   local strtbl = {}
   for w in string.gmatch(str, "%S+") do
 	strtbl[#strtbl+1] = w
   end
   return strtbl
 end
-
-local function ProcessIgnoreList(str)
-  local strtbl = {}
-  for w in string.gmatch(str, "%S+") do
-	strtbl[#strtbl+1] = w
-  end
-  return strtbl
-end
-
 
 if VFS.FileExists("ignorelist.lua") then
   WG.IgnoreList = VFS.Include("ignorelist.lua")
-  isignoring = WG.IgnoreList
 else
   WG.IgnoreList = {}
 end
@@ -48,7 +37,7 @@ if VFS.FileExists("ZeroKLobbyConfig.xml") then -- load ignore list from ZKL conf
   ignroelist = string.gsub(ignorelist,"/","")
   ignorelist = string.gsub(ignorelist,"~"," ")
   --Spring.Echo("Ignore: " .. ignorelist)
-  local names = ProcessIgnoreList(ignorelist)
+  local names = ProcessString(ignorelist)
   for i=1,#names do
     --Spring.Echo("Ignore: found " .. names[i])
     WG.IgnoreList[names[i]] = true
@@ -57,17 +46,13 @@ if VFS.FileExists("ZeroKLobbyConfig.xml") then -- load ignore list from ZKL conf
 end
 
 function widget:Initialize()
-  local playerlist = Spring.GetPlayerList()
-  for i=1,#playerlist do
-    playernames[select(1,Spring.GetPlayerInfo(playerlist[i]))] = true
-  end
   for name,_ in pairs(WG.IgnoreList) do
     widgetHandler:Ignore(name)
   end
 end
 
 function widget:TextCommand(command)
-  local prcmd = ProccessCommand(command)
+  local prcmd = ProcessString(command)
   if string.lower(prcmd[1]) == "ignore" then
     if prcmd[2] then
       Spring.Echo("game_message: Ignoring " .. prcmd[2])
@@ -82,10 +67,10 @@ function widget:TextCommand(command)
     Spring.Echo(ignorestring)
   end
   if string.lower(prcmd[1]) == "unignore" then
-    if prcmd[2] and playernames[prcmd[2]] then
+    if prcmd[2] and WG.IgnoreList[prcmd[2]] then
       Spring.Echo("game_message: Unignoring " .. prcmd[2])
       widgetHandler:Unignore(prcmd[2])
-      isignoring[prcmd[2]] = nil
+      WG.IgnoreList[prcmd[2]] = nil
     end
   end
 end
