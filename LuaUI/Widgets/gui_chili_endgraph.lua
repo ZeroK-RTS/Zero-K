a41081ee38baf306ced2d3ea79c846d260db609d
GoogleFrog <googl3frog@gmail.com>
2016-11-01 23:36:39 +1100

    Endgame graph whitespace.

--- LuaUI/Widgets/gui_chili_endgraph.lua
+++ LuaUI/Widgets/gui_chili_endgraph.lua
@@ -5,15 +5,17 @@
 		Add minimize option
 		Come up with better way of handling specs, active players and players who died (currently doesn't show players who have died
 ]]
-function widget:GetInfo() return {
-	name    = "EndGame Stats",
-	desc    = "v0.913 Chili replacement for default end game statistics",
-	author  = "Funkencool",
-	date    = "2013",
-	license = "public domain",
-	layer   = -1,
-	enabled = true
-} end
+function widget:GetInfo() 
+	return {
+		name    = "EndGame Stats",
+		desc    = "v0.913 Chili replacement for default end game statistics",
+		author  = "Funkencool",
+		date    = "2013",
+		license = "public domain",
+		layer   = -1,
+		enabled = true
+	} 
+end
 
 local buttons = {
 	{"metalProduced"   , "Metal Produced"},
@@ -50,8 +52,10 @@ local curGraph = {}
 local Chili, window0, graphPanel, graphSelect, graphLabel, graphTime
 local wasActive = {}
 local playerNames = {}
+
 ------------------------------------
 --formats final stat to fit in label
+
 local function numFormat(label)
 	if not label then
 		return ''
@@ -69,31 +73,53 @@ local function numFormat(label)
 	end
 	return string
 end
+
 local function formatTime(seconds)
 	local hours = math.floor(seconds/3600)
 	local minutes = math.floor(seconds/60) % 60
 	local seconds = seconds % 60
-	if minutes < 10 then minutes = "0" .. minutes end
-	if seconds < 10 then seconds = "0" .. seconds end
+	if minutes < 10 then 
+		minutes = "0" .. minutes 
+	end
+	if seconds < 10 then 
+		seconds = "0" .. seconds 
+	end
 	return hours .. ":" .. minutes .. ":" .. seconds
 end
+
 local function drawIntervals(graphMax)
-	for i=1, 4 do
-		local line = Chili.Line:New{parent = graphPanel, x = 0, bottom = (i)/5*100 .. "%", width = "100%", color = {0.1,0.1,0.1,0.1}}
-		local label = Chili.Label:New{parent = graphPanel, x = 0, bottom = (i)/5*100 .. "%", width = "100%", caption = numFormat(graphMax*i/5)}
+	for i = 1, 4 do
+		local line = Chili.Line:New{
+			parent = graphPanel, 
+			x = 0, 
+			bottom = (i)/5*100 .. "%", 
+			width = "100%", 
+			color = {0.1,0.1,0.1,0.1}
+		}
+		local label = Chili.Label:New{
+			parent = graphPanel, 
+			x = 0, bottom = (i)/5*100 .. "%", 
+			width = "100%", 
+			caption = numFormat(graphMax*i/5)
+		}
 	end
 end
 
 local function fixLabelAlignment()
 	local doAgain
-	for a=1, #lineLabels.children do
-		for b=1, #lineLabels.children do
+	for a = 1, #lineLabels.children do
+		for b = 1, #lineLabels.children do
 			if lineLabels.children[a] ~= lineLabels.children[b] then
 				if lineLabels.children[a].y >= lineLabels.children[b].y and lineLabels.children[a].y < lineLabels.children[b].y+11 then
 					lineLabels.children[a]:SetPos(0, lineLabels.children[b].y+11)
 					doAgain = false
-	end end end end
-	if doAgain then fixLabelAlignment() end
+				end 
+			end 
+		end 
+	end
+	if doAgain then 
+		fixLabelAlignment() 
+	end
 end
 ------------------------------------------------------------------------
 --Total package of graph: Draws graph and labels for each nonSpec player
@@ -102,10 +128,9 @@ local function drawGraph(graphArray, graph_m, teamID)
 		return
 	end
 
-	local r,g,b,a = Spring.GetTeamColor( usingAllyteams
-		and ((teamID == Spring.GetMyAllyTeamID())
-			and Spring.GetMyTeamID()
-			or Spring.GetTeamList(teamID)[1])
+	local r,g,b,a = Spring.GetTeamColor(
+		usingAllyteams 
+		and ((teamID == Spring.GetMyAllyTeamID()) and Spring.GetMyTeamID() or Spring.GetTeamList(teamID)[1]) 
 		or teamID
 	)
 	local teamColor = {r,g,b,a}
@@ -124,23 +149,38 @@ local function drawGraph(graphArray, graph_m, teamID)
 		end
 	end
 
-	for i=1, #graphArray do
-		if (graph_m < graphArray[i]) then graph_m = graphArray[i] end
+	for i = 1, #graphArray do
+		if (graph_m < graphArray[i]) then 
+			graph_m = graphArray[i]
+		end
 	end
 
 	--gets vertex's from array and plots them
 	local drawLine = function()
-		for i=1, #graphArray do
+		for i = 1, #graphArray do
 			local ordinate = graphArray[i]
 			gl.Vertex((i - 1)/(#graphArray - 1), 1 - ordinate/graph_m)
 		end
 	end
 
 	--adds value to end of graph
-	local label1 = Chili.Label:New{parent = lineLabels, y = (1 - graphArray[#graphArray]/graph_m) * 88 - 1 .. "%", width = "100%", caption = lineLabel, font = {color = teamColor}}
+	local label1 = Chili.Label:New{
+		parent = lineLabels, 
+		y = (1 - graphArray[#graphArray]/graph_m) * 88 - 1 .. "%", 
+		width = "100%",
+		caption = lineLabel,
+		font = {color = teamColor}
+	}
 
 	--adds player to Legend
-	local label2 = Chili.Label:New{parent = graphPanel, x = 55, y = (teamID)*20 + 5, width = "100%", height  = 20, caption = name, font = {color = teamColor}}
+	local label2 = Chili.Label:New{
+		parent = graphPanel,
+		x = 55, y = (teamID)*20 + 5,
+		width = "100%",
+		height = 20, 
+		caption = name,
+		font = {color = teamColor}
+	}
 
 	--creates graph element
 	local graph = Chili.Control:New{
@@ -165,7 +205,7 @@ local function drawGraph(graphArray, graph_m, teamID)
 			gl.BeginEnd(GL.LINE_STRIP, drawLine)
 			gl.PopMatrix()
 		end
-		}
+	}
 end
 ----------------------------------------------------------------
 ----------------------------------------------------------------
@@ -189,7 +229,7 @@ local function getEngineArrays(statistic, labelCaption)
 		and select(6, Spring.GetTeamInfo(Spring.GetGaiaTeamID()))
 		or Spring.GetGaiaTeamID()
 
-	for i=1, #teams do
+	for i = 1, #teams do
 		local teamID = teams[i]
 		local effectiveTeam = usingAllyteams
 			and select(6, Spring.GetTeamInfo(teamID))
@@ -214,7 +254,9 @@ local function getEngineArrays(statistic, labelCaption)
 		end
 	end
 
-	if graphMax > 5 then drawIntervals(graphMax) end
+	if graphMax > 5 then 
+		drawIntervals(graphMax) 
+	end
 
 	for k, v in pairs(teamScores) do
 		if k ~= gaia then
@@ -242,20 +284,25 @@ function loadpanel()
 	local selW = 150
 
 	window0 = Chili.Panel:New {
-		x = "0", y = "0",
-		width = "100%", height = "100%",
+		x = "0",
+		y = "0",
+		width = "100%", 
+		height = "100%",
 		padding = {5,5,5,5}
 	}
 	lineLabels 	= Chili.Control:New {
 		parent = window0,
-		right = 0, y = 0,
-		width = 37, height = "100%",
+		right = 0,
+		y = 0,
+		width = 37, 
+		height = "100%",
 		padding = {0,0,0,0},
 	}
 	graphSelect	= Chili.StackPanel:New {
 		parent = window0,
 		minHeight = 70,
-		x = 0, y = 0,
+		x = 0, 
+		y = 0,
 		width = selW, height = "100%",
 		padding = {0,0,0,0},
 		itemMargin = {0,0,0,0},
@@ -263,23 +310,29 @@ function loadpanel()
 	}
 	graphPanel = Chili.Panel:New {
 		parent = window0,
-		x = selW, right = 30,
-		y = 0, bottom = 40,
+		x = selW, 
+		right = 30,
+		y = 0, 
+		bottom = 40,
 		padding = {10,10,10,10}
 	}
 	graphLabel = Chili.Label:New {
 		parent = window0,
 		caption = "",
-		x = "20%", bottom = 5,
-		width = "70%", height = 30, 
+		x = "20%", 
+		bottom = 5,
+		width = "70%", 
+		height = 30, 
 		align = "center",
 		autosize = true,
 		font = {size = 30,},
 	}
 	graphTime = Chili.Label:New {
 		parent = window0,
-		bottom = 25, right = 50,
-		width = 50, height = 10,
+		bottom = 25, 
+		right = 50,
+		width = 50,
+		height = 10,
 		caption = "",
 	}
 
@@ -288,11 +341,13 @@ function loadpanel()
 			name = buttons[i][1],
 			caption = buttons[i][2],
 			parent = graphSelect,
-			OnClick = { function(obj)
-				graphPanel:ClearChildren()
-				lineLabels:ClearChildren()
-				getEngineArrays(obj.name,obj.caption)
-			end }
+			OnClick = { 
+				function(obj)
+					graphPanel:ClearChildren()
+					lineLabels:ClearChildren()
+					getEngineArrays(obj.name,obj.caption)
+				end 
+			}
 		}
 	end
 
@@ -301,14 +356,16 @@ function loadpanel()
 		caption = " ",
 		right = 32, bottom = 2,
 		checked = false,
-		OnClick = { function()
-			usingAllyteams = not usingAllyteams
-			if curGraph.name then
-				graphPanel:ClearChildren()
-				lineLabels:ClearChildren()
-				getEngineArrays(curGraph.name,curGraph.caption)
+		OnClick = { 
+			function()
+				usingAllyteams = not usingAllyteams
+				if curGraph.name then
+					graphPanel:ClearChildren()
+					lineLabels:ClearChildren()
+					getEngineArrays(curGraph.name,curGraph.caption)
+				end
 			end
-		end}
+		}
 	}
 
 	local allyToggleLabel = Chili.Label:New {
@@ -323,6 +380,6 @@ function loadpanel()
 end
 
 function widget:GameOver()
-	Spring.SendCommands ("endgraph 0")
+	Spring.SendCommands("endgraph 0")
 	loadpanel()
 end
