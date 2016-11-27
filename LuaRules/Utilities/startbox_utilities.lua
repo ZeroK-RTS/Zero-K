a4721c4ddac75e247104b616e7c43e3c30e7b016
sprunk <gnurps@o2.pl>
2016-11-27 22:42:54 +0100

    Boxes: GetTeamCount() doesn't count chickens.

--- LuaRules/Utilities/startbox_utilities.lua
+++ LuaRules/Utilities/startbox_utilities.lua
@@ -311,8 +311,18 @@ function GetTeamCount()
 	for i = 1, #allyTeamList do
 		local teamList = Spring.GetTeamList(allyTeamList[i]) or {}
 		if ((#teamList > 0) and (allyTeamList[i] ~= gaiaAllyTeamID)) then
-			actualAllyTeamList[#actualAllyTeamList+1] = allyTeamList[i]
+			local isTeamValid = true
+			for j = 1, #teamList do
+				local luaAI = Spring.GetTeamLuaAI(teamList[j])
+				if luaAI and luaAI:find("Chicken") then
+					isTeamValid = false
+				end
+			end
+			if isTeamValid then
+				actualAllyTeamList[#actualAllyTeamList+1] = allyTeamList[i]
+			end
 		end
 	end
 	return #actualAllyTeamList
 end
+
