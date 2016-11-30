c64cbf5eeae11f17cc9fd0edbf315b518578dfc0
mtroyka <aeonioshaplo@gmail.com>
2016-11-30 03:47:05 -0500

    -Deferred rendering: cleaned up some dead code from a shader.

--- LuaUI/Widgets/Shaders/bloom_bright.fs
+++ LuaUI/Widgets/Shaders/bloom_bright.fs
@@ -9,17 +9,6 @@ void main(void) {
 	float illum = dot(color, vec3(0.2990, 0.5870, 0.1140));
 
 	if (illum > illuminationThreshold) {
-		// Apply tone mapping when adding to the bloom texture, because otherwise the bloom intensity setting has no effect.
-		// white point correction
-		/*const float whiteStart = 2.0; // the minimum color intensity for starting white point transition
-		const float whiteMax = 0.35; // the maximum amount of white shifting applied
-		const float whiteScale = 0.1; // the rate at which to transition to white 
-		
-		float mx = max(color.r, max(color.g, color.b));
-		if (mx > whiteStart) {
-			color.rgb += min(vec3((mx - whiteStart) * whiteScale), vec3(whiteMax));
-		}*/
-							
 		// tone mapping, because running blurs on HDR float textures would be expensive.
 		color = vec3(1.0) - exp(-color * 1.5);
 		color = mix(vec3(dot(color, vec3(0.299, 0.587, 0.114))), color, 1.15); // increase saturation because exponential exposure reduces it.
