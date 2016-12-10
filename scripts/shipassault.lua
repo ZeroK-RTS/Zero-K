671e111bc7e67a5ef2bbbc6593013e4249dd12a4
sprunk <gnurps@o2.pl>
2016-12-10 20:20:47 +0100

    Siren won't emit wakes when cloaked.
    Fixes #1201

--- scripts/shipassault.lua
+++ scripts/shipassault.lua
@@ -27,8 +27,10 @@ local function Wake()
 	Signal(SIG_MOVE)
 	SetSignalMask(SIG_MOVE)
 	while true do
-		EmitSfx(wake1, 2)
-		EmitSfx(wake2, 2)
+		if not Spring.GetUnitIsCloaked(unitID) then
+			EmitSfx(wake1, 2)
+			EmitSfx(wake2, 2)
+		end
 		Sleep(200)
 	end
 end
