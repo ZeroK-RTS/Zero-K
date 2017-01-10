fc37c1aeb6728c0e65d3c8cc75075359708b15b3
GoogleFrog <googl3frog@gmail.com>
2017-01-11 00:01:51 +1100

    Added a gadget to limit nanoframes to one per constructor per game frame. The limit is set this way because the gadget deletes the order instead of postponing it so the UI is quite bad.

---
+++ LuaRules/Gadgets/unit_nanoframe_rate_limit.lua
@@ -0,0 +1,44 @@
+-- $Id: unit_terraform.lua 3299 2008-11-25 07:25:57Z google frog $
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function gadget:GetInfo()
+	return {
+		name     = "Nano Frame Rate Limit",
+		desc     = "Limits the rate at which constructors (not factories or carriers) can make nanoframes.",
+		author	 = "GoogleFrog",
+		date     = "January 10, 2017",
+		license	 = "GNU GPL, v2 or later",
+		layer    = -10,
+		enabled  = true
+	}
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+if (not gadgetHandler:IsSyncedCode()) then
+	return false	--	no unsynced code
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local unitLastFrame = {}
+local FRAME_GAP = 1
+
+function gadget:AllowUnitCreation(udefID, builderID)
+	Spring.Echo("AllowUnitCreation", builderID)
+	if not builderID then
+		return true
+	end
+	local frame = Spring.GetGameFrame()
+	if (not unitLastFrame[builderID]) or frame >= unitLastFrame[builderID] + FRAME_GAP then
+		unitLastFrame[builderID] = frame
+		return true
+	end
+	return false
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
