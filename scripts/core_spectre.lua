806852966e4367530761e3196c0221a7bbf66209
sprunk <gnurps@o2.pl>
2017-01-31 11:36:29 +0100

    Aspis smokes.

--- scripts/core_spectre.lua
+++ scripts/core_spectre.lua
@@ -151,7 +151,8 @@ function script.Create()
 	Turn(lb_leaf, z_axis, -l_angle, 1)
 	Turn(rb_leaf, x_axis, -l_angle, 1)
 	Turn(rb_leaf, z_axis, l_angle, 1)
-	
+
+	StartThread(SmokeUnit, {glow})
 	StartThread(Flutter)
 end
 
