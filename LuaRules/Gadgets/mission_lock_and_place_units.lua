d6d2ed2273e6ff0cf18b661e2e479bc4ae692a2f
GoogleFrog <googl3frog@gmail.com>
2017-02-07 00:22:53 +1100

    Added spawning units at game start.

--- LuaRules/Gadgets/mission_lock_and_place_units.lua
+++ LuaRules/Gadgets/mission_lock_and_place_units.lua
@@ -32,6 +32,69 @@ local unlockedUnitsByTeam = {}
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+-- Utility
+
+local BUILD_RESOLUTION = 16
+
+local function CustomKeyToUsefulTable(dataRaw)
+	if not dataRaw then
+		return
+	end
+	if not (dataRaw and type(dataRaw) == 'string') then
+		if dataRaw then
+			Spring.Echo("Customkey data error for team", teamID)
+		end
+	else
+		dataRaw = string.gsub(dataRaw, '_', '=')
+		dataRaw = Spring.Utilities.Base64Decode(dataRaw)
+		local dataFunc, err = loadstring("return " .. dataRaw)
+		if dataFunc then 
+			local success, usefulTable = pcall(dataFunc)
+			if success then
+				return usefulTable
+			end
+		end
+		if err then
+			Spring.Echo("Customkey error", err)
+		end
+	end
+end
+
+local function PlaceUnit(unitData, teamID)
+	local name = unitData.name
+	local ud = UnitDefNames[name]
+	if not (ud and ud.id) then
+		Spring.Echo("Missing unit placement", name)
+		return
+	end
+	
+	local x, z, facing = unitData.x, unitData.z, unitData.facing
+	
+	if ud.isBuilding or ud.speed == 0 then
+		local oddX = (ud.xsize % 4 == 2)
+		local oddZ = (ud.zsize % 4 == 2)
+		
+		if facing % 2 == 1 then
+			oddX, oddZ = oddZ, oddX
+		end
+		
+		if oddX then
+			x = math.floor((x + 8)/BUILD_RESOLUTION)*BUILD_RESOLUTION - 8
+		else
+			x = math.floor(x/BUILD_RESOLUTION)*BUILD_RESOLUTION
+		end
+		if oddZ then
+			z = math.floor((z + 8)/BUILD_RESOLUTION)*BUILD_RESOLUTION - 8
+		else
+			z = math.floor(z/BUILD_RESOLUTION)*BUILD_RESOLUTION
+		end
+	end
+	
+	Spring.CreateUnit(ud.id, x, Spring.GetGroundHeight(x,z), z, facing, teamID)
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Implement the locks
 
 local function RemoveUnit(unitID, lockDefID)
@@ -93,41 +156,43 @@ end
 --------------------------------------------------------------------------------
 -- Initialization
 
-local function InitializeUnlocks()
+local function SetTeamUnlocks(teamID, customKeys)
+	local unlockData = CustomKeyToUsefulTable(customKeys and customKeys.campaignunlocks)
+	if not unlockData then
+		return
+	end
+	local unlockedUnits = {}
+	local unlockCount = 0
+	for i = 1, #unlockData do
+		local ud = UnitDefNames[unlockData[i]]
+		if ud and ud.id then
+			unlockCount = unlockCount + 1
+			Spring.SetTeamRulesParam(teamID, "unlockedUnit" .. unlockCount, ud.name, alliedTrueTable)
+			unlockedUnits[ud.id] = true
+		end
+	end
+	Spring.SetTeamRulesParam(teamID, "unlockedUnitCount", unlockCount, alliedTrueTable)
+	unlockedUnitsByTeam[teamID] = unlockedUnits
+end
+
+local function PlaceTeamUnits(teamID, customKeys)
+	local unitData = CustomKeyToUsefulTable(customKeys and customKeys.extrastartunits)
+	if not unitData then
+		return
+	end
+	
+	for i = 1, #unitData do
+		PlaceUnit(unitData[i], teamID)
+	end
+end
+
+local function InitializeUnlocksAndPlacement()
 	local teamList = Spring.GetTeamList()
 	for i = 1, #teamList do
 		local teamID = teamList[i]
 		local customKeys = select(7, Spring.GetTeamInfo(teamID))
-		local unlocksRaw = customKeys and customKeys.campaignunlocks
-		if not (unlocksRaw and type(unlocksRaw) == 'string') then
-			if unlocksRaw then
-				Spring.Echo("Unlock data entry for player " .. teamID .. " is in invalid format")
-			end
-		else
-			unlocksRaw = string.gsub(unlocksRaw, '_', '=')
-			unlocksRaw = Spring.Utilities.Base64Decode(unlocksRaw)
-			local unlockFunc, err = loadstring("return " .. unlocksRaw)
-			if unlockFunc then 
-				local success, unlockData = pcall(unlockFunc)
-				if success then
-					local unlockedUnits = {}
-					local unlockCount = 0
-					for i = 1, #unlockData do
-						local ud = UnitDefNames[unlockData[i]]
-						if ud and ud.id then
-							unlockCount = unlockCount + 1
-							Spring.SetTeamRulesParam(teamID, "unlockedUnit" .. unlockCount, ud.name, alliedTrueTable)
-							unlockedUnits[ud.id] = true
-						end
-					end
-					Spring.SetTeamRulesParam(teamID, "unlockedUnitCount", unlockCount, alliedTrueTable)
-					unlockedUnitsByTeam[teamID] = unlockedUnits
-				end
-			end
-			if err then
-				Spring.Echo("Unlocks error", err)
-			end
-		end
+		SetTeamUnlocks(teamID, customKeys)
+		PlaceTeamUnits(teamID, customKeys)
 	end
 end
 
@@ -143,7 +208,7 @@ function Unlocks.GetIsUnitUnlocked(teamID, unitDefID)
 end
 
 function gadget:Initialize()
-	InitializeUnlocks()
+	InitializeUnlocksAndPlacement()
 	
 	local allUnits = Spring.GetAllUnits()
 	for _, unitID in pairs(allUnits) do
