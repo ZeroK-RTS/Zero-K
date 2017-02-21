88a1e1f2fa2a5b63a807b1472ea9d9925155d051
sprunk <gnurps@o2.pl>
2017-02-21 16:26:10 +0100

    Don't reapply waterlevel on luarules reload

--- LuaRules/Gadgets/start_waterlevel.lua
+++ LuaRules/Gadgets/start_waterlevel.lua
@@ -12,6 +12,10 @@ local FLOODED_AREA = 0.5 -- (0; 1]
 local FLOOD_OFFSET = -6 -- often the median of map height will be some large flat area. Setting waterlevel very close to any flat plane will result in major clipping ugliness (since bumpmapped water (the default) ebbs and flows) so it needs to be a few elmos off (but not too much so as not to skew the ratio). At -6 everything (both ships and ground) can path so it's a decent choice.
 
 function gadget:Initialize()
+	if Spring.GetGameFrame () > 1 then
+		return
+	end
+
 	local waterlevel = Spring.GetModOptions().waterlevel or 0
 
 	local preset = Spring.GetModOptions().waterpreset or "manual"
