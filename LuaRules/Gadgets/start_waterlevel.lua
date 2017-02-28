0d2608da556b6518207cb1223e41cef619d2bb8a
GoogleFrog <googl3frog@gmail.com>
2017-02-28 21:58:56 +1100

    Fix flood modoption.

--- LuaRules/Gadgets/start_waterlevel.lua
+++ LuaRules/Gadgets/start_waterlevel.lua
@@ -9,9 +9,9 @@ function gadget:GetInfo() return {
 local DRY_WATERLEVEL = -1000
 local FLOODED_AREA = 0.5 -- (0; 1]
 
-local FLOOD_OFFSET = -6 -- often the median of map height will be some large flat area. Setting waterlevel very close to any flat plane will result in major clipping ugliness (since bumpmapped water (the default) ebbs and flows) so it needs to be a few elmos off (but not too much so as not to skew the ratio). At -6 everything (both ships and ground) can path so it's a decent choice.
+local FLOOD_OFFSET = -72 -- often the median of map height will be some large flat area. Setting waterlevel very close to any flat plane will result in major clipping ugliness. This ofset is intended to make most of the map covered by a decent depth of sea.
 
-function gadget:Initialize()
+function gadget:Initialize() -- GamePreload causes issues with widgets.
 	if Spring.GetGameFrame () > 1 then
 		return
 	end
