2a787cb4b771c4bb336a6e285c19714e029f2295
Enrico Donnici <enricodonnici@gmail.com>
2017-03-01 02:09:50 +0100

    Analytics widget: * Added design events for categories of units, such as "Builders" or "Factories"  * Fixed the "start_game_proper" event

--- LuaUI/Widgets/api_game_analytics.lua
+++ LuaUI/Widgets/api_game_analytics.lua
@@ -25,8 +25,88 @@ local myTeamID
 
 local ANALYTICS_EVENT = "analyticsEvent_"
 local ANALYTICS_EVENT_ERROR = "analyticsEventError_"
+local ANALYTICS_VERSION = "v2"
+local IS_DRY_RUN = false -- Whether to print the data instead of sending it
 
 local luaMenu = Spring.GetMenuName and Spring.SendLuaMenuMsg and Spring.GetMenuName()
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Unit Categories
+
+VFS.Include("LuaRules/Utilities/tablefunctions.lua")
+
+
+local unitCategories = {}
+
+unitCategories.basicEnergyBuildings = {
+	armsolar     = true,
+	armwin       = true,
+}
+
+unitCategories.advancedEnergyBuildings = {
+	amgeo  = true, 
+	armfus = true, 
+	cafus  = true, 
+	geo    = true, 
+}
+
+unitCategories.energyBuildings = Spring.Utilities.MergeTable(	unitCategories.basicEnergyBuildings, 
+																unitCategories.advancedEnergyBuildings)
+
+unitCategories.lightGroundTurrets = {
+	armartic = true,
+	armdeva  = true,
+	corllt   = true,
+}              
+
+unitCategories.heavyGroundTurrets = {
+	armanni  = true,
+	corbhmth = true,
+	corhlt   = true,
+	corgrav  = true,
+	cortl    = true,
+	cordoom  = true,
+}
+
+unitCategories.basicFactories = {
+	factorycloak  = true,
+	factoryhover  = true,
+	factoryshield = true,
+	factoryspider = true,
+	factoryveh    = true,
+}
+
+unitCategories.advancedFactories = {
+	factoryamph    = true,
+	factorygunship = true,
+	factoryjump    = true,
+	factoryplane   = true,
+	factoryship    = true,
+	factorytank    = true,
+}
+
+unitCategories.factories = Spring.Utilities.MergeTable(	unitCategories.basicFactories,
+														unitCategories.advancedFactories)
+
+unitCategories.builders = {
+	amphcon  = true,
+	armrectr = true,
+	coracv   = true,
+	corfast  = true,
+	corch    = true,
+	cornecro = true,
+	corned   = true,
+}
+
+unitCategories.gameEnder = {
+	zenith    = true,
+	satellite = true,
+	raveparty = true,
+	armbanth  = true, -- Not really a game ender, but beginners shouldn't build it early.
+	armorco   = true,
+}
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -38,11 +118,15 @@ function Analytics.SendOnetimeEvent(eventName, value)
 	if not luaMenu then
 		return
 	end
-	
+
+	local Send = IS_DRY_RUN and Spring.Echo or Spring.SendLuaMenuMsg
+
+	local eventNameWithVersion = "game_" .. ANALYTICS_VERSION .. ":" .. eventName;
+
 	if value then
-		Spring.SendLuaMenuMsg(ANALYTICS_EVENT .. eventName .. "|" .. value)
+		Send(ANALYTICS_EVENT .. eventNameWithVersion .. "|" .. value)
 	else
-		Spring.SendLuaMenuMsg(ANALYTICS_EVENT .. eventName)
+		Send(ANALYTICS_EVENT .. eventNameWithVersion)
 	end
 end
 
@@ -60,6 +144,14 @@ end
 --------------------------------------------------------------------------------
 -- Analytics collectors
 
+
+function widget:GameFrame(frameNumber)
+	if frameNumber == 0 then		
+		Analytics.SendOnetimeEventWithTime("start_game_proper")
+		widgetHandler:RemoveCallIn("GameFrame")
+	end
+end
+
 local loadSent = false
 local startposSent = false
 
@@ -73,11 +165,11 @@ function widget:Update(dt)
 		loadSent = true
 		
 		if spectating then
-			Analytics.SendOnetimeEvent("game:loaded_spectator")
+			Analytics.SendOnetimeEvent("loaded_spectator")
 			widgetHandler:RemoveCallIn("Update")
 			return
 		else
-			Analytics.SendOnetimeEvent("game:loaded_player")
+			Analytics.SendOnetimeEvent("loaded_player")
 		end
 	end
 	myTeamID = Spring.GetMyTeamID()
@@ -86,35 +178,39 @@ function widget:Update(dt)
 		local x,y,z = Spring.GetTeamStartPosition(myTeamID)
 		if x then
 			startposSent = true
-			Analytics.SendOnetimeEventWithTime("game:startpoint_placed")
+			Analytics.SendOnetimeEventWithTime("startpoint_placed")
 		end
 	end
-end
-
-function widget:GameStart()
-	Analytics.SendOnetimeEventWithTime("game:start_game_propper")
 end
 
 function widget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
 	if unitTeam ~= myTeamID then
 		return
 	end
+	
 	local ud = unitDefID and UnitDefs[unitDefID]
 	if not ud then
 		return
 	end
-	
+
 	if builderID then
-		-- Only trigger for build units. Not capture, share, Claw, commander spawn etc..
-		Analytics.SendOnetimeEventWithTime("game:unit:first_built:" .. ud.name)
+		-- Only trigger for builtunits. Not capture, share, Claw, commander spawn etc..
+		Analytics.SendOnetimeEventWithTime("unit:first_start_building:" .. ud.name)
+		
+		for categoryName, units in pairs(unitCategories) do
+			if units[ud.name] then
+				Analytics.SendOnetimeEventWithTime("unit_category:first_start_building_" .. categoryName)
+			end
+		end
+		
 	end
 end
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
--- Widget Interface
-
 function widget:Initialize() 
 	WG.Analytics = Analytics
-	Analytics.SendOnetimeEvent("game:begin_load")
+	Analytics.SendOnetimeEvent("begin_load")
 end
+
+
+
+
