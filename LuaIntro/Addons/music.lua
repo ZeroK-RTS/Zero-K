4259cacd48436f36a7f4ebd292f1d0e58a98066c
GoogleFrog <googl3frog@gmail.com>
2017-03-15 19:54:08 +1100

    Added loading music widget.

---
+++ LuaIntro/Addons/music.lua
@@ -0,0 +1,39 @@
+
+if addon.InGetInfo then
+	return {
+		name    = "Music",
+		desc    = "plays music",
+		author  = "jK",
+		date    = "2012,2013",
+		license = "GPL2",
+		layer   = 0,
+		depend  = {"LoadProgress"},
+		enabled = true,
+	}
+end
+
+------------------------------------------
+
+function addon.DrawLoadScreen()
+	local loadProgress = SG.GetLoadProgress()
+
+	-- fade in & out music with progress
+	if (loadProgress > 0.9) then
+		Spring.SetSoundStreamVolume(1 - ((loadProgress - 0.9) * 10))
+	end
+end
+
+function addon.Shutdown()
+	Spring.StopSoundStream()
+	Spring.SetSoundStreamVolume(1)
+end
+
+function addon.Initialize()
+	Spring.SetSoundStreamVolume(1)
+	local musicfiles = VFS.DirList("sounds/music/loading", "*.ogg")
+	Spring.Echo("musicfiles", #musicfiles)
+	if (#musicfiles > 0) then
+		Spring.PlaySoundStream(musicfiles[ math.random(#musicfiles) ], 1)
+		Spring.SetSoundStreamVolume(1)
+	end
+end
