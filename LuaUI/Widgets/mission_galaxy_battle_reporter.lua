67ddf53cbe0a3715e312a4362a05106359046092
GoogleFrog <googl3frog@gmail.com>
2017-04-16 00:10:50 +1000

    Send planet battle loss to luamenu.

--- LuaUI/Widgets/mission_galaxy_battle_reporter.lua
+++ LuaUI/Widgets/mission_galaxy_battle_reporter.lua
@@ -24,6 +24,7 @@ end
 -- Anyone could write a widget to send this message. However, they would only be
 -- cheating themselves.
 local WIN_MESSAGE = "Campaign_PlanetBattleWon"
+local LOST_MESSAGE = "Campaign_PlanetBattleLost"
 local myAllyTeamID = Spring.GetMyAllyTeamID()
 
 --------------------------------------------------------------------------------
@@ -36,10 +37,19 @@ local function SendVictoryToLuaMenu(planetID)
 	end
 end
 
+local function SendDefeatToLuaMenu(planetID)
+	local luaMenu = Spring.GetMenuName and Spring.SendLuaMenuMsg and Spring.GetMenuName()
+	if luaMenu then
+		Spring.SendLuaMenuMsg(LOST_MESSAGE .. planetID)
+	end
+end
+
 function widget:GameOver(winners)
 	for i = 1, #winners do
 		if winners[i] == myAllyTeamID then
 			SendVictoryToLuaMenu(campaignBattleID)
+			return
 		end
 	end
+	SendDefeatToLuaMenu(campaignBattleID)
 end
