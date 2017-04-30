5443ea6b67abcba1d8e87e96379e3188bbb19fcc
GoogleFrog <googl3frog@gmail.com>
2017-04-30 21:37:59 +1000

    Renamed unitdefs.

--- units/hoverskirm.lua
+++ units/hoverskirm.lua
@@ -1,61 +1,75 @@
 unitDef = {
-  unitname            = [[hoverskirm2]],
-  name                = [[Trisula]],
-  description         = [[Light Assault/Battle Hovercraft]],
-  acceleration        = 0.03,
-  brakeRate           = 0.043,
-  buildCostMetal      = 180,
+  unitname            = [[hoverskirm]],
+  name                = [[Scalpel]],
+  description         = [[Skirmisher/Anti-Heavy Hovercraft]],
+  acceleration        = 0.0435,
+  activateWhenBuilt   = true,
+  brakeRate           = 0.205,
+  buildCostMetal      = 220,
   builder             = false,
-  buildPic            = [[hoverskirm2.png]],
+  buildPic            = [[hoverskirm.png]],
   canGuard            = true,
   canMove             = true,
   canPatrol           = true,
   category            = [[HOVER]],
   collisionVolumeOffsets = [[0 0 0]],
-  collisionVolumeScales  = [[50 25 50]],
+  collisionVolumeScales  = [[38 38 38]],
+
   collisionVolumeType    = [[ellipsoid]], 
   corpse              = [[DEAD]],
 
   customParams        = {
-    helptext       = [[The Trisula is a fairly fast, sturdy combatant armed with a scatter beam weapon that can erase multiple small targets or slag a single large one.]],
+    description_de = [[Skirmisher Gleiter (Anti-Heavy)]],
+    description_fr = [[Hovercraft Tirailleur]],
+    helptext       = [[Use the Scalpel for hit-and-run attacks. Has a long reload time and not too many hit points, and should always be kept at range with the enemy. An arcing projectile allows it to shoot over obstacles and friendly units.]],
+    helptext_de    = [[Nutze den Scalpel für Schlag-und-Renn Attacken. Er hat eine lange Nachladezeit und nicht allzu viele Lebenspunkte. Er sollte immer auf Distanz zum Feind gehalten werden. Die bogenförmige Schussbahn ermöglicht es über Hindernisse und freundliche Einheiten zu schießen.]],
+    helptext_fr    = [[Le Scalpel est un tirailleur, il est utile pour harrasser l'ennemi ? l'aide de son lance roquette. Il tire des roquettes ? t?te chercheuse au dessus des obstacles, mais son temps de rechargement, sa maniabilit? et son faible blindage le rendent vuln?rable aux contre attaques.]],
+	modelradius    = [[19]],
+	turnatfullspeed = [[1]],
   },
 
   explodeAs           = [[BIG_UNITEX]],
-  footprintX          = 2,
-  footprintZ          = 2,
+  footprintX          = 3,
+  footprintZ          = 3,
   iconType            = [[hoverskirm]],
   idleAutoHeal        = 5,
   idleTime            = 1800,
-  maxDamage           = 1300,
-  maxSlope            = 36,
-  maxVelocity         = 2.5,
+  leaveTracks         = true,
+  maxDamage           = 680,
+  maxSlope            = 18,
+  maxVelocity         = 2.1,
+  maxWaterDepth       = 22,
   minCloakDistance    = 75,
   movementClass       = [[HOVER3]],
-  noChaseCategory     = [[TERRAFORM FIXEDWING SUB]],
-  objectName          = [[hoverskirm.s3o]],
-  onoffable           = true,
+  moveState           = 0,
+  noAutoFire          = false,
+  noChaseCategory     = [[TERRAFORM SATELLITE SUB]],
+  objectName          = [[nsaclash.s3o]],
+  script              = [[hoverskirm.lua]],
   selfDestructAs      = [[BIG_UNITEX]],
 
   sfxtypes            = {
 
     explosiongenerators = {
       [[custom:HEAVYHOVERS_ON_GROUND]],
-      [[custom:RAIDMUZZLE]],
-      [[custom:flashmuzzle1]],
+      [[custom:JANUSMUZZLE]],
+      [[custom:JANUSBACK]],
     },
 
   },
 
-  sightDistance       = 450,
+  sightDistance       = 484,
+  sonarDistance       = 484,  
   turninplace         = 0,
-  turnRate            = 500,
-
+  turnRate            = 420,
+  workerTime          = 0,
+  
   weapons             = {
 
     {
-      def                = [[SCATTER_LASER]],
-      badTargetCategory  = [[FIXEDWING]],
-      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+      def                = [[MISSILE]],
+	  badTargetCategory  = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER SUB]],
     },
 
   },
@@ -63,52 +77,62 @@ unitDef = {
 
   weaponDefs          = {
 
-    SCATTER_LASER = {
-      name                    = [[Scatter Beam]],
-      areaOfEffect            = 32,
-      beamDecay               = 0.85,
-      beamTime                = 1/30,
-      beamttl                 = 45,
-      coreThickness           = 0.5,
-      craterBoost             = 0,
-      craterMult              = 0,
-
+    MISSILE = {
+      name                    = [[Heavy Missile Battery]],
+      areaOfEffect            = 80,
+      cegTag                  = [[missiletrailyellow]],
+      craterBoost             = 1,
+      craterMult              = 1.4,
+	  
+      customParams        = {
+		light_camera_height = 3000,
+		light_color = [[1 0.58 0.17]],
+		light_radius = 200,
+      },
+	  
       damage                  = {
-        default = 40,
-        subs    = 2,
+        default = 311,
       },
 
-      explosionGenerator      = [[custom:flash1red]],
-      fireStarter             = 100,
-      --impactOnly              = true,
-      impulseFactor           = 0,
-      interceptedByShieldType = 1,
-      laserFlareSize          = 7.5,
-      minIntensity            = 1,
-      projectiles             = 9,
-      range                   = 360,
-      reloadtime              = 4,
-      rgbColor                = [[1 0 0]],
-      soundStart              = [[weapon/laser/mini_laser]],
-      sprayangle              = 1640,
-      thickness               = 4,
-      tolerance               = 8192,
+      fireStarter             = 70,
+      fixedlauncher           = true,
+      flightTime              = 3.1,
+      impulseBoost            = 0.75,
+      impulseFactor           = 0.3,
+      interceptedByShieldType = 2,
+	  leadlimit               = 0,
+      model                   = [[wep_m_dragonsfang.s3o]],
+      projectiles             = 2,
+      range                   = 440,
+      reloadtime              = 10,
+      smokeTrail              = true,
+      soundHit                = [[explosion/ex_med5]],
+      soundHitVolume          = 8,
+      soundStart              = [[weapon/missile/rapid_rocket_fire2]],
+      soundStartVolume        = 7,
+      startVelocity           = 190,
+      texture2                = [[lightsmoketrail]],
+      tracks                  = true,
+      trajectoryHeight        = 0.4,
+      turnRate                = 21000,
       turret                  = true,
-      weaponType              = [[BeamLaser]],
+      weaponAcceleration      = 90,
+      weaponType              = [[MissileLauncher]],
+      weaponVelocity          = 180,
     },
-
+	
   },
+
 
   featureDefs         = {
 
     DEAD  = {
-      blocking         = false,
+      blocking         = true,
       featureDead      = [[HEAP]],
       footprintX       = 2,
       footprintZ       = 2,
-      object           = [[hoverskirm_dead.s3o]],
+      object           = [[nsaclash_dead.s3o]],
     },
-
 
     HEAP  = {
       blocking         = false,
@@ -121,4 +145,4 @@ unitDef = {
 
 }
 
-return lowerkeys({ hoverskirm2 = unitDef })
+return lowerkeys({ hoverskirm = unitDef })
