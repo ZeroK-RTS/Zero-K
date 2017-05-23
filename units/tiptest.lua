357b50c6e3c9bc41b756165fcbe622ef3645e40d
sprunk <gnurps@o2.pl>
2017-05-23 15:37:34 +0200

    Fix tiptest

--- units/tiptest.lua
+++ units/tiptest.lua
@@ -3,13 +3,12 @@ local unitDef = included.vehassault
 
 unitDef.unitname = "tiptest"
 unitDef.name = "Turn In Place test"
-unitDef.description "Tests turn in place"
-unitDef.customParams.statsname = "vehassault"
+unitDef.description = "Tests turn in place"
 
 unitDef.acceleration = 0.008
-unitDef.maxVelocity = 5
-unitDef.turnRate = 300
+unitDef.maxvelocity = 5
+unitDef.turnrate = 300
 unitDef.turninplace = 0
-unitDef.customParams.turnatfullspeed = 1
+unitDef.customparams.turnatfullspeed = 1
 
 return lowerkeys({ tiptest = unitDef })
