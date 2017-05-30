f046f68edf3dbda968b275d5c6de93fc1f460ac0
GoogleFrog <googl3frog@gmail.com>
2017-05-30 14:08:11 +1000

    Fixed Escarpment-v2.

--- LuaRules/Configs/StartBoxes/Escarpment-v2.lua
+++ LuaRules/Configs/StartBoxes/Escarpment-v2.lua
@@ -1,31 +1,33 @@
+local WIDTH = 2400
+
 return {
 	[0] = {
-		nameLong = "West",
-		nameShort = "W",
+		nameLong = "North",
+		nameShort = "N",
 		startpoints = {
-			{614,4096},
+			{3072, WIDTH/2},
 		},
 		boxes = {
 			{
-				{0,0},
-				{1229,0},
-				{1229,8192},
-				{0,8192},
+				{0, 0},
+				{6144, 0},
+				{6144, WIDTH},
+				{0, WIDTH},
 			},
 		},
 	},
 	[1] = {
-		nameLong = "East",
-		nameShort = "E",
+		nameLong = "South",
+		nameShort = "S",
 		startpoints = {
-			{5530,4096},
+			{3072, 8192 - WIDTH/2},
 		},
 		boxes = {
 			{
-				{4915,0},
-				{6144,0},
-				{6144,8192},
-				{4915,8192},
+				{0, 8192 - WIDTH},
+				{6144, 8192 - WIDTH},
+				{6144, 8192},
+				{0, 8192},
 			},
 		},
 	},
