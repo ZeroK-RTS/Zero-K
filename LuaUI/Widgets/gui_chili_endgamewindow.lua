21239a181fa7a5bc864c9ecdff8fea1afc827dec
GoogleFrog <googl3frog@gmail.com>
2017-06-03 12:41:20 +1000

    Fixed some endgraph brokenness that caused it to periodically spam one update every draw frame. Also made it not do anything when it is not visible, this is a basic requirement! On my computer in http://zero-k.info/Battles/Detail/458881 it would jump up by 0.6% every 15 seconds which could easily cause a noticeable slow period.

--- LuaUI/Widgets/gui_chili_endgamewindow.lua
+++ LuaUI/Widgets/gui_chili_endgamewindow.lua
@@ -483,15 +483,6 @@ function widget:GameOver(winners)
 end
 
 function widget:Update(dt)
-	-- Redraw the currently-displayed stats graph every fifteen seconds
-	local gameSeconds = GetGameSeconds()
-	if (gameSeconds % 15) == 1 then
-		if showingTab == 'stats' then
-			local button = statsSubPanel.buttonPressed or 1
-			statsSubPanel.graphButtons[button].OnClick[1](statsSubPanel.graphButtons[button])
-		end
-	end
-
 	-- If the post-endgame countdown timer has not yet started, don't do anything else
 	-- If the post-endgame countdown has started but not elapsed, decrement it and do nothing else
 	if not showEndgameWindowTimer then
@@ -524,6 +515,17 @@ function widget:Update(dt)
 
 	showEndgameWindowTimer = nil
 	showingEndgameWindow = true
+end
+
+function widget:GameFrame(f)
+	if not window_endgame.visible then
+		return
+	end
+	-- Redraw the currently-displayed stats graph every fifteen seconds
+	if f%450 == 1 and showingTab == 'stats' then
+		local button = statsSubPanel.buttonPressed or 1
+		statsSubPanel.graphButtons[button].OnClick[1](statsSubPanel.graphButtons[button])
+	end
 end
 
 function widget:Shutdown()
