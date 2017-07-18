4c92784e014007b641beb007857425c4bc3c94df
Eshed <ashdnazg@gmail.com>
2017-07-18 11:26:14 +0300

    fix shader warnings and broekn /advmodelshading

--- LuaRules/Gadgets/api_custom_unit_shaders.lua
+++ LuaRules/Gadgets/api_custom_unit_shaders.lua
@@ -140,7 +140,13 @@ local function CompileShader(shader, definitions, plugins)
   --// (this way we can modularize a shader and enable/disable features in it)
   if (definitions or shadows) then
     definitions = definitions or {}
-	table.insert(definitions, 1, "#version 130")
+    hasVersion = false
+    for _, def in pairs(definitions) do
+      hasVersion = hasVersion or string.sub(def,1,string.len("#version")) == "#version"
+    end
+    if not hasVersion then
+      table.insert(definitions, 1, "#version 130")
+    end
     definitions = table.concat(definitions, "\n")
     if (shadows) then
       definitions = definitions .. "\n" .. "#define use_shadows" .. "\n"
