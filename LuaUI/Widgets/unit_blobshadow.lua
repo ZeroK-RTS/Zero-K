a6f2cffb59aacf164c8367e8ad8ad749c1a4ab15
GoogleFrog <googl3frog@gmail.com>
2017-08-06 16:08:07 +1000

    Fixed a build ETA bug. Fixed blob shadows (it had a massive amount of draw duplication) and made it look like it is drawing aoplanes. Did not enable it though.

--- LuaUI/Widgets/unit_blobshadow.lua
+++ LuaUI/Widgets/unit_blobshadow.lua
@@ -13,7 +13,7 @@
 function widget:GetInfo()
   return {
     name      = "BlobShadow",
-    desc      = "shows an blob shadows for aircraft",
+    desc      = "shows blob shadows",
     author    = "jK",
     date      = "2007,2009",
     license   = "GNU GPL, v2 or later",
@@ -26,21 +26,32 @@ local onlyShowOnAirOption = true
 
 local ResetWidget --forward-declared
 
+local BLOB_TEXTURE = 'LuaUI/Images/blob2.dds' -- 'LuaUI/Images/blob.png' vr_grid.png
+local SIZE_MULT = 1.3
+
 local function OnOptionsChange()
-  ResetWidget()
+	ResetWidget()
 end
 
 options_path = 'Settings/Graphics/Unit Visibility/BlobShadow'
 options = {
-  onlyShowOnAir = {
-    name = 'Only on air',
-    desc = 'Only show blob shadows for flying units.  Land units will not display blob shadows',
-    type = 'bool',
-    value = true,
-    advanced = false,
-    OnChange = OnOptionsChange,
-  },
+  --onlyShowOnAir = {
+  --  name = 'Only on air',
+  --  desc = 'Only show blob shadows for flying units.  Land units will not display blob shadows',
+  --  type = 'bool',
+  --  value = true,
+  --  advanced = false,
+  --  OnChange = OnOptionsChange,
+  --},
 }
+
+local shadowUnitDefID = {}
+for i = 1, #UnitDefs do
+	local ud = UnitDefs[i]
+	if (not ud.isImmobile) and ud.isGroundUnit then
+		shadowUnitDefID[i] = true
+	end
+end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -64,60 +75,59 @@ local glPushMatrix    = gl.PushMatrix
 local glPopMatrix     = gl.PopMatrix
 local glPolygonOffset = gl.PolygonOffset
 local insert          = table.insert
-local GetTeamList          = Spring.GetTeamList
-local GetTeamUnits         = Spring.GetTeamUnits
-local GetUnitViewPosition  = Spring.GetUnitViewPosition
-local IsSphereInView       = Spring.IsSphereInView
-local GetUnitDefID         = Spring.GetUnitDefID
-local GetGroundHeight      = Spring.GetGroundHeight
-local GetCameraPosition    = Spring.GetCameraPosition
-local ValidUnitID          = Spring.ValidUnitID
-local GetAllUnits          = Spring.GetAllUnits
+local spGetUnitViewPosition  = Spring.GetUnitViewPosition
+local spIsSphereInView       = Spring.IsSphereInView
+local spGetUnitDefID         = Spring.GetUnitDefID
+local spGetGroundHeight      = Spring.GetGroundHeight
+local spGetCameraPosition    = Spring.GetCameraPosition
+local spValidUnitID          = Spring.ValidUnitID
+local spGetAllUnits          = Spring.GetAllUnits
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 local unitsCount = 0
 local unitsList = {}
+local unitMap = {}
 
+local function AddUnit(unitID, unitDefID)
+	unitDefID = unitDefID or spGetUnitDefID(unitID)
+	if (not shadowUnitDefID[unitDefID]) or unitMap[unitID] then
+		return
+	end
+	unitsCount = unitsCount + 1
+	unitsList[unitsCount] = unitID
+	unitMap[unitID] = unitsCount
+end
 
-local function AddUnit(unitID,unitDefID)
-  
-  local udef = UnitDefs[unitDefID]
-  if not onlyShowOnAirOption or (udef and udef.canFly) then
-    unitsCount = unitsCount + 1
-    unitsList[unitsCount] = (unitID)
-  end
+local function RemoveUnit(unitID)
+	if not unitMap[unitID] then
+		return
+	end
+	
+	local index = unitMap[unitID]
+	unitsList[index] = unitsList[unitsCount]
+	unitMap[unitsList[index]] = index
+	
+	unitMap[unitID] = nil
+	unitsList[unitsCount] = nil
+	unitsCount = unitsCount - 1
 end
 
 function widget:UnitEnteredAir(unitID)
-  if onlyShowOnAirOption then
-    unitsCount = unitsCount + 1
-    unitsList[unitsCount] = (unitID)
-  end
+	RemoveUnit(unitID)
 end
 
 function widget:UnitFinished(unitID, unitDefID)
-  if not onlyShowOnAirOption then
-    AddUnit(unitID,unitDefID)
-  end
+	AddUnit(unitID, unitDefID)
 end
 
 function widget:UnitEnteredLos(unitID, unitDefID)
-  AddUnit(unitID, unitDefID)
+	AddUnit(unitID, unitDefID)
 end
 
 function widget:UnitLeftAir(unitID)
-  if onlyShowOnAirOption then
-    uid = (unitID or _unitID)
-	  for i=1,unitsCount do
-      if unitsList[i] == uid then
-        unitsList[i] = unitsList[unitsCount]
-        unitsCount = unitsCount - 1
-        return
-      end
-	  end
-  end
+	AddUnit(unitID)
 end
 
 
@@ -128,134 +138,133 @@ end
 
 --forward-declared function
 ResetWidget = function()
-  onlyShowOnAirOption = options.onlyShowOnAir.value
-  unitsList = {}
-  unitsCount = 0
-  
-  local units = GetAllUnits() 
-  for _, uid in ipairs(units) do
-    AddUnit(uid, GetUnitDefID(uid))
-  end
+	unitsList = {}
+	unitsCount = 0
+	unitMap = {}
+
+	local units = spGetAllUnits() 
+	for _, unitID in ipairs(units) do
+		AddUnit(unitID)
+	end
 end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 local function MyDrawGroundQuad(wx,wz,quadSize,gy,gy_tl,gy_tr,gy_bl,gy_br,gy_t,gy_b,gy_l,gy_r)
-  --topleft
-  glTexCoord(0,0)
-  glVertex(wx-quadSize,gy_bl,wz-quadSize)
-  glTexCoord(0,0.5)
-  glVertex(wx-quadSize,gy_l,wz)
-  glTexCoord(0.5,0.5)
-  glVertex(wx,gy,wz)
-  glTexCoord(0.5,0)
-  glVertex(wx,gy_t,wz-quadSize)
+	--topleft
+	glTexCoord(0,0)
+	glVertex(wx-quadSize,gy_bl,wz-quadSize)
+	glTexCoord(0,0.5)
+	glVertex(wx-quadSize,gy_l,wz)
+	glTexCoord(0.5,0.5)
+	glVertex(wx,gy,wz)
+	glTexCoord(0.5,0)
+	glVertex(wx,gy_t,wz-quadSize)
 
-  --topright
-  glTexCoord(0.5,0)
-  glVertex(wx,gy_t,wz-quadSize)
-  glTexCoord(0.5,0.5)
-  glVertex(wx,gy,wz)
-  glTexCoord(1,0.5)
-  glVertex(wx+quadSize,gy_r,wz)
-  glTexCoord(1,0)
-  glVertex(wx+quadSize,gy_tr,wz-quadSize)
+	--topright
+	glTexCoord(0.5,0)
+	glVertex(wx,gy_t,wz-quadSize)
+	glTexCoord(0.5,0.5)
+	glVertex(wx,gy,wz)
+	glTexCoord(1,0.5)
+	glVertex(wx+quadSize,gy_r,wz)
+	glTexCoord(1,0)
+	glVertex(wx+quadSize,gy_tr,wz-quadSize)
 
-  --bottomright
-  glTexCoord(0.5,0.5)
-  glVertex(wx,gy,wz)
-  glTexCoord(0.5,1)
-  glVertex(wx,gy_b,wz+quadSize)
-  glTexCoord(1,1)
-  glVertex(wx+quadSize,gy_br,wz+quadSize)
-  glTexCoord(1,0.5)
-  glVertex(wx+quadSize,gy_r,wz)
+	--bottomright
+	glTexCoord(0.5,0.5)
+	glVertex(wx,gy,wz)
+	glTexCoord(0.5,1)
+	glVertex(wx,gy_b,wz+quadSize)
+	glTexCoord(1,1)
+	glVertex(wx+quadSize,gy_br,wz+quadSize)
+	glTexCoord(1,0.5)
+	glVertex(wx+quadSize,gy_r,wz)
 
-  --bottomleft
-  glTexCoord(0.5,0)
-  glVertex(wx-quadSize,gy_l,wz)
-  glTexCoord(1,0)
-  glVertex(wx-quadSize,gy_bl,wz+quadSize)
-  glTexCoord(1,0.5)
-  glVertex(wx,gy_b,wz+quadSize)
-  glTexCoord(0.5,0.5)
-  glVertex(wx,gy,wz)
+	--bottomleft
+	glTexCoord(0.5,0)
+	glVertex(wx-quadSize,gy_l,wz)
+	glTexCoord(1,0)
+	glVertex(wx-quadSize,gy_bl,wz+quadSize)
+	glTexCoord(1,0.5)
+	glVertex(wx,gy_b,wz+quadSize)
+	glTexCoord(0.5,0.5)
+	glVertex(wx,gy,wz)
 end
 
 local function DrawShadows()
-  -- object position
-  local wx, wy, wz,  dist;
-  --local cx, cy, cz = GetCameraPosition()
+	-- object position
+	local wx, wy, wz,  dist
 
-  -- ground heights
-  local gy;
-  local gy_tl,gy_tr;
-  local gy_bl,gy_br;
-  local gy_t,gy_b;
-  local gy_l,gy_r;
+	-- ground heights
+	local gy
+	local gy_tl,gy_tr
+	local gy_bl,gy_br
+	local gy_t,gy_b
+	local gy_l,gy_r
 
-  local uid;
-  local unitDefID;
-  local quadSize;
-  
-  for i=1,unitsCount do
-    uid = unitsList[i]
-    if ValidUnitID(uid) then
-    
-      -- calculate quad size
-      unitDefID = GetUnitDefID(uid)
-      local xsize = UnitDefs[unitDefID].xsize 
-      if xsize then
-        quadSize = xsize * 4
-      else
-        quadSize = 16
-      end
-      
-      wx, wy, wz = GetUnitViewPosition(uid)
-      --dist = (wx - cx)^2 + (wy - cy)^2 + (wz - cz)^2
-      --if (dist<9000000) then
-        gy = GetGroundHeight(wx, wz)
-        if (IsSphereInView(wx,gy,wz,quadSize)) then
+	local unitID
+	local unitDefID
+	local quadSize
 
-          -- get ground heights
-          gy_tl,gy_tr = GetGroundHeight(wx-quadSize,wz-quadSize),GetGroundHeight(wx+quadSize,wz-quadSize)
-          gy_bl,gy_br = GetGroundHeight(wx-quadSize,wz+quadSize),GetGroundHeight(wx+quadSize,wz+quadSize)
-          gy_t,gy_b = GetGroundHeight(wx,wz-quadSize),GetGroundHeight(wx,wz+quadSize)
-          gy_l,gy_r = GetGroundHeight(wx-quadSize,wz),GetGroundHeight(wx+quadSize,wz)
+	for i=1,unitsCount do
+		unitID = unitsList[i]
+		if spValidUnitID(unitID) then
 
-          MyDrawGroundQuad(wx,wz,quadSize,gy,gy_tl,gy_tr,gy_bl,gy_br,gy_t,gy_b,gy_l,gy_r)
+			-- calculate quad size
+			unitDefID = spGetUnitDefID(unitID)
+			local xsize = UnitDefs[unitDefID].xsize 
+			if xsize then
+				quadSize = xsize * 4
+			else
+				quadSize = 16
+			end
+			quadSize = quadSize * SIZE_MULT
 
-        end
-      --end
-    else
-      RemoveUnitByIndex(i)
-    end
-  end
+			wx, wy, wz = spGetUnitViewPosition(unitID)
+			if wx and wy and wz then
+				gy = spGetGroundHeight(wx, wz)
+				if (spIsSphereInView(wx,gy,wz,quadSize)) then
+
+					-- get ground heights
+					gy_tl,gy_tr = spGetGroundHeight(wx-quadSize,wz-quadSize),spGetGroundHeight(wx+quadSize,wz-quadSize)
+					gy_bl,gy_br = spGetGroundHeight(wx-quadSize,wz+quadSize),spGetGroundHeight(wx+quadSize,wz+quadSize)
+					gy_t,gy_b = spGetGroundHeight(wx,wz-quadSize),spGetGroundHeight(wx,wz+quadSize)
+					gy_l,gy_r = spGetGroundHeight(wx-quadSize,wz),spGetGroundHeight(wx+quadSize,wz)
+
+					MyDrawGroundQuad(wx,wz,quadSize,gy,gy_tl,gy_tr,gy_bl,gy_br,gy_t,gy_b,gy_l,gy_r)
+				end
+			end
+			--end
+		else
+			RemoveUnit(unitID)
+		end
+	end
 end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 function widget:Initialize()
-  ResetWidget()
+	ResetWidget()
 end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 function widget:DrawWorld()
-  glColor(1,1,1,0.4)
-  glDepthTest(true)
-  glTexture('LuaUI/Images/blob.png')
-  glPolygonOffset(-7,-10)
+	glColor(1,1,1,0.4)
+	glDepthTest(true)
+	glTexture(BLOB_TEXTURE)
+	glPolygonOffset(0,-10)
 
-  glBeginEnd(GL_QUADS,DrawShadows)
+	glBeginEnd(GL_QUADS, DrawShadows)
 
-  glPolygonOffset(false)
-  glTexture(false)
-  glDepthTest(false)
-  glColor(1,1,1,1)
+	glPolygonOffset(false)
+	glTexture(false)
+	glDepthTest(false)
+	glColor(1,1,1,1)
 end
 
 --------------------------------------------------------------------------------
