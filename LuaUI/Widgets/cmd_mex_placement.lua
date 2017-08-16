ZeroK-RTS/Zero-K

commit 1abd7984a79cf75bd50721547914379d051505fa

A fix to https://github.com/ZeroK-RTS/Zero-K/issues/2448

LuaUI/Widgets/cmd_mex_placement.lua
@@ -821,7 +821,7 @@ function widget:DrawWorld()
 
 	mexSpotToDraw = false
 
-	if WG.metalSpots and pos and (-mexDefID == cmdID or peruse or CMD_AREA_MEX == cmdID) then
+	if WG.metalSpots and pos and WG.selectionEntirelyCons and (-mexDefID == cmdID or peruse or CMD_AREA_MEX == cmdID) then
 
 		-- Find build position and check if it is valid (Would get 100% metal)
 		local bx, by, bz = Spring.Pos2BuildPos(mexDefID, pos[1], pos[2], pos[3])

commit 834cb5528b6dfe89ed1b883789744a498eca5cb0

Fixed pregame mex placement UI bug caused by https://github.com/lhog/Zero-K/commit/1abd7984a79cf75bd50721547914379d051505fa.

LuaUI/Widgets/cmd_mex_placement.lua
@@ -815,7 +815,8 @@ function widget:DrawWorld()
 	-- Check command is to build a mex
 	local _, cmdID = spGetActiveCommand()
 	local showecoMode = WG.showeco
-	local peruse = spGetGameFrame() < 1 or showecoMode or spGetMapDrawMode() == 'metal'
+	local pregame = (spGetGameFrame() < 1)
+	local peruse = pregame or showecoMode or spGetMapDrawMode() == 'metal'
 
 
 	local mx, my = spGetMouseState()
@@ -823,7 +824,7 @@ function widget:DrawWorld()
 
 	mexSpotToDraw = false
 
-	if WG.metalSpots and pos and WG.selectionEntirelyCons and (-mexDefID == cmdID or peruse or CMD_AREA_MEX == cmdID) then
+	if WG.metalSpots and pos and (pregame or WG.selectionEntirelyCons) and (-mexDefID == cmdID or peruse or CMD_AREA_MEX == cmdID) then
 
 		-- Find build position and check if it is valid (Would get 100% metal)
 		local bx, by, bz = Spring.Pos2BuildPos(mexDefID, pos[1], pos[2], pos[3])