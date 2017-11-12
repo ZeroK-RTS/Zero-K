79c08c64331de778a5a9b093d46cbe814e280cfc
sprunk <spr.ng@o2.pl>
2017-11-12 05:48:13 +0100

    Fix #1414, recommended startpoints for some maps.

--- LuaRules/Configs/StartBoxes/Industrial Revolution v1.lua
+++ LuaRules/Configs/StartBoxes/Industrial Revolution v1.lua
@@ -1,9 +1,14 @@
-return {
+local ret = {
 	[0] = {
 		nameLong = "West",
 		nameShort = "W",
 		startpoints = {
-			{1024,3584},
+			{ 750, 2900},
+			{1580, 4444},
+			{ 557, 5189},
+			{1764, 1707},
+			{2040, 3516},
+			{ 790,  530},
 		},
 		boxes = {
 			{
@@ -17,16 +22,22 @@ return {
 	[1] = {
 		nameLong = "East",
 		nameShort = "E",
-		startpoints = {
-			{9165,3584},
-		},
-		boxes = {
-			{
-				{8192,0},
-				{10138,0},
-				{10138,7168},
-				{8192,7168},
-			},
-		},
+
+		-- automirrored
+		startpoints = { },
+		boxes = { },
 	},
 }
+
+-- mirror west into east
+for i = 1, #ret[0].startpoints do
+	ret[1].startpoints[i] = {Game.mapSizeX - ret[0].startpoints[i][1], ret[0].startpoints[i][2]}
+end
+for i = 1, #ret[0].boxes do
+	ret[1].boxes[i] = {}
+	for j = 1, #ret[0].boxes[i] do
+		ret[1].boxes[i][j] = {Game.mapSizeX - ret[0].boxes[i][j][1], ret[0].boxes[i][j][2]}
+	end
+end
+
+return ret
