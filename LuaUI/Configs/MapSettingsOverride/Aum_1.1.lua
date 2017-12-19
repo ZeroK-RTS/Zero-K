665c7e92d165564bb8c8bf280cb53086425a9b9f
TheMooseIsLoose <lwagner5465@gmail.com>
2017-12-19 15:03:07 -0700

    Aum mapsettings override
    Superior water config for Aum. Bumpwater no longer blindingly bright.

---
+++ LuaUI/Configs/MapSettingsOverride/Aum_1.1.lua
@@ -0,0 +1,12 @@
+return {	
+	["water"] = {
+		["surfaceAlpha"]  = 0.01,
+		["fresnelPower"]  = 2.0,
+		["fresnelMin"]  = 0.0,
+		["fresnelMax"]  = 1.8,
+		["perlinLacunarity"]  = 1.7,
+		["perlinAmplitude"]  = 1.1,
+		["specularFactor"]  = 1.0,
+		["ambientFactor"]  = 1.0,
+	}
+}
