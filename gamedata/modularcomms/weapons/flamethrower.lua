78271aa48d59f1c84d3d6ee84a7737fe21e5cb57
GoogleFrog <googl3frog@gmail.com>
2018-03-11 14:00:35 +1100

    Dante and commander also have 40% burn chance.

--- gamedata/modularcomms/weapons/flamethrower.lua
+++ gamedata/modularcomms/weapons/flamethrower.lua
@@ -15,6 +15,7 @@ local weaponDef = {
 		muzzleEffectFire = [[custom:RAIDMUZZLE]],
 		flamethrower = [[1]],
 		setunitsonfire = "1",
+        burnchance = "0.4", -- Per-impact
 		burntime = [[450]],
 
 		light_camera_height = 2800,
