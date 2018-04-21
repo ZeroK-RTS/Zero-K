6b03fd749aa2d2153279953a24eca2b9178a871e
GoogleFrog <googl3frog@gmail.com>
2018-04-22 00:09:51 +1000

    VERSION{v1.6.4.27} It has come to my attention that not every unit is a Meteor Controller.

--- LuaRules/Utilities/unitDefReplacements.lua
+++ LuaRules/Utilities/unitDefReplacements.lua
@@ -63,7 +63,11 @@ local function GetTerraformTooltip(unitID)
 end
 
 local function GetZenithTooltip (unitID)
-	local meteorsControlled = Spring.GetUnitRulesParam(unitID, "meteorsControlled") or 0
+	local meteorsControlled = Spring.GetUnitRulesParam(unitID, "meteorsControlled")
+	if not meteorsControlled then
+		return
+	end
+	
 	return (WG.Translate("units", "zenith.description") or "Meteor Controller") .. " - " .. (WG.Translate("interface", "meteors_controlled") or "Meteors controlled") .. " " .. (meteorsControlled or "0") .. "/500"
 end
 
