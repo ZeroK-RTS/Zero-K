0c50a07cbcda240281da9be4b1dc64eef39bd2f6
lhog <ccna@mail.ru>
2018-04-22 00:24:27 +0300

    0. Some Spaces --> Tabs 1. Modernized ShieldJitter code a bit 2. ShieldJitter should only be shown on active unit. Fixed that. 3. Fixed funny bug in Lups: https://github.com/ZeroK-RTS/Zero-K/issues/2933

--- lups/lups.lua
+++ lups/lups.lua
@@ -176,7 +176,7 @@ end
 --------------------------------------------------------------------------------
 
 --// some global vars (so the effects can use them)
-vsx, vsy, vpx, vpy = Spring.GetViewGeometry() --// screen pos & view pos (view pos only unequal zero if dualscreen+minimapOnTheLeft) 
+vsx, vsy, vpx, vpy = Spring.Orig.GetViewGeometry() --// screen pos & view pos (view pos only unequal zero if dualscreen+minimapOnTheLeft)
 LocalAllyTeamID = 0
 thisGameFrame   = 0
 frameOffset     = 0
@@ -620,7 +620,7 @@ end
 local function DrawParticlesOpaque()
 	if ( not anyFXVisible ) then return end
 
-	vsx, vsy, vpx, vpy = Spring.GetViewGeometry()
+	vsx, vsy, vpx, vpy = Spring.Orig.GetViewGeometry()
 	if (vsx~=oldVsx)or(vsy~=oldVsy) then
 		for _,partClass in pairs(fxClasses) do
 			if partClass.ViewResize then partClass.ViewResize(vsx, vsy) end
