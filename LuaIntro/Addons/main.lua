950333a7ecab57a308202c8a6f3679227bb08f1f
TheMooseIsLoose <lwagner5465@gmail.com>
2018-05-22 11:32:02 -0700

    add main.lua

--- LuaIntro/Addons/main.lua
+++ LuaIntro/Addons/main.lua
@@ -2,7 +2,7 @@
 if addon.InGetInfo then
 	return {
 		name    = "Main",
-		desc    = "displays a simplae loadbar",
+		desc    = "displays a simple loadbar",
 		author  = "jK",
 		date    = "2012,2013",
 		license = "GPL2",
@@ -14,7 +14,7 @@ end
 
 ------------------------------------------
 local BAR_SCALING = 0.72
-local X_OFFSET = -0.19
+local X_OFFSET = -0.1575
 local Y_OFFSET = -0.02
 
 local lastLoadMessage = ""
@@ -25,7 +25,7 @@ local progressByLastLine = {
 	["Loading Weapon Definitions"] = {10, 50},
 	["Loading LuaRules"] = {40, 80},
 	["Loading LuaUI"] = {70, 95},
-	["Finalizing"] = {100, 100}
+	["Loading Skirmish AIs"] = {100, 100},
 }
 for name,val in pairs(progressByLastLine) do
 	progressByLastLine[name] = {val[1]*0.01, val[2]*0.01}
@@ -41,7 +41,7 @@ end
 
 ------------------------------------------
 
-local font = gl.LoadFont("FreeSansBold.otf", 50, 20, 1.95)
+local font = gl.LoadFont("FreeSansBold.otf", 50, 20, 1.75)
 
 function addon.DrawLoadScreen()
 	local loadProgress = SG.GetLoadProgress()
@@ -63,126 +63,24 @@ function addon.DrawLoadScreen()
 	gl.Scale(BAR_SCALING,BAR_SCALING,1)
 	gl.Translate(X_OFFSET,Y_OFFSET,0)
 	
+	gl.Texture(":n:LuaIntro/Images/barframe.png")
+		gl.TexRect(0.188,0.2194,0.810,0.097)
+	gl.Texture(false)
+	
 	gl.BeginEnd(GL.QUADS, function()
-		--shadow topleft
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2-hsw, 0.2    )
-			gl.Vertex(0.2-hsw, 0.2+vsw)
-			gl.Vertex(0.2    , 0.2+vsw)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.2    , 0.2)
-
-		--shadow top
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2, 0.2+vsw)
-			gl.Vertex(0.8, 0.2+vsw)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8, 0.2)
-			gl.Vertex(0.2, 0.2)
-
-		--shadow topright
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.8    , 0.2+vsw)
-			gl.Vertex(0.8+hsw, 0.2+vsw)
-			gl.Vertex(0.8+hsw, 0.2)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8    , 0.2)
-
-		--shadow right
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.8+hsw, 0.2)
-			gl.Vertex(0.8+hsw, 0.15)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8    , 0.15)
-			gl.Vertex(0.8    , 0.2)
-
-		--shadow btmright
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.8    , 0.15-vsw)
-			gl.Vertex(0.8+hsw, 0.15-vsw)
-			gl.Vertex(0.8+hsw, 0.15)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8    , 0.15)
-
-		--shadow btm
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2, 0.15-vsw)
-			gl.Vertex(0.8, 0.15-vsw)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.2, 0.15)
-
-		--shadow btmleft
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2-hsw, 0.15    )
-			gl.Vertex(0.2-hsw, 0.15-vsw)
-			gl.Vertex(0.2    , 0.15-vsw)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.2    , 0.15)
-
-		--shadow left
-		gl.Color(0,0,0,0)
-			gl.Vertex(0.2-hsw, 0.2)
-			gl.Vertex(0.2-hsw, 0.15)
-		gl.Color(0,0,0,0.5)
-			gl.Vertex(0.2    , 0.15)
-			gl.Vertex(0.2    , 0.2)
-
-		--bar bg
-		gl.Color(0,0,0,0.85)
-			gl.Vertex(0.2, 0.2)
-			gl.Vertex(0.8, 0.2)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.2, 0.15)
-
 		--progress
-		gl.Color(1,1,1,0.7)
-			gl.Vertex(0.2, 0.2)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.2)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.15)
-			gl.Vertex(0.2, 0.15)
-		gl.Color(1,1,1,0.7)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.2)
-			gl.Vertex(0.2 + math.max(0, loadProgress-0.01) * 0.6, 0.15)
-		gl.Color(1,1,1,0)
-			gl.Vertex(0.2 + math.min(1, math.max(0, loadProgress+0.01)) * 0.6, 0.15)
-			gl.Vertex(0.2 + math.min(1, math.max(0, loadProgress+0.01)) * 0.6, 0.2)
-
-		--bar borders
-		gl.Color(1,1,1,1)
-			gl.Vertex(0.2, 0.2)
-			gl.Vertex(0.8, 0.2)
-			gl.Vertex(0.8, 0.2-vbw)
-			gl.Vertex(0.2, 0.2-vbw)
-		gl.Color(1,1,1,1)
-			gl.Vertex(0.2, 0.15)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.8, 0.15+vbw)
-			gl.Vertex(0.2, 0.15+vbw)
-		gl.Color(1,1,1,1)
-			gl.Vertex(0.2, 0.2)
-			gl.Vertex(0.2, 0.15)
-			gl.Vertex(0.2+hbw, 0.15)
-			gl.Vertex(0.2+hbw, 0.2)
-		gl.Color(1,1,1,1)
-			gl.Vertex(0.8, 0.2)
-			gl.Vertex(0.8, 0.15)
-			gl.Vertex(0.8-hbw, 0.15)
-			gl.Vertex(0.8-hbw, 0.2)
+		gl.Color(0.37,0.91,0.97,0.9)
+			gl.Vertex(0.2035, 0.186)
+			gl.Vertex(0.2035 + math.max(0, loadProgress-0.01) * 0.595, 0.186)
+			gl.Vertex(0.2035 + math.max(0, loadProgress-0.01) * 0.595, 0.17)
+			gl.Vertex(0.2035, 0.17)
+		gl.Color(0.22,0.73,0.75,0.95)
+			gl.Vertex(0.2035 + math.max(0, loadProgress-0.01) * 0.595, 0.186)
+			gl.Vertex(0.2035 + math.max(0, loadProgress-0.01) * 0.595, 0.17)
+		gl.Color(0.20,0.67,0.69,0)
+			gl.Vertex(0.2035 + math.min(1, math.max(0, loadProgress+0.01)) * 0.595, 0.17)
+			gl.Vertex(0.2035 + math.min(1, math.max(0, loadProgress+0.01)) * 0.595, 0.186)
 	end)
-
---[[
-	gl.Color(0,0,0,1)
-	gl.Rect(0.2,0.15,0.8,0.2)
-	gl.Color(1,1,1,1)
-	gl.Rect(0.2,0.15,0.2 + math.max(0, loadProgress) * 0.6,0.2)
-	gl.LineWidth(5)
-	gl.PolygonMode(GL.FRONT_AND_BACK, GL.LINE)
-	gl.Rect(0.2,0.15,0.8,0.2)
-	gl.PolygonMode(GL.FRONT_AND_BACK, GL.FILL)
-	gl.LineWidth(1)
-	gl.Color(1,1,1,1)
---]]
 
 	-- progressbar text
 	gl.PushMatrix()
@@ -192,11 +90,11 @@ function addon.DrawLoadScreen()
 	--font:Print(lastLoadMessage, vsx * 0.5, vsy * 0.3, 50, "sc")
 	--font:Print(Game.gameName, vsx * 0.5, vsy * 0.95, vsy * 0.07, "sca")
 	--font:Print(lastLoadMessage, vsx * 0.2, vsy * 0.14, barTextSize*0.5, "sa")
-	font:Print(lastLoadMessage, vsx * 0.5, vsy * 0.12, barTextSize*0.8, "oc")
+	font:Print(lastLoadMessage, vsx * 0.5, vsy * 0.125, barTextSize*0.775, "oc")
 	if loadProgress>0 then
-		font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * 0.165, barTextSize, "oc")
+		font:Print(("%.0f%%"):format(loadProgress * 100), vsx * 0.5, vsy * 0.171, barTextSize*0.65, "oc")
 	else
-		font:Print("Loading...", vsx * 0.5, vsy * 0.165, barTextSize, "oc")
+		font:Print("Loading...", vsx * 0.5, vsy * 0.171, barTextSize*0.65, "oc")
 	end
 	gl.PopMatrix()
 	
