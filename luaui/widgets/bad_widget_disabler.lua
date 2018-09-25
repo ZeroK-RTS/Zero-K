2b97ed0181a174c7df071dba7553f9a36bda6b11
_Shaman <turkishshaman@gmail.com>
2018-09-25 06:47:27 -0700

    Fix various issues
    - Licensing now references my policy instead of the "resignateer" license.
    - Documentation included.
    - Some speedups.
    - Non-suspicious layer.

--- luaui/widgets/bad_widget_disabler.lua
+++ luaui/widgets/bad_widget_disabler.lua
@@ -1,15 +1,30 @@
+--[[Shaman's Policy:
+Do whatever you want with this code.
+]]
+
 function widget:GetInfo()
 	return {
 		name      = "Disable Bad Widgets",
 		desc      = "Disables broken widgets based on config.",
 		author    = "_Shaman",
 		date      = "09/24/18",
-		license   = "Resignateer license",
-		layer     = -9999999999999999999999999999999999999999999999999,
+		license   = "Shaman's Policy",
+		layer     = 5,
 		enabled   = true,
 		alwaysStart = true,
 	}
 end
+
+--[[Documentation:
+The table below here controls what widgets are disabled. To add new widgets to it, simply add the line [2] = "widget human name"
+(NOT the file name! It's the string in the name field of the widget!).
+
+When the widget is loaded, it sees if widgets included in the table are loaded, and disables them if they are.
+After this is completed, the widget removes itself.]]
+
+-- Speed ups --
+local spSendCommands = Spring.SendCommands
+local spEcho = Spring.Echo
 
 local badwidgets = {
 	[1] = "*Mearth Location Tags*1.0",
@@ -18,8 +33,8 @@ local badwidgets = {
 function widget:Initialize()
 	for i=1, #badwidgets do
 		if widgetHandler:FindWidget(badwidgets[i]) then
-			Spring.SendCommands("luaui disablewidget " .. badwidgets[i])
-			Spring.Echo("disabled bad widget " .. badwidgets[i])
+			spSendCommands("luaui disablewidget " .. badwidgets[i])
+			spEcho("disabled bad widget " .. badwidgets[i])
 		end
 	end
 	widgetHandler:RemoveWidget()
