c08aa9cb5d175652df938460b45bf8e3a6e6888a
Matt Vollrath <matt@endpoint.com>
2019-01-12 01:33:34 -0500

    Adjust Knight running animation
    * Simplify animation cycle.
    * Animation rate to match movement speed.
    * Make tracks slightly longer to more closely match animation.
    * Adjust for movement speed modifiers.

--- scripts/cloakassault.lua
+++ scripts/cloakassault.lua
@@ -30,276 +30,104 @@ local SIG_STOPMOVE = 8
 
 local RESTORE_DELAY = 3000
 
+local runspeed = 1.9
+local hangtime = 32
+local steptime = 10
+local stride_top = -0.5
+local stride_bottom = -2.75
+
 
 local function walk()
-	
 	Signal(SIG_MOVE)
 	SetSignalMask(SIG_MOVE)
 
+	local speedmod = 1
+	local truespeed = runspeed
 	while true do
-		Move(hips, y_axis, 0.000000)
-		Move(hips, y_axis, -0.169989, 3.000000)
-		Move(lthigh, y_axis, 0.000000)
-		Move(lthigh, y_axis, 0.169989, 3.000000)
-		Move(rthigh, y_axis, 0.400000)
-		Move(rthigh, y_axis, 0.319989, 1.000000)
-		Turn(hips, x_axis, math.rad(7.027473))
-		Turn(hips, x_axis, math.rad(9.000000), math.rad(50.000000))
-		Turn(lthigh, x_axis, math.rad(17.923077))
-		Turn(lthigh, x_axis, math.rad(20.000000), math.rad(46.000000))
-		Turn(rthigh, x_axis, math.rad(-37.967033))
-		Turn(rthigh, x_axis, math.rad(-33.005495), math.rad(82.000000))
-		Turn(rshin, x_axis, math.rad(17.214286))
-		Turn(rshin, x_axis, math.rad(13.000000), math.rad(78.000000))
-		Turn(rfoot, x_axis, math.rad(-22.505495))
-		Turn(rfoot, x_axis, math.rad(-13.000000), math.rad(175.000000))
-		Turn(lshin, x_axis, 0)
-		Turn(lshin, x_axis, math.rad(10.000000), math.rad(215.000000))
-		Turn(lfoot, x_axis, math.rad(-16.868132))
-		Turn(lfoot, x_axis, math.rad(-10.000000), math.rad(136.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, 0.000000)
-			Move(chest, y_axis, -0.119989, 2.000000)
-			Turn(chest, y_axis, math.rad(-9.137363))
-			Turn(chest, y_axis, math.rad(-7.000000), math.rad(35.000000))
-		end
-		
-		Sleep(49)
+		speedmod = (Spring.GetUnitRulesParam(unitID, "totalMoveSpeedChange") or 1)
+                truespeed = runspeed * speedmod
+		Turn(hips, z_axis, 0.08, truespeed*0.15)
 
-		Move(hips, y_axis, -0.350000, 3.000000)
-		Move(lthigh, y_axis, 0.350000, 3.000000)
-		Move(rthigh, y_axis, 0.250000, 1.000000)
-		Turn(hips, x_axis, math.rad(11.000000), math.rad(50.000000))
-		Turn(lthigh, x_axis, math.rad(22.000000), math.rad(46.000000))
-		Turn(rthigh, x_axis, math.rad(-29.005495), math.rad(82.000000))
-		Turn(rshin, x_axis, math.rad(9.000000), math.rad(78.000000))
-		Turn(rfoot, x_axis, math.rad(8.000000), math.rad(462.000000))
-		Turn(lshin, x_axis, math.rad(21.000000), math.rad(215.000000))
-		Turn(lfoot, x_axis, math.rad(-3.000000), math.rad(136.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, -0.239990, 2.000000)
-			Turn(chest, y_axis, math.rad(-5.000000), math.rad(35.000000))
-		end
-		
-		Sleep(49)
-	
-		Move(hips, y_axis, -0.169989, 4.000000)
-		Move(lthigh, y_axis, 0.369989, 0.000000)
-		Move(rthigh, y_axis, 0.119989, 2.000000)
-		Turn(hips, x_axis, math.rad(9.000000), math.rad(62.000000))
-		Turn(lthigh, x_axis, math.rad(7.000000), math.rad(351.000000))
-		Turn(rthigh, x_axis, math.rad(-20.000000), math.rad(221.000000))
-		Turn(rshin, x_axis, math.rad(7.000000), math.rad(54.000000))
-		Turn(rfoot, x_axis, math.rad(4.000000), math.rad(104.000000))
-		Turn(lshin, x_axis, math.rad(27.005495), math.rad(163.000000))
-		Turn(lfoot, x_axis, math.rad(-10.000000), math.rad(163.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, 0.500000, 7.000000)
-			Turn(chest, y_axis, math.rad(-3.000000), math.rad(46.000000))
-		end
-		
-		Sleep(42)
-		
-		Move(hips, y_axis, 0.000000, 3.000000)
-		Move(lthigh, y_axis, 0.400000, 0.000000)
-		Move(rthigh, y_axis, 0.000000, 2.000000)
-		Turn(hips, x_axis, math.rad(6.000000), math.rad(58.000000))
-		Turn(lthigh, x_axis, math.rad(-7.000000), math.rad(328.000000))
-		Turn(rthigh, x_axis, math.rad(-11.000000), math.rad(207.000000))
-		Turn(rshin, x_axis, math.rad(4.000000), math.rad(50.000000))
-		Turn(rfoot, x_axis, 0, math.rad(97.000000))
-		Turn(lshin, x_axis, math.rad(34.005495), math.rad(152.000000))
-		Turn(lfoot, x_axis, math.rad(-17.000000), math.rad(152.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, 0.350000, 6.000000)
-			Turn(chest, y_axis, math.rad(-1.000000), math.rad(42.000000))
-		end
-		
-		Sleep(45)
-	
-		Move(lthigh, y_axis, 0.700000, 4.000000)
-		Turn(hips, x_axis, math.rad(5.000000), math.rad(22.000000))
-		Turn(lthigh, x_axis, math.rad(-13.000000), math.rad(91.000000))
-		Turn(rthigh, x_axis, 0, math.rad(165.000000))
-		Turn(rfoot, x_axis, math.rad(-8.000000), math.rad(113.000000))
-		Turn(lshin, x_axis, math.rad(23.005495), math.rad(158.000000))
-		Turn(lfoot, x_axis, math.rad(-12.000000), math.rad(69.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, 0.169989, 2.000000)
-			Turn(chest, y_axis, math.rad(2.000000), math.rad(54.000000))
-		end
-		
-		Sleep(71)
-	
-		Move(lthigh, y_axis, 1.000000, 4.000000)
-		Turn(hips, x_axis, math.rad(3.000000), math.rad(21.000000))
-		Turn(lthigh, x_axis, math.rad(-20.000000), math.rad(90.000000))
-		Turn(rthigh, x_axis, math.rad(12.000000), math.rad(163.000000))
-		Turn(rfoot, x_axis, math.rad(-16.000000), math.rad(112.000000))
-		Turn(lshin, x_axis, math.rad(12.000000), math.rad(156.000000))
-		Turn(lfoot, x_axis, math.rad(-7.000000), math.rad(68.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, 0.000000, 2.000000)
-			Turn(chest, y_axis, math.rad(5.000000), math.rad(53.000000))
-		end
-		
-		Sleep(72)
-	
-		Move(lthigh, y_axis, 0.700000, 3.000000)
-		Turn(hips, x_axis, math.rad(5.000000), math.rad(18.000000))
-		Turn(lthigh, x_axis, math.rad(-28.005495), math.rad(92.000000))
-		Turn(rthigh, x_axis, math.rad(14.000000), math.rad(26.000000))
-		Turn(rshin, x_axis, math.rad(2.000000), math.rad(26.000000))
-		Turn(rfoot, x_axis, math.rad(-16.000000), 0)
-		Turn(lshin, x_axis, math.rad(14.000000), math.rad(26.000000))
-		Turn(lfoot, x_axis, math.rad(-16.000000), math.rad(100.000000))
-		
-		if not aiming then
-			Turn(chest, y_axis, math.rad(7.000000), math.rad(18.000000))
-		end
-		
-		Sleep(93)
-	
-		Move(lthigh, y_axis, 0.400000, 3.000000)
-		Turn(hips, x_axis, math.rad(7.000000), math.rad(18.000000))
-		Turn(lthigh, x_axis, math.rad(-37.005495), math.rad(90.000000))
-		Turn(rthigh, x_axis, math.rad(17.000000), math.rad(25.000000))
-		Turn(rshin, x_axis, 0, math.rad(25.000000))
-		Turn(rfoot, x_axis, math.rad(-16.000000), 0)
-		Turn(lshin, x_axis, math.rad(17.000000), math.rad(25.000000))
-		Turn(lfoot, x_axis, math.rad(-26.005495), math.rad(98.000000))
-		
-		if not aiming then
-			Turn(chest, y_axis, math.rad(9.000000), math.rad(18.000000))
-		end
-		
-		Sleep(95)
-	
-		Move(hips, y_axis, -0.169989, 3.000000)
-		Move(lthigh, y_axis, 0.319989, 1.000000)
-		Move(rthigh, y_axis, 0.169989, 3.000000)
-		Turn(hips, x_axis, math.rad(9.000000), math.rad(50.000000))
-		Turn(lthigh, x_axis, math.rad(-33.005495), math.rad(78.000000))
-		Turn(rthigh, x_axis, math.rad(19.000000), math.rad(53.000000))
-		Turn(rshin, x_axis, math.rad(10.000000), math.rad(218.000000))
-		Turn(rfoot, x_axis, math.rad(-8.000000), math.rad(161.000000))
-		Turn(lshin, x_axis, math.rad(13.000000), math.rad(78.000000))
-		Turn(lfoot, x_axis, math.rad(-9.000000), math.rad(344.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, -0.119989, 2.000000)
-			Turn(chest, y_axis, math.rad(7.000000), math.rad(39.000000))
-		end
-		
-		Sleep(49)
+		Turn(rthigh, x_axis, -0.65, truespeed*1.25)
+		Turn(rshin, x_axis, 0.8, truespeed*1.25)
+		Turn(rfoot, x_axis, 0, truespeed*0.5)
 
-		Move(hips, y_axis, -0.350000, 3.000000)
-		Move(lthigh, y_axis, 0.250000, 1.000000)
-		Move(rthigh, y_axis, 0.350000, 3.000000)
-		Turn(hips, x_axis, math.rad(11.000000), math.rad(50.000000))
-		Turn(lthigh, x_axis, math.rad(-29.005495), math.rad(78.000000))
-		Turn(rthigh, x_axis, math.rad(22.000000), math.rad(53.000000))
-		Turn(rshin, x_axis, math.rad(21.000000), math.rad(218.000000))
-		Turn(rfoot, x_axis, 0, math.rad(161.000000))
-		Turn(lshin, x_axis, math.rad(9.000000), math.rad(78.000000))
-		Turn(lfoot, x_axis, math.rad(7.000000), math.rad(344.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, -0.239990, 2.000000)
-			Turn(chest, y_axis, math.rad(5.000000), math.rad(39.000000))
-		end
-		
-		Sleep(49)
-	
-		Move(hips, y_axis, -0.169989, 3.000000)
-		Move(lthigh, y_axis, 0.119989, 2.000000)
-		Move(rthigh, y_axis, 0.369989, 0.000000)
-		Turn(hips, x_axis, math.rad(9.000000), math.rad(56.000000))
-		Turn(lthigh, x_axis, math.rad(-20.000000), math.rad(194.000000))
-		Turn(rthigh, x_axis, math.rad(7.000000), math.rad(314.000000))
-		Turn(rshin, x_axis, math.rad(36.005495), math.rad(329.000000))
-		Turn(rfoot, x_axis, math.rad(-5.000000), math.rad(104.000000))
-		Turn(lshin, x_axis, math.rad(7.000000), math.rad(52.000000))
-		Turn(lfoot, x_axis, math.rad(3.000000), math.rad(82.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, 0.500000, 6.000000)
-			Turn(chest, y_axis, math.rad(3.000000), math.rad(37.000000))
-		end
-		
-		Sleep(47)
+		Turn(lshin, x_axis, 0.4, truespeed*0.5)
+		Turn(lthigh, x_axis, 0.5, truespeed*1.25)
+		Turn(lfoot, x_axis, -0.3, truespeed*1)
 
-		Move(hips, y_axis, 0.000000, 3.000000)
-		Move(lthigh, y_axis, 0.000000, 2.000000)
-		Move(rthigh, y_axis, 0.400000, 0.000000)
-		Turn(hips, x_axis, math.rad(6.000000), math.rad(54.000000))
-		Turn(lthigh, x_axis, math.rad(-11.000000), math.rad(190.000000))
-		Turn(rthigh, x_axis, math.rad(-7.000000), math.rad(307.000000))
-		Turn(rshin, x_axis, math.rad(52.005495), math.rad(322.000000))
-		Turn(rfoot, x_axis, math.rad(-10.000000), math.rad(102.000000))
-		Turn(lshin, x_axis, math.rad(4.000000), math.rad(51.000000))
-		Turn(lfoot, x_axis, 0, math.rad(80.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, 0.350000, 6.000000)
-			Turn(chest, y_axis, math.rad(2.000000), math.rad(36.000000))
-		end
-		
-		Sleep(48)
+		Move(hips, y_axis, stride_top, truespeed*4)
 
-		Move(lthigh, y_axis, 0.000000, 0.000000)
-		Move(rthigh, y_axis, 0.700000, 4.000000)
-		Turn(hips, x_axis, math.rad(5.000000), math.rad(21.000000))
-		Turn(lthigh, x_axis, 0, math.rad(161.000000))
-		Turn(rthigh, x_axis, math.rad(-13.000000), math.rad(90.000000))
-		Turn(rshin, x_axis, math.rad(39.005495), math.rad(180.000000))
-		Turn(rfoot, x_axis, math.rad(-7.000000), math.rad(40.000000))
-		Turn(lshin, x_axis, math.rad(4.000000), math.rad(2.000000))
-		Turn(lfoot, x_axis, math.rad(-8.000000), math.rad(109.000000))
-		
 		if not aiming then
-			Move(chest, y_axis, 0.169989, 2.000000)
-			Turn(chest, y_axis, math.rad(-1.000000), math.rad(52.000000))
+			Move(chest, y_axis, -0.15, truespeed*1)
+			Turn(chest, x_axis, -0.08, truespeed*0.25)
+			Turn(chest, y_axis, -0.065, truespeed*0.25)
 		end
-		
-		Sleep(74)
-	
-		Move(lthigh, y_axis, 0.000000, 0.000000)
-		Move(rthigh, y_axis, 1.000000, 3.000000)
-		Turn(hips, x_axis, math.rad(3.000000), math.rad(20.000000))
-		Turn(lthigh, x_axis, math.rad(12.000000), math.rad(157.000000))
-		Turn(rthigh, x_axis, math.rad(-20.000000), math.rad(87.000000))
-		Turn(rshin, x_axis, math.rad(25.005495), math.rad(175.000000))
-		Turn(rfoot, x_axis, math.rad(-4.000000), math.rad(39.000000))
-		Turn(lshin, x_axis, math.rad(4.000000), math.rad(2.000000))
-		Turn(lfoot, x_axis, math.rad(-16.000000), math.rad(106.000000))
-		
-		if not aiming then
-			Move(chest, y_axis, 0.000000, 2.000000)
-			Turn(chest, y_axis, math.rad(-5.000000), math.rad(50.000000))
-		end
-		
-		Sleep(76)
 
-		Move(lthigh, y_axis, 0.000000, 0.000000)
-		Move(rthigh, y_axis, 0.700000, 3.000000)
-		Turn(hips, x_axis, math.rad(5.000000), math.rad(18.000000))
-		Turn(lthigh, x_axis, math.rad(15.000000), math.rad(28.000000))
-		Turn(rthigh, x_axis, math.rad(-29.005495), math.rad(93.000000))
-		Turn(chest, y_axis, math.rad(-7.000000), math.rad(18.000000))
-		Turn(rshin, x_axis, math.rad(21.000000), math.rad(44.000000))
-		Turn(rfoot, x_axis, math.rad(-12.000000), math.rad(86.000000))
-		Turn(lshin, x_axis, math.rad(2.000000), math.rad(22.000000))
-		Turn(lfoot, x_axis, math.rad(-16.000000), math.rad(3.000000))
-		Sleep(94)
+		WaitForMove(hips, y_axis)
+
+		Move(hips, y_axis, stride_bottom, truespeed*1)
+
+		Sleep(hangtime)
+
+		Move(hips, y_axis, stride_bottom, truespeed*4)
+		Turn(rshin, x_axis, 0.0, truespeed*0.75)
+		Turn(rfoot, x_axis, -0.2, truespeed*0.5)
+		Turn(lshin, x_axis, 0.6, truespeed*0.75)
+		Turn(lfoot, x_axis, -0.0, truespeed*1.25)
+
+		if not aiming then
+			Move(chest, y_axis, 0, truespeed*1)
+			Turn(chest, x_axis, 0, truespeed*0.25)
+			Turn(chest, y_axis, 0, truespeed*0.25)
+		end
+
+		WaitForTurn(rthigh, x_axis)
+
+		Sleep(steptime)
+
+		speedmod = (Spring.GetUnitRulesParam(unitID, "totalMoveSpeedChange") or 1)
+                truespeed = runspeed * speedmod
+		Turn(hips, z_axis, -0.08, truespeed*0.15)
+
+		Turn(lthigh, x_axis, -0.65, truespeed*1.25)
+		Turn(lshin, x_axis, 0.8, truespeed*1.25)
+		Turn(lfoot, x_axis, 0, truespeed*0.5)
+
+		Turn(rshin, x_axis, 0.4, truespeed*0.5)
+		Turn(rthigh, x_axis, 0.5, truespeed*1.25)
+		Turn(rfoot, x_axis, -0.3, truespeed*1)
+
+		Move(hips, y_axis, stride_top, truespeed*4)
+
+		if not aiming then
+			Move(chest, y_axis, -0.15, truespeed*1)
+			Turn(chest, x_axis, -0.08, truespeed*0.25)
+			Turn(chest, y_axis, 0.065, truespeed*0.25)
+		end
+
+		WaitForMove(hips, y_axis)
+
+		Move(hips, y_axis, stride_bottom, truespeed*1)
+
+		Sleep(hangtime)
+
+		Move(hips, y_axis, stride_bottom, truespeed*4)
+		Turn(lshin, x_axis, 0.0, truespeed*0.75)
+		Turn(lfoot, x_axis, -0.2, truespeed*0.5)
+		Turn(rshin, x_axis, 0.6, truespeed*0.75)
+		Turn(rfoot, x_axis, -0.0, truespeed*1.25)
+
+		if not aiming then
+			Move(chest, y_axis, 0, truespeed*1)
+			Turn(chest, x_axis, 0, truespeed*0.25)
+			Turn(chest, y_axis, 0, truespeed*0.25)
+		end
+
+		WaitForTurn(lthigh, x_axis)
+
+		Sleep(steptime)
 	end
-
 end
 
 function script.Create()
@@ -324,19 +152,22 @@ local function StopMovingThread()
 	walking = false
 	Signal(SIG_MOVE)
 
-	Turn(rthigh, x_axis, 0, math.rad(400.000000))
-	Turn(rshin, x_axis, 0, math.rad(400.000000))
-	Turn(rfoot, x_axis, 0, math.rad(400.000000))
-	Turn(lthigh, x_axis, 0, math.rad(400.000000))
-	Turn(lshin, x_axis, 0, math.rad(400.000000))
-	Turn(lfoot, x_axis, 0, math.rad(400.000000))
+	Turn(hips, z_axis, 0, math.rad(60.0))
+	Move(hips, y_axis, 0, 8.0)
+	Turn(rthigh, x_axis, 0, math.rad(120.000000))
+	Turn(rshin, x_axis, 0, math.rad(240.000000))
+	Turn(rfoot, x_axis, 0, math.rad(120.000000))
+	Turn(lthigh, x_axis, 0, math.rad(120.000000))
+	Turn(lshin, x_axis, 0, math.rad(240.000000))
+	Turn(lfoot, x_axis, 0, math.rad(120.000000))
 	
 	if not aiming then
-		Turn(chest, y_axis, 0, math.rad(180.000000))
-		Turn(rshoulder, x_axis, 0, math.rad(400.000000))
-		Turn(rforearm, x_axis, 0, math.rad(400.000000))
-		Turn(lshoulder, x_axis, 0, math.rad(400.000000))
-		Turn(lforearm, x_axis, 0, math.rad(400.000000))
+		Move(chest, y_axis, 0, 8.0)
+		Turn(chest, y_axis, 0, math.rad(120.000000))
+		Turn(rshoulder, x_axis, 0, math.rad(120.000000))
+		Turn(rforearm, x_axis, 0, math.rad(120.000000))
+		Turn(lshoulder, x_axis, 0, math.rad(120.000000))
+		Turn(lforearm, x_axis, 0, math.rad(120.000000))
 	end
 end
 
