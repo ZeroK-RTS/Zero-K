c1640fe50033303ebc457ec414d52a5cad95c3cf
Dominic Renaud <dre_naud@yahoo.ca>
2019-02-23 18:00:54 -0800

    Improved DoF widget
    Incorporated a separable Frostbite 2-style DoF implementation into the DoF widget.

--- LuaUI/Widgets/Shaders/dof.fs
+++ LuaUI/Widgets/Shaders/dof.fs
@@ -1,25 +1,200 @@
 uniform sampler2D origTex;
-uniform sampler2D blurTex;
-uniform sampler2D mapdepths;
+
+uniform sampler2D blurTex0;
+uniform sampler2D blurTex1;
+uniform sampler2D blurTex2;
 
 uniform vec3 eyePos;
 uniform mat4 viewProjectionInv;
+uniform vec2 resolution;
 
-void main(void) {
-	vec2 C0 = gl_TexCoord[0].st;
-	vec4 orig = texture2D(origTex, C0);
-	vec4 blur = texture2D(blurTex, C0);
-	
-	// use the depth from the terrain depth buffer because reading 2x 32 bit buffers is _insane_.
-	vec4 worldPos = vec4(vec3(gl_TexCoord[0].st, texture2D(mapdepths, C0).x) * 2.0 - 1.0, 1.0);
-	
-	// then convert to worldspace coords
-	worldPos = viewProjectionInv * worldPos;
-	worldPos.xyz = worldPos.xyz / worldPos.w;
-	
-	float dist = length(eyePos.xz - worldPos.xz);
-	float depthFactor = min(dist/(6000.0 + eyePos.y), 0.85);
-	
-	vec4 color = mix(orig, blur, depthFactor);
-	gl_FragColor = vec4(color.rgb, 1.0);
+// uniform float focusDepth;
+// uniform float fstopFactor;
+
+uniform int pass;
+uniform int channel;
+
+// Circular DOF by Kleber Garcia "Kecho" - 2017
+// Publication & Filter generator: https://github.com/kecho/CircularDofFilterGenerator
+
+/** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+**/
+const int KERNEL_RADIUS = 8;
+const int KERNEL_COUNT = 17;
+vec4 Kernel0BracketsRealXY_ImZW = vec4(-0.038708,0.943062,-0.025574,0.660892);
+vec2 Kernel0Weights_RealX_ImY = vec2(0.411259,-0.548794);
+vec4 Kernel0_RealX_ImY_RealZ_ImW[] = vec4[](
+        vec4(/*XY: Non Bracketed*/0.014096,-0.022658,/*Bracketed WZ:*/0.055991,0.004413),
+        vec4(/*XY: Non Bracketed*/-0.020612,-0.025574,/*Bracketed WZ:*/0.019188,0.000000),
+        vec4(/*XY: Non Bracketed*/-0.038708,0.006957,/*Bracketed WZ:*/0.000000,0.049223),
+        vec4(/*XY: Non Bracketed*/-0.021449,0.040468,/*Bracketed WZ:*/0.018301,0.099929),
+        vec4(/*XY: Non Bracketed*/0.013015,0.050223,/*Bracketed WZ:*/0.054845,0.114689),
+        vec4(/*XY: Non Bracketed*/0.042178,0.038585,/*Bracketed WZ:*/0.085769,0.097080),
+        vec4(/*XY: Non Bracketed*/0.057972,0.019812,/*Bracketed WZ:*/0.102517,0.068674),
+        vec4(/*XY: Non Bracketed*/0.063647,0.005252,/*Bracketed WZ:*/0.108535,0.046643),
+        vec4(/*XY: Non Bracketed*/0.064754,0.000000,/*Bracketed WZ:*/0.109709,0.038697),
+        vec4(/*XY: Non Bracketed*/0.063647,0.005252,/*Bracketed WZ:*/0.108535,0.046643),
+        vec4(/*XY: Non Bracketed*/0.057972,0.019812,/*Bracketed WZ:*/0.102517,0.068674),
+        vec4(/*XY: Non Bracketed*/0.042178,0.038585,/*Bracketed WZ:*/0.085769,0.097080),
+        vec4(/*XY: Non Bracketed*/0.013015,0.050223,/*Bracketed WZ:*/0.054845,0.114689),
+        vec4(/*XY: Non Bracketed*/-0.021449,0.040468,/*Bracketed WZ:*/0.018301,0.099929),
+        vec4(/*XY: Non Bracketed*/-0.038708,0.006957,/*Bracketed WZ:*/0.000000,0.049223),
+        vec4(/*XY: Non Bracketed*/-0.020612,-0.025574,/*Bracketed WZ:*/0.019188,0.000000),
+        vec4(/*XY: Non Bracketed*/0.014096,-0.022658,/*Bracketed WZ:*/0.055991,0.004413)
+);
+vec4 Kernel1BracketsRealXY_ImZW = vec4(0.000115,0.559524,0.000000,0.178226);
+vec2 Kernel1Weights_RealX_ImY = vec2(0.513282,4.561110);
+vec4 Kernel1_RealX_ImY_RealZ_ImW[] = vec4[](
+        vec4(/*XY: Non Bracketed*/0.000115,0.009116,/*Bracketed WZ:*/0.000000,0.051147),
+        vec4(/*XY: Non Bracketed*/0.005324,0.013416,/*Bracketed WZ:*/0.009311,0.075276),
+        vec4(/*XY: Non Bracketed*/0.013753,0.016519,/*Bracketed WZ:*/0.024376,0.092685),
+        vec4(/*XY: Non Bracketed*/0.024700,0.017215,/*Bracketed WZ:*/0.043940,0.096591),
+        vec4(/*XY: Non Bracketed*/0.036693,0.015064,/*Bracketed WZ:*/0.065375,0.084521),
+        vec4(/*XY: Non Bracketed*/0.047976,0.010684,/*Bracketed WZ:*/0.085539,0.059948),
+        vec4(/*XY: Non Bracketed*/0.057015,0.005570,/*Bracketed WZ:*/0.101695,0.031254),
+        vec4(/*XY: Non Bracketed*/0.062782,0.001529,/*Bracketed WZ:*/0.112002,0.008578),
+        vec4(/*XY: Non Bracketed*/0.064754,0.000000,/*Bracketed WZ:*/0.115526,0.000000),
+        vec4(/*XY: Non Bracketed*/0.062782,0.001529,/*Bracketed WZ:*/0.112002,0.008578),
+        vec4(/*XY: Non Bracketed*/0.057015,0.005570,/*Bracketed WZ:*/0.101695,0.031254),
+        vec4(/*XY: Non Bracketed*/0.047976,0.010684,/*Bracketed WZ:*/0.085539,0.059948),
+        vec4(/*XY: Non Bracketed*/0.036693,0.015064,/*Bracketed WZ:*/0.065375,0.084521),
+        vec4(/*XY: Non Bracketed*/0.024700,0.017215,/*Bracketed WZ:*/0.043940,0.096591),
+        vec4(/*XY: Non Bracketed*/0.013753,0.016519,/*Bracketed WZ:*/0.024376,0.092685),
+        vec4(/*XY: Non Bracketed*/0.005324,0.013416,/*Bracketed WZ:*/0.009311,0.075276),
+        vec4(/*XY: Non Bracketed*/0.000115,0.009116,/*Bracketed WZ:*/0.000000,0.051147)
+);
+
+vec2 multComplex(vec2 p, vec2 q)
+{
+    return vec2(p.x*q.x-p.y*q.y, p.x*q.y+p.y*q.x);
+}
+
+vec4 getFilters(int x)
+{
+  vec2 c0 = Kernel0_RealX_ImY_RealZ_ImW[x].xy;
+  vec2 c1 = Kernel1_RealX_ImY_RealZ_ImW[x].xy;
+  return vec4(c0.x, c0.y, c1.x, c1.y);
+}
+
+float LinearizeDepth(vec2 uv){   
+    float n = 0.1; // camera z near
+    float f = 100.0; // camera z far
+    return (2.0 * n) / (f + n - texture2D(blurTex0, uv).r * (f - n));
+  }
+
+void main()
+{
+  vec4 fragColor = vec4(0,0,0,0);
+	vec2 uv = gl_TexCoord[0].st;
+
+	if (pass == FILTER_SIZE_PASS)
+	{  
+    vec4 colors = texture2D(origTex, uv);
+    colors = colors * colors;
+
+    float depth = LinearizeDepth(uv);
+    float centerDepth = LinearizeDepth(vec2(0.5,0.5));
+    float focusDepth = centerDepth;
+    float fstopFactor = 0.4/(max(focusDepth, 0.04)) - 1.4;
+
+  	fragColor = vec4(colors.rgb, abs(depth - focusDepth) * fstopFactor);
+  }
+
+  else if (pass == HORIZ_BLUR_PASS)
+  {
+    vec2 stepVal = 1.0/resolution.xy;
+    
+    vec4 val = vec4(0,0,0,0);
+    float filterRadius = texture2D(origTex, uv).a;
+    int compI = 0;
+    float correctionOffset = 0.0;
+    for (int i=-KERNEL_RADIUS; i <=KERNEL_RADIUS; ++i)
+    {
+      compI = i;
+      vec2 coords = uv + stepVal*vec2(float(i),0.0)*filterRadius;
+      float targetFilterRadius = texture2D(origTex, coords).a;
+      if (targetFilterRadius < 1.2 / float(KERNEL_RADIUS))
+      {
+          compI = -i;
+          correctionOffset = compI < 0 ? 0.5 : -0.5;
+          coords = uv + stepVal*vec2(float(compI) + correctionOffset,0.0)*filterRadius;
+          if (texture2D(origTex, coords).a < 1.2 / float(KERNEL_RADIUS))
+            { compI = 0; coords = uv;}
+      }
+      vec4 imageTexelRGB = texture2D(origTex, coords);
+      float imageTexel = 0.0;
+      if (channel == BLUR_CHANNEL_RED) { imageTexel = imageTexelRGB.r; }
+      if (channel == BLUR_CHANNEL_GREEN) { imageTexel = imageTexelRGB.g; }
+      if (channel == BLUR_CHANNEL_BLUE) { imageTexel = imageTexelRGB.b; }
+      vec4 c0_c1 = getFilters(i+KERNEL_RADIUS);
+      val.xy += imageTexel * c0_c1.xy;
+      val.zw += imageTexel * c0_c1.zw;
+    }
+    fragColor = val; 
+	}
+
+	else if (pass == VERT_BLUR_PASS)
+	{
+    vec2 stepVal = 1.0/resolution.xy;
+  
+    vec4 valR = vec4(0,0,0,0);
+    vec4 valG = vec4(0,0,0,0);
+    vec4 valB = vec4(0,0,0,0);
+    float filterRadius = texture2D(origTex, uv).a;
+    int compI = 0;
+    float correctionOffset = 0.0;
+    for (int i=-KERNEL_RADIUS; i <=KERNEL_RADIUS; ++i)
+    {
+    	compI = i;
+      vec2 coords = uv + stepVal*vec2(0.0,float(i))*filterRadius;
+      float targetFilterRadius = texture2D(origTex, coords).a;
+      if (targetFilterRadius < 1.2 / float(KERNEL_RADIUS))
+      {
+          compI = -i;
+          correctionOffset = compI < 0 ? 0.5 : -0.5;
+          coords = uv + stepVal*vec2(float(compI) + correctionOffset,0.0)*filterRadius;
+          if (texture2D(origTex, coords).a < 1.2 / float(KERNEL_RADIUS))
+            { compI = 0; coords = uv;}
+      }
+      vec4 imageTexelR = texture2D(blurTex0, coords);  
+      vec4 imageTexelG = texture2D(blurTex1, coords);  
+      vec4 imageTexelB = texture2D(blurTex2, coords);  
+      
+      vec4 c0_c1 = getFilters(i+KERNEL_RADIUS);
+      
+      
+      valR.xy += multComplex(imageTexelR.xy,c0_c1.xy);
+      valR.zw += multComplex(imageTexelR.zw,c0_c1.zw);
+      
+      valG.xy += multComplex(imageTexelG.xy,c0_c1.xy);
+      valG.zw += multComplex(imageTexelG.zw,c0_c1.zw);
+      
+      valB.xy += multComplex(imageTexelB.xy,c0_c1.xy);
+      valB.zw += multComplex(imageTexelB.zw,c0_c1.zw);       
+    }
+    
+    float redChannel   = dot(valR.xy,Kernel0Weights_RealX_ImY)+dot(valR.zw,Kernel1Weights_RealX_ImY);
+    float greenChannel = dot(valG.xy,Kernel0Weights_RealX_ImY)+dot(valG.zw,Kernel1Weights_RealX_ImY);
+    float blueChannel  = dot(valB.xy,Kernel0Weights_RealX_ImY)+dot(valB.zw,Kernel1Weights_RealX_ImY);
+    fragColor = vec4(sqrt(redChannel),sqrt(greenChannel),sqrt(blueChannel),filterRadius);   
+	}
+
+	else if (pass == COMPOSITION_PASS)
+	{
+    float filterRadius = texture2D(blurTex0, uv).a;
+		fragColor = mix(texture2D(origTex, uv), texture2D(blurTex0, uv), 
+      clamp((filterRadius - 1.2 / float(KERNEL_RADIUS)) * float(KERNEL_RADIUS) * 2.0, 0.0, 1.0));
+    // if (filterRadius > 2.0 / float(KERNEL_RADIUS))
+    //   fragColor = texture2D(blurTex0, uv);
+    // else
+    //   fragColor = texture2D(origTex, uv);
+	}
+
+  gl_FragColor = fragColor;
 }
--- LuaUI/Widgets/gfx_dof.lua
+++ LuaUI/Widgets/gfx_dof.lua
@@ -1,11 +1,11 @@
 function widget:GetInfo()
 	return {
 		name      = "Depth of Field Shader",
-		version	  = 1.0,
+		version	  = 2.0,
 		desc      = "Blurs far away objects.",
-		author    = "aeonios",
-		date      = "Nov. 2016",
-		license   = "GPL",
+		author    = "aeonios, Shadowfury333 (with some code from Kleber Garcia)",
+		date      = "Feb. 2019",
+		license   = "GPL, MIT",
 		layer     = -1,
 		enabled   = true
 	}
@@ -24,9 +24,7 @@ local function onChangeFunc()
 		widget:Initialize()
 	else
 		if glDeleteTexture then
-			glDeleteTexture(blurTex or "")
-			glDeleteTexture(screenTex or "")
-			blurTex, screenTex = nil, nil
+			CleanupTextures()
 		end
 	end
 end
@@ -51,11 +49,28 @@ local glRenderToTexture		 = gl.RenderToTexture
 local glUseShader            = gl.UseShader
 local glGetUniformLocation   = gl.GetUniformLocation
 local glUniform				 = gl.Uniform
+local glUniformInt				 = gl.UniformInt
 local glUniformMatrix		 = gl.UniformMatrix
+
+local GL_DEPTH_COMPONENT   = 0x1902
+local GL_DEPTH_COMPONENT16 = 0x81A5
+local GL_DEPTH_COMPONENT24 = 0x81A6
+local GL_DEPTH_COMPONENT32 = 0x81A7
 
 -----------------------------------------------------------------
 
 
+local function CleanupTextures()
+	glDeleteTexture(baseBlurTex or "")
+	glDeleteTexture(horizBlurTexR or "")
+	glDeleteTexture(horizBlurTexG or "")
+	glDeleteTexture(horizBlurTexB or "")
+	glDeleteTexture(finalBlurTex or "")
+	glDeleteTexture(screenTex or "")
+	glDeleteTexture(depthTex or "")
+	baseBlurTex, horizBlurTexR, horizBlurTexG, horizBlurTexB, finalBlurTex, screenTex, depthTex = 
+		nil, nil, nil, nil, nil, nil, nil
+end
 -----------------------------------------------------------------
 -- Global Vars
 -----------------------------------------------------------------
@@ -64,31 +79,82 @@ local vsx = nil	-- current viewport width
 local vsy = nil	-- current viewport height
 local dofShader = nil
 local screenTex = nil
-local blurTex = nil
+local depthTex = nil
+local baseBlurTex = nil
+local horizBlurTexR = nil
+local horizBlurTexG = nil
+local horizBlurTexB = nil
+local finalBlurTex = nil
 
 -- shader uniform handles
 local eyePosLoc = nil
 local viewProjectionInvLoc = nil
+local resolutionLoc = nil
+-- local focusDepthLoc = nil
+-- local fstopFactorLoc = nil
+local passLoc = nil
+local channelLoc = nil
+
+-- shader uniform enums
+local shaderPasses = 
+{
+	filterSize = 0,
+	horizBlur = 1,
+	vertBlur = 2,
+	composition = 3,
+}
+local blurChannels =
+{
+	red = 0,
+	green = 1,
+	blue = 2,
+}
 
 -----------------------------------------------------------------
 
 function widget:ViewResize(x, y)
 	vsx, vsy = gl.GetViewSizes()
-	glDeleteTexture(blurTex or "")
-	glDeleteTexture (screenTex or "")
-	blurTex, screenTex = nil, nil
+	CleanupTextures()
 	
 	screenTex = glCreateTexture(vsx, vsy, {
 		fbo = true, min_filter = GL.LINEAR, mag_filter = GL.LINEAR,
 		wrap_s = GL.CLAMP, wrap_t = GL.CLAMP,
 	})
-	
-	blurTex = glCreateTexture(vsx/2, vsy/2, {
+
+	depthTex = gl.CreateTexture(vsx,vsy, {
+		border = false,
+		format = GL_DEPTH_COMPONENT24,
+		min_filter = GL.NEAREST,
+		mag_filter = GL.NEAREST,
+	})	
+
+	baseBlurTex = glCreateTexture(vsx/2, vsy/2, {
 		fbo = true, min_filter = GL.LINEAR, mag_filter = GL.LINEAR,
 		wrap_s = GL.CLAMP, wrap_t = GL.CLAMP,
 	})
 	
-	if not blurTex or not screenTex then
+	horizBlurTexR = glCreateTexture(vsx/2, vsy/2, {
+		fbo = true, min_filter = GL.LINEAR, mag_filter = GL.LINEAR,
+		format = GL_RGBA16F_ARB, wrap_s = GL.CLAMP, wrap_t = GL.CLAMP,
+	})
+	
+	horizBlurTexG = glCreateTexture(vsx/2, vsy/2, {
+		fbo = true, min_filter = GL.LINEAR, mag_filter = GL.LINEAR,
+		format = GL_RGBA16F_ARB, wrap_s = GL.CLAMP, wrap_t = GL.CLAMP,
+	})
+	
+	horizBlurTexB = glCreateTexture(vsx/2, vsy/2, {
+		fbo = true, min_filter = GL.LINEAR, mag_filter = GL.LINEAR,
+		format = GL_RGBA16F_ARB, wrap_s = GL.CLAMP, wrap_t = GL.CLAMP,
+	})
+	
+	finalBlurTex = glCreateTexture(vsx/2, vsy/2, {
+		fbo = true, min_filter = GL.LINEAR, mag_filter = GL.LINEAR,
+		wrap_s = GL.CLAMP, wrap_t = GL.CLAMP,
+	})
+	
+	if not horizBlurTexR or not horizBlurTexG or not horizBlurTexB or 
+		not finalBlurTex or not baseBlurTex or not screenTex or not depthTex then
 		Spring.Echo("Depth of Field: Failed to create textures!")
 		widgetHandler:RemoveWidget()
 		return
@@ -107,9 +173,21 @@ function widget:Initialize()
 	end
 	
 	dofShader = dofShader or glCreateShader({
+		defines = {"#version 120\n",
+			"#define MAX_FILTER_SIZE 1.0\n",
+
+			"#define FILTER_SIZE_PASS " .. shaderPasses.filterSize .. "\n",
+			"#define HORIZ_BLUR_PASS " .. shaderPasses.horizBlur .. "\n",
+			"#define VERT_BLUR_PASS " .. shaderPasses.vertBlur .. "\n",
+			"#define COMPOSITION_PASS " .. shaderPasses.composition .. "\n",
+
+			"#define BLUR_CHANNEL_RED " .. blurChannels.red .. "\n",
+			"#define BLUR_CHANNEL_GREEN " .. blurChannels.green .. "\n",
+			"#define BLUR_CHANNEL_BLUE " .. blurChannels.blue .. "\n",
+		},
 		fragment = VFS.LoadFile("LuaUI\\Widgets\\Shaders\\dof.fs", VFS.ZIP),
 		
-		uniformInt = {origTex = 0, blurTex = 1, mapdepths = 2},
+		uniformInt = {origTex = 0, blurTex0 = 1, blurTex1 = 2, blurTex2 = 3},
 	})
 	
 	if not dofShader then
@@ -121,6 +199,11 @@ function widget:Initialize()
 	
 	eyePosLoc = gl.GetUniformLocation(dofShader, "eyePos")
 	viewProjectionInvLoc = gl.GetUniformLocation(dofShader, "viewProjectionInv")
+	resolutionLoc = gl.GetUniformLocation(dofShader, "resolution")
+	-- focusDepthLoc = gl.GetUniformLocation(dofShader, "focusDepth")
+	-- fstopFactorLoc = gl.GetUniformLocation(dofShader, "fstopFactor")
+	passLoc = gl.GetUniformLocation(dofShader, "pass")
+	channelLoc = gl.GetUniformLocation(dofShader, "channel")
 	
 	widget:ViewResize()
 end
@@ -131,11 +214,74 @@ function widget:Shutdown()
 	end
 	
 	if glDeleteTexture then
-		glDeleteTexture(blurTex or "")
-		glDeleteTexture(screenTex or "")
+		CleanupTextures()
 	end
-	dofShader, blurTex, screenTex = nil, nil, nil
+	dofShader = nil
 end
+
+local function FilterCalculation()
+	local cpx, cpy, cpz = spGetCameraPosition()
+	local gmin, gmax = Spring.GetGroundExtremes()
+	local effectiveHeight = cpy - math.max(0, gmin)
+	cpy = 3.5 * math.sqrt(effectiveHeight) * math.log(effectiveHeight)
+	glUniform(eyePosLoc, cpx, cpy, cpz)
+	glUniformMatrix(viewProjectionInvLoc, "viewprojectioninverse")
+	glUniformInt(passLoc, shaderPasses.filterSize)
+	glTexture(0, screenTex)
+	glTexture(1, depthTex)
+
+  glTexRect(-1-0.5/vsx,1+0.5/vsy,1+0.5/vsx,-1-0.5/vsy)
+	
+	glTexture(0, false)
+	glTexture(1, false)
+end
+
+local function HorizBlur(channel)
+	glUniform(resolutionLoc, vsx/2, vsy/2)
+	glUniformInt(channelLoc, channel)
+	glUniformInt(passLoc, shaderPasses.horizBlur)
+	glTexture(0, baseBlurTex)
+  glTexRect(-1-0.5/vsx,1+0.5/vsy,1+0.5/vsx,-1-0.5/vsy)
+	glTexture(0, false)
+end
+
+local function HorizBlurR()
+	HorizBlur(blurChannels.red)
+end
+local function HorizBlurG()
+	HorizBlur(blurChannels.green)
+end
+local function HorizBlurB()
+	HorizBlur(blurChannels.blue)
+end
+
+local function VertBlur()
+	glUniform(resolutionLoc, vsx/2, vsy/2)
+	glUniformInt(passLoc, shaderPasses.vertBlur)
+	glTexture(1, horizBlurTexR)
+	glTexture(2, horizBlurTexG)
+	glTexture(3, horizBlurTexB)
+  glTexRect(-1-0.5/vsx,1+0.5/vsy,1+0.5/vsx,-1-0.5/vsy)
+	glTexture(1, false)
+	glTexture(2, false)
+	glTexture(3, false)
+
+end
+local function Composition()
+	glUniformInt(passLoc, shaderPasses.composition)
+	glTexture(0, screenTex)
+	glTexture(1, finalBlurTex)
+	glTexRect(0, 0, vsx, vsy, false, true)
+	glTexture(0, false)
+	glTexture(1, false)
+end
+
+-- local function LinearizeDepth(depth)
+-- 	Spring.Echo(depth)
+--     local n = 0.1; --camera z near
+--     local f = 100.0; --camera z far
+--     return (2.0 * n) / (f + n - depth* (f - n));
+-- end
 
 function widget:DrawScreenEffects()
 	if not options.useDoF.value then
@@ -144,26 +290,22 @@ function widget:DrawScreenEffects()
 
 	gl.Blending(false)
 	glCopyToTexture(screenTex, 0, 0, 0, 0, vsx, vsy) -- the original screen image
+	glCopyToTexture(depthTex, 0, 0, 0, 0, vsx, vsy) -- the original screen image
+
+	-- glTexture(depthTex)
+	-- local depth = gl.ReadPixels(vsx/2, vsy/2, 1, 1, GL_DEPTH_COMPONENT24)
+	-- Spring.Echo(depth)
+	-- glUniform(focusDepthLoc, depth)
+	-- glUniform(fstopFactorLoc, math.min(math.max(0.4 / math.max(depth, 0.04) - 1.4, 0), 1))
 	
-	glTexture(screenTex)
-	glRenderToTexture(blurTex, glTexRect, -1, 1, 1, -1)
-	glTexture(false)
-	
-	local cpx, cpy, cpz = spGetCameraPosition()
-	local gmin, gmax = Spring.GetGroundExtremes()
-	local effectiveHeight = cpy - math.max(0, gmin)
-	cpy = 3.5 * math.sqrt(effectiveHeight) * math.log(effectiveHeight)
 	glUseShader(dofShader)
-		glUniform(eyePosLoc, cpx, cpy, cpz)
-		glUniformMatrix(viewProjectionInvLoc, "viewprojectioninverse")
-		glTexture(0, screenTex)
-		glTexture(1, blurTex)
-		glTexture(2, "$map_gbuffer_zvaltex")
 		
-		glTexRect(0, 0, vsx, vsy, false, true)
-		
-		glTexture(0, false)
-		glTexture(1, false)
-		glTexture(2, false)
+		glRenderToTexture(baseBlurTex, FilterCalculation)
+		glRenderToTexture(horizBlurTexR, HorizBlurR)
+		glRenderToTexture(horizBlurTexG, HorizBlurG)
+		glRenderToTexture(horizBlurTexB, HorizBlurB)
+		glRenderToTexture(finalBlurTex, VertBlur)
+		Composition()
+
 	glUseShader(0)
 end
