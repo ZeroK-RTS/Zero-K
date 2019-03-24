ee23be60e5df710347ff9b5c01c63d71e6a1133e
sprunk <spr.ng@o2.pl>
2019-03-24 22:17:18 +0100

    Fix #3485, LuaRules crash on start.

--- LuaRules/Gadgets/unit_thrower.lua
+++ LuaRules/Gadgets/unit_thrower.lua
@@ -196,8 +196,10 @@ function gadget:Initialize()
 	for _, unitID in pairs(Spring.GetAllUnits()) do
 		gadget:UnitCreated(unitID, Spring.GetUnitDefID(unitID), Spring.GetUnitTeam(unitID))
 	end
-	for id, _ in pairs(throwWeaponDef) do
-		Script.SetWatchProjectile(id, true)
+	if Script.SetWatchProjectile then
+		for id, _ in pairs(throwWeaponDef) do
+			Script.SetWatchProjectile(id, true)
+		end
 	end
 end
 
