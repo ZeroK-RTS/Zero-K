fbad7cba8b52d63e9f2c681770e424a7b7d41932
sprunk <spr.ng@o2.pl>
2019-04-14 23:01:23 +0200

    Get rid of large chunks of shitcode
    Some of it even had commentary explaining why it's never going to work.

--- LuaUI/Widgets/gui_blastRadius.lua
+++ LuaUI/Widgets/gui_blastRadius.lua
@@ -175,9 +175,6 @@ function DrawBuildMenuBlastRange()
 	--check if build command
 	local cmdDesc = spGetActiveCmdDesc( idx )
 	
-	--local units = spGetSelectedUnits()
-	--local cmdQ = Spring.GetCommandQueue( units[1], -1 )
-	
 	if ( cmdDesc["type"] ~= 20 ) then
 		--quit here if not a build command
 		return
