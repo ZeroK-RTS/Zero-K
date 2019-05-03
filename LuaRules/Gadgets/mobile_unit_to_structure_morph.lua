6269efc8885ac9e36f0cfb07d20dc94486119cc1
mv <tactii@gmail.com>
2019-05-03 14:17:01 -0400

    Mobile Gunship Factory: land when morphing to static

--- LuaRules/Gadgets/mobile_unit_to_structure_morph.lua
+++ LuaRules/Gadgets/mobile_unit_to_structure_morph.lua
@@ -27,6 +27,8 @@ if (gadgetHandler:IsSyncedCode()) then
 
 include("LuaRules/Configs/customcmds.h.lua")
 
+local LANDING_BOX_SZ = 6
+
 --let's start with Mobile Gunship Plant preMorphing and go from there.
 --for general use we would build tables from all unitdefs.
 local gunshipPlantID = UnitDefNames["factorygunship"].id
@@ -36,36 +38,29 @@ local mobileGunshipPlants = {}
 --keep track of units moving into morph position.
 local preMorphing = {}
 
---track proxy nanoframes for later deletion.
-local preMorphNanos = {}
+function gadget:AllowUnitCreation(unitDefID, builderID, builderTeam, x, y, z, facing)
+  if (unitDefID ~= gunshipPlantID or not mobileGunshipPlants[builderID]) then
+    --if this is not morpher creating morphee, don't care.
+    return true
+  end
+
+  preMorphing[builderID] = {
+    unitID = builderID,
+    x = x,
+    y = y,
+    z = z,
+    facing = facing,
+    needsMove = true,
+  }
+
+  return false
+end
 
 function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
   if unitDefID == mobileGunshipPlantID then
     --keep a list of mobile gunship plants in play.
     mobileGunshipPlants[unitID] = true
-    return
   end
-
-  if (unitDefID ~= gunshipPlantID or not mobileGunshipPlants[builderID]) then
-    --if this is not morpher creating morphee, don't care.
-    return
-  end
-
-  local x, y, z = Spring.GetUnitPosition(unitID)
-  local rx, ry, rz = Spring.GetUnitRotation(unitID)
-
-  Spring.GiveOrderToUnit(builderID, CMD.MOVE, {x, y, z}, 0)
-
-  preMorphing[builderID] = {
-    x = x,
-    y = y,
-    z = z,
-    rx = rx,
-    ry = ry,
-    rz = rz,
-  }
-
-  table.insert(preMorphNanos, unitID)
 end
 
 function gadget:UnitCmdDone(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOpts, cmdTag)
@@ -74,20 +69,25 @@ function gadget:UnitCmdDone(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOp
   end
 
   local preMorphData = preMorphing[unitID]
-  preMorphing[unitID] = false
+
+  if (cmdID == -404) then
+    return --avoid conflicting with the premorph build commands.
+  end
+
+  Spring.Echo('premorph cmdDone for ' .. unitID)
 
   --make sure the build location hasn't become blocked while moving to it.
-  --todo: check actual build facing
-  blocking, featureID = Spring.TestBuildOrder(gunshipPlantID, preMorphData.x, preMorphData.y, preMorphData.z, 0)
+  local blocking, featureID = Spring.TestBuildOrder(gunshipPlantID, preMorphData.x, preMorphData.y, preMorphData.z, preMorphData.facing)
   if (blocking < 2 or featureID ~= nil) then
+    Spring.Echo('cancelling premorph due to landing blocked for ' .. unitID)
+    preMorphing[unitID] = nil
     return
   end
 
-  local genericMorphCmd = GG.MorphInfo["CMD_MORPH_BASE_ID"]
-
-  Spring.SetUnitPosition(unitID, preMorphData.x, preMorphData.y, preMorphData.z)
-  Spring.SetUnitRotation(unitID, preMorphData.rx, preMorphData.ry, preMorphData.rz)
-  Spring.GiveOrderToUnit(unitID, genericMorphCmd, {gunshipPlantID}, 0)
+  Spring.Echo('setting physics for ' .. unitID .. ' post cmd ' .. cmdID)
+  local x, y, z = Spring.GetUnitPosition(unitID)
+  local rx, ry, rz = Spring.GetUnitRotation(unitID)
+  Spring.SetUnitPhysics(unitID, x, y, z, 0, -1, 0, rx, ry, rz, 0, 0, 0)
 end
 
 function gadget:UnitCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOpts, cmdTag)
@@ -97,20 +97,50 @@ function gadget:UnitCommand(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOp
 
   local preMorphData = preMorphing[unitID]
 
-  if (cmdParams[0] == preMorphData.x and cmdParams[1] == preMorphData.y and cmdParams[2] == preMorphData.z) then
-    return --avoid race condition with the premorph move command.
+  if (cmdParams[1] == preMorphData.x and cmdParams[2] == preMorphData.y and cmdParams[3] == preMorphData.z) then
+    return --avoid conflicting with the premorph move commands.
   end
 
-  preMorphing[unitID] = false
+  if (cmdParams[1] == 404) then
+    return --avoid conflicting with the premorph build commands.
+  end
+
+  Spring.Echo('cancelling premorph of ' .. unitID .. ' cmdID ' .. cmdID)
+  Spring.Echo('pmd x: ' .. preMorphData.x .. ' y: ' .. preMorphData.y .. ' z: ' .. preMorphData.z)
+  for i, param in ipairs(cmdParams) do
+    Spring.Echo(i .. ': ' .. param)
+  end
+
+  preMorphing[unitID] = nil
 end
 
 function gadget:GameFrame(frame)
-  --destroying the proxy nanoframe is deferred until next game frame to workaround a nasty bug:
-  --if you destroy the nanoframe in its UnitCreated call-in, the space it occupied can no longer be built on. rude!
-  while table.getn(preMorphNanos) > 0 do
-    local nanoID = table.remove(preMorphNanos)
-    Spring.DestroyUnit(nanoID, false, true)
+  local genericMorphCmd = GG.MorphInfo["CMD_MORPH_BASE_ID"]
+
+  for unitID, preMorphData in pairs(preMorphing) do
+    if preMorphData then
+      if preMorphData.needsMove then
+        --defer move command because it can not be issued in AllowUnitCreation.
+        Spring.GiveOrderToUnit(preMorphData.unitID, CMD.MOVE, {preMorphData.x, preMorphData.y, preMorphData.z}, 0)
+        preMorphing[unitID].needsMove = false
+      end
+
+      local found = Spring.GetUnitsInBox(
+        preMorphData.x - LANDING_BOX_SZ, preMorphData.y - LANDING_BOX_SZ, preMorphData.z - LANDING_BOX_SZ,
+        preMorphData.x + LANDING_BOX_SZ, preMorphData.y + LANDING_BOX_SZ, preMorphData.z + LANDING_BOX_SZ
+      )
+      for _, foundID in ipairs(found) do
+        Spring.Echo('found ' .. foundID)
+        if unitID == foundID then
+          Spring.SetUnitPosition(unitID, preMorphData.x, preMorphData.y, preMorphData.z)
+          Spring.SetUnitRotation(unitID, 0, 0, 0)
+          Spring.GiveOrderToUnit(unitID, genericMorphCmd, {gunshipPlantID}, 0)
+          preMorphing[unitID] = nil
+        end
+      end
+    end
   end
 end
+
 
 end --SYNCED
