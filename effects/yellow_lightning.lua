4ce24c0b32fb76b8a6c934bed51172485a001444
sprunk <gnurps@o2.pl>
2019-05-10 14:37:29 +0200

    :hammer:

--- effects/yellow_lightning.lua
+++ effects/yellow_lightning.lua
@@ -166,49 +166,6 @@ return {
     },
   },
   
-  ["yellow_lightning_stormbolt"] = {
-    groundflash = {
-      circlealpha        = 1,
-      circlegrowth       = 0,
-      flashalpha         = 0.3,
-      flashsize          = 32,
-      ttl                = 3,
-      color = {
-        [1]  = 1,
-        [2]  = 1,
-        [3]  = 1,
-      },
-    },
-    lightningballs = {
-      air                = true,
-      class              = [[CSimpleParticleSystem]],
-      count              = 1,
-      ground             = true,
-      water              = true,
-      properties = {
-        airdrag            = 1,
-        colormap           = [[0 0 0 0.01 1 1 1 0.01 0 0 0 0.01]],
-        directional        = true,
-        emitrot            = 80,
-        emitrotspread      = 0,
-        emitvector         = [[0, 1, 0]],
-        gravity            = [[0, 0, 0]],
-        numparticles       = 1,
-        particlelife       = 3,
-        particlelifespread = 0,
-        particlesize       = 2,
-        particlesizespread = 20,
-        particlespeed      = 0.01,
-        particlespeedspread = 0,
-        pos                = [[-10 r20, 1.0, -10 r20]],
-        sizegrowth         = 0,
-        sizemod            = 1.0,
-        texture            = [[whitelightb]],
-      },
-    },
-  },
-
-
   ["yellow_lightning_bomb"] = {
     bluebolts = {
       air                = true,
