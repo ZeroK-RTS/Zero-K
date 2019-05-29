5c8a51e54f5cf6b28b200cdaa827b91439261443
Анархид <anarchid@mail.ru>
2019-05-29 18:33:04 +0300

    Add Turkish and Chinese translations [PR #3391]

--- LuaUI/Headers/languages.lua
+++ LuaUI/Headers/languages.lua
@@ -3,7 +3,19 @@ local langs = {
 	{ lang = 'ru', flag = 'ru', name = 'Русский' },
 	{ lang = 'pl', flag = 'pl', name = 'Polski' },
 	{ lang = 'de', flag = 'de', name = 'Deutsch' },
+	{ lang = 'tr', flag = 'tr', name = 'Türkçe' },
 }
+
+--[[ On Windows, far eastern fonts are börkçe by default, and
+for them to work the engine needs to have been run with
+the `--gen-fontconfig` CLI param at least once. This is
+the job for Chobby who should run a parallel instance to
+do just that (ideally detecting whether the user's system
+language requires it by default) and leave information
+about it. ]]
+if Platform.osFamily ~= "Windows" or VFS.FileExists("LuaUI/font_config_generated", VFS.RAW) then
+	langs[#langs + 1] = { lang = 'zh', flag = 'cn', name = '中文' }
+end
 
 local flagByLang, langByFlag = {}, {}
 for i = 1, #langs do
@@ -17,3 +29,4 @@ setmetatable(langByFlag, { __index = function()
 end})
 
 return langs, flagByLang, langByFlag
+	
