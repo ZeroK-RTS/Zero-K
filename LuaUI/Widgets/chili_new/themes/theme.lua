deedc003bb80f4de50f82a9dc7335d9f720f9892
GoogleFrog <googl3frog@gmail.com>
2019-06-01 17:02:15 +1000

    Merged theme.

--- LuaUI/Widgets/chili_new/themes/theme.lua
+++ LuaUI/Widgets/chili_new/themes/theme.lua
@@ -9,7 +9,7 @@ theme.name = "default"
 --//=============================================================================
 --// Define default skins
 
-local defaultSkin = "Robocracy"
+local defaultSkin = "Evolved"
 --local defaultSkin = "DarkGlass"
 
 theme.skin = {
