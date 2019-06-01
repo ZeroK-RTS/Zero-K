9cd3fe58c837883f56e8ff66ffa1827d5a76ce68
GoogleFrog <googl3frog@gmail.com>
2019-06-01 20:18:23 +1000

    Removed duplicate chili.

--- LuaUI/Widgets/chili_new/handlers/themehandler.lua
+++
@@ -1,5 +0,0 @@
---//=============================================================================
---// Theme Handler
-
---// TODO: WRITE ME
-VFS.Include((THEME_DIRNAME or (CHILI_DIRNAME .. "themes/")) .. "theme.lua", nil, VFS.RAW_FIRST)
