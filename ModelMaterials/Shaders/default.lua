57ecc765906283cdd27359618baa531b56b2d0da
lhog <ccna@mail.ru>
2019-06-09 22:16:40 +0300

    1) Disabled normalmapping toggle as it breaks the desired CUS use, if disabled. Will fix later 2) Removed noise specular pattern from metal wrecks

--- ModelMaterials/Shaders/default.lua
+++ ModelMaterials/Shaders/default.lua
@@ -255,34 +255,6 @@ fragment = [[
 		return fract((p3.x + p3.y) * p3.z);
 	}
 
-	float Perlin2D(vec2 P) {
-		//  https://github.com/BrianSharpe/Wombat/blob/master/Perlin2D.glsl
-
-		// establish our grid cell and unit position
-		vec2 Pi = floor(P);
-		vec4 Pf_Pfmin1 = P.xyxy - vec4( Pi, Pi + 1.0 );
-
-		// calculate the hash
-		vec4 Pt = vec4( Pi.xy, Pi.xy + 1.0 );
-		Pt = Pt - floor(Pt * ( 1.0 / 71.0 )) * 71.0;
-		Pt += vec2( 26.0, 161.0 ).xyxy;
-		Pt *= Pt;
-		Pt = Pt.xzxz * Pt.yyww;
-		vec4 hash_x = fract( Pt * ( 1.0 / 951.135664 ) );
-		vec4 hash_y = fract( Pt * ( 1.0 / 642.949883 ) );
-
-		// calculate the gradient results
-		vec4 grad_x = hash_x - 0.49999;
-		vec4 grad_y = hash_y - 0.49999;
-		vec4 grad_results = inversesqrt( grad_x * grad_x + grad_y * grad_y ) * ( grad_x * Pf_Pfmin1.xzxz + grad_y * Pf_Pfmin1.yyww );
-
-		// Classic Perlin Interpolation
-		grad_results *= 1.4142135623730950488016887242097;  // scale things to a strict -1.0->1.0 range  *= 1.0/sqrt(0.5)
-		vec2 blend = Pf_Pfmin1.xy * Pf_Pfmin1.xy * Pf_Pfmin1.xy * (Pf_Pfmin1.xy * (Pf_Pfmin1.xy * 6.0 - 15.0) + 10.0);
-		vec4 blend2 = vec4( blend, vec2( 1.0 - blend ) );
-		return dot( grad_results, blend2.zxzx * blend2.wwyy );
-	}
-
 #ifdef use_shadows
 	// Derivatives of light-space depth with respect to texture2D coordinates
 	vec2 DepthGradient(vec2 uv, float z) {
@@ -377,10 +349,6 @@ fragment = [[
 
 		vec4 outColor   = diffuseIn;
 		vec4 extraColor = texture(textureS3o2, tex_coord0.st);
-
-		#ifdef EXTRACOLOR_G_NOISE
-			extraColor.g = smoothstep(-0.5, 0.5, (Perlin2D(EXTRACOLOR_G_NOISE * tex_coord0.st)));
-		#endif
 
 		vec3 V = normalize(viewDir);
 		vec3 Rv = -reflect(V, N);
