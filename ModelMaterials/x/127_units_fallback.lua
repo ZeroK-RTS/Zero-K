a9c147b3effd3aa90df143dcee6b4dfc30a58e5e
lhog <ccna@mail.ru>
2019-06-20 18:26:03 +0300

    Added old, disabled materials, so I can switch branches

---
+++ ModelMaterials/x/127_units_fallback.lua
@@ -0,0 +1,57 @@
+-- $Id$
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local function SunChanged(curShaderObj)
+	curShaderObj:SetUniformAlways("shadowDensity", gl.GetSun("shadowDensity" ,"unit"))
+
+	curShaderObj:SetUniformAlways("sunAmbient", gl.GetSun("ambient" ,"unit"))
+	curShaderObj:SetUniformAlways("sunDiffuse", gl.GetSun("diffuse" ,"unit"))
+	curShaderObj:SetUniformAlways("sunSpecular", gl.GetSun("specular" ,"unit"))
+end
+
+local materials = {
+	unitsFallback = {
+		shaderDefinitions = {
+			"#define deferred_mode 0",
+			"#define SHADOW_SOFTNESS SHADOW_SOFTER",
+		},
+		deferredDefinitions = {
+			"#define deferred_mode 1",
+			"#define SHADOW_SOFTNESS SHADOW_HARD",
+			"#define MAT_IDX 127",
+		},
+		usecamera = false,
+		culling   = GL.BACK,
+		texunits  = {
+			[0] = "%%UNITDEFID:0",
+			[1] = "%%UNITDEFID:1",
+			[2] = "$shadow",
+			--[3] = "$specular",
+			[4] = "$reflection",
+		},
+		SunChanged = SunChanged,
+	},
+}
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+local cusUnitMaterials = GG.CUS[1].bufMaterials
+
+local unitMaterials = {}
+
+for id = 1, #UnitDefs do
+	if not cusUnitMaterials[id] then
+		unitMaterials[id] = {"unitsFallback"}
+	end
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+
+
+return materials, unitMaterials
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
