6665d6228b8379585875770e1c38dea40321c811
GoogleFrog <googl3frog@gmail.com>
2019-07-11 17:18:51 +1000

    Blindly apply font UI scale fix. https://github.com/Spring-Chobby/Chobby/commit/1e116c41fdc1f9453e87896fbd07e0e8643a11a9#diff-cb4cb2401e6e43cf74a54523b8da5f02

--- LuaUI/Widgets/chili/Controls/font.lua
+++ LuaUI/Widgets/chili/Controls/font.lua
@@ -54,7 +54,8 @@ end
 
 function Font:_LoadFont()
 	local oldfont = self._font
-	self._font = FontHandler.LoadFont(self.font, self.size, self.outlineWidth, self.outlineWeight)
+	local uiScale = (WG and WG.uiScale or 1)
+	self._font = FontHandler.LoadFont(self.font, math.floor(self.size*uiScale), math.floor(self.outlineWidth*uiScale), self.outlineWeight)
 	--// do this after LoadFont because it can happen that LoadFont returns the same font again
 	--// but if we Unload our old one before, the gc could collect it before, so the engine would have to reload it again
 	FontHandler.UnloadFont(oldfont)
