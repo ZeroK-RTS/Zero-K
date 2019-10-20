9030e78943efcd231e00ab1b10a000af8fafdd0c
Oflameo <oflameo@oflameo.com>
2019-10-20 12:31:26 -0400

    Laser DPS moved to EMG; Laser range moved to sight.

--- units/jumpaa.lua
+++ units/jumpaa.lua
@@ -40,7 +40,7 @@ unitDef = {
   objectName          = [[hunchback.s3o]],
   script              = [[jumpaa.lua]],
   selfDestructAs      = [[BIG_UNITEX]],
-  sightDistance       = 660,
+  sightDistance       = 820,
   trackOffset         = 0,
   trackStrength       = 8,
   trackStretch        = 1,
@@ -50,12 +50,6 @@ unitDef = {
   upright             = true,
 
   weapons             = {
-
-    {
-      def                = [[LASER]],
-      --badTargetCategory  = [[GUNSHIP]],
-      onlyTargetCategory = [[GUNSHIP FIXEDWING]],
-    },
 
     {
       def                = [[EMG]],
@@ -87,9 +81,9 @@ unitDef = {
       },
 
       damage                  = {
-        default = 0.78,
-        planes  = 7.8,
-        subs    = 0.5,
+        default = 2.42,
+        planes  = 24.2,
+        subs    = 1.39,
       },
 
       explosionGenerator      = [[custom:ARCHPLOSION]],
@@ -110,51 +104,6 @@ unitDef = {
       turret                  = true,
       weaponType              = [[Cannon]],
       weaponVelocity          = 1500,
-    },
-
-
-    LASER         = {
-      name                    = [[Anti-Air Laser Battery]],
-      areaOfEffect            = 12,
-      beamDecay               = 0.736,
-      beamTime                = 1/30,
-      beamttl                 = 15,
-      canattackground         = false,
-      coreThickness           = 0.5,
-      craterBoost             = 0,
-      craterMult              = 0,
-      cylinderTargeting       = 1,
-
-      customParams              = {
-        isaa = [[1]],
-        light_color = [[0.2 1.2 1.2]],
-        light_radius = 120,
-      },
-
-      damage                  = {
-        default = 1.636,
-        planes  = 16.36,
-        subs    = 0.94,
-      },
-
-      explosionGenerator      = [[custom:flash_teal7]],
-      fireStarter             = 100,
-      impactOnly              = true,
-      impulseFactor           = 0,
-      interceptedByShieldType = 1,
-      laserFlareSize          = 3.25,
-      minIntensity            = 1,
-      noSelfDamage            = true,
-      range                   = 820,
-      reloadtime              = 0.1,
-      rgbColor                = [[0 1 1]],
-      soundStart              = [[weapon/laser/rapid_laser]],
-      soundStartVolume        = 4,
-      thickness               = 2.165,
-      tolerance               = 8192,
-      turret                  = true,
-      weaponType              = [[BeamLaser]],
-      weaponVelocity          = 2200,
     },
 
   },
