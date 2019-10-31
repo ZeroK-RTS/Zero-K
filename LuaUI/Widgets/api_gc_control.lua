884c080d129331baa1ceb873798855f11274ced2
sprunk <gnurps@o2.pl>
2019-10-31 14:15:05 +0100

    Fix #3511 (OOM during catching up)

--- LuaUI/Widgets/api_gc_control.lua
+++ LuaUI/Widgets/api_gc_control.lua
@@ -17,13 +17,28 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-function widget:Initialize()
+local CATCHING_UP_THRESHOLD = 5 * Game.gameSpeed
+
+local spGetGameFrame = Spring.GetGameFrame
+local spSendCommands = Spring.SendCommands
+
+function widget:GameProgress (serverFrame)
 	-- See:
 	-- https://springrts.com/mantis/view.php?id=5951
 	-- https://github.com/spring/spring/commit/a8b5ffc86351680c6e0e8d7e8db161e63dbb912e
 	-- https://github.com/spring/spring/commit/20c5c96c9ec4a7810346e0f20abdf1c3ae0f9513
-	Spring.SendCommands({"luagccontrol 1"})
-end
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+	if serverFrame > spGetGameFrame() + CATCHING_UP_THRESHOLD then
+		-- runs GC each simframe (or at 6Hz when paused). Simframes
+		-- are by far the main source of garbage during catching up
+		-- and there can be hundreds of them per second, making the
+		-- usual 30Hz GC struggle to keep up and leading to OOM crashes.
+		spSendCommands("luagccontrol 0")
+	else
+		-- runs GC at 30Hz. Since that is also the usual game speed
+		-- outside of catching up, this should not result in any
+		-- difference to the simframe mode, but empirical evidence
+		-- says it somehow runs more smoothly.
+		spSendCommands("luagccontrol 1")
+	end
+end
