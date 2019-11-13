5ac2319a49c56feea36d0435c324a87290d77c1a
GoogleFrog <googl3frog@gmail.com>
2019-11-13 14:37:30 +1100

    Only apply luagccontrol when a change is required
    Removes console spam.

--- LuaUI/Widgets/api_gc_control.lua
+++ LuaUI/Widgets/api_gc_control.lua
@@ -17,10 +17,19 @@ end
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local CATCHING_UP_THRESHOLD = 5 * Game.gameSpeed
+local CATCHING_UP_THRESHOLD = 150
 
 local spGetGameFrame = Spring.GetGameFrame
 local spSendCommands = Spring.SendCommands
+
+local currentState = 0
+local function SetGcState(newState)
+	if currentState == newState then
+		return
+	end
+	currentState = newState
+	spSendCommands("luagccontrol " .. currentState)
+end
 
 function widget:GameProgress (serverFrame)
 	-- See:
@@ -33,12 +42,12 @@ function widget:GameProgress (serverFrame)
 		-- are by far the main source of garbage during catching up
 		-- and there can be hundreds of them per second, making the
 		-- usual 30Hz GC struggle to keep up and leading to OOM crashes.
-		spSendCommands("luagccontrol 0")
+		SetGcState(0)
 	else
 		-- runs GC at 30Hz. Since that is also the usual game speed
 		-- outside of catching up, this should not result in any
 		-- difference to the simframe mode, but empirical evidence
 		-- says it somehow runs more smoothly.
-		spSendCommands("luagccontrol 1")
+		SetGcState(1)
 	end
 end
