d5e2ce9e1d7325c3eb7d176df334de750e6b87e8
sprunk <spr.ng@o2.pl>
2019-12-03 21:53:56 +0100

    Unduplicate some conversion weapon defs
    Others coming in later when I get more mana

--- gamedata/modularcomms/weapons/heavymachinegun_disrupt.lua
+++ gamedata/modularcomms/weapons/heavymachinegun_disrupt.lua
@@ -1,49 +1,14 @@
-local name = "commweapon_heavymachinegun_disrupt"
-local weaponDef = {
-	name                    = [[Disruptor Heavy Machine Gun]],
-	accuracy                = 1024,
-	alphaDecay              = 0.7,
-	areaOfEffect            = 96,
-	burnblow                = true,
-	craterBoost             = 0.15,
-	craterMult              = 0.3,
+local _, def = VFS.Include("gamedata/modularcomms/weapons/heavymachinegun.lua")
 
-	customParams            = {
-		is_unit_weapon = 1,
-		slot = [[5]],
-		muzzleEffectShot = [[custom:WARMUZZLE]],
-		miscEffectShot = [[custom:DEVA_SHELLS]],
-		timeslow_damagefactor = 2,
+def.name = "Disruptor " .. def.name
+def.customParams.timeslow_damagefactor = 2
+for armorType, damage in pairs (def.damage) do
+	def.damage[armorType] = damage * 0.75
+end
 
-		light_color = [[1.3 0.5 1.6]],
-		light_radius = 180,
-		reaim_time = 1,
-	},
+def.customParams.light_color = "1.3 0.5 1.6"
+def.customParams.altforms = nil -- baseline also has a lime variant, disruptor doesn't yet
+def.explosionGenerator = "custom:BEAMWEAPON_HIT_PURPLE"
+def.rgbColor = "0.9 0.1 0.9"
 
-	damage                  = {
-		default = 22.5,
-		subs    = 0.825,
-	},
-
-	edgeEffectiveness       = 0.5,
-	explosionGenerator      = [[custom:BEAMWEAPON_HIT_PURPLE]],
-	firestarter             = 70,
-	impulseBoost            = 0,
-	impulseFactor           = 0.2,
-	intensity               = 0.7,
-	interceptedByShieldType = 1,
-	noSelfDamage            = true,
-	range                   = 285,
-	reloadtime              = 5/30,
-	rgbColor                = [[0.9 0.1 0.9]],
-	separation              = 1.5,
-	soundHit                = [[weapon/cannon/emg_hit]],
-	soundStart              = [[weapon/sd_emgv7]],
-	soundStartVolume        = 7,
-	stages                  = 10,
-	turret                  = true,
-	weaponType              = [[Cannon]],
-	weaponVelocity          = 550,
-}
-
-return name, weaponDef
+return "commweapon_heavymachinegun_disrupt", def
--- gamedata/modularcomms/weapons/multistunner_improved.lua
+++ gamedata/modularcomms/weapons/multistunner_improved.lua
@@ -1,54 +1,14 @@
-local name = "commweapon_multistunner_improved"
-local weaponDef = {
-	name                    = [[Heavy Multi-Stunner]],
-	areaOfEffect            = 144,
-	avoidFeature            = false,
-	beamTTL                 = 12,
-	burst                   = 16,
-	burstRate               = 0.166,
-	commandFire             = true,
+local _, def = VFS.Include("gamedata/modularcomms/weapons/multistunner.lua")
 
-	customParams            = {
-		is_unit_weapon = 1,
-		muzzleEffectShot = [[custom:YELLOW_LIGHTNING_MUZZLE]],
-		slot = [[3]],
-		manualfire = 1,
+def.name = "Heavy " .. def.name
+def.paralyzeTime = def.paralyzeTime + 2
+for armorType, damage in pairs (def.damage) do
+	def.damage[armorType] = damage * 1.25
+end
 
-		light_color = [[0.75 0.75 0.22]],
-		light_radius = 350,
-		reaim_time = 1,
-	},
+def.customParams.light_color = "0.75 0.75 0.22"
+def.customParams.light_radius = def.customParams.light_radius + 30
+def.thickness = def.thickness + 5
+def.rgbColor = "1 1 0.25"
 
-	craterBoost             = 0,
-	craterMult              = 0,
-	cylinderTargeting       = 0,
-
-	damage                  = {
-		default = 687.5,
-	},
-
-	duration                = 8,
-	edgeEffectiveness       = 0,
-	explosionGenerator      = [[custom:YELLOW_LIGHTNINGPLOSION]],
-	fireStarter             = 0,
-	impulseBoost            = 0,
-	impulseFactor           = 0,
-	intensity               = 10,
-	interceptedByShieldType = 1,
-	noSelfDamage            = true,
-	paralyzer               = true,
-	paralyzeTime            = 10,
-	range                   = 360,
-	reloadtime              = 25,
-	rgbColor                = [[1 1 0.1]],
-	soundStart              = [[weapon/lightning_fire]],
-	soundTrigger            = false,
-	sprayAngle              = 1920,
-	texture1                = [[lightning]],
-	thickness               = 15,
-	turret                  = true,
-	weaponType              = [[LightningCannon]],
-	weaponVelocity          = 450,
-}
-
-return name, weaponDef
+return "commweapon_multistunner_improved", def
