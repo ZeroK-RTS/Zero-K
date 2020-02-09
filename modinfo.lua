79240ed8469cd5b7559833c14d33fa7a916d354b
GoogleFrog <googl3frog@gmail.com>
2020-02-09 18:23:18 +1100

    Oops.

--- modinfo.lua
+++ modinfo.lua
@@ -1,13 +1,13 @@
 return {
-	name = 'Zero-K Benchmark',
-	description = 'Zero-K Benchmark',
-	shortname = 'zk',
-	version = 'v1.7.11.4 select',
-	mutator = '1',
-	game = '',
-	shortGame = '',
-	modtype = 1,
-	depend = {
-		'Zero-K test-15804-635fc24',
-	},
+  name='Zero-K',
+  description='Zero-K',
+  shortname='ZK',
+  version='$VERSION',
+  mutator='1',
+  game='Zero-K',
+  shortGame='ZK',
+  modtype=1,
+  depend = {
+    'Spring content v1',
+  },
 }
