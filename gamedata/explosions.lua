688915208815dc70091a16e9dbc5038c32861f83
sprunk <spr.ng@o2.pl>
2020-02-22 14:58:46 +0100

    Fix maps with CEGs in weird places

--- gamedata/explosions.lua
+++ gamedata/explosions.lua
@@ -1,11 +1,14 @@
 local VFS = VFS
 local vfsInclude = VFS.Include
+local vfsDirList = VFS.DirList
 
 vfsInclude("LuaRules/Utilities/tablefunctions.lua")
 local suCopyTable = Spring.Utilities.CopyTable
 
+local files = vfsDirList("effects", "*.lua")
+suCopyTable(vfsDirList("gamedata/explosions", "*.lua", VFS.MAP), false, files)
+
 local explosionDefs = {}
-local files = VFS.DirList("effects", "*.lua")
 for i = 1, #files do
 	suCopyTable(vfsInclude(files[i]), false, explosionDefs)
 end
