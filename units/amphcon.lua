8a2a9471fc743bcbe2874c90a8d6fd05f0f1b290
GoogleFrog <googl3frog@gmail.com>
2020-03-12 17:34:08 +1100

    Tried Sparkles' armoured Conch idea.  * 4x damage modifier when stationary and not building.  * The model does not really fit.

--- units/amphcon.lua
+++ units/amphcon.lua
@@ -1,7 +1,7 @@
 return { amphcon = {
   unitname            = [[amphcon]],
   name                = [[Conch]],
-  description         = [[Amphibious Construction Bot, Builds at 7.5 m/s]],
+  description         = [[Amphibious Construction Bot, Builds at 7.5 m/s - Armored When Idle]],
   acceleration        = 1.2,
   activateWhenBuilt   = true,
   brakeRate           = 1.5,
@@ -23,6 +23,7 @@ return { amphcon = {
     amph_submerged_at = 40,
   },
 
+  damageModifier      = 0.25,
   explodeAs           = [[BIG_UNITEX]],
   footprintX          = 2,
   footprintZ          = 2,
