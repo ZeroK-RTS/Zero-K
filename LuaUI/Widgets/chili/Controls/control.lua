8406d69e576ea80c9bc431210c2dd3bc86b380b7
GoogleFrog <googl3frog@gmail.com>
2020-04-06 23:38:07 +1000

    Sync new chili.

--- LuaUI/Widgets/chili/Controls/control.lua
+++ LuaUI/Widgets/chili/Controls/control.lua
@@ -78,7 +78,7 @@ Control = Object:Inherit{
 
 	drawcontrolv2 = nil, --// disable backward support with old DrawControl gl state (with 2.1 self.xy translation isn't needed anymore)
 
-	useRTT = ((gl.CreateFBO and gl.BlendFuncSeparate) ~= nil),
+	useRTT = false and ((gl.CreateFBO and gl.BlendFuncSeparate) ~= nil),
 	useDLists = false, --(gl.CreateList ~= nil), --FIXME broken in combination with RTT (wrong blending)
 
 	OnResize        = {},
