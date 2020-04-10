292719708d9b436885f550f0cf098fb34dbb3ab1
Anarchid <anarchid@mail.ru>
2020-04-10 16:47:21 +0300

    strikecom level 1 fixes: - rotation pivot is set to center by hacking offset of the autogenerated Scene piece in metafile (see #3872) - unfuck model by restoring from an older backup - this reintroduces the fatter badder gun by not removing the underslung piece. If necessary, hide in script.

--- Objects3d/strikecom.dae.lua
+++ Objects3d/strikecom.dae.lua
@@ -2,7 +2,12 @@ return {
 	invertteamcolor = false,
 	tex1 = "strikecom.dds",
 	tex2 = "strikecom_2.dds",
-	midpos = {0,25,0},
+	midpos = {0,30,0},
 	radius = 30,
-	height = 50,
+	height = 100,
+	pieces = {
+		Scene = {
+			offset = {0.0, 30.0, 0.0},
+		}
+	}
 }
