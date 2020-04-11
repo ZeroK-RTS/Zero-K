a0fb412694b4a46fa2731965bd117f8328a8810f
sprunk <spr.ng@o2.pl>
2020-04-11 14:33:09 +0200

    Bring back '.h.lua' handling

--- LuaUI/utils.lua
+++ LuaUI/utils.lua
@@ -11,6 +11,14 @@ function Basename(fullpath)
 end
 
 function include(filename, envTable, VFSMODE)
+	--[[ support legacy header paths for the time being
+	     in case people use them in their local widgets ]]
+	if filename == "colors.h.lua" then
+		return include("colors.lua", envTable, VFSMODE)
+	elseif filename == "keysym.h.lua" then
+		return include("keysym.lua", envTable, VFSMODE)
+	end
+
 	if (not filename:find("/", 1, true))or(not VFS.FileExists(filename, VFSMODE or VFS.DEF_MODE)) then
 		if VFS.FileExists(LUAUI_DIRNAME .. filename, VFSMODE or VFS.DEF_MODE) then
 			filename = LUAUI_DIRNAME .. filename
