c39fa351f8447284ac9cd6bcb3693973efb83732
Anarchid <anarchid@mail.ru>
2020-04-17 00:20:36 +0300

    Revert "Revert strikecom fixes."
    This reverts commit 49adb5cca8c3cd07323f8ae6362fab96dc470d3e.

--- Objects3d/strikecom_1.dae.lua
+++ Objects3d/strikecom_1.dae.lua
@@ -2,7 +2,12 @@ return {
 	invertteamcolor = false,
 	tex1 = "strikecom.dds",
 	tex2 = "strikecom_2.dds",
-	midpos = {0,27.5,0},
+	midpos = {0,33,0},
 	radius = 33,
 	height = 55,
+	pieces = {
+		Scene = {
+			offset = {0.0, 33.0, 0.0},
+		}
+	}
 }
--- Objects3d/strikecom_2.dae.lua
+++ Objects3d/strikecom_2.dae.lua
@@ -2,7 +2,12 @@ return {
 	invertteamcolor = false,
 	tex1 = "strikecom.dds",
 	tex2 = "strikecom_2.dds",
-	midpos = {0,30,0},
+	midpos = {0,36,0},
 	radius = 36,
 	height = 60,
+	pieces = {
+		Scene = {
+			offset = {0.0, 36.0, 0.0},
+		}
+	}
 }
