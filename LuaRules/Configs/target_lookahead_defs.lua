7c212f459561f3997e2b3f7a1cb16368c930af87
esainane <esainane+github@gmail.com>
2020-05-03 16:13:51 +1200

    Remove superfluous customParams check
    Co-authored-by: sprunk <gnurps@o2.pl>

--- LuaRules/Configs/target_lookahead_defs.lua
+++ LuaRules/Configs/target_lookahead_defs.lua
@@ -2,10 +2,7 @@
 local unitArray = {}
 
 for i=1,#UnitDefs do
-	local cp = UnitDefs[i].customParams
-	if cp and cp.lookahead then
-		unitArray[i] = cp.lookahead
-	end
+	unitArray[i] = UnitDefs[i].customParams.lookahead
 end
 
 return unitArray
