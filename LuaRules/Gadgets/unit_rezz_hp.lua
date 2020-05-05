e72eca6af9dd224eeed7c1b4e46b189dd0225800
sprunk <spr.ng@o2.pl>
2020-05-05 23:28:54 +0200

    Rez health gadget optimization / readability

--- LuaRules/Gadgets/unit_rezz_hp.lua
+++ LuaRules/Gadgets/unit_rezz_hp.lua
@@ -1,99 +1,73 @@
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-if not gadgetHandler:IsSyncedCode() then
-	return
-end
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+if not Script.GetSynced() then return end
+
 function gadget:GetInfo() return {
 	name      = "Rezz Hp changer + effect",
 	desc      = "Sets rezzed units to full hp",
 	author    = "Google Frog, modified by Rafal & Meep",
 	date      = "Nov 30, 2008",
 	license   = "GNU GPL, v2 or later",
-	layer     = 0,
+	layer     = 1000001, -- after awards, for GG.Awards to exist by Initialize
 	enabled   = true,
 } end
 
-	local spGetUnitHealth   = Spring.GetUnitHealth
-	local spGetUnitPosition = Spring.GetUnitPosition
-	local spSetUnitHealth   = Spring.SetUnitHealth
-	local spSpawnCEG        = Spring.SpawnCEG
-	local CMD_RESURRECT     = CMD.RESURRECT
+local spGetUnitCurrentCommand = Spring.GetUnitCurrentCommand
+local spGetUnitHealth         = Spring.GetUnitHealth
+local spGetUnitPosition       = Spring.GetUnitPosition
+local spSetUnitHealth         = Spring.SetUnitHealth
+local spSpawnCEG              = Spring.SpawnCEG
 
-	local units = {}
-	local unitsCount = 0
+local GG_AddAwardPoints
 
-	-- Engine multiplies rezzed unit HP by 0.05 just after UnitCreated so their HP has to be changed 1 frame later
-	function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
-		if (builderID) then
-			if Spring.GetUnitCurrentCommand(builderID) == CMD_RESURRECT then
-				--spSetUnitHealth(unitID, maxHealth)  -- does nothing, hp overwritten by engine
+local CMD_RESURRECT = CMD.RESURRECT
 
-				-- queue resurrected unit
-				unitsCount = unitsCount + 1
-				units[unitsCount] = unitID
+local units = {}
+local unitsCount = 0
 
-				local unitDef = unitDefID and UnitDefs[unitDefID]
+local costByDefID = {}
+local sizeByDefID = {}
+do
+	local max = math.max
+	for i = 1, #UnitDefs do
+		local unitDef = UnitDefs[i]
+		costByDefID[i] = unitDef.metalCost
+		sizeByDefID[i] = max(unitDef.xsize, unitDef.zsize)
+	end
+end
 
-				-- award calculation
-				if GG.Awards and GG.Awards.AddAwardPoints then
-					GG.Awards.AddAwardPoints( 'rezz', teamID, (unitDef and unitDef.metalCost or 0) )
-				end
-
-				-- add CEG and play sound
-				if unitDef then
-					local size = unitDef.xsize
-					local ux, uy, uz = spGetUnitPosition(unitID)
-					spSpawnCEG("resurrect", ux, uy, uz, 0, 0, 0, size)
-				end
-			end
-		end
+function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
+	if not builderID then
+		return
 	end
 
-	function gadget:GameFrame(n)
-		-- apply pending unit health changes
-		if (unitsCount ~= 0) then
-			for i = 1, unitsCount do
-			local maxHealth = select(2, spGetUnitHealth(units[i]))
-				if maxHealth then
-					spSetUnitHealth(units[i], maxHealth)
-				end
-
-				units[i] = nil
-			end
-			unitsCount = 0
-		end
+	if spGetUnitCurrentCommand(builderID) ~= CMD_RESURRECT then
+		return
 	end
 
- -- UNSYNCED
---[[
-	local spGetLocalAllyTeamID = Spring.GetLocalAllyTeamID
-	local spGetSpectatingState = Spring.GetSpectatingState
-	local spIsPosInLos         = Spring.IsPosInLos
-	local spPlaySoundFile      = Spring.PlaySoundFile
-	local spGetUnitPosition    = Spring.GetUnitPosition
-	local CMD_RESURRECT        = CMD.RESURRECT
+	--[[ cannot do anything immediately, engine multiplies rezzed
+	     unit health by 0.05 right *after* UnitCreated so their
+	     health has to be changed 1 frame later ]]
+	unitsCount = unitsCount + 1
+	units[unitsCount] = unitID
 
-	local function RezSound(x, y, z)
-		local spec = select(2, spGetSpectatingState())
-		local myAllyTeam = spGetLocalAllyTeamID()
-		if (spec or spIsPosInLos(x, y, z, myAllyTeam)) then
-			spPlaySoundFile("sounds/misc/resurrect.wav", 15, x, y, z)
+	GG_AddAwardPoints("rezz", teamID, costByDefID[unitDefID])
+
+	local ux, uy, uz = spGetUnitPosition(unitID)
+	spSpawnCEG("resurrect", ux, uy, uz, 0, 0, 0, sizeByDefID[unitDefID])
+end
+
+function gadget:GameFrame(n)
+	for i = 1, unitsCount do
+		local unitID = units[i]
+		local _, maxHealth = spGetUnitHealth(unitID)
+		if maxHealth then
+			--[[ needs a nil check since the 1 frame delay opens up
+			     a window for the unit to have been removed ]]
+			spSetUnitHealth(unitID, maxHealth)
 		end
 	end
-	
-	function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
-		if (builderID) then
-			local command = Spring.GetCommandQueue(builderID, 1)[1]
-			if (command and command.id == CMD_RESURRECT) then
-				local unitDef = unitDefID and UnitDefs[unitDefID]
-				-- add CEG and play sound
-				if unitDef then
-					local ux, uy, uz = spGetUnitPosition(unitID)
-					RezSound(ux, uy, uz)
-				end
-			end
-		end
-	end
---]]
+	unitsCount = 0
+end
+
+function gadget:Initialize()
+	GG_AddAwardPoints = GG.Awards.AddAwardPoints -- we're in a later layer so this should be guaranteed to exist
+end
