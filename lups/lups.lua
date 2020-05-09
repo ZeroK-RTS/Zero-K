557f22c46067aa9b5cd9da1186649e86d6e97202
GoogleFrog <googl3frog@gmail.com>
2020-05-09 15:23:38 +1000

    Lups fiddling. Added a WIP standalone airjets, lacks visibility test and distortion. Tried making widget Lups always draw unit effects, rather than check visibility.

--- lups/lups.lua
+++ lups/lups.lua
@@ -107,6 +107,8 @@ local GL_ONE     = GL.ONE
 local GL_SRC_ALPHA = GL.SRC_ALPHA
 local GL_ONE_MINUS_SRC_ALPHA = GL.ONE_MINUS_SRC_ALPHA
 
+local isWidget = (widgetHandler and true) or false
+
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
@@ -769,7 +771,7 @@ local function IsUnitFXVisible(fx)
 	end
 	--Spring.Utilities.UnitEcho(unitID, "w")
 	if (unitActive) then
-		if fx.alwaysVisible then
+		if isWidget or fx.alwaysVisible then
 			return true
 		elseif (fx.Visible) then
 			return fx:Visible()
