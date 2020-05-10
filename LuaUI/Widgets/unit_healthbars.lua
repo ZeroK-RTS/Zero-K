f32ad2ca20f0dc2395d43615413d6b0612aef280
sprunk <spr.ng@o2.pl>
2020-05-10 21:41:02 +0200

    Healthbars: remove dragon's teeth list

--- LuaUI/Widgets/unit_healthbars.lua
+++ LuaUI/Widgets/unit_healthbars.lua
@@ -52,9 +52,6 @@ local gameSpeed = Game.gameSpeed
 
 local TELEPORT_CHARGE_NEEDED = Spring.GetGameRulesParam("pw_teleport_time") or gameSpeed*60
 
---// this table is used to shows the hp of perimeter defence, and filter it for default wreckages
-local walls = {dragonsteeth = true, dragonsteeth_core = true, fortification = true, fortification_core = true, spike = true, floatingteeth = true, floatingteeth_core = true, spike = true}
-
 local stockpileH = 24
 local stockpileW = 12
 
@@ -920,7 +917,6 @@ do
 			local featureDef = FeatureDefs[featureDefID or -1] or {height = 0, name = ''}
 			customInfo[featureDefID] = {
 				height = featureDef.height+14,
-				wall   = walls[featureDef.name],
 			}
 		end
 		ci = customInfo[featureDefID]
@@ -936,7 +932,7 @@ do
 
 		hp = (health or 0)/(maxHealth or 1)
 
-		--// filter all walls and none resurrecting features
+		--// filter all intact features
 		if (resurrect == 0) and
 			 (reclaimLeft == 1) and
 			 (hp > featureHpThreshold) then
