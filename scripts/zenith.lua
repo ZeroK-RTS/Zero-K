24cfd75fd563952d10d2d9e3ab9d7c589ddfdb89
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2020-07-09 04:03:03 -0700

    isBlocked ->meteorSpawnBlocked

--- scripts/zenith.lua
+++ scripts/zenith.lua
@@ -192,10 +192,10 @@ local function SpawnProjectileThread()
 			RegainControlOfMeteors()
 			currentlyStunned = false
 		end
-		if not spGetUnitRulesParam(unitID,"isBlocked") or spGetUnitRulesParam(unitID,"isBlocked") == 0 then -- Only spawn if there's a clear view to the sky
+		if spGetUnitRulesParam(unitID,"meteorSpawnBlocked") ~= 0 then -- Only spawn if there's a clear view to the sky
 			SpawnMeteor()
 		end
-		spSetUnitRulesParam(unitID,"isBlocked",0)
+		spSetUnitRulesParam(unitID,"meteorSpawnBlocked",0)
 	end
 end
 
@@ -280,10 +280,11 @@ function OnLoadGame()
 end
 
 function script.Create()
+	spSetUnitRulesParam(unitID,"meteorSpawnBlocked",0)
 	if Spring.GetGameRulesParam("loadPurge") ~= 1 then
 		Spring.SetUnitRulesParam(unitID, "meteorsControlled", 0, INLOS_ACCESS)
 	end
-	Spring.SetUnitRulesParam(unitID, "meteorsControlledMax", METEOR_CAPACITY, INLOS_ACCESS)
+	spSetUnitRulesParam(unitID, "meteorsControlledMax", METEOR_CAPACITY, INLOS_ACCESS)
 	local x, _, z = Spring.GetUnitPosition(unitID)
 	ux, uy, uz = x, Spring.GetGroundHeight(x, z), z
 	
