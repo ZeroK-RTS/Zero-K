21a39c6de5be4caa183a4bb880e7d9607b43e2ec
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2020-07-15 12:29:40 -0700

    Skyline blockage now loses control of meteors

--- scripts/zenith.lua
+++ scripts/zenith.lua
@@ -21,7 +21,7 @@ local fireRange = WeaponDefNames["zenith_meteor"].range
 local smokePiece = {base}
 
 local Vector = Spring.Utilities.Vector
-
+local isBlocked = false
 local projectiles = {}
 local projectileCount = 0
 local oldestProjectile = 1 -- oldestProjectile is for when the projectile table is being cyclicly overridden.
@@ -191,6 +191,13 @@ local function SpawnProjectileThread()
 		if currentlyStunned then
 			RegainControlOfMeteors()
 			currentlyStunned = false
+		end
+		if not currentlyStunned and isBlocked and spGetUnitRulesParam(unitID, "meteorSpawnBlocked") ~= 1 then
+			RegainControlOfMeteors()
+			isBlocked = false
+		elseif not currentlyStunned and spGetUnitRulesParam(unitID, "meteorSpawnBlocked") == 1 then
+			LoseControlOfMeteors()
+			isBlocked = true
 		end
 		if spGetUnitRulesParam(unitID, "meteorSpawnBlocked") ~= 1 then -- Only spawn if there's a clear view to the sky
 			SpawnMeteor()
