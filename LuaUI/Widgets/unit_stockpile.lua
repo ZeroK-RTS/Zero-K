be0c10f683c1b46ac7fecd91fbce8a0d2e7a5b1e
GoogleFrog <googl3frog@gmail.com>
2020-07-18 19:50:21 +1000

    Moved the stockpile initial state to the area associated to each unit. Stopped Artemis construction spamming 100 commands.

--- LuaUI/Widgets/unit_stockpile.lua
+++ LuaUI/Widgets/unit_stockpile.lua
@@ -23,23 +23,6 @@ function widget:GetInfo()
 	}
 end
 
-options_path = "Settings/Unit Behaviour/Default States/Missile Stockpilers"
-options = {}
-for id,ud in pairs(UnitDefs) do
-	if ud.canStockpile then
-		options[ud.name .. "_missiles"] = {
-			name = ud.humanName .. " missiles",
-			desc = "Unit will build missiles whenever it has fewer than this in stock.",
-			type = "number",
-			value = (ud.name == "turretaaheavy" and 100 or 10),
-			min = 0,
-			max = 100,
-			tooltip_format = "%.0f",
-			noHotkey = true,
-		}
-	end
-end
-
 local CMD_STOCKPILE = CMD.STOCKPILE
 local EMPTY_TABLE = {}
 
@@ -50,29 +33,6 @@ function widget:Initialize()
 		Spring.Echo("<Stockpiler>: disabled for spectators")
 		widgetHandler:RemoveWidget()
 		return
-	end
-end
-
-function widget:UnitCreated(unitID, unitDefID, unitTeam)
-	local ud = UnitDefs[unitDefID]
-	if ((ud ~= nil) and (unitTeam == Spring.GetMyTeamID())) then
-		if (ud.canStockpile) then
-			local stocked, queued = Spring.GetUnitStockpile(unitID)
-			local wanted = options[ud.name .. "_missiles"].value
-			local to_add = wanted - stocked - queued
-			for i = 1, to_add do
-				Spring.GiveOrderToUnit(unitID, CMD.STOCKPILE, EMPTY_TABLE, 0)
-			end
-		end
-	end
-end
-
-function widget:GameFrame(n)
-	if n > 1 then
-		for _, unitID in ipairs(Spring.GetAllUnits()) do
-			widget:UnitCreated(unitID, Spring.GetUnitDefID(unitID), Spring.GetUnitTeam(unitID))
-		end
-		widgetHandler:RemoveCallIn("GameFrame")
 	end
 end
 
