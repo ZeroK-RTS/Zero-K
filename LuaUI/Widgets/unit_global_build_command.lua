b0173e0e68baba2486e6aa46fb5e7d4b6426565b
esainane <esainane+github@gmail.com>
2020-08-03 15:48:56 +1200

    GBC: Cleaner handling of reassignedUnits
    This does two things: Move the main assignment of reassignedUnits to
    GiveWorkToUnit, and move unassignment of reassignedUnits
    UnassignUnit (with the inverse in AssignWorker).
    
    Moving the main assignment of reassignedUnits to GiveWorkToUnit lets
    us condense the three former locations where it would be set,
    in two branches of FindEligibleWorker, and in CheckIdlers, to the one
    location where all three branches immediately proceed to.
    The only other place where GiveWorkToUnit is called from, UnitGiven,
    also seems like it wants to have reassignedUnits set, so cohesion
    already seems to be paying off.
    
    Having UnassignWorker clear reassignedUnits lets us use UnassignWorker
    to clean up a few more places, while removing the need for special casing.
    Note that AssignWorker does not need to perform the inverse,
    since UnassignWorker need only clear reassignedUnits when idle, which
    never happens while work is being assigned.

--- LuaUI/Widgets/unit_global_build_command.lua
+++ LuaUI/Widgets/unit_global_build_command.lua
@@ -1010,17 +1010,13 @@ function FindEligibleWorker()
 			-- remove the unit from the list of constructors
 			UnassignWorker(nil, unitID, nil)
 		elseif myCmd.cmdtype == commandType.idle and not reassignedUnits[unitID] then -- first we assign idle units
-			reassignedUnits[unitID] = true
 			return unitID
 		end
 	end
 	--if there are no idlers available, reassign an already assigned unit to see if there is a better job it can be doing.
 	for unitID,cmdstate in pairs(includedBuilders) do
-		if (not reassignedUnits[unitID]) then
-			if cmdstate.cmdtype == commandType.buildQueue then
-				reassignedUnits[unitID] = true
-				return unitID
-			end
+		if (not reassignedUnits[unitID]) and cmdstate.cmdtype == commandType.buildQueue then
+			return unitID
 		end
 	end
 	reassignedUnits = {} --no more unit to be reassigned? then reset list
@@ -1029,6 +1025,7 @@ end
 
 -- This function finds work for all the workers compiled in our eligible worker list and issues the orders.
 function GiveWorkToUnit(unitID)
+	reassignedUnits[unitID] = true -- We have tried to give this unit work, so don't try to reassign it right away.
 	local myJob = FindCheapestJob(unitID) -- find the cheapest job
 	if not myJob then
 		-- Nothing to do, mark us as idle
@@ -1469,13 +1466,11 @@ function CheckIdlers()
 			-- we need to check that the unit's command queue is empty, because other gadgets may invoke UnitIdle erroneously.
 			-- if there's a command on the queue, do nothing and let it be removed from the idle list.
 			if spGetCommandQueue(unitID, 0) == 0 then
-				includedBuilders[unitID].cmdtype = commandType.idle -- then mark it as idle
+				UnassignWorker(nil, unitID, commandType.idle) -- then mark it as idle
 				if newBuilders[unitID] then -- for new units that have just been added and finished following their constructor separator orders.
 					newBuilders[unitID] = nil
 					GiveWorkToUnit(unitID)
-					reassignedUnits[unitID] = true
 				else
-					reassignedUnits[unitID] = nil
 					movingUnits[unitID] = nil
 					if busyUnits[unitID] then -- if the worker is also still on our busy list
 						local key = busyUnits[unitID]
@@ -1509,8 +1504,7 @@ function CheckMovingUnits()
 				spGiveOrderToUnit(unitID, CMD_REMOVE, {CMD_RAW_MOVE}, CMD_OPT_ALT) -- remove the current order
 				-- note: options "alt" with CMD_REMOVE tells it to use params as command ids, which is what we want.
 				spGiveOrderToUnit(unitID, CMD_STOP, EMPTY_TABLE, 0) -- and replace it with a stop order
-				includedBuilders[unitID].cmdtype = commandType.idle
-				reassignedUnits[unitID] = nil
+				UnassignWorker(nil, unitID, commandType.idle)
 			end
 		else -- for units that are actually dead, etc.
 			movingUnits[unitID] = nil
@@ -1915,6 +1909,9 @@ end
 -- when cmdtype is nil, removes all state related to the unit.
 function UnassignWorker(key, unitID, cmdtype)
 	movingUnits[unitID] = nil
+	if cmdtype == commandType.idle then
+		reassignedUnits[unitID] = nil -- Remove them from our reassigned units list, so that they will be immediately processed
+	end
 	if not cmdtype then
 		-- Destroy ALL state tracking this builder (except allBuilders?)
 		includedBuilders[unitID] = nil
