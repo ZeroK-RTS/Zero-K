37894afd63207e66f766ee32dca2bfa41184669f
Anarchid <anarchid@mail.ru>
2020-08-04 22:35:50 +0300

    cloak pad

---
+++ units/padcloak.lua
@@ -0,0 +1,93 @@
+return { padcloak = {
+  unitname                      = [[padcloak]],
+  name                          = [[Cloakbot Pad]],
+  description                   = [[Produces Cloaked, Mobile Robots]],
+  buildCostMetal                = Shared.FACTORY_COST,
+  builder                       = true,
+  buildingGroundDecalDecaySpeed = 30,
+  buildingGroundDecalSizeX      = 13,
+  buildingGroundDecalSizeY      = 13,
+  buildingGroundDecalType       = [[pad_decal.dds]],
+
+  buildoptions                  = {
+    [[cloakcon]],
+    [[cloakraid]],
+    [[cloakheavyraid]],
+    [[cloakskirm]],
+    [[cloakriot]],
+    [[cloakassault]],
+    [[cloakarty]],
+    [[cloaksnipe]],
+    [[cloakaa]],
+    [[cloakbomb]],
+    [[cloakjammer]],
+  },
+
+  buildPic                      = [[factorycloak.png]],
+  canMove                       = true,
+  canPatrol                     = true,
+  category                      = [[SINK UNARMED]],
+  corpse                        = [[DEAD]],
+
+  customParams                  = {
+    sortName        = [[1]],
+    default_spacing = 8,
+    midposoffset    = [[0 0 -24]],
+    solid_factory   = [[7]],
+    unstick_help    = [[1]],
+    selectionscalemult = 1,
+    factorytab       = 1,
+    shared_energy_gen = 1,
+  },
+
+  energyUse                     = 0,
+  explodeAs                     = [[LARGE_BUILDINGEX]],
+  footprintX                    = 7,
+  footprintZ                    = 10,
+  iconType                      = [[fackbot]],
+  idleAutoHeal                  = 5,
+  idleTime                      = 1800,
+  maxDamage                     = 4000,
+  maxSlope                      = 15,
+  maxWaterDepth                 = 0,
+  minCloakDistance              = 150,
+  moveState                     = 1,
+  noAutoFire                    = false,
+  objectName                    = [[pad_cloak.dae]],
+  script                        = [[factorycloak.lua]],
+  selfDestructAs                = [[LARGE_BUILDINGEX]],
+
+  sfxtypes                      = {
+
+    explosiongenerators = {
+      [[custom:WhiteLight]],
+    },
+
+  },
+
+  showNanoSpray                 = false,
+  sightDistance                 = 273,
+  useBuildingGroundDecal        = true,
+  workerTime                    = Shared.FACTORY_BUILDPOWER,
+  yardMap                       = "ooooooo ooooooo ooooooo occccco occccco occccco occccco yyyyyyy yyyyyyy yyyyyyy",
+
+  featureDefs                   = {
+
+    DEAD  = {
+      blocking         = true,
+      featureDead      = [[HEAP]],
+      footprintX       = 7,
+      footprintZ       = 7,
+      object           = [[cremfactorywreck.s3o]],
+    },
+
+    HEAP  = {
+      blocking         = false,
+      footprintX       = 7,
+      footprintZ       = 7,
+      object           = [[debris4x4b.s3o]],
+    },
+
+  },
+
+} }
