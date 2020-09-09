1879c8a9e6a60d95bd3aabe89352139c118d9679
Shark Mobile Workstation <owensilva1282@gmail.com>
2020-09-09 14:07:51 -0400

    Remove more whitespace
    Removed a some trailing whitespace that I had previously missed, that should be everything.

--- scripts/striderarty.lua
+++ scripts/striderarty.lua
@@ -1,64 +1,64 @@
 include "constants.lua"
 
-local torso = piece 'torso' 
-local pelvis = piece 'pelvis' 
-local rupleg = piece 'rupleg' 
-local rleg = piece 'rleg' 
-local rfoot = piece 'rfoot' 
-local rtoer = piece 'rtoer' 
-local rtoef1 = piece 'rtoef1' 
-local rtoef2 = piece 'rtoef2' 
-local lleg = piece 'lleg' 
-local lupleg = piece 'lupleg' 
-local lfoot = piece 'lfoot' 
-local ltoer = piece 'ltoer' 
-local ltoef1 = piece 'ltoef1' 
-local ltoef2 = piece 'ltoef2' 
-local launchers = piece 'launchers' 
-local llauncher = piece 'llauncher' 
-local lfire1 = piece 'lfire1' 
-local lfire2 = piece 'lfire2' 
-local lfire3 = piece 'lfire3' 
-local lfire4 = piece 'lfire4' 
-local lfire5 = piece 'lfire5' 
-local lfire6 = piece 'lfire6' 
-local lfire7 = piece 'lfire7' 
-local lfire8 = piece 'lfire8' 
-local lfire9 = piece 'lfire9' 
-local lfire10 = piece 'lfire10' 
-local lfire11 = piece 'lfire11' 
-local lfire12 = piece 'lfire12' 
-local lfire13 = piece 'lfire13' 
-local lfire14 = piece 'lfire14' 
-local lfire15 = piece 'lfire15' 
-local lfire16 = piece 'lfire16' 
-local lfire17 = piece 'lfire17' 
-local lfire18 = piece 'lfire18' 
-local lfire19 = piece 'lfire19' 
-local lfire20 = piece 'lfire20' 
-local rlauncher = piece 'rlauncher' 
-local rfire1 = piece 'rfire1' 
-local rfire2 = piece 'rfire2' 
-local rfire3 = piece 'rfire3' 
-local rfire4 = piece 'rfire4' 
-local rfire5 = piece 'rfire5' 
-local rfire6 = piece 'rfire6' 
-local rfire7 = piece 'rfire7' 
-local rfire8 = piece 'rfire8' 
-local rfire9 = piece 'rfire9' 
-local rfire10 = piece 'rfire10' 
-local rfire11 = piece 'rfire11' 
-local rfire12 = piece 'rfire12' 
-local rfire13 = piece 'rfire13' 
-local rfire14 = piece 'rfire14' 
-local rfire15 = piece 'rfire15' 
-local rfire16 = piece 'rfire16' 
-local rfire17 = piece 'rfire17' 
-local rfire18 = piece 'rfire18' 
-local rfire19 = piece 'rfire19' 
-local rfire20 = piece 'rfire20' 
+local torso = piece 'torso'
+local pelvis = piece 'pelvis'
+local rupleg = piece 'rupleg'
+local rleg = piece 'rleg'
+local rfoot = piece 'rfoot'
+local rtoer = piece 'rtoer'
+local rtoef1 = piece 'rtoef1'
+local rtoef2 = piece 'rtoef2'
+local lleg = piece 'lleg'
+local lupleg = piece 'lupleg'
+local lfoot = piece 'lfoot'
+local ltoer = piece 'ltoer'
+local ltoef1 = piece 'ltoef1'
+local ltoef2 = piece 'ltoef2'
+local launchers = piece 'launchers'
+local llauncher = piece 'llauncher'
+local lfire1 = piece 'lfire1'
+local lfire2 = piece 'lfire2'
+local lfire3 = piece 'lfire3'
+local lfire4 = piece 'lfire4'
+local lfire5 = piece 'lfire5'
+local lfire6 = piece 'lfire6'
+local lfire7 = piece 'lfire7'
+local lfire8 = piece 'lfire8'
+local lfire9 = piece 'lfire9'
+local lfire10 = piece 'lfire10'
+local lfire11 = piece 'lfire11'
+local lfire12 = piece 'lfire12'
+local lfire13 = piece 'lfire13'
+local lfire14 = piece 'lfire14'
+local lfire15 = piece 'lfire15'
+local lfire16 = piece 'lfire16'
+local lfire17 = piece 'lfire17'
+local lfire18 = piece 'lfire18'
+local lfire19 = piece 'lfire19'
+local lfire20 = piece 'lfire20'
+local rlauncher = piece 'rlauncher'
+local rfire1 = piece 'rfire1'
+local rfire2 = piece 'rfire2'
+local rfire3 = piece 'rfire3'
+local rfire4 = piece 'rfire4'
+local rfire5 = piece 'rfire5'
+local rfire6 = piece 'rfire6'
+local rfire7 = piece 'rfire7'
+local rfire8 = piece 'rfire8'
+local rfire9 = piece 'rfire9'
+local rfire10 = piece 'rfire10'
+local rfire11 = piece 'rfire11'
+local rfire12 = piece 'rfire12'
+local rfire13 = piece 'rfire13'
+local rfire14 = piece 'rfire14'
+local rfire15 = piece 'rfire15'
+local rfire16 = piece 'rfire16'
+local rfire17 = piece 'rfire17'
+local rfire18 = piece 'rfire18'
+local rfire19 = piece 'rfire19'
+local rfire20 = piece 'rfire20'
 
---MAKE SURE FIRE POINTS REMAIN CONSECUTIVEnot 
+--MAKE SURE FIRE POINTS REMAIN CONSECUTIVE!
 local bAiming, gun_1, gun_1_side
 
 --Init variables
@@ -82,7 +82,7 @@ local function Walk()
 			Turn(lfoot, x_axis, math.rad(-15.016484), math.rad(70.016484))
 			Turn(rfoot, x_axis, math.rad(5), math.rad(50.010989))
 			Turn(rleg, x_axis, math.rad(-10), math.rad(70.016484))
-			Sleep(304)	
+			Sleep(304)
 			Turn(lfoot, x_axis, math.rad(20.016484), math.rad(100.021978))
 			Turn(rfoot, x_axis, math.rad(10), math.rad(50.010989))
 			Turn(rleg, x_axis, math.rad(20.016484), math.rad(100.021978))
@@ -90,7 +90,7 @@ local function Walk()
 			Turn(ltoef2, x_axis, math.rad(22.5016484), math.rad(100.021978))
 			Turn(ltoer, x_axis, math.rad(-22.5016484), math.rad(100.021978))
 			Turn(rtoef1, x_axis, 0, math.rad(100.021978))
-			Turn(rtoef2, x_axis, 0, math.rad(100.021978))		
+			Turn(rtoef2, x_axis, 0, math.rad(100.021978))
 			Sleep(360)
 			Turn(rtoer, x_axis, 0, math.rad(100.021978))
 			Move(pelvis, y_axis, 0, 2)
@@ -116,7 +116,7 @@ local function Walk()
 			Turn(rupleg, x_axis, math.rad(-20.016484), math.rad(50.010989))
 			Turn(lfoot, x_axis, math.rad(-20.016484), math.rad(130.027473))
 			Turn(rleg, x_axis, math.rad(-20.016484), math.rad(100.021978))
-			Sleep(650)		
+			Sleep(650)
 			Turn(lfoot, x_axis, math.rad(20.016484), math.rad(100.021978))
 			Turn(rfoot, x_axis, math.rad(20.016484), math.rad(70.016484))
 			Turn(rleg, x_axis, math.rad(20.016484), math.rad(100.021978))
@@ -248,7 +248,7 @@ local function RestoreLegs()
 	Turn(rfoot, x_axis, 0, math.rad(100.021978))
 	Turn(lfoot, x_axis, 0, math.rad(100.021978))
 
-	if not aiming then			
+	if not aiming then
 		Turn(torso, y_axis, 0, math.rad(100.021978))
 		Turn(torso, x_axis, 0, math.rad(20))
 	end
@@ -286,26 +286,26 @@ function script.AimFromWeapon(num)
 	return launchers
 end
 
-function script.FireWeapon(num) 
+function script.FireWeapon(num)
 	gun_1 = -1
 	gun_1_side = 1
 end
 
-function script.Shot(num) 
+function script.Shot(num)
 	if gun_1_side then
 		gun_1 = gun_1 + 1
 	end
 	gun_1_side = not gun_1_side
 end
 
-function script.QueryWeapon(num) 
-	if gun_1 >= 0 and gun_1 < 20 then 
+function script.QueryWeapon(num)
+	if gun_1 >= 0 and gun_1 < 20 then
 		if gun_1_side then
 			return (rfire1 + gun_1)
-		else 
+		else
 			return (lfire1 + gun_1)
 		end
-	else 
+	else
 		return launchers
     end
 end
