9fb5dfad952809991962d4dbc98c9c1fcab60beb
GoogleFrog <googl3frog@gmail.com>
2020-09-10 13:51:41 +1000

    Fixed some Merlin LUS style and removed unused piece definitions.

--- scripts/striderarty.lua
+++ scripts/striderarty.lua
@@ -16,63 +16,19 @@ local ltoef1 = piece 'ltoef1'
 local ltoef2 = piece 'ltoef2'
 local launchers = piece 'launchers'
 local llauncher = piece 'llauncher'
-local lfire1 = piece 'lfire1'
-local lfire2 = piece 'lfire2'
-local lfire3 = piece 'lfire3'
-local lfire4 = piece 'lfire4'
-local lfire5 = piece 'lfire5'
-local lfire6 = piece 'lfire6'
-local lfire7 = piece 'lfire7'
-local lfire8 = piece 'lfire8'
-local lfire9 = piece 'lfire9'
-local lfire10 = piece 'lfire10'
-local lfire11 = piece 'lfire11'
-local lfire12 = piece 'lfire12'
-local lfire13 = piece 'lfire13'
-local lfire14 = piece 'lfire14'
-local lfire15 = piece 'lfire15'
-local lfire16 = piece 'lfire16'
-local lfire17 = piece 'lfire17'
-local lfire18 = piece 'lfire18'
-local lfire19 = piece 'lfire19'
-local lfire20 = piece 'lfire20'
 local rlauncher = piece 'rlauncher'
-local rfire1 = piece 'rfire1'
-local rfire2 = piece 'rfire2'
-local rfire3 = piece 'rfire3'
-local rfire4 = piece 'rfire4'
-local rfire5 = piece 'rfire5'
-local rfire6 = piece 'rfire6'
-local rfire7 = piece 'rfire7'
-local rfire8 = piece 'rfire8'
-local rfire9 = piece 'rfire9'
-local rfire10 = piece 'rfire10'
-local rfire11 = piece 'rfire11'
-local rfire12 = piece 'rfire12'
-local rfire13 = piece 'rfire13'
-local rfire14 = piece 'rfire14'
-local rfire15 = piece 'rfire15'
-local rfire16 = piece 'rfire16'
-local rfire17 = piece 'rfire17'
-local rfire18 = piece 'rfire18'
-local rfire19 = piece 'rfire19'
-local rfire20 = piece 'rfire20'
 
---MAKE SURE FIRE POINTS REMAIN CONSECUTIVE!
-local bAiming, gun_1, gun_1_side
-
---Init variables
-bAiming = false
-gun_1 = 1
-gun_1_side = 0
-
---Create an array to hold all left and right fire poitns
 local lfires = {}
 local rfires = {}
 for i = 1, 20 do
 	lfires[i] = piece('lfire' .. i)
 	rfires[i] = piece('rfire' .. i)
 end
+
+--Init variables
+local bAiming = false
+local gun_1 = 1
+local gun_1_side = 0
 
 -- Signal definitions
 local SIG_WALK = 1
@@ -83,85 +39,93 @@ local function Walk()
 	SetSignalMask(SIG_WALK)
 
 	while true do
-		Turn(lupleg, x_axis, math.rad(20.016484), math.rad(50.010989))
-		Turn(rupleg, x_axis, math.rad(-20.016484), math.rad(50.010989))
-		Turn(lfoot, x_axis, math.rad(-15.016484), math.rad(70.016484))
-		Turn(rfoot, x_axis, math.rad(5), math.rad(50.010989))
-		Turn(rleg, x_axis, math.rad(-10), math.rad(70.016484))
+		Turn(lupleg, x_axis, math.rad(20), math.rad(50))
+		Turn(rupleg, x_axis, math.rad(-20), math.rad(50))
+		Turn(lfoot, x_axis, math.rad(-15), math.rad(70))
+		Turn(rfoot, x_axis, math.rad(5), math.rad(50))
+		Turn(rleg, x_axis, math.rad(-10), math.rad(70))
 		if not bAiming then
 			Turn(torso, x_axis, math.rad(-1), math.rad(5))
 		end
 		Sleep(304)
-		Turn(lfoot, x_axis, math.rad(20.016484), math.rad(100.021978))
-		Turn(rfoot, x_axis, math.rad(10), math.rad(50.010989))
-		Turn(rleg, x_axis, math.rad(20.016484), math.rad(100.021978))
-		Turn(ltoef1, x_axis, math.rad(22.5016484), math.rad(100.021978))
-		Turn(ltoef2, x_axis, math.rad(22.5016484), math.rad(100.021978))
-		Turn(ltoer, x_axis, math.rad(-22.5016484), math.rad(100.021978))
-		Turn(rtoef1, x_axis, 0, math.rad(100.021978))
-		Turn(rtoef2, x_axis, 0, math.rad(100.021978))
+		
+		Turn(lfoot, x_axis, math.rad(20), math.rad(100))
+		Turn(rfoot, x_axis, math.rad(10), math.rad(50))
+		Turn(rleg, x_axis, math.rad(20), math.rad(100))
+		Turn(ltoef1, x_axis, math.rad(22.5), math.rad(100))
+		Turn(ltoef2, x_axis, math.rad(22.5), math.rad(100))
+		Turn(ltoer, x_axis, math.rad(-22.5), math.rad(100))
+		Turn(rtoef1, x_axis, 0, math.rad(100))
+		Turn(rtoef2, x_axis, 0, math.rad(100))
 		Sleep(360)
-		Turn(rtoer, x_axis, 0, math.rad(100.021978))
+		
+		Turn(rtoer, x_axis, 0, math.rad(100))
 		Move(pelvis, y_axis, 0, 2)
 		Turn(pelvis, z_axis, math.rad(-3.50), math.rad(3))
-		Turn(lupleg, x_axis, math.rad(-20.016484), math.rad(50.010989))
-		Turn(rupleg, x_axis, math.rad(20.016484), math.rad(50.010989))
-		Turn(rfoot, x_axis, math.rad(-20.016484), math.rad(130.027473))
-		Turn(lleg, x_axis, math.rad(-20.016484), math.rad(100.021978))
+		Turn(lupleg, x_axis, math.rad(-20), math.rad(50))
+		Turn(rupleg, x_axis, math.rad(20), math.rad(50))
+		Turn(rfoot, x_axis, math.rad(-20), math.rad(130))
+		Turn(lleg, x_axis, math.rad(-20), math.rad(100))
 		Sleep(650)
-		Turn(rfoot, x_axis, math.rad(20.016484), math.rad(100.021978))
-		Turn(lleg, x_axis, math.rad(20.016484), math.rad(100.021978))
+		
+		Turn(rfoot, x_axis, math.rad(20), math.rad(100))
+		Turn(lleg, x_axis, math.rad(20), math.rad(100))
 		Move(pelvis, y_axis, 0, 2)
-		Turn(ltoef1, x_axis, 0, math.rad(100.021978))
-		Turn(ltoef2, x_axis, 0, math.rad(100.021978))
-		Turn(rtoef1, x_axis, math.rad(22.5016484), math.rad(100.021978))
-		Turn(rtoef2, x_axis, math.rad(22.5016484), math.rad(100.021978))
-		Turn(rtoer, x_axis, math.rad(-22.5016484), math.rad(100.021978))
+		Turn(ltoef1, x_axis, 0, math.rad(100))
+		Turn(ltoef2, x_axis, 0, math.rad(100))
+		Turn(rtoef1, x_axis, math.rad(22.5), math.rad(100))
+		Turn(rtoef2, x_axis, math.rad(22.5), math.rad(100))
+		Turn(rtoer, x_axis, math.rad(-22.5), math.rad(100))
 		Sleep(360)
-		Turn(ltoer, x_axis, 0, math.rad(100.021978))
+		
+		Turn(ltoer, x_axis, 0, math.rad(100))
 		Move(pelvis, y_axis, 4, 2)
 		Turn(pelvis, z_axis, math.rad(3.5), math.rad(8))
-		Turn(lupleg, x_axis, math.rad(20.016484), math.rad(50.010989))
-		Turn(rupleg, x_axis, math.rad(-20.016484), math.rad(50.010989))
-		Turn(lfoot, x_axis, math.rad(-20.016484), math.rad(130.027473))
-		Turn(rleg, x_axis, math.rad(-20.016484), math.rad(100.021978))
+		Turn(lupleg, x_axis, math.rad(20), math.rad(50))
+		Turn(rupleg, x_axis, math.rad(-20), math.rad(50))
+		Turn(lfoot, x_axis, math.rad(-20), math.rad(130))
+		Turn(rleg, x_axis, math.rad(-20), math.rad(100))
 		if not bAiming then
 			Turn(torso, y_axis, math.rad(2.5), math.rad(12))
 			Turn(torso, x_axis, math.rad(1), math.rad(6))
 		end
 		Sleep(650)
-		Turn(lfoot, x_axis, math.rad(20.016484), math.rad(100.021978))
-		Turn(rfoot, x_axis, math.rad(20.016484), math.rad(70.016484))
-		Turn(rleg, x_axis, math.rad(20.016484), math.rad(100.021978))
+		
+		Turn(lfoot, x_axis, math.rad(20), math.rad(100))
+		Turn(rfoot, x_axis, math.rad(20), math.rad(70))
+		Turn(rleg, x_axis, math.rad(20), math.rad(100))
 		Move(pelvis, y_axis, 0, 2)
-		Turn(ltoef1, x_axis, math.rad(22.5016484), math.rad(100.021978))
-		Turn(ltoef2, x_axis, math.rad(22.5016484), math.rad(100.021978))
-		Turn(ltoer, x_axis, math.rad(-22.5016484), math.rad(100.021978))
-		Turn(rtoef1, x_axis, 0, math.rad(100.021978))
-		Turn(rtoef2, x_axis, 0, math.rad(100.021978))
+		Turn(ltoef1, x_axis, math.rad(22.5), math.rad(100))
+		Turn(ltoef2, x_axis, math.rad(22.5), math.rad(100))
+		Turn(ltoer, x_axis, math.rad(-22.5), math.rad(100))
+		Turn(rtoef1, x_axis, 0, math.rad(100))
+		Turn(rtoef2, x_axis, 0, math.rad(100))
 		Sleep(360)
-		Turn(rtoer, x_axis, 0, math.rad(100.021978))
+		
+		Turn(rtoer, x_axis, 0, math.rad(100))
 		Move(pelvis, y_axis, 4, 2)
 		Turn(pelvis, z_axis, math.rad(-3.50), math.rad(8))
-		Turn(lupleg, x_axis, math.rad(-20.016484), math.rad(50.010989))
-		Turn(rupleg, x_axis, math.rad(20.016484), math.rad(50.010989))
-		Turn(rfoot, x_axis, math.rad(-20.016484), math.rad(130.027473))
-		Turn(lleg, x_axis, math.rad(-20.016484), math.rad(100.021978))
+		Turn(lupleg, x_axis, math.rad(-20), math.rad(50))
+		Turn(rupleg, x_axis, math.rad(20), math.rad(50))
+		Turn(rfoot, x_axis, math.rad(-20), math.rad(130))
+		Turn(lleg, x_axis, math.rad(-20), math.rad(100))
 		if not bAiming then
 			Turn(torso, y_axis, math.rad(-2.5), math.rad(12))
 			Turn(torso, x_axis, math.rad(-1), math.rad(6))
 		end
 		Sleep(650)
-		Turn(rfoot, x_axis, math.rad(20.016484), math.rad(100.021978))
-		Turn(lleg, x_axis, math.rad(20.016484), math.rad(100.021978))
+		
+		Turn(rfoot, x_axis, math.rad(20), math.rad(100))
+		Turn(lleg, x_axis, math.rad(20), math.rad(100))
 		Move(pelvis, y_axis, 0, 2)
-		Turn(ltoef1, x_axis, 0, math.rad(100.021978))
-		Turn(ltoef2, x_axis, 0, math.rad(100.021978))
-		Turn(rtoef1, x_axis, math.rad(22.5016484), math.rad(100.021978))
-		Turn(rtoef2, x_axis, math.rad(22.5016484), math.rad(100.021978))
-		Turn(rtoer, x_axis, math.rad(-22.5016484), math.rad(100.021978))
+		Turn(ltoef1, x_axis, 0, math.rad(100))
+		Turn(ltoef2, x_axis, 0, math.rad(100))
+		Turn(rtoef1, x_axis, math.rad(22.5), math.rad(100))
+		Turn(rtoef2, x_axis, math.rad(22.5), math.rad(100))
+		Turn(rtoer, x_axis, math.rad(-22.5), math.rad(100))
 		Sleep(360)
-		Turn(ltoer, x_axis, 0, math.rad(100.021978))
+		
+		Turn(ltoer, x_axis, 0, math.rad(100))
 		Move(pelvis, y_axis, 4, 2)
 		Turn(pelvis, z_axis, math.rad(3.5), math.rad(8))
 	end
@@ -171,23 +135,23 @@ local function RestoreLegs()
 	Signal(SIG_WALK)
 	SetSignalMask(SIG_WALK)
 
-	Turn(lupleg, x_axis, 0, math.rad(50.010989))
-	Turn(rupleg, x_axis, 0, math.rad(50.010989))
-	Turn(lleg, x_axis, 0, math.rad(100.021978))
-	Turn(rleg, x_axis, 0, math.rad(100.021978))
+	Turn(lupleg, x_axis, 0, math.rad(50))
+	Turn(rupleg, x_axis, 0, math.rad(50))
+	Turn(lleg, x_axis, 0, math.rad(100))
+	Turn(rleg, x_axis, 0, math.rad(100))
 	Move(pelvis, y_axis, 0, 20)
 	Turn(pelvis, z_axis, math.rad(-(0)), math.rad(20))
-	Turn(rtoef1, x_axis, 0, math.rad(100.021978))
-	Turn(rtoef2, x_axis, 0, math.rad(100.021978))
-	Turn(rtoer, x_axis, 0, math.rad(100.021978))
-	Turn(ltoef1, x_axis, 0, math.rad(100.021978))
-	Turn(ltoef2, x_axis, 0, math.rad(100.021978))
-	Turn(ltoer, x_axis, 0, math.rad(100.021978))
-	Turn(rfoot, x_axis, 0, math.rad(100.021978))
-	Turn(lfoot, x_axis, 0, math.rad(100.021978))
+	Turn(rtoef1, x_axis, 0, math.rad(100))
+	Turn(rtoef2, x_axis, 0, math.rad(100))
+	Turn(rtoer, x_axis, 0, math.rad(100))
+	Turn(ltoef1, x_axis, 0, math.rad(100))
+	Turn(ltoef2, x_axis, 0, math.rad(100))
+	Turn(ltoer, x_axis, 0, math.rad(100))
+	Turn(rfoot, x_axis, 0, math.rad(100))
+	Turn(lfoot, x_axis, 0, math.rad(100))
 
 	if not bAiming then
-		Turn(torso, y_axis, 0, math.rad(100.021978))
+		Turn(torso, y_axis, 0, math.rad(100))
 		Turn(torso, x_axis, 0, math.rad(20))
 	end
 end
@@ -203,17 +167,15 @@ local function RestoreAfterDelay()
 end
 
 function script.Create()
-	Hide(rfire1)
-	Hide(lfire1)
 	StartThread(GG.Script.SmokeUnit, unitID, {torso})
-    Turn(ltoef1, y_axis, math.rad(45))
-    Turn(ltoef2, y_axis, math.rad(-45))
-    Turn(rtoef1, y_axis, math.rad(45))
-    Turn(rtoef2, y_axis, math.rad(-45))
+	Turn(ltoef1, y_axis, math.rad(45))
+	Turn(ltoef2, y_axis, math.rad(-45))
+	Turn(rtoef1, y_axis, math.rad(45))
+	Turn(rtoef2, y_axis, math.rad(-45))
 end
 
 function script.StartMoving()
-    StartThread(Walk)
+	StartThread(Walk)
 end
 
 function script.StopMoving()
@@ -261,9 +223,7 @@ function script.AimWeapon(num, heading, pitch)
 end
 
 function script.Killed(recentDamage, maxHealth)
-    local severity = recentDamage/maxHealth
-	Hide(rfire1)
-	Hide(lfire1)
+	local severity = recentDamage/maxHealth
 	if severity <= 0.25 then
 		Explode(lfoot, SFX.SMOKE + SFX.EXPLODE)
 		Explode(lleg, SFX.SMOKE + SFX.EXPLODE)
@@ -287,13 +247,13 @@ function script.Killed(recentDamage, maxHealth)
 		Explode(rleg, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
 		Explode(torso, SFX.SHATTER + SFX.EXPLODE)
 		return 2
-	else
-	    Explode(lfoot, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
-	    Explode(lleg, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
-        Explode(pelvis, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
-	    Explode(rfoot, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
-	    Explode(rleg, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
-	    Explode(torso, SFX.SHATTER + SFX.EXPLODE)
-        return 2
-    end
+	end
+	
+	Explode(lfoot, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
+	Explode(lleg, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
+	Explode(pelvis, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
+	Explode(rfoot, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
+	Explode(rleg, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
+	Explode(torso, SFX.SHATTER + SFX.EXPLODE)
+	return 2
 end
