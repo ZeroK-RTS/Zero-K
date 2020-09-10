b00893b28d4a61dbefecaa4a21299b79d63d2900
Shark Mobile Workstation <owensilva1282@gmail.com>
2020-09-09 23:19:44 -0400

    Remove reaim time
    Remove reaim time parameter

--- units/striderarty.lua
+++ units/striderarty.lua
@@ -73,7 +73,6 @@ return { striderarty = {
       craterMult              = 2,
       
       customParams              = {
-        reaim_time = 8, -- COB
         light_camera_height = 2500,
         light_color = [[0.35 0.17 0.04]],
         light_radius = 400,
