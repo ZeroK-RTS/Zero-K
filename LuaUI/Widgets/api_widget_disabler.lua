bcc53177c7ad5de5f7c83b9982f0ce1f1fcf7923
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2020-09-12 06:19:16 -0400

    Improved widget disabler api
    Added the ability to tell users a message in event of user created widgets being disabled.

--- LuaUI/Widgets/api_widget_disabler.lua
+++ LuaUI/Widgets/api_widget_disabler.lua
@@ -16,30 +16,56 @@ end
 The table below here controls what widgets are disabled. To add new widgets to it, simply add the line [2] = "widget human name"
 (NOT the file name! It's the string in the name field of the widget!).
 When the widget is loaded, it sees if widgets included in the table are loaded, and disables them if they are.
+The widget then, if necessary, waits to deliver a list of disabled widgets to users.
 After this is completed, the widget removes itself.
-Please also include the reason in the reason table.]]
+Please also include the reason in the table itself under the key 'reason'.
+If the type of widget you want to disable is a user made widget, be sure to make the type 'user' so a message is generated for the user.
+Otherwise, make type equal to 'map' to put the reason in the infolog instead.]]
 
 -- Speed ups --
 local spEcho = Spring.Echo
 
+-- variables --
+local msg = "The following widgets have been disabled:\n" -- message to be displayed on game start.
+local userContentDisabled = false
 -- config --
 local badwidgets = {
-	[1] = "*Mearth Location Tags*1.0", -- Map: Mearth_v4.
-	[2] = "Metalspot Finder (map)", -- oktogon v3
-}
+	[1] = {
+		name = "*Mearth Location Tags*1.0", 
+		reason = "Causes black ground on some graphics cards, possible copyright issues.",
+		type = 'map', -- Map: Mearth_v4.
+	},
+	[2] = {
+		name = "Metalspot Finder (map)",
+		reason = "Breaks everything mex-related.", 
+		type = 'map', -- oktogon v3
+	},
 
-local reason = {
-	[1] = "Causes black ground on some graphics cards, possible copyright issues.",
-	[2] = "breaks everything mex-related",
 }
 
 -- callins --
 function widget:Initialize()
 	for i=1, #badwidgets do
-		if widgetHandler:IsWidgetKnown(badwidgets[i]) then -- If this widget is loaded, unload it and echo a reason.
-			spEcho("Disabled '" .. badwidgets[i] .. "' (Reason: " .. tostring(reason[i]) .. ")")
-			widgetHandler:DisableWidget(badwidgets[i])
+		local widget = badwidgets[i]
+		if widgetHandler:IsWidgetKnown(widget.name) then -- If this widget is loaded, unload it and echo a reason.
+			if widget.type == 'user' then
+				userContentDisabled = true
+				msg = msg .. "\n" .. widget.name .. " (Reason: " .. tostring(widget.reason) .. ")" -- users should be aware of why their local widgets are disabled.
+			else
+				spEcho("Disabled '" .. widget.name .. "' (Reason: " .. tostring(widget.reason) .. ")") -- users don't need this info. Map makers can check infolog.
+			end
+			widgetHandler:DisableWidget(widget.name)
 		end
 	end
-	widgetHandler:RemoveWidget(widget)
+	if spGetGameFrame() > -1 and userContentDisabled then -- in case of reload
+		spEcho("game_message: " .. msg)
+		widgetHandler:RemoveWidget(widget)
+	end
+end
+
+function widget:GameStart()
+	if userContentDisabled then
+		spEcho("game_message: " .. msg)
+		widgetHandler:RemoveWidget(widget)
+	end
 end
