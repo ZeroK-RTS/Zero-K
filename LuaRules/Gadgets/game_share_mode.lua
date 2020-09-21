49f4e81d7bea62121a5d5e9f29d113d010ff4945
_Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2020-09-21 15:20:05 -0400

    New feature: Hivemind names
    When merge=all is on, team names get overridden with a special hive mind name for teams with > 1 players with respect to clan names.
    
    Also makes the ally leader to the highest elo player instead of lowest playerID.

--- LuaRules/Gadgets/game_share_mode.lua
+++ LuaRules/Gadgets/game_share_mode.lua
@@ -16,6 +16,7 @@ if not gadgetHandler:IsSyncedCode() then
 	function gadget:PlayerChanged(playerID)
 		spSendLuaRulesMsg("sharemode playerchanged " .. playerID) -- tell synced land that I changed status.
 	end
+	return -- unsyncedland doesn't need anything else here.
 end
 
 --------------------------------------------------------
@@ -61,7 +62,7 @@ local function GetConfig()
 	return {
 		mergeEnabled = modOptions.sharemode ~= "none",
 		permanentMerge = modOptions.sharemode == "all",
-		mintime   = 5, -- GET RID OF ME EVENTUALLY!
+		mintime = 5, -- Needed to prevent commshare==all from fucking over comm spawns. Remove me once Start Position Rework gets merged.
 	}
 end
 local config = GetConfig()
@@ -69,6 +70,52 @@ local config = GetConfig()
 if not config.mergeEnabled then
 	spEcho("[Commshare] Commshare is off. Shutting down.")
 	return
+end
+
+local Names
+
+if config.permanentMerge then -- only add these if we're on hivemind mode. These give all the 'hiveminds' special names. Mostly a fun little touch to the game mode.
+	--Note: to add a name template, add a new entry to both tables here. Use <leader> to add a clan or player name to the name.
+	--TODO: Translation.
+	Names = {
+		long = {
+			[1] = 'The <leader>', -- EX: The Shaman or The ADVENT
+			[2] = 'The <leader> Collective',
+			[3] = 'The <leader> Transconsciousness',
+			[4] = 'The <leader> Swarm',
+			[5] = 'The <leader> Hivemind',
+			[6] = 'Hyron Core <leader>',
+			[7] = 'The <leader> Core',
+			[8] = 'The <leader> Network',
+			[9] = '<leader> Prime Consciousness',
+			[10] = 'The <leader> Cult',
+		},
+		short = {
+			[1] = '<leader>',
+			[2] = '<leader> Collective',
+			[3] = '<leader> Transconsciousness',
+			[4] = '<leader> Swarm',
+			[5] = '<leader> Hive',
+			[6] = 'Hyron Core <leader>',
+			[7] = '<leader> Core',
+			[8] = '<leader> Network',
+			[9] = '<leader> Prime',
+			[10] = '<leader> Cult',
+		},
+		templates = 0,
+		easteregg = false, -- Have we set the easteregg yet? (Set to true to remove it entirely)
+	}
+	Names.templates = math.min(#Names.long, #Names.short) -- tell name setter how many templates we have to choose from
+	-- self check --
+	if #Names.long > Names.templates then
+		for i=Names.templates, #Names.long do
+			spEcho("[Commshare] Dropped template '" .. Names.long[i] .. "' (ID: " .. i .. ") due to missing Short name.")
+		end
+	elseif #Names.short > Names.templates then
+		for i=Names.templates, #Names.short do
+			spEcho("[Commshare] Dropped short template '" .. Names.short[i] .. "' (ID: " .. i .. ") due to missing Long name.")
+		end
+	end
 end
 
 --------------------------------------------------------
@@ -80,8 +127,6 @@ local invites = {} -- the invites people have out.
 local originalUnits = {} -- contains which units are owned by a team that has commshared.
 local updateplayers = {}
 local updateplayercount = 0
-
-local firstMintime = true -- Partial initialize in gameframe after the game starts
 
 --------------------------------------------------------
 -- Utilities
@@ -243,31 +288,114 @@ local function MergeTeams(team1,team2) -- bandaid for an issue during planning.
 	end
 end
 
-local function MergeAllHumans(teamlist)
-	local mergeid = -1
-	for i = 1, #teamlist do
-		local _, teamLeader, _, AI = spGetTeamInfo(teamlist[i], false)
-		local human = not AI and teamLeader ~= -1
-		if human and mergeid ~= -1 then
-			if debugMode then spEcho("[Commshare] Merging team " .. teamlist[i]) end
-			-- Needed because of recursion error. Only one player on a team at game start anyways.
-			MergePlayer(teamLeader,mergeid)
-		elseif human and mergeid == -1 then
-			mergeid = teamlist[i]
-			if debugMode then spEcho("[Commshare] MergeID is " .. mergeid) end
-		else
-			if debugMode then spEcho("[Commshare] Skipping team " .. i .. " [inhuman]") end
+local function GetAllyTeamLeader(teamlist)
+	local highestElo = -999999
+	local highestEloID = -1
+	local clanSupremecy = false
+	local mergeID = -1
+	local leadername = ""
+	local players = #teamlist
+	local clanthreshhold = math.ceil(players/2) + 1
+	local clans = {}
+	for i=1, #teamlist do
+		local teamID = teamlist[i]
+		local _, _, _, AI = spGetTeamInfo(teamlist[i], false)
+		if not AI then
+			local playerlist = spGetPlayerList(teamlist[i]) -- usually this will be one.
+			for p=1, #playerlist do
+				local playerID = playerlist[p]
+				local cp = select(10,spGetPlayerInfo(playerID, false))
+				local elo = cp.elo
+				local clan = cp.clan or ""
+				if debugMode then spEcho("[Commshare] Stats for " .. spGetPlayerInfo(playerID, false) .. ":\nElo: " .. tostring(elo) .. "\nClan: " .. tostring(clan)) end
+				if elo ~= nil then
+					elo = tonumber(elo)
+				else
+					elo = -999999
+				end
+				if debugMode then spEcho("[Commshare] Player " .. playerID .. "'s Clan: " .. clan) end
+				if elo > highestElo then
+					if debugMode then spEcho("[Commshare] Highest Elo for team is now " .. elo) end
+					highestElo = elo
+					highestEloID = playerID
+					if not clanSupremecy then
+						mergeID = teamID
+					end
+				end
+				if clan ~= "" then
+					if clans[clan] == nil then
+						clans[clan] = {players = 1, highestelo = elo, highestid = teamID}
+					else
+						clans[clan].players = clans[clan].players + 1
+						if elo > clans[clan].highestelo then
+							clans[clan].highestelo = elo
+							clans[clan].highestid = teamID
+							if debugMode then spEcho("[Commshare] Highest Elo for clan " .. clan .. " is now " .. elo) end
+						end
+						if clans[clan].players > clanthreshhold then
+							if debugMode then spEcho("[Commshare] Clan supremecy detected for " .. clan .. "!") end
+							leadername = clan
+							mergeID = clans[clan].highestid
+							clanSupremecy = true
+						end
+					end
+				end
+			end
 		end
 	end
+	if mergeID == -1 then
+		return "", mergeID
+	end
+	if not clanSupremecy then -- fallback to using the highest elo player.
+		leadername = spGetPlayerInfo(highestEloID)
+	end
+	if debugMode then spEcho("[Commshare] mergeID: " .. mergeID .. ", leadername: " .. leadername) end
+	return leadername,mergeID
+end
+
+local function MergeAllHumans(teamlist,allyID)
+	local leaderName,mergeid = GetAllyTeamLeader(teamlist)
+	if mergeid == -1 then -- this is a team of AIs
+		if debugMode then spEcho("[Commshare] AI allyteam detected. Skipping.") end
+		return
+	end
+	if debugMode then spEcho("[Commshare] MergeID: " .. mergeid) end
+	for i = 1, #teamlist do
+		local teamID = teamlist[i]
+		local _, _, _, AI = spGetTeamInfo(teamlist[i], false)
+		if not AI and teamID ~= mergeid then
+			if debugMode then spEcho("[Commshare] Merging team " .. teamlist[i]) end
+			MergeTeams(teamID,mergeid)
+		else
+			if debugMode then spEcho("[Commshare] Skipping team " .. i .. " [inhuman/mergeID]") end
+		end
+	end
+	if debugMode then spEcho("[Commshare] Setting up name for team.") end
+	local templateID = math.random(1, Names.templates)
+	if debugMode then spEcho("[Commshare] Template ID: " .. templateID) end
+	local longName = Names.long[templateID]
+	local shortName = Names.short[templateID]
+	local easteregg = Names.easteregg
+	longName = longName:gsub("<leader>", leaderName)
+	shortName = shortName:gsub("<leader>", leaderName)
+	if not easteregg and math.random(1,1000) == 515 then -- Kshatriya's silly name. Has a 1.49% chance of showing up in 16 way ffas and a .002% chance of showing up in team games.
+		longName = 'Buzzy Buzzy Bumblebees'
+		shortName = 'THE BEES!!'
+		Names.easteregg = true
+	end
+	if debugMode then spEcho("[Commshare] Setting allyteam " .. allyID .. "'s name to: " .. longName .. " ( " .. shortName .. " )") end
+	Spring.SetGameRulesParam("allyteam_short_name_" .. allyID, shortName) -- override any default names with special merge mode names.
+	Spring.SetGameRulesParam("allyteam_long_name_"  .. allyID, longName)
 end
 
 local function MergeAll()
 	local ally = spGetAllyTeamList()
 	for i = 1, #ally do
-		local teamlist = spGetTeamList(ally[i])
+		local allyID = ally[i]
+		local teamlist = spGetTeamList(allyID)
 		if #teamlist > 1 then
 			if debugMode then spEcho("[Commshare] Merging alliance " .. i) end
-			MergeAllHumans(teamlist)
+			MergeAllHumans(teamlist,allyID)
 		end
 	end
 end
@@ -423,9 +551,8 @@ function gadget:GameFrame(frame)
 			end
 		end
 	end
-	if firstMintime and frame >= config.mintime and config.permanentMerge then
+	if frame == config.mintime and config.permanentMerge then
 		MergeAll()
-		firstMintime = false
 	end
 	if updateplayercount > 0 and not spIsGameOver() then -- this is prevent excessive processing on game over.
 		local player
@@ -489,7 +616,7 @@ function gadget:RecvLuaMsg(message, playerID) -- Entry points for widgets to int
 		local targetName, targetActive, targetSpectator, targetTeamID = spGetPlayerInfo(targetID, false)
 		if debugMode then spEcho("[Commshare] Playerchanged: " .. targetID .. "( " .. targetName .. ")") end
 		local commshareID = spGetPlayerRulesParam(targetID, "commshare_team_id")
-		if debugMode then spEcho("[Commshare] playerstates exists for player: " .. tostring(playerstates[targetID] == nil) .. "\nSpectator: " .. tostring(targetSpectator))) end
+		if debugMode then spEcho("[Commshare] playerstates exists for player: " .. tostring(playerstates[targetID] == nil) .. "\nSpectator: " .. tostring(targetSpectator)) end
 		if not targetSpectator then
 			if not playerstates[targetID] then -- this player has commshared or changed state.
 				if debugMode then spEcho("[Commshare] generated playerstate table for playerID " .. targetID .. " (" .. targetName .. ")") end
