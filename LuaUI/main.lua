9c301a1323f5d96420d8360a770ba7df5f169956
_Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2020-09-22 17:23:54 -0400

    More styling guide enforcement
    opps. Missed some.

--- LuaUI/main.lua
+++ LuaUI/main.lua
@@ -2,11 +2,15 @@ local allModOptions = Spring.GetModOptions()
 function Spring.GetModOption(s,bool,default)
 	if (bool) then
 		local modOption = allModOptions[s]
-		if (modOption == nil) then modOption = (default and "1") end
+		if (modOption == nil) then
+			modOption = (default and "1")
+		end
 		return (modOption == "1")
 	else
 		local modOption = allModOptions[s]
-		if (modOption == nil) then modOption = default end
+		if (modOption == nil) then
+			modOption = default
+		end
 		return modOption
 	end
 end
