5ed1235377dfe745542c2760d82a02bd9ebcab76
sprunk <gnurps@o2.pl>
2020-10-07 16:28:39 +0200

    Fixup 2

--- LuaUI/Widgets/gui_chili_integral_menu.lua
+++ LuaUI/Widgets/gui_chili_integral_menu.lua
@@ -193,7 +193,7 @@ options = {
 			{name = 'QWERTY (standard)',key = 'qwerty', hotkey = nil},
 			{name = 'QWERTZ (central Europe)', key = 'qwertz', hotkey = nil},
 			{name = 'AZERTY (France)', key = 'azerty', hotkey = nil},
-			{name = 'DVORAK (standard)', key = 'dvorak', hotkey = nil},
+			{name = 'Dvorak (standard)', key = 'dvorak', hotkey = nil},
 			{name = 'Configure in "Custom" (below)', key = 'custom', hotkey = nil},
 			{name = 'Disable Grid Keys', key = 'none', hotkey = nil},
 		},
