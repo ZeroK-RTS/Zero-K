06caaa440765661844126fdc6418184f8622ce69
GoogleFrog <googl3frog@gmail.com>
2020-10-23 09:34:24 +1100

    Fix repair pad.

--- Objects3d/staticrearm.dae.lua
+++ Objects3d/staticrearm.dae.lua
@@ -1,7 +1,7 @@
 return {
-	height = 85.0,
-	midpos = {0,35.5,0},
-	radius = 36,
+	height = 65.0,
+	midpos = {0,5,0},
+	radius = 90,
 	tex1 = "core_color.dds",
 	tex2 = "core_other.dds",
 	invertteamcolor = false
