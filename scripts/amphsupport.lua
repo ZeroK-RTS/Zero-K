05c26ff0711e73787fc3f3d508334a36eb7edada
GoogleFrog <googl3frog@gmail.com>
2020-11-01 19:25:31 +1100

    Wired up amphsupport firing.

--- scripts/amphsupport.lua
+++ scripts/amphsupport.lua
@@ -1,7 +1,9 @@
 include 'constants.lua'
 
 local base = piece 'base'
+local aimfrom = piece 'aimfrom'
 local pelvis = piece 'pelvis'
+local head_gimbal = piece 'head_gimbal'
 local head = piece 'head'
 
 local rthigh = piece 'rthigh'
@@ -39,11 +41,15 @@ local SIG_WALK = 1
 local SIG_AIM = 2
 local SIG_DEPLOY = 4
 
+local GUN_DEPLOY_DIST = 6
+local AIM_SPEED = 180
+
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 local moving = false
 local deployed = false
+local gun = false
 
 local PACE = 1.8
 
@@ -164,8 +170,8 @@ local function AnimateDeployment(distance, speed, wait)
 		WaitForTurn(lcalf, x_axis)
 	end
 	
-	Move(lbarrel, z_axis, 8*distance, 12*speed)
-	Move(rbarrel, z_axis, 8*distance, 12*speed)
+	Move(lbarrel, z_axis, GUN_DEPLOY_DIST*distance, 12*speed)
+	Move(rbarrel, z_axis, GUN_DEPLOY_DIST*distance, 12*speed)
 	
 	Turn(rthigh, y_axis, math.rad(-35)*distance, math.rad(65)*speed)
 	Turn(rcalf, x_axis, math.rad(12)*distance, math.rad(40)*speed)
@@ -178,7 +184,7 @@ local function AnimateDeployment(distance, speed, wait)
 	Turn(lfoot, z_axis, math.rad(5)*distance, math.rad(10)*speed)
 	
 	if not wait then
-		Turn(cthigh, x_axis, math.rad(-118)*distance, math.rad(180)*speed)
+		Turn(cthigh, x_axis, math.rad(-118)*distance, math.rad(80)*speed)
 		Turn(ccalf, x_axis, math.rad(0)*distance, math.rad(80)*speed)
 		Turn(cfoot, x_axis, math.rad(-25)*distance, math.rad(50)*speed)
 	end
@@ -195,8 +201,14 @@ local function SetDeploy(wantDeploy)
 		AnimateDeployment(1, 1, true)
 		deployed = true
 	else
-		AnimateDeployment(0, 1.2, false)
 		deployed = false
+		Turn(head, y_axis, 0, math.rad(AIM_SPEED))
+		Turn(laxel, x_axis, math.rad(-10), math.rad(AIM_SPEED*1.5))
+		Turn(raxel, x_axis, math.rad(-10), math.rad(AIM_SPEED*1.5))
+		--WaitForTurn(head, y_axis)
+		--WaitForTurn(head, laxel)
+
+		AnimateDeployment(0, 1.2, false)
 	end
 end
 
@@ -229,18 +241,85 @@ local function CheckMoving()
 end
 
 function script.Create()
+	Turn(head_gimbal, x_axis, math.rad(10))
+	Turn(raxel, x_axis, math.rad(-10))
+	Turn(laxel, x_axis, math.rad(-10))
 	moving = false
 	StartThread(SetDeploy, true)
 	StartThread(CheckMoving)
 	StartThread(GG.Script.SmokeUnit, unitID, smokePiece)
 end
 
-function script.Activate()
- StartThread(AutoAttack_Thread)
+local function RestoreAfterDelay()
+	Sleep(2750)
+	Turn(head, y_axis, 0, math.rad(AIM_SPEED*0.25))
+	WaitForTurn(head, y_axis)
+	Turn(laxel, x_axis, math.rad(-10), math.rad(AIM_SPEED*0.37))
+	Turn(raxel, x_axis, math.rad(-10), math.rad(AIM_SPEED*0.37))
 end
 
-function script.Deactivate()
-	Signal(SIG_ACTIVATE)
+function script.QueryWeapon(num)
+	if gun then
+		return lflare
+	else
+		return rflare
+	end
+end
+
+function script.AimFromWeapon(num)
+	return aimfrom
+end
+
+function script.AimWeapon(num, heading, pitch)
+	if num == 1 then
+		Signal(SIG_AIM)
+		SetSignalMask(SIG_AIM)
+		
+		if moving or not deployed then
+			return false
+		else
+			Turn(head, y_axis, heading, math.rad(AIM_SPEED)) -- left-right
+			Turn(laxel, x_axis, -pitch, math.rad(AIM_SPEED*1.5)) --up-down
+			Turn(raxel, x_axis, -pitch, math.rad(AIM_SPEED*1.5)) --up-down
+			WaitForTurn(head, y_axis)
+			WaitForTurn(laxel, x_axis)
+			StartThread(RestoreAfterDelay)
+			return true
+		end
+	elseif num == 2 then
+		--GG.Floating_AimWeapon(unitID)
+		return false
+	end
+end
+
+local function recoil()
+	if gun then
+		EmitSfx(lflare, 1024)
+		EmitSfx(lflare, 1025)
+		Move(lbarrel, z_axis, -2)
+		Move(lbarrel, z_axis, GUN_DEPLOY_DIST, 3)
+	else
+		EmitSfx(rflare, 1024)
+		EmitSfx(rflare, 1025)
+		Move(rbarrel, z_axis, -2)
+		Move(rbarrel, z_axis, GUN_DEPLOY_DIST, 3)
+	end
+end
+
+function script.FireWeapon(num)
+	StartThread(recoil)
+end
+
+function script.BlockShot(num, targetID)
+	if Spring.ValidUnitID(targetID) then
+		local distMult = (Spring.GetUnitSeparation(unitID, targetID) or 0)/350
+		return GG.OverkillPrevention_CheckBlock(unitID, targetID, 141.1, 30 * distMult, false, false, true)
+	end
+	return false
+end
+
+function script.EndBurst()
+	gun = not gun
 end
 
 function script.Killed(recentDamage, maxHealth)
--- units/amphsupport.lua
+++ units/amphsupport.lua
@@ -42,12 +42,17 @@ return { amphsupport = {
   selfDestructAs         = [[BIG_UNITEX]],
 
   sfxtypes               = {
+
     explosiongenerators = {
+      [[custom:RAIDMUZZLE]],
+      [[custom:RAIDDUST]],
+      [[custom:THUDDUST]],
     },
+
   },
 
-  sightDistance          = 500,
-  sonarDistance          = 500,
+  sightDistance          = 660,
+  sonarDistance          = 660,
   trackOffset            = 0,
   trackStrength          = 8,
   trackStretch           = 1,
@@ -72,46 +77,39 @@ return { amphsupport = {
   weaponDefs             = {
 
     CANNON = {
-      name                    = [[Disruption Cannon]],
-      accuracy                = 200,
-      areaOfEffect            = 32,
-      cegTag                  = [[beamweapon_muzzle_purple]],
-      craterBoost             = 1,
-      craterMult              = 2,
+      name                    = [[Light Plasma Cannon]],
+      accuracy                = 220,
+      areaOfEffect            = 16,
+      craterBoost             = 0,
+      craterMult              = 0,
 
-      customparams = {
-        burst = Shared.BURST_RELIABLE,
-
-        timeslow_damagefactor = 1.667,
-        
-        light_camera_height = 2500,
-        light_color = [[1.36 0.68 1.5]],
-        light_radius = 180,
+      customParams        = {
+        light_camera_height = 1400,
+        light_color = [[0.80 0.54 0.23]],
+        light_radius = 200,
       },
 
       damage                  = {
         default = 150.1,
+        planes  = 150.1,
         subs    = 7.5,
       },
 
-      explosionGenerator      = [[custom:flashslowwithsparks]],
-      fireStarter             = 180,
+      edgeEffectiveness       = 0.1,
+      explosionGenerator      = [[custom:MARY_SUE]],
       impactOnly              = true,
       impulseBoost            = 0,
-      impulseFactor           = 0.2,
-      interceptedByShieldType = 2,
-      myGravity               = 0.2,
-      range                   = 450,
-      reloadtime              = 1.8,
-      rgbcolor                = [[0.9 0.1 0.9]],
-      soundHit                = [[weapon/laser/small_laser_fire]],
-      soundHitVolume          = 2.2,
-      soundStart              = [[weapon/laser/small_laser_fire3]],
-      soundStartVolume        = 3.5,
-      soundTrigger            = true,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      myGravity               = 0.09,
+      noSelfDamage            = true,
+      range                   = 600,
+      reloadtime              = 6,
+      soundHit                = [[explosion/ex_med5]],
+      soundStart              = [[weapon/cannon/cannon_fire1]],
       turret                  = true,
       weaponType              = [[Cannon]],
-      weaponVelocity          = 340,
+      weaponVelocity          = 270,
     },
 
     FAKE_CANNON = {
