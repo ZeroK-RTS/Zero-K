52a834076a88d2e398e6cd2150edfb98deb68220
sprunk <spr.ng@o2.pl>
2020-11-08 21:22:04 +0100

    Less repetition for chicken drone starter

--- units/chicken_drone_starter.lua
+++ units/chicken_drone_starter.lua
@@ -1,67 +1,7 @@
-return { chicken_drone_starter = {
-  unitname            = [[chicken_drone_starter]],
-  name                = [[Drone]],
-  description         = [[Morphs Into Chicken Structures]],
-  acceleration        = 1.08,
-  activateWhenBuilt   = true,
-  brakeRate           = 1.23,
-  buildCostEnergy     = 0,
-  buildCostMetal      = 0,
-  builder             = false,
-  buildPic            = [[chicken_drone_starter.png]],
-  buildTime           = 60,
-  canGuard            = true,
-  canMove             = true,
-  canPatrol           = true,
-  category            = [[LAND UNARMED]],
+local unitDef = VFS.Include("units/chicken_drone.lua").chicken_drone
 
-  customParams        = {
-    statsname = "chicken_drone",
-  },
+unitDef.unitname = "chicken_drone_starter"
+unitDef.buildPic = "chicken_drone_starter.png"
+unitDef.customParams.statsname = "chicken_drone"
 
-  energyMake          = 0,
-  energyStorage       = 50,
-  explodeAs           = [[SMALL_UNITEX]],
-  floater             = false,
-  footprintX          = 2,
-  footprintZ          = 2,
-  iconType            = [[builder]],
-  idleAutoHeal        = 20,
-  idleTime            = 300,
-  leaveTracks         = true,
-  maxDamage           = 170,
-  maxSlope            = 36,
-  maxVelocity         = 1.8,
-  maxWaterDepth       = 5000,
-  metalMake           = 0,
-  metalStorage        = 50,
-  minCloakDistance    = 75,
-  movementClass       = [[AKBOT2]],
-  noAutoFire          = false,
-  noChaseCategory     = [[TERRAFORM SATELLITE FIXEDWING GUNSHIP HOVER SHIP SWIM SUB LAND FLOAT SINK TURRET]],
-  objectName          = [[chicken_drone.s3o]],
-  onoffable           = true,
-  power               = 60,
-  reclaimable         = false,
-  selfDestructAs      = [[SMALL_UNITEX]],
-
-  sfxtypes            = {
-
-    explosiongenerators = {
-      [[custom:blood_spray]],
-      [[custom:blood_explode]],
-      [[custom:dirt]],
-    },
-
-  },
-  sightDistance       = 256,
-  trackOffset         = 1,
-  trackStrength       = 6,
-  trackStretch        = 1,
-  trackType           = [[ChickenTrack]],
-  trackWidth          = 10,
-  turnRate            = 967,
-  upright             = false,
-  waterline           = 8,
-  workerTime          = 0,
-} }
+return { chicken_drone_starter = unitDef }
