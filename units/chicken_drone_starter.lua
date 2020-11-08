b2336a64347abaeca6976a8c348afe438f2d3a18
sprunk <spr.ng@o2.pl>
2020-11-08 21:23:22 +0100

    Actually, remove the starter, it doesn't even work

--- units/chicken_drone_starter.lua
+++
@@ -1,7 +0,0 @@
-local unitDef = VFS.Include("units/chicken_drone.lua").chicken_drone
-
-unitDef.unitname = "chicken_drone_starter"
-unitDef.buildPic = "chicken_drone_starter.png"
-unitDef.customParams.statsname = "chicken_drone"
-
-return { chicken_drone_starter = unitDef }
