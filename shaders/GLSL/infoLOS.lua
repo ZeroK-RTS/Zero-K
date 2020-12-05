d1425194416559bf0dcd8f1406e886ecf29d69a5
Luke Hubmayer-Werner <mongoose41@gmail.com>
2020-12-05 18:43:17 +1030

    Make radarColor2 not add to losColor. Enables players to have a two-tone fog of war setup where radar coverage shows as lighter fog, without radar overbrightening areas in LOS.

--- shaders/GLSL/infoLOS.lua
+++ shaders/GLSL/infoLOS.lua
@@ -49,19 +49,20 @@ return {
 		#define getTexel texture2D
 	#endif
 		void main() {
-			gl_FragColor  = vec4(0.0);
-			//gl_FragColor  = alwaysColor;
-			vec2 radarJammer = getTexel(tex2, texCoord).rg;
-			gl_FragColor += jamColor * radarJammer.g;
-			gl_FragColor += radarColor2 * step(0.8, radarJammer.r) * radarJammer.r;
-			gl_FragColor.rgb = fract(gl_FragColor.rgb);
-			
+			vec2 radarJammer = getTexel(tex2, texCoord).rg;  // r = radar, g = jamming
 			float los = getTexel(tex0, texCoord).r;
 			float airlos = getTexel(tex1, texCoord).r;
+
+			gl_FragColor  = vec4(0.0);
+			//gl_FragColor  = alwaysColor;
+			gl_FragColor += jamColor * radarJammer.g;
+			gl_FragColor += radarColor2 * step(0.8, radarJammer.r) * radarJammer.r  * (1.0-floor(los));
+			gl_FragColor.rgb = fract(gl_FragColor.rgb);
+
 			gl_FragColor += losColor * (los * 0.9 + airlos * 0.1);
-			
+
 			gl_FragColor += radarColor * step(0.2, fract(1 - radarJammer.r));
-			
+
 			gl_FragColor += alwaysColor;
 			gl_FragColor.a = 0.05;
 		}
