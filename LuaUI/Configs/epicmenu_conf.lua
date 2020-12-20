e9107bd14e356795571fcece80700843121af675
GoogleFrog <googl3frog@gmail.com>
2020-12-20 23:01:25 +1100

    Added a button to toggle attrition counter.

--- LuaUI/Configs/epicmenu_conf.lua
+++ LuaUI/Configs/epicmenu_conf.lua
@@ -371,6 +371,7 @@ ShLabel('Hotkeys/Commands', 'Command Categories')
 --- HUD Panels --- Only settings that pertain to windows/icons at the drawscreen level should go here.
 local HUDPath = 'Settings/HUD Panels/Extras'
 	ShButton(HUDPath, 'Tweak Mode (Esc to exit)', 'luaui tweakgui', 'Tweak Mode. Move and resize parts of the user interface. (Hit Esc to exit)')
+	ShButton(HUDPath, 'Toggle Attrition Counter', function() spSendCommands{"luaui togglewidget Attrition Counter"} end, "Tracks killed and lost units (only in line of sight while playing)")
 
 local HUDSkinPath = 'Settings/HUD Panels/Extras/HUD Skin'
 	AddOption(HUDSkinPath,
