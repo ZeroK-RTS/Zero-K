5661298a6cd23b206a1e211414703d64a85995cd
esainane <esainane+github@gmail.com>
2021-01-07 15:16:13 +1300

    cmd_state_reverse_toggle: Inline VFSMODE

--- LuaUI/Widgets/cmd_state_reverse_toggle.lua
+++ LuaUI/Widgets/cmd_state_reverse_toggle.lua
@@ -34,8 +34,8 @@ local STANDARD_OPTS = {
 --------------------------------------------------------------------------------
 -- Configuration
 
-local VFSMODE = VFS.RAW_FIRST -- so that users' own commands can also have reversing
-local stateTypes, gadgetReverse, specialHandling = VFS.Include(LUAUI_DIRNAME .. "Configs/stateTypes.lua", nil, VFSMODE)
+-- so that users' own commands can also have reversing
+local stateTypes, gadgetReverse, specialHandling = VFS.Include(LUAUI_DIRNAME .. "Configs/stateTypes.lua", nil, VFS.RAW_FIRST)
 
 local PING_UPDATE_RATE = 0.5
 local PING_MEMORY = 4
