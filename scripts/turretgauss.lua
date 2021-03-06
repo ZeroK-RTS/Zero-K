ed232d9b3bc74b402869554050b508766f4479d8
SteelBlueZK <sbluefaf@gmail.com>
2021-03-06 18:26:41 -0500

    Gauss Animation update. can shoot down cliffs.

--- scripts/turretgauss.lua
+++ scripts/turretgauss.lua
@@ -24,93 +24,56 @@ local spGetUnitIsStunned = Spring.GetUnitIsStunned
 local spGetUnitHealth = Spring.GetUnitHealth
 local spSetUnitHealth = Spring.SetUnitHealth
 
-local unpackSpeed = 5;
-
 local smokePiece = { lidLeft, lidRight, wheel}
 
 --variables
+local unpackSpeed = math.rad(286);
 local weaponRange = WeaponDefNames["turretgauss_gauss"].range
+local isClosed, aimHelp = false, false--used for aim
 local rangeChanged = false
-local is_open = false
 local restore_delay = 2000;
-
 local BUNKERED_AUTOHEAL = tonumber (UnitDef.customParams.armored_regen or 20) / 2 -- applied every 0.5s
 
 --signals
-local aim = 2
-local open = 8
-local close = 16
+local SIGanimation = 1
+local SIGrestore = 2
 
 -- private functions
 
-local function Open()
-	
-	Spring.SetUnitArmored(unitID,false);
-	Signal(aim + open + close)
-	SetSignalMask(open)
-	
-	Turn(lidLeft, y_axis, math.rad(0), unpackSpeed);
-	Turn(lidRight, y_axis, math.rad(0), unpackSpeed);
-	
-	WaitForTurn(lidLeft, y_axis);
-	
-	Turn(wheel, x_axis, math.rad(-15),unpackSpeed);
-	
-	WaitForTurn(wheel, x_axis);
-	
-	Turn(hand, x_axis, math.rad(20), unpackSpeed);
-	Turn(arm,x_axis,math.rad(20), unpackSpeed);
-	Turn(wheel, x_axis, math.rad(-30), unpackSpeed);
-	Turn(cannon, x_axis, math.rad(-10), unpackSpeed);
-	
-	WaitForTurn(cannon, x_axis);
-	WaitForTurn(wheel, x_axis);
-	
-	Move(barrel1,y_axis,0,6);
-	Move(barrel2,y_axis,0,6);
-	Move(barrel3,y_axis,0,6);
-	
-	is_open = true
-end
-
-local function AimBlink()
-	while true do
-		EmitSfx(aimProxy, 1024)
-		Sleep(200)
-	end
-end
-
---closing animation of the factory
+-- Closing animation, contains the armored state.
 local function Close()
-	Signal(aim + open + close)
-	SetSignalMask(close)
-	is_open = false;
-	
-	Move(barrel1,y_axis,1,2);
-	Move(barrel2,y_axis,1,2);
-	Move(barrel3,y_axis,1,2);
-	
-	Turn(wheel, x_axis, math.rad(-15),math.rad(90));
-	Turn(cannon, x_axis, math.rad(65),math.rad(90));
-	
-	
-	
-	Turn(hand, x_axis, math.rad(60),math.rad(90));
-	Turn(arm,x_axis,math.rad(107),math.rad(90));
-	
-	WaitForTurn(arm,x_axis);
-	
-	Turn(wheel, x_axis, math.rad(55),math.rad(160));
-	
-	WaitForTurn(wheel, x_axis);
-	
-	Turn(wheel, x_axis, math.rad(65),math.rad(20));
-	
-	Turn(lidLeft, y_axis, math.rad(-90), math.rad(180));
-	Turn(lidRight, y_axis, math.rad(90), math.rad(180));
+	Signal(SIGanimation)
+	SetSignalMask(SIGanimation)
+
+	aimHelp = true
+	Spin(aimProxy, z_axis, math.rad(270))
+
+	if not isClosed then
+		--animation
+		Move(barrel1,y_axis,3,3);
+		Move(barrel2,y_axis,2,2);
+		Move(barrel3,y_axis,3,3);
+
+		Turn(wheel, x_axis, math.rad(-15),unpackSpeed/2.5);
+		Turn(arm,x_axis,math.rad(30),unpackSpeed)
+		Turn(hand, x_axis, math.rad(60),unpackSpeed/10);
+		Turn(cannon, x_axis, math.rad(65),unpackSpeed/1.5);
+		WaitForTurn(cannon, x_axis);
+
+		Turn(arm,x_axis,math.rad(107),unpackSpeed/2.5);
+		WaitForTurn(arm,x_axis);
+
+		Turn(wheel, x_axis, math.rad(55),unpackSpeed/2);
+		WaitForTurn(wheel, x_axis);
+	end
+	isClosed = true
+	Turn(wheel, x_axis, math.rad(66),unpackSpeed/10);
+	Turn(lidLeft, y_axis, math.rad(-90), unpackSpeed/1.5);
+	Turn(lidRight, y_axis, math.rad(90), unpackSpeed/1.5);
+	WaitForTurn(lidLeft, y_axis)
+	--end of animation
 
 	Spring.SetUnitArmored(unitID,true);
-
 	while true do
 		local stunned_or_inbuild = spGetUnitIsStunned(unitID) or (spGetUnitRulesParam(unitID, "disarmed") == 1)
 		if not stunned_or_inbuild then
@@ -121,39 +84,34 @@ local function Close()
 		end
 		Sleep(500)
 	end
-end
 
+end
+-- Controls delay for when it closes
 local function RestoreAfterDelay()
+	Signal(SIGrestore)
+	SetSignalMask(SIGrestore+SIGanimation)
 	Sleep(restore_delay);
-	
+
 	repeat
 		local inactive = spGetUnitIsStunned(unitID)
 		if inactive then
 			Sleep(restore_delay)
 		end
 	until not inactive
-	
+
 	StartThread(Close);
 end
 
+
+
 -- event handlers
 
-
-function script.Activate ()
-	StartThread(Open)
-end
-
-function script.Deactivate ()
-	is_open = false
-	StartThread(Close)
-end
-
 function script.Create()
-	is_open = true;
-	
-	--StartThread(AimBlink);
 	StartThread(GG.Script.SmokeUnit, unitID, smokePiece)
 	StartThread(RestoreAfterDelay);
+
+	Move(aimProxy, y_axis, -13)
+	Move(muzzleProxy, y_axis, 6)
 
 	Hide(legs);
 	for i = 1,6 do
@@ -199,7 +157,7 @@ function script.EndBurst()
 end
 
 function script.QueryWeapon(n)
-	return muzzleProxy
+	if isClosed then return muzzleProxy else return muzzle end
 end
 
 function script.AimFromWeapon(n)
@@ -207,43 +165,63 @@ function script.AimFromWeapon(n)
 end
 
 function script.AimWeapon(num, heading, pitch)
-	Signal(aim + open + close)
-	SetSignalMask(aim)
-	
-	Turn(belt, z_axis, heading, math.rad(200));
+	Signal(SIGanimation)
+	SetSignalMask(SIGanimation)
+	StartThread(RestoreAfterDelay)
 
-	-- instantly turn the actual sim gun towards target, then wait for the visual pieces to animate
-	Turn(aimProxy, x_axis, -pitch)
-	
-	if (not is_open) then
-		StartThread(Open);
+	Turn(aimProxy, z_axis, heading)
+	if aimHelp then
+		StopSpin(aimProxy, z_axis)
+		aimHelp = false
+		return false
+	end
+	-- animation handled after here
+	Turn(belt, z_axis, heading, math.rad(200))
+	if isClosed then
+		Spring.SetUnitArmored(unitID,false);
 
-		while(not is_open) do
-			Sleep(250);
-		end
+		--Opening animation
+		Turn(lidLeft, y_axis, math.rad(0), unpackSpeed);
+		Turn(lidRight, y_axis, math.rad(0), unpackSpeed);
+		WaitForTurn(lidLeft, y_axis);
+
+		Turn(wheel, x_axis, math.rad(-15),unpackSpeed)
+		WaitForTurn(wheel, x_axis);
+
+		Move(barrel1,y_axis,0,6);
+		Move(barrel2,y_axis,0,6);
+		Move(barrel3,y_axis,0,6);
+
+		isClosed = false
 	end
 
-	--Turn(cannon, y_axis, heading, 1.2)
-	Turn(belt, z_axis, heading, math.rad(200));
-	Turn(wheel, x_axis, -math.rad(30), math.rad(200));
-	Turn(arm, x_axis, math.rad(30),10);
-	Turn(hand, x_axis, math.rad(30),10);
-	Turn(cannon, x_axis, -pitch-math.rad(33),10);
-	 
-	WaitForTurn (belt, z_axis)
-	WaitForTurn (wheel, x_axis)
-	
-	StartThread(RestoreAfterDelay);
 
-	return is_open;
+	local function LeanControl(angle)
+		if angle < 0 then return -angle else return 0 end
+	end
+
+	local lean = LeanControl(pitch+math.rad(30))--min pitch plus pi/6 is -60 degrees or approximately 1 in radians
+	local wh = math.rad(-30) + (lean * math.rad(90));
+	local ar = math.rad(30) + (lean * math.rad(5));
+	local ha = math.rad(30) - (lean * math.rad(25));
+	Turn(wheel, x_axis, wh, unpackSpeed/2.5);--constant a
+	Turn(arm, x_axis, ar, unpackSpeed);--constant b
+	Turn(hand, x_axis, ha, unpackSpeed);--constant c
+	Turn(cannon, x_axis, -wh-ar-ha-pitch, unpackSpeed);--constant (-a-b-c)
+
+	WaitForTurn(belt, z_axis)
+	WaitForTurn(cannon, x_axis)
+	WaitForTurn(wheel, x_axis)
+
+	return true;
 end
 
 function script.FireWeapon(n)
-	EmitSfx(muzzleProxy, 1024)
+	EmitSfx(muzzle, 1024)
 	Move(barrel1,y_axis,1,5);
 	Move(barrel2,y_axis,1,7);
 	Move(barrel3,y_axis,1,9);
-	
+
 	Sleep(200);
 	Move(barrel3,y_axis,0,2);
 	Sleep(100);
