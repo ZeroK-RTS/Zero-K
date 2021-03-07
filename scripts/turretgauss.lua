a592f569c0c659f21a6a2a22b9743fd343d499af
SteelBlueZK <sbluefaf@gmail.com>
2021-03-06 20:56:06 -0500

    Whitespace Cleanup

--- scripts/turretgauss.lua
+++ scripts/turretgauss.lua
@@ -44,36 +44,36 @@ local SIGrestore = 2
 local function Close()
 	Signal(SIGanimation)
 	SetSignalMask(SIGanimation)
-
+	
 	aimHelp = true
 	Spin(aimProxy, z_axis, math.rad(270))
-
+	
 	if not isClosed then
 		--animation
-		Move(barrel1,y_axis,3,3);
-		Move(barrel2,y_axis,2,2);
-		Move(barrel3,y_axis,3,3);
-
-		Turn(wheel, x_axis, math.rad(-15),unpackSpeed/2.5);
-		Turn(arm,x_axis,math.rad(30),unpackSpeed)
-		Turn(hand, x_axis, math.rad(60),unpackSpeed/10);
-		Turn(cannon, x_axis, math.rad(65),unpackSpeed/1.5);
+		Move(barrel1, y_axis, 3, 3);
+		Move(barrel2, y_axis, 2, 2);
+		Move(barrel3, y_axis, 3, 3);
+		
+		Turn(wheel, x_axis, math.rad(-15), unpackSpeed/2.5);
+		Turn(arm,x_axis,math.rad(30), unpackSpeed)
+		Turn(hand, x_axis, math.rad(60), unpackSpeed/10);
+		Turn(cannon, x_axis, math.rad(65), unpackSpeed/1.5);
 		WaitForTurn(cannon, x_axis);
-
-		Turn(arm,x_axis,math.rad(107),unpackSpeed/2.5);
+		
+		Turn(arm,x_axis,math.rad(107), unpackSpeed/2.5);
 		WaitForTurn(arm,x_axis);
-
-		Turn(wheel, x_axis, math.rad(55),unpackSpeed/2);
+		
+		Turn(wheel, x_axis, math.rad(55), unpackSpeed/2);
 		WaitForTurn(wheel, x_axis);
 	end
 	isClosed = true
-	Turn(wheel, x_axis, math.rad(66),unpackSpeed/10);
+	Turn(wheel, x_axis, math.rad(66), unpackSpeed/10);
 	Turn(lidLeft, y_axis, math.rad(-90), unpackSpeed/1.5);
 	Turn(lidRight, y_axis, math.rad(90), unpackSpeed/1.5);
 	WaitForTurn(lidLeft, y_axis)
 	--end of animation
-
-	Spring.SetUnitArmored(unitID,true);
+	
+	Spring.SetUnitArmored(unitID, true);
 	while true do
 		local stunned_or_inbuild = spGetUnitIsStunned(unitID) or (spGetUnitRulesParam(unitID, "disarmed") == 1)
 		if not stunned_or_inbuild then
@@ -84,44 +84,41 @@ local function Close()
 		end
 		Sleep(500)
 	end
-
 end
+
 -- Controls delay for when it closes
 local function RestoreAfterDelay()
 	Signal(SIGrestore)
 	SetSignalMask(SIGrestore+SIGanimation)
 	Sleep(restore_delay);
-
+	
 	repeat
 		local inactive = spGetUnitIsStunned(unitID)
 		if inactive then
 			Sleep(restore_delay)
 		end
 	until not inactive
-
+	
 	StartThread(Close);
 end
-
-
-
+	
 -- event handlers
-
 function script.Create()
 	StartThread(GG.Script.SmokeUnit, unitID, smokePiece)
 	StartThread(RestoreAfterDelay);
-
+	
 	Move(aimProxy, y_axis, -13)
 	Move(muzzleProxy, y_axis, 6)
-
+	
 	Hide(legs);
 	for i = 1,6 do
 		Hide(pillars[i]);
 	end
-
+	
 	local x,y,z = Spring.GetUnitPosition(unitID);
 	local gy = Spring.GetGroundHeight(x,z);
 	pillarHeight = y-gy;
-
+	
 	if(pillarHeight > 0) then
 		Show(legs);
 		-- each pillar segment is 45 elmo tall
@@ -157,7 +154,11 @@ function script.EndBurst()
 end
 
 function script.QueryWeapon(n)
-	if isClosed then return muzzleProxy else return muzzle end
+	if isClosed then
+		return muzzleProxy
+	else
+		return muzzle
+	end
 end
 
 function script.AimFromWeapon(n)
@@ -168,7 +169,7 @@ function script.AimWeapon(num, heading, pitch)
 	Signal(SIGanimation)
 	SetSignalMask(SIGanimation)
 	StartThread(RestoreAfterDelay)
-
+	
 	Turn(aimProxy, z_axis, heading)
 	if aimHelp then
 		StopSpin(aimProxy, z_axis)
@@ -178,29 +179,32 @@ function script.AimWeapon(num, heading, pitch)
 	-- animation handled after here
 	Turn(belt, z_axis, heading, math.rad(200))
 	if isClosed then
-		Spring.SetUnitArmored(unitID,false);
-
+		Spring.SetUnitArmored(unitID, false);
+		
 		--Opening animation
 		Turn(lidLeft, y_axis, math.rad(0), unpackSpeed);
 		Turn(lidRight, y_axis, math.rad(0), unpackSpeed);
 		WaitForTurn(lidLeft, y_axis);
-
-		Turn(wheel, x_axis, math.rad(-15),unpackSpeed)
+		
+		Turn(wheel, x_axis, math.rad(-15), unpackSpeed)
 		WaitForTurn(wheel, x_axis);
-
-		Move(barrel1,y_axis,0,6);
-		Move(barrel2,y_axis,0,6);
-		Move(barrel3,y_axis,0,6);
-
+		
+		Move(barrel1, y_axis, 0, 6);
+		Move(barrel2, y_axis, 0, 6);
+		Move(barrel3, y_axis, 0, 6);
+		
 		isClosed = false
 	end
-
-
+	
 	local function LeanControl(angle)
-		if angle < 0 then return -angle else return 0 end
+		if angle < 0 then 
+			return -angle 
+		else
+			return 0
+		end
 	end
-
-	local lean = LeanControl(pitch+math.rad(30))--min pitch plus pi/6 is -60 degrees or approximately 1 in radians
+	
+	local lean = LeanControl(pitch + math.rad(30))--min pitch plus pi/6 is -60 degrees or approximately 1 in radians
 	local wh = math.rad(-30) + (lean * math.rad(90));
 	local ar = math.rad(30) + (lean * math.rad(5));
 	local ha = math.rad(30) - (lean * math.rad(25));
@@ -208,41 +212,41 @@ function script.AimWeapon(num, heading, pitch)
 	Turn(arm, x_axis, ar, unpackSpeed);--constant b
 	Turn(hand, x_axis, ha, unpackSpeed);--constant c
 	Turn(cannon, x_axis, -wh-ar-ha-pitch, unpackSpeed);--constant (-a-b-c)
-
+	
 	WaitForTurn(belt, z_axis)
 	WaitForTurn(cannon, x_axis)
 	WaitForTurn(wheel, x_axis)
-
+	
 	return true;
 end
 
 function script.FireWeapon(n)
 	EmitSfx(muzzle, 1024)
-	Move(barrel1,y_axis,1,5);
-	Move(barrel2,y_axis,1,7);
-	Move(barrel3,y_axis,1,9);
-
+	Move(barrel1, y_axis, 1, 5);
+	Move(barrel2, y_axis, 1, 7);
+	Move(barrel3, y_axis, 1, 9);
+	
 	Sleep(200);
-	Move(barrel3,y_axis,0,2);
+	Move(barrel3, y_axis, 0, 2);
 	Sleep(100);
-	Move(barrel2,y_axis,0,2);
+	Move(barrel2, y_axis, 0, 2);
 	Sleep(100);
-	Move(barrel1,y_axis,0,2);
+	Move(barrel1, y_axis, 0, 2);
 end
 
 function script.Killed(recentDamage, maxHealth)
 	local severity = recentDamage / maxHealth
-
+	
 	if(pillarHeight > 0) then
 		Explode(legs, SFX.SHATTER)
 	end
-
+	
 	if(numPillars > 0) then
 		for i = 1, numPillars do
 			Explode(pillars[i],SFX.SHATTER);
 		end
 	end
-
+	
 	if (severity <= .25) then
 		return 1 -- corpsetype
 	elseif (severity <= .5) then
