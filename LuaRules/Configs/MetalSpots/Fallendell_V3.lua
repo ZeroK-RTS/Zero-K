741d0d874b3059f3bbdfe6604e21cf02edf79551
Aquanim <threeoneteacup@gmail.com>
2021-04-03 12:54:44 +1100

    Amend start boxes for Fallendell_V3 (and tidy metalconf)

--- LuaRules/Configs/MetalSpots/Fallendell_V3.lua
+++ LuaRules/Configs/MetalSpots/Fallendell_V3.lua
@@ -29,48 +29,7 @@ local allspots = {
     {x = 3520, z = 3605, metal = 2.0},
 }
 
-local duelspots = {
-
-}
-
-local twospots = {
-
-}
-
-local threespots = {
-
-}
-
-local teamspots = {
-
-}
-
--- there's probably a better way to get player count... borrowed from the old and bad player list
-local pcount = 0
-local playerlist = Spring.GetPlayerList()
-local teamsSorted = Spring.GetTeamList()
--- count AIs
-for i = 1, #teamsSorted do
-	local teamID = teamsSorted[i]
-	if teamID ~= Spring.GetGaiaTeamID() then
-		local _,leader,isDead,isAI,_,allyTeamID = Spring.GetTeamInfo(teamID)
-		if isAI then
-			pcount = pcount + 1
-		end
-	end 
-end
--- count humans
-for i = 1, #playerlist do
-	local playerID = playerlist[i]
-	local name, active, spectator, teamID, allyTeamID, pingTime, cpuUsage, country = Spring.GetPlayerInfo(playerID)
-	local isSpec = (teamID == 0 and spectator and (not Spring.GetGameRulesParam("initiallyPlayingPlayer_" .. playerID))) 
-	if not isSpec then
-		pcount = pcount + 1
-	end
-end
-
 local ret = {}
-local v2
 
 local count = 1
 
@@ -78,36 +37,6 @@ for k,v in pairs(allspots) do
 	ret[count] = v 
 	ret[count+1] = {x = 6144-v.x, z = 5120-v.z, metal = v.metal}
 	count = count + 2
-end
-
-if pcount > 2 then
-	if pcount > 4 then
-		if pcount > 6 then
-			for k,v in pairs(teamspots) do
-				ret[count] = v 
-				ret[count+1] = {x = 6144-v.x, z = 5120-v.z, metal = v.metal}
-				count = count + 2
-			end
-		else
-			for k,v in pairs(threespots) do
-				ret[count] = v 
-				ret[count+1] = {x = 6144-v.x, z = 5120-v.z, metal = v.metal}
-				count = count + 2
-			end
-		end
-	else
-		for k,v in pairs(twospots) do
-			ret[count] = v 
-			ret[count+1] = {x = 6144-v.x, z = 5120-v.z, metal = v.metal}
-			count = count + 2
-		end
-	end
-else
-	for k,v in pairs(duelspots) do
-		ret[count] = v 
-		ret[count+1] = {x = 6144-v.x, z = 5120-v.z, metal = v.metal}
-		count = count + 2
-	end
 end
 
 return { spots = ret }
