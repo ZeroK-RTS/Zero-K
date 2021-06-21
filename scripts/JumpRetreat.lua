69a5624a6406bffd29c47aad71cb381ed5534a03
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-06-20 20:34:14 -0400

    Update file to use fixed version from FW

--- scripts/JumpRetreat.lua
+++ scripts/JumpRetreat.lua
@@ -2,30 +2,49 @@
 local CMD_JUMP = Spring.Utilities.CMD.JUMP
 local GiveClampedOrderToUnit = Spring.Utilities.GiveClampedOrderToUnit
 local jumpRange = tonumber(UnitDefs[unitDefID].customParams.jump_range)
+local retreattype = UnitDefs[unitDefID].customParams.jumpretreattype or "always"
 
 local retreating = false
 
 local function RetreatThread(hx, hy, hz)
-	local reload, disarmed
+	--Spring.Echo("RetreatThread")
+	local reload, disarmed, ux, uy, uz, moveDistance, disScale, cx, cy, cz, isstunned
+	local realrange = jumpRange * jumpRangeBonus -- this can't be cached for some reason.
 	while retreating do
 		reload = Spring.GetUnitRulesParam(unitID, "jumpReload") or 1
 		disarmed = (Spring.GetUnitRulesParam(unitID, "disarmed") or 0) == 1
-		if reload >= 1 and not disarmed then
-			local ux, uy, uz = Spring.GetUnitPosition(unitID)
-			local moveDistance = math.sqrt((ux - hx)^2 + (uz - hz)^2)
-			local disScale = jumpRange/moveDistance*0.95
-			local cx, cy, cz = ux + disScale*(hx - ux), hy, uz + disScale*(hz - uz)
-			GiveClampedOrderToUnit(unitID, CMD.INSERT, { 0, CMD_JUMP, CMD.OPT_INTERNAL, cx, cy, cz}, CMD.OPT_ALT)
-		else
-			Sleep(33)
+		isstunned = Spring.GetUnitIsStunned(unitID)
+		--Spring.Echo("Reload: " .. tostring(reload) .. " / 1\nDisarmed: " .. tostring(disarmed))
+		if reload >= 1 and not disarmed and not isstunned then
+			ux, uy, uz = Spring.GetUnitPosition(unitID)
+			moveDistance = math.sqrt(((ux - hx) * (ux - hx)) + ((uz - hz) * (uz - hz)))
+			--Spring.Echo("MoveDistance: " .. moveDistance)
+			if moveDistance >= 200 and moveDistance < realrange then -- jump to finish reteating.
+				GiveClampedOrderToUnit(unitID, CMD.INSERT, { 0, CMD_JUMP, CMD.OPT_INTERNAL, hx, hy, hz}, CMD.OPT_ALT)
+				retreating = false
+			elseif moveDistance < 200 then -- don't jump around in haven or waste it near it.
+				retreating = false -- stop watching reload states.
+			else
+				disScale = realrange/moveDistance*0.95
+				cx, cy, cz = ux + disScale*(hx - ux), hy, uz + disScale*(hz - uz)
+				GiveClampedOrderToUnit(unitID, CMD.INSERT, { 0, CMD_JUMP, CMD.OPT_INTERNAL, cx, cy, cz}, CMD.OPT_ALT)
+				if retreattype == "once" then
+					retreating = false
+				end
+			end
 		end
+		Sleep(66)
 	end
 end
-		
 
 function RetreatFunction(hx, hy, hz)
-	retreating = true
-	StartThread(RetreatThread, hx, hy, hz)
+	if retreattype == "none" then
+		return
+	end
+	if not retreating then
+		retreating = true
+		StartThread(RetreatThread, hx, hy, hz)
+	end
 end
 
 function StopRetreatFunction()
