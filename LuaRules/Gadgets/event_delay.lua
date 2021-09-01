7372eabefe829e3c9f8042d11953f531b9bd36dd
PetTurtle <illya@illyaryhan.xyz>
2021-09-01 17:12:42 -0500

    whitespace

--- LuaRules/Gadgets/event_delay.lua
+++ LuaRules/Gadgets/event_delay.lua
@@ -1,48 +1,48 @@
 function gadget:GetInfo()
-  return {
-    name = "Event Delay",
-    desc = "run event after delay (frames)",
-    author = "PetTurtle",
-    date = "2021",
-    license = "GNU GPL, v2 or later",
-    layer = 0,
-    enabled = true
-  }
+	return {
+		name = "Event Delay",
+		desc = "run event after delay (frames)",
+		author = "PetTurtle",
+		date = "2021",
+		license = "GNU GPL, v2 or later",
+		layer = 0,
+		enabled = true
+	}
 end
 
 if not gadgetHandler:IsSyncedCode() then
-  return false
+	return false
 end
 
 local currentFrame = 0
 local eventBuffer = {}
 
 function gadget:GameFrame(frame)
-  currentFrame = frame
-  if #eventBuffer > 0 then
-    local delayed = {}
+	currentFrame = frame
+	if #eventBuffer > 0 then
+		local delayed = {}
 
-    for i = 1, #eventBuffer do
-      local event = eventBuffer[i]
-      if event.frame <= frame then
-        event.run(unpack(event.args))
-      else
-        delayed[#delayed + 1] = event
-      end
-    end
+		for i = 1, #eventBuffer do
+			local event = eventBuffer[i]
+			if event.frame <= frame then
+				event.run(unpack(event.args))
+			else
+				delayed[#delayed + 1] = event
+			end
+		end
 
-    eventBuffer = delayed
-  end
+		eventBuffer = delayed
+	end
 end
 
 local function EventDelay(delay, event, args)
-  eventBuffer[#eventBuffer + 1] = {
-    run = event,
-    args = args,
-    frame = currentFrame + delay
-  }
+	eventBuffer[#eventBuffer + 1] = {
+		run = event,
+		args = args,
+		frame = currentFrame + delay
+	}
 end
 
 function gadget:Initialize()
-  GG.EventDelay = EventDelay
+	GG.EventDelay = EventDelay
 end
