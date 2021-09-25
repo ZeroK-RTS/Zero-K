8b5d2fb95905cf692e3d92151d65cab472a059ae
Rafał Karczmarczyk <carabusx@gmail.com>
2021-09-25 04:59:59 +0200

    Add support for mod-side explosions_mod.lua file.

--- gamedata/explosions.lua
+++ gamedata/explosions.lua
@@ -5,13 +5,28 @@ local vfsDirList = VFS.DirList
 vfsInclude("LuaRules/Utilities/tablefunctions.lua")
 local suCopyTable = Spring.Utilities.CopyTable
 
+local explosionDefs = {}
+ExplosionDefs = explosionDefs
+
+local shared = {} -- shared amongst the lua explosiondef enviroments
+Shared = shared
+
 local files = vfsDirList("effects", "*.lua")
 suCopyTable(vfsDirList("gamedata/explosions", "*.lua", VFS.MAP), false, files)
 
-local explosionDefs = {}
 for i = 1, #files do
 	suCopyTable(vfsInclude(files[i]), false, explosionDefs)
 end
+
+--[[ This lets mutators add a bit of explosions_post processing without
+     losing access to future gameside updates to explosions_post. ]]
+local MODSIDE_POSTS_FILEPATH = 'gamedata/explosions_mod.lua'
+if VFS.FileExists(MODSIDE_POSTS_FILEPATH, VFS.GAME) then
+	vfsInclude(MODSIDE_POSTS_FILEPATH, nil, VFS.GAME)
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 for edName, eDef in pairs(explosionDefs) do
 	for fxName, fxDef in pairs(eDef) do
