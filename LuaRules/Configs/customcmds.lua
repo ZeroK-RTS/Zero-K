d7b9b61072ce5f69971cbcd011b4e4e5d13f052d
sprunk <spr.ng@o2.pl>
2021-10-04 23:47:44 +0200

    📝 Remove the "proposed ID ranges" from customcmds
    Nobody seems to actually adhere and they aren't binding in any way.

--- LuaRules/Configs/customcmds.lua
+++ LuaRules/Configs/customcmds.lua
@@ -1,15 +1,3 @@
---------------------------------------------------------------------------------
---
---  Proposed Command ID Ranges:
---
---    all negative:  Engine (build commands)
---       0 -   999:  Engine
---    1000 -  9999:  Group AI
---   10000 - 19999:  LuaUI
---   20000 - 29999:  LuaCob
---   30000 - 39999:  LuaRules
---
-
 --[[ WARNING!! Only BASE VANILLA ZK commands belong here!
      If you are a modder adding custom commands, MAKE YOUR OWN FILE.
      By overriding this file you're SETTING YOURSELF UP FOR FAILURE
