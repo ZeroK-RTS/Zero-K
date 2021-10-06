64239a920006b60d1faab2cea04fc43eed90ed3c
Rafał Karczmarczyk <carabusx@gmail.com>
2021-10-06 19:57:11 +0200

    Automatically generate some airPad data from the unitDef.

--- LuaRules/Configs/airpad_defs.lua
+++ LuaRules/Configs/airpad_defs.lua
@@ -1,19 +1,20 @@
 local airpadDefs = {
 	[UnitDefNames["factoryplane"].id] = {
-		mobile = false,
-		cap = 1,
 		padPieceName = {"land"}
 	},
 	[UnitDefNames["staticrearm"].id] = {
-		mobile = false,
-		cap = 4,
 		padPieceName = {"land1","land2","land3","land4"}
 	},
 	[UnitDefNames["shipcarrier"].id] = {
-		mobile = true,
-		cap = 2,
 		padPieceName = {"LandingFore","LandingAft"}
 	},
 }
+
+for unitDefID, config in pairs(airpadDefs) do
+    local ud = UnitDefs[unitDefID]
+
+    config.mobile = (not ud.isImmobile)
+    config.cap = config.cap or ud.customParams.pad_count
+end
 
 return airpadDefs
