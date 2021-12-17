ab192162ded21921305b02e98a210d2e58e16540
GoogleFrog <googl3frog@gmail.com>
2021-12-18 10:45:54 +1100

    Try large raw move distance mult. This should make fight, repair, reclaim etc.. do raw moves rather than zigzag moves. It seems similar to the main function of the raw move gadget, but doesn't include the extra anti-stuck features.

--- gamedata/modrules.lua
+++ gamedata/modrules.lua
@@ -127,7 +127,7 @@ local modrules  = {
   system = {
     pathFinderSystem = 0, --(Spring.GetModOptions() and (Spring.GetModOptions().pathfinder == "qtpfs") and 1) or 0, -- QTPFS causes desync https://springrts.com/mantis/view.php?id=5936
 	pathFinderUpdateRate = 0.0000001,
-	pathFinderRawDistMult = 1.25,
+	pathFinderRawDistMult = 100000,
 	allowTake = false,
   },
 }
