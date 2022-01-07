ef446b2e64ea5904a496a20902ae08126b587efb
Mach565 <97276027+Mach565@users.noreply.github.com>
2022-01-07 20:47:36 +1100

    Nuke is a nuke

--- units/staticnuke.lua
+++ units/staticnuke.lua
@@ -21,6 +21,7 @@ return { staticnuke = {
     stockpiletime  = [[180]],
     stockpilecost  = [[3240]],
     priority_misc  = 1, -- Medium
+    isNuke = true,
   },
 
   explodeAs                     = [[ATOMIC_BLAST]],
