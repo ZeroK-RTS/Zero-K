2c1cf7bdd968dd099b2ceb677ada7a02871cd443
Porkchop <longlivetheporkchop@gmail.com>
2022-04-16 18:08:22 -0600

    Revert "Radar vertical line optimization"
    This reverts commit c6e2cb048f6fc3c991eea34f18f40f14bf2fc2aa.

--- LuaUI/Widgets/gui_vertLineAid.lua
+++ LuaUI/Widgets/gui_vertLineAid.lua
@@ -14,21 +14,8 @@ local last_frame = 0
 local disabled = false
 local enemyDots = {}
 local allyDots = {}
-local dot = { x, y, z, losState, r, g, b}
-local removals = {}
-
 local needsUpdate = false
 local myAllyTeamID
-
-local max = math.max
-local spGetGameFrame = Spring.GetGameFrame
-local spGetUnitAllyTeam = Spring.GetUnitAllyTeam
-local spGetUnitPosition = Spring.GetUnitPosition
-local spGetUnitLosState = Spring.GetUnitLosState
-local spGetTeamColor = Spring.GetTeamColor
-local spGetUnitTeam = Spring.GetUnitTeam
-local spValidUnitID = Spring.ValidUnitID
-local spGetGroundHeight = Spring.GetGroundHeight
 
 local function forceUpdate ()
 	needsUpdate = true
@@ -95,11 +82,11 @@ function widget:PlayerChanged (playerID)
 end
 
 function widget:UnitEnteredRadar (unitID, unitTeam)
-	if (spGetUnitAllyTeam(unitID) ~= myAllyTeamID) then
-		dot.x, dot.y, dot.z = spGetUnitPosition(unitID)
-		dot.losState = spGetUnitLosState(unitID, myAllyTeamID)
-		dot.r, dot.g, dot.b = spGetTeamColor(spGetUnitTeam(unitID))
-		enemyDots[unitID] = {dot.x, dot.y, dot.z, max(spGetGroundHeight(dot.x,dot.z), 0), dot.losState.los, dot.r, dot.g, dot.b} -- x, y, z, ground, inlos, r, g, b
+	if (Spring.GetUnitAllyTeam(unitID) ~= myAllyTeamID) then
+		local x, y, z = Spring.GetUnitPosition (unitID)
+		local losState = Spring.GetUnitLosState(unitID, myAllyTeamID)
+		local r, g, b = Spring.GetTeamColor (Spring.GetUnitTeam(unitID))
+		enemyDots[unitID] = {x, y, z, math.max(Spring.GetGroundHeight(x,z), 0), losState.los, r, g, b} -- x, y, z, ground, inlos, r, g, b
 	end
 end
 
@@ -113,17 +100,17 @@ function widget:UnitDestroyed (unitID, unitTeam)
 end
 
 function widget:UnitCreated (unitID)
-	if (spGetUnitAllyTeam(unitID) == myAllyTeamID) then
-		dot.x, dot.y, dot.z = spGetUnitPosition(unitID)
-		dot.r, dot.g, dot.b = spGetTeamColor(spGetUnitTeam(unitID))
-		allyDots[unitID] = {dot.x, dot.y, dot.z, max(spGetGroundHeight(dot.x,dot.z), 0), true, dot.r, dot.g, dot.b} -- x, y, z, ground, inlos, r, g, b
+	if (Spring.GetUnitAllyTeam(unitID) == myAllyTeamID) then
+		local x, y, z = Spring.GetUnitPosition (unitID)
+		local r, g, b = Spring.GetTeamColor (Spring.GetUnitTeam(unitID))
+		allyDots[unitID] = {x, y, z, math.max(Spring.GetGroundHeight(x,z), 0), true, r, g, b} -- x, y, z, ground, inlos, r, g, b
 	end
 end
 
 function widget:DrawWorld()
 	local needs_update = needsUpdate
 	needsUpdate = false
-	local f = spGetGameFrame()
+	local f = Spring.GetGameFrame()
 	if f > last_frame then
 		last_frame = f
 		needs_update = true
@@ -132,22 +119,24 @@ function widget:DrawWorld()
 	gl.PushAttrib (GL.LINE_BITS)
 	gl.DepthTest (true)
 	gl.LineWidth (1.4)
+
+	local removals = {}
 	for unitID, data in pairs (enemyDots) do
 		if needs_update then
-			if not spValidUnitID(unitID) then
+			if not Spring.ValidUnitID(unitID) then
 				removals[unitID] = true
 			else
-				dot.x, dot.y, dot.z = spGetUnitPosition(unitID)
-				dot.losState = spGetUnitLosState(unitID)
-				dot.r, dot.g, dot.b = spGetTeamColor(spGetUnitTeam(unitID))
-				data[1] = dot.x
-				data[2] = dot.y
-				data[3] = dot.z
-				data[4] = max(spGetGroundHeight(dot.x,dot.z), 0)
-				data[5] = dot.losState.los
-				data[6] = dot.r
-				data[7] = dot.g
-				data[8] = dot.b
+				local x, y, z = Spring.GetUnitPosition (unitID)
+				local losState = Spring.GetUnitLosState(unitID)
+				local r, g, b = Spring.GetTeamColor (Spring.GetUnitTeam(unitID))
+				data[1] = x
+				data[2] = y
+				data[3] = z
+				data[4] = math.max(Spring.GetGroundHeight(x,z), 0)
+				data[5] = losState.los
+				data[6] = r
+				data[7] = g
+				data[8] = b
 			end
 		end
 		if (((data[2] > 0) and ((options.enable_vertical_lines_air.value == "always") or ((options.enable_vertical_lines_air.value == "radar") and (not data[5])))) or ((data[2] < 0) and ((options.enable_vertical_lines_water.value == "always") or ((options.enable_vertical_lines_water.value == "radar") and (not data[5]))))) then
@@ -160,7 +149,6 @@ function widget:DrawWorld()
 	end
 	for unitID in pairs (removals) do
 		enemyDots[unitID] = nil
-		unitID = nil
 	end
 
 	if options.enable_vertical_lines_ally.value ~= "never" then
@@ -168,16 +156,16 @@ function widget:DrawWorld()
 		local show_water = ((options.enable_vertical_lines_ally.value == "always") or (options.enable_vertical_lines_ally.value == "water"))
 		for unitID, data in pairs (allyDots) do
 			if needs_update then
-				dot.x, dot.y, dot.z = spGetUnitPosition(unitID)
-				dot.r, dot.g, dot.b = spGetTeamColor(spGetUnitTeam(unitID))
-				data[1] = dot.x
-				data[2] = dot.y
-				data[3] = dot.z
-				data[4] = max(spGetGroundHeight(dot.x,dot.z), 0)
+				local x, y, z = Spring.GetUnitPosition (unitID)
+				local r, g, b = Spring.GetTeamColor (Spring.GetUnitTeam(unitID))
+				data[1] = x
+				data[2] = y
+				data[3] = z
+				data[4] = math.max(Spring.GetGroundHeight(x,z), 0)
 				data[5] = true
-				data[6] = dot.r
-				data[7] = dot.g
-				data[8] = dot.b
+				data[6] = r
+				data[7] = g
+				data[8] = b
 			end
 			if ((data[2] > 0) and show_air) or ((data[2] < 0) and show_water) then
 				gl.Color (data[6], data[7], data[8], 1)
