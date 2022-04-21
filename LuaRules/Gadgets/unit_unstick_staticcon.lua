7ff697397c7bd0ebec09d7fcbb08cb9b3598b4df
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2022-04-21 15:32:50 -0400

    Suggestion Pass 1
    Have not yet implemented the "iterablemap" suggestion yet.

--- LuaRules/Gadgets/unit_unstick_staticcon.lua
+++ LuaRules/Gadgets/unit_unstick_staticcon.lua
@@ -1,37 +1,31 @@
+if not (gadgetHandler:IsSyncedCode()) then
+	return
+end
+
 function gadget:GetInfo()
 	return {
 		name      = "Static Con Unsticker",
-		desc      = "Implements Caretaker / rejuvenator unstick.",
+		desc      = "Ensures static constructors do not get stuck on tasks outside their build range",
 		author    = "Shaman",
-		date      = "4-20-2022",
+		date      = "2022-04-20",
 		license   = "PD",
 		layer     = 0,
 		enabled   = true,
 	}
 end
 
-if not (gadgetHandler:IsSyncedCode()) then
-	return
-end
-
 local delay = 15
 local debug = false
 
 local handled = {} -- [f] = {[1] = unitID}
-local buildRanges = {} -- [unitDefID] = buildDistance
+local buildRangesSq = {} -- [unitDefID] = buildDistance
 
 for i = 1, #UnitDefs do
 	local def = UnitDefs[i]
 	if def.isBuilder and def.customParams.like_structure then
-		buildRanges[i] = def.buildDistance
+		buildRangesSq[i] = def.buildDistance^2
 	end
 end
-
---[[local buildRanges = {
-	[UnitDefNames["staticrepair"].id] = UnitDefNames["staticrepair"].buildDistance,
-	[UnitDefNames["staticcon"].id] = UnitDefNames["staticcon"].buildDistance,
-	[UnitDefNames["striderhub"].id] = UnitDefNames["striderhub"].buildDistance,
-}]]
 
 -- Speedups --
 local CMD_INSERT = CMD.INSERT
@@ -52,7 +46,7 @@ local spGetFeaturePosition = Spring.GetFeaturePosition
 --local EMPTY = {}
 
 local function DebugEcho(str)
-	spEcho("[unit_staticcon_unsticker]: " .. str)
+	spEcho("[unit_unstick_staticcon]: " .. str)
 end
 
 local function GetDistance(unitID, targetID, isReclaim)
@@ -71,55 +65,67 @@ local function GetDistance(unitID, targetID, isReclaim)
 		if debug then DebugEcho("Target doesn't exist?") end
 		return 9999
 	end
-	return math.sqrt(((x2 - x) * (x2 - x)) + ((z2 - z) * (z2 - z)))
+	return ((x2 - x) * (x2 - x)) + ((z2 - z) * (z2 - z))
 end
 
 local function IsObjectCloseEnough(unitID, targetID, cmd) -- Note: build ranges are 2D usually.
-	if cmd == CMD_REPAIR and spValidUnitID(targetID) then
-		local dist = GetDistance(unitID, targetID, false)
-		if debug then
-			DebugEcho(unitID .. ": Distance: " .. dist .. " (Build Range: " .. buildRanges[spGetUnitDefID(unitID)] .. ")")
-		end
-		return dist <= buildRanges[spGetUnitDefID(unitID)]
-	elseif (cmd == CMD_RECLAIM or cmd == CMD_RESURRECT) and spValidFeatureID(targetID) then
-		local dist = GetDistance(unitID, targetID, true)
-		if debug then
-			DebugEcho(unitID .. ": Distance: " .. dist .. " (Build Range: " .. buildRanges[spGetUnitDefID(unitID)] .. ")")
-		end
-		return dist <= buildRanges[spGetUnitDefID(unitID)]
-	else
-		return false
+	local isReclaim = (cmd == CMD_RECLAIM or cmd == CMD_RESURRECT)
+	local dist = GetDistance(unitID, targetID, isReclaim)
+	local buildRange = buildRangesSq[spGetUnitDefID(unitID)]
+	if debug then
+		DebugEcho(unitID, ": Distance: ", dist, " (Build Range: ", buildRange, ")")
 	end
+	return dist <= buildRange
 end
 
+--[[Some documentation with regards to this HandleUnit function:
+When patrol orders for cons encounters reclaim or repair, it sends the following cmd params:
+[1] = unitID / featureID
+[2] = the patroler's X position
+[3] = the patroler's Y position
+[4] = the patroler's Z position
+[5] = the patroler's buildrange
+
+A normal user added single unit reclaim would have only look like this:
+[1] = Unit / FeatureID
+
+OR (after finding a feature)
+[1] = unitID / featureID
+[2] = center X
+[3] = center Y
+[4] = center Z
+[5] = radius
+]]
+
 local function HandleUnit(unitID, f)
-	if spValidUnitID(unitID) then
-		local _, _, unbuilt = spGetUnitIsStunned(unitID)
-		if not unbuilt then -- if caretaker is reversed built, UnitFinished should retrigger to readd it, though nobody really unbuilds caretakers.
-			local cmdID, _, cmdTag, cmdParam1, cmdParam2, cmdParam3, cmdParam4, cmdParam5 = spGetUnitCurrentCommand(unitID)
-			if cmdParam1 and spValidFeatureID(cmdParam1 - Game.maxUnits) then
-				cmdParam1 = cmdParam1 - Game.maxUnits
-			end
-			if debug then DebugEcho(unitID .. ": " .. tostring(cmdID) .. ", " .. tostring(cmdParam1) .. ", " .. tostring(cmdParam2)) end
-			if cmdID and cmdParam1 and (spValidFeatureID(cmdParam1) or spValidUnitID(cmdParam1)) then
-				if debug then DebugEcho(unitID .. ": Valid command, checking distance.") end
-				if not IsObjectCloseEnough(unitID, cmdParam1, cmdID) then
-					if debug then DebugEcho(unitID .. ": Command dropped (Out of Range)") end
-					spGiveOrderToUnit(unitID, CMD_REMOVE, cmdTag, 0)
-					if cmdParam5 and cmdParam5 ~= buildRanges[spGetUnitDefID(unitID)] then -- this was an area command added by the user.
-						if debug then DebugEcho("Readding area command: " .. tostring(cmdParam2) .. ", " .. tostring(cmdParam3) .. ", " .. tostring(cmdParam4) .. ", " .. tostring(cmdParam5)) end
-						spGiveOrderToUnit(unitID, CMD_INSERT, {0, cmdID, CMD.OPT_SHIFT, cmdParam2, cmdParam3, cmdParam4, cmdParam5}, CMD.OPT_ALT) -- reissue the command (just in case)
-					end
-				end
-			elseif debug then
-				DebugEcho("Invalid command handled for " .. unitID)
-			end
-			handled[f + delay] = handled[f + delay] or {} -- check back in 15 frames.
-			handled[f + delay][#handled[f + delay] + 1] = unitID
-		elseif debug then 
-			DebugEcho(unitID .. ": Exited drop (Reverse Built)")
-		end
+	if not spValidUnitID(unitID) then
+		return
 	end
+	local _, _, unbuilt = spGetUnitIsStunned(unitID)
+	if unbuilt then -- if caretaker is reversed built, UnitFinished should retrigger to readd it, though nobody really unbuilds caretakers.
+		if debug then DebugEcho(unitID .. ": Exited drop (Reverse Built)") end
+		return
+	end
+	local cmdID, _, cmdTag, cmdParam1, cmdParam2, cmdParam3, cmdParam4, cmdParam5 = spGetUnitCurrentCommand(unitID)
+	if cmdParam1 and spValidFeatureID(cmdParam1 - Game.maxUnits) then
+		cmdParam1 = cmdParam1 - Game.maxUnits
+	end
+	if debug then DebugEcho(unitID .. ": " .. tostring(cmdID) .. ", " .. tostring(cmdParam1) .. ", " .. tostring(cmdParam2)) end
+	if cmdID and cmdParam1 and (spValidFeatureID(cmdParam1) or spValidUnitID(cmdParam1)) then
+		if debug then DebugEcho(unitID .. ": Valid command, checking distance.") end
+		if not IsObjectCloseEnough(unitID, cmdParam1, cmdID) then
+			if debug then DebugEcho(unitID .. ": Command dropped (Out of Range)") end
+			spGiveOrderToUnit(unitID, CMD_REMOVE, cmdTag, 0)
+			if cmdParam5 and cmdParam5 ~= buildRangesSq[spGetUnitDefID(unitID)] then -- this was an area command added by the user.
+				if debug then DebugEcho(unitID, cmdID, cmdParam1, cmdParam2) end
+				spGiveOrderToUnit(unitID, CMD_INSERT, {0, cmdID, CMD.OPT_SHIFT, cmdParam2, cmdParam3, cmdParam4, cmdParam5}, CMD.OPT_ALT) -- reissue the command (just in case)
+			end
+		end
+	elseif debug then
+		DebugEcho("Invalid command handled for " .. unitID)
+	end
+	handled[f + delay] = handled[f + delay] or {} -- check back in 15 frames.
+	handled[f + delay][#handled[f + delay] + 1] = unitID
 end
 
 function gadget:GameFrame(f)
@@ -133,7 +139,7 @@ function gadget:GameFrame(f)
 end
 
 function gadget:UnitFinished(unitID, unitDefID, unitTeam)
-	if buildRanges[unitDefID] then
+	if buildRangesSq[unitDefID] then
 		if debug then DebugEcho(unitID .. ": Added to check loop.") end
 		local f = spGetGameFrame() + delay
 		handled[f] = handled[f] or {}
