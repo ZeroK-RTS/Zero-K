3eca0f1be0c0fcaa795adff53a40c321e8b5c637
sprunk <gnurps@o2.pl>
2022-08-16 04:47:11 +0200

    Only the animation changes

--- scripts/vehheavyarty.lua
+++ scripts/vehheavyarty.lua
@@ -19,12 +19,12 @@ local RELEASE_DELAY = 400 -- How long gantry waits after missile leaves
 local TRACK_PERIOD = 50
 
 local BAY_DISTANCE = -10
-local BAY_SPEED_LOADED = 5
+local BAY_SPEED_LOADED = 7
 local BAY_SPEED_UNLOADED = 7
-local GANTRY_SPEED_LOADED = math.rad(40)
-local GANTRY_SPEED_UNLOADED = math.rad(60)
-local CLAMP_SPEED_LOADED = math.rad(120)
-local CLAMP_SPEED_UNLOADED = math.rad(120)
+local GANTRY_SPEED_LOADED = math.rad(90)
+local GANTRY_SPEED_UNLOADED = math.rad(40)
+local CLAMP_SPEED_LOADED = math.rad(360)
+local CLAMP_SPEED_UNLOADED = math.rad(90)
 
 local WHEEL_SPIN_SPEED = math.rad(720)
 local WHEEL_SPIN_ACCEL = math.rad(100)
@@ -35,134 +35,112 @@ local tracks = 1
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
-local ableToMove = true
-local function SetAbleToMove(newMove)
-	if ableToMove == newMove then
-		return
-	end
-	ableToMove = newMove
-	
-	Spring.SetUnitRulesParam(unitID, "selfTurnSpeedChange", (ableToMove and 1) or 0.05)
-	Spring.SetUnitRulesParam(unitID, "selfMoveSpeedChange", (ableToMove and 1) or 0.05)
-	GG.UpdateUnitAttributes(unitID)
-	if newMove then
-		GG.WaitWaitMoveUnit(unitID)
-	end
-end
-
-local function TrackControl()  
-  SetSignalMask(SIG_MOVE)  
+local function TrackControl()
+	SetSignalMask(SIG_MOVE)
 	while isMoving do
-    if ableToMove then      
-      for i=1,#wheels do
-        Spin(wheels[i], x_axis, WHEEL_SPIN_SPEED, WHEEL_SPIN_ACCEL)
-      end      
-      tracks = tracks + 1
-      if tracks == 2 then
-        Hide(tracks1)
-        Show(tracks2)
-      elseif tracks == 3 then
-        Hide(tracks2)
-        Show(tracks3)
-      elseif tracks == 4 then
-        Hide(tracks3)
-        Show(tracks4)
-      else
-        tracks = 1
-        Hide(tracks4)
-        Show(tracks1)
-      end
-    end
+		for i = 1, #wheels do
+			Spin(wheels[i], x_axis, WHEEL_SPIN_SPEED, WHEEL_SPIN_ACCEL)
+		end
+		tracks = tracks + 1
+		if tracks == 2 then
+			Hide(tracks1)
+			Show(tracks2)
+		elseif tracks == 3 then
+			Hide(tracks2)
+			Show(tracks3)
+		elseif tracks == 4 then
+			Hide(tracks3)
+			Show(tracks4)
+		else
+			tracks = 1
+			Hide(tracks4)
+			Show(tracks1)
+		end
 		Sleep(TRACK_PERIOD)
 	end
 end
 
 local function Prepare()
-  Signal(SIG_OPEN)
+	Signal(SIG_OPEN)
 	SetSignalMask(SIG_OPEN)
-  SetAbleToMove(false)  
+
 	Move(bay, x_axis, 0, BAY_SPEED_LOADED)
 	WaitForMove(bay, x_axis)
 	doStrobe = true
 	Turn(gantry, x_axis, math.rad(-90), GANTRY_SPEED_LOADED)
 	WaitForTurn(gantry, x_axis)
-	Turn(clamp1, z_axis, math.rad(-(90)), CLAMP_SPEED_LOADED)
-	Turn(clamp2, z_axis, math.rad(-(-90)), CLAMP_SPEED_LOADED)
+	Turn(clamp1, z_axis, math.rad(-90), CLAMP_SPEED_LOADED)
+	Turn(clamp2, z_axis, math.rad( 90), CLAMP_SPEED_LOADED)
 	WaitForTurn(clamp1, z_axis)
-	WaitForTurn(clamp2, z_axis)  
+	WaitForTurn(clamp2, z_axis)
 	isReady = true
 end
 
 local function Reload()
-  Signal(SIG_OPEN)
+	Signal(SIG_OPEN)
 	SetSignalMask(SIG_OPEN)
-  if not isLoaded then   -- Just fired
-    Sleep(RELEASE_DELAY)  
-  end
+	if not isLoaded then   -- Just fired
+		Sleep(RELEASE_DELAY)
+	end
 	isReady = false
 	doStrobe = false
-  if isLoaded then -- Slow retract if missile still there
-    Turn(clamp1, z_axis, 0, CLAMP_SPEED_LOADED)
-    Turn(clamp2, z_axis, 0, CLAMP_SPEED_LOADED)	
-    Turn(gantry, x_axis, 0, GANTRY_SPEED_LOADED)
-    WaitForTurn(clamp1, z_axis)
-    WaitForTurn(clamp2, z_axis)
-    WaitForTurn(gantry, x_axis)
-    Move(bay, x_axis, -BAY_DISTANCE, BAY_SPEED_LOADED)    
-  else
-    Turn(clamp1, z_axis, 0, CLAMP_SPEED_UNLOADED)
-    Turn(clamp2, z_axis, 0, CLAMP_SPEED_UNLOADED)	
-    Turn(gantry, x_axis, 0, GANTRY_SPEED_UNLOADED)
-    WaitForTurn(clamp1, z_axis)
-    WaitForTurn(clamp2, z_axis)
-    WaitForTurn(gantry, x_axis)
-    Move(bay, x_axis, -BAY_DISTANCE, BAY_SPEED_UNLOADED)
-  end
+	if isLoaded then -- Slow retract if missile still there
+		Turn(clamp1, z_axis, 0, CLAMP_SPEED_LOADED)
+		Turn(clamp2, z_axis, 0, CLAMP_SPEED_LOADED)
+		Turn(gantry, x_axis, 0, GANTRY_SPEED_LOADED)
+		WaitForTurn(clamp1, z_axis)
+		WaitForTurn(clamp2, z_axis)
+		WaitForTurn(gantry, x_axis)
+		Move(bay, x_axis, -BAY_DISTANCE, BAY_SPEED_LOADED)
+	else
+		Turn(clamp1, z_axis, 0, CLAMP_SPEED_UNLOADED)
+		Turn(clamp2, z_axis, 0, CLAMP_SPEED_UNLOADED)
+		Turn(gantry, x_axis, 0, GANTRY_SPEED_UNLOADED)
+		WaitForTurn(clamp1, z_axis)
+		WaitForTurn(clamp2, z_axis)
+		WaitForTurn(gantry, x_axis)
+		Move(bay, x_axis, -BAY_DISTANCE, BAY_SPEED_UNLOADED)
+	end
 	WaitForMove(bay, x_axis)
-	SetAbleToMove(true)  
-  if not isLoaded then -- Only wait if reload required
-    Sleep(LOAD_DELAY) 
-  end
-	isLoaded = 1
-	Show(missile)  
+
+	if not isLoaded then -- Only wait if reload required
+		Sleep(LOAD_DELAY)
+	end
+	isLoaded = true
+	Show(missile)
 end
 
 function script.StartMoving()
-  Signal(SIG_MOVE)
-	isMoving = true  
-	StartThread(TrackControl)  
-  StartThread(Reload)  
+	Signal(SIG_MOVE)
+	isMoving = true
+	StartThread(TrackControl)
 end
 
 local function DelayStopMove()
 	SetSignalMask(SIG_MOVE)
-	Sleep(500)	
+	Sleep(500)
 	isMoving = false
 end
 
 function script.StopMoving()
-  Signal(SIG_MOVE) 
-  StartThread(DelayStopMove)
-  for i=1,#wheels do
-    StopSpin(wheels[i], x_axis, WHEEL_SPIN_DECEL)
-  end	  
+	Signal(SIG_MOVE)
+	StartThread(DelayStopMove)
+	for i = 1, #wheels do
+		StopSpin(wheels[i], x_axis, WHEEL_SPIN_DECEL)
+	end
 end
 
 local function RestoreAfterDelay()
-	Sleep(RESTORE_DELAY)  
+	Sleep(RESTORE_DELAY)
 	StartThread(Reload)
 end
 
 function script.AimWeapon(num, heading, pitch)
 	Signal(SIG_AIM)
 	SetSignalMask(SIG_AIM)
-	
-  if isMoving then
-		return false
-	end
-  
-  GG.DontFireRadar_CheckAim(unitID)
-	
+
+	GG.DontFireRadar_CheckAim(unitID)
+
 	if isLoaded then
 		StartThread(Prepare)
 		if doStrobe then
