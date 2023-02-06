2bcfc43be524d676b99d449973ec0efd96a0c8f5
sprunk <spr.ng@o2.pl>
2023-02-06 21:36:32 +0100

    ⚙️ Graceful Xray widget shutdown on potatos

--- LuaUI/Widgets/gfx_xray_shader.lua
+++ LuaUI/Widgets/gfx_xray_shader.lua
@@ -113,7 +113,10 @@ local shaderFragZMaxLoc = nil
 
 
 function widget:Shutdown()
-  glDeleteShader(shader)
+	if shader then
+		glDeleteShader(shader)
+		shader = nil
+	end
 end
 
 
@@ -177,6 +180,7 @@ function widget:Initialize()
     Spring.Log(widget:GetInfo().name, LOG.ERROR, glGetShaderLog())
     spEcho("Xray shader compilation failed.")
     widgetHandler:RemoveWidget()
+    return
   end
 
   shaderFragZMinLoc = gl.GetUniformLocation(shader, "fragZMin")
