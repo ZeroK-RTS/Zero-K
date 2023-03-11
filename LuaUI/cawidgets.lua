a8f5a7e2f4ee607a6d7e9c50c28914e27582df35
sprunk <spr.ng@o2.pl>
2023-03-11 16:14:05 +0100

    ⚙️ Add widget:UnitResurrected callin
    Partially as a simple example for modders,
    though it's useful on its own.

--- LuaUI/cawidgets.lua
+++ LuaUI/cawidgets.lua
@@ -184,6 +184,7 @@ local flexCallIns = {
 	'DefaultCommand',
 	'UnitCreated',
 	'UnitFinished',
+	'UnitResurrected',
 	'UnitReverseBuilt',
 	'UnitFromFactory',
 	'UnitDestroyed',
@@ -2237,6 +2238,12 @@ function widgetHandler:UnitCreated(unitID, unitDefID, unitTeam, builderID)
 	end
 end
 
+-- NB: called via Lua at the moment, not engine
+function widgetHandler:UnitResurrected(unitID, unitDefID, unitTeam, builderID)
+	for _, w in r_ipairs(self.UnitResurrectedList) do
+		w:UnitResurrected(unitID, unitDefID, unitTeam, builderID)
+	end
+end
 
 function widgetHandler:UnitFinished(unitID, unitDefID, unitTeam)
 	for _, w in r_ipairs(self.UnitFinishedList) do
