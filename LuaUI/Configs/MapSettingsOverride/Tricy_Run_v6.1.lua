7f4a4691268b532d3bde4926fce0146ff418999b
GoogleFrog <googl3frog@gmail.com>
2023-03-19 11:39:26 +1100

    Added some map lightning overrides I had lingering locally.

---
+++ LuaUI/Configs/MapSettingsOverride/Tricy_Run_v6.1.lua
@@ -0,0 +1,23 @@
+return {
+	["water"]= false,
+	["sun"]= {
+		["groundDiffuseColor"]= {0.53809524, 0.36190477, 0.42857143, 1},
+		["groundAmbientColor"]= {0.45238096, 0.56666666, 0.56190479, 1},
+		["unitSpecularColor"]= {0.75714284, 0.71428573, 0.72380954, 1},
+		["modelShadowDensity"]= 0.89999998,
+		["specularExponent"]= 19.539999,
+		["groundSpecularColor"]= {0.03809524, 0.06190476, 0.05714286, 1},
+		["groundShadowDensity"]= 0.75,
+		["unitDiffuseColor"]= {0.80000001, 0.78571427, 0.80000001, 1},
+		["unitAmbientColor"]= {0.81904763, 0.84285712, 0.9285714, 1},
+	},
+	["direction"]= false,
+	["fog"]= {
+		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogStart"]= 0.99000001,
+		["fogColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["cloudColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogEnd"]= 1,
+	},
+}
