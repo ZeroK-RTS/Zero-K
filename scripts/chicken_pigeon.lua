369d4327209ebc7faabac0b5a64f30942dcc21f3
sprunk <spr.ng@o2.pl>
2023-03-29 03:33:36 +0200

    ⚙️ Pigeon LUS work
     * removed head aiming. Most targets are under the Pigeon so
       this made it largely bury its head between the shoulders
    
     * remove BlockShot which blocked its weapon (FW leftover?)
    
     * rolled some functions into the caller where possible
    
     * tweak animations

--- scripts/chicken_pigeon.lua
+++ scripts/chicken_pigeon.lua
@@ -2,8 +2,6 @@ include "constants.lua"
 
 local body, head, tail, lwing, rwing, rblade, lblade = piece("body", "head", "tail", "lwing", "rwing", "rblade", "lblade")
 
-
-local SIG_AIM  = 2 -- doesn't really have an aiming animation. Why is this here?
 local SIG_MOVE = 4
 
 local function FlyThread()
@@ -19,54 +17,25 @@ local function FlyThread()
 	end
 end
 
-local function StopFlying()
-	Turn(lwing, z_axis, 0, math.rad(240))
-	Turn(rwing, z_axis, 0, math.rad(240))
-	Signal(SIG_MOVE)
-end
-
-local function RestoreAfterDelay()
-	Sleep(3000)
-	Turn(head, x_axis, 0, math.rad(100))
-	Turn(head, y_axis, 0, math.rad(100))
-end
-
-local function MoveMouth()
-	Turn(lblade, y_axis, math.rad(20), 1)
-	Turn(rblade, y_axis, math.rad(-20), 1)
-	WaitForTurn(lblade, y_axis)
-	Turn(lblade, y_axis, 0, 0.3)
-	Turn(rblade, y_axis, 0, 0.3)
-end
-
 function script.StartMoving()
 	StartThread(FlyThread)
 end
 
 function script.StopMoving()
-	StopFlying()
+	Turn(lwing, z_axis, 0, math.rad(240))
+	Turn(rwing, z_axis, 0, math.rad(240))
+	Signal(SIG_MOVE)
 end
 
 function script.Shot()
-	StartThread(MoveMouth)
+	Turn(lblade, y_axis, math.rad(45))
+	Turn(rblade, y_axis, math.rad(-45))
+	Turn(lblade, y_axis, 0, 0.3)
+	Turn(rblade, y_axis, 0, 0.3)
 end
 
 function script.AimWeapon(num, heading, pitch)
-	if num == 1 then
-		Signal(SIG_AIM)
-		SetSignalMask(SIG_AIM)
-		Turn(head, x_axis, -pitch, math.rad(200))
-		Turn(head, y_axis, heading, math.rad(200))
-		WaitForTurn(head, y_axis)
-		WaitForTurn(head, x_axis)
-		StartThread(RestoreAfterDelay)
-		return true
-	end
 	return true
-end
-
-function script.BlockShot(num)
-	return num == 2
 end
 
 function script.HitByWeapon(x, z, weaponDefID, damage)
@@ -82,7 +51,8 @@ end
 
 function script.Killed(recentDamage, maxHealth)
 	EmitSfx(body, 1025)
-	local explodables = {body, head, tail, lwing, rwing, rblade, lblade}
+	Explode(body, SFX.SHATTER)
+	local explodables = {head, tail, lwing, rwing, rblade, lblade}
 	for i = 1, #explodables do
 		Explode(explodables[i], SFX.FALL)
 	end
