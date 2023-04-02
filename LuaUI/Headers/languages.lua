a642bdbf485a8d7dd80bc996743b08c4c3e11ad9
Anarchid <anarchid@online.ee>
2023-04-02 18:28:21 +0300

    中文 ACTIVATE

--- LuaUI/Headers/languages.lua
+++ LuaUI/Headers/languages.lua
@@ -6,17 +6,6 @@ local langs = {
 	{ lang = 'tr', flag = 'tr', name = 'Türkçe' },
 }
 
---[[ On Windows, far eastern fonts are börkçe by default, and
-for them to work the engine needs to have been run with
-the `--gen-fontconfig` CLI param at least once. This is
-the job for Chobby who should run a parallel instance to
-do just that (ideally detecting whether the user's system
-language requires it by default) and leave information
-about it. ]]
-if Platform.osFamily ~= "Windows" or VFS.FileExists("LuaUI/font_config_generated", VFS.RAW) then
-	langs[#langs + 1] = { lang = 'zh', flag = 'cn', name = '中文' }
-end
-
 local flagByLang, langByFlag = {}, {}
 for i = 1, #langs do
 	local x = langs[i]
