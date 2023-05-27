fce9515eee0f0dd61fd77861e31b00fead1f2f76
sprunk <spr.ng@o2.pl>
2023-05-27 18:18:34 +0200

    ✨ Tweaks to Aegis unfold animation
     * now the petals unfold when finished. Previously they did that
       at unit creation, so this little detail was never really visible.
    
     * legs now unfold immediately. Previously they moved over a few
       seconds so it was possible to see them sliding if you were
       quick at making a nanoframe.
    
     * tweaked move distances etc so that fewer things clip into each
       other or hang mid-air.

--- scripts/staticshield.lua
+++ scripts/staticshield.lua
@@ -31,92 +31,69 @@ local rb_thigh = piece "rb_thigh"
 local rb_knee = piece "rb_knee"
 local rb_foot = piece "rb_foot"
 
---constants
-local l_angle = math.rad(40)
-local l_speed = 1
-local k_angle = math.rad(45)
-local k_speed = 10
-
 local function hover()
 	local spGetUnitShieldState = Spring.GetUnitShieldState
 	while true do
 		local shieldEnabled, shieldHealth = spGetUnitShieldState(unitID)
 		Spin(glow, y_axis, shieldEnabled and (shieldHealth / 3000) or 0)
-		Move(glow, y_axis, math.random(1,5), 2)
+		Move(glow, y_axis, math.random(11,15), 2)
 		WaitForMove(glow, y_axis)
 		Sleep (200)
-		Move(glow, y_axis, math.random(-6,-1), 2)
+		Move(glow, y_axis, math.random(4,9), 2)
+		WaitForMove(glow, y_axis)
+		Sleep (200)
 	end
 end
 
 local function initialize()
-	Move(lf_ball, y_axis, -10, 5)
-	Move(rf_ball, y_axis, -10, 5)
-	Move(lb_ball, y_axis, -10, 5)
-	Move(rb_ball, y_axis, -10, 5)
-	
-	Turn(lf_knee, x_axis, -k_angle, k_speed)
-	Turn(lf_knee, z_axis, k_angle, k_speed)
-	Turn(lb_knee, x_axis, k_angle, k_speed)
-	Turn(lb_knee, z_axis, k_angle, k_speed)
-	Turn(rf_knee, x_axis, -k_angle, k_speed)
-	Turn(rf_knee, z_axis, -k_angle, k_speed)
-	Turn(rb_knee, x_axis, k_angle, k_speed)
-	Turn(rb_knee, z_axis, -k_angle, k_speed)
-	Sleep(100)
-	
+	local spGetUnitHealth = Spring.GetUnitHealth
+	local sel = select
+	while sel(5, spGetUnitHealth(unitID)) < 1 do
+		Sleep(100)
+	end
+
+	Move(glow, y_axis, 10, 4)
+	Sleep(500)
+
+	-- FIXME: rotations don't stack sensibly, needs the solar cardinal rotation hax
+	Turn(lf_leaf, x_axis, math.rad( 40), math.rad(15))
+	Turn(lf_leaf, z_axis, math.rad(-40), math.rad(15))
+	Turn(rf_leaf, x_axis, math.rad( 40), math.rad(15))
+	Turn(rf_leaf, z_axis, math.rad( 40), math.rad(15))
+	Turn(lb_leaf, x_axis, math.rad(-40), math.rad(15))
+	Turn(lb_leaf, z_axis, math.rad(-40), math.rad(15))
+	Turn(rb_leaf, x_axis, math.rad(-40), math.rad(15))
+	Turn(rb_leaf, z_axis, math.rad( 40), math.rad(15))
+
+	WaitForMove(glow, y_axis)
+	WaitForTurn(lf_leaf, x_axis)
 	StartThread(hover)
 end
-	
-	
+
 function script.Create()
 	Spring.SetUnitRulesParam(unitID, "unitActiveOverride", 1)	-- don't lose jitter effect with on/off button
-	Turn(lf_leaf, x_axis, l_angle, l_speed)
-	Turn(lf_leaf, z_axis, -l_angle, l_speed)
-	
-	Turn(rf_leaf, x_axis, l_angle, l_speed)
-	Turn(rf_leaf, z_axis, l_angle, l_speed)
-	
-	Turn(lb_leaf, x_axis, -l_angle, l_speed)
-	Turn(lb_leaf, z_axis, -l_angle, l_speed)
-	
-	Turn(rb_leaf, x_axis, -l_angle, l_speed)
-	Turn(rb_leaf, z_axis, l_angle, l_speed)
-	
+
+	-- FIXME: these should be reflected in the model (building ghost mismatch)
+	Move(base, y_axis, -8)
+	Turn(lf_knee, x_axis, math.rad(-45))
+	Turn(lf_knee, z_axis, math.rad( 45))
+	Turn(lb_knee, x_axis, math.rad( 45))
+	Turn(lb_knee, z_axis, math.rad( 45))
+	Turn(rf_knee, x_axis, math.rad(-45))
+	Turn(rf_knee, z_axis, math.rad(-45))
+	Turn(rb_knee, x_axis, math.rad( 45))
+	Turn(rb_knee, z_axis, math.rad(-45))
+
 	StartThread(GG.Script.SmokeUnit, unitID, {glow})
 	StartThread(initialize)
 end
 
 function script.Activate()
-	--Turn(lf_leaf, x_axis, l_angle, l_speed)
-	--Turn(lf_leaf, z_axis, -l_angle, l_speed)
-	--
-	--Turn(rf_leaf, x_axis, l_angle, l_speed)
-	--Turn(rf_leaf, z_axis, l_angle, l_speed)
-	--
-	--Turn(lb_leaf, x_axis, -l_angle, l_speed)
-	--Turn(lb_leaf, z_axis, -l_angle, l_speed)
-	--
-	--Turn(rb_leaf, x_axis, -l_angle, l_speed)
-	--Turn(rb_leaf, z_axis, l_angle, l_speed)
-	
 	Spring.SetUnitRulesParam(unitID, "shieldChargeDisabled", 0, ALLY_ACCESS)
 	--spSetUnitShieldState(unitID, 1, true)
 end
 
 function script.Deactivate()
-	--Turn(lf_leaf, x_axis, 0, 1)
-	--Turn(lf_leaf, z_axis, 0, 1)
-	--
-	--Turn(rf_leaf, x_axis, 0, 1)
-	--Turn(rf_leaf, z_axis, 0, 1)
-	--
-	--Turn(lb_leaf, x_axis, 0, 1)
-	--Turn(lb_leaf, z_axis, 0, 1)
-	--
-	--Turn(rb_leaf, x_axis, 0, 1)
-	--Turn(rb_leaf, z_axis, 0, 1)
-	
 	Spring.SetUnitRulesParam(unitID, "shieldChargeDisabled", 1, ALLY_ACCESS)
 	--spSetUnitShieldState(unitID, 1, false)
 end
