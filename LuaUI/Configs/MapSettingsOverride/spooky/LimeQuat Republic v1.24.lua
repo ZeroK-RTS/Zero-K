d096936348fd95d52c0948acb7e81e202003ed39
sprunk <spr.ng@o2.pl>
2023-10-07 19:42:10 +0200

    ⚙️ Add spooky map configs

---
+++ LuaUI/Configs/MapSettingsOverride/spooky/LimeQuat Republic v1.24.lua
@@ -0,0 +1,33 @@
+return {
+	["water"]= false,
+	["sun"]= {
+		["groundDiffuseColor"]= {0.69849247, 0.33668342, 0, 1},
+		["groundAmbientColor"]= {0.21105528, 0.03015075, 0, 1},
+		["unitSpecularColor"]= {0.69999999, 0.39698493, 0, 1},
+		["modelShadowDensity"]= 1,
+		["specularExponent"]= 50,
+		["groundSpecularColor"]= {1, 0, 0, 1},
+		["groundShadowDensity"]= 1,
+		["unitDiffuseColor"]= {0.69999999, 0.31658292, 0, 1},
+		["unitAmbientColor"]= {0.5, 0, 0, 1},
+	},
+	["direction"]= {
+		["sunDir"]= 2.58999991,
+		["sunPitch"]= 1.05999994,
+	},
+	["fog"]= {
+		["sunColor"]= {0.05238095, 0, 0, 0.87142855},
+		["fogStart"]= 0.85999995,
+		["fogColor"]= {
+			[0] = 0,
+			[1] = 0.15238096,
+			[2] = 0.05238095,
+			[3] = 0.05714286,
+			[4] = 0,
+			[5] = 0,
+		},
+		["cloudColor"]= {0.05714286, 0, 0, 0.80000001, 0.96666664},
+		["skyColor"]= {0.07142857, 0, 0, 0.99523807},
+		["fogEnd"]= -0.55,
+	},
+}
