a75651c8c6873ef9ca6c89345a3f0b63f8f21f0f
GoogleFrog <googl3frog@gmail.com>
2023-11-13 22:47:37 +1100

    Fix sun on Mercurial and Rogues River.

---
+++ LuaUI/Configs/MapSettingsOverride/Mercurial v1.1.lua
@@ -0,0 +1,23 @@
+return {
+	["sun"]= {
+		["groundDiffuseColor"]= {0.34999999, 0.34999999, 0.34999999, 1},
+		["groundAmbientColor"]= {0.69999999, 0.60000002, 0.69999999, 1},
+		["unitSpecularColor"]= {0.50476193, 0.50476193, 0.48571429, 1},
+		["modelShadowDensity"]= 0.80000001,
+		["specularExponent"]= 31.6099987,
+		["groundSpecularColor"]= {0, 0, 0, 1},
+		["groundShadowDensity"]= 0.80000001,
+		["unitDiffuseColor"]= {0.51428574, 0.49047619, 0.5, 1},
+		["unitAmbientColor"]= {0.64285713, 0.60952383, 0.58095241, 1},
+	},
+	["fixDefaultWater"]= true,
+	["direction"]= false,
+	["fog"]= {
+		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogStart"]= 0.99000001,
+		["fogColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["cloudColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogEnd"]= 1,
+	},
+}
---
+++ LuaUI/Configs/MapSettingsOverride/Rogues River v1.2.lua
@@ -0,0 +1,23 @@
+return {
+	["water"]= false,
+	["sun"]= {
+		["groundDiffuseColor"]= {0.34999999, 0.34999999, 0.34999999, 1},
+		["groundAmbientColor"]= {0.60000002, 0.60000002, 0.80000001, 1},
+		["unitSpecularColor"]= {0.62380952, 0.61904764, 0.6047619, 1},
+		["modelShadowDensity"]= 0.80000001,
+		["specularExponent"]= 17.2399998,
+		["groundSpecularColor"]= {0, 0, 0, 1},
+		["groundShadowDensity"]= 0.80000001,
+		["unitDiffuseColor"]= {0.8142857, 0.78571427, 0.69999999, 1},
+		["unitAmbientColor"]= {0.67142856, 0.65714288, 0.80000001, 1},
+	},
+	["direction"]= false,
+	["fog"]= {
+		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogStart"]= 0.99000001,
+		["fogColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["cloudColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogEnd"]= 1,
+	},
+}
