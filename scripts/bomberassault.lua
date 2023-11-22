05bfefd8506c8293aa70f6c561dbe9becabfc474
Анархид <anarchid@online.ee>
2023-11-22 18:08:26 +0200

    restore Odin liftoff sound in fog-safe manner

--- scripts/bomberassault.lua
+++ scripts/bomberassault.lua
@@ -85,17 +85,19 @@ local function IsManualFireTargetValid()
 	return false
 end
 
---function script.Deactivate()
---	StopSpin(turbineTop, z_axis, 0.5);
---	StopSpin(turbineLeft, z_axis, 0.5);
---	StopSpin(turbineRight, z_axis, 0.5);
---end
+function script.Deactivate()
+	StopSpin(turbineTop, y_axis, 0.5);
+	StopSpin(turbineLeft, y_axis, 0.5);
+	StopSpin(turbineRight, y_axis, 0.5);
+end
 
---function script.Activate()
---	Spin(turbineTop, z_axis, 8,2);
---	Spin(turbineLeft, z_axis, 8,2);
---	Spin(turbineRight, z_axis, -8,2);
---end
+function script.Activate()
+	local px, py, pz = Spring.GetUnitPosition(unitID)
+	GG.PlayFogHiddenSound("sounds/misc/blowtorch.wav", 10, px, py, pz)
+	--Spin(turbineTop, y_axis, 8,2);
+	--Spin(turbineLeft, y_axis, 8,2);
+	--Spin(turbineRight, y_axis, -8,2);
+end
 
 function script.Create()
 	StartThread(GG.Script.SmokeUnit, unitID, smokePiece)
