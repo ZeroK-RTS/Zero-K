3c0f60d60d04a15c57d74b51f0b3960f2e2731f1
GoogleFrog <googl3frog@gmail.com>
2023-12-06 19:04:31 +1100

    Thermite held in the air by a shield no longer goes through shields if the shield moves towards it. This is done by pushing projectiles that end up inside a shield to the outside while adjusting velocities slightly so the right trajectory is maintained.

--- LuaRules/Gadgets/weapon_noexplode_speed_damage.lua
+++ LuaRules/Gadgets/weapon_noexplode_speed_damage.lua
@@ -19,6 +19,17 @@ local IterableMap = VFS.Include("LuaRules/Gadgets/Include/IterableMap.lua")
 local activeProjectiles = IterableMap.New()
 local inGameFrameLoop = false
 
+local shieldPieceByUnitDef  = {}
+local shieldRadiusByUnitDef = {}
+local shieldPieceByUnit  = {}
+local shieldRadiusByUnit = {}
+
+local SHIELD_PUSH_VERT_FACTOR = 3
+
+local g_CHAR = string.byte('g')
+local u_CHAR = string.byte('u')
+local f_CHAR = string.byte('f')
+
 for i = 1,#WeaponDefs do
 	local wcp = WeaponDefs[i].customParams
 	if wcp and (wcp.noexplode_speed_damage or wcp.thermite_frames) then
@@ -30,7 +41,7 @@ for i = 1,#WeaponDefs do
 			hitSound = wcp.thermite_sound_hit,
 		}
 		if wcp.thermite_dps_start and wcp.thermite_dps_end then
-			local damage = WeaponDefs[i].damages[0]
+			local damage = WeaponDefs[i].damages[1]
 			handledDefs[i].initDamageMod = wcp.thermite_dps_start / damage / 30
 			handledDefs[i].damageModPerFrame = (wcp.thermite_dps_end - wcp.thermite_dps_start) / wcp.thermite_frames / damage / 30
 			handledDefs[i].baseDamage = damage
@@ -41,17 +52,126 @@ for i = 1,#WeaponDefs do
 	end
 end
 
+local function GetShieldRadius(unitID)
+	local unitDefID = Spring.GetUnitDefID(unitID)
+end
+
+local function CacheShieldPieceAndRadius(unitID, unitDefID)
+	if shieldPieceByUnitDef[unitDefID] then
+		shieldPieceByUnit[unitID]  = shieldPieceByUnitDef[unitDefID]
+		shieldRadiusByUnit[unitID] = shieldRadiusByUnitDef[unitDefID]
+		return
+	end
+	local ud = UnitDefs[unitDefID]
+	local shieldWeaponDefID, shieldNum
+	if ud.customParams.dynamic_comm then
+		if GG.Upgrades_UnitShieldDef then
+			shieldWeaponDefID, shieldNum = GG.Upgrades_UnitShieldDef(unitID)
+		end
+	else
+		shieldWeaponDefID = ud.shieldWeaponDef
+		for i = 1, #ud.weapons do
+			if ud.weapons[i].weaponDef == shieldWeaponDefID then
+				shieldNum = i
+				break
+			end
+		end
+	end
+	
+	if shieldWeaponDefID then
+		local shieldWep = WeaponDefs[shieldWeaponDefID]
+		shieldRadiusByUnit[unitID] = shieldWep.shieldRadius
+	else
+		shieldRadiusByUnit[unitID] = 80
+	end
+	if shieldNum then
+		local env = Spring.UnitScript.GetScriptEnv(unitID)
+		if env.script.QueryWeapon then
+			shieldPieceByUnit[unitID] = env.script.QueryWeapon(shieldNum)
+		end
+	end
+	
+	if not ud.customParams.dynamic_comm then -- Comms are dynamic
+		shieldPieceByUnitDef[unitDefID]  = shieldPieceByUnit[unitID]
+		shieldRadiusByUnitDef[unitDefID] = shieldRadiusByUnit[unitID]
+	end
+end
+
+local function GetShieldParameters(unitID)
+	local unitDefID = Spring.GetUnitDefID(unitID)
+	if not shieldRadiusByUnit[unitID] then
+		CacheShieldPieceAndRadius(unitID, unitDefID)
+	end
+	local shieldPiece = shieldPieceByUnit[unitID]
+	local sx, sy, sz
+	if shieldPiece then
+		sx, sy, sz = Spring.GetUnitPiecePosDir(unitID, shieldPiece)
+	else
+		sx, sy, sz = Spring.GetUnitPosition(unitID)
+	end
+	return sx, sy, sz, shieldRadiusByUnit[unitID]
+end
+
+local function SnapToShieldHeight(shieldCarrierUnitID, px, py, pz, vx, vy, vz, targetYpos)
+	if not Spring.ValidUnitID(shieldCarrierUnitID) then
+		return px, py, pz, vx, vy, vz
+	end
+	local sx, sy, sz, radius = GetShieldParameters(shieldCarrierUnitID)
+	if not sx then
+		return px, py, pz, vx, vy, vz
+	end
+	local dx, dy, dz = sx - px, sy - py, sz - pz
+	if dy > 0 then
+		-- Don't bother handling shields that smother thermite
+		return px, py, pz, vx, vy, vz
+	end
+	local delta = 0.2
+	local ox, oy, oz = px, py, pz
+	while math.sqrt(dx*dx + dy*dy + dz*dz) < radius + 1 do
+		px = px + delta*vx/vy
+		py = py + delta * SHIELD_PUSH_VERT_FACTOR
+		pz = pz + delta*vz/vy
+		dx, dy, dz = sx - px, sy - py, sz - pz
+	end
+	
+	if oy ~= py then
+		-- The assumption here is that by pushing the projectile upwards we have increased
+		-- flight time, so horizontal velocity should be reduced by an appropriate factor
+		-- to keep the projectile on target.
+		targetYpos = targetYpos or (Spring.GetGroundHeight(px, pz) or py)
+		local heightFactor = math.max(50, py - targetYpos)
+		local factor = heightFactor / ((py - oy) / SHIELD_PUSH_VERT_FACTOR + heightFactor)
+		vx = vx*factor
+		vz = vz*factor
+	end
+	return px, py, pz, vx, vy, vz
+end
+
 function gadget:ProjectileCreated(proID, proOwnerID, weaponDefID)
 	if not (weaponDefID and handledDefs[weaponDefID]) then
 		return
 	end
 	local def = handledDefs[weaponDefID]
 	local px, py, pz = Spring.GetProjectilePosition(proID)
+	
+	local targetType, target = Spring.GetProjectileTarget(proID)
+	local targetYpos
+	if targetType == u_CHAR then
+		local tx, ty, tz = Spring.GetUnitPosition(target)
+		targetYpos = ty
+	elseif targetType == g_CHAR then
+		targetYpos = target[2]
+	elseif targetType == f_CHAR then
+		local tx, ty, tz = Spring.GetFeaturePosition(target)
+		targetYpos = ty
+	end
+	
 	local proData = {
 		px = px,
 		py = py,
 		pz = pz,
 		def = def,
+		targetYpos = targetYpos,
 		damageMod = def.initDamageMod or 1,
 		killFrame = def.thermiteFrames and (def.thermiteFrames + Spring.GetGameFrame()),
 	}
@@ -80,6 +200,12 @@ function gadget:ShieldPreDamaged(projectileID, proOwnerID, shieldEmitterWeaponNu
 	end
 	Spring.SetUnitShieldState(shieldCarrierUnitID, -1, true, charge - fullDamage)
 	proData.resetNextFrame = true
+	if proData.resetNextFrameShield then
+		proData.resetNextFrameShield = {proData.resetNextFrameShield}
+		proData.resetNextFrameShield[#proData.resetNextFrameShield + 1] = shieldCarrierUnitID
+	else
+		proData.resetNextFrameShield = shieldCarrierUnitID
+	end
 	if GG.Lups_DoShieldDamage then
 		GG.Lups_DoShieldDamage(shieldCarrierUnitID, fullDamage, hitX, hitY, hitZ)
 	end
@@ -118,11 +244,22 @@ local function UpdateProjectile(proID, proData, index, frame)
 	end
 	if def.thermiteFrames then
 		local height = Spring.GetGroundHeight(px, pz) or py
-		local vx, vy, vz = Spring.GetProjectileVelocity(proID)
+		local vx, vy, vz, speed = Spring.GetProjectileVelocity(proID)
 		if proData.resetNextFrame then
 			if def.hitSound and not proData.playedHitAlready then
 				Spring.PlaySoundFile(def.hitSound, 14, px, py, pz, 'sfx')
 				proData.playedHitAlready = true
+			end
+			px, py, pz = proData.px, proData.py, proData.pz
+			if proData.resetNextFrameShield then
+				if type(proData.resetNextFrameShield) == "table" then
+					for i = 1, #proData.resetNextFrameShield do
+						px, py, pz, vx, vy, vz = SnapToShieldHeight(proData.resetNextFrameShield[i], px, py, pz, vx, vy, vz, proData.targetYpos)
+					end
+				else
+					px, py, pz, vx, vy, vz = SnapToShieldHeight(proData.resetNextFrameShield, px, py, pz, vx, vy, vz, proData.targetYpos)
+				end
+				proData.resetNextFrameShield = false
 			end
 			if py < height + 4 then
 				Spring.SetProjectileVelocity(proID, 0, 0, 0)
@@ -130,7 +267,6 @@ local function UpdateProjectile(proID, proData, index, frame)
 				-- Counteract gravity
 				Spring.SetProjectileVelocity(proID, vx, vy + 0.08, vz)
 			end
-			px, py, pz = proData.px, proData.py, proData.pz
 			Spring.SetProjectilePosition(proID, px, py, pz)
 			proData.resetNextFrame = false
 			if def.sound then
@@ -172,6 +308,11 @@ function gadget:GameFrame(n)
 	if alreadyTakenDamage then
 		alreadyTakenDamage = false
 	end
+end
+
+function gadget:UnitDestroyed(unitID)
+	shieldPieceByUnit[unitID]  = nil
+	shieldRadiusByUnit[unitID] = nil
 end
 
 function gadget:Initialize()
