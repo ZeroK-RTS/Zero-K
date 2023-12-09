68d50ab09f120a605aefe9b6d9e56734ae9213cc
GoogleFrog <googl3frog@gmail.com>
2023-12-09 14:56:08 +1100

    Improve sun on Small Divide Remake and LLTA Complex.

---
+++ LuaUI/Configs/MapSettingsOverride/LLTAComplexV2.lua
@@ -0,0 +1,27 @@
+return {
+	["water"]= false,
+	["sun"]= {
+		["groundDiffuseColor"]= {0.75, 0.75, 0.75, 1},
+		["groundAmbientColor"]= {0.5, 0.5, 0.5, 1},
+		["unitSpecularColor"]= {0.50476193, 0.56190479, 0.74285716, 1},
+		["modelShadowDensity"]= 0.2,
+		["specularExponent"]= 19.539999,
+		["groundSpecularColor"]= {0, 0, 0, 1},
+		["groundShadowDensity"]= 0.80000001,
+		["unitDiffuseColor"]= {0.62380952, 0.66666669, 0.6761905, 1},
+		["unitAmbientColor"]= {0.69523811, 0.67142856, 0.67142856, 1},
+	},
+	["fog"]= {
+		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogStart"]= 0.99000001,
+		["fogColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["cloudColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogEnd"]= 1,
+	},
+	["fixDefaultWater"]= true,
+	["direction"]= {
+		["sunDir"]= 1.94999993,
+		["sunPitch"]= 1.19999993,
+	},
+}
---
+++ LuaUI/Configs/MapSettingsOverride/Small_Divide-Remake-v04.lua
@@ -0,0 +1,27 @@
+return {
+	["water"]= false,
+	["sun"]= {
+		["groundDiffuseColor"]= {0.48571429, 0.4952381, 0.49047619, 1},
+		["groundAmbientColor"]= {0.63333333, 0.62380952, 0.6047619, 1},
+		["unitSpecularColor"]= {0.72000003, 0.69999999, 0.52999997, 1},
+		["modelShadowDensity"]= 0.81,
+		["specularExponent"]= 36.2099991,
+		["groundSpecularColor"]= {0, 0, 0, 1},
+		["groundShadowDensity"]= 1,
+		["unitDiffuseColor"]= {0.72000003, 0.69999999, 0.52999997, 1},
+		["unitAmbientColor"]= {0.41999999, 0.41, 0.51999998, 1},
+	},
+	["fog"]= {
+		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogStart"]= 0.99000001,
+		["fogColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["cloudColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogEnd"]= 1,
+	},
+	["fixDefaultWater"]= true,
+	["direction"]= {
+		["sunDir"]= 0.71999997,
+		["sunPitch"]= 1.04999995,
+	},
+}
