9938b0636162b4bfdf4bfa5f84f58c4264f6972b
GoogleFrog <googl3frog@gmail.com>
2023-12-31 11:04:47 +1100

    Improved lighting on Izki Channel v1.0 and MiniChess_v2.

---
+++ LuaUI/Configs/MapSettingsOverride/MiniChess_v2.lua
@@ -0,0 +1,27 @@
+return {
+	["water"]= false,
+	["sun"]= {
+		["groundDiffuseColor"]= {0.64761907, 0.63333333, 0.80476189, 1},
+		["groundAmbientColor"]= {0.71904761, 0.7095238, 0.69047618, 1},
+		["unitSpecularColor"]= {1, 1, 1, 1},
+		["modelShadowDensity"]= 1,
+		["specularExponent"]= 17.8199997,
+		["groundSpecularColor"]= {0, 0, 0, 1},
+		["groundShadowDensity"]= 1,
+		["unitDiffuseColor"]= {0.64761907, 0.63333333, 0.62857145, 1},
+		["unitAmbientColor"]= {0.74285716, 0.75238097, 0.71904761, 1},
+	},
+	["fog"]= {
+		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogStart"]= 0.99000001,
+		["fogColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["cloudColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogEnd"]= 1,
+	},
+	["fixDefaultWater"]= true,
+	["direction"]= {
+		["sunDir"]= 5.62999964,
+		["sunPitch"]= 0.88,
+	},
+}
