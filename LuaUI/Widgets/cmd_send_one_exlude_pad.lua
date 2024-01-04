2b9b5f585edd1f3950fcac8d61cc7df8c7d421fb
Helwor <helwor@yahoo.com>
2024-01-05 00:29:59 +0100

    Update cmd_send_one_exlude_pad.lua
    removed Echo and opts coding

--- LuaUI/Widgets/cmd_send_one_exlude_pad.lua
+++ LuaUI/Widgets/cmd_send_one_exlude_pad.lua
@@ -10,7 +10,7 @@ function widget:GetInfo()
 		handler   = true,
 	}
 end
-local Echo 				= Spring.Echo
+
 local spGiveOrderToUnit 		= Spring.GiveOrderToUnit
 local spGetSelectedUnitsSorted 		= Spring.GetSelectedUnitsSorted
 
@@ -26,20 +26,6 @@ do
 	local customCmds = VFS.Include("LuaRules/Configs/customcmds.lua")
 	CMD_EXCLUDE_PAD = customCmds.EXCLUDE_PAD
 end
-local GetOptionsCode
-do
-	local code={meta=CMD.OPT_META, internal=CMD.OPT_INTERNAL, right=CMD.OPT_RIGHT, shift=CMD.OPT_SHIFT, ctrl=CMD.OPT_CTRL, alt=CMD.OPT_ALT} -- 4, 8, 16, 32, 64, 128
-	GetOptionsCode = function(options)
-		local coded = 0
-		for opt, isTrue in pairs(options) do
-		    if isTrue then 
-			coded = coded + (code[opt] or 0)
-		    end
-		end
-		options.coded = coded
-		return coded
-	end
-end
 
 function widget:CommandNotify(cmd, params, opts)
 	if cmd ~= CMD_EXCLUDE_PAD then
@@ -48,7 +34,7 @@ function widget:CommandNotify(cmd, params, opts)
 	local selTypes = WG.selectionDefID or spGetSelectedUnitsSorted()
 	for defID, units in pairs(selTypes) do
 		if airUnitDefID[defID] then
-			spGiveOrderToUnit(units[1], cmd, params, opts.coded or GetOptionsCode(opts))
+			spGiveOrderToUnit(units[1], cmd, params, opts)
 			return true
 		end
 	end
