af9646c48133feb8c2137a115b351bab8efcca4f
Helwor <helwor@yahoo.com>
2024-01-04 02:47:06 +0100

    cmd_send_one_exlude_pad.lua fixed wrong option code and wrong WG var
    Also the filename miss the 'c' in 'exlude'.

--- LuaUI/Widgets/cmd_send_one_exlude_pad.lua
+++ LuaUI/Widgets/cmd_send_one_exlude_pad.lua
@@ -10,6 +10,9 @@ function widget:GetInfo()
 		handler   = true,
 	}
 end
+local Echo 				= Spring.Echo
+local spGiveOrderToUnit 		= Spring.GiveOrderToUnit
+local spGetSelectedUnitsSorted 		= Spring.GetSelectedUnitsSorted
 
 local airUnitDefID = {}
 for defID, def in pairs(UnitDefs) do
@@ -23,15 +26,29 @@ do
 	local customCmds = VFS.Include("LuaRules/Configs/customcmds.lua")
 	CMD_EXCLUDE_PAD = customCmds.EXCLUDE_PAD
 end
+local GetOptionsCode
+do
+	local code={meta=CMD.OPT_META, internal=CMD.OPT_INTERNAL, right=CMD.OPT_RIGHT, shift=CMD.OPT_SHIFT, ctrl=CMD.OPT_CTRL, alt=CMD.OPT_ALT} -- 4, 8, 16, 32, 64, 128
+	GetOptionsCode = function(options)
+		local coded = 0
+		for opt, isTrue in pairs(options) do
+		    if isTrue then 
+			coded = coded + (code[opt] or 0)
+		    end
+		end
+		options.coded = coded
+		return coded
+	end
+end
 
-function widget:CommandNotify(cmd,params,opts)
+function widget:CommandNotify(cmd, params, opts)
 	if cmd ~= CMD_EXCLUDE_PAD then
 		return false
 	end
-	local selTypes = WG.selectionMap or Spring.GetSelectedUnitsSorted()
-	for defID, t in pairs(selTypes) do
+	local selTypes = WG.selectionDefID or spGetSelectedUnitsSorted()
+	for defID, units in pairs(selTypes) do
 		if airUnitDefID[defID] then
-			Spring.GiveOrderToUnit(t[1],cmd,params,opts)
+			spGiveOrderToUnit(units[1], cmd, params, opts.coded or GetOptionsCode(opts))
 			return true
 		end
 	end
