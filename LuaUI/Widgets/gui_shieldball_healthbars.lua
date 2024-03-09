2a2294172e90cc4c2da06b6c54dd2e372403b180
danfireman <dyth68@yahoo.com>
2024-03-09 22:39:51 +0000

    Adding regen and team awareness to shieldball healthbars widget

--- LuaUI/Widgets/gui_shieldball_healthbars.lua
+++ LuaUI/Widgets/gui_shieldball_healthbars.lua
@@ -11,6 +11,7 @@ function widget:GetInfo()
 end
 
 local UPDATE_FRAME=7
+
 local glCallList = gl.CallList
 local glPushMatrix = gl.PushMatrix
 local glTranslate = gl.Translate
@@ -25,11 +26,15 @@ local glPolygonMode = gl.PolygonMode
 local glBeginEnd = gl.BeginEnd
 local glLineWidth = gl.LineWidth
 local glColor = gl.Color
+
 local spGetUnitPosition = Spring.GetUnitPosition
 local spGetUnitShieldState = Spring.GetUnitShieldState
 local spGetUnitsInCylinder = Spring.GetUnitsInCylinder
 local spGetUnitDefID = Spring.GetUnitDefID
 local spGetUnitHealth = Spring.GetUnitHealth
+local spGetUnitRulesParam = Spring.GetUnitRulesParam
+
+local strFormat = string.format
 
 local Optics = VFS.Include("LuaRules/Gadgets/Include/Optics.lua")
 ---------------------------------
@@ -56,6 +61,19 @@ for unitDefID=1, #UnitDefs do
 end
 ---------------------------------
 
+local function regenStr(regen)
+	local sign = (regen >= 0) and "+" or ""
+	if regen == 0 then
+		return ""
+	end
+	if math.abs(math.ceil(regen) - regen) < 0.05 then
+		return " (" .. sign .. math.ceil(regen - 0.2) .. ")"
+	end
+	return " (" .. sign .. strFormat("%+.1f", regen) .. ")"
+end
+
+---------------------------------
+
 local BASE_FONT_SIZE = 192
 local healthBarOffsetZ = 300
 local healthBarHeight = 30
@@ -77,10 +95,10 @@ local function DrawHullVertices(hull)
 end
 
 local function DrawBallHealthbar()
-	for teamID, teamBalls in pairs(shieldBalls) do
+	for allyTeamID, teamBalls in pairs(shieldBalls) do
+		--local isAllied = Spring.AreTeamsAllied(teamID, Spring.GetMyTeamID())
+		local isAllied = (Spring.GetLocalAllyTeamID() == allyTeamID)
 		for _, shieldBall in pairs(teamBalls) do
-			glColor(0.8, 0.8, 0.8, 0.5)
-			glPolygonMode(GL.FRONT_AND_BACK, GL.FILL)
 			local healthBarHeightAboveTheZero = shieldBall.highestTopOfShield + 40
 			local healthBarOffsetZ = shieldBall.zStdDev * 2 + 60
 			-- The outer bar is actually an Octagon to give the impression of rounded corners
@@ -94,8 +112,20 @@ local function DrawBallHealthbar()
 				{x = shieldBall.x - healthBarWidth, y = healthBarHeightAboveTheZero, z = shieldBall.z - (healthBarOffsetZ + healthBarRoundiness)},
 				{x = shieldBall.x - healthBarWidth, y = healthBarHeightAboveTheZero, z = shieldBall.z - (healthBarOffsetZ + healthBarHeight - healthBarRoundiness)},
 			}
+			glPolygonMode(GL.FRONT_AND_BACK, GL.LINE)
+			glColor(0.5, 0.5, 0.5, 0.5)
+			for i = 1, 8 do
+				local j = (i % 8) + 1
+				glBeginEnd(GL.LINE_STRIP, DrawHullVertices, {healthBarRoundedCorners[i], healthBarRoundedCorners[j]})
+			end
+			if isAllied then
+				glColor(0.5, 0.8, 0.5, 0.5)
+			else
+				glColor(0.8, 0.5, 0.5, 0.5)
+			end
+			glPolygonMode(GL.FRONT_AND_BACK, GL.FILL)
 			glBeginEnd(GL.TRIANGLE_FAN, DrawHullVertices, healthBarRoundedCorners)
-			glColor(0.5, 0.5, 1.0, 0.5)
+
 			local innerBarWidthWhenFull = healthBarWidth - healthBarMargin
 			local innerBarHeight = healthBarHeight - healthBarMargin
 			local proportionFull = shieldBall.currShield / shieldBall.totShield
@@ -106,13 +136,20 @@ local function DrawBallHealthbar()
 				{ x = shieldBall.x - innerBarWidthWhenFull + innerBarWidth, y = healthBarHeightAboveTheZero, z = shieldBall.z - (healthBarOffsetZ + healthBarMargin)},
 				{ x = shieldBall.x - innerBarWidthWhenFull, y = healthBarHeightAboveTheZero, z = shieldBall.z - (healthBarOffsetZ + healthBarMargin)},
 			}
+			if isAllied then
+				glColor(0.5, 0.3, 1.0, 0.5)
+			else
+				glColor(0.5, 0.3, 1.0, 0.5)
+			end
 			glBeginEnd(GL.TRIANGLE_FAN, DrawHullVertices, innerHealthBarCorners)
 			local fontSize = BASE_FONT_SIZE / 10
 			--glScale(fontSize / BASE_FONT_SIZE, fontSize / BASE_FONT_SIZE, fontSize / BASE_FONT_SIZE)
 			glPushMatrix()
 			glTranslate(shieldBall.x, healthBarHeightAboveTheZero, shieldBall.z - healthBarOffsetZ - innerBarHeight/2 - healthBarMargin/2)
 			glRotate(-90, 1, 0, 0)
-			local shieldHpText = tostring(math.floor(shieldBall.currShield)) .. " / " .. tostring(shieldBall.totShield)
+
+			local regen = regenStr(shieldBall.regen)
+			local shieldHpText = tostring(math.floor(shieldBall.currShield)) .. " / " .. tostring(shieldBall.totShield) .. regen
 			font:Begin()
 				font:SetTextColor(0.2, 0.2, 0.2, 0.6)
 				font:Print(shieldHpText, 0, 0, fontSize, "cv")
@@ -141,8 +178,14 @@ local shieldBallsIdsByTeam = {}
 local allShieldUnitsByTeam = {}
 
 local function updateClustering()
-	for _, teamID in pairs(Spring.GetTeamList()) do
-		local allUnits = Spring.GetTeamUnits(teamID)
+	for _, allyTeamID in pairs(Spring.GetAllyTeamList()) do
+		local allUnits = {}
+		for _, teamID in pairs(Spring.GetTeamList(allyTeamID)) do
+			local teamUnits = Spring.GetTeamUnits(teamID)
+			for _, unitID in pairs(teamUnits) do
+				allUnits[#allUnits + 1] = unitID
+			end
+		end
 		local allShieldUnits = {}
 		for _,unitID in pairs(allUnits) do
 			local unitDefID = spGetUnitDefID(unitID)
@@ -189,14 +232,42 @@ local function updateClustering()
 		--printThing(unitNeighborsMatrix, "unitNeighborsMatrix", "")
 		local opticsObject = Optics.new(unitLocations, unitNeighborsMatrix, 2, false)
 		opticsObject:Run()
-		shieldBallsIdsByTeam[teamID] = opticsObject:Clusterize(700)
-		allShieldUnitsByTeam[teamID] = allShieldUnits
+		shieldBallsIdsByTeam[allyTeamID] = opticsObject:Clusterize(700)
+		allShieldUnitsByTeam[allyTeamID] = allShieldUnits
 	end
 end
 
 ---------------------------------
+local function getUnitShieldRegen(unitID, ud)
+	if spGetUnitRulesParam(unitID, "att_shieldDisabled") == 1 then
+		return 0
+	end
+
+	local shieldRegen = spGetUnitRulesParam(unitID, "shieldRegenTimer")
+	if shieldRegen and shieldRegen > 0 then
+		return 0
+	end
+
+	local mult = spGetUnitRulesParam(unitID,"totalReloadSpeedChange") or 1 * (1 - (spGetUnitRulesParam(unitID, "shieldChargeDisabled") or 0))
+	if mult == 0 then
+		return 0
+	end
+
+	-- FIXME: take energy stall into account
+	local wd = WeaponDefs[ud.shieldWeaponDef]
+	local wdc = wd.customParams
+	local regen = (wdc.shield_rate_charge and spGetUnitRulesParam(unitID, "shield_rate_override") and
+			math.floor(spGetUnitRulesParam(unitID, "shield_rate_override")*15 + 0.5)) or
+			tonumber(wdc.shield_rate or wd.shieldPowerRegen)
+	if not wd.customParams.slow_immune then
+		regen = mult * regen
+	end
+	return regen
+end
+
+---------------------------------
 local function updateCurrentShieldBalls()
-	for _, teamID in pairs(Spring.GetTeamList()) do
+	for _, teamID in pairs(Spring.GetAllyTeamList()) do
 		local shieldBallsIds = shieldBallsIdsByTeam[teamID] or {}
 		local allShieldUnits = allShieldUnitsByTeam[teamID] or {}
 		local ballsInTeam = {}
@@ -204,6 +275,7 @@ local function updateCurrentShieldBalls()
 			local thisBall = shieldBallsIds[i]
 			local totalCurrentShield = 0
 			local totalMaxShield = 0
+			local totalRegen = 0
 			local x_avg, z_avg = 0, 0
 			local numUnits = 0
 			local highestTopOfShield = 0
@@ -218,9 +290,12 @@ local function updateCurrentShieldBalls()
 					x_avg = x_avg + x
 					z_avg = z_avg + z
 					numUnits = numUnits + 1
-					if enabled then
+					if enabled and currPower then
 						totalCurrentShield = totalCurrentShield + currPower
 						highestTopOfShield = math.max(highestTopOfShield, y + shieldProps.shieldRadius)
+						if currPower < shieldProps.shieldMaxCharge then
+							totalRegen = totalRegen + getUnitShieldRegen(unitID, UnitDefs[spGetUnitDefID(unitID)])
+						end
 					end
 				end
 				memberPositionsByUnitID[unitID] = {x = x, y = y, z = z}
@@ -244,6 +319,7 @@ local function updateCurrentShieldBalls()
 			xStdDev = math.sqrt(xStdDev / numUnits)
 			zStdDev = math.sqrt(zStdDev / numUnits)
 
+
 			local ballData = {
 				currShield = totalCurrentShield,
 				totShield = totalMaxShield,
@@ -253,6 +329,7 @@ local function updateCurrentShieldBalls()
 				xStdDev = xStdDev,
 				zStdDev = zStdDev,
 				numUnits = numUnits,
+				regen = totalRegen,
 			}
 			ballsInTeam[#ballsInTeam + 1] = ballData
 		end
