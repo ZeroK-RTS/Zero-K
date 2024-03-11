e1648b46ad6a4996760ad92100be14a9fd951b8b
sprunk <spr.ng@o2.pl>
2024-03-11 02:34:01 +0100

    🐛 Fix #5166, aircraft not landing on airpads
    Earlier attempt: 25c3633d4fcd0207cfe45649df328aa202d83737
    Revert: a5cacfd59f698b661f9b28a4afcb303ca9d1f6c0

--- LuaRules/Gadgets/unit_bomber_command.lua
+++ LuaRules/Gadgets/unit_bomber_command.lua
@@ -306,10 +306,7 @@ local function FindNearestAirpad(unitID, team)
 	end
 	-- first go through all the pads to see which ones are unbooked
 	for airpadID,airpadDefID in pairs(airpadsPerAllyteam[allyTeam]) do
-		if spGetUnitIsDead(airpadID) or (not airpadsData[airpadID]) then --rare case. Can happen if airpad is built & die in same frame
-			if (not airpadsData[airpadID]) then
-				Spring.Echo("Warning: airpadsData for " .. airpadID .. " is NIL")
-			end
+		if spGetUnitIsDead(airpadID) then --rare case. Can happen if airpad is built & die in same frame
 			airpadsPerAllyteam[allyTeam][airpadID] = nil
 		else
 		-- Need to ensure that excludedairpad[teamid][padID] is not nil before running!
@@ -511,6 +508,29 @@ local function CancelAirpadReservation(unitID)
 	end
 end
 
+function gadget:UnitReverseBuilt(unitID, unitDefID)
+	local airpadData = airpadsData[unitID]
+	if not airpadData then
+		return
+	end
+
+	local bombers, count = {}, 0
+	for bomberID in pairs(airpadData.reservations.units) do
+		count = count + 1
+		bombers[count] = bomberID
+	end
+
+	for i = 1, count do
+		CancelAirpadReservation(bombers[i])
+	end
+	airpadsData[unitID] = nil
+	airpadsPerAllyteam[Spring.GetUnitAllyTeam(unitID)][unitID] = nil
+
+	for i = 1, count do
+		RequestRearm(bombers[i], nil, false, true)
+	end
+end
+
 function ReserveAirpad(bomberID,airpadID)
 	spSetUnitRulesParam(bomberID, "airpadReservation",1)
 	local reservations = airpadsData[airpadID].reservations
@@ -586,21 +606,6 @@ function gadget:GameFrame(n)
 	for bomberID in pairs(rearmRequest) do
 		RequestRearm(bomberID, nil, true)
 		rearmRequest[bomberID] = nil
-	end
-	
-	if n%120 == 0 then
-		for airpadID, data in pairs(airpadsData) do
-			local _,_,inbuild = spGetUnitIsStunned(airpadID)
-			if inbuild then --if user reclaim airpad, send airplane elsewhere
-				local allyTeam = spGetUnitAllyTeam(airpadID)
-				airpadsPerAllyteam[allyTeam][airpadID] = nil
-				for bomberID in pairs(airpadsData[airpadID].reservations.units) do
-					CancelAirpadReservation(bomberID) -- send anyone who was going here elsewhere
-					RequestRearm(bomberID, nil, false, true) --argument: (bomberID, team, forceNow, replaceExisting)
-				end
-				airpadsData[airpadID] = nil
-			end
-		end
 	end
 	-- track bomber-airpad distance
 	if n%10 == 0 then
