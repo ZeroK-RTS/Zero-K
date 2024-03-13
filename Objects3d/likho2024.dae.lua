c0f38b9fa8109e11c8aa6f092055b7c184b39179
sprunk <spr.ng@o2.pl>
2024-03-14 00:53:50 +0100

    ✨ Likho remodel.

---
+++ Objects3d/likho2024.dae.lua
@@ -0,0 +1,10 @@
+return {
+	--radius = 30,
+	--height = 110,
+	--midpos = {0, -6, 0},
+	tex1 = "likho2024-1.dds",	--4k
+	tex2 = "likho2024-2.dds",
+	normaltex = "likho2024-N.dds",
+	--tex1 = "likho2024-1k-1.dds",	--1k
+	--tex2 = "likho2024-1k-2.dds",
+}
