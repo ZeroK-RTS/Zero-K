3007ac146894060e28be7bb8d21dec9f844ea583
GoogleFrog <googl3frog@gmail.com>
2024-04-22 00:25:36 +1000

    Fix sun on Tempest. Fix Athena wing ribbons. Well, fix for when it is flying forwards, but it can fly sideways and the ribbons are not emitted from the centre of the texture so it can end up looking weird.

---
+++ LuaUI/Configs/MapSettingsOverride/Tempest.lua
@@ -0,0 +1,27 @@
+return {
+	["water"]= false,
+	["sun"]= {
+		["groundDiffuseColor"]= {0.3952381, 0.30000001, 0.31904763, 1},
+		["groundAmbientColor"]= {0.62380952, 0.6047619, 0.47619048, 1},
+		["unitSpecularColor"]= {0.64285713, 0.6047619, 0.60952383, 1},
+		["modelShadowDensity"]= 1,
+		["specularExponent"]= 23.75,
+		["groundSpecularColor"]= {0, 0, 0, 1},
+		["groundShadowDensity"]= 1,
+		["unitDiffuseColor"]= {0.53809524, 0.45238096, 0.47142857, 1},
+		["unitAmbientColor"]= {0.88571429, 0.87619048, 0.76666665, 1},
+	},
+	["fog"]= {
+		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogStart"]= 0.99000001,
+		["fogColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["cloudColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["fogEnd"]= 1,
+	},
+	["fixDefaultWater"]= true,
+	["direction"]= {
+		["sunDir"]= 6.06999969,
+		["sunPitch"]= 0.75999999,
+	},
+}
