9ffe320cad4b3c0db91012bb6c465d1fec10a259
Tim Moore <moreginger@warpmail.net>
2024-04-29 20:32:47 +0100

    fix: Add comments to explain decoration widget layers

--- LuaUI/Widgets/gui_selectedunits_gl4.lua
+++ LuaUI/Widgets/gui_selectedunits_gl4.lua
@@ -7,7 +7,7 @@ function widget:GetInfo()
 		license = "GNU GPL, v2 or later",
 		-- Somewhere between layer -40 and -30 GetUnitUnderCursor starts
 		-- returning nil before GetUnitsInSelectionBox includes that unit.
-		layer = -8, -- Draw below health bars and selection.
+		layer = -9, -- below unit_healthbars 
 		enabled = true,
 	}
 end
