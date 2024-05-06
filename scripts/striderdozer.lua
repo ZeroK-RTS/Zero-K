b8beaf125882e632fec1b3abef3a7cec4ce921b1
GoogleFrog <googl3frog@gmail.com>
2024-05-06 22:49:28 +1000

    Placeholder for strider tank.

---
+++ scripts/striderdozer.lua
@@ -0,0 +1,211 @@
+include "constants.lua"
+include "rockPiece.lua"
+local dynamicRockData
+include "trackControl.lua"
+include "pieceControl.lua"
+
+local base, body, turret1, sleeve1, barrel1, firepoint1, turret2, sleeve2, gun2, firepoint2
+	= piece("base", "body", "turret1", "sleeve1", "barrel1", "firepoint1", "turret2", "sleeve2", "gun2", "firepoint2")
+	
+-- Signal definitions
+local SIG_AIM1 = 1
+local SIG_AIM2 = 2
+local SIG_MOVE = 8
+local SIG_ROCK_X = 16
+local SIG_ROCK_Z = 32
+
+local ROCK_FIRE_FORCE = 0.06
+local ROCK_SPEED = 9
+local ROCK_DECAY = -0.18
+local ROCK_PIECE = base
+local ROCK_MIN = 0.001 --If around axis rock is not greater than this amount, rocking will stop after returning to center.
+local ROCK_MAX = 1.5
+
+local hpi = math.pi*0.5
+
+rockData = {
+	[x_axis] = {
+		piece = ROCK_PIECE,
+		speed = ROCK_SPEED,
+		decay = ROCK_DECAY,
+		minPos = ROCK_MIN,
+		maxPos = ROCK_MAX,
+		signal = SIG_ROCK_X,
+		axis = x_axis,
+	},
+	[z_axis] = {
+		piece = ROCK_PIECE,
+		speed = ROCK_SPEED,
+		decay = ROCK_DECAY,
+		minPos = ROCK_MIN,
+		maxPos = ROCK_MAX,
+		signal = SIG_ROCK_Z,
+		axis = z_axis,
+	},
+}
+
+local trackData = {
+	wheels = {
+		large = {piece('wheels1'), piece('wheels6')},
+		small = {},
+	},
+	tracks = {},
+	signal = SIG_MOVE,
+	smallSpeed = math.rad(480),
+	smallAccel = math.rad(80),
+	smallDecel = math.rad(100),
+	largeSpeed = math.rad(360),
+	largeAccel = math.rad(40),
+	largeDecel = math.rad(50),
+	trackPeriod = 50,
+}
+
+for i = 1, 3 do
+	trackData.tracks[i] = piece ('tracks' .. i)
+end
+for i = 2, 5 do
+	trackData.wheels.small[i - 1] = piece('wheels' .. i)
+end
+
+local gunHeading = 0
+
+local smokePiece = {base, turret1}
+
+local RESTORE_DELAY = 3000
+local LARGE_MUZZLE_FLASH_FX = 1024
+local HUGE_MUZZLE_FLASH_FX = 1025
+
+local ROCK_X_FIRE_1 = -24
+
+local aimPoints = {
+	turret1,
+	turret2,
+}
+local firePoints = {
+	firepoint1,
+	firepoint2,
+}
+
+function RestoreMainGun()
+	Sleep(RESTORE_DELAY)
+	Turn(turret1, y_axis, 0, math.rad(45))
+	Turn(sleeve1, x_axis, 0, math.rad(15))
+end
+
+function RestoreSideGun()
+	Sleep(RESTORE_DELAY)
+	Turn(turret2, y_axis, 0, math.rad(120))
+	Turn(sleeve2, x_axis, 0, math.rad(90))
+end
+
+function RestoreBarrel()
+	Sleep(125)
+	Move(barrel1, z_axis, 0, 10)
+end
+
+function script.Create()
+	dynamicRockData = GG.ScriptRock.InitializeRock(rockData)
+	InitiailizeTrackControl(trackData)
+
+	while (select(5, Spring.GetUnitHealth(unitID)) < 1) do
+		Sleep (250)
+	end
+	StartThread (GG.Script.SmokeUnit, unitID, smokePiece)
+end
+
+function script.StartMoving()
+	StartThread(TrackControlStartMoving)
+end
+
+function script.StopMoving()
+	TrackControlStopMoving()
+end
+
+-- Weapons
+local spGetUnitSeparation = Spring.GetUnitSeparation
+function script.BlockShot(num, targetID)
+	if num == 1 and Spring.ValidUnitID(targetID) then
+		-- TTL at max range determined to be 50f empirically
+		-- at projectile speed 270 elmo/s and 450 range
+		local framesETA = 50 * (spGetUnitSeparation(unitID, targetID) or 0) / 450
+		return GG.OverkillPrevention_CheckBlock(unitID, targetID, 1000.1, framesETA, false, false, true)
+	end
+	return false
+end
+
+function script.AimFromWeapon(num)
+	return aimPoints[num]
+end
+
+function script.QueryWeapon(num)
+	return firePoints[num]
+end
+
+function script.AimWeapon(num, heading, pitch)
+	if num == 1 then
+		Signal(SIG_AIM1)
+		SetSignalMask(SIG_AIM1)
+		
+		Turn(turret1, y_axis, heading, math.rad(60))
+		Turn(sleeve1, x_axis, -pitch, math.rad(30))
+		WaitForTurn(turret1, y_axis)
+		WaitForTurn(sleeve1, x_axis)
+		StartThread(RestoreMainGun)
+		gunHeading = heading
+		return true
+	else
+		Signal(SIG_AIM2)
+		SetSignalMask(SIG_AIM2)
+
+		Turn(turret2, y_axis, heading, math.rad(180))
+		Turn(sleeve2, x_axis, -pitch, math.rad(90))
+		WaitForTurn(turret2, y_axis)
+		WaitForTurn(sleeve2, x_axis)
+
+		StartThread(RestoreSideGun)
+		return true
+	end
+end
+
+function script.Shot(num)
+	if num ~= 1 then
+		return
+	end
+	StartThread(GG.ScriptRock.Rock, dynamicRockData[z_axis], gunHeading, ROCK_FIRE_FORCE)
+	StartThread(GG.ScriptRock.Rock, dynamicRockData[x_axis], gunHeading - hpi, ROCK_FIRE_FORCE)
+	
+	EmitSfx(firepoint1, LARGE_MUZZLE_FLASH_FX)
+	Move(barrel1, z_axis, -5)
+	StartThread(RestoreBarrel)
+end
+
+function script.Killed(recentDamage, maxHealth)
+	local severity = recentDamage / maxHealth
+	if (severity < 0.25) then
+		Explode(turret1, SFX.SMOKE)
+		return 1
+	elseif (severity < 0.5) then
+		Explode(barrel1, SFX.FALL)
+		Explode(gun2, SFX.SHATTER)
+		Explode(sleeve1, SFX.FALL)
+		Explode(turret1, SFX.SHATTER)
+		Explode(turret2, SFX.FALL)
+		Explode(body, SFX.SMOKE)
+		return 1
+	elseif (severity < 1) then
+		Explode(barrel1, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
+		Explode(gun2, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
+		Explode(sleeve1, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
+		Explode(turret1, SFX.SHATTER)
+		Explode(turret2, SFX.SHATTER)
+		return 2
+	end
+	
+	Explode(barrel1, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
+	Explode(gun2, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
+	Explode(sleeve1, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
+	Explode(turret1, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
+	Explode(turret2, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
+	Explode(body, SFX.SHATTER)
+	return 2
+end
---
+++ units/striderdozer.lua
@@ -0,0 +1,185 @@
+return { striderdozer = {
+  name                = [[Dozer]],
+  description         = [[Heavy Buster, Very Tank]],
+  acceleration        = 0.17,
+  brakeRate           = 0.624,
+  builder             = false,
+  buildPic            = [[tankheavyassault.png]],
+  canGuard            = true,
+  canMove             = true,
+  canPatrol           = true,
+  category            = [[LAND]],
+  selectionVolumeOffsets = [[0 0 0]],
+  selectionVolumeScales  = [[96 96 96]],
+  selectionVolumeType    = [[ellipsoid]],
+  corpse              = [[DEAD]],
+
+  customParams        = {
+    bait_level_default = 0,
+    decloak_footprint     = 5,
+
+    outline_x = 110,
+    outline_y = 110,
+    outline_yoff = 13.5,
+  },
+
+  explodeAs           = [[BIG_UNIT]],
+  footprintX          = 4,
+  footprintZ          = 4,
+  health              = 12000,
+  iconType            = [[tankskirm]],
+  leaveTracks         = true,
+  maxSlope            = 18,
+  maxWaterDepth       = 22,
+  metalCost           = 2200,
+  movementClass       = [[TANK4]],
+  noAutoFire          = false,
+  noChaseCategory     = [[TERRAFORM FIXEDWING SATELLITE GUNSHIP SUB]],
+  objectName          = [[terraTank.s3o]],
+  script              = [[striderdozer.lua]],
+  selfDestructAs      = [[BIG_UNIT]],
+
+  sfxtypes            = {
+
+    explosiongenerators = {
+      [[custom:LARGE_MUZZLE_FLASH_FX]],
+    },
+
+  },
+  sightDistance       = 540,
+  speed               = 57,
+  trackOffset         = 8,
+  trackStrength       = 10,
+  trackStretch        = 1,
+  trackType           = [[StdTank]],
+  trackWidth          = 50,
+  turninplace         = 0,
+  turnRate            = 500,
+  workerTime          = 0,
+
+  weapons             = {
+
+    {
+      def                = [[COR_GOL]],
+      badTargetCategory  = [[FIXEDWING GUNSHIP]],
+      onlyTargetCategory = [[SWIM LAND SINK TURRET FLOAT SHIP HOVER GUNSHIP FIXEDWING]],
+    },
+    {
+      def                = [[SLOWBEAM]],
+      badTargetCategory  = [[FIXEDWING UNARMED]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+
+  },
+
+  weaponDefs          = {
+
+    COR_GOL             = {
+      name                    = [[Tankbuster Cannon]],
+      areaOfEffect            = 32,
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      customParams            = {
+        burst = Shared.BURST_RELIABLE,
+        gatherradius = [[105]],
+        smoothradius = [[70]],
+        smoothmult   = [[0.4]],
+        force_ignore_ground = [[1]],
+        
+        light_color = [[3 2.33 1.5]],
+        light_radius = 150,
+      },
+      
+      damage                  = {
+        default = 1000.1,
+      },
+
+      explosionGenerator      = [[custom:TESS]],
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      noSelfDamage            = true,
+      range                   = 450,
+      reloadtime              = 3.5,
+      soundHit                = [[weapon/cannon/supergun_bass_boost]],
+      soundStart              = [[weapon/cannon/rhino]],
+      turret                  = true,
+      weaponType              = [[Cannon]],
+      weaponVelocity          = 270,
+    },
+    
+    SLOWBEAM = {
+      name                    = [[Slowing Beam]],
+      areaOfEffect            = 8,
+      beamDecay               = 0.9,
+      beamTime                = 0.1,
+      beamttl                 = 50,
+      coreThickness           = 0,
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      customparams = {
+        timeslow_onlyslow = 1,
+        timeslow_smartretarget = 0.33,
+        
+        light_camera_height = 1800,
+        light_color = [[0.6 0.22 0.8]],
+        light_radius = 200,
+      },
+
+      damage                  = {
+        default = 2000,
+      },
+
+      explosionGenerator      = [[custom:flashslow]],
+      fireStarter             = 30,
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      largeBeamLaser          = true,
+      laserFlareSize          = 4,
+      minIntensity            = 1,
+      noSelfDamage            = true,
+      range                   = 440,
+      reloadtime              = 2.5,
+      rgbColor                = [[0.27 0 0.36]],
+      soundStart              = [[weapon/laser/pulse_laser2]],
+      soundStartVolume        = 15,
+      soundTrigger            = true,
+      sweepfire               = false,
+      texture1                = [[largelaser]],
+      texture2                = [[flare]],
+      texture3                = [[flare]],
+      texture4                = [[smallflare]],
+      thickness               = 11,
+      tolerance               = 18000,
+      turret                  = true,
+      weaponType              = [[BeamLaser]],
+      weaponVelocity          = 500,
+    },
+  },
+
+
+  featureDefs         = {
+
+    DEAD       = {
+      blocking         = true,
+      featureDead      = [[HEAP]],
+      footprintX       = 4,
+      footprintZ       = 4,
+      object           = [[golly_d.s3o]],
+    },
+
+    
+    HEAP       = {
+      blocking         = false,
+      footprintX       = 4,
+      footprintZ       = 4,
+      object           = [[debris4x4c.s3o]],
+    },
+
+  },
+
+} }
