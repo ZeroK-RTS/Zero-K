b1207fd99ad7ca9018e887452347c10627803a8b
GoogleFrog <googl3frog@gmail.com>
2024-05-06 23:03:27 +1000

    Refactor explosion terraform 1/2

--- LuaRules/Gadgets/unit_terraform.lua
+++ LuaRules/Gadgets/unit_terraform.lua
@@ -207,8 +207,6 @@ local terraUnitHP = UnitDefs[terraunitDefID].health - 1000 -- Stop terraunit hav
 local terraUnitCost = UnitDefs[terraunitDefID].metalCost
 local terraBuildMult = terraUnitHP/UnitDefs[terraunitDefID].health
 
-local terraformOnUnitDestroyed = VFS.Include("LuaRules/Configs/unit_terraform_defs.lua", nil, VFS.GAME)
-
 local terraformUnitDefIDs = {}
 for i = 1, #UnitDefs do
 	local ud = UnitDefs[i]
@@ -3712,74 +3710,6 @@ end
 --------------------------------------------------------------------------------
 
 function gadget:UnitDestroyed(unitID, unitDefID)
-	local config = terraformOnUnitDestroyed[unitDefID]
-	if config then
-		local  _,_,_,_,buildProgress = spGetUnitHealth(unitID)
-		if buildProgress == 1 then
-			local posX, posY, posZ = config.posX, config.posY, config.posZ
-
-			local ux, uy, uz = spGetUnitPosition(unitID)
-			ux = floor((ux+8)/16)*16
-			uz = floor((uz+8)/16)*16
-			
-			local shrakaCliff = config.shrakaPyramidDiff
-			
-			local heightCache = {}
-			local function Height(x, z)
-				if heightCache[x] and heightCache[x][z] then
-					return heightCache[x][z]
-				end
-				heightCache[x] = heightCache[x] or {}
-				heightCache[x][z] = spGetGroundHeight(x, z)
-				return heightCache[x][z]
-			end
-			
-			spSetHeightMapFunc(
-				function()
-					local maxDiff, x, z, toAdd
-					for i = 1, #posX, 1 do
-						x, z = posX[i] + ux, posZ[i] + uz
-						toAdd = posY[i]
-						if shrakaCliff then
-							if toAdd > 0 then
-								maxDiff = Height(x, z) - min(Height(x - 8, z), Height(x + 8, z), Height(x, z - 8), Height(x, z + 8))
-								maxDiff = shrakaCliff - maxDiff
-								if toAdd > maxDiff then
-									if maxDiff > 0 then
-										toAdd = maxDiff
-									else
-										toAdd = false
-									end
-								end
-							else
-								maxDiff = max(Height(x - 8, z), Height(x + 8, z), Height(x, z - 8), Height(x, z + 8)) - Height(x, z)
-								maxDiff = maxDiff - shrakaCliff
-								if toAdd < maxDiff then
-									if maxDiff < 0 then
-										toAdd = maxDiff
-									else
-										toAdd = false
-									end
-								end
-							end
-						end
-						if toAdd and IsPositionTerraformable(x, z) then
-							spAddHeightMap(x, z, toAdd)
-						end
-					end
-				end
-			)
-			
-			local units = Spring.GetUnitsInCylinder(ux, uz, config.impulseRadius)
-			for i = 1, #units do
-				local hitUnitID = units[i]
-				if hitUnitID ~= unitID then
-					GG.AddGadgetImpulseRaw(hitUnitID, 0, config.impulseY, 0, true, true)
-				end
-			end
-		end
-	end
-
 	if constructor[unitID] then
 		local index = constructor[unitID].index
 		if index ~= constructors then
@@ -3859,6 +3789,7 @@ end
 
 local TerraformFunctions = {}
 TerraformFunctions.DoSmooth = DoSmooth
+TerraformFunctions.IsPositionTerraformable = IsPositionTerraformable
 
 function TerraformFunctions.ForceTerraformCompletion(pregame, needSaveHax)
 	if pregame and needSaveHax then
