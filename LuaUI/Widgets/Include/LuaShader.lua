745532acf609272b3f6bb1ec114172857a9f2f2d
GoogleFrog <googl3frog@gmail.com>
2024-05-18 11:33:16 +1000

    Update luashader.

--- LuaUI/Widgets/Include/LuaShader.lua
+++ LuaUI/Widgets/Include/LuaShader.lua
@@ -183,7 +183,7 @@ mat4 mat4mix(mat4 a, mat4 b, float alpha) {
 
 // Additional helper functions useful in Spring
 
-vec2 heighmapUVatWorldPos(vec2 worldpos){
+vec2 heightmapUVatWorldPos(vec2 worldpos){
 	const vec2 inverseMapSize = 1.0 / mapSize.xy;
 	// Some texel magic to make the heightmap tex perfectly align:
 	const vec2 heightmaptexel = vec2(8.0, 8.0);
@@ -191,6 +191,17 @@ vec2 heighmapUVatWorldPos(vec2 worldpos){
 	vec2 uvhm = clamp(worldpos, heightmaptexel, mapSize.xy - heightmaptexel);
 	uvhm = uvhm	* inverseMapSize;
 	return uvhm;
+}
+
+// This does 'mirror' style tiling of UVs like the way the map edge extension works
+vec2 heightmapUVatWorldPosMirrored(vec2 worldpos) {
+	vec2 inverseMapSize = vec2(1.0) / mapSize.xy;
+	// Some texel magic to make the heightmap tex perfectly align:
+	vec2 heightmaptexel = vec2(8.0, 8.0);
+	worldpos +=  vec2(-8.0, -8.0) * (worldpos * inverseMapSize) + vec2(4.0, 4.0) ;
+	vec2 uvhm = worldpos * inverseMapSize;
+	
+	return abs(fract(uvhm * 0.5 + 0.5) - 0.5) * 2.0;
 }
 
 // Note that this function does not check the Z or depth of the clip space, but in regular springrts top-down views, this isnt needed either. 
@@ -279,7 +290,7 @@ LuaShader.CreateShaderDefinesString = CreateShaderDefinesString
 
 local function CheckShaderUpdates(shadersourcecache, delaytime)
 	-- todo: extract shaderconfig
-	if shadersourcecache.lastshaderupdate == nil or 
+	if shadersourcecache.forceupdate or shadersourcecache.lastshaderupdate == nil or 
 		Spring.DiffTimers(Spring.GetTimer(), shadersourcecache.lastshaderupdate) > (delaytime or 0.5) then 
 		shadersourcecache.lastshaderupdate = Spring.GetTimer()
 		local vsSrcNew = (shadersourcecache.vssrcpath and VFS.LoadFile(shadersourcecache.vssrcpath)) or shadersourcecache.vsSrc
@@ -297,7 +308,7 @@ local function CheckShaderUpdates(shadersourcecache, delaytime)
 			shadersourcecache.fsSrc = fsSrcNew
 			shadersourcecache.gsSrc = gsSrcNew
 			shadersourcecache.forceupdate = nil
-			
+			shadersourcecache.updateFlag = true
 			local engineUniformBufferDefs = LuaShader.GetEngineUniformBufferDefs()
 			local shaderDefines = LuaShader.CreateShaderDefinesString(shadersourcecache.shaderConfig)
 			if vsSrcNew then 
@@ -536,7 +547,7 @@ function LuaShader:Compile(suppresswarnings)
 	local shaderObj = self.shaderObj
 
 	local shLog = gl.GetShaderLog() or ""
-
+	self.shLog = shLog
 	if not shaderObj then
 		self:ShowError(shLog)
 		return false
@@ -754,32 +765,40 @@ LuaShader.SetUniformFloatAlways = LuaShader.SetUniformAlways
 
 
 --INTEGER UNIFORMS
-local function setUniformIntAlwaysImpl(uniform, ...)
-	glUniformInt(uniform.location, ...)
+local function setUniformIntAlwaysImpl(uniform,  u1, u2, u3, u4)
+	if u4 ~= nil then 
+		glUniformInt(uniform.location, u1, u2, u3, u4)
+	elseif u3 ~= nil then 
+		glUniformInt(uniform.location, u1, u2, u3)
+	elseif u2 ~= nil then 
+		glUniformInt(uniform.location, u1, u2)
+	else
+		glUniformInt(uniform.location, u1)
+	end
 	return true --currently there is no way to check if uniform is set or not :(
 end
 
-function LuaShader:SetUniformIntAlways(name, ...)
+function LuaShader:SetUniformIntAlways(name,  u1, u2, u3, u4)
 	local uniform = getUniform(self, name)
 	if not uniform then
 		return false
 	end
-	return setUniformIntAlwaysImpl(uniform, ...)
+	return setUniformIntAlwaysImpl(uniform,  u1, u2, u3, u4)
 end
 
-local function setUniformIntImpl(uniform, ...)
-	if isUpdateRequired(uniform, {...}) then
-		return setUniformIntAlwaysImpl(uniform, ...)
+local function setUniformIntImpl(uniform, u1, u2, u3, u4)
+	if isUpdateRequiredNoTable(uniform, u1, u2, u3, u4) then
+		return setUniformIntAlwaysImpl(uniform, u1, u2, u3, u4)
 	end
 	return true
 end
 
-function LuaShader:SetUniformInt(name, ...)
+function LuaShader:SetUniformInt(name,  u1, u2, u3, u4)
 	local uniform = getUniform(self, name)
 	if not uniform then
 		return false
 	end
-	return setUniformIntImpl(uniform, ...)
+	return setUniformIntImpl(uniform,  u1, u2, u3, u4)
 end
 
 
