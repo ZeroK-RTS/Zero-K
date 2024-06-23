c04f4f4e81f05a07dafd1b6c07b6ef74d383674b
sprunk <spr.ng@o2.pl>
2024-06-23 20:39:52 +0200

    📝 Add some mex config examples

---
+++ LuaRules/Configs/MetalSpots/_example_basic.lua
@@ -0,0 +1,12 @@
+-- Basic example.
+-- Specify x/z coordinates and metal value.
+return {
+	spots = {
+		{ x = 1234, z =  567, metal = 2.0  },
+		{ x =  333, z =  444, metal = 2.0  }, 
+		{ x =  123, z =  456, metal = 2.0  },
+		{ x = 1111, z = 2222, metal = 3.5  },
+		{ x =  111, z =  222, metal = 1.25 },
+		-- as many spots as you need/want
+	},
+}
