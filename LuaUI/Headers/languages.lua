6ff8d06a24cdf48e28cd3ea588b441165dea789e
sprunk <spr.ng@o2.pl>
2024-08-10 23:18:53 +0200

    Move Chinese up the lang picker list
    High completion ratio and perhaps more "needed" than for euro languages

--- LuaUI/Headers/languages.lua
+++ LuaUI/Headers/languages.lua
@@ -2,14 +2,14 @@ local langs = {
 	{ lang = 'en', flag = 'gb', name = 'English' },
 	{ lang = 'ru', flag = 'ru', name = 'Русский' },
 	{ lang = 'pl', flag = 'pl', name = 'Polski' },
+	{ lang = 'zh_CN', flag = 'cn', name = '簡體中文' },
+	{ lang = 'zh_TW', flag = 'tw', name = '繁體中文' },
 	{ lang = 'it', flag = 'it', name = 'Italiano' },
 	{ lang = 'fr', flag = 'fr', name = 'Français' },
 	{ lang = 'de', flag = 'de', name = 'Deutsch' },
 	{ lang = 'tr', flag = 'tr', name = 'Türkçe' },
 	{ lang = 'uk_UA', flag = 'ua', name = 'Українська' },
 	{ lang = 'sl', flag = 'si', name = 'Slovenščina' },
-	{ lang = 'zh_CN', flag = 'cn', name = '簡體中文' },
-	{ lang = 'zh_TW', flag = 'tw', name = '繁體中文' },
 }
 
 local flagByLang, langByFlag = {}, {}
