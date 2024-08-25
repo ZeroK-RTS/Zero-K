3ce2cf0c5bc15d7cc546bfa049f6ce2cb2b39b30
GoogleFrog <googl3frog@gmail.com>
2024-08-25 17:19:52 +1000

    De-duplicate config files.

--- LuaUI/Configs/DeferredLightsGL4config.lua
+++ LuaUI/Configs/DeferredLightsGL4config.lua
@@ -41,6 +41,19 @@ local unitLights = {
 	},
 }
 
+
+local gibLight = {
+	lightType = 'point', -- or cone or beam
+	pieceName = nil, -- optional
+	lightConfig = {
+		posx = 0, posy = 0, posz = 0, radius = 360,
+		r = 1, g = 0.9, b = 0.5, a = 0.2,
+		color2r = 0.9, color2g = 0.75, color2b = 0.25, colortime = 0.3, -- point lights only, colortime in seconds for unit-attache
+		modelfactor = 0.4, specular = 0.5, scattering = 0.5, lensflare = 0,
+		lifetime = 300, sustain = 3, aninmtype = 0 -- unused
+	},
+}
+
 -- convert unitname -> unitDefID
 local unitDefLights = {}
 for unitName, lights in pairs(unitLights) do
@@ -50,7 +63,15 @@ for unitName, lights in pairs(unitLights) do
 end
 unitLights = nil
 
-local allLights = {unitEventLights = {}, unitDefLights = unitDefLights, featureDefLights = {}}
+local allLights = {
+	unitEventLights = {},
+	unitDefLights = unitDefLights,
+	featureDefLights = {},
+	muzzleFlashLights = {},
+	projectileDefLights = {},
+	explosionLights = {},
+	gibLight = gibLight,
+}
 
 return allLights
 
