96af8aa7d9809a8f4c921e3da08afc2be9760f73
sprunk <spr.ng@o2.pl>
2024-12-17 05:59:49 +0100

    Remove `debug.lua`
    Its contents could in theory be useful to a modder but they are too obscure
    for anybody who needs them to know about them. Not used anywhere in vanilla.

--- LuaUI/camain.lua
+++ LuaUI/camain.lua
@@ -23,7 +23,6 @@ vfsInclude('LuaRules/engine_compat.lua', nil, vfsGame)
 vfsInclude("LuaUI/utils.lua"    , nil, vfsGame)
 vfsInclude("LuaUI/setupdefs.lua", nil, vfsGame)
 vfsInclude("LuaUI/savetable.lua", nil, vfsGame)
-vfsInclude("LuaUI/debug.lua"    , nil, vfsGame)
 vfsInclude("LuaUI/modfonts.lua" , nil, vfsGame)
 vfsInclude("LuaUI/layout.lua"   , nil, vfsGame)   -- contains a simple LayoutButtons()
 vfsInclude("LuaUI/cawidgets.lua", nil, vfsGame)  -- the widget handler
