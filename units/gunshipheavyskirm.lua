b3246e9bef8ce654c58dd5eac83059d570875329
Aquanim <threeoneteacup@gmail.com>
2024-12-19 18:31:49 +1100

    Proposed balance changes:  * Cutter cost 65 -> 55  * Flail damage 375 -> 475 (DPS 72 -> 91)  * Locust movespeed 212 -> 250  * Phoenix cost 460 -> 520, Rearm time 13 -> 15  * Cyclops turn rate decreased by 10 percent  * Magpie rearm time 13s -> 12s  * Bulkhead cost 230 -> 250  * Felon speed 45 -> 42, Reload time 0.133 -> 0.166 (Max DPS 705 -> 564)  * Aspis cost 600 -> 700 (no change to Aegis)  * Thug and Convict shield radius 80 -> 90  * Nimbus vision radius 600 -> 660  * Hacksaw cost 220 -> 210  * Ripper range 280 -> 290

--- units/gunshipheavyskirm.lua
+++ units/gunshipheavyskirm.lua
@@ -52,7 +52,7 @@ return { gunshipheavyskirm = {
     },
 
   },
-  sightDistance       = 600,
+  sightDistance       = 660,
   speed               = 99,
   turnRate            = 600,
   workerTime          = 0,
