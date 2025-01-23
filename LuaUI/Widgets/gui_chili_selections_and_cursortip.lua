5620d63ee319d293549af56051f49b9aa526a0c8
Amnykon <amnykon@gmail.com>
2025-01-23 17:22:47 -0600

    fix selections and cursortip crash.

--- LuaUI/Widgets/gui_chili_selections_and_cursortip.lua
+++ LuaUI/Widgets/gui_chili_selections_and_cursortip.lua
@@ -2128,7 +2128,7 @@ local function GetSingleUnitInfoPanel(parentControl, isTooltipVersion)
 		
 		local healthPos
 		if shieldBarUpdate then
-			if ud and (ud.shieldPower > 0 or ud.level) then
+			if ud and ((ud.shieldPower or 0) > 0 or ud.level) then
 				local shieldPower = (spGetUnitRulesParam(unitID, "comm_shield_max") or ud.shieldPower) * (Spring.GetUnitRulesParam(unitID, "totalShieldMaxMult") or 1)
 				local _, shieldCurrentPower = spGetUnitShieldState(unitID, -1)
 				if shieldCurrentPower and shieldPower then
