f5ffc0499f86ee26a61389d4c57525a8ea748e8c
GoogleFrog <googl3frog@gmail.com>
2025-04-12 22:56:11 +1000

    Revert "Fix Merlin/Grizzly shooting out of their face in Tech-K (#5423)"
    This reverts commit 9a0e09bff27f40309cd458686b503b68a5646780.

--- scripts/striderarty.lua
+++ scripts/striderarty.lua
@@ -28,7 +28,7 @@ end
 --Init variables
 local bAiming = false
 local gun_1 = 1
-local gun_1_side = false
+local gun_1_side = 0
 
 -- Signal definitions
 local SIG_WALK = 1
@@ -184,15 +184,12 @@ end
 
 function script.FireWeapon(num)
 	gun_1 = 0
-	gun_1_side = true
+	gun_1_side = 1
 end
 
 function script.Shot(num)
 	if gun_1_side then
 		gun_1 = gun_1 + 1
-	end
-	if gun_1==21 then
-		gun_1=1
 	end
 	gun_1_side = not gun_1_side
 end
