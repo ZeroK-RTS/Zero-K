2c43a58330b2e09435aa128ab3b5c3f84ff24737
GoogleFrog <googl3frog@gmail.com>
2025-05-10 16:57:23 +1000

    Laser Raven.

---
+++ scripts/bomberlaser.lua
@@ -0,0 +1,179 @@
+include "bombers.lua"
+include "fakeUpright.lua"
+include "constants.lua"
+include "fixedwingTakeOff.lua"
+
+local base = piece 'base'
+local fuselage = piece 'fuselage'
+local wingl1 = piece 'wingl1'
+local wingr1 = piece 'wingr1'
+local wingl2 = piece 'wingl2'
+local wingr2 = piece 'wingr2'
+local engines = piece 'engines'
+local fins = piece 'fins'
+local rflap = piece 'rflap'
+local lflap = piece 'lflap'
+local predrop = piece 'predrop'
+local drop = piece 'drop'
+local thrustl = piece 'thrustl'
+local thrustr = piece 'thrustr'
+local wingtipl = piece 'wingtipl'
+local wingtipr = piece 'wingtipr'
+local xp,zp = piece("x","z")
+
+local spGetUnitPosition     = Spring.GetUnitPosition
+local spGetUnitHeading      = Spring.GetUnitHeading
+local spGetUnitVelocity     = Spring.GetUnitVelocity
+local spGetUnitMoveTypeData = Spring.GetUnitMoveTypeData
+local spSetAirMoveTypeData  = Spring.MoveCtrl.SetAirMoveTypeData
+local spGetGroundHeight     = Spring.GetGroundHeight
+
+local EstimateCurrentMaxSpeed = Spring.Utilities.EstimateCurrentMaxSpeed
+
+local doingRun = false
+
+local SIG_TAKEOFF = 1
+local SIG_NOT_BLOCKED = 2
+local predictMult = 3
+
+local takeoffHeight = UnitDefNames["bomberprec"].cruiseAltitude
+local takeoffHeightInElmos = takeoffHeight*1.5
+local smokePiece = {fuselage, thrustr, thrustl}
+
+function script.StartMoving()
+	--Turn(fins, z_axis, math.rad(30), math.rad(50))
+	Move(wingr1, x_axis, 0, 50)
+	Move(wingr2, x_axis, 0, 50)
+	Move(wingl1, x_axis, 0, 50)
+	Move(wingl2, x_axis, 0, 50)
+end
+
+function script.StopMoving()
+	--Turn(fins, z_axis, 0, math.rad(80))
+	Move(wingr1, x_axis, 5, 30)
+	Move(wingr2, x_axis, 5, 30)
+	Move(wingl1, x_axis, -5, 30)
+	Move(wingl2, x_axis, -5, 30)
+	StartThread(GG.TakeOffFuncs.TakeOffThread, takeoffHeight, SIG_TAKEOFF)
+end
+
+local function Lights()
+	while select(5, Spring.GetUnitHealth(unitID)) < 1 do
+		Sleep(400)
+	end
+	while true do
+		EmitSfx(wingtipr, 1024)
+		EmitSfx(wingtipl, 1025)
+		Sleep(2000)
+	end
+end
+
+function script.Create()
+	StartThread(GG.Script.SmokeUnit, unitID, smokePiece)
+	StartThread(GG.TakeOffFuncs.TakeOffThread, takeoffHeight, SIG_TAKEOFF)
+	GG.FakeUpright.FakeUprightInit(xp, zp, drop)
+	Move (drop, y_axis, -8)
+	Move (drop, z_axis, 2)
+	--StartThread(Lights)
+end
+
+function script.QueryWeapon(num)
+	return drop
+end
+
+function script.AimFromWeapon(num)
+	return drop
+end
+
+function script.AimWeapon(num, heading, pitch)
+	return not RearmBlockShot()
+end
+
+local function ResetTurnRadius()
+	Signal(SIG_NOT_BLOCKED)
+	SetSignalMask(SIG_NOT_BLOCKED)
+	Sleep(500)
+	SetUnarmedAI(300)
+end
+
+function script.FireWeapon()
+	if doingRun then
+		return
+	end
+	if RearmBlockShot() then
+		return
+	end
+	SetUnarmedAI()
+	
+	doingRun = true
+	Sleep(500)
+	local i = 1
+	while i <= 9 do
+		local stunned_or_inbuild = Spring.GetUnitIsStunned(unitID) or (Spring.GetUnitRulesParam(unitID,"disarmed") == 1)
+		if not stunned_or_inbuild then
+			local xx, xy, xz = Spring.GetUnitPiecePosDir(unitID,xp)
+			local zx, zy, zz = Spring.GetUnitPiecePosDir(unitID,zp)
+			local bx, by, bz = Spring.GetUnitPiecePosDir(unitID,base)
+			local xdx = xx - bx
+			local xdy = xy - by
+			local xdz = xz - bz
+			local zdx = zx - bx
+			local zdy = zy - by
+			local zdz = zz - bz
+			local angle_x = math.atan2(xdy, math.sqrt(xdx^2 + xdz^2))
+			local angle_z = math.atan2(zdy, math.sqrt(zdx^2 + zdz^2))
+	
+			Turn(predrop, x_axis, angle_x)
+			Turn(predrop, z_axis, -angle_z)
+			if i == 1 then
+				Spring.PlaySoundFile("sounds/weapon/laser/heavy_laser3.wav", 4, px, py, pz)
+			end
+			EmitSfx(drop, GG.Script.FIRE_W2)
+			GG.PokeDecloakUnit(unitID, unitDefID)
+			i = i + 1
+		end
+		local slowMult = (Spring.GetUnitRulesParam(unitID,"baseSpeedMult") or 1)
+		Sleep(35/slowMult) -- fire density
+	end
+	
+	doingRun = false
+	Reload()
+end
+
+function StartRun()
+	script.FireWeapon()
+end
+
+function script.BlockShot()
+	return (GetUnitValue(GG.Script.CRASHING) == 1)
+end
+
+function script.Killed(recentDamage, maxHealth)
+	Signal(SIG_TAKEOFF)
+	local severity = recentDamage/maxHealth
+	if severity <= 0.25 then
+		Explode(fuselage, SFX.NONE)
+		Explode(engines, SFX.NONE)
+		Explode(wingl1, SFX.NONE)
+		Explode(wingr2, SFX.NONE)
+		return 1
+	elseif severity <= 0.50 or (Spring.GetUnitMoveTypeData(unitID).aircraftState == "crashing") then
+		Explode(fuselage, SFX.NONE)
+		Explode(engines, SFX.NONE)
+		Explode(wingl2, SFX.NONE)
+		Explode(wingr1, SFX.NONE)
+		return 1
+	elseif severity <= 1 then
+		Explode(fuselage, SFX.NONE)
+		Explode(engines, SFX.FALL + SFX.SMOKE + SFX.FIRE)
+		Explode(wingl1, SFX.FALL + SFX.SMOKE + SFX.FIRE)
+		Explode(wingr2, SFX.FALL + SFX.SMOKE + SFX.FIRE)
+		return 2
+	else
+		Explode(fuselage, SFX.NONE)
+		Explode(engines, SFX.FALL + SFX.SMOKE + SFX.FIRE)
+		Explode(wingl1, SFX.FALL + SFX.SMOKE + SFX.FIRE)
+		Explode(wingl2, SFX.FALL + SFX.SMOKE + SFX.FIRE)
+		return 2
+	end
+end
---
+++ units/bomberlaser.lua
@@ -0,0 +1,185 @@
+return { bomberlaser = {
+  name                = [[Laser Raven]],
+  description         = [[Precision Bomber (Anti-Sub)]],
+  brakerate           = 0.4,
+  builder             = false,
+  buildPic            = [[bomberprec.png]],
+  canFly              = true,
+  canGuard            = true,
+  canMove             = true,
+  canPatrol           = true,
+  canSubmerge         = false,
+  category            = [[FIXEDWING]],
+  collide             = false,
+  collisionVolumeOffsets = [[0 0 0]],
+  collisionVolumeScales  = [[100 18 50]],
+  collisionVolumeType    = [[ellipsoid]],
+  selectionVolumeOffsets = [[0 0 0]],
+  selectionVolumeScales  = [[129 34 81]],
+  selectionVolumeType    = [[box]],
+  corpse              = [[DEAD]],
+  cruiseAltitude      = 180,
+
+  customParams        = {
+    modelradius      = [[15]],
+    refuelturnradius = [[120]],
+    reammoseconds    = [[8]],
+    can_set_target   = [[1]],
+
+    outline_x = 130,
+    outline_y = 130,
+    outline_yoff = 10,
+  },
+
+  explodeAs           = [[GUNSHIPEX]],
+  floater             = true,
+  footprintX          = 3,
+  footprintZ          = 3,
+  health              = 1000,
+  iconType            = [[bomberassault]],
+  maneuverleashlength = [[1380]],
+  maxAcc              = 0.5,
+  maxBank             = 0.6,
+  maxElevator         = 0.02,
+  maxRudder           = 0.013,
+  maxFuel             = 1000000,
+  maxPitch            = 0.4,
+  metalCost           = 300,
+  noAutoFire          = false,
+  noChaseCategory     = [[TERRAFORM FIXEDWING SATELLITE GUNSHIP TOOFAST]],
+  objectName          = [[corshad.s3o]],
+  script              = [[bomberlaser.lua]],
+  selfDestructAs      = [[GUNSHIPEX]],
+
+  sfxtypes            = {
+
+    explosiongenerators = {
+      [[custom:light_red]],
+      [[custom:light_green]],
+    },
+
+  },
+  sightDistance       = 780,
+  speed               = 234,
+  turnRadius          = 300,
+  workerTime          = 0,
+
+  weapons             = {
+
+    {
+      def                = [[BOGUS_BOMB]],
+      onlyTargetCategory = [[LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER SUB]],
+    },
+    {
+      def                = [[LASER]],
+      mainDir            = [[0 -1 0]],
+      maxAngleDif        = 0,
+      onlyTargetCategory = [[NONE]],
+    },
+  },
+
+
+  weaponDefs          = {
+
+    BOGUS_BOMB = {
+      name                    = [[Fake Bomb]],
+      avoidFeature            = false,
+      avoidFriendly           = false,
+      burst                   = 2,
+      burstrate               = 1,
+      collideFriendly         = false,
+
+      customParams            = {
+        bogus = 1,
+      },
+
+      damage                  = {
+        default = 0,
+      },
+
+      explosionGenerator      = [[custom:NONE]],
+      interceptedByShieldType = 1,
+      intensity               = 0,
+      myGravity               = 40,
+      noSelfDamage            = true,
+      range                   = 20,
+      reloadtime              = 1,
+      sprayangle              = 2000,
+      weaponType              = [[AircraftBomb]],
+    },
+
+
+    LASER = {
+      name                    = [[High-Energy Laserbeam]],
+      areaOfEffect            = 14,
+      beamTime                = 9/30,
+      coreThickness           = 0.5,
+      craterBoost             = 0,
+      craterMult              = 0,
+      
+      customParams            = {
+        burst = Shared.BURST_UNRELIABLE,
+        prevent_overshoot_fudge = 15,
+
+        light_color = [[1.25 1.25 3.75]],
+        light_radius = 180,
+      },
+
+      damage                  = {
+        default = 800.1,
+        planes  = 800.1,
+      },
+
+      explosionGenerator      = [[custom:flash1green]],
+      fireStarter             = 90,
+      fireTolerance           = 8192, -- 45 degrees
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      largeBeamLaser          = true,
+      laserFlareSize          = 10.4,
+      leadLimit               = 18,
+      minIntensity            = 1,
+      noSelfDamage            = true,
+      projectiles             = 1,
+      range                   = 800,
+      reloadtime              = 4.5,
+      rgbColor                = [[0 1 0]],
+      scrollSpeed             = 5,
+      soundStart              = [[weapon/laser/heavy_laser3]],
+      sweepfire               = false,
+      texture1                = [[largelaser]],
+      texture2                = [[flare]],
+      texture3                = [[flare]],
+      texture4                = [[smallflare]],
+      thickness               = 10.4024486300101,
+      tileLength              = 300,
+      tolerance               = 10000,
+      turret                  = true,
+      weaponType              = [[BeamLaser]],
+      weaponVelocity          = 2250,
+    },
+  },
+
+
+  featureDefs         = {
+
+    DEAD  = {
+      blocking         = true,
+      featureDead      = [[HEAP]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      object           = [[spirit_dead.s3o]],
+    },
+
+    HEAP  = {
+      blocking         = false,
+      footprintX       = 2,
+      footprintZ       = 2,
+      object           = [[debris2x2c.s3o]],
+    },
+
+  },
+
+} }
