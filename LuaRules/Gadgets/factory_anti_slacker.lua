8ef7c3fd237437ff6a78b0017847fa0b3809e5ee
GoogleFrog <googl3frog@gmail.com>
2025-07-03 15:19:30 +1000

    Units are no longer pushed sideways by the next nanoframe when they leave a factory. This seems to be due to a recent engine change that modified unit-nanoframe collision. Blastwing now flies up when built, rather than down.

--- LuaRules/Gadgets/factory_anti_slacker.lua
+++ LuaRules/Gadgets/factory_anti_slacker.lua
@@ -34,25 +34,67 @@ local noEject = {
 	[UnitDefNames["pw_dropfac"].id] = true,
 	[UnitDefNames["pw_bomberfac"].id] = true,
 }
-local ghostFrames = 30	--how long the unit will be ethereal
+
+local pushDefs = {}
+local factoryDefs = {}
+for i = 1, #UnitDefs do
+	local ud = UnitDefs[i]
+	if ud.customParams.factory_creation_push then
+		pushDefs[i] = tonumber(ud.customParams.factory_creation_push)
+	end
+	if (ud.isFactory and (not ud.customParams.notreallyafactory) and ud.buildOptions) then
+		factoryDefs[i] = true
+	end
+end
+
+
+local ghostFrames = 30 --how long the unit will be ethereal
+local nanoGhostFrames = 50
 
 local setBlocking = {} --indexed by gameframe, contains a subtable of unitIDs
 
+local function SetTemporaryNoBlock(unitID, frames)
+	local frame = spGetGameFrame() + ghostFrames
+	if not setBlocking[frame] then
+		setBlocking[frame] = {}
+	end
+	setBlocking[frame][unitID] = true
+	spSetUnitBlocking(unitID, false)
+end
+
 function gadget:GameFrame(n)
-	if setBlocking[n] then	--restore blocking
+	if setBlocking[n] then --restore blocking
 		for unitID, _ in pairs(setBlocking[n]) do
-			if not spGetUnitIsDead(unitID) then spSetUnitBlocking(unitID, true) end
+			if not spGetUnitIsDead(unitID) then
+				spSetUnitBlocking(unitID, true)
+			end
 		end
 		setBlocking[n] = nil
 	end
 end
 
+function gadget:UnitCreated(unitID, unitDefID, unitTeam, builderID)
+	if not builderID then
+		return
+	end
+	local builderDefID = Spring.GetUnitDefID(builderID)
+	if not builderDefID then
+		return
+	end
+	if not factoryDefs[builderDefID] or noEject[builderDefID] then
+		return
+	end
+	SetTemporaryNoBlock(unitID, nanoGhostFrames)
+end
+
 function gadget:UnitFromFactory(unitID, unitDefID, teamID, builderID, builderDefID)
-	if not noEject[builderDefID] then
+	if factoryDefs[builderDefID] then
 		--Spring.Echo("Ejecting unit")
-		local frame = spGetGameFrame() + ghostFrames
-		if not setBlocking[frame] then setBlocking[frame] = {} end
-		setBlocking[frame][unitID] = true
-		spSetUnitBlocking(unitID, false)
+		if not noEject[builderDefID] then
+			SetTemporaryNoBlock(unitID, ghostFrames)
+		end
+		if pushDefs[unitDefID] then
+			GG.AddGadgetImpulseRaw(unitID, 0, 8, 0)
+		end
 	end
 end
