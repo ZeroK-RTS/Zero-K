7b4cb4f70edc8eff0b6968dfeafc6f91afd10e1b
Bill Tyros <me@strategineer.com>
2025-12-06 01:09:25 -0500

    Clean up more

--- LuaUI/Widgets/gui_chili_core_selector.lua
+++ LuaUI/Widgets/gui_chili_core_selector.lua
@@ -432,6 +432,10 @@ local function ConvertConIDToTransportIdCarryingItIfNeeded(unitID)
 	return unitID
 end
 
+local function IsConNotCarriedByEnemyTransport(unitID)
+	return consCarriedByEnemyTransports[unitID] == nil
+end
+
 -- comm selection functionality
 local commIndex = 1
 local function SelectComm()
@@ -461,11 +465,11 @@ local function SelectComm()
 		end
 	end
 	
-	unitID = ConvertConIDToTransportIdCarryingItIfNeeded(unitID)
 	local alt, ctrl, meta, shift = Spring.GetModKeyState()
-	Spring.SelectUnit(unitID, shift)
+	local tuid = ConvertConIDToTransportIdCarryingItIfNeeded(unitID)
+	Spring.SelectUnit(tuid, shift)
 	if not shift then
-		local x, y, z = Spring.GetUnitPosition(unitID)
+		local x, y, z = Spring.GetUnitPosition(tuid)
 		SetCameraTarget(x, y, z)
 	end
 end
@@ -513,10 +517,6 @@ local function SelectPrecBomber()
 	Spring.SelectUnitArray(toBeSelected)
 end
 
-local function IsConNotCarriedByEnemyTransport(unitID)
-	return consCarriedByEnemyTransports[unitID] == nil
-end
-
 local function SelectIdleCon_all()
 	local consToSelect = {}
 	for uid in pairs(idleCons) do
@@ -541,6 +541,7 @@ local function SelectIdleCon()
 		local pos = select(2, spTraceScreenRay(mx,my,true)) or mapMiddle
 		local mindist = math.huge
 		local muid = nil
+
 		for uid, v in pairs(idleCons) do
 			if uid ~= "count" then
 				tuid = ConvertConIDToTransportIdCarryingItIfNeeded(uid)
@@ -1860,12 +1861,6 @@ function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
 		idleTransports[unitID] = nil
 		wantUpdateCons = true
 	end
-	-- todo(strat) I don't think the block below is needed
-	--[[ -- transport can't be idle when it's destroyed
-	if idleTransports[unitID] then
-		idleTransports[unitID] = nil
-		wantUpdateCons = true
-	end ]]
 	if idleCons[unitID] then
 		idleCons[unitID] = nil
 		wantUpdateCons = true
