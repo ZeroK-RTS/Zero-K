4c0d4ab9e299d43e014b23aab335dff96a8045e7
GoogleFrog <googl3frog@gmail.com>
2025-12-13 21:51:32 +1100

    Some map settings.

--- LuaUI/Configs/MapSettingsOverride/Enclaves v1.lua
+++ LuaUI/Configs/MapSettingsOverride/Enclaves v1.lua
@@ -1,15 +1,15 @@
 return {
 	["water"]= false,
 	["sun"]= {
-		["groundDiffuseColor"]= {0.75, 0.75, 0.75, 1},
+		["groundDiffuseColor"]= {0.94999999, 0.88, 0.82999998, 1},
 		["groundAmbientColor"]= {0.5, 0.5, 0.5, 1},
-		["unitSpecularColor"]= {0.83417088, 0.81909549, 0.78391957, 1},
-		["modelShadowDensity"]= 0.2,
+		["unitSpecularColor"]= {0.76666665, 0.69999999, 0.40000001, 1},
+		["modelShadowDensity"]= 0.5,
 		["specularExponent"]= 30,
 		["groundSpecularColor"]= {0, 0, 0, 1},
-		["groundShadowDensity"]= 0.80000001,
-		["unitDiffuseColor"]= {1, 1, 1, 1},
-		["unitAmbientColor"]= {0.40000001, 0.40000001, 0.40000001, 1},
+		["groundShadowDensity"]= 0.5,
+		["unitDiffuseColor"]= {0.73809522, 0.69999999, 0.50476193, 1},
+		["unitAmbientColor"]= {0.97619045, 0.86666667, 0.73809522, 1},
 	},
 	["fog"]= {
 		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
@@ -19,9 +19,5 @@ return {
 		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
 		["fogEnd"]= 1,
 	},
-	["fixDefaultWater"]= true,
-	["direction"]= {
-		["sunDir"]= 5.16394758,
-		["sunPitch"]= 1.10000002,
-	},
+	["direction"]= false,
 }
--- LuaUI/Configs/MapSettingsOverride/StormSiege_v3.lua
+++ LuaUI/Configs/MapSettingsOverride/StormSiege_v3.lua
@@ -1,15 +1,15 @@
 return {
 	["water"]= false,
 	["sun"]= {
-		["groundDiffuseColor"]= {0.75, 0.75, 0.75, 1},
-		["groundAmbientColor"]= {0.5, 0.5, 0.5, 1},
-		["unitSpecularColor"]= {0.83417088, 0.81909549, 0.78391957, 1},
-		["modelShadowDensity"]= 0.2,
+		["groundDiffuseColor"]= {0.69999999, 0.69999999, 0.69999999, 1},
+		["groundAmbientColor"]= {0.40000001, 0.40000001, 0.40000001, 1},
+		["unitSpecularColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["modelShadowDensity"]= 0.80000001,
 		["specularExponent"]= 30,
 		["groundSpecularColor"]= {0, 0, 0, 1},
 		["groundShadowDensity"]= 0.80000001,
-		["unitDiffuseColor"]= {1, 1, 1, 1},
-		["unitAmbientColor"]= {0.40000001, 0.40000001, 0.40000001, 1},
+		["unitDiffuseColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
+		["unitAmbientColor"]= {0.57619047, 0.63333333, 0.63333333, 1},
 	},
 	["fog"]= {
 		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
@@ -19,9 +19,5 @@ return {
 		["skyColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
 		["fogEnd"]= 1,
 	},
-	["fixDefaultWater"]= true,
-	["direction"]= {
-		["sunDir"]= 5.16394758,
-		["sunPitch"]= 1.10000002,
-	},
+	["direction"]= false,
 }
