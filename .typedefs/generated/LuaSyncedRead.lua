fd1108c04188a09f4b69985aac2350e7fb786342
XNTEABDSC <2364400855@qq.com>
2025-12-31 23:09:33 +0800

    remove Spring.GetUnitDefID's nil return

--- .typedefs/generated/LuaSyncedRead.lua
+++ .typedefs/generated/LuaSyncedRead.lua
@@ -622,7 +622,7 @@ function Spring.GetUnitPosErrorParams(unitID, allyTeamID) end
 function Spring.GetUnitTooltip(unitID) end
 
 ---@param unitID UnitId
----@return UnitDefId?
+---@return UnitDefId
 function Spring.GetUnitDefID(unitID) end
 
 ---@param unitID UnitId
