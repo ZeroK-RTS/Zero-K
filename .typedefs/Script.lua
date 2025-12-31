43fb1f2105c50eb2424dbb0a483eeb4399bc4daa
XNTEABDSC <2364400855@qq.com>
2025-12-31 21:19:16 +0800

    .typedefs

---
+++ .typedefs/Script.lua
@@ -0,0 +1,16 @@
+
+
+
+---@meta
+
+
+Script={}
+
+---check whether ergine's version is higher than given
+---@param major number
+---@param minor number
+---@param commit number
+---@return boolean|nil
+function Script.IsEngineMinVersion(major,minor,commit)
+    
+end
