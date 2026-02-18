6bec28af5548ea7b0b651d4a1d8af2ae680ed436
MexxPex <88838894+MexxPex@users.noreply.github.com>
2026-02-19 00:21:00 +0100

    Refactor letsNotFailAtTrig.lua into a proper module

--- scripts/letsNotFailAtTrig.lua
+++ scripts/letsNotFailAtTrig.lua
@@ -1,46 +1,73 @@
--- TODO: CACHE INCLUDE FILE
 -- Only Krow and Jugglenaut use this
 local hpi = math.pi*0.5
 
 local cos = math.cos
 local sin = math.sin
 local acos = math.acos
-local asin = math.asin
+local sqrt = math.sqrt
 
-function rotateXaxis(v, angle)
-	return {v[1], v[2]*cos(angle) - v[3]*sin(angle), v[3]*cos(angle) + v[2]*sin(angle)}
+local trig = {}
+
+-- Vector Rotationeering
+function trig.rotateXaxis(v, angle)
+	return {
+		v[1],
+		v[2] * cos(angle) - v[3] * sin(angle),
+		v[3] * cos(angle) + v[2] * sin(angle),
+	}
 end
 
-function rotateYaxis(v, angle)
-	return {v[1]*cos(angle) + v[3]*sin(angle), v[2], v[3]*cos(angle) - v[1]*sin(angle)}
+function trig.rotateYaxis(v, angle)
+	return {
+		v[1] * cos(angle) + v[3] * sin(angle),
+		v[2],
+		v[3] * cos(angle) - v[1] * sin(angle),
+	}
 end
 
-function rotateZaxis(v, angle)
-	return {v[1]*cos(angle) - v[2]*sin(angle), v[2]*cos(angle) + v[1]*sin(angle), v[3]}
+function trig.rotateZaxis(v, angle)
+	return {
+		v[1] * cos(angle) - v[2] * sin(angle),
+		v[2] * cos(angle) + v[1] * sin(angle),
+		v[3],
+	}
 end
 
-function dot(v1, v2)
-	return v1[1]*v2[1] + v1[2]*v2[2] + v1[3]*v2[3]
+-- Vector Operations
+function trig.dot(v1, v2)
+	return v1[1] * v2[1] + v1[2] * v2[2] + v1[3] * v2[3]
 end
 
-function cross(v1, v2)
-	return {v1[2]*v2[3] - v1[3]*v2[2], v1[3]*v2[1] - v1[1]*v2[3], v1[1]*v2[2] - v1[2]*v2[1]}
+function trig.cross(v1, v2)
+	return {
+		v1[2] * v2[3] - v1[3] * v2[2],
+		v1[3] * v2[1] - v1[1] * v2[3],
+		v1[1] * v2[2] - v1[2] * v2[1],
+	}
 end
 
-function add(v1, v2)
-	return {v1[1]+v2[1], v1[2]+v2[2], v1[3]+v2[3]}
+function trig.add(v1, v2)
+	return {
+		v1[1] + v2[1],
+		v1[2] + v2[2],
+		v1[3] + v2[3],
+	}
 end
 
-function hat(v)
-	return mult(1/modulus(v),v)
+function trig.hat(v)
+	return trig.mult(1 / trig.modulus(v), v)
 end
 
-function modulus(v)
-	return math.sqrt(v[1]^2 + v[2]^2 + v[3]^2)
+function trig.modulus(v)
+	return sqrt(v[1]^2 + v[2]^2 + v[3]^2)
 end
 
-function mult(s, v)
-	return {v[1]*s, v[2]*s, v[3]*s}
+function trig.mult(s, v)
+	return {
+		v[1] * s,
+		v[2] * s,
+		v[3] * s,
+	}
 end
 
 -- Up is along y axis
@@ -57,19 +84,19 @@ end
 -- orthagonal is the component of the aim vector that is perpendicular to normal.
 -- theta is the angle from the radial vector, in the plane perpendicular to normal, to rotate the turret.
 
-function getTheActuallyCorrectHeadingAndPitch(heading, pitch, normal, radial, right) -- desired up, front and right vector, must be unit
+function trig.getTheActuallyCorrectHeadingAndPitch(heading, pitch, normal, radial, right) -- desired up, front and right vector, must be unit
 	local aim = {sin(heading)*cos(pitch), sin(pitch), cos(heading)*cos(pitch)}
-	local phi = acos(dot(aim, normal)) - hpi
+	local phi = acos(trig.dot(aim, normal)) - hpi
 	
-	local orthagonal = add(mult(-dot(aim,normal), normal), aim)
-	local modOtho = modulus(orthagonal)
+	local orthagonal = trig.add(trig.mult(-trig.dot(aim,normal), normal), aim)
+	local modOtho = trig.modulus(orthagonal)
 	if modOtho <= 0.0001 then
 		return 0, -hpi -- Fire straight up
 	end
-	local xFactor = dot(radial, orthagonal)/modOtho
+	local xFactor = trig.dot(radial, orthagonal)/modOtho
 	local theta = (xFactor >= 1 and 0) or (xFactor <= -1 and math.pi) or acos(xFactor)
 	
-	if dot(right, aim) < 0 then
+	if trig.dot(right, aim) < 0 then
 		theta = -theta
 	end
 	
@@ -77,3 +104,5 @@ function getTheActuallyCorrectHeadingAndPitch(heading, pitch, normal, radial, ri
 	--Spring.Echo((phi+hpi)*180/math.pi)
 	return theta, phi
 end
+
+return trig
