27d164b4088034a58cc4458fa38945c06b27f1b6
GoogleFrog <googl3frog@gmail.com>
2026-03-14 11:33:23 +1100

    BAR style luashader includes.

--- LuaRules/Utilities/globals.lua
+++ LuaRules/Utilities/globals.lua
@@ -1,1 +1,8 @@
 Spring.Utilities.CMD = VFS.Include("LuaRules/Configs/customcmds.lua", nil, VFS.GAME)
+
+-- Inserts
+-- gl.InstanceVBOTable = VFS.Include("modules/graphics/instancevbotable.lua")
+-- gl.InstanceVBOIdTable = VFS.Include("modules/graphics/instancevboidtable.lua")
+-- gl.LuaShader = VFS.Include("modules/graphics/LuaShader.lua")
+-- gl.R2tHelper = VFS.Include("modules/graphics/r2thelper.lua")
+VFS.Include("modules/graphics/init.lua").Init(gl)
