19c7f17ef11c5b47ef12610ad995448804dac681
GoogleFrog <googl3frog@gmail.com>
2026-05-23 11:16:37 +1000

    Work around setting flight height while landing causing planes to hover mid air. Fixes #5673

--- scripts/fixedwingTakeOff.lua
+++ scripts/fixedwingTakeOff.lua
@@ -8,21 +8,35 @@ function GG.TakeOffFuncs.NotTakingOff(unitID)
 	return state and (state.aircraftState ~= "takeoff")
 end
 
+function GG.TakeOffFuncs.NotLanding(unitID)
+	local state = Spring.GetUnitMoveTypeData(unitID)
+	return state and (state.aircraftState ~= "landing")
+end
+
+
 function GG.TakeOffFuncs.TakeOffThread(unitID, height, signal)
 	local FUDGE_FACTOR = 1.5
 	
 	Signal(signal)
 	SetSignalMask(signal)
+	local giveUp = 20
 	while GG.TakeOffFuncs.NotTakingOff(unitID) do
-		Sleep(1000)
+		Sleep(100)
+		giveUp = giveUp - 1
+		if giveUp <= 0 then
+			return
+		end
 	end
+	Spring.MoveCtrl.SetAirMoveTypeData(unitID, "wantedHeight", height*FUDGE_FACTOR)
 	for i = 1, 5 do
 		Sleep(100)
 		if not Spring.ValidUnitID(unitID) then
 			return
 		end
 		if not Spring.MoveCtrl.GetTag(unitID) then
-			Spring.MoveCtrl.SetAirMoveTypeData(unitID, "wantedHeight", 10)
+			if GG.TakeOffFuncs.NotLanding(unitID) then
+				Spring.MoveCtrl.SetAirMoveTypeData(unitID, "wantedHeight", 10)
+			end
 			Sleep(33)
 		end
 		while Spring.MoveCtrl.GetTag(unitID) do
@@ -31,6 +45,8 @@ function GG.TakeOffFuncs.TakeOffThread(unitID, height, signal)
 		if not Spring.ValidUnitID(unitID) then
 			return
 		end
-		Spring.MoveCtrl.SetAirMoveTypeData(unitID, "wantedHeight", height*FUDGE_FACTOR)
+		if GG.TakeOffFuncs.NotLanding(unitID) then
+			Spring.MoveCtrl.SetAirMoveTypeData(unitID, "wantedHeight", height*FUDGE_FACTOR)
+		end
 	end
 end
