4bb918d9f5ce95dd4d387222ed8a9eaf78d16a0e
GoogleFrog <googl3frog@gmail.com>
2026-07-11 14:57:24 +1000

    Improve sun on Otago 1.4 and Rainbow Comet v1.25.

--- LuaUI/Configs/MapSettingsOverride/Rainbow Comet v1.25.lua
+++ LuaUI/Configs/MapSettingsOverride/Rainbow Comet v1.25.lua
@@ -1,15 +1,15 @@
 return {
 	["defaultSunDir"]= false,
 	["sun"]= {
-		["groundDiffuseColor"]= {1, 0.75, 0.80000001, 1},
-		["groundAmbientColor"]= {0.94, 0.63999999, 0.74000001, 1},
-		["unitSpecularColor"]= {1, 0.75, 0.80000001, 1},
+		["groundDiffuseColor"]= {0.5, 0.5, 0.5, 1},
+		["groundAmbientColor"]= {0.74874371, 0.73869348, 0.74874371, 1},
+		["unitSpecularColor"]= {0.69999999, 0.69999999, 0.69999999, 1},
 		["modelShadowDensity"]= 0.75,
 		["specularExponent"]= 30,
 		["groundSpecularColor"]= {0, 0, 0, 1},
 		["groundShadowDensity"]= 0.75,
-		["unitDiffuseColor"]= {0.78095239, 0.71428573, 0.7095238, 1},
-		["unitAmbientColor"]= {0.70476192, 0.6047619, 0.59523809, 1},
+		["unitDiffuseColor"]= {0.89999998, 0.89999998, 0.89999998, 1},
+		["unitAmbientColor"]= {0.68341708, 0.67839193, 0.67839193, 1},
 	},
 	["fog"]= {
 		["sunColor"]= {0.80000001, 0.80000001, 0.80000001, 1},
@@ -20,7 +20,10 @@ return {
 		["fogEnd"]= 1,
 	},
 	["water"]= false,
-	["fixDefaultWater"]= true,
-	["direction"]= false,
+	["fixDefaultWater"]= false,
+	["direction"]= {
+		["sunDir"]= 3.81999993,
+		["sunPitch"]= 0.64999998,
+	},
 	["skipShadowDensityFix"]= false,
 }
